f65f19af70a1e6bdc99fc4e1b3c2440c44418ffc
yl <ylstrive@163.com>
2022-07-29 14:22:26 +0800

    2022-07-29 yl 添加公共的 常量 文件

---
+++ erp-common/erp-common-core/src/main/java/com/commm/core/constant/ThirdConstants.java
@@ -0,0 +1,32 @@
+package com.commm.core.constant;
+
+/**
+ * @Classname 第三方常量
+ * @Description TODO
+ * @Date 2022-07-20 18:09
+ * @Created by yl
+ */
+public interface ThirdConstants {
+
+    String FS_GRANT_TYPE="authorization_code";
+
+
+    String FS_TOKEN_URL="https://passport.feishu.cn/suite/passport/oauth/token";
+
+    String FS_USER_URL="https://passport.feishu.cn/suite/passport/oauth/userinfo";
+
+    String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+    //飞书
+    String FS_PLATFORM="FS";
+
+    //钉钉
+    String DD_PLATFORM="DD";
+
+
+    String THIRD_BINDING_TYPE="binding";
+
+
+    String THIRD_LOGIN_TYPE="login";
+
+}
