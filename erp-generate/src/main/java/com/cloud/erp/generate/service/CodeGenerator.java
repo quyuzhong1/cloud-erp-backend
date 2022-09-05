a2ac32df6dbc90f5b139f89c1515688c72d39efe
yl <ylstrive@163.com>
2022-09-05 11:31:43 +0800

    2022-09-05 yl 修改 nacos 地址

--- erp-generate/src/main/java/com/cloud/erp/generate/service/CodeGenerator.java
+++ erp-generate/src/main/java/com/cloud/erp/generate/service/CodeGenerator.java
@@ -55,7 +55,7 @@ public class CodeGenerator {
         // 全局变量配置
         GlobalConfig gc = new GlobalConfig();
         String projectPath = System.getProperty("user.dir"); //当前项目(写死无需更改！！！)
-        gc.setOutputDir(projectPath + "/erp-admin/src/main/java"); // 输出路径(写死无需更改！！！)
+        gc.setOutputDir(projectPath + "/erp-chrome/src/main/java"); // 输出路径(写死无需更改！！！)
         gc.setFileOverride(true); // 默认 false ,是否覆盖已生成文件
         gc.setOpen(false); //默认true ,是否打开输出目录
         gc.setEnableCache(false); // 默认false,是否开启二级缓存
@@ -69,7 +69,7 @@ public class CodeGenerator {
         gc.setMapperName("%sMapper");
         gc.setXmlName("%sMapper");
         gc.setEntityName("%sEntity");
-        gc.setIdType(IdType.ASSIGN_ID); // 指定生成的主键类型
+        gc.setIdType(IdType.AUTO); // 指定生成的主键类型
         generator.setGlobalConfig(gc);
 
         // =============想要生成对应代码的数据表所在数据库配置===========
@@ -78,7 +78,7 @@ public class CodeGenerator {
         dc.setDbQuery(new PostgreSqlQuery()); // 数据库信息查询 //默认mysql
         dc.setDbType(DbType.POSTGRE_SQL);// 数据库类型
         dc.setTypeConvert(new PostgreSqlTypeConvert()); //类型转换 默认mysql
-        dc.setUrl("jdbc:postgresql://172.16.105.216:5432/erp-sys?useSSL=false&serverTimezone=GMT%2B8");
+        dc.setUrl("jdbc:postgresql://192.168.31.86:5432/erp-chrome?useSSL=false&serverTimezone=GMT%2B8");
         dc.setDriverName("org.postgresql.Driver");
         dc.setUsername("postgres");
         dc.setPassword("admin@viji");
@@ -88,7 +88,7 @@ public class CodeGenerator {
         // ==================代码生成所在包的位置======================
         PackageConfig pc = new PackageConfig();
         // 你的包名在这里指定(生成的实体类等java代码保存的位置由这个决定)
-        pc.setParent("com.cloud.erp.admin.modules.sys");//代码生成到哪个包下面
+        pc.setParent("com.cloud.erp.chrome");//代码生成到哪个包下面
 //        pc.setModuleName(""); //此处是所属模块名称
         generator.setPackageInfo(pc);
         // 自定义配置
@@ -111,7 +111,7 @@ public class CodeGenerator {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath +"/erp-admin"+"/src/main/resources/mapper/"
+                return projectPath +"/erp-chrome"+"/src/main/resources/mapper/"
                          + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
