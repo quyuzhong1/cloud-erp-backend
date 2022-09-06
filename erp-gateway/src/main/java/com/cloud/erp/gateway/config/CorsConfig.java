52dc59fb6fb264cb8f2b6da8fc47ca80a797b13d
yl <ylstrive@163.com>
2022-09-06 14:08:43 +0800

    2022-09-06 yl 修改 pom 文件

--- erp-gateway/src/main/java/com/cloud/erp/gateway/config/CorsConfig.java
+++ erp-gateway/src/main/java/com/cloud/erp/gateway/config/CorsConfig.java
@@ -4,8 +4,9 @@ package com.cloud.erp.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 /**
  * @Classname CorsConfig
@@ -17,24 +18,19 @@ import org.springframework.web.filter.CorsFilter;
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsWebFilter(){
-        //1. 添加 CORS配置信息
-        CorsConfiguration config = new CorsConfiguration();
-        //放行哪些原始域
-        config.addAllowedOrigin("*");
-        //是否发送 Cookie
-        config.setAllowCredentials(true);
-        //放行哪些请求方式
-        config.addAllowedMethod("*");
-        //放行哪些原始请求头部信息
-        config.addAllowedHeader("*");
-        //暴露哪些头部信息
-        config.addExposedHeader("*");
-        //2. 添加映射路径
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**",config);
-        //3. 返回新的CorsFilter
-        return new CorsFilter(corsConfigurationSource);
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        //跨域配置
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //支持哪些来源的请求跨域
+        corsConfiguration.addAllowedOrigin("*");
+
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        //可以让所有的请求 来访问
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
 
     }
 }
