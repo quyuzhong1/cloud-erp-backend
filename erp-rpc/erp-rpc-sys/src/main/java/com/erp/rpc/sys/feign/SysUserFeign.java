baf793cd094dc2c0ba9717eb8e00ea2a919f591d
yl <ylstrive@163.com>
2022-09-07 16:10:49 +0800

    2022-09-07 yl 修改已知的bug

--- erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysUserFeign.java
+++ erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysUserFeign.java
@@ -15,20 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody;
  * @Date 2022-07-08 16:52
  * @Created by yl
  */
-@FeignClient("erp-admin")
+@FeignClient("erp-sys")
 public interface SysUserFeign {
 
     //账号登录
-    @PostMapping("sys/feign/user/accountLogin")
+    @PostMapping("sys/admin/feign/user/accountLogin")
     ApiResult<SysUserDTO> accountLogin(@RequestBody AccountLoginDTO loginDTO);
 
 
 
     //设置登录ip账号登录
-    @PostMapping("sys/feign/user/setLoginIp")
+    @PostMapping("sys/admin/feign/user/setLoginIp")
     ApiResult<SysUserDTO> setLoginIp(@RequestBody SysLoginIpDTO loginDTO);
 
     //扫码登录
-    @PostMapping("sys/feign/user/scanCodeLogin")
+    @PostMapping("sys/admin/feign/user/scanCodeLogin")
     ApiResult<SysUserDTO> scanCodeLogin(@RequestBody SysUserThirdDTO loginDTO);
 }
