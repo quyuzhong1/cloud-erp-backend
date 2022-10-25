168bc8a034a208b92e260fa0c1923082997f3067
luoweigang <821969511@qq.com>
2022-10-25 16:07:54 +0800

    修改品名修改

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -317,6 +317,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
 
         //2.修改/新增 sku信息
         ProductSkuBaseInfoDTO productSkuBaseInfoDTO = productNoSpecDTO.getProductBaseInfoDTO().getProductSkuBaseInfoDTO();
+
         productSkuBaseInfoDTO.setProductId(id);
         //如果是修改sku图片 还需要修改图片表
         if (StringUtils.isNotBlank(productSkuBaseInfoDTO.getImagesUrl())) {
