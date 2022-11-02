aa4d59c889ee0259d73fb2bc60f59a33ea57f446
yl <ylstrive@163.com>
2022-11-02 18:23:08 +0800

    2022-11-02 Lambda 更改 处理任务情况

--- erp-rpc/erp-rpc-plm/src/main/java/com/erp/rpc/plm/feign/PlmTaskFeign.java
+++ erp-rpc/erp-rpc-plm/src/main/java/com/erp/rpc/plm/feign/PlmTaskFeign.java
@@ -18,5 +18,5 @@ public interface PlmTaskFeign {
 
     //流程审核通过 改变任务状态
     @PostMapping("plm/task/feign/process/pass")
-    void processPass(@RequestParam(value="processId") String processId);
+    void processPass(@RequestBody String processId);
 }
