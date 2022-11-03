144c62e21f48aa81847171a38241196ccf1919d1
yl <ylstrive@163.com>
2022-11-03 16:35:33 +0800

    2022-11-03 修复 工作流超时的问题

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/ProjectTaskController.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/ProjectTaskController.java
@@ -309,11 +309,13 @@ public class ProjectTaskController extends BaseController {
      * 以及
      * @return
      */
-    @PostMapping("/feign/process/pass")
+    @PostMapping("/workflow/pass")
     public ApiResult processPass(@RequestBody String processId) {
         taskService.approvalTaskPass(processId);
         return success();
     }
+
+
 
 
 }
