3610898f5993e6023e2ef88f18c6dfc8e1c1bb25
yl <ylstrive@163.com>
2022-11-04 10:15:25 +0800

    2022-11-03 系统通用设置计划开始时间显示问题

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/entity/ProjectTaskSysEntity.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/entity/ProjectTaskSysEntity.java
@@ -1,16 +1,11 @@
 package com.erp.model.plm.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
-import java.util.Date;
-
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.util.Date;
 
 /**
  * <p>
@@ -63,13 +58,13 @@ public class ProjectTaskSysEntity implements Serializable {
     /**
      * 计划开始时间
      */
-    @TableField("plan_start_time")
+    @TableField(value = "plan_start_time" ,insertStrategy = FieldStrategy.IGNORED ,updateStrategy = FieldStrategy.IGNORED)
     private Date planStartTime;
 
     /**
      * j计划结束时间
      */
-    @TableField("plan_end_time")
+    @TableField(value = "plan_end_time" ,insertStrategy =FieldStrategy.IGNORED ,updateStrategy = FieldStrategy.IGNORED)
     private Date planEndTime;
 
     /**
