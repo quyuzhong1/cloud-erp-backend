0b6cd287c3b31ac54be50b48156c8adf47f43eb2
Will <wangwei@ulanzi.cn>
2022-12-15 18:19:54 +0800

    添加成本明细和自定义明细

---
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/BiDataSourceCostDetailMapper.java
@@ -0,0 +1,15 @@
+package com.erp.server.bi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.erp.model.dmp.entity.BiDataSourceCostDetailEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author Will
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/12/15 18:16
+ */
+@Mapper
+public interface BiDataSourceCostDetailMapper extends BaseMapper<BiDataSourceCostDetailEntity> {
+}
---
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/BiDataSourceCustomDetailService.java
@@ -0,0 +1,13 @@
+package com.erp.server.bi.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.erp.model.dmp.entity.BiDataSourceCustomDetailEntity;
+
+/**
+ * @author Will
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/12/15 18:15
+ */
+public interface BiDataSourceCustomDetailService  extends IService<BiDataSourceCustomDetailEntity> {
+}
