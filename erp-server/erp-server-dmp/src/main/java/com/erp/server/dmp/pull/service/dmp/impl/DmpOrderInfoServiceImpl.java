646a8e172431f3b359f7d1fe05ecad60e6ec3f46
Cloud <cloud@ulanzi.cn>
2022-12-15 15:41:19 +0800

    add date

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/dmp/impl/DmpOrderInfoServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/dmp/impl/DmpOrderInfoServiceImpl.java
@@ -16,6 +16,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 /**
