cc4eff11c623a8b25d3e7fcefe9617f9b5482f67
yl <ylstrive@163.com>
2022-12-15 16:38:58 +0800

    Lambda 2022-12-15 bi 添加远程调用注解

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/ErpServerBiApplication.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/ErpServerBiApplication.java
@@ -2,8 +2,12 @@ package com.erp.server.bi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages ={"com.erp.*"})
+@EnableFeignClients(basePackages = {"com.erp.rpc.*"})
+@EnableDiscoveryClient
 public class ErpServerBiApplication {
 
     public static void main(String[] args) {
