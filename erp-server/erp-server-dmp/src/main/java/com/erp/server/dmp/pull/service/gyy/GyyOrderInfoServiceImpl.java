9ed056774c263a904c3f105f030a560990eabb39
luoweigang <821969511@qq.com>
2022-12-16 19:15:58 +0800

    修改管易云拉取数据

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/gyy/GyyOrderInfoServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/gyy/GyyOrderInfoServiceImpl.java
@@ -177,6 +177,7 @@ public class GyyOrderInfoServiceImpl implements IReportSaveService {
             datas.put("appkey", gyyAppEntity.getAppKey());
             datas.put("sessionkey", gyyAppEntity.getSessionKey());
             datas.put("date_type", 3);
+            datas.put("order_state", 2);
             datas.put("start_date", st);
             datas.put("end_date", sd);
             datas.put("page_no", pageIndex);
