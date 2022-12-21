bfe05d066ea3601ae372ec6d9c84c1ddf77d4e9f
yl <ylstrive@163.com>
2022-12-21 12:29:26 +0800

    Lambda 2022-12-21 bi 模块API

---
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/constant/ChartType.java
@@ -0,0 +1,14 @@
+package com.erp.server.bi.constant;
+
+/**
+ * @Classname ChartType
+ * @Description TODO
+ * @Date 2022-12-21 9:19
+ * @Created by yl
+ */
+public interface ChartType {
+
+    String PIE="pie";
+
+    String BAR="bar";
+}
--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/controller/BiSalesModuleController.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/controller/BiSalesModuleController.java
@@ -1,12 +1,11 @@
 package com.erp.server.bi.controller;
 
-import com.erp.common.business.annotation.DataPermission;
 import com.erp.common.controller.BaseController;
 import com.erp.common.dto.base.ApiResult;
-import com.erp.common.enums.DataAttributeEnum;
 import com.erp.model.bi.dto.BiFilterDTO;
 import com.erp.model.bi.vo.SalesByCountryVO;
 import com.erp.model.bi.vo.SalesVO;
+import com.erp.model.bi.vo.ShopSalesVO;
 import com.erp.model.bi.vo.StatisticalDataVO;
 import com.erp.server.bi.service.SalesOrderService;
 import org.springframework.validation.annotation.Validated;
@@ -38,11 +37,11 @@ public class BiSalesModuleController extends BaseController {
      * @return
      */
     @GetMapping("/byMonth")
-    @DataPermission(operationType = DataAttributeEnum.LIST,
-            tableField = "charge_id",
-            menuCode = "bi:sales:byMonth",
-            tableAlias = "dmp_order_info"
-    )
+//    @DataPermission(operationType = DataAttributeEnum.LIST,
+//            tableField = "charge_id",
+//            menuCode = "bi:sales:byMonth",
+//            tableAlias = "dmp_order_info"
+//    )
     public ApiResult<StatisticalDataVO> getMonth() {
         StatisticalDataVO statistical = salesOrderService.getMonthSales();
         return success(statistical);
@@ -97,6 +96,58 @@ public class BiSalesModuleController extends BaseController {
     public ApiResult<List<SalesByCountryVO>> getByCountry(@RequestBody @Validated BiFilterDTO dto) {
         List<SalesByCountryVO> resultList = salesOrderService.getByCountry(dto);
         return success(resultList);
+    }
+
+
+    /**
+     * 销售相关-一级模块-平台销售额
+     * @param dto
+     * @return
+     */
+    @PostMapping("/byPlatform")
+//    @DataPermission(operationType = DataAttributeEnum.LIST,
+//            tableField = "charge_id",
+//            menuCode = "bi:sales:byCountry",
+//            tableAlias = "o"
+//    )
+    public ApiResult<StatisticalDataVO> byPlatform(@RequestBody @Validated BiFilterDTO dto) {
+        StatisticalDataVO result = salesOrderService.getByPlatform(dto);
+        return success(result);
+    }
+
+
+
+    /**
+     * 销售相关-一级模块-店铺销售额
+     * @param dto
+     * @return
+     */
+    @PostMapping("/byShop")
+//    @DataPermission(operationType = DataAttributeEnum.LIST,
+//            tableField = "charge_id",
+//            menuCode = "bi:sales:byCountry",
+//            tableAlias = "o"
+//    )
+    public ApiResult<List<ShopSalesVO>> byShop(@RequestBody @Validated BiFilterDTO dto) {
+        List<ShopSalesVO> resultList = salesOrderService.getByShop(dto);
+        return success(resultList);
+    }
+
+
+    /**
+     * 销售相关-一级模块-店铺销售额
+     * @param dto
+     * @return
+     */
+    @PostMapping("/byTopShop")
+//    @DataPermission(operationType = DataAttributeEnum.LIST,
+//            tableField = "charge_id",
+//            menuCode = "bi:sales:byCountry",
+//            tableAlias = "o"
+//    )
+    public ApiResult<StatisticalDataVO> byTopShop(@RequestBody @Validated BiFilterDTO dto) {
+        StatisticalDataVO result = salesOrderService.byTopShop(dto);
+        return success(result);
     }
 
 
