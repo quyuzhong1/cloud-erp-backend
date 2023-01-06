9ae05552efac6f8faad919e3c97afbe96317c158
yl <ylstrive@163.com>
2023-01-06 18:45:34 +0800

    Lambda 2023-01-06 修改已知bug

--- erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/LayoutDetailsDTO.java
+++ erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/LayoutDetailsDTO.java
@@ -41,7 +41,7 @@ public class LayoutDetailsDTO  implements Serializable {
      * 块的编号
      */
     @NotBlank(message = "模块编号不能为空")
-    @StateEnumValue(strValues = {"L01","L02","L04","L06"},message = "模块编号有误")
+    @StateEnumValue(strValues = {"L01","L02","L03","L04","L06"},message = "模块编号有误")
     private String blockNo;
 
 
