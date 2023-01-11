78e98a6128e2e42e5fbb9301e9a3070f7fa6cf36
Cloud <cloud@ulanzi.cn>
2023-01-11 12:12:44 +0800

    【 not empty 】

--- erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/SkuDateFilterDTO.java
+++ erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/SkuDateFilterDTO.java
@@ -3,6 +3,7 @@ package com.erp.model.bi.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -11,13 +12,13 @@ public class SkuDateFilterDTO extends BiFilterDTO {
     /**
      * sku
      */
-    @NotNull(message = "sku不能为空")
+    @NotEmpty(message = "sku不能为空")
     private String skuNo;
 
     /**
      * 时间类型：日: DAY; 周: WEEK; 月: MONTH;
      */
-    @NotNull(message = "时间类型不能为空")
+    @NotEmpty(message = "时间类型不能为空")
     private String dateType;
 
 }
