7a04482ee0a8273cff7c4660eacbac22af70d70e
Will <wangwei@ulanzi.cn>
2023-01-31 18:32:20 +0800

    搜索条件调整

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/TaskPagingDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/TaskPagingDTO.java
@@ -19,20 +19,16 @@ import java.util.List;
 @NoArgsConstructor
 public class TaskPagingDTO extends PermissionsDTO implements Serializable {
 
-
-     //产品id
      /**
       * 产品id
       */
      @NotBlank(message = "产品id不能为空")
      private String productId;
 
-
      /**
       * 阶段id 如果没有就是全部
       */
      private String phaseId;
-
 
      /**
       * 任务类型
@@ -43,20 +39,15 @@ public class TaskPagingDTO extends PermissionsDTO implements Serializable {
      @StateEnumValue(intValues = {0,1,2}, message = "任务类型有误")
      private Integer taskFlag;
 
-
-     //
      /**
-      * 筛选条件
+      * 高级搜索筛选条件
       */
-     List<TaskSearchDTO>  searchList;
-
+     TaskSearchDTO  taskSearchDTO;
 
      /**
       * 搜索关键字
       */
      private String searchKeyword;
-
-
 
      /**
       *  状态值 0:待发布 1:未开始 2:进行中 3 已完成, 4.完成待确认 5.审核中  6 审核通过 7 审核不通过
