81cb6942b389303570e53566b1d32d50769fb1d0
yl <ylstrive@163.com>
2023-02-15 19:04:16 +0800

    2023-02-15 Lambda 修复 变更 已知bug

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductChangeServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductChangeServiceImpl.java
@@ -33,6 +33,7 @@ import com.erp.server.plm.controller.AuditParamDTO;
 import com.erp.server.plm.enums.ProductChangeStateEnum;
 import com.erp.server.plm.mapper.ProductChangeMapper;
 import com.erp.server.plm.service.*;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +53,7 @@ import java.util.stream.Collectors;
  * @since 2023-01-11 14:05:03
  */
 @Service
+@Slf4j
 public class ProductChangeServiceImpl extends ServiceImpl<ProductChangeMapper, ProductChangeEntity> implements ProductChangeService {
 
 
@@ -721,11 +723,12 @@ public class ProductChangeServiceImpl extends ServiceImpl<ProductChangeMapper, P
         if (StringUtils.isNotBlank(id)) {
             //获取到变更信息
             ProductChangeEntity change = this.getById(id);
-            String type = change.getType();
-            change.setApprovalFinishTime(new Date());
-            change.setState(ProductChangeStateEnum.AUDIT_PASS.getState());
-            this.updateById(change);
             if (change != null) {
+                String type = change.getType();
+                change.setApprovalFinishTime(new Date());
+                change.setState(ProductChangeStateEnum.AUDIT_PASS.getState());
+                log.info("change.State ==",change.getState());
+                this.updateById(change);
                 //获取到对应的 json
                 String detailsJson = changeDetailsService.getDetailsJson(change.getId());
                 if (StringUtils.isNotBlank(detailsJson)) {
