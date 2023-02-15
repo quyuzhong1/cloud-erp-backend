fef5e451a31e70707639d854bc9d2c93f11ac959
yl <ylstrive@163.com>
2023-02-15 15:17:16 +0800

    2023-02-15 Lambda 修复 变更 已知bug

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/CommonServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/CommonServiceImpl.java
@@ -35,7 +35,7 @@ public class CommonServiceImpl implements CommonService {
      */
     @Override
     public LoginUser getUserInfo() {
-        String userId = "1610806688959561729";
+        String userId = "";
         String userName = "";
         LoginUser loginUser = CommonInterceptor.threadLocal.get();
         if (Objects.isNull(loginUser)) {
