58a2fcdb3a25b231ba1ea58c5f3f50a860ed29c6
Will <wangwei@ulanzi.cn>
2023-02-20 10:07:10 +0800

    导入验证调整

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/listener/ProductDetailExcelListener.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/listener/ProductDetailExcelListener.java
@@ -4,7 +4,6 @@ import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.common.core.utils.BeanMapper;
-import com.common.core.utils.FieldValidUtil;
 import com.common.core.utils.StrUtils;
 import com.erp.common.enums.ApiError;
 import com.erp.common.modules.sys.dto.FindUserDTO;
