577c5215e850607bc2037ac014f19870356cf61a
Will <wangwei@ulanzi.cn>
2023-02-28 12:14:25 +0800

    上传交付物调整

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/TaskDocsFinishServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/TaskDocsFinishServiceImpl.java
@@ -1,6 +1,6 @@
 package com.erp.server.plm.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
