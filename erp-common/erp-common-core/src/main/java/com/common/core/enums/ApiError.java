d548e5b99d3dc65893cfd730817d5314cfbf3a19
Will <wangwei@ulanzi.cn>
2023-03-07 19:34:41 +0800

    上传交付物名称长度限制

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -49,6 +49,7 @@ public enum ApiError implements Serializable {
     ERROR_1015(1015, "导出失败"),
     ERROR_1016(1016, "导入文件格式错误，请使用xlsx文件"),
     ERROR_1017(1017, "参数列表不能为空"),
+    ERROR_1018(1018, "导入名称不能大于200字符"),
     /**
      * 警告信息 从800 开始
      */
