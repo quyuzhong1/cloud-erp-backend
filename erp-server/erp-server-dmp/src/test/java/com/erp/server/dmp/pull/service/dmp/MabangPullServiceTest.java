39515ced180fde1a2379ac53e9d2d4fc879bc662
Cloud <cloud@ulanzi.cn>
2023-03-21 18:24:03 +0800

    [ update bug  ]
    (cherry picked from commit fbfa060c6e92297c093fd3008cf9adff93e7fa57)

--- erp-server/erp-server-dmp/src/test/java/com/erp/server/dmp/pull/service/dmp/MabangPullServiceTest.java
+++ erp-server/erp-server-dmp/src/test/java/com/erp/server/dmp/pull/service/dmp/MabangPullServiceTest.java
@@ -43,8 +43,8 @@ public class MabangPullServiceTest {
         jobTaskDTO.setApiName("获取订单列表");
         jobTaskDTO.setId(30L);
         jobTaskDTO.setIntervalTime(1800);
-        jobTaskDTO.setLastTime(LocalDateTime.parse("2022-02-10 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-        jobTaskDTO.setNextTime(LocalDateTime.parse("2022-02-11 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        jobTaskDTO.setLastTime(LocalDateTime.parse("2022-10-10 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        jobTaskDTO.setNextTime(LocalDateTime.parse("2022-10-11 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
         jobTaskDTO.setPlatformId(1);
         jobTaskDTO.setState(1);
         RequestDTO requestDTO = new RequestDTO();
