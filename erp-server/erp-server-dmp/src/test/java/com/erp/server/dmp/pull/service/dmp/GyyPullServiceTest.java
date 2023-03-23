c97fbf6569b617664a865b1f348a27a7cab51eed
Cloud <cloud@ulanzi.cn>
2023-03-23 19:17:44 +0800

    [  订单时间修改]
    (cherry picked from commit d5b8d5d5c928d6c689eb1dcc906c24f3d7c3a44d)

--- erp-server/erp-server-dmp/src/test/java/com/erp/server/dmp/pull/service/dmp/GyyPullServiceTest.java
+++ erp-server/erp-server-dmp/src/test/java/com/erp/server/dmp/pull/service/dmp/GyyPullServiceTest.java
@@ -131,6 +131,55 @@ public class GyyPullServiceTest {
     }
 
     @Test
+    public void pullRefundSalesTest(){
+        GyyRefundServiceImpl gyyOrderInfoService = new GyyRefundServiceImpl();
+        PlatformApiEnum platformApiEnum = PlatformApiEnum.GY_ERP_TRADE_REFUND_GET;
+        JobTaskDTO jobTaskDTO = new JobTaskDTO();
+        jobTaskDTO.setApiCode(platformApiEnum.getTaskName());
+        jobTaskDTO.setApiId(7);
+        jobTaskDTO.setApiName("管易云查询订单列表");
+        jobTaskDTO.setId(32L);
+        jobTaskDTO.setIntervalTime(1800);
+        jobTaskDTO.setLastTime(LocalDateTime.of(2022, 4, 8, 8, 0));
+        jobTaskDTO.setNextTime(LocalDateTime.of(2022, 4, 8, 10, 0));
+        jobTaskDTO.setPlatformId(1);
+        jobTaskDTO.setState(1);
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setPlatformApiEnum(platformApiEnum);
+        requestDTO.setJobTaskDTO(jobTaskDTO);
+        try {
+            gyyOrderInfoService.pullDataSave(requestDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    @Test
+    public void pullReturnSalesTest(){
+        GyyReturnOrderInfoServiceImpl gyyOrderInfoService = new GyyReturnOrderInfoServiceImpl();
+        PlatformApiEnum platformApiEnum = PlatformApiEnum.GY_ERP_TRADE_RETURN_GET;
+        JobTaskDTO jobTaskDTO = new JobTaskDTO();
+        jobTaskDTO.setApiCode(platformApiEnum.getTaskName());
+        jobTaskDTO.setApiId(7);
+        jobTaskDTO.setApiName("管易云查询订单列表");
+        jobTaskDTO.setId(32L);
+        jobTaskDTO.setIntervalTime(1800);
+        jobTaskDTO.setLastTime(LocalDateTime.of(2022, 4, 8, 8, 0));
+        jobTaskDTO.setNextTime(LocalDateTime.of(2022, 4, 8, 10, 0));
+        jobTaskDTO.setPlatformId(1);
+        jobTaskDTO.setState(1);
+        RequestDTO requestDTO = new RequestDTO();
+        requestDTO.setPlatformApiEnum(platformApiEnum);
+        requestDTO.setJobTaskDTO(jobTaskDTO);
+        try {
+            gyyOrderInfoService.pullDataSave(requestDTO);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
     public void pullKingdeeDeliveryTest(){
         GyyDeliveryDetailServiceImpl gyyOrderInfoService = new GyyDeliveryDetailServiceImpl();
         PlatformApiEnum platformApiEnum = PlatformApiEnum.SAL_OUTSTOCK;
