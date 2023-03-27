701dc2c062929fd30039fd1b35193f41b5afa6b6
Will <wangwei@ulanzi.cn>
2023-03-27 19:43:04 +0800

    查询条件调整

---
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ArrivalStatusEnum.java
@@ -0,0 +1,48 @@
+package com.erp.model.scm.enums;
+
+/**
+ * @author Will
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/3/27 19:13
+ */
+public enum ArrivalStatusEnum {
+
+    NON_ARRIVAL("1", "未到货"),
+    ARRIVED("2", "已到货"),
+    PARTIAL_ARRIVAL("3", "部分到货");
+
+    private String code;
+    private String name;
+
+    ArrivalStatusEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public static String getNameByCode(String code) {
+        ArrivalStatusEnum[] stateEnums = values();
+        for (ArrivalStatusEnum stateEnum : stateEnums) {
+            if (stateEnum.getCode().equals(code) ) {
+                return stateEnum.getName();
+            }
+        }
+        return "";
+    }
+
+}
