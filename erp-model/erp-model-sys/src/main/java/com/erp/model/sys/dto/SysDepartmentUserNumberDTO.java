800b2ad529b1e506949c00a3eb1bbcd711d7af50
Will <wangwei@ulanzi.cn>
2023-03-27 12:22:56 +0800

    部门接口

--- erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/SysDepartmentUserNumberDTO.java
+++ erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/SysDepartmentUserNumberDTO.java
@@ -1,5 +1,6 @@
 package com.erp.model.sys.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.io.Serializable;
  */
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class SysDepartmentUserNumberDTO implements Serializable {
 
 
