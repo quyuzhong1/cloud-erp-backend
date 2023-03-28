9789d6106b7d836e7965aa48cce8ec50b181be55
yl <ylstrive@163.com>
2023-03-28 18:38:00 +0800

    Lambda 2023-03-28 采购价目变更 代码提交

---
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchasePriceHistoryServiceImpl.java
@@ -0,0 +1,20 @@
+package com.erp.server.scm.service.impl;
+
+import com.common.business.service.SuperServiceImpl;
+import com.erp.model.scm.entity.PurchasePriceHistoryEntity;
+import com.erp.server.scm.mapper.PurchasePriceHistoryMapper;
+import com.erp.server.scm.service.PurchasePriceHistoryService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author Lambda
+ * @since 2023-03-28
+ */
+@Service
+public class PurchasePriceHistoryServiceImpl extends SuperServiceImpl<PurchasePriceHistoryMapper, PurchasePriceHistoryEntity> implements PurchasePriceHistoryService {
+
+}
