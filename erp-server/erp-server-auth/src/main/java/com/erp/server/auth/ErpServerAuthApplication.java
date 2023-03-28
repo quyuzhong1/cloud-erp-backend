3e05c111338774a32406276c64626c3ef538ea7d
Cloud <cloud@ulanzi.cn>
2023-03-28 15:34:41 +0800

    【 reimport  dependcy】

--- erp-server/erp-server-auth/src/main/java/com/erp/server/auth/ErpServerAuthApplication.java
+++ erp-server/erp-server-auth/src/main/java/com/erp/server/auth/ErpServerAuthApplication.java
@@ -1,6 +1,5 @@
 package com.erp.server.auth;
 
-import io.seata.spring.boot.autoconfigure.SeataAutoConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
