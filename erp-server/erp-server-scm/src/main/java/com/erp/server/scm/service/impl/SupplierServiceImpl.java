dc5e5fd3377e7a5ecbcf59e0750058583504c0c3
yl <ylstrive@163.com>
2023-03-31 16:30:15 +0800

    Lambda 2023-03-31  修改已知bug

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
@@ -450,7 +450,7 @@ public class SupplierServiceImpl extends SuperServiceImpl<SupplierMapper, Suppli
             //审核通过
             String approveStatus = ApproveStatusEnum.APPROVE.getStatus();
             result = this.updateApproveStatus(list, ApproveStatusEnum.getByStatus(approveStatus));
-            content = String.format("状态由[%s]变更为[%s]", ingStatusName, ApproveStatusEnum.APPROVE.getName());
+            content = String.format("状态由[%s]变更为[%s] , 意见:%s", ingStatusName, ApproveStatusEnum.APPROVE.getName(),comment);
         } else {
             //审核不通过
             String rejectStatus = ApproveStatusEnum.REJECT.getStatus();
