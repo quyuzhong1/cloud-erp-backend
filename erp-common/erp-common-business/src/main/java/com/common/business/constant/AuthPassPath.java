d8d0794ebe6f670a33ab35db93489e127d4fe231
luoweigang <821969511@qq.com>
2023-04-03 17:42:48 +0800

    修改接口过滤配置

--- erp-common/erp-common-business/src/main/java/com/common/business/constant/AuthPassPath.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/constant/AuthPassPath.java
@@ -9,7 +9,7 @@ package com.common.business.constant;
 public class AuthPassPath {
 
 
-    public static final String PASS_PATH_LIST = "/user/accountLogin;/user/scanCodeLogin;/sys/calendar/save/year";
+    public static final String PASS_PATH_LIST = "/user/accountLogin;/user/scanCodeLogin;/calendar/save/year";
 
 
 
