fb9cd74d607b84639c1b0aba07b743f854609690
yl <ylstrive@163.com>
2023-04-13 09:55:57 +0800

    Lambda 2023-04-13  质检单 代码提交

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/PurchaseStorageDetailEntity.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/PurchaseStorageDetailEntity.java
@@ -1,4 +1,4 @@
-package com.erp.server.wms.entity;
+package com.erp.model.wms.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/PurchaseStorageMapper.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/PurchaseStorageMapper.java
@@ -1,5 +1,7 @@
 package com.erp.server.wms.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.erp.model.wms.entity.PurchaseStorageEntity;
 import org.apache.ibatis.annotations.Mapper;
 
 /**
