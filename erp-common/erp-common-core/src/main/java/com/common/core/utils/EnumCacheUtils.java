4e4cb15c9420e5692746cd670493444a75324894
zhangchunlin <Polazhang89>
2023-04-14 12:15:33 +0800

    提供枚举下拉框，供前端调用接口，不用每个枚举类都提供一个单独的接口（每个服务都有专属自己的）

--- erp-common/erp-common-core/src/main/java/com/common/core/utils/EnumCacheUtils.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/utils/EnumCacheUtils.java
@@ -1,12 +1,12 @@
 package com.common.core.utils;
 
 import cn.hutool.core.util.ClassUtil;
+import cn.hutool.core.util.StrUtil;
 import com.common.core.enums.ApiError;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
@@ -16,7 +16,8 @@ import java.util.stream.Collectors;
 
 /**
  * @Classname: EnumCacheUtils
- * @Description: 枚举类缓存，并转换成下拉供前端使用，统一提供
+ * @Description: 枚举类缓存，并转换成下拉接口供前端使用，统一提供，为确保数据隔离，每个服务单独读取该服务的枚举类
+ * 请求路径如/api/sys/common/enumDropDown?type=FieldFormatPatternType
  * @CreateTime: 2023-04-13  17:57
  * @Author: zhangchunlin
  */
@@ -26,17 +27,28 @@ public class EnumCacheUtils {
     // 为保证可见性和有序性，防止出现半初始化
     private static volatile EnumCacheUtils INSTANCE;
 
-    // 扫描的包路径
-    private static final String[] SCAN_ENUM_PACKAGE_PATH = {"com.common.business.enums", "com.common.core.enums","com.erp.model"};
+    // 扫描的公共包路径
+    private static final String[] SCAN_COMMON_ENUM_PACKAGE_PATH = {"com.common.business.enums", "com.common.core.enums"};
+
+    // 服务内枚举包
+    private static String SERVICE_ENUM_PACKAGE_PATH = "";
 
     // 枚举缓存
     private static volatile Map<String,List<Map<String,Object>>> enumMaps = new ConcurrentHashMap();
 
     private static final Lock lock = new ReentrantLock();
 
-    private static final String ENUM_CODE_FIELD = "code";
+    private static final String SHOW_CODE = "code";
 
-    private static final String ENUM_VALUE_FIELD = "name";
+    private static final String SHOW_VALUE = "value";
+
+    private static final String ENUM_GET_CODE = "getCode";
+
+    private static final String ENUM_GET_NAME = "getName";
+
+    public void setServiceEnumPackagePath(String serviceEnumPackagePath) {
+        SERVICE_ENUM_PACKAGE_PATH = serviceEnumPackagePath;
+    }
 
     /**
      * 不扫描的枚举类
@@ -54,7 +66,8 @@ public class EnumCacheUtils {
         if (INSTANCE == null) {
             synchronized (EnumCacheUtils.class) {
                 if (INSTANCE == null) {
-                    return new EnumCacheUtils();
+                    INSTANCE = new EnumCacheUtils();
+                    return INSTANCE;
                 }
             }
         }
@@ -76,15 +89,21 @@ public class EnumCacheUtils {
         try {
             // 读取某个包及子包下面的所有枚举类
             Set<Class<?>> searchClazzSets = Sets.newHashSet();
-            Arrays.asList(SCAN_ENUM_PACKAGE_PATH).forEach(enumPackagePath-> searchClazzSets.addAll(ClassUtil.scanPackage(enumPackagePath,(clazz)-> clazz.isEnum())));
+            Arrays.asList(SCAN_COMMON_ENUM_PACKAGE_PATH).forEach(enumPackagePath-> {
+                log.info("扫描包{}下面的所有枚举类", enumPackagePath);
+                searchClazzSets.addAll(ClassUtil.scanPackage(enumPackagePath,(clazz)-> clazz.isEnum()));
+            });
+            if(StrUtil.isNotBlank(SERVICE_ENUM_PACKAGE_PATH)) {
+                log.info("扫描包{}下面的所有枚举类", SERVICE_ENUM_PACKAGE_PATH);
+                searchClazzSets.addAll(ClassUtil.scanPackage(SERVICE_ENUM_PACKAGE_PATH,(clazz)-> clazz.isEnum()));
+            }
             // 遍历所有的枚举类
             if (searchClazzSets != null && searchClazzSets.size() > 0) {
                 for (Class clazz : searchClazzSets) {
                     if(excludeScan.contains(clazz.getSimpleName())) {
                         continue;
                     }
-                    // 获取某个具体枚举类的
-                    // 只处理枚举类中包含code和name的枚举类
+                    // 获取某个具体枚举类的，只处理枚举类中包含code和name的枚举类
                     if(checkPermitEnum(clazz)) {
                         List<Map<String, Object>> list = getEnumValueByClass(clazz);
                         enumMaps.put(clazz.getSimpleName().replace("Enum","").replace("enum",""), list);
@@ -97,22 +116,22 @@ public class EnumCacheUtils {
         return enumMaps;
     }
 
-    public static boolean checkPermitEnum(Class tt) {
-       Set<String> fieldNames = Arrays.asList(tt.getFields()).stream().map(Field::getName).collect(Collectors.toSet());
-       return fieldNames.contains(ENUM_CODE_FIELD) && fieldNames.contains(ENUM_VALUE_FIELD);
+    private static boolean checkPermitEnum(Class tt) {
+       Set<String> fieldNames = Arrays.asList(tt.getDeclaredMethods()).stream().map(Method::getName).collect(Collectors.toSet());
+       return fieldNames.contains(ENUM_GET_CODE) && fieldNames.contains(ENUM_GET_NAME);
     }
 
     public static List<Map<String,Object>> getEnumValueByClass(Class tt) {
         List<Map<String,Object>> list = Lists.newArrayList();
         Object[] objects = tt.getEnumConstants();
         try {
-            Method getCode = tt.getMethod("getCode");
-            Method getName = tt.getMethod("getName");
+            Method getCode = tt.getMethod(ENUM_GET_CODE);
+            Method getName = tt.getMethod(ENUM_GET_NAME);
             Map<String, Object> map;
             for (Object object : objects) {
                 map = new HashMap();
-                map.put("code", getCode.invoke(object));
-                map.put("value", getName.invoke(object));
+                map.put(SHOW_CODE, getCode.invoke(object));
+                map.put(SHOW_VALUE, getName.invoke(object));
                 list.add(map);
             }
         } catch (Exception e) {
--- erp-server/erp-server-admin/src/main/java/com/erp/server/admin/controller/api/CommonController.java
+++ erp-server/erp-server-admin/src/main/java/com/erp/server/admin/controller/api/CommonController.java
@@ -1,68 +1,24 @@
-package com.erp.server.sys.controller.api;
+package com.erp.server.admin.controller.api;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.common.core.controller.BaseController;
 import com.common.core.controller.vo.ApiResult;
 import com.common.core.utils.EnumCacheUtils;
-import com.common.core.utils.FastDFSClientUtil;
 import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 /**
- * @author Lambda
  * @Classname CommonController
  * @Description TODO
- * @Date 2023-03-23 18:54
- * @Created by yl
+ * @Date 2023-04-14 12:06
+ * @Created by zhangchunlin
  */
 @RestController
 @RequestMapping("common")
 public class CommonController extends BaseController {
-
-
-    /**
-     * 上传图片
-     *
-     * @param multipartFile 图片流
-     * @return com.common.core.vo.ApiResult
-     * @Author Luo_WG
-     * @Date 2022/10/9 17:35
-     **/
-    @PostMapping("/upload")
-    public ApiResult upload(@RequestParam("multipartFile") MultipartFile[] multipartFile, HttpServletRequest request) {
-        List<String> list = new ArrayList<>();
-        for (MultipartFile file : multipartFile) {
-            String filePath = FastDFSClientUtil.uploadFile(file);
-            list.add(filePath);
-        }
-        return this.success(list);
-    }
-
-    /**
-     * 删除 上传utl
-     *
-     * @return com.common.core.vo.ApiResult
-     * @Author Luo_WG
-     * @Date 2022/10/9 17:35
-     **/
-    @PostMapping("/deleteUrl")
-    public ApiResult deleteUrl(@RequestParam("url") String url) {
-        if (StringUtils.isNotBlank(url)) {
-            int num = FastDFSClientUtil.deleteFile(url);
-            if (num == 0) {
-                return success();
-            }
-        }
-        return failure();
-
-    }
 
     /**
      * 批量获取枚举下拉框，供前端调用，不用每个枚举类都提供一个单独的接口（每个服务都有专属自己的）
