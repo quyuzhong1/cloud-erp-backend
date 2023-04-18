b9282e610607b9fd7e8cb5376aed39b4ab42cbba
luoweigang <821969511@qq.com>
2023-04-18 12:38:37 +0800

    修改收货单导出实体

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/ReturnOrderExcelDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/ReturnOrderExcelDTO.java
@@ -18,22 +18,16 @@ public class ReturnOrderExcelDTO {
     /**
      * 退货单号
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "签收单号", index = 0)
     private String code;
 
     /**
      * 采购单号
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "采购单号", index = 1)
     private String purchaseOrderCode;
 
     /**
      * 供应商名称
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "供应商名称", index = 2)
     private String supplierName;
 
     /**
@@ -44,8 +38,6 @@ public class ReturnOrderExcelDTO {
     /**
      * 审核状态名称
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "审核状态名称", index = 3)
     private String approveStatusName;
 
     /**
@@ -56,8 +48,6 @@ public class ReturnOrderExcelDTO {
     /**
      * 作废状态名称
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "作废状态名称", index = 4)
     private String invalidStatusName;
 
     /**
@@ -68,92 +58,66 @@ public class ReturnOrderExcelDTO {
     /**
      * skuNo
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "skuNo", index = 5)
     private String skuNo;
 
     /**
      * 产品名称
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "productName", index = 6)
     private String productName;
 
     /**
      * 退货日期
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货日期", index = 7)
     private LocalDate billDate;
 
     /**
      * 交货仓库
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "交货仓库", index = 8)
     private String deliveryWarehouseName;
 
     /**
      * 退货数量
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货数量", index = 9)
     private String realityReturnQty;
 
     /**
      * 退货原因
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货原因", index = 10)
     private String returnRemark;
 
     /**
      * 退货方式
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货方式", index = 11)
     private String returnMode;
 
     /**
      * 采购员
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "采购员", index = 12)
     private String purchaseUserName;
 
     /**
      * 退货员
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货员", index = 13)
     private String returnUserName;
 
     /**
      * 退货备注
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "退货备注", index = 14)
     private String remark;
 
     /**
      * 审核人
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "审核人", index = 15)
     private String approveUserName;
 
     /**
      * 创建人
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "创建人", index = 16)
     private String createUserName;
 
     /**
      * 创建时间
      */
-    @ColumnWidth(30)
-    @ExcelProperty(value = "创建时间", index = 17)
     private String createTime;
 
 }
