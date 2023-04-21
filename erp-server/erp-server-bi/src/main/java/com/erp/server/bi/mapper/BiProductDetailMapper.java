53d5777266148596445fba29e8218d2c6cf16fdf
yl <ylstrive@163.com>
2023-04-21 11:07:03 +0800

    2023-04-21 bi 店铺 品类筛选条件更改

---
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/BiProductDetailMapper.java
@@ -0,0 +1,18 @@
+package com.erp.server.bi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.erp.model.bi.entity.BiProductDetailEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 产品sku表 Mapper 接口
+ * </p>
+ *
+ * @author lambda
+ * @since 2023-04-21
+ */
+@Mapper
+public interface BiProductDetailMapper extends BaseMapper<BiProductDetailEntity> {
+
+}
