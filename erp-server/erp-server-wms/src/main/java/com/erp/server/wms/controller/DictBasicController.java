9559f477f9e89202b7c7fa6aa112bc6673c9bf50
yl <ylstrive@163.com>
2023-04-23 10:36:15 +0800

    2023-04-23 质检单 打印日志

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/DictBasicController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/DictBasicController.java
@@ -48,7 +48,7 @@ public class DictBasicController extends BaseController {
      *
      * @return
      */
-    @PostMapping("/list")
+    @GetMapping("/list")
     public ApiResult<List<DictBasicDTO>> list(@RequestParam("key") String key) {
         List<DictBasicDTO> list = dictBasicService.getByKey(key);
         return success(list);
