cdc50b201fd99bfbb79a3e47eaf27f372133acb7
zhangchunlin <Polazhang89>
2023-04-24 09:53:15 +0800

    消息服务maven文件修改，删掉邮件包

--- erp-server/erp-server-msg/src/main/java/com/erp/server/msg/config/AbstractMongoConfig.java
+++ erp-server/erp-server-msg/src/main/java/com/erp/server/msg/config/AbstractMongoConfig.java
@@ -1,5 +1,6 @@
 package com.erp.server.msg.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.util.ObjectUtils;
 
 
+@Slf4j
 public abstract class AbstractMongoConfig {
 	public MongoDatabaseFactory mongoDbFactory(ConfigurableEnvironment env, String dbName) {
         String uri = getPropertyUriAsString(env, dbName, "");
@@ -16,6 +18,7 @@ public abstract class AbstractMongoConfig {
 
 	private String getPropertyUriAsString(ConfigurableEnvironment env, String key, String defaultVal) {
 		String k = "spring.data."+key+".mongodb.uri";
+        log.info("环境：{},数据库：{}",env.getProperty("spring.profiles.active"),key);
 		return ObjectUtils.isEmpty(env.getProperty(k))?defaultVal:env.getProperty(k);
 	}
 
