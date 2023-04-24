a53c76880218e63a9e810e98b4661b3f2369c394
luoweigang <821969511@qq.com>
2023-04-24 14:35:14 +0800

    修改退货单

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/excel/ReturnOrderExportExcelDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/excel/ReturnOrderExportExcelDTO.java
@@ -3,6 +3,7 @@ package com.erp.model.wms.dto.excel;
 import com.alibaba.excel.annotation.ExcelProperty;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.common.business.service.LocalDateStringConverter;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -139,7 +140,7 @@ public class ReturnOrderExportExcelDTO {
      * 创建时间
      */
     @ColumnWidth(30)
-    @ExcelProperty(value = "创建时间", index = 17)
+    @ExcelProperty(value = "创建时间", index = 17, converter= LocalDateStringConverter.class)
     private String createTime;
 
 }
