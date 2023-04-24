2732aee36a3d88130809e4c8829fc8d43dbc004d
Cloud <cloud@ulanzi.cn>
2023-04-24 15:30:27 +0800

    update  count salevo

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpOrderInfoServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpOrderInfoServiceImpl.java
@@ -170,18 +170,19 @@ public class DmpOrderInfoServiceImpl extends ServiceImpl<DmpOrderInfoMapper, Dmp
 
     @Override
     public TargetSaleCountVO countSalesVolume(BiFilterDTO dto) {
-        Integer count = 0;
-        QueryWrapper<DmpOrderInfoEntity> query = getDmpOrderInfoEntityQueryWrapper(dto);
-        // 先查询订单号
-        query.select("id")
-                .last(StringUtils.isNotBlank(dto.getParam()), dto.getParam());
-        List<DmpOrderInfoEntity> list = baseMapper.selectList(query);
-        if (CollectionUtils.isEmpty(list)) {
-            return new TargetSaleCountVO(count);
-        }
-        List<String> orderIds = list.stream().map(DmpOrderInfoEntity::getId).collect(Collectors.toList());
-        // 根据订单号获取订单详情，筛选sku
-        count = dmpOrderItemService.countSalesVolume(orderIds, dto.getSku());
+//        Integer count = 0;
+//        QueryWrapper<DmpOrderInfoEntity> query = getDmpOrderInfoEntityQueryWrapper(dto);
+//        // 先查询订单号
+//        query.select("id")
+//                .last(StringUtils.isNotBlank(dto.getParam()), dto.getParam());
+//        List<DmpOrderInfoEntity> list = baseMapper.selectList(query);
+//        if (CollectionUtils.isEmpty(list)) {
+//            return new TargetSaleCountVO(count);
+//        }
+//        List<String> orderIds = list.stream().map(DmpOrderInfoEntity::getId).collect(Collectors.toList());
+//        // 根据订单号获取订单详情，筛选sku
+//        count = dmpOrderItemService.countSalesVolume(orderIds, dto.getSku());
+        Integer count = baseMapper.countSalesVolume(dto);
         return new TargetSaleCountVO(count);
     }
 
@@ -867,7 +868,7 @@ public class DmpOrderInfoServiceImpl extends ServiceImpl<DmpOrderInfoMapper, Dmp
     @Override
     public TargetSaleAndYoyCountVO countSalesVolumeAndYoy(BiFilterDTO dto) {
         // 查询当期销售额
-        dto.setEndTime(dto.getEndTime());
+//        dto.setEndTime(dto.getEndTime());
         TargetSaleCountVO currentVo = countSalesVolume(dto);
         Integer currentAmount = currentVo.getValue();
         if (0 == currentAmount){
