c23b4f7a3f06c1b1636483e933fecba118706e1d
luoweigang <821969511@qq.com>
2023-04-25 11:17:54 +0800

    修改实体类

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductDetailShowDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductDetailShowDTO.java
@@ -243,15 +243,15 @@ public class ProductDetailShowDTO implements Serializable {
      * 上市时间
      * product_sale
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime listingTime;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private LocalDate listingTime;
 
     /**
      * 退市时间
      * product_sale
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private LocalDateTime delistingTime;
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private LocalDate delistingTime;
 
     /**
      * 箱单数量
