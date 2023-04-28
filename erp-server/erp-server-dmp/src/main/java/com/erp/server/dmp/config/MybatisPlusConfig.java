3bf1077236869176b7790ebc05d5b951762f61d4
Cloud <cloud@ulanzi.cn>
2023-04-28 12:32:06 +0800

    [ add config ]

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/config/MybatisPlusConfig.java
@@ -0,0 +1,40 @@
+package com.erp.server.dmp.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author lambda
+ * @Classname MybatisPlusConfig
+ * @Description TODO
+ * @Date 2022-07-06 9:30
+ * @Created by yl
+ */
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+
+    }
+
+
+
+
+
+
+
+}
