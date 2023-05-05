b720b05ddae2058fba955b32aa5d9aedb676da53
luoweigang <821969511@qq.com>
2023-05-05 10:39:45 +0800

    修改产品管理同步新品

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/customer/ProductListingTimeCustomer.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/customer/ProductListingTimeCustomer.java
@@ -1,5 +1,6 @@
 package com.erp.server.plm.rocketmq.customer;
 
+import cn.hutool.json.JSONUtil;
 import com.common.business.utils.RedisUtil;
 import com.common.message.constant.RedisKeyConstant;
 import com.common.message.constant.RocketMqConsumerGroup;
@@ -31,10 +32,15 @@ public class ProductListingTimeCustomer implements RocketMQListener<Map<String, 
 
     @Override
     public void onMessage(Map<String, Object> stringObjectMap) {
-
+        log.info("监听到上市时间需要修改：entity={}", JSONUtil.toJsonStr(stringObjectMap));
         String skuNo = String.valueOf(stringObjectMap.get("skuNo"));
         String listingTime = String.valueOf(stringObjectMap.get("listingTime"));
         ProductDetailEntity productIdBySku = productDetailService.getProductIdBySku(skuNo);
+
+        ProductSaleEntity bySkuId = productSaleService.getBySkuId(productIdBySku.getId());
+        if (bySkuId.getListingTime() != null) {
+            listingTime = bySkuId.getListingTime().toString();
+        }
         productSaleService.lambdaUpdate()
                 .set(ProductSaleEntity::getListingTime, listingTime)
                 .eq(ProductSaleEntity::getSkuId, productIdBySku.getId())
