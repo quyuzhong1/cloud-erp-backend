b992e4f25a2b602daded14bff9f98211c290f227
luoweigang <821969511@qq.com>
2023-05-08 09:30:28 +0800

    修改新品标识清洗

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/customer/ProductListingTimeCustomer.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/customer/ProductListingTimeCustomer.java
@@ -42,10 +42,10 @@ public class ProductListingTimeCustomer implements RocketMQListener<Map<String, 
         if (ObjectUtil.isEmpty(productIdBySku)) {
             return;
         }
-        ProductSaleEntity bySkuId = productSaleService.getBySkuId(productIdBySku.getId());
+        /*ProductSaleEntity bySkuId = productSaleService.getBySkuId(productIdBySku.getId());
         if (bySkuId.getListingTime() != null) {
             listingTime = bySkuId.getListingTime().toString();
-        }
+        }*/
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         productSaleService.lambdaUpdate()
                 .set(ProductSaleEntity::getListingTime, LocalDate.parse(listingTime, dateTimeFormatter))
