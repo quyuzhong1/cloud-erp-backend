8678d191c93ab26bcb64b818831f5a7ad986a4ae
yl <ylstrive@163.com>
2023-05-16 14:59:22 +0800

    2023-05-16  Lambda 文档权限更改 UAT

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/mapper/TaskDocsMapper.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/mapper/TaskDocsMapper.java
@@ -24,7 +24,7 @@ public interface TaskDocsMapper  extends BaseMapper<TaskDeliveryDocsEntity> {
 
     List<CountDTO> getTaskDocsCount(@Param("taskIds") List<String> taskIds);
 
-    IPage paging(Page query, @Param("params") BaseSearchDTO params,@Param("ids") List<String> ids);
+    IPage<DeliveryDocsDTO> paging(Page query, @Param("params") BaseSearchDTO params,@Param("power")DocsDTO.DeliveryDocsPowerDTO  power);
 
     List<DeliveryDocsDTO> list(@Param("params") BaseSearchDTO params,@Param("ids") List<String> ids);
 
@@ -35,4 +35,7 @@ public interface TaskDocsMapper  extends BaseMapper<TaskDeliveryDocsEntity> {
     List<DocsDTO> getDocsByTaskId(@Param("taskId") String taskId);
 
 
+    IPage<DeliveryDocsDTO> containPaging(Page query, BaseSearchDTO params, @Param("ids")List<String> containDocsPowerList);
+
+    IPage<DeliveryDocsDTO> noContainPaging(Page query, BaseSearchDTO params, @Param("ids")List<String> noContainDocsPowerList);
 }
