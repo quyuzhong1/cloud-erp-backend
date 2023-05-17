c134b8e5d19d97debce6a3c1fe620b7fec918d77
luoweigang <821969511@qq.com>
2023-05-17 10:10:47 +0800

    修改入库单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/SoOutstockFeignController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/SoOutstockFeignController.java
@@ -7,10 +7,12 @@ import com.erp.server.wms.service.SoReturnNoticeDetailService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@RestController
 @RequestMapping("feign/soOutstock")
 public class SoOutstockFeignController {
     @Resource
