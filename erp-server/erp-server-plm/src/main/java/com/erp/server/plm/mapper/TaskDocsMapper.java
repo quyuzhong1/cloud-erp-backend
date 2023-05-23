d2f54a7364f2d420e9e1fcc5e3d57529b6435d5a
yl <ylstrive@163.com>
2023-05-23 15:08:16 +0800

    2023-05-23  Lambda 文档权限更改

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/mapper/TaskDocsMapper.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/mapper/TaskDocsMapper.java
@@ -24,7 +24,7 @@ public interface TaskDocsMapper  extends BaseMapper<TaskDeliveryDocsEntity> {
 
     List<CountDTO> getTaskDocsCount(@Param("taskIds") List<String> taskIds);
 
-    IPage<DeliveryDocsDTO> paging(Page query, @Param("params") BaseSearchDTO params,@Param("power")DocsDTO.DeliveryDocsPowerDTO  power);
+    IPage<DeliveryDocsDTO> paging(Page query, @Param("params") BaseSearchDTO params,@Param("ids")List<String>  ids);
 
     List<DeliveryDocsDTO> list(@Param("params") BaseSearchDTO params,@Param("ids") List<String> ids);
 
