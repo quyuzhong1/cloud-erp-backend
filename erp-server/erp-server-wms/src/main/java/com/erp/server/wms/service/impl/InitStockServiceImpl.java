61def09945cf2eb9b99ae33bb9e8d3a415ce1bd3
zhangchunlin <Polazhang89>
2023-05-24 17:41:12 +0800

    期初库存修复bug

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/InitStockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/InitStockServiceImpl.java
@@ -252,7 +252,7 @@ public class InitStockServiceImpl extends SuperServiceImpl<InitStockMapper, Init
         // 判断状态是否允许操作（只有待提交且未作废的的才允许修改）
         ValidatorUtil.isTrue((Objects.equals(originInitStock.getApproveStatus(), ApproveStatusEnum.WAIT_SUBMIT.getStatus()) || Objects.equals(originInitStock.getApproveStatus(), ApproveStatusEnum.REJECT.getStatus()) )
                 && Objects.equals(originInitStock.getInvalidStatus(),Boolean.FALSE),
-                ()->new ServiceException("当前单据状态不允许修改"));
+                ()->new ServiceException("只有待提交或审核不通过并且未作废数据支持提交"));
 
         InitStockEntity nowInitStock =  BeanMapperUtils.map(InitStockEntity.class, originInitStock);
         fillingAddOrUpdate(nowInitStock, dto.getWarehouseId());
