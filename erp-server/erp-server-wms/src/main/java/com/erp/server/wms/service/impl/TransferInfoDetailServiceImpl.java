aed517bf71b635d0236b379e7a9f4136a5b52732
Will <wangwei@ulanzi.cn>
2023-05-26 16:13:32 +0800

    代码调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/TransferInfoDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/TransferInfoDetailServiceImpl.java
@@ -1,6 +1,5 @@
 package com.erp.server.wms.service.impl;
 
-import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.common.business.enums.SourceTypeEnum;
@@ -157,7 +156,7 @@ public class TransferInfoDetailServiceImpl extends SuperServiceImpl<TransferInfo
             }
             //数量检验
             if (detailEntity.getQty().intValue() > pickingQty.intValue() - hasPickingQty.intValue()) {
-                throw new ServiceException(ApiError.ERROR_99051.code, StrUtil.format(ApiError.ERROR_99051.msg,transferInfoEntity.getSourceCode(), detailEntity.getSkuNo()));
+                throw new ServiceException(ApiError.ERROR_99051.code, String.format(ApiError.ERROR_99051.msg,transferInfoEntity.getSourceCode(), detailEntity.getSkuNo()));
             }
         }
 
