b6f2abed4b4037233490ecf404161ec539404c46
Cloud <cloud@ulanzi.cn>
2023-05-29 09:30:53 +0800

    代码合并 2

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/dmp/impl/DmpReturnOrderInfoServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/dmp/impl/DmpReturnOrderInfoServiceImpl.java
@@ -59,9 +59,9 @@ public class DmpReturnOrderInfoServiceImpl extends ServiceImpl<DmpReturnOrderInf
      **/
     @Override
     public DmpReturnOrderInfoEntity getOrderByPlatformOrderId(DmpReturnOrderInfoEntity returnOrderInfoEntity) {
-        LambdaQueryWrapper<DmpReturnOrderInfoEntity> lambdaQueryWrapper = new LambdaQueryWrapper();
-        lambdaQueryWrapper.eq(DmpReturnOrderInfoEntity::getPlatformOrderId, returnOrderInfoEntity.getPlatformOrderId());
-        lambdaQueryWrapper.eq(StrUtil.isNotBlank(returnOrderInfoEntity.getReturnOrderId()), DmpReturnOrderInfoEntity::getReturnOrderId, returnOrderInfoEntity.getReturnOrderId());
+        LambdaQueryWrapper<DmpReturnOrderInfoEntity> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(DmpReturnOrderInfoEntity::getReturnCode, returnOrderInfoEntity.getReturnCode());
+        lambdaQueryWrapper.eq(StrUtil.isNotBlank(returnOrderInfoEntity.getPlatformOrderId()), DmpReturnOrderInfoEntity::getPlatformOrderId, returnOrderInfoEntity.getPlatformOrderId());
         return this.getOne(lambdaQueryWrapper);
     }
 
@@ -89,8 +89,8 @@ public class DmpReturnOrderInfoServiceImpl extends ServiceImpl<DmpReturnOrderInf
     @Override
     public Boolean updateOrderByPlatformOrderId(DmpReturnOrderInfoEntity dmpReturnOrderInfoEntity) {
         LambdaQueryWrapper<DmpReturnOrderInfoEntity> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.eq(DmpReturnOrderInfoEntity::getReturnCode, dmpReturnOrderInfoEntity.getReturnCode());
         lambdaQueryWrapper.eq(DmpReturnOrderInfoEntity::getPlatformOrderId, dmpReturnOrderInfoEntity.getPlatformOrderId());
-        lambdaQueryWrapper.eq(DmpReturnOrderInfoEntity::getReturnOrderId, dmpReturnOrderInfoEntity.getReturnOrderId());
         return this.update(dmpReturnOrderInfoEntity, lambdaQueryWrapper);
     }
 
