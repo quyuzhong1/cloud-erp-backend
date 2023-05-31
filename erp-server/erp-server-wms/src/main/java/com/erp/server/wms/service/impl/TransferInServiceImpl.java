dfdb2f796d3026c697b4371b0a8b9fb3581df38e
zhangchunlin <Polazhang89>
2023-05-31 11:13:57 +0800

    代码生成器调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/TransferInServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/TransferInServiceImpl.java
@@ -369,6 +369,7 @@ public class TransferInServiceImpl extends SuperServiceImpl<TransferInMapper, Tr
         InventorySourceTypeEnum transferIn = InventorySourceTypeEnum.TRANSFER_IN;
         members.stream().forEach(m -> m.setSourceType(transferIn));
         if (CollectionUtils.isNotEmpty(members)) {
+            inventoryInOutStockDTO.setMembers(members);
             inventoryTransCoreService.approveByType(inventoryInOutStockDTO);
         }
 
