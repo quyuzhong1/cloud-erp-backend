129907f576939af118061e1b0220c68f5799e9d5
luoweigang <821969511@qq.com>
2023-06-05 15:03:32 +0800

    修改客户列表

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeCustomerConsumer.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeCustomerConsumer.java
@@ -129,7 +129,7 @@ public class KingdeeCustomerConsumer implements RocketMQListener<Map<String, Obj
         //创建状态则直接修改、删除
         if (KingdeeDocStatusEnum.CREATED.getCode().equals(documentStatus) || KingdeeDocStatusEnum.REAPPROVE.getCode().equals(documentStatus) || flag) {
             //主单据id
-            KingdeeUtils.makeFieldJson(json,"FStockId",".", id);
+            KingdeeUtils.makeFieldJson(json,"FCustId",".", id);
             StringBuffer allKey = FastJsonUtil.getAllKey(json);
             ArrayList<String> apiFieldList = (ArrayList) Arrays.stream(allKey.toString().split(",")).collect(Collectors.toList());
             param.setNeedUpDateFields(apiFieldList);
