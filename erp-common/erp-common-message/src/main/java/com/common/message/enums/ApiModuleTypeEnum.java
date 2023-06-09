1d526af05d47a02f65342f15a4a1f4b05f06f84d
luoweigang <821969511@qq.com>
2023-06-09 10:21:59 +0800

    修改退货单同步金蝶

--- erp-common/erp-common-message/src/main/java/com/common/message/enums/ApiModuleTypeEnum.java
+++ erp-common/erp-common-message/src/main/java/com/common/message/enums/ApiModuleTypeEnum.java
@@ -32,7 +32,7 @@ public enum ApiModuleTypeEnum {
     SO_INFO(21, "soInfo", "销售订单","oms"),
     SO_CHANGE(22, "soChange", "销售变更单","oms"),
     SO_OUTSTOCK(23, "soOutstock", "销售出库单","wms"),
-    SO_RETURN(24, "soReturn", "销售退货单","wms"),
+    SO_RETURN(24, "soReturn", "销售退货单","oms"),
     CUSTOMER_GROUP(25, "customerGroup", "客户分组","oms"),
 
     ;
