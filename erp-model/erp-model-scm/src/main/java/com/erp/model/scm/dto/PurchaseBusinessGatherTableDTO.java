3030ce282f64e188407ec966a6ad88ec431c5cfa
luoweigang <821969511@qq.com>
2023-06-12 18:30:05 +0800

    修改scm报表管理

---
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/PurchaseBusinessGatherTableDTO.java
@@ -0,0 +1,125 @@
+package com.erp.model.scm.dto;
+
+import com.common.business.dto.base.PermissionsDTO;
+import com.common.business.dto.base.SortDTO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 采购业务汇总表
+ * @Author Luo_WG
+ * @Date 2023/6/12 18:06
+ **/
+public class PurchaseBusinessGatherTableDTO extends PermissionsDTO implements Serializable {
+
+    /**
+     * 分页信息
+     */
+    @Data
+    @NoArgsConstructor
+    public static class PagingViewDTO {
+        /**
+         * 采购组织
+         */
+        private String purchaseOrgName;
+        /**
+         * 供应商名称
+         */
+        private String supplierName;
+        /**
+         * sku
+         */
+        private String skuNo;
+        /**
+         * 产品名称
+         */
+        private String productName;
+        /**
+         * spu型号
+         */
+        private String spuNo;
+        /**
+         * 平均价格
+         */
+        private String avgPrice;
+        /**
+         * 订单数量
+         */
+        private String orderQty;
+        /**
+         * 订单金额
+         */
+        private String orderAmount;
+        /**
+         * 收货数量
+         */
+        private String receiveQty;
+        /**
+         * 赠品数量
+         */
+        private String giftQty;
+        /**
+         * 收货金额
+         */
+        private String receiveAmount;
+        /**
+         * 入库数量
+         */
+        private String stockInQty;
+        /**
+         * 超收数量
+         */
+        private String exceedQty;
+        /**
+         * 入库金额
+         */
+        private String stockInAmount;
+        /**
+         * 退货扣款数量
+         */
+        private String refundQty;
+        /**
+         * 退货补货数量
+         */
+        private String replenishQty;
+        /**
+         * 退货金额
+         */
+        private String returnAmount;
+    }
+
+    /**
+     * 分页参数
+     */
+    @Data
+    @NoArgsConstructor
+    public static class PagingParamDTO extends SortDTO {
+        /**
+         * ids
+         */
+        private List<String> ids;
+        /**
+         * 单据日期范围
+         */
+        private List<String> billDateList;
+        /**
+         * spu集合
+         */
+        private List<String> spuList;
+        /**
+         * 仓库id
+         */
+        private List<String> warehouseIdList;
+        /**
+         * 采购组织id
+         */
+        private List<String> purchaseOrgIdList;
+        /**
+         * 供应商id
+         */
+        private List<String> supplierIdList;
+    }
+}
--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/ReportFormsManageController.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/ReportFormsManageController.java
@@ -1,8 +1,17 @@
 package com.erp.server.scm.controller.api;
 
+import com.common.business.annotation.DataPermission;
+import com.common.business.enums.DataAttributeEnum;
 import com.common.core.controller.BaseController;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.scm.dto.PurchaseBusinessGatherTableDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 报表管理
@@ -13,4 +22,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ReportFormsManage")
 public class ReportFormsManageController extends BaseController {
 
+    /**
+     * 采购业务汇总表
+     * @Author Luo_WG
+     * @Date 2023/6/12 18:24
+     * @param dto dto
+     * @return com.common.core.controller.vo.ApiResult<java.util.List<com.erp.model.scm.dto.PurchaseBusinessGatherTableDTO.PagingViewDTO>>
+     **/
+    @PostMapping(value = "/purchaseBusinessGatherTable")
+    @DataPermission(operationType = DataAttributeEnum.LIST,
+            tableField = "create_user_id",
+            menuCode = "scm:ReportFormsManage:purchaseBusinessGatherTable",
+            tableAlias = "pod")
+    public ApiResult<List<PurchaseBusinessGatherTableDTO.PagingViewDTO>> purchaseBusinessGatherTable(@RequestBody PurchaseBusinessGatherTableDTO.PagingParamDTO dto) {
+        return null;
+    }
+
+    /**
+     * 导出采购业务汇总表
+     * @Author Luo_WG
+     * @Date 2023/6/12 18:27
+     * @param dto
+     * @param response
+     * @return com.common.core.controller.vo.ApiResult
+     **/
+    @PostMapping(value = "/exportExcelPurchaseBusiness")
+    @DataPermission(operationType = DataAttributeEnum.LIST,
+            tableField = "create_user_id",
+            menuCode = "scm:ReportFormsManage:exportExcelPurchaseBusiness",
+            tableAlias = "pod")
+    public ApiResult exportExcelPurchaseBusiness(@RequestBody PurchaseBusinessGatherTableDTO.PagingParamDTO dto, HttpServletResponse response) {
+        Boolean flag = Boolean.TRUE;
+        return flag == true ? success() : failure();
+    }
 }
