4f3a104a33a6d1954f3e2d9ee332ebc6a3926d5d
luoweigang <821969511@qq.com>
2023-06-12 17:39:51 +0800

    新增报表管理controller

---
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/ReportFormsManageController.java
@@ -0,0 +1,16 @@
+package com.erp.server.scm.controller.api;
+
+import com.common.core.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 报表管理
+ * @Author Luo_WG
+ * @Date 2023/6/12 17:31
+ **/
+@RestController
+@RequestMapping("/ReportFormsManage")
+public class ReportFormsManageController extends BaseController {
+
+}
