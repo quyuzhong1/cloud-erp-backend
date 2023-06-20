279457a788e6992be81fe88d75c0d13fd390e2d3
Cloud <cloud@ulanzi.cn>
2023-06-20 16:55:54 +0800

    update type
    (cherry picked from commit 698b1580c69fb89713ee660de2af2c146335d902)

--- erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/gyy/GyyDeliveryDetailEntity.java
+++ erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/gyy/GyyDeliveryDetailEntity.java
@@ -112,7 +112,7 @@ public class GyyDeliveryDetailEntity {
      * 是否货到付款
      */
     @SerializedName("cod")
-    private Boolean cod;
+    private Object cod;
     /**
      * 是否退款 0:无退款 1:有退款
      */
@@ -162,7 +162,7 @@ public class GyyDeliveryDetailEntity {
      * 是否多包裹 true：多包裹 false：单包裹
      */
     @SerializedName("picture_bill")
-    private Boolean pictureBill;
+    private Object pictureBill;
     /**
      * 平台单号
      */
