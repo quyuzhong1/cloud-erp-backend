5e17743c14b97055497172f8e75832965e3021e4
luoweigang <821969511@qq.com>
2023-06-20 11:41:09 +0800

    修改采购业务汇总表

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/ReportFormsManageController.java
@@ -0,0 +1,65 @@
+package com.erp.server.wms.controller.api;
+
+import com.common.business.annotation.DataPermission;
+import com.common.business.dto.base.PagingDTO;
+import com.common.business.enums.DataAttributeEnum;
+import com.common.business.vo.PagingVO;
+import com.common.core.controller.BaseController;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.scm.dto.PurchaseBusinessGatherTableDTO;
+import com.erp.server.wms.service.ReportFormsManageService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * 报表管理
+ * @Author Luo_WG
+ * @Date 2023/6/12 17:31
+ **/
+@RestController
+@RequestMapping("/reportFormsManage")
+public class ReportFormsManageController extends BaseController {
+    @Resource
+    private ReportFormsManageService reportFormsManageService;
+
+    /**
+     * 采购业务汇总表列表查询
+     * @Author Luo_WG
+     * @Date 2023/6/12 18:24
+     * @param dto dto
+     * @return com.common.core.controller.vo.ApiResult<java.util.List<com.erp.model.scm.dto.PurchaseBusinessGatherTableDTO.PagingViewDTO>>
+     **/
+    @PostMapping(value = "/purchaseBusinessGatherTablePaging")
+    @DataPermission(operationType = DataAttributeEnum.LIST,
+            tableField = "create_user_id",
+            menuCode = "scm:ReportFormsManage:purchaseBusinessGatherTablePaging",
+            tableAlias = "pod")
+    public ApiResult<PagingVO<List<PurchaseBusinessGatherTableDTO.PagingViewDTO>>> purchaseBusinessGatherTablePaging(@RequestBody PagingDTO<PurchaseBusinessGatherTableDTO.PagingParamDTO> dto) {
+        PagingVO<List<PurchaseBusinessGatherTableDTO.PagingViewDTO>> listPagingVO = reportFormsManageService.purchaseBusinessGatherTablePaging(dto);
+        return success(listPagingVO);
+    }
+
+    /**
+     * 导出采购业务汇总表
+     * @Author Luo_WG
+     * @Date 2023/6/12 18:27
+     * @param dto
+     * @param response
+     * @return com.common.core.controller.vo.ApiResult
+     **/
+    @PostMapping(value = "/exportExcelPurchaseBusiness")
+    @DataPermission(operationType = DataAttributeEnum.LIST,
+            tableField = "create_user_id",
+            menuCode = "scm:ReportFormsManage:exportExcelPurchaseBusiness",
+            tableAlias = "pod")
+    public ApiResult exportExcelPurchaseBusiness(@RequestBody PurchaseBusinessGatherTableDTO.PagingParamDTO dto, HttpServletResponse response) {
+        Boolean flag = reportFormsManageService.exportExcelPurchaseBusiness(dto, response);
+        return flag == true ? success() : failure();
+    }
+}
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/PurchaseOrderDetailService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/PurchaseOrderDetailService.java
@@ -1,12 +1,14 @@
 package com.erp.server.wms.service;
 
+import com.common.business.service.SuperService;
 import com.erp.model.plm.entity.ProductDetailEntity;
 import com.erp.model.plm.entity.ProductInfoEntity;
 import com.erp.model.scm.entity.PurchaseOrderDetailEntity;
+import com.erp.model.wms.entity.PurchaseReturnOrderDetailEntity;
 
 import java.util.List;
 
-public interface PurchaseOrderDetailService {
+public interface PurchaseOrderDetailService extends SuperService<PurchaseOrderDetailEntity> {
 
     /**
      * 根据主键Id查询表信息
