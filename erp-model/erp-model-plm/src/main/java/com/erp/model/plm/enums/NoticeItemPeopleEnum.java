3b7d7855123e3410d22f45c0321ba3fac0598661
luoweigang <821969511@qq.com>
2023-06-21 14:46:23 +0800

    修改采购业务汇总表

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/enums/NoticeItemPeopleEnum.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/enums/NoticeItemPeopleEnum.java
@@ -18,7 +18,8 @@ public enum NoticeItemPeopleEnum {
     ITEM_MANAGER("projectCharge", "项目经理"),
     PRODUCT_MANAGER("productCharge", "产品经理"),
     AUDITOR("auditor", "审核人"),
-    TASK_CHARGE("taskCharge", "任务负责人");
+    TASK_CHARGE("taskCharge", "任务负责人"),
+    CONCERN("concern", "关注人");
 
     private String flag;
     private String name;
