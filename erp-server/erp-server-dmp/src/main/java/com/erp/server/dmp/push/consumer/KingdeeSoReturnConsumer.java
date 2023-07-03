6db4ada754f3ef7ab5bb48d237a4f55388ccdb8d
luoweigang <821969511@qq.com>
2023-07-03 12:10:11 +0800

    修改产品管理

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeSoReturnConsumer.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeSoReturnConsumer.java
@@ -52,7 +52,7 @@ public class KingdeeSoReturnConsumer implements RocketMQListener<Map<String, Obj
         KingdeeApiUtils apiUtils = new KingdeeApiUtils(KingdeePushModuleEnum.SAL_RETURNSTOCK.getCode());
         LinkedList<String> queryFilters = new LinkedList<>();
 //        queryFilters.add(String.format("FDocumentStatus = '%s'", "C"));
-        queryFilters.add(StrUtil.format("FDocumentStatus in ({})", "'A','B','C','D'"));
+        queryFilters.add(StrUtil.format("FDocumentStatus in ({})", "'B','C','D'"));
         String filterStr = String.join(" and ", queryFilters);
         String fieldKeys = "FID,FBillTypeID,FBillTypeID.FName,FBillTypeID.FNumber,FBillNo,FDate,FDocumentStatus,FSaleOrgId,FSaleOrgId.FName,FRetcustId," +
                 "FRetcustId.FName,FRetcustId.FNumber,FSalesManId,FSalesManId.FName,FCreateDate,FModifyDate,FCancelStatus,FReceiverCountry,FLinkMan,FExchangeRate," +
