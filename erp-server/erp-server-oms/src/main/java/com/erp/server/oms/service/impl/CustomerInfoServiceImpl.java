bb37d5514928a4816e8789f1ac185d849e68f2d3
Will <wangwei@ulanzi.cn>
2023-07-04 19:45:29 +0800

    客户反审核调整

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/CustomerInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/CustomerInfoServiceImpl.java
@@ -669,16 +669,6 @@ public class CustomerInfoServiceImpl extends SuperServiceImpl<CustomerInfoMapper
             throw new ServiceException(ApiError.ERROR_98014);
         }
 
-        //撤销现有流程
-        LoginUser userInfo = commonService.getUserInfo();
-        ids.forEach(obj ->{
-            ProcessManagementDTO.RevokeDTO revokeDTO = new ProcessManagementDTO.RevokeDTO();
-            revokeDTO.setBusinessId(obj);
-            revokeDTO.setBusinessKey(SourceTypeEnum.CUSTOMER_INFO.getCode());
-            revokeDTO.setUserId(userInfo.getUid());
-            workflowFeign.revokeProcess(revokeDTO);
-        });
-
         List<Pair<String, String>> pairList = list.stream().filter(s -> s.getApproveStatus().equals(ApproveStatusEnum.getByStatus(approveIngStatus))).
                 map(obj -> new Pair<>(obj.getId(), "")).collect(Collectors.toList());
 
