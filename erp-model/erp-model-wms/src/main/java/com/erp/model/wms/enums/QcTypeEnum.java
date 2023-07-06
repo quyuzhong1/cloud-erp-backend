a02fb940394080a3cf0e49dc6236f2ac44965b98
luoweigang <821969511@qq.com>
2023-07-06 17:03:47 +0800

    修改质检单

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/QcTypeEnum.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/QcTypeEnum.java
@@ -21,7 +21,7 @@ public enum QcTypeEnum {
     INSIDE_QC("insideQc", "在库质检",true),
     NEW_PRODUCT_STOCK_IN("newProductStockIn", "新品入库质检",true),
     B2B_OUTSIDE_QC("b2bOutsideQc", "B2B外检",false),
-    RETURN_QC("returnQc", "退货质检",false);
+    RETURN_QC("returnQc", "退货质检",true);
 
 
     /**
