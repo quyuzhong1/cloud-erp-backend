70bdefd621665544f85da095f1f3271052f4d5bb
luoweigang <821969511@qq.com>
2023-07-06 19:23:26 +0800

    修改质检单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
@@ -1567,7 +1567,9 @@ public class QcInfoServiceImpl extends SuperServiceImpl<QcInfoMapper, QcInfoEnti
             view.setReceiveQty(soReturnReceiveDetailEntity.getReceiveQty());
             view.setRealQty(soReturnReceiveDetailEntity.getReceiveQty());
             view.setReturnTypeDict(soReturnDetailEntity.getReturnTypeDict());
+            view.setReturnTypeDictName(ReturnTypeEnum.getName(soReturnDetailEntity.getReturnTypeDict()));
             view.setReturnReasonDict(soReturnDetailEntity.getReturnReasonDict());
+            view.setReturnReasonDictName(ReturnReasonEnum.getName(soReturnDetailEntity.getReturnReasonDict()));
             view.setWarehouseId(view.getWarehouseId());
             List<WarehouseDTO.UpdateDTO> warehouseList = warehouseService.listWarehouseByIds(Arrays.asList(view.getWarehouseId()));
             if (CollectionUtils.isNotEmpty(warehouseList)) {
