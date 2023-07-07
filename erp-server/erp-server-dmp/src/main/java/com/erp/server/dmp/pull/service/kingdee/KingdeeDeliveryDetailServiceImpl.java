3b66e4a4607734f60557f935ed89f2a7af8263d4
yl <ylstrive@163.com>
2023-07-07 20:41:51 +0800

    2023-07-07 Lambda 添加字段 数据来源类型

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/kingdee/KingdeeDeliveryDetailServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/kingdee/KingdeeDeliveryDetailServiceImpl.java
@@ -228,7 +228,7 @@ public class KingdeeDeliveryDetailServiceImpl implements IReportSaveService<King
                 "FSrcBillNo,FCustMatName,F_ulz_BaseProperty1,FMaterialID,FMaterialID.FNumber,FMaterialID.FName,FStockLocID," +
                 "FBarcode,FMateriaModel,FMateriaType,FRealQty,FUnitID.FName,FPrice,FIsFree,FArrivalStatus,FArrivalDate," +
                 "FAmount,FStockStatusID,FStockStatusID.FName,FStockID.FName,FStockID.FNumber,F_ulz_Text1,FEntryCostAmount,FEntrynote,FSrcBillNo,FSrcType,FTaxPrice," +
-                "FCostPrice,FCostAmount_LC,FSalCostPrice,FDocumentStatus";
+                "FCostPrice,FCostAmount_LC,FSalCostPrice,FDocumentStatus,F_ULZ_data_sources";
 
         Boolean dataSign = true;
         //当前页数
