1bfda1be2e860db70467b7f20bd71274677b25b0
Will <wangwei@ulanzi.cn>
2023-07-10 13:04:04 +0800

    采购报价信息推送调整

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/kingdee/impl/SyncKingdeePurchasePriceServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/kingdee/impl/SyncKingdeePurchasePriceServiceImpl.java
@@ -83,11 +83,13 @@ public class SyncKingdeePurchasePriceServiceImpl implements SyncKingdeePurchaseP
         //采购组织
         resultMap.put("purchaseOrgName",entity.getPurchaseOrgName());
 
-        FindUserDTO findUserDTO = sysUserFeign.getUserByUserId(entity.getPricingUserId());
+        if (StringUtils.isNotBlank(entity.getPricingUserId())) {
+            FindUserDTO findUserDTO = sysUserFeign.getUserByUserId(entity.getPricingUserId());
 
-        if (ObjectUtils.isNotEmpty(findUserDTO)) {
-            //定价员
-            resultMap.put("pricingUserCode",findUserDTO.getCode());
+            if (ObjectUtils.isNotEmpty(findUserDTO)) {
+                //定价员
+                resultMap.put("pricingUserCode",findUserDTO.getCode());
+            }
         }
 
         //价目明细
