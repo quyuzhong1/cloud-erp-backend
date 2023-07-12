91f158914d0d9850b3601aaf33dfbfb466723b38
Will <wangwei@ulanzi.cn>
2023-07-12 21:17:05 +0800

    删除多余含税单价

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoInfoDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoInfoDTO.java
@@ -245,11 +245,6 @@ public class SoInfoDTO implements Serializable {
         private BigDecimal amount;
 
         /**
-         * 含税单价
-         */
-        private BigDecimal taxPrice;
-
-        /**
          * 含税销售金额
          */
         private BigDecimal taxAmount;
