af016a5c7f640715d48f95aeb610cc43f63a70bb
zhangchunlin <Polazhang89>
2023-07-13 13:04:51 +0800

    库存忽略费用或服务的sku
    (cherry picked from commit 186220dab6b8f3d12e49abdad45cef567eec275c)

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/AbstractInventoryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/AbstractInventoryServiceImpl.java
@@ -127,7 +127,12 @@ public abstract class AbstractInventoryServiceImpl {
         log.warn("库存交易反审核，单据类型：{}, 单据id：{}", dto.getSourceType().getName(), dto.getBillId());
         // 根据单据类型和单据id查询出未反审核过的对应的交易流水，一个单据对应多个SKU， 按创建时间正序排序
         List<TransactionFlowEntity> txnFlows = transactionFlowService.getUnApprovedTxnFlows(dto.getSourceType().getCode(), dto.getBillId());
-        ValidatorUtil.isTrue(CollUtil.isNotEmpty(txnFlows),()->new ServiceException(ApiError.ERROR_99040));
+        if(CollUtil.isEmpty(txnFlows)) {
+            // 产品属性为费用或服务的sku没有交易流水
+            log.warn("库存交易反审核，单据类型：{}, 单据id：{}，未找到未审核过的交易流水，不处理", dto.getSourceType().getName(), dto.getBillId());
+            return;
+        }
+        // ValidatorUtil.isTrue(CollUtil.isNotEmpty(txnFlows),()->new ServiceException(ApiError.ERROR_99040));
         // 先按交易时间升序排
         txnFlows = txnFlows.stream().sorted(Comparator.comparing(TransactionFlowEntity::getTradeTime).thenComparing(TransactionFlowEntity::getId)).collect(Collectors.toList());
         // 关联交易号
