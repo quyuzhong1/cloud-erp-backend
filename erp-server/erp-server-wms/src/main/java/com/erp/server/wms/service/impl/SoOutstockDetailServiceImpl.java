5dec26af491aa41f03e1c3d56a08d9db5755882c
luoweigang <821969511@qq.com>
2023-07-13 19:39:11 +0800

    修改销售出库打印排序

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
@@ -550,7 +550,7 @@ public class SoOutstockDetailServiceImpl extends SuperServiceImpl<SoOutstockDeta
      */
     private List<SoOutstockDetailEntity> listBaseByMainId(String mainId) {
         if (StringUtils.isNotBlank(mainId)) {
-            return this.lambdaQuery().eq(SoOutstockDetailEntity::getMainId, mainId).list();
+            return this.lambdaQuery().eq(SoOutstockDetailEntity::getMainId, mainId).orderByAsc(SoOutstockDetailEntity::getId).list();
         }
         return Collections.emptyList();
 
