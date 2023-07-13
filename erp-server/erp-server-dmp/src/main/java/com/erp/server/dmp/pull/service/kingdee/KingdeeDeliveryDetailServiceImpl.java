17277da17b52c80d9d6782f29c57731f65901558
Cloud <cloud@ulanzi.cn>
2023-07-13 17:59:27 +0800

    [ 金蝶审核时间  ]

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/kingdee/KingdeeDeliveryDetailServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/kingdee/KingdeeDeliveryDetailServiceImpl.java
@@ -214,11 +214,11 @@ public class KingdeeDeliveryDetailServiceImpl implements IReportSaveService<King
         queryFilters.add(StrUtil.format(" FModifyDate >= '{}'", sdf.format(lastTime.minusMinutes(2))));
         queryFilters.add(StrUtil.format(" FModifyDate <= '{}'", sdf.format(nextTime)));
         //审核状态
-        queryFilters.add(StrUtil.format(" FDocumentStatus in ({})", "'A','B','C','D'"));
+        queryFilters.add(StrUtil.format(" FDocumentStatus in ({})", "'B','C','D'"));
         // 过滤组织内订单
         queryFilters.add(StrUtil.format(" FISGENFORIOS = {}", "0"));
         String filterStr = String.join(" and ", queryFilters);
-//        filterStr = filterStr.concat(StrUtil.format(" OR (FApproveDate >= '{}' and FApproveDate < '{}')", sdf.format(lastTime), sdf.format(nextTime)));
+        filterStr = filterStr.concat(StrUtil.format(" OR (FApproveDate >= '{}' and FApproveDate < '{}')", sdf.format(lastTime), sdf.format(nextTime)));
         log.info("拉取金蝶条件为>>>>>>>>>>{}", filterStr);
         String fieldKeys = "FID,FBillTypeID,FBillTypeID.FName,FBillNo,FSoOrDerNo,FDate,FSaleOrgId,FSaleOrgId.FName,FCarriageNO,FStockerID.FNumber,FStockerID.FName," +
                 "FCustomerID,FCustomerID.FName,FCustomerID.FNumber,FSaleDeptID.FName,FSalesManID,FSalesManID.FName,FSalesManID.FNumber,FReceiverID.FName," +
