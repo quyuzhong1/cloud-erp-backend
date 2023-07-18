d67e79ee6fd0cf8a5e44efb79780bd52d846699f
zhangchunlin <Polazhang89>
2023-07-18 22:07:59 +0800

    新增仓库表清除缓存定时器

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/schedule/BaseDataJob.java
@@ -0,0 +1,78 @@
+package com.erp.server.wms.schedule;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.common.business.service.RedisService;
+import com.common.core.utils.StrUtils;
+import com.erp.model.wms.entity.WarehouseEntity;
+import com.erp.model.wms.enums.WmsRedisKeyEnum;
+import com.erp.server.wms.service.WarehouseService;
+import com.google.common.collect.Lists;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * 基础数据定时器
+ * @CreateTime: 2023-07-18  21:16
+ * @Author: zhangchunlin
+ */
+@Component
+@Slf4j
+public class BaseDataJob {
+
+    @Autowired
+    private WarehouseService warehouseService;
+
+    @Autowired
+    private RedisService redisService;
+
+    /**
+     * 仓库缓存清除
+     */
+    @XxlJob("warehouseCacheClean")
+    public ReturnT<String> warehouseCacheClean() {
+        String jobParam = XxlJobHelper.getJobParam();
+        String warehouseName = StrUtils.null2EmptyWithTrim(jobParam);
+        if(StrUtil.isNotEmpty(warehouseName)) {
+            log.warn("定时器传入的需要清除缓存的仓库名称：{}", warehouseName);
+            WarehouseEntity warehouseEntity = warehouseService.lambdaQuery().eq(WarehouseEntity::getName, warehouseName).last("LIMIT 1").one();
+            if(Objects.nonNull(warehouseEntity)) {
+                redisService.deleteObject(WmsRedisKeyEnum.WMS_WAREHOUSE_DETAIL_ID.keyBuilder(warehouseEntity.getId()));
+                log.warn("清除仓库：{}缓存成功", warehouseName);
+            }
+            return ReturnT.SUCCESS;
+        }
+
+        List<WarehouseEntity> list = warehouseService.lambdaQuery().list();
+        if (CollUtil.isEmpty(list)) {
+            log.info("没有启用的仓库");
+            return ReturnT.SUCCESS;
+        }
+
+        List<String> removeRedisKeys = Lists.newArrayList();
+        list.forEach(warehouse->{
+            try {
+                String redisKey = WmsRedisKeyEnum.WMS_WAREHOUSE_DETAIL_ID.keyBuilder(warehouse.getId());
+                Object obj = redisService.getCacheObject(redisKey);
+                if (Objects.nonNull(obj) ) {
+                    removeRedisKeys.add(redisKey);
+                }
+            } catch (Exception e) {
+                XxlJobHelper.log("仓库【{}】缓存清除失败",warehouse.getName());
+                log.error("仓库【{}】缓存清除失败",warehouse.getName());
+            }
+        });
+        if(CollUtil.isNotEmpty(removeRedisKeys)) {
+            redisService.deleteObject(removeRedisKeys);
+        }
+        return ReturnT.SUCCESS;
+    }
+
+}
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/AbstractInventoryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/AbstractInventoryServiceImpl.java
@@ -385,12 +385,13 @@ public abstract class AbstractInventoryServiceImpl {
                 // 判断是否含有小于等于0库存
                 inventoryDetails = inventoryDetailService.findListQtyLeZero(inventory.getId());
                 if(CollUtil.isEmpty(inventoryDetails)) {
-                    log.warn("仓库【{}】SKU【{}】允许负库存，且没有大于0的库存明细，也没有小于等于0的库存明细", warehouseInfo.getName(), param.getSkuNo());
+                    log.warn("仓库【{}】SKU【{}】允许负库存，即时库存id【{}】,且没有大于0的库存明细，也没有小于等于0的库存明细", warehouseInfo.getName(), param.getSkuNo(), inventory.getId());
                     String errMsg=StrUtil.format(ApiError.ERROR_99035.msg, param.getSkuNo(), warehouseInfo.getName(), param.getWarehouseLocation(), inventoryStatusName,(Objects.isNull(inventory)?0:inventory.getQty()),param.getQty());
                     log.error(errMsg);
                     throw new ServiceException(ApiError.ERROR_99035.code, errMsg);
                 } else {
                     // 取最后一条负库存明细
+                    log.warn("仓库【{}】SKU【{}】允许负库存，即时库存id【{}】,有小于等于0的库存明细，从最后一条库存明细出库", warehouseInfo.getName(), param.getSkuNo(), inventory.getId());
                     InventoryDetailEntity lastInventoryDetailEntity =  inventoryDetails.get(inventoryDetails.size() - 1);
                     inventoryDetails = new ArrayList<>();
                     inventoryDetails.add(lastInventoryDetailEntity);
@@ -408,6 +409,7 @@ public abstract class AbstractInventoryServiceImpl {
                 // 仓库负库存是否允许
                 if(inventoryHelper.allowNegativeInventory(param.getWarehouseId())) {
                     if (i == inventoryDetails.size() -1 && tradeQty < waitOutQty ){
+                        log.warn("仓库【{}】SKU【{}】允许负库存，即时库存id【{}】，剩余待出库数量【{}】，交易数量【{}】不足出库，从最后一条库存明细出库",warehouseInfo.getName(), param.getSkuNo(), inventory.getId(), waitOutQty, tradeQty);
                         tradeQty = waitOutQty;
                     }
                 }
