2cee15fe015f9fd0ec8b6be5705c5bbc99555e65
Will <wangwei@ulanzi.cn>
2023-07-19 18:35:58 +0800

    付款条件调整

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/DropDownListController.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/DropDownListController.java
@@ -73,13 +73,13 @@ public class DropDownListController extends BaseController {
      * @return
      */
     @GetMapping("/supplier/list")
-    public ApiResult<List<BaseDropDownDTO.RemarkDTO>> listSupplierDropDown() {
+    public ApiResult<List<BaseDropDownDTO.DisabledDTO>> listSupplierDropDown() {
         List<Map<String, Object>> mapList = supplierService.listApproveSupplier();
         if (CollectionUtils.isEmpty(mapList)) {
             return success(new ArrayList<>());
         }
-        List<BaseDropDownDTO.RemarkDTO> result = mapList.stream()
-                .map(x -> new BaseDropDownDTO.RemarkDTO(x.get("id").toString(), x.get("name").toString(), x.get("paymentCondition").toString(),(Boolean)x.get("disabled")))
+        List<BaseDropDownDTO.DisabledDTO> result = mapList.stream()
+                .map(x -> new BaseDropDownDTO.DisabledDTO(x.get("id").toString(), x.get("name").toString(),(Boolean)x.get("disabled")))
                 .collect(Collectors.toList());
         return success(result);
     }
@@ -91,13 +91,13 @@ public class DropDownListController extends BaseController {
      * @return
      */
     @GetMapping("/supplier/allList")
-    public ApiResult<List<BaseDropDownDTO.DisabledDTO>> listALLSupplierDropDown() {
+    public ApiResult<List<BaseDropDownDTO.RemarkDTO>> listALLSupplierDropDown() {
         List<SupplierEntity> mapList = supplierService.list();
         if (CollectionUtils.isEmpty(mapList)) {
             return success(new ArrayList<>());
         }
-        List<BaseDropDownDTO.DisabledDTO> result = mapList.stream()
-                .map(x -> new BaseDropDownDTO.DisabledDTO(x.getId(), x.getName(),x.getDisabled()))
+        List<BaseDropDownDTO.RemarkDTO> result = mapList.stream()
+                .map(x -> new BaseDropDownDTO.RemarkDTO(x.getId(), x.getName(),x.getPaymentCondition(),x.getDisabled()))
                 .collect(Collectors.toList());
         return success(result);
     }
