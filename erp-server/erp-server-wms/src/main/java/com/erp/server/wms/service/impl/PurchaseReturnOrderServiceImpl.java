f99e1c9f525ca83e1e76eb19d41fadcd65b95b9f
luoweigang <821969511@qq.com>
2023-07-20 21:52:18 +0800

    修改采购退货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
@@ -380,7 +380,7 @@ public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PurchaseRet
             }
         }
 
-        if (SourceTypeEnum.QC_INFO.getCode().equals(purchaseReturnOrderEntity.getSourceType())) {
+        if (ReturnOrderSourceEnum.QC.getCode().equals(purchaseReturnOrderEntity.getSourceType())) {
             viewDTO.setSourceType(ReturnOrderSourceEnum.QC.getCode());
             viewDTO.setSourceTypeName(ReturnOrderSourceEnum.QC.getName());
         } else {
