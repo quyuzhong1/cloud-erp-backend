5c1fffdea1c1cfa589ff2cf7d1d8404be86d72c2
luoweigang <821969511@qq.com>
2023-07-20 15:22:31 +0800

    修改工作台

--- erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/WorkOptionDTO.java
+++ erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/WorkOptionDTO.java
@@ -112,6 +112,10 @@ public class WorkOptionDTO extends BaseEntity<WorkOptionDTO> {
          * 模块状态名称
          */
         private String moduleStatusName;
+        /**
+         * 系统菜单id
+         */
+        private String sysMenuId;
     }
 
     @Data
