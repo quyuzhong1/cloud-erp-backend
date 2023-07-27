8fb3726d83e01ed420a7056b6cf85068ed4a37bc
yl <ylstrive@163.com>
2023-07-27 12:42:16 +0800

    2023-07-27 Lambda 销售退货单 问题修复

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
@@ -1604,14 +1604,18 @@ public class QcInfoServiceImpl extends SuperServiceImpl<QcInfoMapper, QcInfoEnti
             SoReturnReceiveEntity soReturnReceiveEntity = soReturnReceiveEntities.stream().filter(req -> req.getId().equals(view.getSourceId())).findFirst().orElse(new SoReturnReceiveEntity());
             SoReturnReceiveDetailEntity soReturnReceiveDetailEntity = soReturnReceiveDetailEntities.stream().filter(req -> req.getId().equals(view.getSourceDetailId())).findFirst().orElse(new SoReturnReceiveDetailEntity());
             SoReturnEntity soReturnEntity = returnEntityList.stream().filter(req -> req.getId().equals(soReturnReceiveEntity.getSourceId())).findFirst().orElse(new SoReturnEntity());
-            if (StringUtils.isNotBlank(soReturnReceiveDetailEntity.getReturnTypeDict())) {
-                view.setReturnTypeDictName(ReturnTypeEnum.getName(soReturnReceiveDetailEntity.getReturnTypeDict()));
+            //退货方式
+            String returnTypeDict = soReturnReceiveDetailEntity.getReturnTypeDict();
+            if (StringUtils.isNotBlank(returnTypeDict)) {
+                view.setReturnTypeDictName(ReturnTypeEnum.getName(returnTypeDict));
+                view.setReturnTypeDict(returnTypeDict);
             }
-            if (StringUtils.isNotBlank(soReturnReceiveDetailEntity.getReturnTypeDict())) {
-                view.setReturnReasonDictName(ReturnReasonEnum.getName(soReturnReceiveDetailEntity.getReturnReasonDict()));
+            //退货原因
+            String returnReason = soReturnReceiveDetailEntity.getReturnReasonDict();
+            if (StringUtils.isNotBlank(returnReason)) {
+                view.setReturnReasonDictName(ReturnReasonEnum.getName(returnReason));
+                view.setReturnReasonDict(returnReason);
             }
-
-
             view.setId(view.getId());
             view.setMainId(view.getId());
             view.setSourceId(soReturnReceiveEntity.getSourceId());
