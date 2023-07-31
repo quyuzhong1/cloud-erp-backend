ad8d36f611e08b233b978777dce4059f7680f74c
zhangchunlin <Polazhang89>
2023-07-31 11:36:14 +0800

    单据tab标签修复问题20230731

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderServiceImpl.java
@@ -1495,6 +1495,10 @@ public class PurchaseOrderServiceImpl extends SuperServiceImpl<PurchaseOrderMapp
             }
             params.setIdList(businessIds);
         }
+        // 待提交
+        if (PurchaseListTypeEnum.WAIT_SUBMIT.getCode().equals(params.getSearchType())) {
+            approveStatusList.add(ApproveStatusEnum.WAIT_SUBMIT.getStatus());
+        }
         //待到货
         if (PurchaseListTypeEnum.TO_BE_CREATE.getCode().equals(params.getSearchType())) {
             approveStatusList.add(ApproveStatusEnum.APPROVE.getStatus());
