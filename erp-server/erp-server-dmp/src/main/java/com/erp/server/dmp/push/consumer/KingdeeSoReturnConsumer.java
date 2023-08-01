b2b55ac05b2141d886c490d6c398eba91c78bded
luoweigang <821969511@qq.com>
2023-08-01 17:49:17 +0800

    修改销售退货定时补偿改成销售退货入库

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeSoReturnConsumer.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeeSoReturnConsumer.java
@@ -2,6 +2,7 @@ package com.erp.server.dmp.push.consumer;
 
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
@@ -20,6 +21,8 @@ import com.erp.server.dmp.push.service.kingdee.impl.KingdeeCommonServiceImpl;
 import com.erp.server.dmp.utils.KingdeeApiUtils;
 import com.erp.server.dmp.utils.KingdeeUtils;
 import com.kingdee.bos.webapi.entity.SaveParam;
+import com.kingdee.bos.webapi.entity.SaveResult;
+import com.kingdee.bos.webapi.sdk.K3CloudApi;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -116,8 +119,64 @@ public class KingdeeSoReturnConsumer implements RocketMQListener<Map<String, Obj
                 "F_ULZ_data_sources";
 
         System.out.println(filterStr);
-        List<Map<String, Object>> queryList = apiUtils.queryList(filterStr, fieldKeys, 100, 1,11);
-        System.out.println(queryList);
+     /*   List<Map<String, Object>> queryList = apiUtils.queryList(filterStr, fieldKeys, 100, 1,11);
+        System.out.println(queryList);*/
+
+
+
+        K3CloudApi client = new K3CloudApi();
+        JSONObject json = JSONUtil.parseObj("{ \"FBillTypeID\" :{ \"FNUMBER\" : \"XSTHD01_SYS\" },\n" +
+                "\"FStockOrgId\" :{ \"FNumber\" : \"113\" },\n" +
+                "\"FSaleOrgId\" :{ \"FNumber\" : \"112\" },\n" +
+                "\"FBillNo\" : \"XSTH23072800002\",\n" +
+                "\"FDate\" : \"2023-07-28\",\n" +
+                "\"FSaledeptid\" :{ \"FNumber\" : \"\" },\n" +
+                "\"FSalesManId\" :{ \"FNumber\" : \"zf_GW000117_1\" },\n" +
+                "\"FRetcustId\" :{ \"FNumber\" : \"CUST23070900002\" },\n" +
+                "\"FSettleOrgId\" :{ \"FNumber\" : \"112\" },\n" +
+                "\"FSettleCurrId\" :{ \"FNumber\" : \"PRE001\" },\n" +
+                "\"FReturnReason\" :{ \"FNumber\" : \"\" },\n" +
+                "\"SubHeadEntity\" :{ \"FExchangeTypeId\" :{ \"FNumber\" : \"HLTX01_SYS\" },\n" +
+                "\"FExchangeRate\" : \"1\" },\n" +
+                "\"F_ULZ_data_sources\" : \"UlanziERP\",\n" +
+                "\"FSettleTypeId\" :{ \"FNumber\" : \"JSFS01_SYS\" },\n" +
+                "\"FChageCondition\" :{ \"FNumber\" : \"\" },\n" +
+                "\"FStockerId\" :{ \"FNumber\" : \"00029\" },\n" +
+                "\"FEntity\" :[{ \"FMaterialId\" :{ \"FNumber\" : \"2109\" },\n" +
+                "\"FStockId\" :{ \"FNumber\" : \"jp-tmjdc\" },\n" +
+                "\"FIsFree\" : \"\",\n" +
+                "\"FReturnType\" :{ \"FNumber\" : \"THLX01_SYS\" },\n" +
+                "\"FOwnerTypeID\" : \"BD_OwnerOrg\",\n" +
+                "\"FOwnerId\" :{ \"FNumber\" : \"112\" },\n" +
+                "\"FDeliveryDate\" : \"2023-07-28\",\n" +
+                "\"FNote\" : \"\",\n" +
+                "\"FSrcBillTypeID\" : \"\",\n" +
+                "\"FSrcBillNo\" : \"\",\n" +
+                "\"FSalUnitID\" :{ \"FNumber\" : \"Pcs\" },\n" +
+                "\"FStockstatusId\" :{ \"FNumber\" : \"KCZT01_SYS\" },\n" +
+                "\"FUnitID\" :{ \"FNumber\" : \"Pcs\" },\n" +
+
+                "\"FPrice\" : \"\",\n" +
+                "\"FTaxPrice\" : 0,\n" +
+                "\"FMustqty\" : 4,\n" +
+                "\"FAmount\" : \"\",\n" +
+                "\"FSalUnitQty\" : 4,\n" +
+                "\"FPriceUnitQty\" : \"\",\n" +
+                "\"FRealQty\" : 4,\n" +
+                "\"FOrderNo\" : \"\",\n" +
+                "\"FStockLocId\" :{ \"FSTOCKLOCID__FF100014\" :{ \"FNumber\" : \"\" }}}]}");
+
+        //判断金蝶系统是否已存在该数据
+        SaveParam param = new SaveParam(json);
+        SaveResult result;
+        try {
+            result = client.save(KingdeePushModuleEnum.SAL_RETURNSTOCK.getCode(), param);
+            if (!result.isSuccessfully()) {
+                throw new RuntimeException("【保存】出错:" + JSONUtil.toJsonStr(result.getResult().getResponseStatus().getErrors()));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage());
+        }
     }
 
     @Override
--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/schedule/KingdeePushJob.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/schedule/KingdeePushJob.java
@@ -42,10 +42,6 @@ public class KingdeePushJob {
     @Resource
     private SyncKingdeeSoChangeService syncKingdeeSoChangeService;
     @Resource
-    private SoReturnService soReturnService;
-    @Resource
-    private SyncKingdeeSoReturnService syncKingdeeSoReturnService;
-    @Resource
     private SoInfoService soInfoService;
     @Resource
     private SyncKingdeeSoService syncKingdeeSoService;
@@ -141,32 +137,6 @@ public class KingdeePushJob {
             }
         });
     }
-
-
-    /**
-     * 推送销售退货单
-     */
-    @XxlJob("kingdeePushSoReturn")
-    public void kingdeePushSoReturn() {
-        List<SoReturnEntity> list = soReturnService.lambdaQuery()
-                .in(SoReturnEntity::getSyncKingdeeStatus, Arrays.asList(SyncKingdeeStatusEnum.TO_BE_SYNC.getCode(), SyncKingdeeStatusEnum.FAILED_SYNC.getCode()))
-                .or(obj -> obj.eq(SoReturnEntity::getSyncKingdeeStatus,SyncKingdeeStatusEnum.IN_SYNC.getCode()).le(SoReturnEntity::getSyncKingdeeTime, LocalDateTime.now().minusMinutes(10)))
-                .list();
-        if (ObjectUtils.isEmpty(list)) {
-            log.info("无需要同步的销售退货单");
-            return;
-        }
-        list.forEach(obj->{
-            try {
-                syncKingdeeSoReturnService.syncDataToKingdee(obj, obj.getSyncOperate());
-            } catch (Exception e) {
-                XxlJobHelper.log("销售退货单【{}】推送金蝶失败",obj.getCode());
-                log.error("销售退货单【{}】推送金蝶失败",obj.getCode());
-            }
-        });
-    }
-
-
 
     /**
      * 推送销售订单
