7d3b187d684c9527ddbc4f2d69535b2f3823dcd6
Will <wangwei@ulanzi.cn>
2023-08-02 14:43:29 +0800

    直接调拨单代码调整

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeeTransferInfoConsumerServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeeTransferInfoConsumerServiceImpl.java
@@ -1,10 +1,8 @@
 package com.erp.server.dmp.push.service.business.impl;
 
 import cn.hutool.json.JSONObject;
-import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.common.business.enums.SyncKingdeeOperateEnum;
 import com.common.core.enums.ApiError;
 import com.common.core.utils.FastJsonUtil;
@@ -49,8 +47,8 @@ public class KingdeeTransferInfoConsumerServiceImpl implements KingdeeTransferIn
         //模块类型
         Integer type = ApiModuleTypeEnum.TRANSFER_INFO.getCode();
 
-        //业务编码
-        String code = (String) map.get("code");
+        //业务id
+        String  businessId = String.valueOf(map.get("id"));
 
         PlatformEntity platformEntity = kingdeeCommonService.getPlatformEntity(map, type);
         if (ObjectUtils.isEmpty(platformEntity)) {
@@ -59,109 +57,121 @@ public class KingdeeTransferInfoConsumerServiceImpl implements KingdeeTransferIn
         //读取配置，初始化SDK
         KingdeeApiUtils apiUtils = new KingdeeApiUtils(KingdeePushModuleEnum.STK_TRANSFERDIRECT.getCode());
 
-        //操作项
-        String operate = (String) map.get("operate");
-        /**
-         * 作废
-         */
-        if (SyncKingdeeOperateEnum.OPERATE_INVALID.getCode().equals(operate)) {
-            operateInvalid(apiUtils, platformEntity, map, type, code, operate);
-        }
-        /**
-         * 反审核
-         */
-        if (SyncKingdeeOperateEnum.OPERATE_DISAPPROVE.getCode().equals(operate)) {
-            operateDisapprove(apiUtils, platformEntity, map, type);
-        }
-        /**
-         * 审核
-         */
-        if (SyncKingdeeOperateEnum.OPERATE_APPROVE.getCode().equals(operate)) {
-            operateApprove(apiUtils, platformEntity, map, type);
-        }
-
-    }
-
-
-    /**
-     * @param apiUtils
-     * @param platformEntity
-     * @param map
-     * @param type
-     * @param code
-     * @param operate
-     * @description: 作废
-     * @author Will
-     * @date: 2023/5/24 17:57
-     */
-    public void operateInvalid(KingdeeApiUtils apiUtils, PlatformEntity platformEntity, Map<String, Object> map, Integer type, String code, String operate) {
-        //作废
-        kingdeeCommonService.excuteOperation(apiUtils, platformEntity, map, type, code, operate);
-        return;
-    }
-
-    /**
-     * @param apiUtils
-     * @param platformEntity
-     * @param map
-     * @param type
-     * @description: 反审核
-     * @author Will
-     * @date: 2023/5/24 17:57
-     */
-    public void operateDisapprove(KingdeeApiUtils apiUtils, PlatformEntity platformEntity, Map<String, Object> map, Integer type) {
-        String syncKingdeeId = (String) map.get("syncKingdeeId");
-        if (StringUtils.isBlank(syncKingdeeId)) {
-            return;
-        }
-        //反审核
-        kingdeeCommonService.unAudit(platformEntity, map, apiUtils, syncKingdeeId, type);
-        return;
-    }
-
-
-    /**
-     * @param apiUtils
-     * @param platformEntity
-     * @param map
-     * @param type
-     * @description: 审核
-     * @author Will
-     * @date: 2023/5/24 18:10
-     */
-    public void operateApprove(KingdeeApiUtils apiUtils, PlatformEntity platformEntity, Map<String, Object> map, Integer type) {
-        //业务id
-        String businessId = String.valueOf(map.get("id"));
-
         //根据录入值和字段配置生成JSONObject
-        JSONObject json = kingdeeCommonService.makeApiFieldJson(map, platformEntity.getId(), type);
+        JSONObject json = kingdeeCommonService.makeApiFieldJson(map, platformEntity.getId(),type);
+
 
         //未配置发送字段
         if (CollectionUtils.isEmpty(json)) {
             log.error(ApiError.ERROR_97025.msg);
             //错误日志
-            kingdeeCommonService.insertLogWriteBackSyncKingdeeStatus(platformEntity, businessId, "", "未配置同步字段", type, ApiSendStatusEnum.FAILURE.getCode());
+            kingdeeCommonService.insertLogWriteBackSyncKingdeeStatus(platformEntity, businessId,"","未配置同步字段",type, ApiSendStatusEnum.FAILURE.getCode());
             return;
         }
-
         //判断金蝶系统是否已存在该数据
         SaveParam param = new SaveParam(json);
+        Boolean isAdd = Boolean.FALSE;
         JSONObject model;
         try {
-            model = kingdeeCommonService.view(apiUtils, platformEntity.getId(), map);
+            model = kingdeeCommonService.view(apiUtils,platformEntity.getId(),map);
         } catch (Exception e) {
-            //更新数据
-            kingdeeCommonService.saveOrUpdate(platformEntity, map, apiUtils, json, param, type);
+            //新增数据
+            Boolean isSaveOrUpdate = kingdeeCommonService.saveOrUpdate(platformEntity,map,apiUtils , json, param,type);
+            if (!isSaveOrUpdate) {
+                return;
+            }
+            isAdd = Boolean.TRUE;
+            model = kingdeeCommonService.view(apiUtils,platformEntity.getId(),map);
+            //执行操作
+            operate (platformEntity,map,apiUtils,model,json,isAdd);
             return;
         }
-        //查找到数据后，判断其审核状态
-        String documentStatus = (String) model.get("DocumentStatus");
-        String id = String.valueOf(model.get("Id"));
-        Boolean flag = Boolean.FALSE;
+        //执行操作
+        operate (platformEntity,map,apiUtils,model,json,isAdd);
+    }
 
+    /**
+     * 直接调拨单操作
+     */
+    public void operate (PlatformEntity platformEntity, Map<String, Object> map, KingdeeApiUtils apiUtils,JSONObject model,JSONObject json,Boolean isAdd) {
+        //查找到数据后，判断其审核状态
+        String documentStatus = (String)model.get("DocumentStatus");
+        String id = String.valueOf(model.get("Id")) ;
+        //操作项
+        String operate = (String) map.get("operate");
+        //作废
+        if (SyncKingdeeOperateEnum.OPERATE_INVALID.getCode().equals(operate)) {
+            operateInvalid(platformEntity,map,apiUtils,id,documentStatus);
+        }
+        //反审核
+        if (SyncKingdeeOperateEnum.OPERATE_DISAPPROVE.getCode().equals(operate)) {
+            operateDisapprove(platformEntity,map,apiUtils,id,documentStatus);
+        }
+        //审核
+        if (SyncKingdeeOperateEnum.OPERATE_APPROVE.getCode().equals(operate) && !isAdd) {
+            operateApprove(platformEntity,map,apiUtils,id,documentStatus,json);
+        }
+    }
+
+
+    /**
+     * @param apiUtils
+     * @param platformEntity
+     * @param map
+     * @description: 作废
+     * @author Will
+     * @date: 2023/5/24 17:57
+     */
+    public void operateInvalid (PlatformEntity platformEntity, Map<String, Object> map, KingdeeApiUtils apiUtils, String id,String documentStatus){
+        if (KingdeeDocStatusEnum.REAPPROVE.getCode().equals(documentStatus)) {
+            //提交审核
+            Boolean submit = kingdeeCommonService.submit(platformEntity, map, apiUtils, id, ApiModuleTypeEnum.TRANSFER_INFO.getCode());
+            if (!submit) {
+                return;
+            }
+        }
+        //作废
+        kingdeeCommonService.excuteOperation(apiUtils, platformEntity, map, ApiModuleTypeEnum.TRANSFER_INFO.getCode(),(String) map.get("code"),(String) map.get("operate"));
+        return;
+    }
+
+    /**
+     * @param apiUtils
+     * @param platformEntity
+     * @param map
+     * @description: 反审核
+     * @author Will
+     * @date: 2023/5/24 17:57
+     */
+    public void operateDisapprove (PlatformEntity platformEntity, Map<String, Object> map, KingdeeApiUtils apiUtils, String id,String documentStatus){
+        if (KingdeeDocStatusEnum.REAPPROVE.getCode().equals(documentStatus)) {
+            //提交审核
+            Boolean submit = kingdeeCommonService.submit(platformEntity, map, apiUtils, id, ApiModuleTypeEnum.TRANSFER_INFO.getCode());
+            if (!submit) {
+                return;
+            }
+        }
+        //反审核
+        kingdeeCommonService.unAudit(platformEntity, map, apiUtils, id, ApiModuleTypeEnum.TRANSFER_INFO.getCode());
+        return;
+    }
+
+
+    /**
+     * @param apiUtils
+     * @param platformEntity
+     * @param map
+     * @description: 审核
+     * @author Will
+     * @date: 2023/5/24 18:10
+     */
+    public void operateApprove (PlatformEntity platformEntity, Map<String, Object> map, KingdeeApiUtils apiUtils, String id,String documentStatus,JSONObject json){
+        //查找到数据后，判断其审核状态
+        Boolean flag = Boolean.FALSE;
+        SaveParam param = new SaveParam(json);
         //审核中或已审核则要先反审
         if (KingdeeDocStatusEnum.APPROVING.getCode().equals(documentStatus) || KingdeeDocStatusEnum.APPROVED.getCode().equals(documentStatus)) {
-            flag = kingdeeCommonService.unAudit(platformEntity, map, apiUtils, id, type);
+            flag = kingdeeCommonService.unAudit(platformEntity, map, apiUtils, id, ApiModuleTypeEnum.TRANSFER_INFO.getCode());
         }
         //创建状态则直接修改、删除
         if (KingdeeDocStatusEnum.CREATED.getCode().equals(documentStatus) || KingdeeDocStatusEnum.REAPPROVE.getCode().equals(documentStatus) || flag) {
@@ -173,7 +183,7 @@ public class KingdeeTransferInfoConsumerServiceImpl implements KingdeeTransferIn
             ArrayList<String> apiFieldList = (ArrayList) Arrays.stream(allKey.toString().split(",")).collect(Collectors.toList());
             param.setNeedUpDateFields(apiFieldList);
             //更新数据
-            kingdeeCommonService.saveOrUpdate(platformEntity, map, apiUtils, json, param, type);
+            kingdeeCommonService.saveOrUpdate(platformEntity, map, apiUtils, json, param, ApiModuleTypeEnum.TRANSFER_INFO.getCode());
         }
     }
 }
