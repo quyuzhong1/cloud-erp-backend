c0aa26d70b30a4c0bf54f0447ab37fae4a8ffb18
luoweigang <821969511@qq.com>
2023-08-03 17:57:25 +0800

    修改销售退货单

--- erp-rpc/erp-rpc-wms/src/main/java/com/erp/rpc/wms/feign/SoDeliveryNoticeFeign.java
+++ erp-rpc/erp-rpc-wms/src/main/java/com/erp/rpc/wms/feign/SoDeliveryNoticeFeign.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
 
 @FeignClient(name = "erp-wms", contextId = "soDeliveryNotice")
 public interface SoDeliveryNoticeFeign {
