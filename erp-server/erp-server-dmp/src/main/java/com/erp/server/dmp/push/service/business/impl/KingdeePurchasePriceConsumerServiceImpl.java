b253e195164c71a8a429a1c239b8d2fb99c9aedf
zhangchunlin <Polazhang89>
2023-08-08 11:18:20 +0800

    采购价目表反审核修复BUG

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeePurchasePriceConsumerServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeePurchasePriceConsumerServiceImpl.java
@@ -109,7 +109,12 @@ public class KingdeePurchasePriceConsumerServiceImpl implements KingdeePurchaseP
 
         if (SyncKingdeeOperateEnum.OPERATE_DISAPPROVE.getCode().equals(operate)) {
             //反审核
-            kingdeeCommonService.unAudit(platformEntity, map, apiUtils, id, type);
+            //审核中或已审核则要先反审
+            if (KingdeeDocStatusEnum.APPROVING.getCode().equals(documentStatus) || KingdeeDocStatusEnum.APPROVED.getCode().equals(documentStatus)) {
+                flag = kingdeeCommonService.unAudit(platformEntity, map, apiUtils, id, type);
+            } else {
+                kingdeeCommonService.insertLogWriteBackSyncKingdeeStatus(platformEntity, businessId, "", "已经反审核", type, ApiSendStatusEnum.SUCCESS.getCode());
+            }
             return;
         }
 
