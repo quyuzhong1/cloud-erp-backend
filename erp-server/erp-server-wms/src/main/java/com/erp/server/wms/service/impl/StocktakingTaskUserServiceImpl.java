cf075baf6c8ff0287a832eb741eb161051397474
yl <ylstrive@163.com>
2023-08-08 12:29:04 +0800

    2023-08-08 Lambda  盘点任务功能开发

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingTaskUserServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingTaskUserServiceImpl.java
@@ -4,7 +4,11 @@ import com.erp.model.wms.entity.StocktakingTaskUserEntity;
 import com.erp.server.wms.mapper.StocktakingTaskUserMapper;
 import com.erp.server.wms.service.StocktakingTaskUserService;
 import com.common.business.service.SuperServiceImpl;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class StocktakingTaskUserServiceImpl extends SuperServiceImpl<StocktakingTaskUserMapper, StocktakingTaskUserEntity> implements StocktakingTaskUserService {
 
+
+    /**
+     * 获取盘点人信息
+     *
+     * @param taskIdList
+     * @return java.util.List<com.erp.model.wms.entity.StocktakingTaskUserEntity>
+     * @author yl
+     * @date 2023-08-08 12:16
+     */
+    @Override
+    public List<StocktakingTaskUserEntity> listBaseByTaskIds(List<String> taskIdList) {
+        if (CollectionUtils.isEmpty(taskIdList)) {
+            return Collections.emptyList();
+        }
+        return this.lambdaQuery().in(StocktakingTaskUserEntity::getStocktakingTaskId,taskIdList).list();
+    }
 }
