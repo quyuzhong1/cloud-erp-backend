04abd3b918dc9f93b22a27505c7957c61d7a0e30
luoweigang <821969511@qq.com>
2023-08-15 15:23:15 +0800

    修改PDA销售退货签收单

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/pda/PdaSoReturnController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/pda/PdaSoReturnController.java
@@ -1,8 +1,15 @@
 package com.erp.server.oms.controller.pda;
 
 import com.common.core.controller.BaseController;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.oms.entity.SoReturnEntity;
+import com.erp.server.oms.service.SoReturnService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * PDA:销售退货单
@@ -12,5 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/pdaSoReturn")
 public class PdaSoReturnController extends BaseController {
+    @Resource
+    private SoReturnService soReturnService;
 
+    /**
+     * 获取所有已审核订单
+     * @Author Luo_WG
+     * @Date 2023/4/13 18:59
+     * @return com.common.core.controller.vo.ApiResult
+     **/
+    @GetMapping(value = "/listSoReturnByApproveStatus")
+    public ApiResult<List<SoReturnEntity>> listSoReturnByApproveStatus() {
+        List<SoReturnEntity> entityList = soReturnService.listSoReturnByApproveStatus();
+        return success(entityList);
+    }
 }
