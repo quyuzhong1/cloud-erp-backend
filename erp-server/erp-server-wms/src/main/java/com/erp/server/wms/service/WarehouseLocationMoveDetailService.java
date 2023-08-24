e4f2473387efeee62f220b115b428566bf96f4c2
luoweigang <821969511@qq.com>
2023-08-24 18:48:37 +0800

    PDA:修改销售退货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/WarehouseLocationMoveDetailService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/WarehouseLocationMoveDetailService.java
@@ -1,8 +1,12 @@
 package com.erp.server.wms.service;
+import com.erp.model.wms.entity.StocktakingPlanDetailEntity;
 import com.erp.model.wms.entity.WarehouseLocationMoveDetailEntity;
 import com.common.business.service.SuperService;
 import com.common.business.dto.base.*;
 import com.erp.model.wms.dto.WarehouseLocationMoveDetailDTO;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * <p>
@@ -19,18 +23,36 @@ public interface WarehouseLocationMoveDetailService extends SuperService<Warehou
     * @author Luo_WG
     * @date: 2023-08-24
     * @param dto
+    * @param mainId
     * @return
     */
-    String add(WarehouseLocationMoveDetailDTO.AddDTO dto);
+    void add(List<WarehouseLocationMoveDetailDTO.AddDTO> dto, String mainId);
 
     /**
     * 修改
     * @author Luo_WG
     * @date: 2023-08-24
-    * @param dto
+    * @param detailList
+    * @param mainId
     * @return
     */
-    Boolean update(WarehouseLocationMoveDetailDTO.UpdateDTO dto);
+    Boolean update(List<WarehouseLocationMoveDetailDTO.UpdateDTO> detailList, String mainId);
 
+    /**
+     * 根据主表id查询详情
+     * @Author Luo_WG
+     * @Date 2023/8/24 18:18
+     * @param mainIds
+     * @return java.util.List<com.erp.model.wms.entity.WarehouseLocationMoveDetailEntity>
+     **/
+    List<WarehouseLocationMoveDetailEntity> listByMainIds(List<String> mainIds);
 
+    /**
+     * 根据主表id删除详情
+     * @Author Luo_WG
+     * @Date 2023/8/24 18:18
+     * @param mainId
+     * @return java.lang.Boolean
+     **/
+     Boolean removeByMainId(String mainId);
 }
