ebb918168ba00b2b0fe80b79faeb2f41ad755c12
luoweigang <821969511@qq.com>
2023-08-25 09:47:24 +0800

    PDA:修改销售退货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseReceiveServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseReceiveServiceImpl.java
@@ -1367,6 +1367,7 @@ public class WarehouseReceiveServiceImpl extends SuperServiceImpl<WarehouseRecei
                 }
 
             } else {
+                updateDTO.setId("");
                 addDTOList.add(updateDTO);
             }
         }
