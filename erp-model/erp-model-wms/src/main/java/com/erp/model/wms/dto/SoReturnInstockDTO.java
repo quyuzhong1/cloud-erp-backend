2024724085d7efdfc0c3069145775e4dc700ea5a
luoweigang <821969511@qq.com>
2023-08-29 17:09:04 +0800

    PDA:修改仓位移动

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoReturnInstockDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoReturnInstockDTO.java
@@ -411,6 +411,10 @@ public class SoReturnInstockDTO {
          */
         private String type;
         /**
+         * 单据类型名称
+         */
+        private String typeName;
+        /**
          * 客户id
          */
         private String customerId;
