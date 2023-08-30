f9067d331ca965b43c3f2b6c6fc812969a4e073d
Will <wangwei@ulanzi.cn>
2023-08-30 19:06:21 +0800

    销售退货下推

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cRefCategoryServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cRefCategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.erp.server.oms.service.impl;
 
 import com.common.business.service.SuperServiceImpl;
+import com.common.core.enums.ApiError;
+import com.common.core.exception.ServiceException;
 import com.common.core.utils.BeanMapperUtils;
 import com.erp.model.oms.dto.SoB2cRefCategoryDTO;
 import com.erp.model.oms.entity.OrderCategoryDetailEntity;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -38,21 +41,12 @@ public class SoB2cRefCategoryServiceImpl extends SuperServiceImpl<SoB2cRefCatego
             return Boolean.TRUE;
         }
         List<SoB2cRefCategoryEntity> soB2cRefCategoryList = BeanMapperUtils.copyList(SoB2cRefCategoryEntity.class, addList);
+        //数据处理
         handleCategory(soB2cRefCategoryList);
 
         return this.saveOrUpdateBatch(soB2cRefCategoryList);
     }
 
-    private void handleCategory (List<SoB2cRefCategoryEntity> soB2cRefCategoryList) {
-        if (CollectionUtils.isEmpty(soB2cRefCategoryList)) {
-            return;
-        }
-        List<String> categoryIdList = soB2cRefCategoryList.stream().map(SoB2cRefCategoryEntity::getCategoryId).collect(Collectors.toList());
-        List<OrderCategoryDetailEntity> orderCategoryDetailList = orderCategoryDetailService.listByIds(categoryIdList);
-
-        for (SoB2cRefCategoryEntity entity : soB2cRefCategoryList) {
-        }
-    }
 
 
     @Override
@@ -68,5 +62,24 @@ public class SoB2cRefCategoryServiceImpl extends SuperServiceImpl<SoB2cRefCatego
         return lambdaUpdate().in(SoB2cRefCategoryEntity::getSoB2cId,mainIds).remove();
     }
 
-
+    /**
+     * 数据处理
+     */
+    private void handleCategory (List<SoB2cRefCategoryEntity> soB2cRefCategoryList) {
+        if (CollectionUtils.isEmpty(soB2cRefCategoryList)) {
+            return;
+        }
+        List<String> categoryIdList = soB2cRefCategoryList.stream().map(SoB2cRefCategoryEntity::getCategoryId).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(categoryIdList)) {
+            return;
+        }
+        List<OrderCategoryDetailEntity> orderCategoryDetailList = orderCategoryDetailService.listByIds(categoryIdList);
+        if (CollectionUtils.isEmpty(orderCategoryDetailList)) {
+            throw new ServiceException(ApiError.ERROR_SO_B2C_REF_CATEGORY_NOT_EXIST);
+        }
+        for (SoB2cRefCategoryEntity entity : soB2cRefCategoryList) {
+            String categoryName = orderCategoryDetailList.stream().filter(obj -> obj.getId().equals(entity.getCategoryId())).findFirst().flatMap(obj -> Optional.ofNullable(obj.getName())).orElse("");
+            entity.setCategoryName(categoryName);
+        }
+    }
 }
