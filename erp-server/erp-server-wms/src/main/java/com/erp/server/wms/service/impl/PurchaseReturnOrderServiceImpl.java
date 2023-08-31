dc0818aa16d30050463d7f2d2e5a0169b727c3e9
luoweigang <821969511@qq.com>
2023-08-31 18:05:04 +0800

    PDA:修改收货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
@@ -482,6 +482,7 @@ public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PurchaseRet
 
             WarehouseLocationEntity warehouseLocationEntity = warehouseLocationEntities.stream().filter(req -> req.getCode().equals(detailView.getWarehouseLocation())).findFirst().orElse(new WarehouseLocationEntity());
             detailView.setWarehouseLocationName(warehouseLocationEntity.getName());
+
             detailViewDTOS.add(detailView);
         }
         viewDTO.setPurchasePriceDetailList(detailViewDTOS);
