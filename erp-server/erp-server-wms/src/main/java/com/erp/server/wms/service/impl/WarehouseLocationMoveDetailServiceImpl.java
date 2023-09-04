fd771f3e9db79840110f15e39d2980c00331df62
luoweigang <821969511@qq.com>
2023-09-04 15:29:01 +0800

    PDA:修改收货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveDetailServiceImpl.java
@@ -114,7 +114,7 @@ public class WarehouseLocationMoveDetailServiceImpl extends SuperServiceImpl<War
             paramDTO.setSkuIds(Arrays.asList(detailEntity.getSkuId()));
             paramDTO.setWarehouseLocations(Arrays.asList(detailEntity.getOutWarehouseLocation()));
             List<InventoryDTO.PdaInventoryDTO> inventoryByParams = inventoryService.getInventoryByParam(paramDTO);
-            InventoryDTO.PdaInventoryDTO inventoryByParam = inventoryByParams.stream().filter(req -> req.getWarehouseId().equals(detailEntity.getOutWarehouseLocation())
+            InventoryDTO.PdaInventoryDTO inventoryByParam = inventoryByParams.stream().filter(req -> req.getWarehouseId().equals(warehouseId)
                     && req.getSkuId().equals(detailEntity.getSkuId())).findFirst().orElse(null);
             if (ObjectUtil.isEmpty(inventoryByParam) || detailEntity.getQty() > inventoryByParam.getUsableQty()) {
                 throw new ServiceException(ApiError.LOCATION_MOVE_QTY_ERROR, detailEntity.getSkuNo());
