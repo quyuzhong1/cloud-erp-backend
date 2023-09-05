a61a77e5f1694f30898189dee09b65ab5e34daa8
luoweigang <821969511@qq.com>
2023-09-05 17:24:28 +0800

    PDA:产品查询

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/PdaProductDetailDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/PdaProductDetailDTO.java
@@ -32,7 +32,7 @@ public class PdaProductDetailDTO implements Serializable {
         /**
          * 图片
          */
-        private String imageUrl;
+        private String imagesUrl;
         /**
          * 变体信息
          */
