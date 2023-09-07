2b1ddc258be58bd8d12e7907184c0f90886208b0
Will <wangwei@ulanzi.cn>
2023-09-07 15:58:19 +0800

    委外明细数据查询调整

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderServiceImpl.java
@@ -803,8 +803,11 @@ public class SubcontractOrderServiceImpl extends SuperServiceImpl<SubcontractOrd
                 poDetailList.add(poDetailAddDTO);
             }
             addDTO.setDetails(poDetailList);
-            String poId = purchaseOrderService.add(addDTO);
-            poIds.add(poId);
+            PurchaseOrderEntity purchaseOrderEntity = purchaseOrderService.add(addDTO);
+            if (ObjectUtils.isEmpty(purchaseOrderEntity)) {
+                throw new ServiceException(ApiError.ERROR_1019);
+            }
+            poIds.add(purchaseOrderEntity.getId());
         }
 
         //采购订单提交审核
