9273bad7c595c40f0280e1e16128cd7deb591597
luoweigang <821969511@qq.com>
2023-09-12 15:09:20 +0800

    PDA:发版信息

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/PdaUserSkipVersionServiceImpl.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/PdaUserSkipVersionServiceImpl.java
@@ -2,11 +2,10 @@ package com.erp.server.sys.service.impl;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.common.business.service.impl.SuperServiceImpl;
 import com.erp.model.sys.entity.PdaUserSkipVersionEntity;
 import com.erp.server.sys.mapper.PdaUserSkipVersionMapper;
 import com.erp.server.sys.service.PdaUserSkipVersionService;
-import com.common.business.service.SuperServiceImpl;
-import com.erp.server.sys.service.OperateLogService;
 import com.erp.server.sys.service.CommonService;
 import com.common.core.exception.ServiceException;
 import org.springframework.stereotype.Service;
@@ -29,8 +28,7 @@ import com.common.core.enums.ApiError;
 @Slf4j
 @Service
 public class PdaUserSkipVersionServiceImpl extends SuperServiceImpl<PdaUserSkipVersionMapper, PdaUserSkipVersionEntity> implements PdaUserSkipVersionService {
-    @Autowired
-    private OperateLogService operateLogService;
+
     @Autowired
     private CommonService commonService;
 
@@ -49,12 +47,6 @@ public class PdaUserSkipVersionServiceImpl extends SuperServiceImpl<PdaUserSkipV
         if(!save) {
             throw new ServiceException("PDA用户跳过版本升级记录单保存失败");
         }
-
-        // 操作日志
-        String msg = StrUtil.format("用户【{}】新增【{}】单据id为【{}】", commonService.getUserInfo().getUserName(), "PDA用户跳过版本升级记录单" , pdaUserSkipVersionEntity.getId());
-        // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
-        operateLogService.addModuleOperateLog(msg, null, pdaUserSkipVersionEntity.getId(), "新增操作");
-        // TODO 新增明细（如果有明细的话）
         return pdaUserSkipVersionEntity.getId();
     }
 
@@ -75,13 +67,6 @@ public class PdaUserSkipVersionServiceImpl extends SuperServiceImpl<PdaUserSkipV
         if(!save) {
             throw new ServiceException("PDA用户跳过版本升级记录单保存失败");
         }
-        // TODO 修改明细数据（包含增删改）（如果有明细的话）
-
-        // 记录主单操作日志
-            log.info("编辑 开始记录PDA用户跳过版本升级记录单日志数据，id：【{}】", pdaUserSkipVersionEntity.getId());
-            String msg = StrUtil.format("用户【{}】编辑id为【{}】的【{}】单据 ", commonService.getUserInfo().getUserName(), pdaUserSkipVersionEntity.getId(), "PDA用户跳过版本升级记录单");
-        // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
-        operateLogService.addModuleOperateLogByObj(old, pdaUserSkipVersionEntity, null, pdaUserSkipVersionEntity.getId(), msg);
         return Boolean.TRUE;
     }
 
