a8fdec72614f47131575e876a00cd420b185ca19
Will <wangwei@ulanzi.cn>
2023-09-13 11:34:12 +0800

    b2c销售订单调整

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoB2cDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoB2cDTO.java
@@ -696,7 +696,7 @@ public class SoB2cDTO implements Serializable {
         /**
          * 编辑分类类型 字典 categoryType类型
          */
-        @NotBlank(message = "类型不能为空")
+        @NotNull(message = "类型不能为空")
         private SoB2cCategoryTypeEnum typeEnum;
 
         /**
