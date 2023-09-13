f1a75408c548edc2c560997482bb6d0978a99844
luoweigang <821969511@qq.com>
2023-09-13 15:49:19 +0800

    PDA:退货入库单

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderServiceImpl.java
@@ -2165,6 +2165,8 @@ public class PurchaseOrderServiceImpl extends SuperServiceImpl<PurchaseOrderMapp
         if (CollectionUtils.isEmpty(list)) {
             return new ArrayList<>();
         }
+        list.sort(Comparator.comparing(PurchaseOrderDTO.PdaPurchaseOrder::getCode).reversed());
+        list.forEach(req -> req.setApproveStatusName(ApproveStatusEnum.getName(req.getApproveStatus())));
         return list;
     }
 }
