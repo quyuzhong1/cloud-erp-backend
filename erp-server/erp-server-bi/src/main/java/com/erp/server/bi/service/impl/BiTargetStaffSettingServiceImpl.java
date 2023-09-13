0ae1d79a8affd51ee9f82abe7cb10786b1b80cff
yl <ylstrive@163.com>
2023-09-13 11:54:27 +0800

    2023-09-13 Lambda bi 功能开发代码提交

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiTargetStaffSettingServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiTargetStaffSettingServiceImpl.java
@@ -5,16 +5,22 @@ import com.common.business.service.impl.SuperServiceImpl;
 import com.erp.model.bi.entity.BiTargetStaffSettingEntity;
 
 import com.common.core.exception.ServiceException;
+import com.erp.model.bi.entity.BiTargetYearEntity;
 import com.erp.server.bi.mapper.BiTargetStaffSettingMapper;
 import com.erp.server.bi.service.BiTargetStaffSettingService;
+import com.erp.server.bi.service.BiTargetYearService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import com.erp.model.bi.dto.BiTargetStaffSettingDTO;
+
 import java.util.*;
+
 import com.common.core.utils.*;
 import com.common.core.enums.ApiError;
+
 /**
  * <p>
  * 人员目标设置表 服务实现类
@@ -27,20 +33,23 @@ import com.common.core.enums.ApiError;
 @Service
 public class BiTargetStaffSettingServiceImpl extends SuperServiceImpl<BiTargetStaffSettingMapper, BiTargetStaffSettingEntity> implements BiTargetStaffSettingService {
 
+    @Autowired
+    private BiTargetYearService biTargetYearService;
 
     @GlobalTransactional(rollbackFor = Exception.class)
     @Transactional(rollbackFor = Exception.class)
     @Override
     public String add(BiTargetStaffSettingDTO.AddDTO addDTO) {
-        BiTargetStaffSettingEntity biTargetStaffSettingEntity = new BiTargetStaffSettingEntity();
-        BeanMapperUtils.copy(addDTO, biTargetStaffSettingEntity);
+        BiTargetYearEntity targetYear = new BiTargetYearEntity();
+        BeanMapperUtils.copy(addDTO, targetYear);
+        List<BiTargetStaffSettingDTO.CommonDTO> detailList = addDTO.getDetailList();
 
         // 数据处理
-        handleData(biTargetStaffSettingEntity);
+        handleData(targetYear.getYear(), detailList);
 
         log.info("开始新增人员目标设置单");
-        boolean save = super.save(biTargetStaffSettingEntity);
-        if(!save) {
+        boolean save = biTargetYearService.save(targetYear);
+        if (!save) {
             throw new ServiceException("人员目标设置单保存失败");
         }
 
@@ -48,20 +57,20 @@ public class BiTargetStaffSettingServiceImpl extends SuperServiceImpl<BiTargetSt
     }
 
     /**
-    * 修改
-    */
+     * 修改
+     */
     @Transactional(rollbackFor = Exception.class)
     @Override
     public Boolean update(BiTargetStaffSettingDTO.UpdateDTO updateDTO) {
         BiTargetStaffSettingEntity old = super.getById(updateDTO.getId());
-        Optional.ofNullable(old).orElseThrow(()->new ServiceException(ApiError.NOT_EXIST_BILL, "人员目标设置单"));
-        BiTargetStaffSettingEntity biTargetStaffSettingEntity =  BeanMapperUtils.map(BiTargetStaffSettingEntity.class, updateDTO);
+        Optional.ofNullable(old).orElseThrow(() -> new ServiceException(ApiError.NOT_EXIST_BILL, "人员目标设置单"));
+        BiTargetStaffSettingEntity biTargetStaffSettingEntity = BeanMapperUtils.map(BiTargetStaffSettingEntity.class, updateDTO);
 
         // 数据处理
         handleData(biTargetStaffSettingEntity);
         log.info("编辑 开始修改人员目标设置单数据，id：【{}】", old.getId());
         boolean save = super.updateById(biTargetStaffSettingEntity);
-        if(!save) {
+        if (!save) {
             throw new ServiceException("人员目标设置单保存失败");
         }
         return Boolean.TRUE;
@@ -69,9 +78,15 @@ public class BiTargetStaffSettingServiceImpl extends SuperServiceImpl<BiTargetSt
 
 
     /**
-    * 新增修改处理数据
-    */
-    private void handleData(BiTargetStaffSettingEntity biTargetStaffSettingEntity) {
-    // TODO 验证数据 & 数据赋值
+     * 新增修改处理数据
+     */
+    private void handleData(String year, List<BiTargetStaffSettingDTO.CommonDTO> detailList) {
+        List<BiTargetStaffSettingDTO.ListDetailDTO> existList = baseMapper.listByYear(year);
+        for(BiTargetStaffSettingDTO.CommonDTO item : detailList){
+            //existList.stream().filter(e->)
+        }
+
+        // TODO 验证数据 & 数据赋值
+
     }
 }
