8ee6c827063936051cd1d782883c64053e010b59
yl <ylstrive@163.com>
2023-09-15 08:56:42 +0800

    2023-09-15 Lambda bi 功能开发代码提交

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiTargetCategorySettingServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiTargetCategorySettingServiceImpl.java
@@ -2,17 +2,33 @@ package com.erp.server.bi.service.impl;
 
 
 import com.common.business.service.impl.SuperServiceImpl;
+import com.erp.model.bi.dto.BiTargetSkuSettingDTO;
+import com.erp.model.bi.entity.BiProductDetailEntity;
 import com.erp.model.bi.entity.BiTargetCategorySettingEntity;
 
 import com.common.core.exception.ServiceException;
+import com.erp.model.bi.entity.BiTargetSkuSettingEntity;
+import com.erp.model.bi.entity.BiTargetYearEntity;
+import com.erp.model.bi.enums.MetricsEnum;
+import com.erp.model.bi.enums.MonthEnum;
+import com.erp.model.sys.dto.CurrencyDTO;
+import com.erp.model.sys.dto.SysDepartmentDTO;
+import com.erp.rpc.sys.feign.SysUserFeign;
 import com.erp.server.bi.mapper.BiTargetCategorySettingMapper;
 import com.erp.server.bi.service.BiTargetCategorySettingService;
+import com.erp.server.bi.service.BiTargetYearService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import com.erp.model.bi.dto.BiTargetCategorySettingDTO;
+
+import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
+
 import com.common.core.utils.*;
 import com.common.core.enums.ApiError;
 /**
@@ -26,26 +42,145 @@ import com.common.core.enums.ApiError;
 @Slf4j
 @Service
 public class BiTargetCategorySettingServiceImpl extends SuperServiceImpl<BiTargetCategorySettingMapper, BiTargetCategorySettingEntity> implements BiTargetCategorySettingService {
+    @Autowired
+    private BiTargetYearService biTargetYearService;
 
+    @Autowired
+    private SysUserFeign sysUserFeign;
 
     @GlobalTransactional(rollbackFor = Exception.class)
     @Transactional(rollbackFor = Exception.class)
     @Override
     public String add(BiTargetCategorySettingDTO.AddDTO addDTO) {
-        BiTargetCategorySettingEntity biTargetCategorySettingEntity = new BiTargetCategorySettingEntity();
-        BeanMapperUtils.copy(addDTO, biTargetCategorySettingEntity);
+        BiTargetYearEntity targetYear = new BiTargetYearEntity();
+        BeanMapperUtils.copy(addDTO, targetYear);
+        List<String> metricsList = addDTO.getMetricsList();
+        targetYear.setMetrics(metricsList.stream().collect(Collectors.joining(",")));
+        List<BiTargetCategorySettingDTO.CommonDTO> detailList = addDTO.getDetailList();
 
         // 数据处理
-        handleData(biTargetCategorySettingEntity);
+        handleData(targetYear, detailList);
 
         log.info("开始新增分类 目标设置单");
-        boolean save = super.save(biTargetCategorySettingEntity);
+        boolean save = biTargetYearService.save(targetYear);
         if(!save) {
             throw new ServiceException("分类 目标设置单保存失败");
         }
+        //添加明细
+        this.batchAdd(targetYear.getId(), detailList);
+        return targetYear.getId();
+    }
 
 
-        return biTargetCategorySettingEntity.getId();
+    /**
+     * 添加明细的
+     *
+     * @param mainId
+     * @param detailList
+     */
+    public void batchAdd(String mainId, List<BiTargetCategorySettingDTO.CommonDTO> detailList) {
+        if (CollectionUtils.isEmpty(detailList)) {
+            return;
+        }
+        List<BiTargetCategorySettingEntity> addList = new ArrayList<>(20);
+        for (BiTargetCategorySettingDTO.CommonDTO item : detailList) {
+            List<BiTargetCategorySettingEntity> list = listAdd(item);
+            addList.addAll(list);
+        }
+        if (CollectionUtils.isNotEmpty(addList)) {
+
+        }
+    }
+
+    /**
+     * 获取到添加的数据
+     *
+     * @param item
+     * @return
+     */
+    private List<BiTargetCategorySettingEntity> listAdd(BiTargetCategorySettingDTO.CommonDTO item) {
+        List<BiTargetCategorySettingEntity> addList = new ArrayList<>(12);
+        String categoryId = item.getCategoryId();
+        //指标
+        MetricsEnum metrics = item.getMetrics();
+
+        //一月值
+        if (Objects.nonNull(item.getJanuary())) {
+            addList.add(putEntity(categoryId, item.getJanuary(), metrics, MonthEnum.JANUARY.getValue()));
+        }
+        //二月值
+        BigDecimal february = item.getFebruary();
+        if (Objects.nonNull(february)) {
+            addList.add(putEntity(categoryId, february, metrics, MonthEnum.FEBRUARY.getValue()));
+        }
+        //三月值
+        BigDecimal march = item.getMarch();
+        if (Objects.nonNull(march)) {
+            addList.add(putEntity(categoryId, march, metrics, MonthEnum.MARCH.getValue()));
+        }
+        //四月值
+        BigDecimal april = item.getApril();
+        if (Objects.nonNull(april)) {
+            addList.add(putEntity(categoryId, april, metrics, MonthEnum.APRIL.getValue()));
+        }
+        //五月值
+        BigDecimal may = item.getMay();
+        if (Objects.nonNull(may)) {
+            addList.add(putEntity(categoryId, may, metrics, MonthEnum.MAY.getValue()));
+        }
+        //六月值
+        BigDecimal june = item.getJune();
+        if (Objects.nonNull(june)) {
+            addList.add(putEntity(categoryId, june, metrics, MonthEnum.JUNE.getValue()));
+        }
+        //七月值
+        BigDecimal july = item.getJuly();
+        if (Objects.nonNull(july)) {
+            addList.add(putEntity(categoryId, july, metrics, MonthEnum.JULY.getValue()));
+        }
+        //八月值
+        BigDecimal august = item.getAugust();
+        if (Objects.nonNull(august)) {
+            addList.add(putEntity(categoryId, august, metrics, MonthEnum.AUGUST.getValue()));
+        }
+        //九月值
+        BigDecimal september = item.getSeptember();
+        if (Objects.nonNull(september)) {
+            addList.add(putEntity(categoryId, september, metrics, MonthEnum.SEPTEMBER.getValue()));
+        }
+        //十月值
+        BigDecimal october = item.getOctober();
+        if (Objects.nonNull(october)) {
+            addList.add(putEntity(categoryId, october, metrics, MonthEnum.OCTOBER.getValue()));
+        }
+        //十一月值
+        BigDecimal november = item.getNovember();
+        if (Objects.nonNull(november)) {
+            addList.add(putEntity(categoryId, november, metrics, MonthEnum.NOVEMBER.getValue()));
+        }
+        //十二月值
+        BigDecimal december = item.getDecember();
+        if (Objects.nonNull(december)) {
+            addList.add(putEntity(categoryId, december, metrics, MonthEnum.DECEMBER.getValue()));
+        }
+        return addList;
+
+    }
+
+    /**
+     * 填充保存数据
+     *
+     * @param
+     * @param month 月份
+     * @return
+     */
+    private BiTargetCategorySettingEntity putEntity(String categorgId, BigDecimal value, MetricsEnum metrics, Integer month) {
+        BiTargetCategorySettingEntity entity = new BiTargetCategorySettingEntity();
+        entity.setMonth(month);
+        entity.setCategoryId(categorgId);
+        entity.setValue(value);
+        entity.setMetrics(metrics);
+        return entity;
     }
 
     /**
@@ -59,7 +194,7 @@ public class BiTargetCategorySettingServiceImpl extends SuperServiceImpl<BiTarge
         BiTargetCategorySettingEntity biTargetCategorySettingEntity =  BeanMapperUtils.map(BiTargetCategorySettingEntity.class, updateDTO);
 
         // 数据处理
-        handleData(biTargetCategorySettingEntity);
+       // handleData(biTargetCategorySettingEntity);
         log.info("编辑 开始修改分类 目标设置单数据，id：【{}】", old.getId());
         boolean save = super.updateById(biTargetCategorySettingEntity);
         if(!save) {
@@ -74,7 +209,18 @@ public class BiTargetCategorySettingServiceImpl extends SuperServiceImpl<BiTarge
     /**
     * 新增修改处理数据
     */
-    private void handleData(BiTargetCategorySettingEntity biTargetCategorySettingEntity) {
-    // TODO 验证数据 & 数据赋值
+    private void handleData(BiTargetYearEntity targetYear,List<BiTargetCategorySettingDTO.CommonDTO> detailList) {
+        //部门id
+        String deptId = targetYear.getDeptId();
+        //币种符号
+        String currency = targetYear.getCurrency();
+        SysDepartmentDTO department = sysUserFeign.getUserDeptById(deptId);
+        if (Objects.nonNull(department)) {
+            targetYear.setDeptName(department.getName());
+        }
+        List<CurrencyDTO.ViewDTO> currencyList = sysUserFeign.listByCurrency(Arrays.asList(currency));
+        if (CollectionUtils.isNotEmpty(currencyList)) {
+            targetYear.setCurrencySymbol(currencyList.get(0).getSymbol());
+        }
     }
 }
