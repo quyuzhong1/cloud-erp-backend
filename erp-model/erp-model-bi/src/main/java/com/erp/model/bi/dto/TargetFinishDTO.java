ac740a130dec5600f2c8c45b58843c09cf2b609e
Will <wangwei@ulanzi.cn>
2023-09-18 16:47:30 +0800

    业绩目标完成

--- erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/TargetFinishDTO.java
+++ erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/TargetFinishDTO.java
@@ -25,20 +25,21 @@ public class TargetFinishDTO implements Serializable {
     public static class ParamDTO extends BiFilterDTO{
 
         /**
-         * http://172.16.100.11:3002/project/74/interface/api/5167 type = metrics
+         * http://172.16.100.11:3002/project/74/interface/api/23416 type = metrics
          * 考核维度（财务销售额、销售额、净销售额、销量、毛利润、毛利率）
          */
         @NotBlank(message = "考核维度不能为空")
         private String metrics;
         /**
-         * http://172.16.100.11:3002/project/74/interface/api/5167 type = targetViewType
-         * 查看类型（完成率、占比）
+         * http://172.16.100.11:3002/project/74/interface/api/23416 type = targetViewType
+         *
+         * 查看类型（完成率 finishRate、占比 ratio）
          */
         @NotBlank(message = "查看类型不能为空")
         private String viewType;
         /**
-         * http://172.16.100.11:3002/project/74/interface/api/5167 type = targetSearchType
-         * 搜索类型（二级部门、人员、店铺、品类、SKU）
+         * http://172.16.100.11:3002/project/74/interface/api/23416 type = targetSearchType
+         * 搜索类型（二级部门 dept、人员 user、店铺 shop,、品类 category、SKU sku）
          */
         @NotBlank(message = "搜索类型不能为空")
         private String searchType;
--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiDictServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/BiDictServiceImpl.java
@@ -146,6 +146,14 @@ public class BiDictServiceImpl extends ServiceImpl<BiDictMapper, BiDictEntity> i
     }
 
     @Override
+    public List<Map<String, Object>> listValueByType(String type) {
+        LambdaQueryWrapper<BiDictEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.select(BiDictEntity::getValue, BiDictEntity::getName);
+        queryWrapper.eq(BiDictEntity::getType, type);
+        return this.listMaps(queryWrapper);
+    }
+
+    @Override
     public Map<String, BiDictEntity> listByValues(List<String> dictValues) {
         if (CollectionUtils.isEmpty(dictValues)) {
             return new HashMap<>(0);
