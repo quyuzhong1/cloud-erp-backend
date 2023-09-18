8d73bf07bb85943809af55b2ef823be4799b9509
luoweigang <821969511@qq.com>
2023-09-18 17:16:50 +0800

    修改新品销售额

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/SalesOrderServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/SalesOrderServiceImpl.java
@@ -2343,9 +2343,13 @@ public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderServiceMapper, 
                 vo.setNewSalesAmountTarget(targetNewProductSalesAmount.getValue());
                 vo.setNewSalesQuantityTarget(targetNewProductSalesQty.getValue());
             }
+            if (newProductSales.compareTo(BigDecimal.ZERO) > 0) {
+                vo.setNewProductSalesRatio(newProductSales.add(oldProductSales).divide(newProductSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
+            }
 
-            vo.setNewProductSalesRatio(newProductSales.add(oldProductSales).divide(newProductSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
-            vo.setOldProductSalesRatio(newProductSales.add(oldProductSales).divide(oldProductSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
+            if (oldProductSales.compareTo(BigDecimal.ZERO) > 0) {
+                vo.setOldProductSalesRatio(newProductSales.add(oldProductSales).divide(oldProductSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
+            }
             resultList.add(vo);
         }
         return resultList;
@@ -2417,7 +2421,13 @@ public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderServiceMapper, 
                 vo.setNewSalesAmountTarget(targetNewProductSalesAmount.getValue());
                 vo.setNewSalesQuantityTarget(targetNewProductSalesQty.getValue());
             }
+            if (newItemSales.compareTo(BigDecimal.ZERO) > 0) {
+                vo.setNewProductSalesRatio(newItemSales.add(oldItemSales).divide(newItemSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
+            }
 
+            if (oldItemSales.compareTo(BigDecimal.ZERO) > 0) {
+                vo.setOldProductSalesRatio(newItemSales.add(oldItemSales).divide(oldItemSales, 4, BigDecimal.ROUND_HALF_UP).multiply(MathUtil.BigDecimal_100) + "%");
+            }
             resultList.add(vo);
         }
         return resultList;
