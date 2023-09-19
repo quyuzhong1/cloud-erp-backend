adba232b001e246212c65d97aa9b976ad0a1e95d
luoweigang <821969511@qq.com>
2023-09-19 10:38:58 +0800

    PDA:BUG修复

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
@@ -157,6 +157,8 @@ public class PoInstockServiceImpl extends SuperServiceImpl<PoInstockMapper, PoIn
 
         //清空明细数据
         records.forEach(obj -> {
+            WarehouseLocationEntity warehouseLocationEntity = warehouseLocationEntities.stream().filter(req -> req.getWarehouseId().equals(obj.getDeliveryWarehouseId()) && req.getCode().equals(obj.getWarehouseLocation())).findFirst().orElse(new WarehouseLocationEntity());
+            obj.setWarehouseLocationName(warehouseLocationEntity.getName());
             boolean contains = list.contains(obj.getId());
             if (contains) {
                 obj.setCode(null);
@@ -169,8 +171,6 @@ public class PoInstockServiceImpl extends SuperServiceImpl<PoInstockMapper, PoIn
                 obj.setCreateUserName(null);
                 return;
             }
-            WarehouseLocationEntity warehouseLocationEntity = warehouseLocationEntities.stream().filter(req -> req.getWarehouseId().equals(obj.getDeliveryWarehouseId()) && req.getCode().equals(obj.getWarehouseLocation())).findFirst().orElse(new WarehouseLocationEntity());
-            obj.setWarehouseLocationName(warehouseLocationEntity.getName());
             list.add(obj.getId());
         });
         return new PagingVO(pageData);
