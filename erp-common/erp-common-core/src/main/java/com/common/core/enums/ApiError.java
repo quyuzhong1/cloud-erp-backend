8cf849fd2cd8aabd03f6ca7d6cd2c9a9c2e02b95
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-09-20 09:37:54 +0800

    ApiError标签错误类型位置调整

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -450,14 +450,17 @@ public enum ApiError implements Serializable {
     ERROR_BOM_COMBINATION_SKU(95246,"组合产品SKU【{}】已存在"),
     ERROR_BOM_SKU_REPEAT(95244,"父级sku【{}】和子级sku不能重复"),
     ERROR_BOM_PARENT_SKU_REPEAT(95245,"父级sku【{}】已生成BOM"),
-    ERROR_BOM_COMBINATION_STATE(95246,"组合品状态有误"),
+    ERROR_BOM_COMBINATION_STATE(95246,"组合产品状态有误"),
     ERROR_BOM_COMBINATION_PARENT_SKU_NAME_DIFFERENT(95247,"组合产品SKU【{}】对应名称不一致"),
     ERROR_BOM_COMBINATION_EXPORT(95247,"组合产品【{}】已存在不支持导入"),
     ERROR_BOM_COMBINATION_NOT_EXIST(95248,"组合产品不存在"),
     ERROR_BOM_COMBINATION_SKU_NOT_CHINESE(95249,"组合产品【{}】不能输入中文"),
     ERROR_BOM_COMBINATION_SKU_APPROVE_PASS(95250,"组合产品SKU【{}】未审核成功"),
     ERROR_BOM_COMBINATION_CHILD_SKU_REPEAT(95251,"组合产品子SKU【{}】不能重复"),
-
+    NOT_EXIST(95252, "{}不存在"),
+    ERROR_EXIST_BASIC_LABEL(95253, "基础标签单{}已存在"),
+    NOT_EXIST_BASIC_LABEL_LEVEL(95254, "基础标签单级别{}不存在"),
+    ERROR_EXIST_BASIC_LABEL_NAME(95255, "基础标签单标签名称{}数据重复"),
 
 
     /**
@@ -499,12 +502,6 @@ public enum ApiError implements Serializable {
     MABANG_BOM_EXIST(97032, "sku{【%s】}在马帮的BOM中不存在！"),
     YEAR_METRICS_EXIST(97030, "{}已设置,不可重复设置"),
     SEARCH_TYPE_EXIST(97031, "搜索类型不存在"),
-
-
-    NOT_EXIST(97033, "{}不存在"),
-    ERROR_EXIST_BASIC_LABEL(97034, "基础标签单{}已存在"),
-    NOT_EXIST_BASIC_LABEL_LEVEL(97035, "基础标签单级别{}不存在"),
-    ERROR_EXIST_BASIC_LABEL_NAME(97036, "基础标签单标签名称{}数据重复"),
 
 
     /**
