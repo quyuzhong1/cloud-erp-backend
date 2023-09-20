48a631ce72227ccdde54f330f91a811f877c01cf
luoweigang <821969511@qq.com>
2023-09-20 17:33:21 +0800

    修改销售趋势

--- erp-common/erp-common-business/src/main/java/com/common/business/vo/SeriesVO.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/vo/SeriesVO.java
@@ -14,9 +14,12 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class SeriesVO<T> {
-
-    private String name;    //当前图名
-    private List<T> data;   //数据
+    //当前图名
+    private String name;
+    //当前图类型 line为折线， bar为柱状
+    private String type;
+    //数据
+    private List<T> data;
 
 
 }
