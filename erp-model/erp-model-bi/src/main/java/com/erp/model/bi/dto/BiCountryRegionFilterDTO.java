c3692cb2dc075b17a0c57cf6dc3cee13ee410178
Jim <zhoujingbo@ulanzi.cn>
2023-09-21 12:01:57 +0800

    Bi子区域过滤fix

--- erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/BiCountryRegionFilterDTO.java
+++ erp-model/erp-model-bi/src/main/java/com/erp/model/bi/dto/BiCountryRegionFilterDTO.java
@@ -3,6 +3,7 @@ package com.erp.model.bi.dto;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang.StringUtils;
 
 /**
  * BI国家区域 筛选条件
@@ -23,4 +24,20 @@ public class BiCountryRegionFilterDTO extends BiFilterDTO {
      * 子区域代号
      */
     public String subregionCode;
+
+    /**
+     * 过滤区域
+     */
+    public boolean filterRegion(BiCountryAnalyzeDTO e) {
+        // 区域和子区域都不过滤
+        if (StringUtils.isBlank(this.getSubregionCode()) && StringUtils.isBlank(this.getSubregionCode())){
+            return true;
+        }
+        // 子区域过滤
+        if (StringUtils.isNotBlank(this.getSubregionCode())){
+            return e.getSubregionCode().equalsIgnoreCase(this.getSubregionCode());
+        }
+        // 区域过滤
+        return e.getRegionCode().equalsIgnoreCase(this.getRegionCode());
+    }
 }
