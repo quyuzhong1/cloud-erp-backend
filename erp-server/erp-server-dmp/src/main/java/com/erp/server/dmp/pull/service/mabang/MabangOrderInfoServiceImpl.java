2807df00da4f251184ef5fb18fe73c9fc6c2527e
Cloud <cloud@ulanzi.cn>
2023-09-22 22:09:36 +0800

    udpate date
    (cherry picked from commit f61f6474fb3d41ba945317ca7c9ffff9e09fcb80)

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/mabang/MabangOrderInfoServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/service/mabang/MabangOrderInfoServiceImpl.java
@@ -159,7 +159,7 @@ public class MabangOrderInfoServiceImpl implements IReportSaveService<OrderEntit
         // 查询mongo待推送数据
         String value = cfgSettingService.getValue(SettingEnum.CLEAN_JOB_DELAY_MINUTE);
         Integer delayMinute = null != value ? NumberUtil.parseInt(value) : 0;
-        OrderMongoDTO orderMongoDTO = OrderMongoDTO.getByIsClean(CleanStatusEnum.UNCLEAN.getCode(), delayMinute);
+        OrderMongoDTO orderMongoDTO = OrderMongoDTO.getByIsCleanDateStr(CleanStatusEnum.UNCLEAN.getCode(), delayMinute);
         List<OrderEntity> mongoData = mongoService.findMongoData(orderMongoDTO, 1, size, MongoTableNameContant.ORIGINAL_MABANG_ORDER, OrderEntity.class);
         if (CollectionUtil.isEmpty(mongoData)) {
             return;
