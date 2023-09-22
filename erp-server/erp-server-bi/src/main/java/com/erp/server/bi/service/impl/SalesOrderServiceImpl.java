34bbf8164b3338c3d0b26aae5de5a7580879558e
luoweigang <821969511@qq.com>
2023-09-22 09:16:01 +0800

    BI：修改销售趋势

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/SalesOrderServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/SalesOrderServiceImpl.java
@@ -2336,6 +2336,7 @@ public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderServiceMapper, 
      */
     private StatisticalDataVO byDateFinanceSales(DateSalesTrendDTO.SearchDTO dto) {
 
+
         StatisticalDataVO statistical = new StatisticalDataVO();
         statistical.setName("销售趋势");
         statistical.setChartType(ChartType.BAR);
@@ -2535,10 +2536,12 @@ public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderServiceMapper, 
      */
     @Override
     public StatisticalDataVO byDate(DateSalesTrendDTO.SearchDTO dto) {
-        //如果查询客单价
+        //如果查询财务销售额
         if (DateSalesTrendSearchTypeEnum.FINANCE_SALES_QUANTITY.getCode().equals(dto.getSearchType())) {
             return this.byDateFinanceSales(dto);
         }
+
+
 
         StatisticalDataVO statistical = new StatisticalDataVO();
         statistical.setName("销售趋势");
@@ -2551,9 +2554,11 @@ public class SalesOrderServiceImpl extends ServiceImpl<SalesOrderServiceMapper, 
         if (dto.getTimeType() != null && BiConstant.OLD.equals(dto.getTimeType())) {
             timeFlag = "platform_create_time";
         }
+        //类别查询
         if (CollectionUtils.isNotEmpty(dto.getCategory())) {
             return this.byDateStackedColumnChart(dto, timeFlag, settleRate);
         }
+
         List<SalesFlagVO> salesList = new ArrayList();
         List<SalesFlagVO> lastYearSalesList = new ArrayList();
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
