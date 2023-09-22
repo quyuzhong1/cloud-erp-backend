f16959e5bb75767bff73f8a3b1eee7757c0fa65b
Will <wangwei@ulanzi.cn>
2023-09-22 15:24:17 +0800

    库存校验设置

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PurchaseReturnOrderServiceImpl.java
@@ -225,27 +225,6 @@ public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PurchaseRet
             ignoreInventorySkuIds = ignoreInventorySkuList.stream().map(SkuVO::getSkuId).distinct().collect(Collectors.toList());
         }
 
-        // 增加库存数量验证（库存退货）
-        String sourceType = dto.getSourceType();
-        if(!Objects.equals(sourceType, SourceTypeEnum.QC_INFO.getCode())) {
-            String returnMode = dto.getReturnMode();
-            List<PurchaseReturnOrderDetailDTO.AddDTO> purchasePriceDetailList = dto.getPurchasePriceDetailList();
-            for(PurchaseReturnOrderDetailDTO.AddDTO detail : purchasePriceDetailList) {
-                if(ignoreInventorySkuIds.contains(detail.getSkuId())) {
-                    log.warn("sku id: {}，sku编号：{}产品属性是费用或服务，不参与库存出入库，不做库存验证", detail.getSkuId(), detail.getSkuNo());
-                    continue;
-                }
-                Integer usableQty = inventoryService.getInventoryTotal(warehouseEntity.getOrgId(), warehouseEntity.getId(), detail.getSkuId(), detail.getWarehouseLocation(), InventoryStatusEnum.USABLE.getCode());
-                if(Objects.equals(returnMode, ReturnModeEnum.REPLENISHMENT.getCode())
-                        && usableQty < detail.getReplenishQty()) {
-                    throw new ServiceException(ApiError.ERROR_99070);
-                } else if (Objects.equals(returnMode, ReturnModeEnum.DEDUCTION.getCode())
-                        && usableQty < detail.getDeductAmountQty()) {
-                    throw new ServiceException(ApiError.ERROR_99070);
-                }
-            }
-        }
-
         //生成单号
         String code = sysUserFeign.getBusinessNo(new SysCodeDTO(BusinessNoConstant.CGTH, BusinessNoTypeEnum.CODE_CGTH.getCode()));
         //设置收货单主表
@@ -327,27 +306,6 @@ public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PurchaseRet
         List<String> ignoreInventorySkuIds = Lists.newArrayList();
         if(CollUtil.isNotEmpty(ignoreInventorySkuList)) {
             ignoreInventorySkuIds = ignoreInventorySkuList.stream().map(SkuVO::getSkuId).distinct().collect(Collectors.toList());
-        }
-
-        // 增加库存数量验证（库存退货）
-        String sourceType = dto.getSourceType();
-        if(!Objects.equals(sourceType, ReturnOrderSourceEnum.QC.getCode())) {
-            String returnMode = dto.getReturnMode();
-            List<PurchaseReturnOrderDetailDTO.UpdateDTO> purchasePriceDetailList = dto.getPurchasePriceDetailList();
-            for (PurchaseReturnOrderDetailDTO.UpdateDTO detail : purchasePriceDetailList) {
-                if(ignoreInventorySkuIds.contains(detail.getSkuId())) {
-                    log.warn("sku id: {}，sku编号：{}产品属性是费用或服务，不参与库存出入库，不做库存验证", detail.getSkuId(), detail.getSkuNo());
-                    continue;
-                }
-                Integer usableQty = inventoryService.getInventoryTotal(warehouseEntity.getOrgId(), warehouseEntity.getId(), detail.getSkuId(), detail.getWarehouseLocation(), InventoryStatusEnum.USABLE.getCode());
-                if(Objects.equals(returnMode, ReturnModeEnum.REPLENISHMENT.getCode())
-                        && usableQty < detail.getReplenishQty()) {
-                    throw new ServiceException(ApiError.ERROR_99070);
-                } else if (Objects.equals(returnMode, ReturnModeEnum.DEDUCTION.getCode())
-                        && usableQty < detail.getDeductAmountQty()) {
-                    throw new ServiceException(ApiError.ERROR_99070);
-                }
-            }
         }
 
         //设置收货单主表
