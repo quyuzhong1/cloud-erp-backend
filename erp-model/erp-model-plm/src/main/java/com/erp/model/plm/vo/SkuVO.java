beb6eb17c68cee6d2eab5dcc8fb307d7a4cdadc1
luoweigang <821969511@qq.com>
2023-09-25 18:09:09 +0800

    PDA:修改产品查询新增一级/二级供应商

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/vo/SkuVO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/vo/SkuVO.java
@@ -153,4 +153,24 @@ public class SkuVO implements Serializable {
      * 标准零售价
      */
     private BigDecimal retailPrice;
+
+    /**
+     * 一级供应商
+     */
+    private String mainSupplier;
+
+    /**
+     * 一级供应商名称
+     */
+    private String mainSupplierName;
+
+    /**
+     * 二级供应商
+     */
+    private String secondSupplier;
+
+    /**
+     * 二级供应商名称
+     */
+    private String secondSupplierName;
 }
--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -3778,6 +3778,22 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
         Integer state = ProductDetailStatusEnum.APPROVAL_PASS.getCode();
         dto.setStatus(state);
         List<SkuVO> skuVOS = baseMapper.pdaSearchSku(dto);
+        List<String> mainSupplierIds = skuVOS.stream().map(SkuVO::getMainSupplier).distinct().collect(Collectors.toList());
+        List<String> secondSupplierIds = skuVOS.stream().map(SkuVO::getSecondSupplier).distinct().collect(Collectors.toList());
+        mainSupplierIds.addAll(secondSupplierIds);
+        List<String> supplierIds = mainSupplierIds.stream().distinct().collect(Collectors.toList());
+        Map<String, SupplierDTO.SupplierSimpleDTO> supplierMap = supplierFeign.getSupplierSimpleInfo(supplierIds);
+        skuVOS.forEach(req -> {
+            // 一级供应商名称
+            if (StrUtils.isNotEmpty(req.getMainSupplier()) && supplierMap.containsKey(req.getMainSupplier())) {
+                req.setMainSupplierName(supplierMap.get(req.getMainSupplier()).getName());
+            }
+
+            // 二级供应商名称
+            if (StrUtils.isNotEmpty(req.getSecondSupplier()) && supplierMap.containsKey(req.getSecondSupplier())) {
+                req.setSecondSupplierName(supplierMap.get(req.getSecondSupplier()).getName());
+            }
+        });
         if (CollectionUtils.isEmpty(skuVOS)) {
             throw new ServiceException(ApiError.ERROR_95107);
         }
