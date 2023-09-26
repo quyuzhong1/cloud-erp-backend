b1f8537d91ad14225c8ce7606c4adba004713d17
Jim <zhoujingbo@ulanzi.cn>
2023-09-26 21:03:07 +0800

    Bi商品分析趋势fix

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/controller/api/BiComprehensiveAnalyseController.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/controller/api/BiComprehensiveAnalyseController.java
@@ -153,6 +153,8 @@ public class BiComprehensiveAnalyseController extends BaseController {
     //@DataPermission(operationType = DataAttributeEnum.LIST, tableField = "charge_id", menuCode = "bi:module:content", tableAlias = "doi")
     public ApiResult<List<SaleDetailVO>> saleDetailDate(@RequestBody @Validated SkuDateFilterDTO biFilterDTO) {
         biFilterDTO.setEndTime(biFilterDTO.getEndTime(), 1);
+        biFilterDTO.setTimeType(null == biFilterDTO.getTimeType() ? 0 : biFilterDTO.getTimeType());
+        biFilterDTO.setSettleMethod(null == biFilterDTO.getSettleMethod() ? 0 : biFilterDTO.getSettleMethod());
         List<SaleDetailVO> saleDetailVOList = biComprehensiveAnalyseService.saleDetailDate(biFilterDTO);
         return success(saleDetailVOList);
     }
@@ -168,6 +170,8 @@ public class BiComprehensiveAnalyseController extends BaseController {
     //@DataPermission(operationType = DataAttributeEnum.LIST, tableField = "charge_id", menuCode = "bi:module:content", tableAlias = "doi")
     public ApiResult<List<SkuDateSaleTrendVO>> skuDateSaleTrend(@RequestBody @Validated SkuDateFilterDTO biFilterDTO) {
         biFilterDTO.setEndTime(biFilterDTO.getEndTime(), 1);
+        biFilterDTO.setTimeType(null == biFilterDTO.getTimeType() ? 0 : biFilterDTO.getTimeType());
+        biFilterDTO.setSettleMethod(null == biFilterDTO.getSettleMethod() ? 0 : biFilterDTO.getSettleMethod());
         List<SkuDateSaleTrendVO> skuDateSaleTrendVOS = biComprehensiveAnalyseService.skuDateSaleTrend(biFilterDTO);
         return success(skuDateSaleTrendVOS);
     }
