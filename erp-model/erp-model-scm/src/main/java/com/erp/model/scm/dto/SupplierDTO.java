6f16e7203419e2b7fcfa0b62b3ffb6de1dbc157b
luoweigang <821969511@qq.com>
2023-10-10 10:36:18 +0800

    迭代：修改退货单无币别问题

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/SupplierDTO.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/SupplierDTO.java
@@ -742,6 +742,11 @@ public class SupplierDTO implements Serializable {
         private String payCurrency;
 
         /**
+         * 币种符号
+         */
+        private String currencySymbol;
+
+        /**
          * 付款条件
          */
         private String paymentCondition;
