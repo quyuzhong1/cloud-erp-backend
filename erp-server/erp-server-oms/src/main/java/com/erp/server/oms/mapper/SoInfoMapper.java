db16eb3a7e8109df8340102527280a8d7ba687ab
luoweigang <821969511@qq.com>
2023-10-13 14:04:08 +0800

    迭代：自动填入上市时间（首单销售日期）

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/mapper/SoInfoMapper.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/mapper/SoInfoMapper.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.common.business.dto.base.PushSyncStatusDTO;
 import com.erp.model.oms.dto.ListingTimeDTO;
+import com.erp.model.oms.dto.ListingTimeDTO;
 import com.erp.model.oms.dto.SoInfoDTO;
 import com.erp.model.oms.entity.SoInfoEntity;
 import org.apache.ibatis.annotations.Mapper;
