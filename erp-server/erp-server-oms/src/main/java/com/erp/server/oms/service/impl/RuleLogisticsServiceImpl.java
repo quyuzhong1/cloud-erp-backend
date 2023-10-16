8360cc3f1ccebf387e2fabd81e65ce399776dd32
yl <ylstrive@163.com>
2023-10-16 18:18:32 +0800

    Lambda OMS 2023-10-16 订单规则联调问题修复

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/RuleLogisticsServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/RuleLogisticsServiceImpl.java
@@ -217,7 +217,7 @@ public class RuleLogisticsServiceImpl extends SuperServiceImpl<RuleLogisticsMapp
             for (JSONObject jsonObject : jsonObjectList) {
                 Boolean matchResult = spElServer.matchExpressionByConditionList(conditionElementList, jsonObject);
                 if (matchResult) {
-                    ruleMatchResult.setLogisticsSupplier(item.getLogisticsSupplier());
+                    ruleMatchResult.setLogisticsSupplier(item.getSupplier());
                     ruleMatchResult.setAutoGetTrackNo(item.getAutoGetTrackNo());
                     return ruleMatchResult;
                 }
