c3d15406d8f4c06a5f2a2c16ab4b551bf6f1bd8b
luoweigang <821969511@qq.com>
2023-10-17 12:20:10 +0800

    沃尔玛

--- erp-sdk/erp-sdk-oms-walmart/src/main/java/com/sdk/oms/walmart/service/WalmartSdkClientService.java
+++ erp-sdk/erp-sdk-oms-walmart/src/main/java/com/sdk/oms/walmart/service/WalmartSdkClientService.java
@@ -3,6 +3,7 @@ package com.sdk.oms.walmart.service;
 
 import com.common.core.utils.OkHttpUtils;
 import com.common.core.utils.UUID;
+import com.sdk.oms.walmart.api.WalmartSign;
 import com.sdk.oms.walmart.api.WalmartStaticKey;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
@@ -26,7 +27,7 @@ public class WalmartSdkClientService {
         String clientId = "2434a35c-7c42-4420-9618-0c179b68a8c2";
         String clientSecret = "AMW5lbVFqG2DMP4DuLezhSkbk4u0JLGUjdFlsrl_p0sagsBkYPPiQhRbEvkE4a6k6KXNKhB--RGlqPKIfhUoV28";
         WalmartSdkClientService walmartSdkClientService = new WalmartSdkClientService();
-        String s = walmartSdkClientService.sendWalmartPostRefreshToeken(baseUrl, consumerId, clientId, clientSecret);
+        String s = walmartSdkClientService.sendWalmartPostToeken(baseUrl, consumerId, clientId, clientSecret);
         System.out.println(s);
     }
 
@@ -49,8 +50,33 @@ public class WalmartSdkClientService {
         headers.put("Authorization", "Basic "+ Base64.encodeBase64String(getclient(clientId, clientSecret).getBytes()));
 
         Map<String, Object> param = new HashMap();
-        param.put("grant_type", "authorization_code");
+        param.put("grant_type", "client_credentials");
         String bodyStr = OkHttpUtils.doPost(baseUrl, param, headers);
+        return bodyStr;
+    }
+
+    /**
+     * 发送请求到沃尔玛
+     * @param baseUrl
+     * @param consumerId 对此ID使用随机生成的GUID
+     * @param clientId
+     * @param clientSecret
+     * @return java.lang.String
+     */
+    public String sendWalmartPostCode(String baseUrl, String consumerId, String clientId, String clientSecret) {
+        Map<String, String> headers = new HashMap();
+        headers.put("Content-Type", WalmartStaticKey.accept_application);
+        headers.put("WM_SVC.NAME", WalmartStaticKey.WM_SVC_NAME);
+        headers.put("WM_QOS.CORRELATION_ID", consumerId);
+        headers.put("Accept", WalmartStaticKey.accept_json);
+        headers.put("WM_SVC.VERSION", "1.0.0");
+//        headers.put("Authorization", "Basic "+ Base64.encodeBase64String(getclient(clientId, clientSecret).getBytes()));
+        headers.put("Authorization", "Basic "+ Base64.encodeBase64String(getclient(clientId, clientSecret).getBytes()));
+
+        Map<String, Object> param = new HashMap();
+        param.put("grant_type", "authorization_code");
+        param.put("grant_type", "eyJraWQiOiI4OTljZWU0ZS00NTAwLTRjYTItOTNmMC0wYzExMDZmOWZlNWEiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..hyqrkig7QOo_HAZy.n1E9sIpicLinNVhTLC-2IFjQ_S7nKnTcvDbPBOU-CD084dHmUvQht6sKKp1UNKug6cOntj03Shko8vxpjohnanmsbT-CHBZxyjI_kTGhtMk_g4hU8DOtBcddI3Pql794utv-SIFFCnZGQjDUz1GAF5OAsBkLKT6ctlcYAGcULENBqI_rxMbNuiZQa-GIZqBNNe3CJtunkZ-vcwdXP8FY7EBJHGKNLbDk5dESQKdy64QQ6QZvJZWW_ccPLA23URXgBLHgg3umATj65jpysB2Hti1r-rtyFqvZHWo1p1Lv4sxjORi8t5XNF9SI9OHRWPkz8Ic8vguBQgmrpC7veV8MuCkUS_xUIOIE-gDb-A8M9rFtP22d4nlnxyunsl6ot32OkBrd7pX50NcbWROHqEERgs1w-UpRhq6jmYblYJwoOcN3Vc3_MHmKO2ZV16dftc_3bkBd_oLdMsFbzPUNnlKbAtAF2jUHTelXeZy8lrrSgMcN-xOswIGyMcF1iYpbwA3_p2oPeDFfBuL7anATn8eiTkAEP3sMvNKlYVCEXhenZUsMM-OHEiCZ0zXO1oaLHlq_rlXU7cCUuNQWOcjDqBUM5eX5RfNPD23TltXVYWvuMcUF46WvrrCzYze3ueR2jWZA7D3gUcl6nEUcwSQpZo9935VYZI11TvAF-mGjliJhDpadNbIpN29d2Q2lhzUk.ca5mN8q74ErIwi9AgHWQSQ");
+        String bodyStr = OkHttpUtils.doPost("", param, headers);
         return bodyStr;
     }
 
@@ -74,10 +100,34 @@ public class WalmartSdkClientService {
 
         Map<String, Object> param = new HashMap();
         param.put("grant_type", "refresh_token");
-        param.put("refresh_token", "eyJraWQiOiI4OTljZWU0ZS00NTAwLTRjYTItOTNmMC0wYzExMDZmOWZlNWEiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..MVRtY67kPIG5wGpV.n8aq61jeTefcYxDeNUXdbJBmomWKKk9gOTJXjJrYJdRnmTFkBNN-mKEKrL-9hCbzBiRTiM48iFUONouZDiikydWA4xuDJtheFHo2cSi7Rt31EWHYLotLnyDAOtMKvVDF7YWq_UEJga9Jvb8koI1ORZkeN8_1bYqvhdK9wrMzndVNITbeS93sY-ABAzlPIx1jqw1-xjQd5cWppQg_fvkpUedfR1AJJxoZ7w7uqmvRrg5NOwu058KtElCxx64-nVpaq8q7Tim71Y5Xu9wr9wUAG6Q6CkB1mLyhnoyNr_09ssyLwFrvCU9cwvIheoIH23z-4yMF8wO2nHngogh1lOmuvjjy7nuJfpm8HmxroTPoxwfkSGDcn7m4edHIox2ZZTsh2TJy1ThfZQXDnszIGooDxQuAaz2fYHehB9R4fV1ZxZot9rpF2u_KjbklVgvLEufg_jRkgH1TyRQM1jcES0uw57F-9pi2fqGCysiBgvLa9DVNpn5_mCYemhCeuI81PnK0rV5kP3Ns3bd6Q8DkdqWzqQEOsEMpurVdwm3HJtwMvQt46oM7undMDLTPgRa4pB8MLDPNzdIftpjoqtET8TYwY1GJS2EB2Y2teGWf-22IpgpuboCkY_q_IV99VM3BTwRHQyGMKboawjDP-ZDPmxW6ZutJLZ25kYTzG8l33LW-C1nE3Yo0wJMsGdBbUP_u.5ou4B-gFK0xQBKBGLMUIpQ");
+        param.put("refresh_token", "eyJraWQiOiI4OTljZWU0ZS00NTAwLTRjYTItOTNmMC0wYzExMDZmOWZlNWEiLCJlbmMiOiJBMjU2R0NNIiwiYWxnIjoiZGlyIn0..hyqrkig7QOo_HAZy.n1E9sIpicLinNVhTLC-2IFjQ_S7nKnTcvDbPBOU-CD084dHmUvQht6sKKp1UNKug6cOntj03Shko8vxpjohnanmsbT-CHBZxyjI_kTGhtMk_g4hU8DOtBcddI3Pql794utv-SIFFCnZGQjDUz1GAF5OAsBkLKT6ctlcYAGcULENBqI_rxMbNuiZQa-GIZqBNNe3CJtunkZ-vcwdXP8FY7EBJHGKNLbDk5dESQKdy64QQ6QZvJZWW_ccPLA23URXgBLHgg3umATj65jpysB2Hti1r-rtyFqvZHWo1p1Lv4sxjORi8t5XNF9SI9OHRWPkz8Ic8vguBQgmrpC7veV8MuCkUS_xUIOIE-gDb-A8M9rFtP22d4nlnxyunsl6ot32OkBrd7pX50NcbWROHqEERgs1w-UpRhq6jmYblYJwoOcN3Vc3_MHmKO2ZV16dftc_3bkBd_oLdMsFbzPUNnlKbAtAF2jUHTelXeZy8lrrSgMcN-xOswIGyMcF1iYpbwA3_p2oPeDFfBuL7anATn8eiTkAEP3sMvNKlYVCEXhenZUsMM-OHEiCZ0zXO1oaLHlq_rlXU7cCUuNQWOcjDqBUM5eX5RfNPD23TltXVYWvuMcUF46WvrrCzYze3ueR2jWZA7D3gUcl6nEUcwSQpZo9935VYZI11TvAF-mGjliJhDpadNbIpN29d2Q2lhzUk.ca5mN8q74ErIwi9AgHWQSQ");
         String bodyStr = OkHttpUtils.doPost(baseUrl, param, headers);
         return bodyStr;
     }
+
+/*    *//**
+     * 发送请求到沃尔玛
+     * @param baseUrl
+     * @param consumerId 对此ID使用随机生成的GUID
+     * @param clientId
+     * @param clientSecret
+     * @return java.lang.String
+     *//*
+    public String sendWalmartPostRefreshToeken(String baseUrl, String consumerId, String clientId, String clientSecret) {
+        WalmartSign walmartSign = new WalmartSign(consumerId, "https://marketplace.walmartapis.com/v3/items", privateEncodedStr);
+        Map<String, Object> headers = new HashMap<String, Object>();
+        headers.put("Content-Type", WalmartStaticKey.accept_json);
+        headers.put("WM_SVC.NAME", WalmartStaticKey.WM_SVC_NAME);
+        headers.put("WM_QOS.CORRELATION_ID", WalmartStaticKey.CORRELATION_ID);
+        headers.put("WM_SEC.TIMESTAMP", walmartSign.getTimestamp());
+        headers.put("WM_SEC.AUTH_SIGNATURE", walmartSign.getSign("POST"));
+        headers.put("WM_CONSUMER.CHANNEL.TYPE", channelType);
+        headers.put("WM_CONSUMER.ID", consumerId);
+        headers.put("Accept", WalmartStaticKey.accept_json);
+        if(sslClient == null)sslClient = new SSLClient();
+        return sslClient.doPost(walmartSign.getBaseUrl(), headers, null, entity, 1000*60);
+        return bodyStr;
+    }*/
 
 
     private String getclient(String clientId, String clientSecret) {
