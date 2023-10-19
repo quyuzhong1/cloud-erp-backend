bc76d881ab264e37b09411aa1af59b0ee46501be
luoweigang <821969511@qq.com>
2023-10-19 10:34:59 +0800

    迭代：收货单同步金蝶

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeePoReceiveConsumer.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/KingdeePoReceiveConsumer.java
@@ -2,17 +2,10 @@ package com.erp.server.dmp.push.consumer;
 
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import com.common.business.dto.DmpSyncMqDTO;
-import com.common.business.dto.DmpSyncTaskIdDTO;
-import com.common.business.enums.SyncStatusEnum;
-import com.common.core.controller.vo.ApiResult;
 import com.common.message.constant.RocketMqConsumerGroup;
 import com.common.message.constant.RocketMqTopic;
-import com.common.message.handler.AbstractPlatformConsumerHandler;
 import com.erp.server.dmp.push.service.business.KingdeePoReceiveConsumerService;
 import com.erp.server.dmp.push.service.business.KingdeeProductDetailConsumerService;
-import com.erp.server.dmp.push.service.business.KingdeeReturnOrderConsumerService;
-import com.erp.server.dmp.service.DmpPushTaskService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.annotation.ConsumeMode;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
@@ -32,23 +25,19 @@ import java.util.Map;
 @Service
 @Slf4j
 @RocketMQMessageListener(topic = RocketMqTopic.SYNC_KINGDEE_ERP_TOPIC, selectorExpression = "kingdee_po_receive_tag", consumerGroup = RocketMqConsumerGroup.SYNC_ERP_PO_RECEIVE,consumeMode = ConsumeMode.ORDERLY)
-public class KingdeePoReceiveConsumer<T extends DmpSyncTaskIdDTO> extends AbstractPlatformConsumerHandler<T> {
+public class KingdeePoReceiveConsumer implements RocketMQListener<Map<String, Object>> {
     @Resource
     private KingdeePoReceiveConsumerService kingdeePoReceiveConsumerService;
 
-    @Resource
-    private DmpPushTaskService dmpPushTaskService;
-
 
     @Override
-    public void updateSyncTaskStatus(String syncTaskId, SyncStatusEnum code, String msg) {
-        dmpPushTaskService.updateStatus(new DmpSyncMqDTO.ParamDTO(syncTaskId, code.getCode(), msg));
-    }
+    public void onMessage(Map<String, Object> map) {
+        try {
+            kingdeePoReceiveConsumerService.executeConsumer(map);
+        }catch (Exception e){
+            log.error("KingdeePoReceiveConsumer>>>onMessage>>>map ={}>>>e={}", map, e);
+        }
 
-    @Override
-    public ApiResult<?> handle(Object ext) {
-        Map<String, Object> map = JSONUtil.parseObj(ext);
-        kingdeePoReceiveConsumerService.executeConsumer(map);
-        return ApiResult.success();
     }
 }
+
