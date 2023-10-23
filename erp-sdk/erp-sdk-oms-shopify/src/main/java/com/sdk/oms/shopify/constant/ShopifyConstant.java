d9e4d8c52d7d3bee80a9d6e738ebf42cb0ae2ae4
Jim <zhoujingbo@ulanzi.cn>
2023-10-23 20:33:02 +0800

    ADD:OMS shopify 添加权限2

--- erp-sdk/erp-sdk-oms-shopify/src/main/java/com/sdk/oms/shopify/constant/ShopifyConstant.java
+++ erp-sdk/erp-sdk-oms-shopify/src/main/java/com/sdk/oms/shopify/constant/ShopifyConstant.java
@@ -13,7 +13,7 @@ public interface ShopifyConstant {
     /**
      * 店铺授权的范围
      */
-    String SHOP_SCOPE = "read_orders,read_products,write_products,write_orders,write_fulfillments,read_fulfillments";
+    String SHOP_SCOPE = "read_orders,read_products,write_products,write_orders,write_fulfillments,read_fulfillments,read_merchant_managed_fulfillment_orders,write_merchant_managed_fulfillment_orders";
 
     String DOMAIN = ".myshopify.com";
 
