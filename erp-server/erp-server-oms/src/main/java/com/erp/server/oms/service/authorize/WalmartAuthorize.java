f88fc399aa5349445b871ae1be92f315a4c6bd8d
luoweigang <821969511@qq.com>
2023-10-24 10:59:14 +0800

    OMS:修改平台授权

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/authorize/WalmartAuthorize.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/authorize/WalmartAuthorize.java
@@ -1,8 +1,14 @@
 package com.erp.server.oms.service.authorize;
 
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.common.business.constant.RedisCacheConstants;
 import com.common.business.enums.PlatformDictEnum;
+import com.common.business.utils.RedisUtil;
+import com.common.core.enums.ApiError;
+import com.common.core.exception.ServiceException;
 import com.erp.model.dmp.dto.CfgAppClientDTO;
+import com.erp.model.dmp.dto.PlatformTaskDTO;
 import com.erp.model.dmp.enums.AppClientEnum;
 import com.erp.model.oms.dto.ShopAuthorizeDTO;
 import com.erp.model.oms.entity.ShopAuthEntity;
@@ -14,14 +20,19 @@ import com.erp.server.oms.service.AuthSaveData;
 import com.erp.server.oms.service.IShopAuthorizeService;
 import com.erp.server.oms.service.ShopAuthService;
 import com.erp.server.oms.service.ShopInfoService;
+import com.sdk.oms.shopify.constant.ShopifyConstant;
+import com.sdk.oms.shopify.dto.ShopifyShopInfoDTO;
 import com.sdk.oms.walmart.api.WalmartStaticKey;
+import com.sdk.oms.walmart.dto.WalmartShopInfoDTO;
 import com.sdk.oms.walmart.dto.walmart.WalmartTokenDTO;
 import com.sdk.oms.walmart.service.WalmartSdkClientService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import javax.sql.rowset.serial.SerialException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
@@ -30,6 +41,8 @@ import java.util.Objects;
  * @Author Luo_WG
  * @Date 2023/10/23 18:01
  **/
+
+@Slf4j
 @Component
 @AuthSaveData(method = PlatformDictEnum.WALMART)
 public class WalmartAuthorize implements IShopAuthorizeService<T> {
@@ -42,6 +55,9 @@ public class WalmartAuthorize implements IShopAuthorizeService<T> {
     @Resource
     private DmpTaskFeign dmpTaskFeign;
 
+    @Resource
+    private RedisUtil redisUtil;
+
     /**
      * 授权
      * @param dto
@@ -50,50 +66,89 @@ public class WalmartAuthorize implements IShopAuthorizeService<T> {
     @Override
     public Boolean shopAuthorize(ShopAuthorizeDTO dto) {
         String shopId = dto.getShopId();
-        ShopInfoEntity shopInfo = shopInfoService.getById(shopId);
-
-        //根据店铺id 获取到授权信息
-        ShopAuthEntity shopAuth = shopAuthService.getByShopId(shopId);
-        if (Objects.isNull(shopAuth)) {
-            shopAuth = new ShopAuthEntity();
+        if (StringUtils.isBlank(shopId)) {
+            throw new ServiceException(ApiError.ERROR_WALMART_SHOP_ID_NOT_NULL);
         }
-        String appClientId = shopAuth.getAppClientId();
-        //判断是否有授权过，如果没有就新增保存店铺秘钥信息，如果有就修改秘钥信息重新授权
-        if (StringUtils.isBlank(shopAuth.getAppClientId())) {
-            CfgAppClientDTO.AddDTO addDTO = new CfgAppClientDTO.AddDTO();
-            addDTO.setBusinessType(AppClientEnum.WALMART_AUTHORIZE.getBusinessType());
-            addDTO.setPlatformType(AppClientEnum.WALMART_AUTHORIZE.getPlatformType());
-            addDTO.setDictPlatform(dto.getPlatformCode());
-            addDTO.setClientId(dto.getClientId());
-            addDTO.setClientSecret(dto.getClientSecret());
-            appClientId = dmpTaskFeign.addCfgAppClient(addDTO);
-        } else {
-            CfgAppClientDTO.UpdateDTO updateDTO = new CfgAppClientDTO.UpdateDTO();
-            updateDTO.setId(appClientId);
-            updateDTO.setBusinessType(AppClientEnum.WALMART_AUTHORIZE.getBusinessType());
-            updateDTO.setPlatformType(AppClientEnum.WALMART_AUTHORIZE.getPlatformType());
-            updateDTO.setDictPlatform(dto.getPlatformCode());
-            updateDTO.setClientId(dto.getClientId());
-            updateDTO.setClientSecret(dto.getClientSecret());
-            dmpTaskFeign.updateCfgAppClient(updateDTO);
+        if (StringUtils.isBlank(dto.getClientId())) {
+            throw new ServiceException(ApiError.ERROR_WALMART_CLIENT_ID_NOT_NULL);
         }
-
-        //获取地址
-        String url = WalmartStaticKey.baseUrl + "token";
-
-        WalmartSdkClientService walmartSdkClientService = new WalmartSdkClientService();
-        WalmartTokenDTO walmartTokenDTO = walmartSdkClientService.sendWalmartPostToken(url, dto.getClientId(), dto.getClientSecret());
-        if (ObjectUtil.isEmpty(walmartTokenDTO)) {
-            return Boolean.FALSE;
+        if (StringUtils.isBlank(dto.getClientSecret())) {
+            throw new ServiceException(ApiError.ERROR_WALMART_CLIENT_SECRET_NOT_NULL);
         }
-        shopAuth.setShopId(shopId);
-        shopAuth.setExpiresIn(Integer.valueOf(walmartTokenDTO.getExpiresIn()));
-        shopAuth.setAppClientId(appClientId);
-        shopAuth.setAccessToken(walmartTokenDTO.getAccessToken());
-        shopInfo.setAuthStatus(AuthStatusEnum.ALREADY.getCode());
-        shopInfo.setAuthTime(LocalDateTime.now());
-        shopAuthService.saveOrUpdate(shopAuth);
-        return Boolean.TRUE;
+        try {
+            ShopInfoEntity shopInfo = shopInfoService.getById(shopId);
+            //根据店铺id 获取到授权信息
+            ShopAuthEntity shopAuth = shopAuthService.getByShopId(shopId);
+            if (Objects.isNull(shopAuth)) {
+                shopAuth = new ShopAuthEntity();
+            }
+            String appClientId = shopAuth.getAppClientId();
+            //判断是否有授权过，如果没有就新增保存店铺秘钥信息，如果有就修改秘钥信息重新授权
+            if (StringUtils.isBlank(shopAuth.getAppClientId())) {
+                CfgAppClientDTO.AddDTO addDTO = new CfgAppClientDTO.AddDTO();
+                addDTO.setBusinessType(AppClientEnum.WALMART_AUTHORIZE.getBusinessType());
+                addDTO.setPlatformType(AppClientEnum.WALMART_AUTHORIZE.getPlatformType());
+                addDTO.setDictPlatform(dto.getPlatformCode());
+                addDTO.setClientId(dto.getClientId());
+                addDTO.setClientSecret(dto.getClientSecret());
+                appClientId = dmpTaskFeign.addCfgAppClient(addDTO);
+            } else {
+                CfgAppClientDTO.UpdateDTO updateDTO = new CfgAppClientDTO.UpdateDTO();
+                updateDTO.setId(appClientId);
+                updateDTO.setBusinessType(AppClientEnum.WALMART_AUTHORIZE.getBusinessType());
+                updateDTO.setPlatformType(AppClientEnum.WALMART_AUTHORIZE.getPlatformType());
+                updateDTO.setDictPlatform(dto.getPlatformCode());
+                updateDTO.setClientId(dto.getClientId());
+                updateDTO.setClientSecret(dto.getClientSecret());
+                dmpTaskFeign.updateCfgAppClient(updateDTO);
+            }
+
+            //获取地址
+            String url = WalmartStaticKey.baseUrl + "token";
+
+            WalmartSdkClientService walmartSdkClientService = new WalmartSdkClientService();
+            WalmartTokenDTO walmartTokenDTO = walmartSdkClientService.sendWalmartPostToken(url, dto.getClientId(), dto.getClientSecret());
+            if (ObjectUtil.isEmpty(walmartTokenDTO)) {
+                return Boolean.FALSE;
+            }
+            shopAuth.setShopId(shopId);
+            shopAuth.setExpiresIn(Integer.valueOf(walmartTokenDTO.getExpiresIn()));
+            shopAuth.setAppClientId(appClientId);
+            shopAuth.setAccessToken(walmartTokenDTO.getAccessToken());
+            shopInfo.setAuthStatus(AuthStatusEnum.ALREADY.getCode());
+            shopInfo.setAuthTime(LocalDateTime.now());
+            shopAuthService.saveOrUpdate(shopAuth);
+            boolean result = shopInfoService.updateById(shopInfo);
+            // 授权后添加任务
+            dmpTaskFeign.createPlatformTask(new PlatformTaskDTO.AddDTO(shopInfo.getId(), shopInfo.getDictPlatform()));
+            shopInfo.setIsGenTask(Boolean.TRUE);
+            shopInfoService.updateShopInfoById(shopInfo);
+            // 添加到缓存redis
+            WalmartShopInfoDTO shopInfoDTO = new WalmartShopInfoDTO()
+                    // 店铺ID
+                    .setId(shopInfo.getId())
+                    // 访问token
+                    .setAccessToken(walmartTokenDTO.getAccessToken())
+                    // 店铺名称
+                    .setName(shopInfo.getName())
+                    // 区域id
+                    .setDictAreaCode(shopInfo.getDictAreaCode())
+                    // 国家id
+                    .setDictCountryCode(shopInfo.getDictCountryCode())
+                    // 负责人id
+                    .setChargeId(shopInfo.getChargeId())
+                    //平台账户id
+                    .setClientId(dto.getClientId())
+                    //平台店铺秘钥
+                    .setClientSecret(dto.getClientSecret());
+            // platform-token:平台名称:店铺ID
+            String tokenKey = StrUtil.format(RedisCacheConstants.REDIS_PLATFORM_TOKEN, PlatformDictEnum.WALMART.getCode(), shopId);
+            redisUtil.set(tokenKey, shopInfoDTO);
+            return result;
+        } catch (Exception e) {
+            log.error("沃尔玛平台店铺授权出错了===> bodyStr==>{} e==>{}", dto, e);
+        }
+        return Boolean.FALSE;
     }
 
     /**
