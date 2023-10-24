3a03ca77567752317c9c7ee15c5ea7212bd6d896
luoweigang <821969511@qq.com>
2023-10-24 14:22:42 +0800

    新增沃尔玛拉取任务

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/schedule/PullWalmartJob.java
@@ -0,0 +1,32 @@
+package com.erp.server.dmp.pull.schedule;
+
+import com.common.business.enums.PlatformDictEnum;
+import com.erp.server.dmp.pull.thread.PlatformDataThread;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+@Slf4j
+@EnableScheduling
+public class PullWalmartJob {
+    @Resource
+    private PlatformDataThread platformDataThread;
+
+    @Resource(name = "pullErpOpenApi")
+    private ThreadPoolTaskExecutor threadPoolTaskExecutor;
+
+    /**
+     * 拉取Shopify任务
+     */
+    @XxlJob("walmartExecute")
+    public void execute() {
+        threadPoolTaskExecutor.execute(() -> {
+            platformDataThread.executeTask(PlatformDictEnum.WALMART.getCode());
+        });
+    }
+}
