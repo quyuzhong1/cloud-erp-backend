fae3dd27ddd8ec937df76a04868c1ce7c8e523e7
Cloud <cloud@ulanzi.cn>
2023-10-25 19:21:53 +0800

    【 注释 盘盈盘亏单审核 时盘点仓库 库区，库位禁用 】

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingProfitLossServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingProfitLossServiceImpl.java
@@ -366,8 +366,9 @@ public class StocktakingProfitLossServiceImpl extends SuperServiceImpl<Stocktaki
         if(CollectionUtil.isEmpty(detailList)){
             throw new ServiceException("未找到盘盈盘亏单详情");
         }
-        List<WarehouseDTO.WarehouseDisabledAssertDTO> assertList = detailList.stream().map(WarehouseDTO.WarehouseDisabledAssertDTO::new).collect(Collectors.toList());
-        warehouseService.assertDisabled(assertList);
+        // TODO 未测试完成，先做注释处理，本周五完成测试后放开
+//        List<WarehouseDTO.WarehouseDisabledAssertDTO> assertList = detailList.stream().map(WarehouseDTO.WarehouseDisabledAssertDTO::new).collect(Collectors.toList());
+//        warehouseService.assertDisabled(assertList);
         // 调用流程审核
         approveProcess(entity, dto);
         // 操作日志
