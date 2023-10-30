1426c67a7220441f11b53640d038e634a3b3b5b3
luoweigang <821969511@qq.com>
2023-10-30 12:35:55 +0800

    新增同步wms

--- erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysUserFeign.java
+++ erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysUserFeign.java
@@ -1,6 +1,7 @@
 package com.erp.rpc.sys.feign;
 
 
+import com.common.business.dto.DmpSyncMqDTO;
 import com.common.business.dto.FindUserDTO;
 import com.common.business.dto.UserRequestPermissionsDTO;
 import com.common.business.dto.base.BaseIdDTO;
