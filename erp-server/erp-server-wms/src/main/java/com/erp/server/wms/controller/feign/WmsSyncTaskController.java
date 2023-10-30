efa444b89d0017e39f47113fde498e55acbf15fa
luoweigang <821969511@qq.com>
2023-10-30 12:23:09 +0800

    新增同步wms

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/WmsSyncTaskController.java
@@ -0,0 +1,37 @@
+package com.erp.server.wms.controller.feign;
+
+import com.common.business.dto.DmpSyncMqDTO;
+import com.erp.server.wms.service.SyncTaskService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 推送数据
+ * @Author Luo_WG
+ * @Date 2023/10/30 11:49
+ **/
+
+@RestController
+@RequestMapping("feign/wmsSyncTask")
+public class WmsSyncTaskController {
+
+
+    @Resource
+    private SyncTaskService syncTaskService;
+
+    /**
+     * 更新任务列表负责人名称
+     * @author Will
+     * @date: 2023/10/19 11:07
+     * @param syncParamDTO
+     */
+    @PostMapping("/findDataSendSyncTask")
+    public void findDataSendSyncTask(@RequestBody @Validated DmpSyncMqDTO.SyncParamDTO syncParamDTO) {
+        syncTaskService.findDataSendSyncTask(syncParamDTO);
+    }
+}
