a9ae10f83d7f2358b129677915f4ff5d5bc07d48
Will <wangwei@ulanzi.cn>
2023-11-01 16:36:15 +0800

    销售出库单调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/SoOutstockMapper.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/SoOutstockMapper.java
@@ -63,4 +63,13 @@ public interface SoOutstockMapper extends BaseMapper<SoOutstockEntity> {
      * @param soOutstockList
      */
     void updateBatch(@Param("params") List<SoOutstockEntity> soOutstockList);
+    /**
+     * @description: 查询合计数据
+     * @author Will
+     * @date: 2023/11/1 14:25
+     * @param params
+     * @param approveList
+     * @return PagingTotalDTO
+     */
+    SoOutstockDTO.PagingTotalDTO getTotalByQuery(@Param("params") SoOutstockDTO.PagingParamDTO params,@Param("approveList") List<String> approveList);
 }
