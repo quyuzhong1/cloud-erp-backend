3877d20eff06ba495f97b3a44dc4ef910cea84f5
liuruipeng <arthur@ulanzi.cn>
2023-11-03 14:10:40 +0800

    ADD 纬狮物流接口

---
+++ erp-sdk/erp-sdk-tms-weishi/src/main/java/com/sdk/tms/weishi/dto/response/WeiShiCreateOrder.java
@@ -0,0 +1,31 @@
+package com.sdk.tms.weishi.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author liuruipeng
+ */
+@Data
+@NoArgsConstructor
+@ToString
+public class WeiShiCreateOrder extends WeiShiResponse<Object>{
+
+    private String agentNumber;
+
+    //纬狮跟踪单号
+    private String orderCode;
+
+    //客户参考号
+    private String referenceNo;
+
+    //最后一公里服务商单号
+    private String shippingMethodNo;
+
+    //标签下载地址
+    private String labelUrl;
+
+    //标签类型
+    private String label_type;
+}
