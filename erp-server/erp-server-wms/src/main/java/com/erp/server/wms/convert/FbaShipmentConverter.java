17eec74e0f0f570b8f19e1c4e4975267be948fd2
luoweigang <821969511@qq.com>
2023-11-07 15:44:49 +0800

    FBA：接口更新11-7-2

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/convert/FbaShipmentConverter.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/convert/FbaShipmentConverter.java
@@ -1,11 +1,11 @@
 package com.erp.server.wms.convert;
 
 import com.common.business.dto.base.BaseIdDTO;
-import com.erp.model.wms.dto.FbaDeliveryDTO;
-import com.erp.model.wms.dto.FbaDeliveryDetailDTO;
-import com.erp.model.wms.dto.FbaShipmentDTO;
-import com.erp.model.wms.dto.FbaShipmentDetailDTO;
+import com.common.business.enums.SourceTypeEnum;
+import com.common.core.anno.StateEnumValue;
+import com.erp.model.wms.dto.*;
 import com.erp.model.wms.entity.*;
+import com.erp.model.wms.enums.TransferDirectionEnum;
 import com.erp.server.wms.convert.tool.TypeConversionWorker;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,9 +14,11 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -71,4 +73,20 @@ public interface FbaShipmentConverter {
             @Mapping(target = "stockSku", defaultValue = "")
     })
     FbaDeliveryDetailDTO.AddDTO fbaGenerateDeliverViewToDeliveryDetailAdd(FbaShipmentDTO.GenerateDeliverView view);
+
+/*    @Mappings({
+
+            @Mapping(target = "sourceId", source = "id"),
+            @Mapping(target = "sourceCode", source = "code"),
+            @Mapping(target = "type", source = ""),
+            @Mapping(target = "billDate", source = ""),
+            @Mapping(target = "detailList", source = ""),
+            @Mapping(target = "inWarehouseId", source = ""),
+            @Mapping(target = "outWarehouseId", source = ""),
+            @Mapping(target = "transferDirection", source = ""),
+            @Mapping(target = "type", source = ""),
+            @Mapping(target = "warehouseKeeperId", source = "")
+    })
+    TransferOutDTO.AddDTO fbaGenerateDeliverViewToDeliveryDetailAdd(FbaDeliveryEntity entity);*/
 }
+
