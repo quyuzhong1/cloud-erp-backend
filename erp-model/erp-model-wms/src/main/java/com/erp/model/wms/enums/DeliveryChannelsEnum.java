468af14a54ca090cae43b8199dbb06b2fd1aed7b
luoweigang <821969511@qq.com>
2023-11-08 17:00:07 +0800

    FBA:修改发货单123

---
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/DeliveryChannelsEnum.java
@@ -0,0 +1,55 @@
+package com.erp.model.wms.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.common.core.constant.EnumMessage;
+import com.fasterxml.jackson.annotation.JsonValue;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 配送渠道
+ */
+public enum DeliveryChannelsEnum implements EnumMessage {
+    SELF_DELIVERY("self_delivery","卖家自配送"),
+    AMAZON_DELIVERY("amazon_delivery","亚马逊配送"),
+    ;
+
+    ;
+    /**
+     * 类型
+     */
+    @EnumValue
+    @JsonValue
+    private String code;
+    /**
+     * 名称
+     */
+    private String name;
+
+
+    DeliveryChannelsEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static String getName(String code) {
+        if (StringUtils.isBlank(code)) {
+            return "";
+        }
+        for (DeliveryChannelsEnum channelsEnum : DeliveryChannelsEnum.values()) {
+            if (code.equals(channelsEnum.getCode())) {
+                return channelsEnum.getName();
+            }
+        }
+        return "";
+    }
+
+}
