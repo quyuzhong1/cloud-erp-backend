5729841ae2b92f64e0738baefc3cc1b91e693c75
luoweigang <821969511@qq.com>
2023-11-08 14:36:34 +0800

    FBA:修改发货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaDeliveryServiceImpl.java
@@ -569,9 +569,7 @@ public class FbaDeliveryServiceImpl extends SuperServiceImpl<FbaDeliveryMapper, 
         //来源类型名称
         data.setSourceTypeName(SourceTypeEnum.getName(data.getSourceType()));
         //设置状态中文名称
-        data.setApproveStatusName(ApproveStatusEnum.getName(data.getApproveStatus()));
-        //审核状态名称
-        data.setApproveStatusName(ApproveStatusEnum.getName(data.getApproveStatus()));
+        data.setApproveStatusName(ApproveStatusEnum.getName(data.getApproveStatus().getStatus()));
         //备货类型名称
         data.setDemandTypeName(FbaDemandTypeEnum.getName(data.getDemandType()));
         //作废状态名称
