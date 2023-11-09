d38e2f2eaec6aa08718bf156dd5aa2b1e4828ba8
luoweigang <821969511@qq.com>
2023-11-09 11:50:30 +0800

    FBA:修改发货单119-3

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/FbaInventoryDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/FbaInventoryDTO.java
@@ -424,6 +424,53 @@ public class FbaInventoryDTO implements Serializable {
     }
 
     /**
+     * 列表汇总数量
+     */
+    @Data
+    @NoArgsConstructor
+    public static class SummaryNumber {
+        /**
+         * FBM可售
+         */
+        private String fbmFulfillableQty;
+
+        /**
+         * 计划入库数量
+         */
+        private Integer inboundWorkingQty;
+
+        /**
+         * 已发货数量
+         */
+        private Integer inboundShippedQty;
+
+        /**
+         * 入库中数量
+         */
+        private Integer inboundReceivingQty;
+
+        /**
+         * FBI可售
+         */
+        private Integer fulfillableQty;
+
+        /**
+         * 预留
+         */
+        private Integer reservedQty;
+
+        /**
+         * 调查中数量
+         */
+        private Integer researchingQty;
+
+        /**
+         * 不可售数量
+         */
+        private Integer unsellableQty;
+    }
+
+    /**
      * 导出Excel
      */
     @EqualsAndHashCode(callSuper = true)
