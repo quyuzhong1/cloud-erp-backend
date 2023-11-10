f427b48d30014d8b3fd179ee39e1f8fadb2eea93
Will <wangwei@ulanzi.cn>
2023-11-10 19:37:24 +0800

    费用计算调整

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/mapper/ShippingTemplateOtherCostMapper.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/mapper/ShippingTemplateOtherCostMapper.java
@@ -1,8 +1,14 @@
 package com.erp.server.tms.mapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.erp.model.tms.dto.ShippingCalculationDTO;
 import com.erp.model.tms.entity.ShippingTemplateOtherCostEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 /**
@@ -15,5 +21,21 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ShippingTemplateOtherCostMapper extends BaseMapper<ShippingTemplateOtherCostEntity> {
-
+    /**
+     * @description: 分页查询
+     * @author Will
+     * @date: 2023/11/10 17:44
+     * @param query
+     * @param params
+     * @return IPage<ListDTO>
+     */
+    IPage<ShippingCalculationDTO.ListDTO> paging(Page query, @Param("params") ShippingCalculationDTO.PagingParamDTO params);
+    /**
+     * @description: 导出查询
+     * @author Will
+     * @date: 2023/11/10 17:45
+     * @param params
+     * @return List<ListDTO>
+     */
+    List<ShippingCalculationDTO.ListDTO> listByExportExcel( @Param("params")ShippingCalculationDTO.PagingParamDTO params);
 }
