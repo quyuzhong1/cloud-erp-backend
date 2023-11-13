9bfb347782171b5a02306324a9f7a1e74d39a476
luoweigang <821969511@qq.com>
2023-11-13 19:00:51 +0800

    FBA:bug修复

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -805,6 +805,7 @@ public enum ApiError implements Serializable {
     FBA_DELIVERY_INVENTORY_INSUFFICIENT(99104,"提示：SKU【】发货仓【】可用库存不足，无法审核发货单"),
     ERROR_GENERATE_TRANSFER_OUT(99105,"生成分步式调出单失败!"),
     FBA_SHIPMENT_RECEIVE_EXIST(99106,"已经有货件签收数量的发货单不允许反审核!"),
+    IS_DELIVERY_FINISH(99107,"只有已发货的货件才能完结!"),
 
 
 
