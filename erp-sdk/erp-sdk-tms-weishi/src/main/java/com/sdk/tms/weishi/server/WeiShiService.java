56170fd964eb5a124aebf3962b3e1cb8504016f2
liuruipeng <arthur@ulanzi.cn>
2023-11-13 17:18:28 +0800

    纬狮物流接口修改密钥获取

--- erp-sdk/erp-sdk-tms-weishi/src/main/java/com/sdk/tms/weishi/server/WeiShiService.java
+++ erp-sdk/erp-sdk-tms-weishi/src/main/java/com/sdk/tms/weishi/server/WeiShiService.java
@@ -2,6 +2,7 @@ package com.sdk.tms.weishi.server;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.erp.model.tms.entity.LogisticsAuthEntity;
 import com.sdk.tms.weishi.constants.WeiShiConstants;
 import com.sdk.tms.weishi.dto.request.*;
 import com.sdk.tms.weishi.dto.response.*;
@@ -20,48 +21,48 @@ public class WeiShiService {
      *  查询全部已开通的渠道
      * @return List<YanWenChannel>
      */
-    public WeiShiResponse<List<WeiShiChannel>> getAllChannel(){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_SHIPPING,null);
+    public WeiShiResponse<List<WeiShiChannel>> getAllChannel(LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_SHIPPING,null,logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,new TypeReference<WeiShiResponse<List<WeiShiChannel>>>() {}.getType());
     }
 
     /**
      *  创建订单
      */
-    public WeiShiCreateOrder createOrder(@Valid WeiShiCreateOrderRequest request){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_CREATE_ORDER,JSONObject.toJSONString(request));
+    public WeiShiCreateOrder createOrder(@Valid WeiShiCreateOrderRequest request,LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_CREATE_ORDER,JSONObject.toJSONString(request),logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,WeiShiCreateOrder.class);
     }
 
     /**
      *  打印标签
      */
-    public WeiShiGetLabelUrl getLabelUrl(@Valid WeiShiGetLabelUrlRequest request){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_LABEL,JSONObject.toJSONString(request));
+    public WeiShiGetLabelUrl getLabelUrl(@Valid WeiShiGetLabelUrlRequest request,LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_LABEL,JSONObject.toJSONString(request),logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,WeiShiGetLabelUrl.class);
     }
 
     /**
      * 拦截订单
      */
-    public WeiShiResponse<String> interceptOrder(@Valid WeiShiInterceptOrderRequest request){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_INTERCEPT_ORDER,JSONObject.toJSONString(request));
+    public WeiShiResponse<String> interceptOrder(@Valid WeiShiInterceptOrderRequest request,LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_INTERCEPT_ORDER,JSONObject.toJSONString(request),logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,new TypeReference<WeiShiResponse<String>>() {}.getType());
     }
 
     /**
      * 取消订单
      */
-    public WeiShiResponse<String> cancelOrder(@Valid WeiShiCancelOrderRequest request){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_ORDER_CANCEL,JSONObject.toJSONString(request));
+    public WeiShiResponse<String> cancelOrder(@Valid WeiShiCancelOrderRequest request,LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_ORDER_CANCEL,JSONObject.toJSONString(request),logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,new TypeReference<WeiShiResponse<String>>() {}.getType());
     }
 
     /**
      * 查询跟踪号
      */
-    public WeiShiResponse<List<WeiShiGetTrackNumber>> getTrackNumber(@Valid WeiShiGetTrackNumberRequest request){
-        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_TRACK_NUMBER,JSONObject.toJSONString(request));
+    public WeiShiResponse<List<WeiShiGetTrackNumber>> getTrackNumber(@Valid WeiShiGetTrackNumberRequest request,LogisticsAuthEntity logisticsAuthEntity){
+        String response = WeiShiUtils.sendPost(WeiShiConstants.METHOD_GET_TRACK_NUMBER,JSONObject.toJSONString(request),logisticsAuthEntity.getAccount(),logisticsAuthEntity.getPassword());
         return JSONObject.parseObject(response,new TypeReference<WeiShiResponse<List<WeiShiGetTrackNumber>>>() {}.getType());
     }
 }
