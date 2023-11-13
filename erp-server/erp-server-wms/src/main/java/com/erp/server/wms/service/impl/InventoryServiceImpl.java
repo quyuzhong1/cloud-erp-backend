3e5d96cca36d6ef8f8bef586c9edab5323485a91
Will <wangwei@ulanzi.cn>
2023-11-13 14:29:33 +0800

    即时库存调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/InventoryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/InventoryServiceImpl.java
@@ -654,15 +654,11 @@ public class InventoryServiceImpl extends SuperServiceImpl<InventoryMapper, Inve
             //组织编码
             String orgCode = orgList.stream().filter(obj -> obj.getId().equals(data.getOrgId())).findFirst()
                     .flatMap(obj -> Optional.ofNullable(obj.getCode())).orElse("");
-            Map<String, Object> map = mapList.stream().filter(obj -> data.getSkuNo().equals(obj.get("FMaterialId.FNumber")) && warehouseCode.equals(obj.get("FStockId.FNumber")) && orgCode.equals(obj.get("FStockOrgId.FNumber")))
-                    .findFirst().orElse(null);
-            if (ObjectUtil.isNotEmpty(map)) {
-                //基本单位库存量
-                Object fBaseQty = map.get("FBASEQTY");
-                //单位换算率
-                Object fStoreurnom = map.get("FMaterialid.FSTOREURNOM");
-                Object fStoreurnum = map.get("FMaterialid.FSTOREURNUM");
-                BigDecimal divide = MathUtil.divide(MathUtil.multiply(MathUtil.valueOf(fBaseQty), MathUtil.valueOf(fStoreurnom)), MathUtil.valueOf(fStoreurnum));
+            List<Map<String, Object>> mapQtyList = mapList.stream().filter(obj -> data.getSkuNo().equals(obj.get("FMaterialId.FNumber")) && warehouseCode.equals(obj.get("FStockId.FNumber")) && orgCode.equals(obj.get("FStockOrgId.FNumber")))
+                    .collect(Collectors.toList());
+            if (CollectionUtils.isNotEmpty(mapQtyList)) {
+                //基本单位库存量(需要单位换算)，相同仓库、组织、sku存在多个数据需要合计
+                BigDecimal divide = mapQtyList.stream().map(obj -> MathUtil.divide(MathUtil.multiply(MathUtil.valueOf(obj.get("FBASEQTY")), MathUtil.valueOf(obj.get("FMaterialid.FSTOREURNOM"))), MathUtil.valueOf(obj.get("FMaterialid.FSTOREURNUM")))).reduce(BigDecimal.ZERO,BigDecimal::add);
 
                 //金蝶库存(fBaseQty*fStoreurnom/fStoreurnum)
                 data.setKingdeeQty(divide.stripTrailingZeros().toPlainString());
