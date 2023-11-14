d8cc9daa7fd83f519a449963eb6cf6532b27483d
yl <ylstrive@163.com>
2023-11-14 10:13:07 +0800

    Lambda 2023-11-14 TMS 物流产品联调

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsChannelBlacklistServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsChannelBlacklistServiceImpl.java
@@ -10,15 +10,20 @@ import com.common.business.service.impl.SuperServiceImpl;
 import com.erp.server.tms.service.OperateLogService;
 import com.erp.server.tms.service.CommonService;
 import com.common.core.exception.ServiceException;
+import org.apache.commons.collections4.CollectionUtils;
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import com.erp.model.tms.dto.LogisticsChannelBlacklistDTO;
+
 import java.util.*;
+
 import com.common.core.utils.*;
 import com.common.core.enums.ApiError;
+
 /**
  * <p>
  * 渠道黑名单表 服务实现类
@@ -35,53 +40,46 @@ public class LogisticsChannelBlacklistServiceImpl extends SuperServiceImpl<Logis
     @Autowired
     private CommonService commonService;
 
-    @GlobalTransactional(rollbackFor = Exception.class)
     @Transactional(rollbackFor = Exception.class)
     @Override
-    public BaseResultDTO.AddDTO add(LogisticsChannelBlacklistDTO.AddDTO addDTO) {
-        LogisticsChannelBlacklistEntity logisticsChannelBlacklistEntity = new LogisticsChannelBlacklistEntity();
-        BeanMapperUtils.copy(addDTO, logisticsChannelBlacklistEntity);
+    public Boolean add(String channelId, List<LogisticsChannelBlacklistDTO.AddDTO> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return Boolean.FALSE;
+        }
+        List<LogisticsChannelBlacklistEntity> blacklistList = BeanMapperUtils.copyList(LogisticsChannelBlacklistEntity.class, list);
 
         // 数据处理
-        handleData(logisticsChannelBlacklistEntity);
+        handleData(blacklistList);
 
-        log.info("开始新增渠道黑名单表");
-        boolean save = super.save(logisticsChannelBlacklistEntity);
-        if(!save) {
+        boolean save = super.saveBatch(blacklistList);
+        if (!save) {
             throw new ServiceException("渠道黑名单表保存失败");
         }
 
-        // 操作日志
-        String msg = StrUtil.format("用户【{}】新增【{}】单据id为【{}】", commonService.getUserInfo().getUserName(), "渠道黑名单表" , logisticsChannelBlacklistEntity.getId());
-        // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
-        operateLogService.addModuleOperateLog(msg, null, logisticsChannelBlacklistEntity.getId(), "新增操作");
-        // TODO 新增明细（如果有明细的话）
-
-        return new BaseResultDTO.AddDTO(logisticsChannelBlacklistEntity.getId(), logisticsChannelBlacklistEntity.getId());
+        return save;
     }
 
     /**
-    * 修改
-    */
+     * 修改
+     */
     @Transactional(rollbackFor = Exception.class)
     @Override
     public Boolean update(LogisticsChannelBlacklistDTO.UpdateDTO updateDTO) {
         LogisticsChannelBlacklistEntity old = super.getById(updateDTO.getId());
-        Optional.ofNullable(old).orElseThrow(()->new ServiceException(ApiError.NOT_EXIST_BILL, "渠道黑名单表"));
-        LogisticsChannelBlacklistEntity logisticsChannelBlacklistEntity =  BeanMapperUtils.map(LogisticsChannelBlacklistEntity.class, updateDTO);
+        Optional.ofNullable(old).orElseThrow(() -> new ServiceException(ApiError.NOT_EXIST_BILL, "渠道黑名单表"));
+        LogisticsChannelBlacklistEntity logisticsChannelBlacklistEntity = BeanMapperUtils.map(LogisticsChannelBlacklistEntity.class, updateDTO);
 
-        // 数据处理
-        handleData(logisticsChannelBlacklistEntity);
+
         log.info("编辑 开始修改渠道黑名单表数据，id：【{}】", old.getId());
         boolean save = super.updateById(logisticsChannelBlacklistEntity);
-        if(!save) {
+        if (!save) {
             throw new ServiceException("渠道黑名单表保存失败");
         }
         // TODO 修改明细数据（包含增删改）（如果有明细的话）
 
         // 记录主单操作日志
-            log.info("编辑 开始记录渠道黑名单表日志数据，id：【{}】", logisticsChannelBlacklistEntity.getId());
-            String msg = StrUtil.format("用户【{}】编辑id为【{}】的【{}】单据 ", commonService.getUserInfo().getUserName(), logisticsChannelBlacklistEntity.getId(), "渠道黑名单表");
+        log.info("编辑 开始记录渠道黑名单表日志数据，id：【{}】", logisticsChannelBlacklistEntity.getId());
+        String msg = StrUtil.format("用户【{}】编辑id为【{}】的【{}】单据 ", commonService.getUserInfo().getUserName(), logisticsChannelBlacklistEntity.getId(), "渠道黑名单表");
         // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
         operateLogService.addModuleOperateLogByObj(old, logisticsChannelBlacklistEntity, null, logisticsChannelBlacklistEntity.getId(), msg);
         return Boolean.TRUE;
@@ -89,9 +87,12 @@ public class LogisticsChannelBlacklistServiceImpl extends SuperServiceImpl<Logis
 
 
     /**
-    * 新增修改处理数据
-    */
-    private void handleData(LogisticsChannelBlacklistEntity logisticsChannelBlacklistEntity) {
-    // TODO 验证数据 & 数据赋值
+     * 新增修改处理数据
+     */
+    private void handleData(List<LogisticsChannelBlacklistEntity> list) {
+        if (CollectionUtils.isEmpty(list)) {
+            return;
+        }
+        List<String> countryIdList = new ArrayList<>(2);
     }
 }
