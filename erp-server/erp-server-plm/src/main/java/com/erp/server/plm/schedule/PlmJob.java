335be9c6761bfdaddcce5b3ff1fbcf0f57ed06b9
luoweigang <821969511@qq.com>
2023-11-15 09:14:53 +0800

    FBA:bug问题修复迭代上线：产品导入推荐仓位

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/schedule/PlmJob.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/schedule/PlmJob.java
@@ -86,7 +86,7 @@ public class PlmJob {
      */
     @XxlJob("newProductToDmp")
 //    @Scheduled(cron = “0 0 2 * * ?"")
-    @PostConstruct
+//    @PostConstruct
     public void newProductToDmp() {
         syncProductService.syncNewProductToDmp();
     }
