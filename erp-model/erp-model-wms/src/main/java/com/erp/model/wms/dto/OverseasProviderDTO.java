1ca1fc9283d83b9618229186f9826f81628e34ad
luoweigang <821969511@qq.com>
2023-11-16 17:06:02 +0800

    第三方仓：更新第一版代码

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/OverseasProviderDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/OverseasProviderDTO.java
@@ -3,12 +3,14 @@ package com.erp.model.wms.dto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import cn.hutool.json.JSONObject;
 import com.common.business.dto.base.SortDTO;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import java.io.Serializable;
 import java.util.List;
+import javax.json.JsonObject;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -94,6 +96,11 @@ public class OverseasProviderDTO implements Serializable {
         @NotBlank(message = "主键id不能为空")
         private String id;
 
+        /**
+         * 详情
+         */
+        private List<OverseasProviderWarehouseDTO.UpdateDTO> detailList;
+
     }
 
     @Data
@@ -103,15 +110,11 @@ public class OverseasProviderDTO implements Serializable {
         /**
         * 服务商名称
         */
-        @NotBlank(message = "服务商名称不能为空")
-        @Size(max = 255,message = "服务商名称最大长度不能超过255位")
         private String name;
 
         /**
         * 授权状态 already 已授权 not未授权 cancel 取消授权
         */
-        @NotBlank(message = "授权状态 already 已授权 not未授权 cancel 取消授权不能为空")
-        @Size(max = 255,message = "授权状态 already 已授权 not未授权 cancel 取消授权最大长度不能超过255位")
         private String authStatus;
 
         /**
@@ -122,10 +125,7 @@ public class OverseasProviderDTO implements Serializable {
         /**
         * 授权的信息json格式 例如：{'app_key':'test','token':'test'}
         */
-        @NotBlank(message = "授权的信息json格式 例如：{'app_key':'test','token':'test'}不能为空")
         private String authJson;
-
-
     }
 
     /**
@@ -159,6 +159,8 @@ public class OverseasProviderDTO implements Serializable {
     /**
      * 列表查询入参
      */
+    @Data
+    @NoArgsConstructor
     public static class ListDTO {
         /**
          * 主键id
@@ -196,5 +198,24 @@ public class OverseasProviderDTO implements Serializable {
          * 修改人中文名
          */
         private String updateUserName;
+    }
+
+    /**
+     * 授权参数
+     */
+    @Data
+    @NoArgsConstructor
+    public static class AuthorizeParamDTO {
+        /**
+         * id
+         */
+        @NotBlank(message = "id不能为空")
+        private String id;
+
+        /**
+         * 授权的信息json格式 例如：{'app_key':'test','token':'test'}
+         */
+        @NotNull(message = "授权的信息不能为空")
+        private JSONObject authJson;
     }
 }
