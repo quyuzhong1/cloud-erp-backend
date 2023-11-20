9533c089582bca077eb5f072b3dc100cf59be2bd
luoweigang <821969511@qq.com>
2023-11-20 15:37:37 +0800

    第三方仓：FBA货件，FBA发货单接口生成

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
@@ -9,6 +9,7 @@ import com.common.business.dto.base.PermissionsDTO;
 import com.common.business.enums.BusinessNoTypeEnum;
 import com.common.business.vo.PagingVO;
 import com.erp.model.scm.dto.SubcontractOrderDTO;
+import com.erp.model.scm.enums.ModuleTypeEnum;
 import com.erp.model.wms.dto.OverseasDeliveryPlanDTO;
 import com.erp.model.wms.entity.RequisitionApplicationEntity;
 import com.erp.server.wms.mapper.RequisitionApplicationMapper;
@@ -71,8 +72,7 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
 
         // 操作日志
         String msg = StrUtil.format("用户【{}】新增【{}】单据单号为【{}】", commonService.getUserInfo().getUserName(), "要货申请单" , requisitionApplicationEntity.getCode());
-        // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
-        operateLogService.addModuleOperateLog(msg, null, requisitionApplicationEntity.getId(), "新增操作");
+        operateLogService.addModuleOperateLog(msg, ModuleTypeEnum.REQUISITION_APPLICATION.getCode(), requisitionApplicationEntity.getId(), "新增操作");
         // TODO 新增明细（如果有明细的话）
 
         return new BaseResultDTO.AddDTO(requisitionApplicationEntity.getId(), code);
