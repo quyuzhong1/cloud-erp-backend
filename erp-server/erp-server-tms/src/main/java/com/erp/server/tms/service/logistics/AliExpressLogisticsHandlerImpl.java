41acdf9ca783202221f20086d7ba05026e561ed7
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-11-21 18:59:54 +0800

    TMS 速卖通物流接口开发

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/logistics/AliExpressLogisticsHandlerImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/logistics/AliExpressLogisticsHandlerImpl.java
@@ -103,34 +103,17 @@ public class AliExpressLogisticsHandlerImpl extends AbstractLogisticsHandler {
         List<DeclareProduct> declareProducts = LogisticsOrderConverter.INSTANCE.orderRequestProductByAliExpress(logisticsOrderVO.getLogisticsProductVOList());
         //收寄信息
         AddressDTO addressDTO = new AddressDTO();
-        Address refund = new Address();
-//        ContactInfo sender = LogisticsOrderConverter.INSTANCE.orderRequestSendUserByExpress(logisticsOrderVO);
-//        ContactInfo receiver = LogisticsOrderConverter.INSTANCE.orderRequestReceiverUserByExpress(logisticsOrderVO);
-//        contactInfoList.add(sender);
-//        contactInfoList.add(receiver);
-//        //报关信息
-//        CustomsInfo customsInfo = LogisticsOrderConverter.INSTANCE.orderRequestCustomsInfoByExpress(logisticsOrderVO.getParceInfoVO());
-//        //托寄物信息
-//        List<CargoDetail> cargoDetails = LogisticsOrderConverter.INSTANCE.orderRequestCargoDetailByExpress(logisticsOrderVO.getLogisticsProductVOList());
+        addressDTO.setSender(LogisticsOrderConverter.INSTANCE.orderRequestSendUserByAliExpress(logisticsOrderVO));
+        addressDTO.setPickup(addressDTO.getSender());
+        addressDTO.setRefund(addressDTO.getSender());
+        addressDTO.setReceiver(LogisticsOrderConverter.INSTANCE.orderRequestReceiverUserByAliExpress(logisticsOrderVO));
         OrderRequest orderRequest = OrderRequest.builder()
                 .declareProducts(declareProducts)
                 .trade_order_id(logisticsOrderVO.getDeliveryNo())
                 .warehouse_carrier_service(logisticsOrderVO.getLogisticsSaleChannel().getCode())
                 //托寄物信息
-//                .address_d_t_os(cargoDetails)
-//                .cargoDesc(null)
-//                //增值服务
-//                .serviceList(null)
-//                //收寄双方信息
-//                .contactInfoList(contactInfoList)
-//                //顺丰月结卡号 月结支付时传值，现结不需传值；沙箱联调可使用测试月结卡号7551234567（非正式，无须绑定，仅支持联调使用）
-//                .monthlyCard("7551234567")
-//                .payMethod(1)
-//                //快件产品类别
-//                .expressTypeId(1)
-//                .parcelQty(1)
-//                //是否返回路由标签： 默认1， 1：返回路由标签， 0：不返回；除部分特殊用户外，其余用户都默认返回
-//                .isReturnRoutelabel(1)
+                .address_d_t_os(addressDTO)
+                .is_agree_upgrade_reverse_parcel_insure(false)
                 .build();
         return orderRequest;
     }
