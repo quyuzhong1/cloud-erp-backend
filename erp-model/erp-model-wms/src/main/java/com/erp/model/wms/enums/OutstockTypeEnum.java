db625d1b126051fbb578d4eb84d52b3a8dd45ecd
Jim <zhoujingbo@ulanzi.cn>
2023-11-24 08:59:31 +0800

    ADD:映射出库类型

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/OutstockTypeEnum.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/OutstockTypeEnum.java
@@ -16,7 +16,11 @@ public enum OutstockTypeEnum implements EnumMessage {
     KOL_DELIVERY("KOLDelivery", "KOL寄送"),
     AUXILIARY("auxiliary", "辅料包材"),
     ACCESSORY("accessory", "配件"),
-    SCRAP("scrap", "报废");
+    SCRAP("scrap", "报废"),
+    // TODO 更换
+    CLAIM("claim", "索赔")
+
+    ;
 
     /**
      * 类型
