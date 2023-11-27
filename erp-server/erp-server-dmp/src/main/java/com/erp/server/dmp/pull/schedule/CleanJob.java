38e6a652995ebd15421b558c52895f593b92fa35
Will <wangwei@ulanzi.cn>
2023-11-27 14:23:22 +0800

    feat,sku拆分和成本调整

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/schedule/CleanJob.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/pull/schedule/CleanJob.java
@@ -18,6 +18,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
