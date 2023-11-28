83dbe76b9d799f86b444f052b0822efe7835eee3
luoweigang <821969511@qq.com>
2023-11-28 19:56:25 +0800

    修改发货单装箱2

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -844,6 +844,7 @@ public enum ApiError implements Serializable {
     SKU_NOT_MAPPING_PLATFORM_SKU(99126,"sku【{}】无平台sku映射关系，请绑定第三方sku后操作"),
     THIRD_WAREHOUSE_INTERFACE_EXCEPTION(99129,"调用第三方仓接口异常"),
     APPROVE_ING_IS_PACKING(99130,"待审核的数据才可以上传装箱数据"),
+    OVERSEAS_WAREHOUSE_INBOUND_EXIST(99131,"已下推海外仓入库单【{}】，不允许修改装箱数据"),
 
 
     /**
