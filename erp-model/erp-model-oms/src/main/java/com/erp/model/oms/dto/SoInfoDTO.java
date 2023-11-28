69d1e0fb0824b09de66d9ed8a68308555bb0aa68
Will <wangwei@ulanzi.cn>
2023-11-28 11:31:00 +0800

    feat,1129迭代

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoInfoDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoInfoDTO.java
@@ -534,6 +534,10 @@ public class SoInfoDTO implements Serializable {
          */
         private String customerOrderNo;
 
+        /**
+         * 是否报关
+         */
+        private Boolean isDeclare;
 
         /**
          * 运单号
@@ -593,6 +597,10 @@ public class SoInfoDTO implements Serializable {
          */
         private String code;
 
+        /**
+         * 是否报关
+         */
+        private Boolean isDeclare;
 
         /**
          * 类型
@@ -769,6 +777,12 @@ public class SoInfoDTO implements Serializable {
          */
         @PositiveOrZero(message = "运费不能为负数", groups = {AddGroup.class})
         private BigDecimal shippingFee;
+
+        /**
+         * 是否报关
+         */
+        @NotNull(message = "是否报关不能为空")
+        private Boolean isDeclare;
 
         /**
          * 报关费
@@ -1069,6 +1083,11 @@ public class SoInfoDTO implements Serializable {
          * 报关费
          */
         private BigDecimal customsFee;
+
+        /**
+         * 是否报关
+         */
+        private Boolean isDeclare;
 
 
         /**
@@ -1399,6 +1418,11 @@ public class SoInfoDTO implements Serializable {
         @PositiveOrZero(message = "报关费不能为负数", groups = {AddGroup.class})
         private BigDecimal customsFee;
 
+        /**
+         * 是否报关
+         */
+        @NotNull(message = "是否报关不能为空")
+        private Boolean isDeclare;
 
         /**
          * 客户id
--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -251,6 +251,10 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
         if (userInfo != null) {
             addEntity.setSellerName(userInfo.getUserName());
         }
+        //报关费
+        if (!addEntity.getIsDeclare()) {
+            addEntity.setCustomsFee(BigDecimal.ZERO);
+        }
 
         //仓库id
         String warehouseId = dto.getWarehouseId();
@@ -794,6 +798,7 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
                 item.setReceiveAmount(null);
                 item.setRemark("");
                 item.setCustomerOrderNo("");
+                item.setIsDeclare(null);
             }
             flagList.add(item.getId());
         }
@@ -867,6 +872,10 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
         SoInfoEntity draftEntity = new SoInfoEntity();
         BeanMapper.copy(dto, draftEntity);
         draftEntity.setId(id);
+        //报关费
+        if (!draftEntity.getIsDeclare()) {
+            draftEntity.setCustomsFee(BigDecimal.ZERO);
+        }
         //销售组织
         String salesOrgId = dto.getSalesOrgId() == null ? "" : dto.getSalesOrgId();
         //销售员
@@ -967,6 +976,11 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
             if (requireDate.compareTo(billDate) < 0) {
                 throw new ServiceException(ApiError.ERROR_92059);
             }
+        }
+
+        //报关费
+        if (!soInfo.getIsDeclare()) {
+            soInfo.setCustomsFee(BigDecimal.ZERO);
         }
 
         //销售组织
