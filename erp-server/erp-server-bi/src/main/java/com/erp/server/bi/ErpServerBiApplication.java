3d155c0efbfc9e537d4b829c5d87e071c58b5e56
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-11-28 15:38:42 +0800

    OMS 增加cacahe标签使用

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/ErpServerBiApplication.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/ErpServerBiApplication.java
@@ -3,10 +3,12 @@ package com.erp.server.bi;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
+@EnableCaching
 @SpringBootApplication(scanBasePackages = {"com.erp.*","com.common.*"})
 @EnableFeignClients(basePackages = {"com.erp.rpc.*"})
 @EnableDiscoveryClient
