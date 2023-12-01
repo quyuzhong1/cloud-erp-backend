cf9fb2d950fe27ae9a99e3bdcc42132f60bab6d3
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-01 12:36:05 +0800

    OMS BI报表优化 战略目标达成接口优化

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/SalesOrderServiceMapper.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/SalesOrderServiceMapper.java
@@ -156,6 +156,7 @@ public interface SalesOrderServiceMapper  extends BaseMapper<DmpOrderInfoEntity>
 
     List<SalesFlagVO> byTobToc(@Param("params") BiFilterDTO dto, @Param("settleRate") String settleRate);
 
+    List<SalesFlagVO> getSalesByReport(@Param("params") DateSalesTrendDTO.SearchDTO biFilterDTO, @Param("timeFlag") String timeFlag, @Param("settleRate") String settleRate, @Param("searchType") String searchType, @Param("groupName") String groupName);
     List<SalesFlagVO> getByDay(@Param("params") DateSalesTrendDTO.SearchDTO biFilterDTO, @Param("timeFlag") String timeFlag, @Param("settleRate") String settleRate, @Param("searchType") String searchType);
 
     List<SalesFlagVO> getByDayCategory(@Param("params") DateSalesTrendDTO.SearchDTO biFilterDTO, @Param("timeFlag") String timeFlag, @Param("settleRate") String settleRate, @Param("groupName") String groupName, @Param("searchType") String searchType);
