67ca7ec4cdd1bfb87ed06a918f7cddb3b3f1d4bc
Will <wangwei@ulanzi.cn>
2023-12-04 14:12:18 +0800

    feat,客户字典

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/ModuleOperateLogFieldTypeEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/ModuleOperateLogFieldTypeEnum.java
@@ -13,8 +13,8 @@ public enum ModuleOperateLogFieldTypeEnum {
     TYPE_ENUM(2,"枚举"),
     TYPE_DIST(3,"字典"),
     TYPE_USER(4,"人员"),
-    TYPE_DEPT(5,"部门"),
-    TYPE_COUNTRY(6,"国家"),
+    TYPE_COUNTRY(5,"国家"),
+    TYPE_DEPT(6,"部门"),
     TYPE_CITY(7,"城市"),
     TYPE_CUSTOMER(8,"客户"),
     TYPE_CURRENCY(9,"币别")
