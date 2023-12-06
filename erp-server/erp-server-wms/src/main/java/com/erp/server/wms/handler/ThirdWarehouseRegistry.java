ee9f1ff41913dd4505d7cf68a9829e0dd885eefb
liuruipeng <arthur@ulanzi.cn>
2023-12-06 09:17:57 +0800

    update:新增海外仓service获取方法

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/handler/ThirdWarehouseRegistry.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/handler/ThirdWarehouseRegistry.java
@@ -1,5 +1,11 @@
 package com.erp.server.wms.handler;
 
+import com.common.business.threadlocal.ThirdWarehouseContext;
+import com.common.core.enums.ApiError;
+import com.common.core.exception.ServiceException;
+import com.erp.model.oms.enums.AuthStatusEnum;
+import com.erp.model.wms.entity.OverseasProviderEntity;
+import com.erp.server.wms.service.OverseasProviderService;
 import com.erp.server.wms.service.ThirdWarehouseService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
@@ -8,6 +14,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * 海外仓初始化处理
@@ -18,6 +25,9 @@ public class ThirdWarehouseRegistry {
 
     @Resource
     private ApplicationContext context;
+
+    @Resource
+    private OverseasProviderService overseasProviderService;
 
     @PostConstruct
     public void init() {
@@ -30,4 +40,17 @@ public class ThirdWarehouseRegistry {
     public ThirdWarehouseService getHandler(String platform) {
         return handlers.get(platform);
     }
+
+    public ThirdWarehouseService getHandlerByAuthID(String authId) {
+        OverseasProviderEntity authEntity = overseasProviderService.getById(authId);
+        if(Objects.isNull(authEntity)){
+            throw new ServiceException(ApiError.NOT_FOUND_OVERSEAS_PROVIDE);
+        }
+        ThirdWarehouseService warehouseService = getHandler(authEntity.getCode());
+        if(Objects.isNull(warehouseService)){
+            throw new ServiceException(ApiError.OVERSEAS_PROVIDE_NOT_SERVICE);
+        }
+        return warehouseService;
+    }
+
 }
