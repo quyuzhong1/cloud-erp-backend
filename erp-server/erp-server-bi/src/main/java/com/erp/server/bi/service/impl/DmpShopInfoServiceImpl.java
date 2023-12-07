3da44ebf8d08c3d14bcca40ac7ff67730ec3369a
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-07 17:14:03 +0800

    OMS BI报表优化 接口优化

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpShopInfoServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpShopInfoServiceImpl.java
@@ -293,6 +293,7 @@ public class DmpShopInfoServiceImpl extends ServiceImpl<DmpShopInfoMapper, DmpSh
         List<DmpShopInfoEntity> list = this.lambdaQuery()
                 .eq(DmpShopInfoEntity::getIsVijim, Boolean.TRUE)
                 .eq(null != status, DmpShopInfoEntity::getStatus, status)
+                .orderByAsc(DmpShopInfoEntity::getName)
                 .list();
         if (CollectionUtil.isEmpty(list)) {
             return Collections.emptyList();
