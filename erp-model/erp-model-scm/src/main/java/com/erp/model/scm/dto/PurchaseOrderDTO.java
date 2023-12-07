cedf1a3dcde0fbc90539a02c5a6a754e18d628a1
Will <wangwei@ulanzi.cn>
2023-12-07 11:00:57 +0800

    feat,采购订单添加来源单号

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/PurchaseOrderDTO.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/PurchaseOrderDTO.java
@@ -308,6 +308,11 @@ public class PurchaseOrderDTO implements Serializable {
         private String subContractCode;
 
         /**
+         * 来源单号
+         */
+        private String sourceCode;
+
+        /**
          * 产品名称
          */
         private String productName;
