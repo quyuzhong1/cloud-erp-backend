7bc3225454ce18bc5d11c978c6c23c6403381218
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-07 19:06:33 +0800

    OMS BI报表优化 接口优化

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpOrderInfoServiceImpl.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/service/impl/DmpOrderInfoServiceImpl.java
@@ -156,7 +156,11 @@ public class DmpOrderInfoServiceImpl extends ServiceImpl<DmpOrderInfoMapper, Dmp
         //获取到结算汇率
         String settleRate = getSettleRate(dto.getSettleMethod());
         BigDecimal amount = baseMapper.sumSales(dto, settleRate);
-        return new TargetSaleSumVO(amount.setScale(4, RoundingMode.DOWN));
+        if (Objects.nonNull(amount)){
+            return new TargetSaleSumVO(amount);
+        }else {
+            return new TargetSaleSumVO(BigDecimal.ZERO);
+        }
     }
 
     @Override
