fd5735108f30b04f6c2df25c1a02d550dd1dd89e
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-08 14:19:22 +0800

    OMS BI报表优化 增加时间维度记录表

--- erp-common/erp-common-core/src/main/java/com/common/core/utils/date/DateUtil.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/utils/date/DateUtil.java
@@ -493,4 +493,20 @@ public class DateUtil {
         }
         return dates;
     }
+
+    /**
+     * 获取某年的全部日子
+     * @param year
+     * @return
+     */
+    public static List<LocalDateTime> getDatesInYear(int year) {
+        List<LocalDateTime> dates = new ArrayList<>();
+        LocalDateTime startDate = LocalDateTime.of(year, 1, 1, 0, 0);
+        LocalDateTime endDate = LocalDateTime.of(year, 12, 31, 23, 59);
+        while (startDate.isBefore(endDate) || startDate.isEqual(endDate)) {
+            dates.add(startDate);
+            startDate = startDate.plusDays(1);
+        }
+        return dates;
+    }
 }
