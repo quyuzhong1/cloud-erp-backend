9cbc0511fab0fad6b0362976952655a1d994204f
liuruipeng <arthur@ulanzi.cn>
2023-12-08 15:43:00 +0800

    fix:修改海外入库清洗问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasWarehouseInboundServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasWarehouseInboundServiceImpl.java
@@ -659,9 +659,6 @@ public class OverseasWarehouseInboundServiceImpl extends SuperServiceImpl<Overse
         entity.setFinishStatus(OverseasFinishStatusEnum.MANUAL.getCode());
         entity.setInstockStatus(OverseasInstockStatusEnum.FINISH.getCode());
         entity.setFinishReason(dto.getFinishReason());
-        for (OverseasWarehouseInboundDetailEntity detailEntity : updateDetailEntityList) {
-            detailEntity.setDiffQty(0);
-        }
         // 详情更新签收数量
         if (!this.updateById(entity)) {
             throw new ServiceException("海外仓入库单更新失败");
