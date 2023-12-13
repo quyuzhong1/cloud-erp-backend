badbe52feb2b4187baeae1bbd71bc03de9645601
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-13 14:51:37 +0800

    OMS 虾皮店铺授权

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
@@ -901,15 +901,6 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
         //配置是否存在
         if (Objects.isNull(shopAuth)) {
             shopAuth = new ShopAuthEntity();
-            shopAuth.setType(type);
-            shopAuth.setRefreshToken(refreshToken);
-            shopAuth.setAccessToken(accessToken);
-            shopAuth.setShopeeId(shopeeId);
-            if (Objects.nonNull(expireIn)) {
-                shopAuth.setExpiresIn(Math.toIntExact(expireIn));
-            }
-            shopAuth.setAppClientId(cfClientId);
-
         } else {
             //配置存在时
             shopId = shopAuth.getShopId();
