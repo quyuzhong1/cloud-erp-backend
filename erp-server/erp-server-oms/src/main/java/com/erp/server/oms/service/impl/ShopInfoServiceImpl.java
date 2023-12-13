afde7b4cd329c22bc021821f67ecb043203d83f0
yl <ylstrive@163.com>
2023-12-13 10:19:13 +0800

    Lambda 2023-12-13 OMS 修复bug

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
@@ -60,6 +60,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
@@ -179,7 +180,7 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
         boolean result = this.save(shop);
         if (result) {
             //店铺客户信息
-            autoCreateShopCustomer(shop);
+            autoCreateShopCustomer(shop.getId());
         }
         return Collections.singletonList(shop);
 
@@ -188,9 +189,14 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
     /**
      * 店铺保存成功后 自动创建客户
      *
-     * @param shop
+     * @param shopId
      */
-    public void autoCreateShopCustomer(ShopInfoEntity shop) {
+    @Transactional(rollbackFor = Exception.class)
+    public void autoCreateShopCustomer(String shopId) {
+        ShopInfoEntity shop = this.getById(shopId);
+        if (Objects.isNull(shop)) {
+            return;
+        }
         CustomerDTO.AddDTO customer = new CustomerDTO.AddDTO();
         customer.setUseOrgId(shop.getSalesOrgId());
         customer.setInnerOrgId(shop.getSalesOrgId());
@@ -911,7 +917,7 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
         ShopInfoEntity shopInfoEntity = new ShopInfoEntity();
         if (Objects.nonNull(shopId)) {
             shopInfoEntity = this.getById(shopId);
-            if (Objects.isNull(shopInfoEntity)){
+            if (Objects.isNull(shopInfoEntity)) {
                 shopInfoEntity = new ShopInfoEntity();
             }
         }
@@ -926,7 +932,7 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
         if (Objects.nonNull(shopInfo) && StringUtils.isNotEmpty(shopInfo.getCountryName())) {
             shopInfoEntity.setCountryName(shopInfo.getCountryName());
         }
-        if (Objects.nonNull(shopInfo) && StringUtils.isNotEmpty(shopInfo.getAccount())){
+        if (Objects.nonNull(shopInfo) && StringUtils.isNotEmpty(shopInfo.getAccount())) {
             shopInfoEntity.setAccount(shopInfo.getAccount());
         }
         shopInfoEntity.setAuthStatus(AuthStatusEnum.ALREADY.getCode());
