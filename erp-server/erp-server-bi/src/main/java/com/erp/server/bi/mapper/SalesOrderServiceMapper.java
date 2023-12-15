cd9cbdafd9d4e285d0a0a30ab199eb7528e92dec
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-15 15:23:18 +0800

    BI优化 统计销售额表替换

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/SalesOrderServiceMapper.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/mapper/SalesOrderServiceMapper.java
@@ -156,9 +156,7 @@ public interface SalesOrderServiceMapper  extends BaseMapper<DmpOrderInfoEntity>
     List<SalesFlagVO> byTobToc(@Param("params") BiFilterDTO dto, @Param("settleRate") String settleRate);
 
     List<SalesFlagVO> getSalesByReport(@Param("params") DateSalesTrendDTO.SearchDTO biFilterDTO, @Param("timeFlag") String timeFlag, @Param("settleRate") String settleRate, @Param("searchType") String searchType, @Param("groupName") String groupName);
-    List<SalesFlagVO> deptNewAndOldSalesAmount(@Param("params") NewAndOldSalesSearchDTO.SearchDTO dto, @Param("settleRate") String settleRate);
 
-    List<SalesFlagVO> userNewAndOldSalesAmount(@Param("params") NewAndOldSalesSearchDTO.SearchDTO dto, @Param("settleRate") String settleRate);
     List<SalesFlagVO> newAndOldSalesAmount(@Param("params") NewAndOldSalesSearchDTO.SearchDTO dto, @Param("settleRate") String settleRate,@Param("dataType") String dataType);
 
 
