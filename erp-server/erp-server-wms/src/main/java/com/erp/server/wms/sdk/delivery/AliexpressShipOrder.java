b925d1b8b165799d3cf8d7e9e186671095c4dfd9
luoweigang <821969511@qq.com>
2023-12-19 16:06:38 +0800

    新增平台sdk发货

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/delivery/AliexpressShipOrder.java
@@ -0,0 +1,25 @@
+package com.erp.server.wms.sdk.delivery;
+
+import com.common.business.annotation.PlatformShipOrderAnno;
+import com.common.business.dto.PlatformShipOrderDTO;
+import com.common.business.enums.PlatformDictEnum;
+import com.common.business.service.IPlatformService;
+import com.erp.rpc.oms.feign.SoB2cFeign;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@Component
+@PlatformShipOrderAnno(method = PlatformDictEnum.WALMART)
+public class AliexpressShipOrder implements IPlatformService {
+
+    @Resource
+    private SoB2cFeign soB2cFeign;
+
+    @Override
+    public void shipOrder(PlatformShipOrderDTO dto) {
+
+    }
+}
