82b98421e7efb8fc9596daaaefd86abc67e3bd5a
Jim <zhoujingbo@ulanzi.cn>
2023-12-20 18:57:22 +0800

    BUG:签收时间fix

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
@@ -894,6 +894,10 @@ public class FbaShipmentServiceImpl extends SuperServiceImpl<FbaShipmentMapper, 
                 // 修改
                 e.setId(detailEntity.getId());
                 e.setDeliveryQty(detailEntity.getDeliveryQty());
+                e.setReceiveDate(detailEntity.getReceiveDate());
+                if (!Objects.equals(e.getReceiveQty(), detailEntity.getReceiveQty()) && e.getReceiveQty() > 0){
+                    e.setReceiveDate(LocalDateTime.now(ZoneId.systemDefault()));
+                }
                 if (!oldEntity.getDeliveryStatus().equalsIgnoreCase(DeliveryStatusEnum.UN_SHIPPED.getCode())){
                     e.setDiffQty(e.getReceiveQty() - detailEntity.getDeliveryQty());
                 }
