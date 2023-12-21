ea4c5a31ca019381889fa71035dd4c4b0d9e1f4b
luoweigang <821969511@qq.com>
2023-12-21 13:20:09 +0800

    B2C发货单修改加工单下推校验（临时上线需求）1

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -875,7 +875,7 @@ public enum ApiError implements Serializable {
     IS_NOT_MANUAL_DELIVERY(99152,"已发货、取消发货的数据不允许手动发货"),
     PLATFORM_SHIP_ORDER_ERROR(99152,"平台发货失败，错误信息【{}】"),
     ERROR_PDF_MERGE(92115,"打印面单/配货单失败，合并PDF时出错"),
-    DELIVERY_NOT_COMBINATION_NOT_MACHINE(92116,"发货单【{}】的组合SKU不包含销售套装BOM，无需下推加工单"),
+    DELIVERY_NOT_COMBINATION_NOT_MACHINE(92116,"组合SKU不包含销售套装BOM，无需下推加工单"),
 
 
 
