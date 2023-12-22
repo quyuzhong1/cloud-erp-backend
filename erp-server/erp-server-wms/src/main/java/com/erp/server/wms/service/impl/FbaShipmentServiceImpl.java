33a7395e5c4b2254982013365d00941f2279d892
yl <ylstrive@163.com>
2023-12-22 18:47:19 +0800

    Lambda 2023-12-22 WMS fba 添加事物

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
@@ -1050,6 +1050,8 @@ public class FbaShipmentServiceImpl extends SuperServiceImpl<FbaShipmentMapper, 
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
+    @GlobalTransactional(rollbackFor = Exception.class)
     public void handlerWarehouse(FbaShipmentEntity entity, List<FbaShipmentReceiveEntity> saveReceiveList) {
         // 查询是否有发货单号
         FirstMileDeliveryEntity deliveryEntity = firstMileDeliveryService.findBySourceId(entity.getId());
