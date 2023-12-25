949e3ec25276c7c14f18d20af1c611062a3b0df6
Will <wangwei@ulanzi.cn>
2023-12-25 16:38:47 +0800

    feat,平台枚举添加视频号

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
@@ -33,6 +33,7 @@ public enum PlatformDictEnum implements EnumMessage {
     PDD("PDD", "拼多多", "Temu", "009"),
     TMALL("Tmall", "天猫", "Tmall", "006"),
     SOP("JDZY", "京东自营厂送", "京东自营", "007"),
+    WE_CHAT_VEDIO("WeChatVedio", "微信视频号", "微信视频号", "027"),
     //物流平台
     DSF("DSF", "递四方", "递四方(新)", "41"),
     SF_EXPRESS("EXPRESS", "顺丰-丰桥", "顺丰国内物流", "42"),
