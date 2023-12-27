7ba6f6d0cb6dd91509056c370a2b3843953290c1
liuruipeng <arthur@ulanzi.cn>
2023-12-27 19:50:50 +0800

    fix:修复包装验货多个相同SKU问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackingInspectionServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackingInspectionServiceImpl.java
@@ -162,11 +162,12 @@ public class PackingInspectionServiceImpl implements PackingInspectionService {
                     }
                     scanSkuInfo.setWaitScanQty(scanSkuInfo.getWaitScanQty()-1);
                     scanSkuInfo.setScannedQty(scanSkuInfo.getSaleQty() - scanSkuInfo.getWaitScanQty());
-                }
-                //如果已扫描数等于销售数，放到已扫描队列
-                if(scanSkuInfo.getScannedQty().equals(scanSkuInfo.getSaleQty())){
-                    viewDTO.getScannedSkuList().add(scanSkuInfo);
-                    it.remove();
+                    //如果已扫描数等于销售数，放到已扫描队列
+                    if(scanSkuInfo.getScannedQty().equals(scanSkuInfo.getSaleQty())){
+                        viewDTO.getScannedSkuList().add(scanSkuInfo);
+                        it.remove();
+                    }
+                    break;
                 }
             }
             if(!matchFlag){
