78d22dc395a50e21e0bba0ca588a55e4ff3b3a2c
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-27 16:39:40 +0800

    TMS 燕文数据面单打印修改

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/logistics/YanWenLogisticsHandlerImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/logistics/YanWenLogisticsHandlerImpl.java
@@ -127,7 +127,6 @@ public class YanWenLogisticsHandlerImpl extends AbstractLogisticsHandler {
                     response.setBase64(labelResponse.getData().getBase64String());
                     response.setTransportNoList(Collections.singletonList(labelResponse.getData().getWaybillNumber()));
                     response.setDeliveryNoList(Collections.singletonList(logisticsGetLabelVO.getDeliveryNo()));
-                    result.add(response);
                     logisticsOperateService.pullOperateLog(logisticsGetLabelVO.getAuthMap().get("id"),
                             logisticsGetLabelVO.getDeliveryNo(), BusinessTypeEnum.GET_LABEL.getCode(), LogisticsPlatformEnum.YAN_WEN.getCode(),
                             RequestStatusEnums.SUCCESS.getCode(), JSONUtil.toJsonStr(logisticsGetLabelVO), JSONUtil.toJsonStr(labelResponse));
