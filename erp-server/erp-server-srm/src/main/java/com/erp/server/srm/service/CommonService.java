7dff36e50dabe065584210bad99201e7fd2d82df
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2023-12-27 17:01:39 +0800

    SRM 新增项目

---
+++ erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/CommonService.java
@@ -0,0 +1,24 @@
+package com.erp.server.srm.service;
+
+import com.common.business.vo.LoginUser;
+
+/**
+ * @author Lambda
+ * @Classname CommonService
+
+ * @Date 2023-05-11 19:21
+ * @Created by yl
+ */
+public interface CommonService {
+
+    /**
+     * 获取用户信息
+     * @author yl
+     * @date 2023-03-15 11:58
+     * @param
+     * @return com.common.business.vo.LoginUser
+     */
+    public LoginUser getUserInfo();
+
+
+}
