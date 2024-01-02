c26a1118b5a0cac2c26b08db13bae652ba74f192
Will <wangwei@ulanzi.cn>
2024-01-02 09:12:01 +0800

    feat,b2c客户启用查询调整

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/CustomerB2cController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/CustomerB2cController.java
@@ -277,7 +277,7 @@ public class CustomerB2cController extends BaseController {
     @DataPermission(operationType = DataAttributeEnum.LIST,
             tableField = "create_user_id,seller_id",
             menuCode = "oms:customerB2c:paging",
-            tableAlias = "customerB2c_info"
+            tableAlias = "customer_b2c"
     )
     public ApiResult<List<CustomerDTO.InfoDTO>> listEnable(PermissionsDTO dto) {
         List<CustomerDTO.InfoDTO> list = customerB2cService.listEnable(dto.getPermissionSql());
