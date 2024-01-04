b28ffc4dcca86ee72c902e1e0e5ca6ecad55dfb6
Jim <zhoujingbo@ulanzi.cn>
2024-01-04 16:03:07 +0800

    ADD:销售出库单判断

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/feign/DmpMongoDbFeignController.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/feign/DmpMongoDbFeignController.java
@@ -1,24 +1,31 @@
 package com.erp.server.dmp.controller.feign;
 
 import com.alibaba.fastjson.JSONObject;
+import com.common.business.constant.MongoTableNameContant;
+import com.common.business.enums.PlatformDictEnum;
+import com.common.core.exception.ServiceException;
 import com.common.core.utils.MapUtil;
 import com.erp.model.dmp.dto.AmazonShopInfoDTO;
 import com.erp.model.dmp.dto.DmpPullShipmentDTO;
 import com.erp.model.dmp.dto.MongoDBUpdateDTO;
+import com.erp.model.dmp.kingdee.KingdeeDeliveryDetailEntity;
 import com.erp.model.oms.dto.OmsMongoDTO;
+import com.erp.sdk.oms.amz.spapi.dto.PlatformAmazonOrderDTO;
 import com.erp.server.dmp.enums.CleanDataTableEnum;
 import com.erp.server.dmp.pull.mongo.MongoService;
 import com.erp.server.dmp.service.CfgAppClientService;
 import com.erp.server.dmp.service.ReportHandleService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -33,7 +40,10 @@ import java.util.Objects;
 public class DmpMongoDbFeignController {
 
     @Resource
-    MongoService mongoService;
+    private MongoService mongoService;
+    @Resource
+    private MongoTemplate mongoTemplate;
+
     /**
      * 拉取货件
      *
@@ -55,6 +65,27 @@ public class DmpMongoDbFeignController {
         MapUtil mapUtil = new MapUtil();
         mapUtil.put("isClean", dto.getIsClean());
         mongoService.updateMongoData(updateDto, mapUtil, dto.getTableName(), tClass);
+    }
+
+    /**
+     * 查询mongodb是否有销售出库单
+     *
+     * @Author Jim
+     * @since 2024-01-04
+     **/
+    @GetMapping("/checkHasDeliveryDetail")
+    public Boolean updateMongoDbData(@RequestParam(value = "platformCode") String platformCode, @RequestParam(value = "platform") String platform){
+        if (StringUtils.isBlank(platformCode)){
+            throw new ServiceException("订单id为空");
+        }
+        if (PlatformDictEnum.AMAZON.getCode().equalsIgnoreCase(platform)){
+            platformCode = platformCode.replaceAll("-","");
+        }
+        Query query = new Query();
+        query.addCriteria(Criteria.where("kingdeeOutStockItemEntityList.fSrcBillNo").is(platformCode)).limit(1);
+        List<KingdeeDeliveryDetailEntity> list = mongoTemplate.find(query, KingdeeDeliveryDetailEntity.class, MongoTableNameContant.ORIGINAL_KINGDEE_DELIVERY_DETAIL);
+        return CollectionUtils.isNotEmpty(list);
+
     }
 
 }
