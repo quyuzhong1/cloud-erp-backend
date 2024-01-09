c45411188573cf9298615c1225a577960ab3128d
Jim <zhoujingbo@ulanzi.cn>
2024-01-09 12:29:12 +0800

    UPDATE:优化创建任务逻辑

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/TbTaskTypeService.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/TbTaskTypeService.java
@@ -62,6 +62,8 @@ public class TbTaskTypeService {
         LocalDateTime localTime = LocalDateTime.now();
         // 查询任务列表
         List<JobTaskDTO> jobTaskDTOList = platformApiTaskService.listApiTask(localTime, "pull");
+        //
+        jobTaskDTOList = jobTaskDTOList.stream().filter(e-> e.getDictPlatform().equalsIgnoreCase(PlatformDictEnum.ALI_EXPRESS.getCode())).collect(Collectors.toList());
         // 任务量等于0，任务重新开始,分页设置成0
         if (CollectionUtil.isEmpty(jobTaskDTOList)) {
             return jobTaskDTOList;
