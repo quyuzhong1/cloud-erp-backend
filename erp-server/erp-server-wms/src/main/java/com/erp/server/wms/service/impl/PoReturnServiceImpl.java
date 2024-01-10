e546c233e0f0354c3b1b0d00bc9602b3be2e2c8b
luoweigang <821969511@qq.com>
2024-01-10 16:08:39 +0800

    修改采购退货单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoReturnServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoReturnServiceImpl.java
@@ -73,7 +73,7 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 @RefreshScope
-public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PurchaseReturnOrderMapper, PurchaseReturnOrderEntity> implements PurchaseReturnOrderService {
+public class PurchaseReturnOrderServiceImpl extends SuperServiceImpl<PoReturnMapper, PoReturnEntity> implements PoReturnService {
 
     @Resource
     private ScmTaskFeign scmTaskFeign;
