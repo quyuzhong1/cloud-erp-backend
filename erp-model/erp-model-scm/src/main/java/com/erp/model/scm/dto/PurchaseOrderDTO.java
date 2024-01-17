0b21cf1e3551cad906178164fbb42e0d093e7f80
liuruipeng <arthur@ulanzi.cn>
2024-01-17 12:05:35 +0800

    fix:调整字段名

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/PurchaseOrderDTO.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/PurchaseOrderDTO.java
@@ -277,7 +277,7 @@ public class PurchaseOrderDTO implements Serializable {
         /**
          * 页面高级查询
          */
-        private List<AdvanceQueryDTO> advanceQueryList;
+        private List<AdvanceQueryDTO> advanceQueryDTOList;
 
         /**
          * sqlMap 默认key default
