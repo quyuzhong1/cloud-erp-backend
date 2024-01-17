f506636cd4e4a3913229f132b92481928f81fc03
luoweigang <821969511@qq.com>
2024-01-17 18:32:22 +0800

    修改SCM价目表

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchasePriceDetailServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchasePriceDetailServiceImpl.java
@@ -353,7 +353,7 @@ public class PurchasePriceDetailServiceImpl extends SuperServiceImpl<PurchasePri
 
     @Override
     public List<PurchasePriceDetailDTO.ViewDTO> listByPurchasePriceDetailIds(List<String> purchasePriceDetailIds) {
-        List<PurchasePriceDetailEntity> list = baseMapper.listByIds(purchasePriceDetailIds);
+        List<PurchasePriceDetailEntity> list = baseMapper.listDetailByIds(purchasePriceDetailIds);
         List<PurchasePriceDetailDTO.ViewDTO> viewList = BeanMapper.copyList(list, PurchasePriceDetailDTO.ViewDTO.class);
         //处理采购价目明细信息
         handlePurchasePriceDetail(viewList);
