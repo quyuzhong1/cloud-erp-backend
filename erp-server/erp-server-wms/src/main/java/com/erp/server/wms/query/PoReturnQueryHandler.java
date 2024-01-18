08aeeeea35610d8a76fb0801aaa6c2abea63276e
luoweigang <821969511@qq.com>
2024-01-18 17:30:54 +0800

    修改退货单高级查询

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/PoReturnQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/PoReturnQueryHandler.java
@@ -8,8 +8,11 @@ public class PoReturnQueryHandler extends AbstractQueryHandler {
 
     @Override
     protected String handleSqlLogic(String field, Object value, String compareCodeSplicingValueSql) {
-        if("so.code".equals(field)){
-
+        if("returnOrderSource".equals(field)){
+            value = "'"+value+"'";
+            return "case when  subString ( " + value + ",1,2) != 'PL' then po.source_code "+ compareCodeSplicingValueSql +
+                    " else EXISTS ( select pa.id from purchase_application pa left join purchase_application_ref_po parp on parp.is_deleted = false and pa.id = parp.purchase_application_id " +
+                    " where parp.purchase_order_id = po.id and pa.code "+ compareCodeSplicingValueSql +" ) end";
         }
         return null;
     }
