6b79ab13c8ab0f7008e6d5d0269d84e3b33cdb47
liuruipeng <arthur@ulanzi.cn>
2024-01-20 17:51:45 +0800

    pda提交采购收货单校验收货数量不大于送货数量

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -927,6 +927,7 @@ public enum ApiError implements Serializable {
     PO_RETURN_NOT_EXISTS(92129,"未找到采购退货单"),
     CFG_SETTING_NOT_EXISTS(92129,"退货配置不存在，请先配置异常处理人"),
     RECEIVE_SHOULD_GENERATE_BY_DELIVERY(92130,"【{}】已开启系统收货协同，请从送货单下推收货单"),
+    RECEIVE_QTY_ERROR(92131,"收货数量不能大于送货数量"),
 
 
 
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseReceiveServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseReceiveServiceImpl.java
@@ -1343,14 +1343,22 @@ public class WarehouseReceiveServiceImpl extends SuperServiceImpl<WarehouseRecei
         List<String> poReceiveDetailIds = warehouseReceiveDetailList.stream().map(WarehouseReceiveDetailDTO.AddDTO::getPurchaseOrderDetailId).distinct().collect(Collectors.toList());
         List<PurchaseOrderDetailEntity> purchaseOrderDetailEntityList = scmTaskFeign.listPurchaseOrderDetailById(poReceiveDetailIds);
         List<PurchaseOrderDetailEntity> detailEntityListByPoId = scmTaskFeign.listByPurchaseOrderIds(Arrays.asList(dto.getPurchaseOrderId()));
+        List<DeliveryOrderDetailEntity> allDeliveryOrderDetailList = srmDeliveryOrderFeign.listDetailByDetailSourceIds(poReceiveDetailIds);
 
         for (WarehouseReceiveDetailDTO.AddDTO addDTO : warehouseReceiveDetailList) {
             PurchaseOrderDetailEntity detailEntity = purchaseOrderDetailEntityList.stream().filter(req -> req.getId().equals(addDTO.getPurchaseOrderDetailId())).findFirst().orElse(null);
             if (ObjectUtils.isEmpty(detailEntity)) {
                 throw new ServiceException(ApiError.ERROR_PURCHASE_DETAIL_SKU_NOT_EXIST, addDTO.getSkuNo());
             }
-
             List<PurchaseOrderDetailEntity> detailEntityList = detailEntityListByPoId.stream().filter(req -> req.getSkuId().equals(detailEntity.getSkuId())).collect(Collectors.toList());
+            List<DeliveryOrderDetailEntity> deliveryOrderDetailist = allDeliveryOrderDetailList.stream().filter(req -> req.getSourceDetailId().equals(detailEntity.getId())).collect(Collectors.toList());
+            if(CollectionUtils.isNotEmpty(deliveryOrderDetailist)){
+                int deliveryQty = deliveryOrderDetailist.stream().mapToInt(DeliveryOrderDetailEntity::getDeliveryQty).sum();
+                if(addDTO.getReceiveQty() > deliveryQty){
+                    throw new ServiceException(ApiError.RECEIVE_QTY_ERROR);
+                }
+            }
+
             //校验sku是否有重复，重复需要拆单
             if (detailEntityList.size() > MathUtil.ONE) {
                 List<String> podIds = detailEntityList.stream().map(req -> req.getId()).collect(Collectors.toList());
