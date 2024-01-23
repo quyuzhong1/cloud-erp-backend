6ac21f51037b344550ea1c4c37fa2ca55ac6ae49
luoweigang <821969511@qq.com>
2024-01-23 17:02:50 +0800

    修改中转报关单

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TransferDeclareServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TransferDeclareServiceImpl.java
@@ -208,13 +208,12 @@ public class TransferDeclareServiceImpl extends SuperServiceImpl<TransferDeclare
         if(!save) {
             throw new ServiceException("中转报关单保存失败");
         }
-        // 修改明细数据（包含增删改）（如果有明细的话）
-
+        // 修改明细数据（包含增删改）
+        transferDeclareDetailService.update(updateDTO, transferDeclareEntity.getId());
         // 记录主单操作日志
-            log.info("编辑 开始记录中转报关单日志数据，单号：【{}】", transferDeclareEntity.getCode());
-            String msg = StrUtil.format("用户【{}】编辑单号为【{}】的【{}】单据 ", commonService.getUserInfo().getUserName(), transferDeclareEntity.getCode(), "中转报关单");
-        // TODO 此处的null需修改为日志模块类型，moduleType查看ModuleTypeEnum枚举类
-        operateLogService.addModuleOperateLogByObj(old, transferDeclareEntity, null, transferDeclareEntity.getId(), msg);
+        log.info("编辑 开始记录中转报关单日志数据，单号：【{}】", transferDeclareEntity.getCode());
+        String msg = StrUtil.format("用户【{}】编辑单号为【{}】的【{}】单据 ", commonService.getUserInfo().getUserName(), transferDeclareEntity.getCode(), "中转报关单");
+        operateLogService.addModuleOperateLogByObj(old, transferDeclareEntity, ModuleTypeEnum.TRANSFER_DECLARE.getCode(), transferDeclareEntity.getId(), msg);
         return Boolean.TRUE;
     }
 
