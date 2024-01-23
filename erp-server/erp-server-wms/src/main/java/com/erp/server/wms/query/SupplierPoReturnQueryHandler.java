008e24347b386148a65b01001a0b1e8880baf3be
luoweigang <821969511@qq.com>
2024-01-23 16:29:57 +0800

    修改退货单高级查询

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SupplierPoReturnQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SupplierPoReturnQueryHandler.java
@@ -2,12 +2,7 @@ package com.erp.server.wms.query;
 
 import com.common.business.query.AbstractQueryHandler;
 import com.erp.model.wms.enums.PoReturnConfirmStatusEnum;
-import com.erp.model.wms.enums.PoReturnStatusEnum;
-import com.erp.rpc.sys.feign.SysPostFeign;
-import com.erp.server.wms.service.CommonService;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
 
 @Component
 public class SupplierPoReturnQueryHandler extends AbstractQueryHandler {
