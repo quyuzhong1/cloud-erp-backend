78ee0a4c58b7bcc445988bb7463e4594ac9f0328
luoweigang <821969511@qq.com>
2024-01-26 10:33:04 +0800

    修改报关单

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TransferLogisticsAuthServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TransferLogisticsAuthServiceImpl.java
@@ -296,7 +296,7 @@ public class TransferLogisticsAuthServiceImpl extends SuperServiceImpl<TransferL
     @Override
     public void syncUpdateSaleChannel(String logisticsPlatform, Map<String, String> authConfig) {
         // TODO 待开发同步渠道
-        /*authConfig.put("logisticsPlatform", logisticsPlatform);
+/*        authConfig.put("logisticsPlatform", logisticsPlatform);
         asyncService.asyncUpdateSaleChannel(authConfig);*/
     }
 
