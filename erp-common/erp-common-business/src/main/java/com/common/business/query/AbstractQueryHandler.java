8a7cad8f8d58a8a57fe0b9ffdfbcac5e56969225
Will <wangwei@ulanzi.cn>
2024-01-26 17:09:26 +0800

    feat,高级查询

--- erp-common/erp-common-business/src/main/java/com/common/business/query/AbstractQueryHandler.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/query/AbstractQueryHandler.java
@@ -57,7 +57,11 @@ public abstract class AbstractQueryHandler implements IQueryHandler{
     }
 
     protected String getQueryEmptySql(){
-        return " 1 = 2 ";
+        try {
+            return " 1 = 2 ";
+        }   finally {
+            AdvanceQueryContext.remove();
+        }
     }
 
     protected String getQueryAllSql(){
