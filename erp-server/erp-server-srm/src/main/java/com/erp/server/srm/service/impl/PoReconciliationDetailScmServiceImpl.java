9e15df785ecca3dbef3f06d1b70de7d080500a38
Will <wangwei@ulanzi.cn>
2024-01-26 12:08:58 +0800

    feat,对账单代码

--- erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PoReconciliationDetailScmServiceImpl.java
+++ erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PoReconciliationDetailScmServiceImpl.java
@@ -36,6 +36,7 @@ import com.erp.model.srm.enums.PoReconciliationDetailEnum;
 import com.erp.model.srm.enums.PoReconciliationEnum;
 import com.erp.model.sys.dto.CurrencyDTO;
 import com.erp.model.sys.enums.SysDictBasicEnum;
+import com.erp.model.wms.entity.SubcontractIssueDetailEntity;
 import com.erp.model.wms.enums.ReturnOrderSourceEnum;
 import com.erp.rpc.plm.feign.PlmTaskFeign;
 import com.erp.rpc.sys.feign.SysDictFeign;
@@ -49,6 +50,7 @@ import com.google.common.collect.Lists;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.math3.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -127,12 +129,33 @@ public class PoReconciliationDetailScmServiceImpl extends SuperServiceImpl<PoRec
 
         handleUpdateData (list,mainId);
 
+        //原明细数据被删除的需要清除mainId
+        List<PoReconciliationDetailEntity> oldList = this.listMainIdList(Arrays.asList(mainId));
+        List<String> deleteIds = getDeleteIds(list, oldList);
+        if (CollectionUtils.isNotEmpty(deleteIds)) {
+            List<PoReconciliationDetailEntity> deleteList = oldList.stream().filter(obj -> deleteIds.contains(deleteIds)).collect(Collectors.toList());
+            if (CollectionUtils.isNotEmpty(deleteList)) {
+                deleteList.stream().forEach(obj -> obj.setMainId(""));
+                list.addAll(deleteList);
+            }
+        }
+
         log.info("编辑 开始修改采购对账单数据，id：【{}】", mainId);
         boolean save = super.saveOrUpdateBatch(list);
         if(!save) {
             throw new ServiceException("采购对账单明细保存失败");
         }
         return Boolean.TRUE;
+    }
+
+    /**
+     * 查询需要删除的数据
+     */
+    private List<String> getDeleteIds(List<PoReconciliationDetailEntity> newList, List<PoReconciliationDetailEntity> oldList) {
+        List<String> newIds = newList.stream().filter(g -> StringUtils.isNotBlank(g.getId())).
+                map(PoReconciliationDetailEntity::getId).collect(Collectors.toList());
+        List<String> oldIds = oldList.stream().map(PoReconciliationDetailEntity::getId).collect(Collectors.toList());
+        return oldIds.stream().filter(s -> !newIds.contains(s)).collect(Collectors.toList());
     }
 
     @Override
