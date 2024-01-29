d93fe0011b92a396a4b46048f3247b0e56094397
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-01-29 09:39:14 +0800

    SRM 采购订单数据同步描述信息修改

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/schedule/ScmJob.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/schedule/ScmJob.java
@@ -56,15 +56,15 @@ public class ScmJob {
     }
 
     /**
-     * 同步采购信息已审核待确认订单到srm
+     * 同步采购信息已审核已确认订单到srm
      */
     @XxlJob("syncPurchaseToSrm")
     public void syncPurchaseToSrm() {
-        XxlJobHelper.log("=====采购订单自动确认 开始任务=====");
+        XxlJobHelper.log("=====同步采购信息已审核已确认订单 开始任务=====");
         long start = System.currentTimeMillis();
         purchaseOrderService.syncConfirmOrder();
         long end = System.currentTimeMillis();
         XxlJobHelper.log("主线程花费时间：{}", (end - start));
-        XxlJobHelper.log("=====采购订单自动确认 结束任务=====");
+        XxlJobHelper.log("=====同步采购信息已审核已确认订单 结束任务=====");
     }
 }
