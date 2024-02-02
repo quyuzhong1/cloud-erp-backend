82e5fa2c02ab7472b47152de0979fffd04caeaf8
yl <ylstrive@163.com>
2024-02-02 18:43:34 +0800

    Lambda 2024-02-02   oms  保宏组包预报代码提交

--- erp-rpc/erp-rpc-tms/src/main/java/com/erp/rpc/tms/feign/ForecastFeign.java
+++ erp-rpc/erp-rpc-tms/src/main/java/com/erp/rpc/tms/feign/ForecastFeign.java
@@ -2,11 +2,9 @@ package com.erp.rpc.tms.feign;
 
 import com.erp.model.tms.dto.SettingForecastDTO;
 import com.erp.model.tms.entity.ProductRegistrationEntity;
+import com.erp.model.tms.entity.SettingForecastEntity;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -45,4 +43,8 @@ public interface ForecastFeign {
      */
     @PostMapping("/feign/settingForecast/getByLogisticsSupplier")
     SettingForecastDTO.ForecastStatusDTO getByLogisticsSupplier(@RequestBody SettingForecastDTO.FindByLogisticsSupplierDTO dto);
+
+
+    @GetMapping("/feign/settingForecast/getSettingForecastByLogisticsSupplierId")
+    SettingForecastEntity getSettingForecastByLogisticsSupplierId(@RequestParam("logisticsSupplierId") String logisticsSupplierId);
 }
