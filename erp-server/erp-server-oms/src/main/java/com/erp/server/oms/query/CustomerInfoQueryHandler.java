a84cd3dc78aaac1d19e627f036785b0544ce612a
liuruipeng <arthur@ulanzi.cn>
2024-02-19 10:12:08 +0800

    fix:修复启动报错

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/CustomerInfoQueryHandler.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/CustomerInfoQueryHandler.java
@@ -2,18 +2,8 @@ package com.erp.server.oms.query;
 
 import com.common.business.constant.SearchType;
 import com.common.business.enums.ApproveStatusEnum;
-import com.common.business.enums.QueryConditionEnum;
-import com.common.business.enums.QueryDataTypeEnum;
-import com.common.business.enums.SourceTypeEnum;
 import com.common.business.query.AbstractQueryHandler;
-import com.erp.model.scm.enums.ArrivalStatusEnum;
-import com.erp.model.scm.enums.PurchaseListTypeEnum;
-import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * @author liuruipeng
