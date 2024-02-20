40f33ffb2e072df157a713edb0a6c13fddcb8e18
luoweigang <821969511@qq.com>
2024-02-20 16:18:45 +0800

    修改shopify授权1

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/authorize/ShopfiyAuthorize.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/authorize/ShopfiyAuthorize.java
@@ -138,8 +138,7 @@ public class ShopfiyAuthorize implements IShopAuthorizeService<T> {
         findShopAuthorize.setTimestamp(dto.getTimestamp());
         try {
             bodyStr = shopSdkServer.getShopAuthorizeInfo(findShopAuthorize);
-        } catch (ServiceException e) {
-            throw e;
+
         } catch (Exception e) {
             throw new ServiceException(e.getMessage());
         }
