88473894c816031d35f0b6320e9b59ceba77a66a
luoweigang <821969511@qq.com>
2024-02-22 14:02:07 +0800

    修改shopify授权1

--- erp-sdk/erp-sdk-oms-shopify/src/main/java/com/sdk/oms/shopify/service/ShopSdkServer.java
+++ erp-sdk/erp-sdk-oms-shopify/src/main/java/com/sdk/oms/shopify/service/ShopSdkServer.java
@@ -17,7 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
