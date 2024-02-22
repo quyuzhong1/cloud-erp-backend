a94d44b2592618cd2f6136cfc60c07234b9beef6
Will <wangwei@ulanzi.cn>
2024-02-22 12:15:38 +0800

    feat，产品认证调整

--- erp-common/erp-common-core/src/main/java/com/common/core/file/FileUpload.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/file/FileUpload.java
@@ -4,14 +4,10 @@ import com.common.core.security.SBase64;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.FileItemFactory;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
@@ -256,38 +252,5 @@ public class FileUpload {
 			throw new RuntimeException(e);
 		}
 	}
-
-	/**
-	 * 获取MultipartFile
-	 */
-	private MultipartFile getMultipartFile (String pathUrl) {
-		File tempFile = new File(pathUrl);
-		FileItem fileItem = createFileItem(tempFile, tempFile.getName());
-		MultipartFile multipartFile = null;
-		return multipartFile;
-	}
-
-	/**
-	 * 创建FileItem
-	 */
-	private FileItem createFileItem(File file, String fieldName) {
-		FileItemFactory factory = new DiskFileItemFactory(16, null);
-		FileItem item = factory.createItem(fieldName, "text/plain", true, file.getName());
-		int bytesRead = 0;
-		byte[] buffer = new byte[8192];
-		try {
-			FileInputStream fis = new FileInputStream(file);
-			OutputStream os = item.getOutputStream();
-			while ((bytesRead = fis.read(buffer, 0, 8192)) != -1) {
-				os.write(buffer, 0, bytesRead);
-			}
-			os.close();
-			fis.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return item;
-	}
-
 
 }
--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/excel/ProductCertificateExcelDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/excel/ProductCertificateExcelDTO.java
@@ -40,6 +40,7 @@ public class ProductCertificateExcelDTO implements Serializable {
      * 文件名称
      */
     @ExcelProperty(value = "*文件名称", index = 3)
+    @FieldValid(fieldName = "文件名称",isNotBlank = true,maxLength = 255)
     private String pathUrl;
 
     /**
