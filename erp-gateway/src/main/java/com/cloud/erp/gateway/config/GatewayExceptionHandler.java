eda887e5a927844d96e8f9fa98532ff8eb1495b3
luoweigang <821969511@qq.com>
2024-02-22 11:12:43 +0800

    修改shopify授权

--- erp-gateway/src/main/java/com/cloud/erp/gateway/config/GatewayExceptionHandler.java
+++ erp-gateway/src/main/java/com/cloud/erp/gateway/config/GatewayExceptionHandler.java
@@ -78,6 +78,9 @@ public class GatewayExceptionHandler extends DefaultErrorWebExceptionHandler {
 	 */
 	@Override
 	protected int getHttpStatus(Map<String, Object> errorAttributes) {
+		if (errorAttributes.get("code").equals(200)) {
+			return HttpStatus.UNAUTHORIZED.value();
+		}
 		return HttpStatus.OK.value();
 	}
 
