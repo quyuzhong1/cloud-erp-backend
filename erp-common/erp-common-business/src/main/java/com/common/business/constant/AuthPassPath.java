b932d9c59c43052ec3928571766a7b556da8244a
luoweigang <821969511@qq.com>
2024-02-23 16:55:22 +0800

    修改shopify授权02/23 6

--- erp-common/erp-common-business/src/main/java/com/common/business/constant/AuthPassPath.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/constant/AuthPassPath.java
@@ -9,7 +9,7 @@ package com.common.business.constant;
 public class AuthPassPath {
 
     public static final String PASS_PATH_LIST = "/user/accountLogin;/user/scanCodeLogin;/calendar/save/year;/user/forgotPasswordGetCode;" +
-            "/user/forgotPassword;/shop/shopAuthorize;/shop/shopifyAuthorizeIndex;/shop/shopifyUrl;/shopifyWebhook/customersDataRequest" +
+            "/user/forgotPassword;/shop/shopAuthorize;/shop/shopifyAuthorizeIndex;/shop/shopifyUrl;/shopifyWebhook/customersDataRequest;" +
             "/shopifyWebhook/customersRedact;/shopifyWebhook/shopRedact";
 
 
