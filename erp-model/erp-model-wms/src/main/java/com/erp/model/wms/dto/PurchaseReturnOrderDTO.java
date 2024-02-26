06683a25f1bf25b00454b0cd83ca7d10eb97cd9b
Will <wangwei@ulanzi.cn>
2024-02-26 15:13:49 +0800

    feat，其他入库高级查询调整

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/PurchaseReturnOrderDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/PurchaseReturnOrderDTO.java
@@ -673,35 +673,7 @@ public class PurchaseReturnOrderDTO {
     @Data
     @NoArgsConstructor
     public static class PagingParamDTO extends SortDTO {
-        /**
-         * 订单编号
-         */
-        private String code;
 
-        /**
-         * ids
-         */
-        private List<String> ids;
-
-        /**
-         * skuNo集合
-         */
-        private List<String> skuNoList;
-
-        /**
-         * 供应商id集合
-         */
-        private List<String> supplierIdList;
-
-        /**
-         * 采购单号
-         */
-        private String purchaseOrderCode;
-
-        /**
-         * 采购员id集合
-         */
-        private List<String> purchaseUserIdList;
 
         /**
          * 状态(waitSubmit 待提交，approveIng 审核中，reject 审核不通过，approve 已审核，waitForMeHandle 待我处理)
@@ -714,64 +686,11 @@ public class PurchaseReturnOrderDTO {
          * 作废状态（false未作废，true已作废）
          */
         private Boolean invalidStatus;
-
-        /**
-         * 退货方式
-         */
-        private String returnMode;
-
         /**
          * 退货日期
          */
         private List<LocalDate> billDateList;
 
-        /**
-         * 退货创库id集合
-         */
-        private List<String> returnWarehouseIdList;
-
-        /**
-         * 创建人id集合
-         */
-        private List<String> createUserIdList;
-
-        /**
-         * 创建时间
-         */
-        private List<LocalDate> createTimeList;
-
-        /**
-         * 退货来源
-         * 参考枚举ReturnOrderSourceEnum
-         */
-        private String returnOrderSource;
-
-        /**
-         * 明细备注
-         */
-        private String detailRemark;
-
-        /**
-         * 退货确认状态
-         * 来源：http://172.16.100.11:3002/project/92/interface/api/7186
-         * 入参：key=poReturnConfirmStatus
-         */
-        private String confirmStatus;
-        /**
-         * 异常分类
-         * 来源：http://172.16.100.11:3002/project/92/interface/api/7186
-         * 入参：key=poReturnUnusualType
-         */
-        private String unusualType;
-        /**
-         * 异常反馈描述
-         */
-        private String unusualRemark;
-        /**
-         * 异常处理人
-         * 来源：http://172.16.100.11:3002/project/92/interface/api/27679
-         */
-        private List<String> unusualHandleUserIdList;
 
 
         /**
