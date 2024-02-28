b076618ed98536847a427cfcba81b23b1f4ed01b
Jim <zhoujingbo@ulanzi.cn>
2024-02-28 09:27:26 +0800

    ADD:仓库发货配置map

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ListingInfoWithSkuMappingDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ListingInfoWithSkuMappingDTO.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -141,6 +142,18 @@ public class ListingInfoWithSkuMappingDTO {
      * 平台最后修改时间
      */
     private LocalDateTime platformUpdateTime;
+
+    /**
+     * 仓库发货配置map
+     * key:
+     * 仓库经营类型:selfBuild=自建,thirdParty=第三方
+     * {@link com.erp.model.wms.enums.WarehouseManageTypeEnum}
+     * value:
+     * 发货类型:single=子件发货,combine=捆绑Sku发货
+     * {@link com.erp.model.wms.enums.WarehouseDeliveryTypeEnum}
+     *
+     */
+    private Map<String, String> extendMap;
 
     public String checkAndGetProductSkuId() {
         if (StringUtils.isNotBlank(this.productSkuId)){
