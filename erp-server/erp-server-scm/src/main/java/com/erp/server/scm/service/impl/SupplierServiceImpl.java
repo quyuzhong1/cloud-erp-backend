1b49f2ced4110a0d180530575cd2cc5ff3411fbb
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-02-28 12:43:04 +0800

    SRM 协同启用日志记录修改

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
@@ -801,7 +801,7 @@ public class SupplierServiceImpl extends SuperServiceImpl<SupplierMapper, Suppli
             supplier.setSrmOperateUserName(userInfo.getUserName());
         }
         //添加日志
-        String content = String.format("编辑了供应商[%s] 启用SRM协同状态 有[%s] 变更为[%s]", supplier.getName(), supplier.getSrmDisabled() ? "停用" : "启用", dto.getState() ? "停用" : "启用");
+        String content = String.format("编辑了供应商[%s] 启用SRM协同状态 有[%s] 变更为[%s]", supplier.getName(), dto.getState() ? "启用" : "停用", dto.getState() ? "停用" : "启用");
         addModuleOperateLog(content, ModuleTypeEnum.SUPPLIER.getCode(), supplierId, "修改操作");
         this.updateById(supplier);
         return ApiResult.successMsg(msg);
