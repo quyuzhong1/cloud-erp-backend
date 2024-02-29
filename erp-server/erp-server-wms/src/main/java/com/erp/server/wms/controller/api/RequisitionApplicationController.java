e4bd280361fc67b9867b75b5cf4f0b28a472db28
luoweigang <821969511@qq.com>
2024-02-29 10:09:55 +0800

    修改要货申请删除权限（临时上线）

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/RequisitionApplicationController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/RequisitionApplicationController.java
@@ -302,7 +302,7 @@ public class RequisitionApplicationController extends BaseController {
     @DataPermission(operationType = DataAttributeEnum.CHECK_BY_ID,
             tableField = "create_user_id",
             menuCode = "wms:requisitionApplication:delete",
-            serviceClass = OverseasDeliveryPlanService.class,
+            serviceClass = RequisitionApplicationService.class,
             keyIdName = "ids")
     @LogAction(value = LogActionEnum.DELETE, desc = "要货申请删除")
     public ApiResult<List<BatchResultDTO>> delete(@RequestBody @Validated BaseIdsDTO.IdsDTO dto) {
