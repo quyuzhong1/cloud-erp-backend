4955fea667aaae905d3eb802fb3472de66e1bb13
luoweigang <821969511@qq.com>
2024-02-29 09:30:57 +0800

    修改shopify日志

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
@@ -1113,9 +1113,6 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
-        Map<String, String[]> parameterMap = request.getParameterMap();
-        log.warn("Shopify ERP方法：HttpServletRequest, Request body: {}", parameterMap.toString());
-
         // 处理请求体数据
         log.warn("Shopify ERP方法：customersDataRequest, dto: {}", requestBody);
 
