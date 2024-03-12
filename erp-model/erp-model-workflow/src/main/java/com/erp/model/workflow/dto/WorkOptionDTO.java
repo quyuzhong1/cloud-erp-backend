2e1465c6076ba52fde332d374e7b9af0dbfcd4e6
luoweigang <821969511@qq.com>
2024-03-12 17:16:19 +0800

    修改首页查询产品变更【首页1】

--- erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/WorkOptionDTO.java
+++ erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/WorkOptionDTO.java
@@ -168,7 +168,7 @@ public class WorkOptionDTO extends BaseEntity<WorkOptionDTO> {
         private String moduleStatus;
 
         /**
-         * 地址（预留）
+         * 地址
          */
         private String moduleUrl;
 
@@ -178,7 +178,7 @@ public class WorkOptionDTO extends BaseEntity<WorkOptionDTO> {
         private String path;
 
         /**
-         * 入参（预留）
+         * 入参
          */
         private String moduleParam;
 
---
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/UserDatePermissionService.java
@@ -0,0 +1,13 @@
+package com.erp.server.sys.service;
+
+public interface UserDatePermissionService {
+    /**
+     * 查询用户数据权限,获取到权限sql
+     * @Author Luo_WG
+     * @Date 2024/3/12 16:45
+     * @param tableField 权限过滤字段
+     * @param menuCode 菜单编号
+     * @return java.lang.String
+     **/
+    String getUserDatePermissionSql(String tableField, String menuCode);
+}
