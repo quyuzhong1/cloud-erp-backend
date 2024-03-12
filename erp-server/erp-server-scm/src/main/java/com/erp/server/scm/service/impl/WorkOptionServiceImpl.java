bb6f88e8277ead8d4b8253e739e5b9e84fa6b9bc
luoweigang <821969511@qq.com>
2024-03-12 18:29:20 +0800

    修改首页查询产品变更【首页】2

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/WorkOptionServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/WorkOptionServiceImpl.java
@@ -1,8 +1,13 @@
 package com.erp.server.scm.service.impl;
 
 import com.common.business.enums.SourceTypeEnum;
+import com.common.business.validator.ValidList;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.workflow.dto.ProcessManagementDTO;
 import com.erp.model.workflow.dto.WorkOptionDTO;
+import com.erp.rpc.workflow.WorkflowFeign;
 import com.erp.server.scm.mapper.WorkOptionMapper;
+import com.erp.server.scm.service.CommonService;
 import com.erp.server.scm.service.WorkOptionService;
 import org.springframework.stereotype.Service;
 
@@ -20,12 +25,29 @@ public class WorkOptionServiceImpl implements WorkOptionService {
     @Resource
     private WorkOptionMapper workOptionMapper;
 
+    @Resource
+    private WorkflowFeign workflowFeign;
+
+    @Resource
+    private CommonService commonService;
+
     @Override
     public List<WorkOptionDTO.MyWorkOptionDTO> getTableNum(List<WorkOptionDTO.MyWorkOptionDTO> myWorkOptionDTOList) {
         for (WorkOptionDTO.MyWorkOptionDTO myWorkOptionDTO : myWorkOptionDTOList) {
-
-            myWorkOptionDTO.setModuleCode(SourceTypeEnum.getByCode(myWorkOptionDTO.getModuleCode()).getTableName());
-            myWorkOptionDTO.setTableNumber(workOptionMapper.getTableNum(myWorkOptionDTO));
+            if (!myWorkOptionDTO.getIsApprovalWorkflow()) {
+                myWorkOptionDTO.setModuleCode(SourceTypeEnum.getByCode(myWorkOptionDTO.getModuleCode()).getTableName());
+                myWorkOptionDTO.setTableNumber(workOptionMapper.getTableNum(myWorkOptionDTO));
+            } else {
+                //获取当前人需要审核的业务ids
+                ValidList<ProcessManagementDTO.ApproveActivityDTO> dtoList = new ValidList<>();
+                ProcessManagementDTO.ApproveActivityDTO approveActivityDTO = new ProcessManagementDTO.ApproveActivityDTO();
+                approveActivityDTO.setCurApproveId(commonService.getUserInfo().getUid());
+                approveActivityDTO.setBusinessKey(myWorkOptionDTO.getModuleCode());
+                dtoList.add(approveActivityDTO);
+                ApiResult<List<ProcessManagementDTO.CurApproveInfoDTO>> listApiResult = workflowFeign.batchCurApproverByApprove(dtoList);
+                myWorkOptionDTO.setModuleCode(SourceTypeEnum.getByCode(myWorkOptionDTO.getModuleCode()).getTableName());
+                myWorkOptionDTO.setTableNumber(listApiResult.getData().size());
+            }
         }
         return myWorkOptionDTOList;
     }
