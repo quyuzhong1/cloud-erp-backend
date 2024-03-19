a84bd03b85bed72af101abf3a4d43088a1b98375
liuruipeng <arthur@ulanzi.cn>
2024-03-19 17:49:25 +0800

    feat:艾姆勒编辑不传smcode

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ImlHandlerServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ImlHandlerServiceImpl.java
@@ -58,6 +58,7 @@ public class ImlHandlerServiceImpl extends AbstractThirdWarehouseHandler {
     protected ApiResult<String> editInboundBill(ThirdWarehouseCreateInboundReq createInboundReq) {
         ImlCreateInboundReq imlCreateInboundReq = OverseasWarehouseInboundConverter.INSTANCE.inboundDtoToIml(createInboundReq);
         // 修改入库单
+        imlCreateInboundReq.setSmCode(null);
         ImlResponse<String> imlResponse = imlService.editInboundBill(imlCreateInboundReq);
         return isSuccess(imlResponse.getAsk()) ? success(imlResponse.getData()) : failure(imlResponse.getMessage());
     }
