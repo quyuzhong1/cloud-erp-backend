3bcb000682f0009bbc0ab58fe85a1115633cee59
luoweigang <821969511@qq.com>
2024-04-02 12:25:00 +0800

    【DMP清洗2】

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpOrderInfoServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpOrderInfoServiceImpl.java
@@ -228,12 +228,12 @@ public class DmpOrderInfoServiceImpl extends ServiceImpl<DmpOrderInfoMapper, Dmp
                                     .or().eq(DmpOrderInfoEntity::getSite, "")
                     )
                     .orderByAsc(DmpOrderInfoEntity::getRetryCount, DmpOrderInfoEntity::getId)
-                    .last("LIMIT " + pageSize + " OFFSET " + (pageNumber-1) * 20)
+                    .last("LIMIT " + pageSize + " OFFSET " + (pageNumber-1) * pageSize)
                     .list();
         } catch (Exception e) {
             XxlJobHelper.log("查询需要清洗的数据时报错， message={}", e.getMessage());
         }
-
+        pageNumber++;
         if (CollectionUtil.isEmpty(list)) {
             pageNumber = 1;
             XxlJobHelper.log("清洗订单数据 cleanOrder 需要清洗数据为空 pageSize={}", pageSize);
