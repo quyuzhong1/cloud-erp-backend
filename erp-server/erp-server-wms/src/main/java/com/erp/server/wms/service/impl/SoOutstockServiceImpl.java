743e07ccf3faca53d037ad4d03ac87697b571f04
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-04-07 10:59:45 +0800

    merge:销售出库单保存渠道和跟踪号信息

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
@@ -782,6 +782,8 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
                         addDTO.setToCountry("");
                     }
                     addDTO.setCurrency(soInfo.getCurrency());
+                    addDTO.setChannelId(entity.getLogisticsChannelId());
+                    addDTO.setTransportNo(entity.getTrackNo());
                 }
             } else {
                 //表示是b2c
@@ -1202,13 +1204,17 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
             soB2cEntities = soB2cFeign.listByIds(soIds);
         }
         String b2c = OrderTypeEnum.B2C.getCode();
-        List<String> ids = list.stream().map(SoOutstockDTO.PagingViewDTO::getId).distinct().collect(Collectors.toList());
+//        List<String> ids = list.stream().map(SoOutstockDTO.PagingViewDTO::getId).distinct().collect(Collectors.toList());
         //跟踪单号
-        Map<String,List<String>> trackNoMAp = logisticsBillFeign.mapTrackNoAndSoOutId(ids);
+//        Map<String,List<String>> trackNoMAp = logisticsBillFeign.mapTrackNoAndSoOutId(ids);
         for (SoOutstockDTO.PagingViewDTO item : list) {
             //设置跟踪单号
-            if(trackNoMAp.containsKey(item.getId())){
-                item.setTrackNo(trackNoMAp.get(item.getId()));
+//            if(trackNoMAp.containsKey(item.getId())){
+//                item.setTrackNo(trackNoMAp.get(item.getId()));
+//            }
+            if (StringUtils.isNotEmpty(item.getTrackNos())){
+                String[] split = item.getTrackNos().split(",");
+                item.setTrackNo(Arrays.stream(split).collect(Collectors.toList()));
             }
             //设置拦截标识
             SoB2cEntity soB2cEntity = soB2cEntities.stream().filter(req -> req.getId().equals(item.getSoId())).findFirst().orElse(null);
@@ -1730,6 +1736,11 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
                 LogisticsChannelDTO.BaseDTO logisticsInfo = logisticsInfoList.stream().filter(v->v.getId().equals(pagingUpdateDTO.getLogisticsChannelId())).findFirst().orElse(null);
                 if(Objects.nonNull(logisticsInfo)){
                     soOutstock.setCarrierId(logisticsInfo.getSupplierId());
+                    //记录 运输单号和渠道信息
+                    if (CollectionUtils.isNotEmpty(pagingUpdateDTO.getTrackNoList())){
+                        soOutstock.setTrackNo(String.join(",", pagingUpdateDTO.getTrackNoList()));
+                    }
+                    soOutstock.setLogisticsChannelId(pagingUpdateDTO.getLogisticsChannelId());
                     updateList.add(soOutstock);
                 }
             }
