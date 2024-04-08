d6e4a87617f80ff2b8b58f7378c32998ac5930fe
luoweigang <821969511@qq.com>
2024-04-08 15:51:48 +0800

    修改sql注入正则

--- erp-gateway/src/main/java/com/cloud/erp/gateway/utils/SqlInjectionRuleUtils.java
+++ erp-gateway/src/main/java/com/cloud/erp/gateway/utils/SqlInjectionRuleUtils.java
@@ -6,7 +6,6 @@ import cn.hutool.json.JSONUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.MultiValueMap;
-import org.thymeleaf.util.MapUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -26,7 +25,7 @@ public class SqlInjectionRuleUtils {
      * SQL的正则表达式
      */
     private static String badStrReg = "\\b(and|or)\\b.{1,6}?(=|>|<|\\bin\\b|\\blike\\b)|\\/\\*.+?\\*\\/|<\\s*script\\b|\\bEXEC\\b|UNION.+?SELECT|UPDATE.+?" +
-            "SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE).+?FROM|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)";
+            "SET|INSERT\\s+INTO.+?VALUES|(SELECT|DELETE)\\s+\\bFROM\\b|(CREATE|ALTER|DROP|TRUNCATE)\\s+(TABLE|DATABASE)|\\bfrom\\b";
     
     /**
      * SQL的正则表达式
