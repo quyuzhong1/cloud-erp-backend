c72ab71ddb9dbdcadbe4ea78ef22041a904c4d57
liuruipeng <arthur@ulanzi.cn>
2024-04-10 15:05:40 +0800

    feat:导入日期转换器

--- erp-common/erp-common-business/src/main/java/com/common/business/utils/LocalDateStringConverter.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/utils/LocalDateStringConverter.java
@@ -7,6 +7,7 @@ import com.alibaba.excel.metadata.GlobalConfiguration;
 import com.alibaba.excel.metadata.property.ExcelContentProperty;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 /**
@@ -28,6 +29,13 @@ public class LocalDateStringConverter implements Converter<LocalDateTime> {
 
     @Override
     public LocalDateTime convertToJavaData(CellData cellData, ExcelContentProperty excelContentProperty, GlobalConfiguration globalConfiguration) throws Exception {
+        if (cellData.getType().equals(CellDataTypeEnum.NUMBER)) {
+            // Excel中的日期时间值是基于1900年1月1日的序列值，加上数值即可转换为Java的LocalDateTime
+            // 时间值是小数部分，代表一天中的时间，乘以86400（秒数）可转换为秒数
+            double num = cellData.getNumberValue().doubleValue();
+            long secondsSinceEpoch = (long) ((num - 25569) * 86400);
+            return LocalDateTime.ofEpochSecond(secondsSinceEpoch, 0, ZoneOffset.UTC);
+        }
         return LocalDateTime.parse(cellData.getStringValue(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
