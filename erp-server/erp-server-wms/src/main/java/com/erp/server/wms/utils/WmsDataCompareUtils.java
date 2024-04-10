a5a1a2f975cda2d5e15d81f7b59c3ebe91190ce9
shukai <shukai@ulanzi.cn>
2024-04-10 22:54:57 +0800

    fix:最终测试使用easyexcel

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/utils/WmsDataCompareUtils.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/utils/WmsDataCompareUtils.java
@@ -39,91 +39,91 @@ public class WmsDataCompareUtils {
 		public List<List<String>> datas = new ArrayList<>();
 	}
 	
-//	public static WmsDataCompareExcelDto getWmsDataCompareExcelDto(List<String> excelFiles , boolean needHeadAndCount) {
-//		WmsDataCompareExcelDto dto = new WmsDataCompareExcelDto();
-//		if(CollUtil.isEmpty(excelFiles)) {
-//			return dto;
-//		}
-//		Integer importDataCount = 0;
-//        for(String excelFile : excelFiles) {
-//        	InputStream inputStream = null;
-//        	try {
-//				inputStream = FastDFSClientUtil.getInputStream(excelFile);
-//			} catch (Exception e) {
-//				log.error("获取文件失败" , e);
-//				throw new ServiceException("获取文件失败");
-//			}
-//        	List<Map<String, String>> makeDataInputStream = null;
-//        	try {
-//        		makeDataInputStream = ExcelPrintUtils.makeDataInputStream(inputStream);
-//			} catch (Exception e) {
-//				log.error("读取excel失败" , e);
-//				throw new ServiceException("读取excel失败");
-//			}
-//        	
-//        	if(CollUtil.isNotEmpty(makeDataInputStream)) {
-//        		Map<String, String> map = makeDataInputStream.get(0);
-//        		List<String> headFields = new ArrayList<>();
-//        		for(Map.Entry<String, String> m : map.entrySet()) {
-//        			headFields.add(m.getKey());
-//        		}
-//        		dto.getHeadFieldLists().add(headFields);
-//        		
-//        		importDataCount = importDataCount + makeDataInputStream.size();
-//        		
-//        		for(Map<String, String> makeData : makeDataInputStream) {
-//        			List<String> data = new ArrayList<>();
-//        			for(Map.Entry<String, String> m : makeData.entrySet()) {
-//        				data.add(m.getValue());
-//        			}
-//        			dto.getDatas().add(data);
-//        		}
-//        	}
-//        }
-//        dto.setImportDataCount(importDataCount);
-//        return dto;
-//	}
-	
 	public static WmsDataCompareExcelDto getWmsDataCompareExcelDto(List<String> excelFiles , boolean needHeadAndCount) {
 		WmsDataCompareExcelDto dto = new WmsDataCompareExcelDto();
 		if(CollUtil.isEmpty(excelFiles)) {
 			return dto;
 		}
 		Integer importDataCount = 0;
-		for(String excelFile : excelFiles) {
-			InputStream inputStream = null;
-			try {
+        for(String excelFile : excelFiles) {
+        	InputStream inputStream = null;
+        	try {
 				inputStream = FastDFSClientUtil.getInputStream(excelFile);
 			} catch (Exception e) {
 				log.error("获取文件失败" , e);
 				throw new ServiceException("获取文件失败");
 			}
-			try {
-				Workbook workbook = WorkbookFactory.create(inputStream);
-				Sheet sheet = workbook.getSheetAt(0); // 获取第一个工作表
-				int totalRows = sheet.getPhysicalNumberOfRows();
-				Integer headSize = 0; 
-				if(totalRows > 0) {
-					List<String> cellValues = getCellValues(sheet.getRow(0) , null);
-					headSize = cellValues.size();
-					dto.getHeadFieldLists().add(cellValues);
-					importDataCount = importDataCount + totalRows - 1; // 获取总行数
-				}
-				if(needHeadAndCount && totalRows > 1) {
-					for(int i = 1; i < totalRows; i++) {
-						dto.getDatas().add(getCellValues(sheet.getRow(i) , headSize));
-					}
-				}
-				
+        	List<Map<String, String>> makeDataInputStream = null;
+        	try {
+        		makeDataInputStream = ExcelPrintUtils.makeDataInputStream(inputStream);
 			} catch (Exception e) {
 				log.error("读取excel失败" , e);
 				throw new ServiceException("读取excel失败");
 			}
-			
-		}
-		dto.setImportDataCount(importDataCount);
-		return dto;
+        	
+        	if(CollUtil.isNotEmpty(makeDataInputStream)) {
+        		Map<String, String> map = makeDataInputStream.get(0);
+        		List<String> headFields = new ArrayList<>();
+        		for(Map.Entry<String, String> m : map.entrySet()) {
+        			headFields.add(m.getKey());
+        		}
+        		dto.getHeadFieldLists().add(headFields);
+        		
+        		importDataCount = importDataCount + makeDataInputStream.size();
+        		
+        		for(Map<String, String> makeData : makeDataInputStream) {
+        			List<String> data = new ArrayList<>();
+        			for(Map.Entry<String, String> m : makeData.entrySet()) {
+        				data.add(m.getValue());
+        			}
+        			dto.getDatas().add(data);
+        		}
+        	}
+        }
+        dto.setImportDataCount(importDataCount);
+        return dto;
 	}
+	
+//	public static WmsDataCompareExcelDto getWmsDataCompareExcelDto(List<String> excelFiles , boolean needHeadAndCount) {
+//		WmsDataCompareExcelDto dto = new WmsDataCompareExcelDto();
+//		if(CollUtil.isEmpty(excelFiles)) {
+//			return dto;
+//		}
+//		Integer importDataCount = 0;
+//		for(String excelFile : excelFiles) {
+//			InputStream inputStream = null;
+//			try {
+//				inputStream = FastDFSClientUtil.getInputStream(excelFile);
+//			} catch (Exception e) {
+//				log.error("获取文件失败" , e);
+//				throw new ServiceException("获取文件失败");
+//			}
+//			try {
+//				Workbook workbook = WorkbookFactory.create(inputStream);
+//				Sheet sheet = workbook.getSheetAt(0); // 获取第一个工作表
+//				int totalRows = sheet.getPhysicalNumberOfRows();
+//				Integer headSize = 0; 
+//				if(totalRows > 0) {
+//					List<String> cellValues = getCellValues(sheet.getRow(0) , null);
+//					headSize = cellValues.size();
+//					dto.getHeadFieldLists().add(cellValues);
+//					importDataCount = importDataCount + totalRows - 1; // 获取总行数
+//				}
+//				if(needHeadAndCount && totalRows > 1) {
+//					for(int i = 1; i < totalRows; i++) {
+//						dto.getDatas().add(getCellValues(sheet.getRow(i) , headSize));
+//					}
+//				}
+//				
+//			} catch (Exception e) {
+//				log.error("读取excel失败" , e);
+//				throw new ServiceException("读取excel失败");
+//			}
+//			
+//		}
+//		dto.setImportDataCount(importDataCount);
+//		return dto;
+//	}
 	
 	private static List<String> getCellValues(Row row , Integer headSize){
 		List<String> cellValues = new ArrayList<>();
