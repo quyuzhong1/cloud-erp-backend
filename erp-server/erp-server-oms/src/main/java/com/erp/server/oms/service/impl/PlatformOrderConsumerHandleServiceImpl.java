741a369b575ba7b8feab5821cd35ea7ab47e8ed7
Jim <zhoujingbo@ulanzi.cn>
2024-04-10 16:38:49 +0800

    BUG:SKu尺寸部分无映射fix

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/PlatformOrderConsumerHandleServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/PlatformOrderConsumerHandleServiceImpl.java
@@ -306,17 +306,23 @@ public class PlatformOrderConsumerHandleServiceImpl implements PlatformOrderCons
                 });
             }else {
                 SkuInfoSimpleVO simpleSkuVO = sourceSkuMap.get(addDTO.getSkuId());
-                skuVO = BomChildrenSkuDTO.builder()
-                        .skuId(simpleSkuVO.getSkuId())
-                        .productSize(simpleSkuVO.getProductSize())
-                        .build();
-                soB2cService.buildProductSize(skuVO);
-                splitSkuDTOS.add(SplitSkuDTO.builder().skuId(addDTO.getSkuId()).qty(addDTO.getQty())
-                        .skuNo(Objects.nonNull(skuVO) && StrUtil.isNotEmpty(skuVO.getSkuNo()) ? skuVO.getSkuNo() : "")
-                        .length(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getLength()) ? skuVO.getLength() : BigDecimal.ZERO)
-                        .width(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getWidth()) ? skuVO.getWidth() : BigDecimal.ZERO)
-                        .height(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getHeight()) ? skuVO.getHeight() : BigDecimal.ZERO)
-                        .build());
+                if (null == simpleSkuVO){
+                    // 部分无映射关系设置为空
+                    splitSkuDTOS.add(new SplitSkuDTO(addDTO.getSkuId(), addDTO.getSkuNo()));
+                } else {
+                    skuVO = BomChildrenSkuDTO.builder()
+                            .skuId(simpleSkuVO.getSkuId())
+                            .productSize(simpleSkuVO.getProductSize())
+                            .build();
+                    soB2cService.buildProductSize(skuVO);
+                    splitSkuDTOS.add(SplitSkuDTO.builder().skuId(addDTO.getSkuId()).qty(addDTO.getQty())
+                            .skuNo(Objects.nonNull(skuVO) && StrUtil.isNotEmpty(skuVO.getSkuNo()) ? skuVO.getSkuNo() : "")
+                            .length(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getLength()) ? skuVO.getLength() : BigDecimal.ZERO)
+                            .width(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getWidth()) ? skuVO.getWidth() : BigDecimal.ZERO)
+                            .height(Objects.nonNull(skuVO) && Objects.nonNull(skuVO.getHeight()) ? skuVO.getHeight() : BigDecimal.ZERO)
+                            .build());
+                }
+
             }
         });
         return splitSkuDTOS;
