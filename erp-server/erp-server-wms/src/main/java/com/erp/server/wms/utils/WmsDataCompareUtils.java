cb3fcdad55188b8ebeb9e94abf6d2cd390d05f7a
shukai <shukai@ulanzi.cn>
2024-04-10 18:37:59 +0800

    fix：使用easyexcel解析

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/utils/WmsDataCompareUtils.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/utils/WmsDataCompareUtils.java
@@ -2,8 +2,8 @@ package com.erp.server.wms.utils;
 
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.poi.ss.usermodel.BorderStyle;
@@ -21,6 +21,7 @@ import org.apache.poi.ss.util.CellRangeAddress;
 
 import com.alibaba.excel.metadata.Head;
 import com.alibaba.excel.write.merge.AbstractMergeStrategy;
+import com.common.core.excel.ExcelPrintUtils;
 import com.common.core.exception.ServiceException;
 import com.common.core.utils.FastDFSClientUtil;
 import com.erp.model.wms.dto.WmsDataComparePlanDTO.ImportDataMappingDTO;
@@ -44,40 +45,85 @@ public class WmsDataCompareUtils {
 			return dto;
 		}
 		Integer importDataCount = 0;
-		for(String excelFile : excelFiles) {
-			InputStream inputStream = null;
-			try {
+        for(String excelFile : excelFiles) {
+        	InputStream inputStream = null;
+        	try {
 				inputStream = FastDFSClientUtil.getInputStream(excelFile);
 			} catch (Exception e) {
 				log.error("获取文件失败" , e);
 				throw new ServiceException("获取文件失败");
 			}
-			try {
-				Workbook workbook = WorkbookFactory.create(inputStream);
-				Sheet sheet = workbook.getSheetAt(0); // 获取第一个工作表
-				int totalRows = sheet.getPhysicalNumberOfRows();
-				Integer headSize = 0; 
-				if(totalRows > 0) {
-					List<String> cellValues = getCellValues(sheet.getRow(0) , null);
-					headSize = cellValues.size();
-					dto.getHeadFieldLists().add(cellValues);
-					importDataCount = importDataCount + totalRows - 1; // 获取总行数
-				}
-				if(needHeadAndCount && totalRows > 1) {
-					for(int i = 1; i < totalRows; i++) {
-						dto.getDatas().add(getCellValues(sheet.getRow(i) , headSize));
-					}
-				}
-				
+        	List<Map<String, String>> makeDataInputStream = null;
+        	try {
+        		makeDataInputStream = ExcelPrintUtils.makeDataInputStream(inputStream);
 			} catch (Exception e) {
 				log.error("读取excel失败" , e);
 				throw new ServiceException("读取excel失败");
 			}
-			
-		}
-		dto.setImportDataCount(importDataCount);
-		return dto;
+        	
+        	if(CollUtil.isNotEmpty(makeDataInputStream)) {
+        		Map<String, String> map = makeDataInputStream.get(0);
+        		List<String> headFields = new ArrayList<>();
+        		for(Map.Entry<String, String> m : map.entrySet()) {
+        			headFields.add(m.getKey());
+        		}
+        		dto.getHeadFieldLists().add(headFields);
+        		
+        		importDataCount = importDataCount + makeDataInputStream.size();
+        		
+        		for(Map<String, String> makeData : makeDataInputStream) {
+        			List<String> data = new ArrayList<>();
+        			for(Map.Entry<String, String> m : makeData.entrySet()) {
+        				data.add(m.getValue());
+        			}
+        			dto.getDatas().add(data);
+        		}
+        	}
+        }
+        dto.setImportDataCount(importDataCount);
+        return dto;
 	}
+	
+//	public static WmsDataCompareExcelDto getWmsDataCompareExcelDto(List<String> excelFiles , boolean needHeadAndCount) {
+//		WmsDataCompareExcelDto dto = new WmsDataCompareExcelDto();
+//		if(CollUtil.isEmpty(excelFiles)) {
+//			return dto;
+//		}
+//		Integer importDataCount = 0;
+//		for(String excelFile : excelFiles) {
+//			InputStream inputStream = null;
+//			try {
+//				inputStream = FastDFSClientUtil.getInputStream(excelFile);
+//			} catch (Exception e) {
+//				log.error("获取文件失败" , e);
+//				throw new ServiceException("获取文件失败");
+//			}
+//			try {
+//				Workbook workbook = WorkbookFactory.create(inputStream);
+//				Sheet sheet = workbook.getSheetAt(0); // 获取第一个工作表
+//				int totalRows = sheet.getPhysicalNumberOfRows();
+//				Integer headSize = 0; 
+//				if(totalRows > 0) {
+//					List<String> cellValues = getCellValues(sheet.getRow(0) , null);
+//					headSize = cellValues.size();
+//					dto.getHeadFieldLists().add(cellValues);
+//					importDataCount = importDataCount + totalRows - 1; // 获取总行数
+//				}
+//				if(needHeadAndCount && totalRows > 1) {
+//					for(int i = 1; i < totalRows; i++) {
+//						dto.getDatas().add(getCellValues(sheet.getRow(i) , headSize));
+//					}
+//				}
+//				
+//			} catch (Exception e) {
+//				log.error("读取excel失败" , e);
+//				throw new ServiceException("读取excel失败");
+//			}
+//			
+//		}
+//		dto.setImportDataCount(importDataCount);
+//		return dto;
+//	}
 	
 	private static List<String> getCellValues(Row row , Integer headSize){
 		List<String> cellValues = new ArrayList<>();
