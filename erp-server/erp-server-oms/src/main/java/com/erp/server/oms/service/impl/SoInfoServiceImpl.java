07acf8fd3ba447b474e94c5ec6d71e91752e4a4d
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-04-15 10:34:05 +0800

    add:v1.22 销售毛利计算修改

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -2117,6 +2117,7 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
                 purchasePrice = rate.multiply(skuCostProfitResult.getPurchasePrice()).setScale(4, BigDecimal.ROUND_HALF_UP);
             }
         }
+        costParam.setAmountLocalCurrency(skuCostProfitResult.getAmountLocalCurrency());
         // 计算成本毛利信息
         skuCostProfitResult = SoUtils.calCostProfit(purchasePrice, costParam, skuCostProfitResult);
         return skuCostProfitResult;
@@ -3155,84 +3156,5 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
             }
 
         }
-    }
-
-
-    public SkuCostProfitDTO.SkuCostProfitResult getSkuCostProfitt(SkuCostProfitDTO.SkuCostProfitParam costParam) {
-        if (Objects.isNull(costParam.getQty()) || costParam.getQty() < 0) {
-            costParam.setQty(0);
-        }
-        SkuCostProfitDTO.SkuCostProfitResult skuCostProfitResult = new SkuCostProfitDTO.SkuCostProfitResult();
-        skuCostProfitResult.setSkuId(costParam.getSkuId());
-        skuCostProfitResult.setPurchasePrice(BigDecimal.ZERO);
-        skuCostProfitResult.setSaleCost(BigDecimal.ZERO);
-        skuCostProfitResult.setSaleProfit(BigDecimal.ZERO);
-        skuCostProfitResult.setSaleProfitRate(BigDecimal.ZERO);
-        BigDecimal purchasePrice = BigDecimal.ZERO;
-
-        List<SkuVO> skuList = plmTaskFeign.getSkuInfoByIds(Lists.newArrayList(costParam.getSkuId()));
-        // 供应商id集合
-        List<String> supplierIds = skuList.stream().filter(r -> StrUtil.isNotEmpty(r.getSupplierId())).map(SkuVO::getSupplierId).distinct().collect(Collectors.toList());
-        String supplierId = skuList.stream().filter(r -> Objects.equals(r.getSkuId(), costParam.getSkuId())).findFirst().map(SkuVO::getSupplierId).orElse(null);
-        List<PurchasePriceDTO.SupplierSkuPrice> purchasePriceList = Lists.newArrayList();
-        if (CollUtil.isNotEmpty(supplierIds)) {
-            purchasePriceList = scmTaskFeign.listSupplierSkuPrice(supplierIds);
-        }
-        PurchasePriceDTO.SupplierSkuPrice supplierSkuPrice = null;
-        if (CollUtil.isNotEmpty(purchasePriceList)) {
-            supplierSkuPrice = purchasePriceList.stream().filter(r -> Objects.equals(r.getSkuId(), costParam.getSkuId()) && Objects.equals(r.getSupplierId(), supplierId)).findFirst().orElse(null);
-            if (Objects.nonNull(supplierSkuPrice)) {
-                purchasePrice = supplierSkuPrice.getTaxPrice();
-                BigDecimal purchaseTaxRate = supplierSkuPrice.getTaxRate();
-                //不含税单价（不含税价格=含税价格/（1+增值税税率））
-                purchasePrice = MathUtil.divide(purchasePrice, MathUtil.add(BigDecimal.ONE, purchaseTaxRate));
-                skuCostProfitResult.setPurchasePrice(purchasePrice);
-            }
-        }
-        log.info("提交的币制：{}", costParam.getCurrency());
-
-        BigDecimal rate = BigDecimal.ZERO;
-        if (Objects.nonNull(supplierSkuPrice)) {
-            LocalDate purchaseDate = costParam.getBillDate();
-            if (Objects.nonNull(purchaseDate)) {
-                String purchaseDateStr = purchaseDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                rate = dmpTaskFeign.getRate(purchaseDateStr, supplierSkuPrice.getCurrency());
-            }
-
-        }
-        // 最新的采购单价币制转换（非人民币）
-        if (Objects.nonNull(skuCostProfitResult.getPurchasePrice()) &&
-                skuCostProfitResult.getPurchasePrice().compareTo(BigDecimal.ZERO) == 1 &&
-                !Objects.equals(supplierSkuPrice.getCurrency(), "CNY")) {
-
-            log.info("找到的最新的采购价目的币制：{}，含税单价：【{}】, 销售订单日期：{}，转换后汇率：{}", supplierSkuPrice.getCurrency(), supplierSkuPrice.getTaxPrice(), rate);
-            // 未找到汇率直接返回
-            if (Objects.isNull(rate) || rate.compareTo(BigDecimal.ZERO) <= 0) {
-                skuCostProfitResult.setPurchasePrice(BigDecimal.ZERO);
-                skuCostProfitResult.setExchangeRate(BigDecimal.ZERO);
-                return skuCostProfitResult;
-            } else {
-                skuCostProfitResult.setExchangeRate(rate);
-                // 转换成人民币采购单价
-                purchasePrice = rate.multiply(skuCostProfitResult.getPurchasePrice()).setScale(4, BigDecimal.ROUND_HALF_UP);
-            }
-        }
-        // 销售金额转换
-        if (Objects.nonNull(costParam.getSaleAmount()) &&
-                costParam.getSaleAmount().compareTo(BigDecimal.ZERO) == 1 &&
-                !Objects.equals(costParam.getCurrency(), "CNY")) {
-            rate = dmpTaskFeign.getRate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), costParam.getCurrency());
-            log.info("提交的币制：{}，转换后汇率：{}", costParam.getCurrency(), rate);
-            if (Objects.isNull(rate) || rate.compareTo(BigDecimal.ZERO) <= 0) {
-                costParam.setSaleAmount(BigDecimal.ZERO);
-            } else {
-                // 转换成人民币销售金额
-                costParam.setSaleAmount(rate.multiply(costParam.getSaleAmount()).setScale(4, BigDecimal.ROUND_HALF_UP));
-            }
-        }
-        // 计算成本毛利信息
-        skuCostProfitResult = SoUtils.calCostProfit(purchasePrice, costParam, skuCostProfitResult);
-        skuCostProfitResult.setExchangeRate(rate);
-        return skuCostProfitResult;
     }
 }
