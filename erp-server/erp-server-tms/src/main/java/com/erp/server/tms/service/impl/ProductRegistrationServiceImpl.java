bb93eae2d4d9c9cfc6fd6d1ae59851a16316e32f
liuruipeng <arthur@ulanzi.cn>
2024-04-16 18:33:55 +0800

    feat:保宏拉取产品判断相等

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/ProductRegistrationServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/ProductRegistrationServiceImpl.java
@@ -516,11 +516,17 @@ public class ProductRegistrationServiceImpl extends SuperServiceImpl<ProductRegi
         if(Objects.isNull(addEntity.getGrossWeight())){
             addEntity.setGrossWeight(BigDecimal.ZERO);
         }
+        if(Objects.isNull(addEntity.getDeclarePrice())){
+            addEntity.setDeclarePrice(BigDecimal.ZERO);
+        }
+        if(Objects.isNull(productDTO.getDeclarePrice())){
+            productDTO.setDeclarePrice(BigDecimal.ZERO);
+        }
         return Objects.equals(addEntity.getSkuNo(), productDTO.getSkuNo()) &&
                 Objects.equals(addEntity.getProductName(), productDTO.getCnName()) &&
                 Objects.equals(addEntity.getProductNameEn(), productDTO.getEnName()) &&
                 (Objects.equals(addEntity.getCurrency(), productDTO.getDeclareCurrency()) || (Objects.equals(productDTO.getDeclareCurrency(), "CNY") && Objects.equals(addEntity.getCurrency(), "RMB"))) &&
-                Objects.equals(addEntity.getDeclarePrice(), productDTO.getDeclarePrice()) &&
+                addEntity.getDeclarePrice().compareTo(productDTO.getDeclarePrice()) == 0 &&
                 addEntity.getGrossWeight().compareTo(productDTO.getGrossWeight()) == 0 &&
                 Objects.equals(addEntity.getDeclareNameCn(), productDTO.getDeclareChineseName()) &&
                 Objects.equals(addEntity.getCustomsCode(), productDTO.getCustomsCode()) &&
