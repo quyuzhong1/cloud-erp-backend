d5aa29ca0f030155b021f90dded72e663665afb8
luoweigang <821969511@qq.com>
2024-04-18 17:31:28 +0800

    修改物流拦截成功清楚订单的物流相关信息【临时上线】

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillServiceImpl.java
@@ -1075,7 +1075,7 @@ public class LogisticsBillServiceImpl extends SuperServiceImpl<LogisticsBillMapp
         //删除物流详情
         List<LogisticsBillEntity> logisticsBillEntityList = this.listBySourceIds(sourceId);
         List<String> ids = logisticsBillEntityList.stream().map(req -> req.getId()).collect(Collectors.toList());
-        logisticsBillDetailService.removeByMainIds(ids);
+        logisticsBillDetailService.removeByMainIds(ids, Boolean.TRUE);
 
         //删除主表
         return lambdaUpdate().in(LogisticsBillEntity::getSourceId, sourceId).remove();
