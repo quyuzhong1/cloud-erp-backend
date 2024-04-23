f9664d2156ce23084a9996dd93e527fa8ba1deef
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-04-23 17:32:52 +0800

    启用供应商显示在前面

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierServiceImpl.java
@@ -828,7 +828,7 @@ public class SupplierServiceImpl extends SuperServiceImpl<SupplierMapper, Suppli
         //审核通过
         String approveStatus = ApproveStatusEnum.APPROVE.getStatus();
         queryWrapper.eq(SupplierEntity::getApproveStatus, ApproveStatusEnum.getByStatus(approveStatus));
-        queryWrapper.orderByAsc(SupplierEntity::getDisabled);
+        queryWrapper.orderByAsc(SupplierEntity::getSrmDisabled);
         return this.listMaps(queryWrapper);
     }
 
