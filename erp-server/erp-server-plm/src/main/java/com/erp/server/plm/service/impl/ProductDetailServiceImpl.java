b77b79f1d19ccadbdf8612fcecb87102164816b6
liaohui <liaohui@ulanzi.cn>
2024-04-24 15:03:46 +0800

    fix: 产品尺寸和箱规校验

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -4134,11 +4134,17 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
                 continue;
             }
             //包装尺寸
-            if (ObjectUtils.isEmpty(productPackEntity.getProductLength()) || ObjectUtils.isEmpty(productPackEntity.getProductWidth()) && ObjectUtils.isEmpty(productPackEntity.getProductLength())) {
+            if (ObjectUtils.isEmpty(productPackEntity.getProductLength()) || ObjectUtils.isEmpty(productPackEntity.getProductWidth()) || ObjectUtils.isEmpty(productPackEntity.getProductHeight())) {
+                errMsg.append(StrUtil.format(ApiError.ERROR_PRODUCT_SIZE_NOT_EXIST.msg, detailEntity.getSkuNo())).append("</br>");
+            }
+            if (BigDecimal.ZERO.compareTo(productPackEntity.getProductLength()) >= 0 || BigDecimal.ZERO.compareTo(productPackEntity.getProductWidth()) >= 0 || BigDecimal.ZERO.compareTo(productPackEntity.getProductHeight()) >= 0) {
                 errMsg.append(StrUtil.format(ApiError.ERROR_PRODUCT_SIZE_NOT_EXIST.msg, detailEntity.getSkuNo())).append("</br>");
             }
             //箱规
-            if (ObjectUtils.isEmpty(productPackEntity.getBoxLength()) || ObjectUtils.isEmpty(productPackEntity.getBoxWidth()) && ObjectUtils.isEmpty(productPackEntity.getBoxHeight())) {
+            if (ObjectUtils.isEmpty(productPackEntity.getBoxLength()) || ObjectUtils.isEmpty(productPackEntity.getBoxWidth()) || ObjectUtils.isEmpty(productPackEntity.getBoxHeight())) {
+                errMsg.append(StrUtil.format(ApiError.ERROR_BOX_SIZE_NOT_EXIST.msg, detailEntity.getSkuNo())).append("</br>");
+            }
+            if (BigDecimal.ZERO.compareTo(productPackEntity.getBoxLength()) >= 0 || BigDecimal.ZERO.compareTo(productPackEntity.getBoxWidth()) >= 0 || BigDecimal.ZERO.compareTo(productPackEntity.getBoxHeight()) >= 0) {
                 errMsg.append(StrUtil.format(ApiError.ERROR_BOX_SIZE_NOT_EXIST.msg, detailEntity.getSkuNo())).append("</br>");
             }
             //毛重
