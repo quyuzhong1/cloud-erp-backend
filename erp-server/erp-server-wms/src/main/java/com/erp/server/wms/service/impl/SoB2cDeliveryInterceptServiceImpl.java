a7e343ce5633c7ffa3304acfb73818beb0f7d1bc
Will <wangwei@ulanzi.cn>
2024-04-25 12:28:10 +0800

    Revert "物流拦截单【临时上线】"
    This reverts commit 344236efc9bd5dbb3de387861fd8b1ca4158c0d7.
    
    (cherry picked from commit 12b95d1d57c932996e21558407eb93e40b7a35c4)

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryInterceptServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryInterceptServiceImpl.java
@@ -307,8 +307,6 @@ public class SoB2cDeliveryInterceptServiceImpl extends SuperServiceImpl<SoB2cDel
     }
 
     @Override
-    @Transactional(rollbackFor = Exception.class)
-    @GlobalTransactional(rollbackFor = Exception.class)
     public BatchResultDTO interceptResultConfirm(SoB2cDeliveryInterceptDTO.InterceptResultConfirmDTO dto, String id) {
         SoB2cDeliveryInterceptEntity entity = this.getById(id);
         if (ObjectUtil.isEmpty(entity)) {
@@ -369,7 +367,7 @@ public class SoB2cDeliveryInterceptServiceImpl extends SuperServiceImpl<SoB2cDel
                 BaseIdsDTO.IdsDTO approveIdDto = new BaseIdsDTO.IdsDTO();
                 approveIdDto.setIds(approveIds);
                 if (CollectionUtils.isNotEmpty(approveIds)) {
-                    soOutstockService.disApprove(approveIdDto, Boolean.FALSE);
+                    soOutstockService.disApprove(approveIdDto, Boolean.TRUE);
                 }
 
                 //查询已提交的出库单，进行撤销
