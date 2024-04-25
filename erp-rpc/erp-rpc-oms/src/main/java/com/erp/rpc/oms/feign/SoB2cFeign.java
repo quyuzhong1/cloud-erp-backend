9b22739631f374dfe3e33a8793f0c9766a753afa
Jim <zhoujingbo@ulanzi.cn>
2024-04-25 11:34:36 +0800

    UPDATE:异常生成销售出库检查

--- erp-rpc/erp-rpc-oms/src/main/java/com/erp/rpc/oms/feign/SoB2cFeign.java
+++ erp-rpc/erp-rpc-oms/src/main/java/com/erp/rpc/oms/feign/SoB2cFeign.java
@@ -481,4 +481,11 @@ public interface SoB2cFeign {
      **/
     @PostMapping("/feign/soB2cError/deleteAndAddErrorBatch")
     void deleteAndAddErrorBatch(@RequestBody SoB2cErrorDTO.AddAndDeleteDTO addAndDeleteDTO);
+
+
+    /**
+     * 添加销售订单日志
+     */
+    @PostMapping("/feign/soB2c/addModuleOperateLog")
+    Boolean addModuleOperateLog(OperateLogDTO.AddModuleOperateLogDTO operateLogDTO);
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoB2cDeliveryService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoB2cDeliveryService.java
@@ -5,6 +5,7 @@ import com.common.business.dto.base.PagingDTO;
 import com.common.business.dto.base.PermissionsDTO;
 import com.common.business.service.SuperService;
 import com.common.business.vo.PagingVO;
+import com.erp.model.oms.entity.SoB2cEntity;
 import com.erp.model.wms.dto.SoB2cDeliveryDTO;
 import com.erp.model.wms.entity.SoB2cDeliveryEntity;
 
@@ -204,4 +205,11 @@ public interface SoB2cDeliveryService extends SuperService<SoB2cDeliveryEntity> 
      * @return java.lang.Boolean
      **/
     Boolean falseDeliveryBatch(List<String> ids);
+
+    /**
+     * 检查自发货订单的发货单状态
+     * true=无已发货的发货单
+     * false=有已发货的发货单
+     */
+    boolean hasNotShippedDeliveryAndLog(SoB2cEntity currentEntity);
 }
