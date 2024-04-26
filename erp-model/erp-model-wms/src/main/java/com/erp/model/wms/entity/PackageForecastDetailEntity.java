72bd87f823e4e7b9cee1025f899280ec1bf9aa9a
luoweigang <821969511@qq.com>
2024-04-26 14:13:51 +0800

    修改中转报关报错

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/PackageForecastDetailEntity.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/PackageForecastDetailEntity.java
@@ -83,18 +83,6 @@ public class PackageForecastDetailEntity extends BaseEntity<PackageForecastDetai
     @TableField("source_code")
     private String sourceCode;
 
-    /**
-     * 中转商渠道id
-     */
-    @TableField("transfer_logistics_channel_id")
-    private String transferLogisticsChannelId;
-
-    /**
-     * 中转商渠道名
-     */
-    @TableField("transfer_logistics_channel_name")
-    private String transferLogisticsChannelName;
-
 
     public static final String MAIN_ID = "main_id";
 
