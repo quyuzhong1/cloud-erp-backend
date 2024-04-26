a09b3ffa25cb3598e7d3330e0bb2f324d34d8a69
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-04-26 11:11:29 +0800

    提审采购入库单时，计算最大入库数量时，区分skuId对应的退货数量

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
@@ -401,6 +401,7 @@ public class PoInstockServiceImpl extends SuperServiceImpl<PoInstockMapper, PoIn
                     Integer receiveQty = receiveList.stream().map(WarehouseReceiveDetailEntity::getReceiveQty).reduce(MathUtil.ZERO, Integer::sum);
                     //已退货数量 根据收货单获取对应退货明细
                     Integer returnQty = returnDetailDTOS.stream().filter(e -> poInstockDetailEntity.getSourceDetailId().equals(e.getReceiveDetailId())
+                                    && poInstockDetailEntity.getSkuId().equals(e.getSkuId())
                                     && Objects.equals(e.getApproveStatus(), ApproveStatusEnum.APPROVE.getStatus()))
                             .map(WarehouseReceiveDTO.PoReturnDetailDTO::getReturnQty).reduce(MathUtil.ZERO,Integer::sum);
                     //最大入库数量
