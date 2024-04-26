c62be65b45fd3aa91136b9fb2ff9a1972a3d9c65
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-04-26 16:00:10 +0800

    合并冲突处理

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/feign/SoB2cFeignController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/feign/SoB2cFeignController.java
@@ -630,16 +630,4 @@ public class SoB2cFeignController extends BaseController {
                 operateLogDTO.getBusinessId(),
                 operateLogDTO.getOperation());
     }
-
-
-    /**
-     * 添加操作日志
-     */
-    @PostMapping("/addModuleOperateLog")
-    public Boolean addModuleOperateLog(OperateLogDTO.AddModuleOperateLogDTO operateLogDTO){
-        return operateLogService.addModuleOperateLog(operateLogDTO.getContent(),
-                operateLogDTO.getModuleType(),
-                operateLogDTO.getBusinessId(),
-                operateLogDTO.getOperation());
-    }
 }
