49c1aaa3a81a583534a441a96150f16eec28428b
Will <wangwei@ulanzi.cn>
2024-04-26 12:30:59 +0800

    fix,质检暂存、免检添加事务

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
@@ -902,6 +902,7 @@ public class QcInfoServiceImpl extends SuperServiceImpl<QcInfoMapper, QcInfoEnti
      * @date 2023-04-20 14:01
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Boolean draft(QcInfoDTO.SaveOrUpdateDTO dto) {
         //质检单
         QcInfoEntity bill = new QcInfoEntity();
@@ -961,6 +962,7 @@ public class QcInfoServiceImpl extends SuperServiceImpl<QcInfoMapper, QcInfoEnti
      * @date 2023-04-20 15:29
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public Boolean exemption(QcInfoDTO.SaveOrUpdateDTO dto) {
         String id = dto.getId();
         QcInfoEntity bill = this.getById(id);
