08fc0cbf4138e0ae7d38d91e2c27374bd1805645
luoweigang <821969511@qq.com>
2024-04-28 09:49:20 +0800

    修改推送保宏入库预报状态为确认

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cLogisticsEntity.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cLogisticsEntity.java
@@ -151,12 +151,6 @@ public class SoB2cLogisticsEntity extends BaseEntity<SoB2cLogisticsEntity> {
     @TableField("transfer_logistics_channel_id")
     private String transferLogisticsChannelId;
 
-    /**
-     * 物流类型
-     */
-    @TableField("logistic_type")
-    private String logisticType;
-
     public static final String MAIN_ID = "main_id";
 
     public static final String CODE = "code";
