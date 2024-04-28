afaddb734b16b8763224b94be45bb09de73dc26d
luoweigang <821969511@qq.com>
2024-04-28 16:08:01 +0800

    修改报关单查询

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/convert/PackageForecastConverter.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/convert/PackageForecastConverter.java
@@ -35,7 +35,6 @@ public interface PackageForecastConverter {
             @Mapping(target = "packageWeight", source = "weight"),
             @Mapping(target = "weightUnit", source = "weightUnit"),
             @Mapping(target = "weightUnit", source = "weightUnit"),
-            @Mapping(target = "orderUploadStatus", constant = ""),
     })
     TransferDeclareDetailDTO.AddDTO convertDeclareDetail(PackageForecastDetailEntity entity);
     List<TransferDeclareDetailDTO.AddDTO> convertDeclareDetail(List<PackageForecastDetailEntity> list);
