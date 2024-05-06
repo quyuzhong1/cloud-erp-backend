22b8b7f68642eda1f439288b307502624ae18f64
liuruipeng <arthur@ulanzi.cn>
2024-05-06 11:38:17 +0800

    feat: 速卖通发货详情实体类

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/AliexpressDeliveryDetailMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.wms.mapper;
+import com.erp.model.wms.entity.AliexpressDeliveryDetailEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 速卖通发货单详情 Mapper 接口
+ * </p>
+ *
+ * @author lrp
+ * @since 2024-05-06
+ */
+@Mapper
+public interface AliexpressDeliveryDetailMapper extends BaseMapper<AliexpressDeliveryDetailEntity> {
+
+}
