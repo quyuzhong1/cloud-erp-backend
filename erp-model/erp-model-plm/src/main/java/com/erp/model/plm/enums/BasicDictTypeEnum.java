d0d3131f18393bf1c372a04a9185614bf5b1e722
tanmujin <tanmujin@ulanzi.cn>
2024-05-06 09:31:15 +0800

    产品开发管理：增加”项目状态“，”立项状态“字典

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/enums/BasicDictTypeEnum.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/enums/BasicDictTypeEnum.java
@@ -50,7 +50,12 @@ public enum  BasicDictTypeEnum {
     /**
      * 项目状态
      */
-    PROJECT_STATE("projectState", "项目状态")
+    PROJECT_STATE("projectState", "项目状态"),
+
+    /**
+     * 立项状态
+     */
+    APPROVAL_STATUS("approvalStatus", "立项状态")
     ;
 
 
