d6b80eb5d2693d95ef280ec1e907940b22005468
luoweigang <821969511@qq.com>
2024-05-06 17:16:40 +0800

    修改产品导入校验1

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -4784,12 +4784,12 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
             //产品包装信息
             ProductPackDTO productPackDTO = new ProductPackDTO();
             BeanMapper.copy(dto, productPackDTO);
-            productPackDTO.setProductLength(LengthConverterUtil.cmToMm(new BigDecimal(dto.getProductLength())));
-            productPackDTO.setProductWidth(LengthConverterUtil.cmToMm(new BigDecimal(dto.getProductWidth())));
-            productPackDTO.setProductHeight(LengthConverterUtil.cmToMm(new BigDecimal(dto.getProductHeight())));
-            productPackDTO.setBoxLength(LengthConverterUtil.cmToMm(new BigDecimal(dto.getBoxLength())));
-            productPackDTO.setBoxWidth(LengthConverterUtil.cmToMm(new BigDecimal(dto.getBoxWidth())));
-            productPackDTO.setBoxHeight(LengthConverterUtil.cmToMm(new BigDecimal(dto.getBoxHeight())));
+            productPackDTO.setProductLength(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getProductLength())));
+            productPackDTO.setProductWidth(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getProductWidth())));
+            productPackDTO.setProductHeight(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getProductHeight())));
+            productPackDTO.setBoxLength(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getBoxLength())));
+            productPackDTO.setBoxWidth(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getBoxWidth())));
+            productPackDTO.setBoxHeight(LengthConverterUtil.cmToMm(MathUtil.valueOf(dto.getBoxHeight())));
             /**
              * 毛重
              */
