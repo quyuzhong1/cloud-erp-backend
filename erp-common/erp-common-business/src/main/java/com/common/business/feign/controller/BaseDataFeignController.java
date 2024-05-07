fce8096c32c9cecdad82f882ac58d2906373efa8
shukai <shukai@ulanzi.cn>
2024-05-07 11:57:54 +0800

    add:支持集合参数

--- erp-common/erp-common-business/src/main/java/com/common/business/feign/controller/BaseDataFeignController.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/feign/controller/BaseDataFeignController.java
@@ -88,11 +88,7 @@ public class BaseDataFeignController extends BaseController implements BaseDataF
 				Object [] paramVarArgs = new Object[param.size()];
 				Class<?>[] parameterTypes = invokeMethod.getParameterTypes();
 				for(int i = 0; i < parameterTypes.length; i++) {
-					if(parameterTypes[i].newInstance() instanceof Collection) {
-						paramVarArgs[i] = JSON.parseArray(JSON.toJSONString(param.get(i)), parameterTypes[i]);
-					}else {
-						paramVarArgs[i] = JSON.parseObject(JSON.toJSONString(param.get(i)), parameterTypes[i]);
-					}
+					paramVarArgs[i] = JSON.parseObject(JSON.toJSONString(param.get(i)), parameterTypes[i]);
 				}
 				result = invokeMethod.invoke(bean , paramVarArgs);
 			}else {
