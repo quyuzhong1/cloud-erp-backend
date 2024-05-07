46c1784c17f28863d5cb2097322250fcb01c4786
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-07 18:51:50 +0800

    V1.25 track123对接-无船公司字段支持查询，需要迭代优化4

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/entity/LogisticsChannelEntity.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/entity/LogisticsChannelEntity.java
@@ -69,10 +69,6 @@ public class LogisticsChannelEntity extends BaseEntity<LogisticsChannelEntity> {
     */
     @TableField("track_query_mode")
     private String trackQueryMode;
-    /**
-     * 船司/航司
-     */
-    private String carrier;
 
     @TableField("paper_size")
     private String paperSize;
