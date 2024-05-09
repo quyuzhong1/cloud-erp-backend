f4cdbed4626153db3fb743eb97bce3c5cda1407c
Will <wangwei@ulanzi.cn>
2024-05-09 14:45:19 +0800

    feat,订单处理规则条件下拉

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/CfgConditionService.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/CfgConditionService.java
@@ -1,7 +1,8 @@
 package com.erp.server.oms.service;
-import com.erp.model.oms.entity.CfgConditionEntity;
+
 import com.common.business.service.SuperService;
 import com.erp.model.oms.dto.CfgConditionDTO;
+import com.erp.model.oms.entity.CfgConditionEntity;
 
 import java.util.List;
 
@@ -72,4 +73,11 @@ public interface CfgConditionService extends SuperService<CfgConditionEntity> {
      *@date 2023-12-06
      */
     List<CfgConditionEntity> listByFields(List<String> fieldList);
+    /**
+     * @description: 订单处理的条件下拉
+     * @author Will
+     * @date: 2024/5/9 14:34
+     * @return List<ListDTO>
+     */
+    List<CfgConditionDTO.ListDTO> listOrderHandleCondition();
 }
