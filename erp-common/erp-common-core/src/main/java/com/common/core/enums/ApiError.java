ae80a4fb75173a531813a7eeb1d4f014c9131f9f
Will <wangwei@ulanzi.cn>
2024-05-09 11:59:57 +0800

    feat,订单处理规则

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -110,6 +110,7 @@ public enum ApiError implements Serializable {
     ERROR_BILL_APPROVE(1057,"{}单据审核失败"),
     LOGIN_ERROR(1058,"账户密码已输入错误5次，请在1小时后重试"),
     LOGIN_USER_ERROR(1059,"账号密码错误,还可尝试【{}】次,失败后将锁定1小时"),
+    ERROR_NAME_EXIST(1060,"名称【{}】已存在"),
 
     /**
      * 警告信息 从800 开始
