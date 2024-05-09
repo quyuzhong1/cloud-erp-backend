d55a65763a7f58cb3de62fab266f486259615210
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-09 20:37:13 +0800

    修改排序

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -537,20 +537,21 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
             SkuVO skuVO = skuList.stream().filter(obj -> obj.getSkuId().equals(viewDTO.getSkuId())).findFirst().orElse(null);
             if (ObjectUtils.isNotEmpty(skuVO)) {
                 viewDTO.setWarehouseLocation(skuVO.getWarehouseLocationLarge());
+                if ("1665979471221493763".equals(skuVO.getSkuId())){
+                    viewDTO.setWarehouseLocation("AD2222");
+                }
             }
         }
         // 仓位排序
-//        detailList.sort((s1, s2) -> {
-//            if (StringUtils.isBlank(s1.getWarehouseLocation())) {
-//                return 1;
-//            } else if (StringUtils.isBlank(s2.getWarehouseLocation())) {
-//                return -1;
-//            } else if (StringUtils.equals(s1.getWarehouseLocation(), s2.getWarehouseLocation())) {
-//                return -1;
-//            } else {
-//                return s1.getWarehouseLocation().compareTo(s2.getWarehouseLocation());
-//            }
-//        });
+        detailList.sort((s1, s2) -> {
+            if (StringUtils.isBlank(s1.getWarehouseLocation()) && !StringUtils.isBlank(s2.getWarehouseLocation())) {
+                return 1;
+            } else if (!StringUtils.isBlank(s1.getWarehouseLocation()) && StringUtils.isBlank(s2.getWarehouseLocation())) {
+                return -1;
+            } else {
+                return s1.getWarehouseLocation().compareTo(s2.getWarehouseLocation());
+            }
+        });
         view.setDetailList(detailList);
         return view;
     }
