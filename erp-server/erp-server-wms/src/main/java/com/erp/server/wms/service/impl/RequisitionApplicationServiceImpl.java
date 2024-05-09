6ee3d5587b0f14afac7b01bd58f21f786686a989
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-09 17:28:39 +0800

    添加等值排序

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
@@ -485,6 +485,8 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
                 return 1;
             } else if (s2.getWarehouseLocation().isEmpty()) {
                 return -1;
+            } else if (Objects.equals(s1.getWarehouseLocation(), s2.getWarehouseLocation())) {
+                return 0;
             }
             return s1.getWarehouseLocation().compareTo(s2.getWarehouseLocation());
         });
