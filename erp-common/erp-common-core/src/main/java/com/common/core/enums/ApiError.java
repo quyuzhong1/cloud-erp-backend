11dbb47e8998a3e75490313413c00c789c4e4775
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-10 09:10:04 +0800

    添加尺寸校验

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -1283,6 +1283,10 @@ public enum ApiError implements Serializable {
     ERROR_OUT_WAREHOUSELOCATION_NOT_FOUND(94101,"取货仓位不存在"),
     ERROR_IN_WAREHOUSELOCATION_NOT_FOUND(94102,"上架仓位不存在"),
 
+    ERROR_LENGTH_BOX_LITTER_THAN_PRODUCT(94201,"箱规长度必须大于包装长度"),
+    ERROR_WIDTH_BOX_LITTER_THAN_PRODUCT(94202,"箱规宽度必须大于包装宽度"),
+    ERROR_HEIGH_BOX_LITTER_THAN_PRODUCT(94203,"箱规高度必须大于包装高度"),
+
 
 
     // 亚马逊公用异常
