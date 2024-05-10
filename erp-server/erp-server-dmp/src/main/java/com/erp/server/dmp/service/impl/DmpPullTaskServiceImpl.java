9ac40f6c580544ec1759f4634a2eada7f36391e2
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-10 18:28:31 +0800

    修改为同步中和同步失败不能变更为无需修改

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpPullTaskServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpPullTaskServiceImpl.java
@@ -347,10 +347,12 @@ public class DmpPullTaskServiceImpl extends SuperServiceImpl<DmpPullTaskMapper, 
             throw new ServiceException(ApiError.ERROR_NOT_EXIST_KINGDEE_DATA);
         }
         List<DmpPullTaskEntity> noNeedSyncIds = list.stream().filter(obj ->
-                (!SyncStatusEnum.FAILED_SYNC.getCode().equals(obj.getStatus()) && !SyncStatusEnum.NO_NEED_SYNC.getCode().equals(obj.getStatus())))
+                (!SyncStatusEnum.IN_SYNC.getCode().equals(obj.getStatus()) && !SyncStatusEnum.NO_NEED_SYNC.getCode().equals(obj.getStatus())))
                 .collect(Collectors.toList());
         noNeedSyncIds.forEach(dmpPushTaskEntity -> dmpPushTaskEntity.setStatus(SyncStatusEnum.NO_NEED_SYNC.getCode()));
-        updateBatchById(list,500);
+        if (CollectionUtils.isNotEmpty(noNeedSyncIds)) {
+            updateBatchById(noNeedSyncIds, 500);
+        }
         return Boolean.TRUE;
     }
     /**
