8183b533883e02b6f5db689a4f2c29c1bb23e629
liaohui <liaohui@ulanzi.cn>
2024-05-10 09:12:54 +0800

    refactor: 优化统一获取当前登录接口位置

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/MergePackageDeliveryConsumer.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/MergePackageDeliveryConsumer.java
@@ -5,7 +5,7 @@ import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.core.util.StrUtil;
 import com.common.business.dto.base.BatchResultDTO;
-import com.common.business.utils.CollectionUtils;
+import com.common.business.threadlocal.UserContext;
 import com.common.core.exception.ServiceException;
 import com.common.message.constant.RocketMqConsumerGroup;
 import com.common.message.constant.RocketMqTopic;
@@ -26,11 +26,8 @@ import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.lang.reflect.Array;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -100,7 +97,7 @@ public class MergePackageDeliveryConsumer implements RocketMQListener<String> {
         //出库
         soB2cDeliveryService.generateB2cSoOutstock(deliveryEntities.get(0));
 
-        String msg = StrUtil.format("用户【{}】通过【{}】触发单据编号【{}】的自动发货功能", commonService.getUserInfo().getUserName(), "组包称重", deliveryEntity.getCode());
+        String msg = StrUtil.format("用户【{}】通过【{}】触发单据编号【{}】的自动发货功能", UserContext.getDefaultLoginUser().getUserName(), "组包称重", deliveryEntity.getCode());
         operateLogService.addModuleOperateLog(msg, ModuleTypeEnum.SO_B2C_DELIVERY.getCode(), deliveryEntity.getId(), "组包称重");
     }
 }
