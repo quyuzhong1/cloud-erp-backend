e3533e4d25e99a585de1915234e819698c872a5b
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-13 17:02:43 +0800

    V1.25 申报信息增加消息描述

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -7761,7 +7761,6 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
      */
     @Override
     public BatchResultDTO declareRule(String id, HashMap<String, Object> map, Boolean isUpdate) {
-        BatchResultDTO resultDTO = new BatchResultDTO();
         //已存在申报信息 则不进行规则匹配
         List<SoB2cDeclareProductEntity> declareProductList = soB2cDeclareProductService.listBySoId(id);
         if (Objects.nonNull(isUpdate) && !isUpdate && CollectionUtils.isNotEmpty(declareProductList)){
@@ -7795,8 +7794,7 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
         }
         //规则结果
         cfgRuleDeclareService.getRuleDeclareMatchResult(map, maxCustomsAmount, minCustomsAmount, isUpdate,declareProductList);
-        resultDTO.setSuccess(Boolean.TRUE);
-        return resultDTO;
+        return BatchResultDTO.success(id, entity.getCode(), OperationTypeEnum.DECLARE_RULE);
     }
 
     @Override
