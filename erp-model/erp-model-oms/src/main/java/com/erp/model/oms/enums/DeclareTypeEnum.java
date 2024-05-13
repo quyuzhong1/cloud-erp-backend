9b45530b9a5026f3464d8888ecd0caa6eadb350f
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-13 16:45:47 +0800

    V1.25 申报枚举修改2

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/DeclareTypeEnum.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/DeclareTypeEnum.java
@@ -1,13 +1,14 @@
 package com.erp.model.oms.enums;
 
 import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.common.core.constant.EnumMessage;
 import com.fasterxml.jackson.annotation.JsonValue;
 import org.apache.commons.lang3.StringUtils;
 
 /**
  * 申报类型
  */
-    public enum DeclareTypeEnum {
+    public enum DeclareTypeEnum implements EnumMessage {
 
     FIXED_PRICE("fixedPrice", "按固定价格申报"),
     PRICE_PERCENTAGE("pricePercentage", "按价格百分比申报"),
