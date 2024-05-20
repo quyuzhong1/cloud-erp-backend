e849d0e6848874b0148bd5b4c955052ba73ff326
luoweigang <821969511@qq.com>
2024-05-20 18:06:49 +0800

    修改清洗TikTok订单信息

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/rocketmq/consumer/PlatformListingConsumerService.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/rocketmq/consumer/PlatformListingConsumerService.java
@@ -109,7 +109,7 @@ public class PlatformListingConsumerService<T extends DmpSyncTaskIdDTO> extends 
                         PlatformDictEnum.MERCADOLIBRE.getCode().equalsIgnoreCase(dto.getPlatform()) ||
                         PlatformDictEnum.TIK_TOK.getCode().equalsIgnoreCase(dto.getPlatform())){
                     paramDTO.setPlatformSpuNoList(Collections.singletonList(dto.getPlatformProductNo()));
-                    paramDTO.setPlatformSkuIdList(Collections.singletonList(dto.getPlatformSkuId()));
+                    paramDTO.setPlatformSkuIdList(StringUtils.isNotBlank(dto.getPlatformSkuId()) ? Collections.singletonList(dto.getPlatformSkuId()) : null);
                 }
                 paramDTO.setIsExpire(false);
                 List<ListingInfoWithSkuMappingDTO> listDto = skuMappingService.findListDto(paramDTO);
