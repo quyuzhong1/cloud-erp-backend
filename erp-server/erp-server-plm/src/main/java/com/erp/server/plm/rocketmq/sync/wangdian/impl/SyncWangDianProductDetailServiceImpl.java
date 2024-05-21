4b7d13edf2e05e063308a10fedccdda071af16d0
liaohui <liaohui@ulanzi.cn>
2024-05-21 09:45:56 +0800

    fix：合并冲突

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/sync/wangdian/impl/SyncWangDianProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/sync/wangdian/impl/SyncWangDianProductDetailServiceImpl.java
@@ -2,12 +2,12 @@ package com.erp.server.plm.rocketmq.sync.wangdian.impl;
 
 import cn.hutool.json.JSONUtil;
 import cn.wangdian.erp.sdk.api.goods.dto.GoodsBatchPushDTO;
-import com.common.business.dto.DmpPushTaskFeignDTO;
 import com.common.business.enums.SourceTypeEnum;
 import com.common.business.enums.SyncOperateEnum;
 import com.common.core.utils.LengthConverterUtil;
 import com.common.message.constant.RocketMqTopic;
 import com.common.message.enums.RocketMqTagEnum;
+import com.erp.model.dmp.entity.DmpPushTaskEntity;
 import com.erp.model.dmp.enums.PlatformEnum;
 import com.erp.model.plm.entity.ProductDetailEntity;
 import com.erp.model.plm.entity.ProductInfoEntity;
@@ -20,6 +20,8 @@ import com.erp.server.plm.service.ProductInfoService;
 import com.erp.server.plm.service.ProductPackService;
 import com.erp.server.plm.service.ProductPurchaseService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationAdapter;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -39,6 +41,7 @@ public class SyncWangDianProductDetailServiceImpl implements SyncWangDianProduct
     private ProductPackService productPackService;
     @Resource
     private DmpMqFeign dmpMqFeign;
+
     @Override
     public void syncDataToWangDian(ProductDetailEntity entity) {
         ProductInfoEntity info = productInfoService.getById(entity.getProductId());
@@ -47,7 +50,7 @@ public class SyncWangDianProductDetailServiceImpl implements SyncWangDianProduct
         GoodsBatchPushDTO dto = new GoodsBatchPushDTO();
         dto.setGoodsNo(info.getSpuNo());
         dto.setGoodsName(info.getName());
-        dto.setGoodsType(getGoodsType(info.getSaleMethod(),info.getProperty()));
+        dto.setGoodsType(getGoodsType(info.getSaleMethod(), info.getProperty()));
         GoodsBatchPushDTO.SpecList specList = new GoodsBatchPushDTO.SpecList();
         specList.setSpecNo(entity.getSkuNo());
         specList.setBarcode(productPurchase.getEan());
@@ -62,31 +65,40 @@ public class SyncWangDianProductDetailServiceImpl implements SyncWangDianProduct
     }
 
 
-    private int getGoodsType(String saleMethod, String property){
+    private int getGoodsType(String saleMethod, String property) {
         SaleMethodEnum saleMethodEnum = SaleMethodEnum.getEnumByType(saleMethod);
-        if (ObjectUtils.isEmpty(saleMethodEnum)){
+        if (ObjectUtils.isEmpty(saleMethodEnum)) {
             return 0;
         }
-        switch (saleMethodEnum){
-            case GOODS:return (PRODUCT_PROPERTY_COST.equals(property) || PRODUCT_PROPERTY_SERVICE.equals(property)) ? 5 : 1;
-            case PACKAGING_MATERIALS: return 3;
-            case SEMI_FINISHED:return 2;
-            default: return 0;
+        switch (saleMethodEnum) {
+            case GOODS:
+                return (PRODUCT_PROPERTY_COST.equals(property) || PRODUCT_PROPERTY_SERVICE.equals(property)) ? 5 : 1;
+            case PACKAGING_MATERIALS:
+                return 3;
+            case SEMI_FINISHED:
+                return 2;
+            default:
+                return 0;
         }
     }
 
-    private void sendMqAndSaveTask (ProductDetailEntity entity, GoodsBatchPushDTO dto) {
+    private void sendMqAndSaveTask(ProductDetailEntity entity, GoodsBatchPushDTO dto) {
         //添加推送任务
-        DmpPushTaskFeignDTO taskFeignDTO = new DmpPushTaskFeignDTO();
-        taskFeignDTO.setSourceId(entity.getId());
-        taskFeignDTO.setSourceCode(entity.getSkuNo());
-        taskFeignDTO.setSourceType(SourceTypeEnum.PRODUCT_DETAIL.getCode());
-        taskFeignDTO.setMqTopic(RocketMqTopic.SYNC_WANGDIAN_ERP_TOPIC);
-        taskFeignDTO.setMqTag(RocketMqTagEnum.WANGDIAN_PRODUCT_DETAIL_TAG.getName());
-        taskFeignDTO.setMqData(JSONUtil.toJsonStr(dto));
-        taskFeignDTO.setSourcePlatformName(PlatformEnum.ERP.getDesc());
-        taskFeignDTO.setTargetPlatformName(PlatformEnum.WANGDIAN.getDesc());
-        taskFeignDTO.setSyncOperate(SyncOperateEnum.OPERATE_APPROVE.getCode());
-        dmpMqFeign.sendMqAndSaveTask(taskFeignDTO);
+        DmpPushTaskEntity taskEntity = new DmpPushTaskEntity();
+        taskEntity.setSourceId(entity.getId());
+        taskEntity.setSourceCode(entity.getSkuNo());
+        taskEntity.setSourceType(SourceTypeEnum.PRODUCT_DETAIL.getCode());
+        taskEntity.setMqTopic(RocketMqTopic.SYNC_WANGDIAN_ERP_TOPIC);
+        taskEntity.setMqTag(RocketMqTagEnum.WANGDIAN_PRODUCT_DETAIL_TAG.getName());
+        taskEntity.setMqData(JSONUtil.toJsonStr(dto));
+        taskEntity.setSourcePlatformName(PlatformEnum.ERP.getDesc());
+        taskEntity.setTargetPlatformName(PlatformEnum.WANGDIAN.getDesc());
+        taskEntity.setSyncOperate(SyncOperateEnum.OPERATE_APPROVE.getCode());
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
+            @Override
+            public void afterCommit() {
+                dmpMqFeign.sendTask(Collections.singletonList(taskEntity));
+            }
+        });
     }
 }
