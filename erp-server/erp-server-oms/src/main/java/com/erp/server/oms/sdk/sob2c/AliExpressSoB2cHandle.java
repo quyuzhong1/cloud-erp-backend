5eb9ff448637da71792e96201ca624c5d1deaf2e
Jim <zhoujingbo@ulanzi.cn>
2024-05-21 18:54:47 +0800

    BUG:事务异常预警

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/sob2c/AliExpressSoB2cHandle.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/sob2c/AliExpressSoB2cHandle.java
@@ -73,7 +73,6 @@ public class AliExpressSoB2cHandle implements ISoB2cHandleService {
     private SkuMappingFeign skuMappingFeign;
 
     @Override
-    @Transactional(rollbackFor = Exception.class)
     public Boolean handleRule(SoB2cEntity mainEntity) {
         //平台仓订单不走任何规则
         if (mainEntity.hasPlatformWarehouseOrder()) {
--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/sob2c/MercadoSoB2cHandle.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/sdk/sob2c/MercadoSoB2cHandle.java
@@ -33,7 +33,6 @@ public class MercadoSoB2cHandle implements ISoB2cHandleService {
     private SoB2cService soB2cService;
 
     @Override
-    @Transactional(rollbackFor = Exception.class)
     public Boolean handleRule(SoB2cEntity mainEntity) {
         //平台仓订单不走任何规则
         if (mainEntity.hasPlatformWarehouseOrder()) {
