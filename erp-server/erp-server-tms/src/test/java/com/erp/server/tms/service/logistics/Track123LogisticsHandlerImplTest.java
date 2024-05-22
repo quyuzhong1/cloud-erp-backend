3ef1d04d061d88e0c776725bba27af283ef343a1
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-22 14:17:24 +0800

    TMS 物流注册增加手机尾号注册2

--- erp-server/erp-server-tms/src/test/java/com/erp/server/tms/service/logistics/Track123LogisticsHandlerImplTest.java
+++ erp-server/erp-server-tms/src/test/java/com/erp/server/tms/service/logistics/Track123LogisticsHandlerImplTest.java
@@ -40,10 +40,10 @@ public class Track123LogisticsHandlerImplTest {
 
     public Track123LogisticsHandlerImplTest(){
         //test
-//        authMap.put("clientSecret","9fa500686633410a84ff0b00daed555e");
+        authMap.put("clientSecret","9fa500686633410a84ff0b00daed555e");
         //pro
-        authMap.put("clientId","Yg4Zf06w_sxZs3A5D");
-        authMap.put("clientSecret","579cf53f55694d89aef0887d81886aec");
+//        authMap.put("clientId","Yg4Zf06w_sxZs3A5D");
+//        authMap.put("clientSecret","579cf53f55694d89aef0887d81886aec");
     }
 
     public Map<String, String> getLogisticsAuthConfig(){
@@ -59,8 +59,8 @@ public class Track123LogisticsHandlerImplTest {
     public void registerLogisticsNumber(){
         List<LogisticsRegisterVO> registerVOS = new ArrayList<>();
         LogisticsRegisterVO vo = LogisticsRegisterVO.builder()
-                .trackNo("SF1694930851235")
-                .phoneSuffix("18855193495")
+                .trackNo("SF1693167040751")
+                .phoneSuffix("18770946068")
                 .build();
         registerVOS.add(vo);
 //        LogisticsRegisterVO vo1 = LogisticsRegisterVO.builder()
@@ -102,7 +102,7 @@ public class Track123LogisticsHandlerImplTest {
     public void getTrack(){
         LogisticsTrackVO logisticsQueryVO = new LogisticsTrackVO();
         List<String> trackNos = new ArrayList<>();
-        trackNos.add("SF1694930851235");
+        trackNos.add("SF1693167040751");
 //        trackNos.add("00369744292706509832");
         logisticsQueryVO.setTrackNos(trackNos);
         logisticsQueryVO.setAuthMap(authMap);
