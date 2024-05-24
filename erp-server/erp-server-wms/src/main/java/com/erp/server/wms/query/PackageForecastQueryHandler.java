3797707b0bfdc0e539108172c0acce9e485d1363
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-24 11:13:49 +0800

    WMS 组包预报单查询报错处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/PackageForecastQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/PackageForecastQueryHandler.java
@@ -39,7 +39,7 @@ public class PackageForecastQueryHandler extends AbstractQueryHandler {
             return super.getSplicingSQL();
         }
         if("platformNo".equals(field)){
-           return "and (pf.handover_no ILIKE concat('%',"+value+"::text,'%') " +
+           return " (pf.handover_no ILIKE concat('%',"+value+"::text,'%') " +
                    "OR pf.platform_package_no ILIKE concat('%',"+value+"::text,'%'))";
         }
         return null;
