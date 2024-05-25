0acb65777d9bc57ae017b826cbdb514336cb4d96
liaohui <liaohui@ulanzi.cn>
2024-05-25 17:18:51 +0800

    fix:删除多余代码

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/api/ThirdShopController.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/api/ThirdShopController.java
@@ -2,7 +2,6 @@ package com.erp.server.dmp.controller.api;
 
 
 import com.common.business.annotation.DataPermission;
-import com.common.business.dto.JobTaskDTO;
 import com.common.business.dto.base.BaseResultDTO;
 import com.common.business.dto.base.PagingDTO;
 import com.common.business.enums.DataAttributeEnum;
@@ -12,17 +11,16 @@ import com.common.core.anno.LogSystemModule;
 import com.common.core.controller.BaseController;
 import com.common.core.controller.vo.ApiResult;
 import com.common.core.enums.LogActionEnum;
-import com.common.core.utils.date.DateUtil;
 import com.erp.model.dmp.dto.ThirdShopDTO;
-import com.erp.server.dmp.pull.thread.PlatformDataThread;
 import com.erp.server.dmp.service.ThirdShopService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * 第三方系统店铺表
@@ -84,21 +82,5 @@ public class ThirdShopController extends BaseController {
     @PostMapping("/pagingSelect")
     public ApiResult<PagingVO<ThirdShopDTO.PageSelectDTO>> pagingSelect(@RequestBody @Validated PagingDTO<ThirdShopDTO.SelectDTO> dto) {
         return success(thirdShopService.pagingSelect(dto));
-    }
-
-    @Resource
-    private PlatformDataThread platformDataThread;
-    @GetMapping("/test")
-    public ApiResult<String> test() {
-        JobTaskDTO dto = new JobTaskDTO();
-        dto.setNextTime(LocalDateTime.parse("2024-05-25 18:00:00", DateTimeFormatter.ofPattern(DateUtil.fmt)));
-        dto.setLastTime(LocalDateTime.parse("2024-05-25 09:15:00", DateTimeFormatter.ofPattern(DateUtil.fmt)));
-        dto.setRetryTimes(3600);
-        dto.setApiCode("wdt.wms.stockin.Refund.queryWithDetail");
-        dto.setBillType("wdt_return_order");
-        dto.setPlatformCategory("third_system");
-        dto.setDictPlatform("wdt");
-        platformDataThread.pullOrderSync(dto);
-        return ApiResult.success();
     }
 }
