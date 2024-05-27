da1b65eb15e19e2e1f8e792d65c9d6645707a037
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-27 10:11:41 +0800

    添加第一个仓库显示

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveServiceImpl.java
@@ -750,6 +750,10 @@ public class WarehouseLocationMoveServiceImpl extends SuperServiceImpl<Warehouse
         }
 
         data.setDetailList(detailList);
+        if (StringUtils.isBlank(data.getWarehouseId()) && CollectionUtils.isNotEmpty(detailList)) {
+            data.setWarehouseId(detailList.get(0).getWarehouseId());
+            data.setWarehouseName(detailList.get(0).getWarehouseName());
+        }
         return data;
     }
     @Override
@@ -910,6 +914,10 @@ public class WarehouseLocationMoveServiceImpl extends SuperServiceImpl<Warehouse
                         StringUtils.isBlank(data.getWarehouseId()) ? pdaItemDTO.getWarehouseId() : data.getWarehouseId(), pdaItemDTO.getInWarehouseLocation()).getName());
             }
             data.setItemList(itemDTOList);
+            if (StringUtils.isBlank(data.getWarehouseId()) && CollectionUtils.isNotEmpty(itemDTOList)) {
+                data.setWarehouseId(itemDTOList.get(0).getWarehouseId());
+                data.setWarehouseName(itemDTOList.get(0).getWarehouseName());
+            }
         }
     }
 
