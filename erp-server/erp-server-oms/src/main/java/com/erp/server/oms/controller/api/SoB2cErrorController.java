5ec1709313e025abe9dc2215b5890409657fd41c
Jim <zhoujingbo@ulanzi.cn>
2024-05-28 16:44:52 +0800

    UPDATE:标记发货优化-移动

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoB2cErrorController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoB2cErrorController.java
@@ -1,24 +1,23 @@
 package com.erp.server.oms.controller.api;
 
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import javax.annotation.Resource;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-import com.common.core.anno.LogAction;
+import cn.hutool.core.util.StrUtil;
+import com.common.business.dto.base.BaseIdsDTO;
+import com.common.business.dto.base.BatchResultDTO;
 import com.common.core.anno.LogSystemModule;
-import com.common.core.anno.LogViewService;
-import com.common.core.enums.LogActionEnum;
-import com.common.business.dto.base.*;
+import com.common.core.controller.BaseController;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.oms.dto.SoB2cErrorDTO;
+import com.erp.server.oms.service.SoB2cErrorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.common.core.controller.BaseController;
-import com.erp.server.oms.service.SoB2cErrorService;
-import com.common.core.controller.vo.ApiResult;
-import com.common.business.annotation.DataPermission;
-import com.common.business.enums.DataAttributeEnum;
-import com.erp.model.oms.dto.SoB2cErrorDTO;
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * B2C销售订单异常信息
@@ -46,6 +45,27 @@ public class SoB2cErrorController extends BaseController {
         SoB2cErrorDTO.ViewDTO result=soB2cErrorService.info(dto);
         return success(result);
     }
+
+    /**
+     * 订单标记发货失败后再次触发 ids 为销售订单id
+     *
+     */
+    @PostMapping("/retryFalseDelivery")
+    public ApiResult<List<BatchResultDTO>> retryDelivery(@RequestBody BaseIdsDTO.IdsDTO dto) {
+        List<BatchResultDTO> resultDTOS = new ArrayList<>(dto.getIds().size());
+        //id 为销售订单id
+        for (String id : dto.getIds()) {
+            try {
+                BatchResultDTO resultDTO = soB2cErrorService.retryFalseDelivery(id);
+                resultDTOS.add(resultDTO);
+            } catch (Exception e) {
+                log.error("重新标记发货失败", e);
+                resultDTOS.add(BatchResultDTO.fail(id, id, StrUtil.format("标记发货失败：{}", e.getMessage())));
+            }
+        }
+        return resultDTOS.stream().allMatch(BatchResultDTO::getSuccess) ? success(resultDTOS) : failure(resultDTOS);
+    }
+
 
 
 
