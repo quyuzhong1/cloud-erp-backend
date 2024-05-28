7c4f5d1c2c2321498ebf35ef60bb4f0134e25375
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-05-28 12:33:33 +0800

    修改第三方信息返回

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/ThirdMappingServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/ThirdMappingServiceImpl.java
@@ -249,7 +249,7 @@ public class ThirdMappingServiceImpl extends SuperServiceImpl<ThirdMappingMapper
                 viewDTOList.add(viewDTO);
             });
             mappingViewDTO.setOrgId(listDTOS.get(0).getOrgId());
-            mappingViewDTO.setOrgName(listDTOS.get(0).getName());
+            mappingViewDTO.setOrgName(listDTOS.get(0).getOrgName());
             mappingViewDTO.setSysName(listDTOS.get(0).getName());
             mappingViewDTO.setSysId(listDTOS.get(0).getId());
             mappingViewDTO.setThirdList(viewDTOList);
