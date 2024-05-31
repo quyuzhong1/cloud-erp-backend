c49bfacc778a89e6c96b6fead5b1a8937e3ff890
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-05-31 15:21:42 +0800

    WMS 采购入库单提审增加字段判空2

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PoInstockServiceImpl.java
@@ -402,9 +402,7 @@ public class PoInstockServiceImpl extends SuperServiceImpl<PoInstockMapper, PoIn
                     Integer receiveQty = receiveList.stream().map(WarehouseReceiveDetailEntity::getReceiveQty).reduce(MathUtil.ZERO, Integer::sum);
                     //已退货数量 根据收货单获取对应退货明细
                     Integer returnQty = returnDetailDTOS.stream().filter(e -> poInstockDetailEntity.getSourceDetailId().equals(e.getReceiveDetailId())
-                                    && Objects.nonNull(e.getPurchaseOrderDetailId()) && StringUtils.isNotBlank(e.getSkuId())
-                                    && StringUtils.isNotBlank(e.getApproveStatus())
-                                    && e.getPurchaseOrderDetailId().equals(poInstockDetailEntity.getPurchaseOrderDetailId())
+                                    && poInstockDetailEntity.getPurchaseOrderDetailId().equals(e.getPurchaseOrderDetailId())
                                     && poInstockDetailEntity.getSkuId().equals(e.getSkuId())
                                     && Objects.equals(e.getApproveStatus(), ApproveStatusEnum.APPROVE.getStatus()))
                             .map(WarehouseReceiveDTO.PoReturnDetailDTO::getReturnQty).reduce(MathUtil.ZERO,Integer::sum);
