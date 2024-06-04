5e75848fe26da1fa6fcc33ae9d53b72115b4a43d
Jim <zhoujingbo@ulanzi.cn>
2024-06-04 19:54:30 +0800

    UPDATE:并发消费

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformSoOutStockConsumerService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformSoOutStockConsumerService.java
@@ -50,7 +50,9 @@ import java.util.stream.Collectors;
 @RocketMQMessageListener(topic = RocketMqTopic.PLATFORM_PULL_DATA_TOPIC,
         selectorExpression = "third_system_so_out_stock_tag",
         consumerGroup = "${spring.cloud.nacos.discovery.namespace}-platform_pull_so_out_stock_consumer",
-        consumeMode = ConsumeMode.ORDERLY)
+        consumeMode = ConsumeMode.CONCURRENTLY,
+        consumeThreadNumber = 5
+)
 public class PlatformSoOutStockConsumerService<T extends DmpSyncTaskIdDTO> extends AbstractPlatformConsumerHandler<T> {
 
     @Resource
