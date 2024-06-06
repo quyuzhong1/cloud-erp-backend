f75f204fb0f3eca8530d5fe8d3f9bc52435b881c
hanyuejiao <hanyuejiao@ulanzi.cn>
2024-06-06 09:14:17 +0800

    修改报错

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
@@ -1449,7 +1449,7 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
             }
             //只有禁用的店铺允许删除
             if (Objects.equals(shopInfoEntity.getDisabled(), false)) {
-                deleteResult = BatchResultDTO.fail(id, id, ApiError.ERROR_SHOP_UNDISABLED.msg);
+                deleteResult = BatchResultDTO.fail(id, shopInfoEntity.getAccount(), ApiError.ERROR_SHOP_UNDISABLED.msg);
                 resultDTOS.add(deleteResult);
                 continue;
             }
