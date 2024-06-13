88613c65ef90d0096e5c63e297d307e12c3887bd
liuruipeng <arthur@ulanzi.cn>
2024-06-13 19:10:22 +0800

    feat:迭代1.27.4 订单发货信息接口

---
+++ erp-rpc/erp-rpc-oms/src/main/java/com/erp/rpc/oms/feign/SoB2cForeignFeign.java
@@ -0,0 +1,21 @@
+package com.erp.rpc.oms.feign;
+
+import com.common.business.dto.base.PagingDTO;
+import com.common.business.vo.PagingVO;
+import com.erp.model.oms.dto.SoB2cForeignDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name = "erp-oms", contextId = "soB2c")
+public interface SoB2cForeignFeign {
+
+    /**
+     * 根据b2c订单id获取物流信息
+     */
+    @PostMapping("/foreign/feign/soB2c/getOrderDeliveryInfo")
+    PagingVO<List<SoB2cForeignDTO.OrderDeliveryResp>> getOrderDeliveryInfo(@RequestBody PagingDTO<SoB2cForeignDTO.OrderDeliveryReq> orderDeliveryReq);
+
+}
