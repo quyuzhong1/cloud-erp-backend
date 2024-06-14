1d47338eba45946b8d1e5f0bba59299e6d1811f2
Will <wangwei@ulanzi.cn>
2024-06-14 10:18:37 +0800

    feat，虚拟库存调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/VirtualTransFlowQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/VirtualTransFlowQueryHandler.java
@@ -39,18 +39,18 @@ public class VirtualTransFlowQueryHandler extends AbstractQueryHandler {
         }
         // 昨天
         if ("yesterday".equals(value)) {
-            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.GE,  LocalDateUtil.formatTime(dateTime.minusDays(1L), DateUtil.fmt_day), QueryDataTypeEnum.DATE);
-            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LT, date, QueryDataTypeEnum.DATE);
+            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.GT,  LocalDateUtil.formatTime(dateTime.minusDays(1L), DateUtil.fmt_day), QueryDataTypeEnum.DATE);
+            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LE, date, QueryDataTypeEnum.DATE);
         }
         // 近七天
         if ("week".equals(value)) {
-            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.GE,  LocalDateUtil.formatTime(dateTime.minusDays(7L), DateUtil.fmt_day), QueryDataTypeEnum.DATE);
-            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LT, date, QueryDataTypeEnum.DATE);
+            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.GT,  LocalDateUtil.formatTime(dateTime.minusDays(7L), DateUtil.fmt_day), QueryDataTypeEnum.DATE);
+            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LE, date, QueryDataTypeEnum.DATE);
         }
         //近一个月
         if ("month".equals(value)) {
             super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.GT,  LocalDateUtil.formatTime(dateTime.minusDays(30L), DateUtil.fmt_day), QueryDataTypeEnum.DATE);
-            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LT, date, QueryDataTypeEnum.DATE);
+            super.buildSplicingSQLDTO("vtf.trade_time", QueryConditionEnum.LE, date, QueryDataTypeEnum.DATE);
         }
         return super.getSplicingSQL();
     }
