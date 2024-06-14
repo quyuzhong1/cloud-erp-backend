813dcac481ed850a63c409b2621eec1d74457c0b
Will <wangwei@ulanzi.cn>
2024-06-14 11:53:47 +0800

    feat，虚拟库存调整

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/VirtualWarehouseChannelDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/VirtualWarehouseChannelDTO.java
@@ -212,7 +212,7 @@ public class VirtualWarehouseChannelDTO implements Serializable {
          * 关联id（如店铺id）,无关联id时传空字符
          */
         @NotNull(message = "关联id不能为null")
-        private String relationId;
+        private String relationId = null;
 
         /**
          * 平台
