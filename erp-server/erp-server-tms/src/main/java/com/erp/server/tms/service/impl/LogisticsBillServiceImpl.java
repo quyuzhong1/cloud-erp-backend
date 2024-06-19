4e11423007dc63982fc855732ef14cfb50ade15a
liuruipeng <arthur@ulanzi.cn>
2024-06-19 15:21:22 +0800

    feat:物流商更新重量新增日志

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillServiceImpl.java
@@ -1073,6 +1073,7 @@ public class LogisticsBillServiceImpl extends SuperServiceImpl<LogisticsBillMapp
         //平台
         String logisticsPlatform = auth.getLogisticsPlatform();
         LogisticsService service = logisticsRegistry.getHandler(logisticsPlatform);
+        log.info("物流商更新重量:{}",JSONUtil.toJsonStr(logisticsUpdateWeightVO));
         return service.updateWeight(logisticsUpdateWeightVO);
     }
 
