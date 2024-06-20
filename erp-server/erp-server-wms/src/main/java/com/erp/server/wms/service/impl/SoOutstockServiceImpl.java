eceeef92ed0e7e8066af1f547d1174276418617c
liaohui <liaohui@ulanzi.cn>
2024-06-20 17:37:56 +0800

    fix: 填充仓库明细id

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
@@ -1586,6 +1586,7 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
                     detail.setSourceDetailId(item.getSourceDetailId());
                     detail.setSkuId(item.getSkuId());
                     detail.setRemark(item.getRemark());
+                    detail.setWarehouseId(item.getWarehouseId());
                     detail.setWarehouseLocation(item.getWarehouseLocation());
                     detail.setActualQty(item.getQty());
                     detail.setPlanQty(item.getQty());
