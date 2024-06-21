80148938395c46116182f1fa3ad594f9d7ad010d
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-06-21 16:26:47 +0800

    飞书增加预警信息定时推送管理-直接删除已推送消息

--- erp-server/erp-server-msg/src/main/java/com/erp/server/msg/schedule/FeiShuMsgJob.java
+++ erp-server/erp-server-msg/src/main/java/com/erp/server/msg/schedule/FeiShuMsgJob.java
@@ -73,7 +73,7 @@ public class FeiShuMsgJob {
 //         设置需要更新的字段和值
 //        update.set("isSend", MathUtil.TWO);
 //        UpdateResult result = mongoTemplate.updateMulti(query,update,WarnMsgInfoDTO.class,MongoTableConstant.FEISHU_WARN_MSG);
-        mongoTemplate.findAndRemove(query,WarnMsgInfoDTO.class,MongoTableConstant.FEISHU_WARN_MSG);
+        mongoTemplate.remove(query,WarnMsgInfoDTO.class,MongoTableConstant.FEISHU_WARN_MSG);
 //        XxlJobHelper.log("批量发送飞书预警消息结果:{}", result.getMatchedCount());
         try {
             XxlJobHelper.log("发送飞书预警消息休眠 start:{}", System.currentTimeMillis());
