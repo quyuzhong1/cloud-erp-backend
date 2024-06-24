3c1e17a1a47a23ef29872074614f833f52aa0a64
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-06-24 10:53:59 +0800

    备货申请单事务拆分 审核/反审核2

--- erp-rpc/erp-rpc-scm/src/main/java/com/erp/rpc/wms/feign/ScmTaskFeign.java
+++ erp-rpc/erp-rpc-scm/src/main/java/com/erp/rpc/wms/feign/ScmTaskFeign.java
@@ -5,6 +5,7 @@ import com.common.business.dto.DmpSyncMqDTO;
 import com.common.business.dto.base.ApproveOneDTO;
 import com.common.business.dto.base.BaseApproveParamDTO;
 import com.common.business.dto.base.BaseDropDownDTO;
+import com.common.business.dto.base.BatchResultDTO;
 import com.erp.model.scm.dto.PurchaseOrderDTO;
 import com.erp.model.scm.dto.PurchasePriceDTO;
 import com.erp.model.scm.dto.SkuCostDTO;
@@ -216,7 +217,7 @@ public interface ScmTaskFeign {
      * @return java.lang.Boolean
      **/
     @PostMapping("feign/scmWorkOption/salesDemandApprove")
-    Boolean salesDemandApprove(@RequestBody @Validated BaseApproveParamDTO dto);
+    List<BatchResultDTO> salesDemandApprove(@RequestBody @Validated BaseApproveParamDTO dto);
 
     /**
      * 审核
@@ -266,7 +267,7 @@ public interface ScmTaskFeign {
      * @return java.lang.Boolean
      **/
     @PostMapping("feign/scmWorkOption/purchaseApplicationApprove")
-    Boolean purchaseApplicationApprove(@RequestBody @Validated BaseApproveParamDTO dto);
+    List<BatchResultDTO> purchaseApplicationApprove(@RequestBody @Validated BaseApproveParamDTO dto);
 
     /**
      * 根据来源id集合获取到下推数据
--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/feign/ScmWorkOptionFeignController.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/feign/ScmWorkOptionFeignController.java
@@ -2,8 +2,12 @@ package com.erp.server.scm.controller.feign;
 
 import com.common.business.dto.base.ApproveOneDTO;
 import com.common.business.dto.base.BaseApproveParamDTO;
+import com.common.business.dto.base.BatchResultDTO;
+import com.erp.model.scm.entity.PurchaseApplicationEntity;
+import com.erp.model.scm.entity.SalesDemandEntity;
 import com.erp.model.workflow.dto.WorkOptionDTO;
 import com.erp.server.scm.service.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,7 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 采购订单feign
@@ -19,6 +25,7 @@ import java.util.List;
  * @Author Luo_WG
  * @Date 2023/4/13 11:11
  **/
+@Slf4j
 @RestController
 @RequestMapping("feign/scmWorkOption")
 public class ScmWorkOptionFeignController {
@@ -61,8 +68,23 @@ public class ScmWorkOptionFeignController {
      * @return java.lang.Boolean
      **/
     @PostMapping("/salesDemandApprove")
-    public Boolean salesDemandApprove(@RequestBody @Validated BaseApproveParamDTO dto) {
-        return salesDemandService.approve(dto);
+    public List<BatchResultDTO> salesDemandApprove(@RequestBody @Validated BaseApproveParamDTO dto) {
+        List<BatchResultDTO> resultDTOS = new ArrayList<>(dto.getIds().size());
+        List<SalesDemandEntity> entityList = salesDemandService.listByIds(dto.getIds());
+        for (String id : dto.getIds()) {
+            SalesDemandEntity entity = entityList.stream().filter(v->v.getId().equals(id)).findFirst().orElse(null);
+            if(Objects.isNull(entity)){
+                resultDTOS.add(BatchResultDTO.fail(id,id,"备货申请单不存在"));
+                continue;
+            }
+            try {
+                resultDTOS.add(salesDemandService.approve(entity,dto.getType(),dto.getComment(),dto.getIsNeedProcess()));
+            }catch (Exception e){
+                log.error("备货申请单审核失败",e);
+                resultDTOS.add(BatchResultDTO.fail(entity.getId(), entity.getCode(), e.getMessage()));
+            }
+        }
+        return resultDTOS;
     }
 
     /**
@@ -122,7 +144,22 @@ public class ScmWorkOptionFeignController {
      * @return java.lang.Boolean
      **/
     @PostMapping("/purchaseApplicationApprove")
-    public Boolean purchaseApplicationApprove(@RequestBody @Validated BaseApproveParamDTO dto) {
-        return purchaseApplicationService.approve(dto);
+    public List<BatchResultDTO> purchaseApplicationApprove(@RequestBody @Validated BaseApproveParamDTO dto) {
+        List<BatchResultDTO> resultDTOS = new ArrayList<>(dto.getIds().size());
+        List<PurchaseApplicationEntity> entityList = purchaseApplicationService.listByIds(dto.getIds());
+        for (String id : dto.getIds()) {
+            PurchaseApplicationEntity entity = entityList.stream().filter(v->v.getId().equals(id)).findFirst().orElse(null);
+            if(Objects.isNull(entity)){
+                resultDTOS.add(BatchResultDTO.fail(id,id,"采购申请单不存在"));
+                continue;
+            }
+            try {
+                resultDTOS.add(purchaseApplicationService.approve(entity,dto.getType(),dto.getComment(),dto.getIsNeedProcess()));
+            }catch (Exception e){
+                log.error("采购申请单审核失败",e);
+                resultDTOS.add(BatchResultDTO.fail(entity.getId(), entity.getCode(), e.getMessage()));
+            }
+        }
+        return resultDTOS;
     }
 }
