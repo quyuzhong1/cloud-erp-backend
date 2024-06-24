0f2f304fe0ed6d02a5db9a3d9c0d9269eeb91962
liuruipeng <arthur@ulanzi.cn>
2024-06-24 10:46:00 +0800

    refactor ：B2B客户列表审核，反审核接口调整

--- erp-common/erp-common-business/src/main/java/com/common/business/dto/base/BatchResultDTO.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/dto/base/BatchResultDTO.java
@@ -47,7 +47,9 @@ public class BatchResultDTO implements Serializable {
         code = null == code ? "" : code;
         return new BatchResultDTO(id, code, "成功", Boolean.TRUE);
     }
-
+    public static BatchResultDTO success() {
+        return new BatchResultDTO("","","",Boolean.TRUE);
+    }
     public static BatchResultDTO success(String id, String code, String msg) {
         code = null == code ? "" : code;
         return new BatchResultDTO(id, code, msg, Boolean.TRUE);
