2bf3948a4ccd4b41d7c1d3cf9191916af888ec1a
tanmujin <tanmujin@ulanzi.cn>
2024-06-26 14:50:59 +0800

    新增拣货波次Feign

---
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/PickingWaveDto.java
@@ -0,0 +1,42 @@
+package com.erp.model.wms.dto;
+/**
+ * 拣货波次
+ * @date 2024-06-26
+ * @author tanmujin
+ */
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+public class PickingWaveDto implements Serializable {
+    @Data
+    public static class AddDto{
+        /**
+         * 波次编码
+         */
+        private String code;
+
+        /**
+         * 发货单号集合
+         */
+        private List<String> deliveryIdList;
+
+        /**
+         * 分拣方式
+         */
+        private String pickingType;
+
+        /**
+         * 拣货车类型
+         */
+        private String pickCartType;
+
+        /**
+         * 波次类型
+         */
+        private String waveType;
+    }
+}
---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/PickingWaveFeignController.java
@@ -0,0 +1,24 @@
+package com.erp.server.wms.controller.feign;
+
+import com.common.business.dto.base.BatchResultDTO;
+import com.common.core.controller.vo.ApiResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.erp.model.wms.dto.PickingWaveDto;
+
+/**
+ * 拣货波次Feign
+ * @date 2024-06-26
+ * @author tanmujin
+ */
+@RestController
+@RequestMapping("/feign/pickingWave")
+public class PickingWaveFeignController {
+
+    @PostMapping("/add")
+    public ApiResult<BatchResultDTO> add(@RequestBody PickingWaveDto.AddDto addDto){
+        return null;
+    }
+}
