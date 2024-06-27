6e2ca5f62b66ed1b824b28bec4af23d1a7738770
liaohui <liaohui@ulanzi.cn>
2024-06-27 16:53:54 +0800

    fix：发货单数据

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/PickingWaveMapper.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/PickingWaveMapper.java
@@ -1,6 +1,7 @@
 package com.erp.server.wms.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.common.business.dto.base.PermissionsDTO;
 import com.erp.model.wms.dto.renovation.PickingWaveDTO;
 import com.erp.model.wms.entity.PickingWaveEntity;
 import org.apache.ibatis.annotations.Mapper;
@@ -13,4 +14,6 @@ public interface PickingWaveMapper extends BaseMapper<PickingWaveEntity> {
     List<String> listDeliveryIdByStatus(@Param("status") String status);
 
     List<PickingWaveDTO.PickingWaveDetailDTO> listDetailByMainId(@Param("waveId")String waveId,@Param("basketNo") String basketNo, @Param("skuId") String skuId);
+
+    int countDelivery(PermissionsDTO param);
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SoB2cDeliveryQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SoB2cDeliveryQueryHandler.java
@@ -6,6 +6,7 @@ import com.common.business.query.AbstractQueryHandler;
 import com.common.core.entity.BaseEntity;
 import com.erp.model.oms.entity.SoB2cEntity;
 import com.erp.model.wms.entity.SoB2cDeliveryInterceptEntity;
+import com.erp.model.wms.enums.PickingWaveStatusEnum;
 import com.erp.model.wms.enums.SoB2cDeliveryInterceptStatusEnum;
 import com.erp.model.wms.enums.SoB2cDeliveryStatusEnum;
 import com.erp.rpc.oms.feign.SoB2cFeign;
@@ -89,7 +90,7 @@ public class SoB2cDeliveryQueryHandler extends AbstractQueryHandler {
 
             if ("generation_waves".equals(searchType)) {
 
-                List<String> ids = pickingWaveService.listDeliveryIdByStatus("");
+                List<String> ids = pickingWaveService.listDeliveryIdByStatus(PickingWaveStatusEnum.AWAIT_PICK.getCode());
                 if (CollectionUtils.isEmpty(ids)) {
                     return getQueryEmptySql();
                 }
