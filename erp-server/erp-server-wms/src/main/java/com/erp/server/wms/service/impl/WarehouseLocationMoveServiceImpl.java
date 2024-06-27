452838b9f2c917df351aeeb329e04b29387274ff
liaohui <liaohui@ulanzi.cn>
2024-06-27 15:29:59 +0800

    fix: 合并冲突

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseLocationMoveServiceImpl.java
@@ -26,6 +26,7 @@ import com.common.core.excel.ExcelPrintUtils;
 import com.common.core.exception.ServiceException;
 import com.common.core.utils.*;
 import com.common.core.utils.date.DateUtil;
+import com.erp.model.dmp.dto.ThirdMappingDTO;
 import com.erp.model.dmp.entity.DmpPushTaskEntity;
 import com.erp.model.plm.vo.SkuVO;
 import com.erp.model.scm.enums.ModuleTypeEnum;
@@ -39,6 +40,7 @@ import com.erp.model.wms.entity.*;
 import com.erp.model.wms.enums.inventory.*;
 import com.erp.model.workflow.dto.ProcessManagementDTO;
 import com.erp.rpc.dmp.feign.DmpMqFeign;
+import com.erp.rpc.dmp.feign.DmpThirdMappingFeign;
 import com.erp.rpc.plm.feign.PlmTaskFeign;
 import com.erp.rpc.sys.feign.SysUserFeign;
 import com.erp.rpc.workflow.WorkflowFeign;
@@ -117,6 +119,8 @@ public class WarehouseLocationMoveServiceImpl extends SuperServiceImpl<Warehouse
 
     @Resource
     private SyncWdtOtherInStockService wdtOtherInStockService;
+    @Resource
+    private DmpThirdMappingFeign dmpThirdMappingFeign;
 
     @Transactional(rollbackFor = Exception.class)
     @Override
@@ -536,19 +540,33 @@ public class WarehouseLocationMoveServiceImpl extends SuperServiceImpl<Warehouse
     private void syncDisApproveInfoToWdt(WarehouseLocationMoveEntity entity,SyncOperateEnum operateCode) {
 
         List<WarehouseLocationMoveDetailEntity> detailEntityList = warehouseLocationMoveDetailService.listByMainIds(Arrays.asList(entity.getId()));
+
+        HashSet<String> warehouseIdSet = new HashSet<>();
+        for (WarehouseLocationMoveDetailEntity detail : detailEntityList) {
+            warehouseIdSet.add(detail.getWarehouseId());
+        }
+        //查询三方仓库映射
+        List<ThirdMappingDTO.WarehouseMappingDTO> mappingList = dmpThirdMappingFeign.listMappingBySysIds(new ArrayList<>(warehouseIdSet), "wdt");
+        if(mappingList.isEmpty()){
+            return;
+        }
+        Map<String, String> thirdWarehouseMap = mappingList.stream().collect(Collectors.toMap(item1 -> item1.getSysWarehouseId(), item2 -> item2.getThirdWarehouseCode()));
+
         //同步旺店通 审核{调入仓位做其他入库单，调出仓位做其他出库单}，反审核{调入仓位做其他出库单，调出仓位做其他入库单}
         List<DmpPushTaskEntity> dmpPushTaskEntityList = new ArrayList<>();
         for (WarehouseLocationMoveDetailEntity moveDetailEntity : detailEntityList) {
             //转换成出库单
+            if(! thirdWarehouseMap.containsKey(moveDetailEntity.getWarehouseId())){
+                continue;
+            }
             CreateOtherStockoutRequest.GoodsList outGoods = new CreateOtherStockoutRequest.GoodsList();
             outGoods.setSpecNo(moveDetailEntity.getSkuNo());
             outGoods.setNum(BigDecimal.valueOf(moveDetailEntity.getQty()));
             outGoods.setPositionNo(operateCode.equals(SyncOperateEnum.OPERATE_APPROVE)?moveDetailEntity.getOutWarehouseLocation():moveDetailEntity.getInWarehouseLocation());
 
             String outCode = docNoGenHelper.generateCode(BusinessNoTypeEnum.CODE_QTCK);
-            String outWarehouseId = moveDetailEntity.getWarehouseId();
-            OtherOutstockEntity outEntity = new OtherOutstockEntity(entity.getId(), outCode, outWarehouseId);
-            DmpPushTaskEntity outDmpPushTask = wdtOtherOutStockService.saveTask(Collections.singletonList(outGoods), outEntity, SyncOperateEnum.OPERATE_APPROVE.getCode(), entity.getCode(), moveDetailEntity.getId(), outCode, outWarehouseId);
+            String outWarehouseId = thirdWarehouseMap.get(moveDetailEntity.getWarehouseId());
+            DmpPushTaskEntity outDmpPushTask = wdtOtherOutStockService.saveTask(Collections.singletonList(outGoods), SyncOperateEnum.OPERATE_APPROVE.getCode(), entity.getCode(), moveDetailEntity.getId(), outCode, outWarehouseId, false);
             if(outDmpPushTask != null){
                 dmpPushTaskEntityList.add(outDmpPushTask);
             }
@@ -559,9 +577,8 @@ public class WarehouseLocationMoveServiceImpl extends SuperServiceImpl<Warehouse
             inGoods.setPositionNo(operateCode.equals(SyncOperateEnum.OPERATE_APPROVE)?moveDetailEntity.getInWarehouseLocation():moveDetailEntity.getOutWarehouseLocation());
 
             String inCode = docNoGenHelper.generateCode(BusinessNoTypeEnum.CODE_QTRK);
-            String inWarehouseId = moveDetailEntity.getWarehouseId();
-            OtherInstockEntity inEntity = new OtherInstockEntity(entity.getId(), inCode, inWarehouseId);
-            DmpPushTaskEntity inDmpPushTask = wdtOtherInStockService.saveTask(Collections.singletonList(inGoods), inEntity, SyncOperateEnum.OPERATE_APPROVE.getCode(), entity.getCode(), moveDetailEntity.getId(), inCode, inWarehouseId);
+            String inWarehouseId = thirdWarehouseMap.get(moveDetailEntity.getWarehouseId());
+            DmpPushTaskEntity inDmpPushTask = wdtOtherInStockService.saveTask(Collections.singletonList(inGoods), SyncOperateEnum.OPERATE_APPROVE.getCode(), entity.getCode(), moveDetailEntity.getId(), inCode, inWarehouseId, false);
             if(inDmpPushTask != null){
                 dmpPushTaskEntityList.add(inDmpPushTask);
             }
