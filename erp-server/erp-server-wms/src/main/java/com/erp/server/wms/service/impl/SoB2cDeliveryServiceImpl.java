5c70576acd306da23dca3b32520e01fd82799b0f
liaohui <liaohui@ulanzi.cn>
2024-06-28 15:29:19 +0800

    fix：修复bug

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
@@ -1447,6 +1447,7 @@ public class SoB2cDeliveryServiceImpl extends SuperServiceImpl<SoB2cDeliveryMapp
                     .filter(e -> e.getCode().equals(cancelShipment.getWarehouseLocation()))
                     .findFirst().orElse(new WarehouseLocationEntity());
             cancelShipment.setWarehouseLocationName(location.getName());
+            cancelShipment.setReturnWarehouseLocationId(location.getId());
             cancelShipment.setReturnWarehouseLocation(cancelShipment.getWarehouseLocation());
             cancelShipment.setReturnWarehouseLocationName(cancelShipment.getWarehouseLocationName());
             cancelShipment.setReturnQty(cancelShipment.getPickingQty());
