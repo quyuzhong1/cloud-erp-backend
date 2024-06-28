8d95522f52ff593dc58a477b0bfcd04c63c60fa3
cloud@ulanzi.cn <cloud@ulanzi.cn>
2024-06-28 17:01:26 +0800

    【 修改 mybaitisplus的雪花算法id生成规则，支持集群】
    (cherry picked from commit b0500dc3f2306fac357d5c0d42b263247f95674c)

--- erp-server/erp-server-bi/src/main/java/com/erp/server/bi/config/MybatisPlusConfig.java
+++ erp-server/erp-server-bi/src/main/java/com/erp/server/bi/config/MybatisPlusConfig.java
@@ -1,13 +1,23 @@
 package com.erp.server.bi.config;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.baomidou.mybatisplus.extension.MybatisMapWrapperFactory;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.common.business.constant.RedisCacheConstants;
+import com.common.business.utils.IdGeneratorUtil;
+import com.common.business.utils.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
 
 /**
  * @Classname MybatisPlusConfig
@@ -15,6 +25,7 @@ import org.springframework.context.annotation.Configuration;
  * @Date 2022-07-06 9:30
  * @Created by yl
  */
+@Slf4j
 @Configuration
 public class MybatisPlusConfig {
 
@@ -33,6 +44,16 @@ public class MybatisPlusConfig {
     @Bean
     public ConfigurationCustomizer mybatisConfigurationCustomizer() {
         return configuration -> configuration.setObjectWrapperFactory(new MybatisMapWrapperFactory());
+    }
+
+    /**
+     * 修改雪花算法生成方式
+     */
+    @Resource
+    private IdGeneratorUtil idGeneratorUtil;
+    @Bean
+    public IdentifierGenerator idGenerator() {
+        return idGeneratorUtil.idGenerator();
     }
 
 
