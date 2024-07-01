37715a3f9db9aa493786fd09b3454d90a449d45f
liaohui <liaohui@ulanzi.cn>
2024-07-01 10:07:48 +0800

    fix: 旺店通仓位

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
@@ -256,8 +256,6 @@ public class SyncB2CSoOutstockServiceImpl implements SyncB2CSoOutstockService {
                 }
             } else {
                 for (WdtSoOutStockDetailDTO.PositionDetailsList detail : detailDTO.getPositionDetailsList()) {
-                    //暂时使用空仓位
-                    detail.setPositionNo("");
                     SoOutstockDetailEntity detailEntity = BeanMapperUtils.map(SoOutstockDetailEntity.class, detailDTO);
                     detailEntity.setId(IdWorker.getIdStr());
                     String skuId = skuList.stream().filter(s -> s.getSkuNo().equals(detailEntity.getSkuNo())).
