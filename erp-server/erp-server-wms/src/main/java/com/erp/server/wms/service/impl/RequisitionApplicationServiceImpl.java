cd96f97374d81e34b5dc25c262c45bbaa1dfeb14
Will <wangwei@ulanzi.cn>
2024-07-02 11:51:40 +0800

    feat，要货申请撤销处理清空虚拟仓

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
@@ -668,6 +668,7 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public BatchResultDTO cancelProcess(String id) {
         RequisitionApplicationEntity entity = super.getByIdOpt(id).orElseThrow(() -> new ServiceException("未找到要货申请单数据"));
 /*        // 只有待处理的单据允许撤销
@@ -707,6 +708,8 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
         }
         log.info("撤销 开始修改要货申请状态，id：【{}】", id);
 
+        //清空明细中的虚拟仓
+        requisitionApplicationDetailService.cleanVirtualWarehouseIdByMianId(id);
 
         //操作日志
         log.info("撤销 开始记录操作日志，id：【{}】", id);
