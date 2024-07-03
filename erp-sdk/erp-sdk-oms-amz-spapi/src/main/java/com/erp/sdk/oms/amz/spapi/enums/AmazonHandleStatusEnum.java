89b58f78e150d759a664002ca6b196428e3d0d44
Jim <zhoujingbo@ulanzi.cn>
2024-07-03 11:48:19 +0800

    BUG:亚马逊记录异常配置数据

--- erp-sdk/erp-sdk-oms-amz-spapi/src/main/java/com/erp/sdk/oms/amz/spapi/enums/AmazonHandleStatusEnum.java
+++ erp-sdk/erp-sdk-oms-amz-spapi/src/main/java/com/erp/sdk/oms/amz/spapi/enums/AmazonHandleStatusEnum.java
@@ -15,6 +15,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AmazonHandleStatusEnum {
 
+    ERROR("-2",	"异常数据(无法匹配销售渠道或仓储中心)"),
     NONE("-1",	"无需处理(已有订单直接处理)"),
     WAIT_DOWNLOAD("0",	"待下载主订单信息(检查订单下载处理)"),
     WAIT_DOWNLOAD_DETAIL("1",	"待下载明细订单信息(主订单已下载)"),
