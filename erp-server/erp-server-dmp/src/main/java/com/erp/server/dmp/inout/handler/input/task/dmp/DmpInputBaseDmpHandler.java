432345a7eec6950c6a55c041f6be01669447828e
shukai <shukai@ulanzi.cn>
2024-07-03 09:11:17 +0800

    add:为空不添加

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputBaseDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputBaseDmpHandler.java
@@ -16,6 +16,8 @@ import com.erp.server.dmp.inout.dto.response.DmpInputMongoResponse;
 import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
 import com.erp.server.dmp.inout.handler.input.task.mongo.DmpInputBaseMongoHandler;
 
+import cn.hutool.core.collection.CollUtil;
+
 /**
  * dmp输入任务dmp基础处理器，被dmp任务状态执行器继承，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
  * @author Administrator
@@ -34,7 +36,9 @@ public class DmpInputBaseDmpHandler extends DmpInputDmpHandler{
 		List<Map<String, Object>> dataList = new ArrayList<>();
 		Collection<List<Map<String, Object>>> values = dmpResponse.getConvertInputMongoEntityListMaps().values();
 		for(List<Map<String, Object>> v : values) {
-			dataList.addAll(v);
+			if(CollUtil.isNotEmpty(v)) {
+				dataList.addAll(v);
+			}
 		}
 		return dataList;
 	}
