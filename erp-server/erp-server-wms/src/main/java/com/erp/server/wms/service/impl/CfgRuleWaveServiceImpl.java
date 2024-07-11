e0ab1c11f57a72a8fd5234dd833a7442e7494ca6
Will <wangwei@ulanzi.cn>
2024-07-11 12:21:19 +0800

    feat,生成波次拣货车类型传多个

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/CfgRuleWaveServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/CfgRuleWaveServiceImpl.java
@@ -364,6 +364,9 @@ public class CfgRuleWaveServiceImpl extends SuperServiceImpl<CfgRuleWaveMapper, 
         addDTO.setWaveType(entity.getWaveType());
         addDTO.setPickingType(entity.getPickingType());
         addDTO.setName(entity.getName());
+        //
+        List<String> pickingCartTypeIdList = Arrays.stream(JSONUtil.parseArray(entity.getPickingCartTypeJson()).stream().toArray(String[]::new)).collect(Collectors.toList());
+        addDTO.setPickCartTypeIdList(pickingCartTypeIdList);
         //需要更新发货单的异常状态
         List<String> updateDeliveryList = new ArrayList<>();
         //波次中的发货单
