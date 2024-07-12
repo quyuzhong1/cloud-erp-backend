33818093df13bd0882a826b2216ec689928d1503
liuruipeng <arthur@ulanzi.cn>
2024-07-12 19:14:58 +0800

    fix:打印配货单缺货优化

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoB2cDeliveryDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoB2cDeliveryDTO.java
@@ -1,5 +1,6 @@
 package com.erp.model.wms.dto;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.common.business.annotation.Dict;
 import com.common.business.dto.AdvanceQueryDTO;
 import com.common.business.dto.base.SortDTO;
@@ -595,6 +596,11 @@ public class SoB2cDeliveryDTO implements Serializable {
          * 备注
          */
         private String remark;
+        /**
+         * 是否缺货
+         */
+        private Boolean isOutStock;
+
 
         @Override
         public boolean equals(Object o) {
