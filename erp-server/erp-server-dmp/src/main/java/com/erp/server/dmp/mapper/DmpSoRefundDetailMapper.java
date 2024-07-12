97e6c6abe5900fb2aa00916e4e52e163ea2fe164
shukai <shukai@ulanzi.cn>
2024-07-12 10:17:58 +0800

    add:新加旺店通退款

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/mapper/DmpSoRefundDetailMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.dmp.mapper;
+import com.erp.model.dmp.entity.DmpSoRefundDetailEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 中台销售退款单明细表 Mapper 接口
+ * </p>
+ *
+ * @author shukai
+ * @since 2024-07-12
+ */
+@Mapper
+public interface DmpSoRefundDetailMapper extends BaseMapper<DmpSoRefundDetailEntity> {
+
+}
