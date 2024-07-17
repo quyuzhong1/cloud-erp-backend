788df620e7c4cf1c014f74d84dd486c20ed1faa9
liaohui <liaohui@ulanzi.cn>
2024-07-17 19:08:23 +0800

    fix: 修复bug

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoB2cDeliveryService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoB2cDeliveryService.java
@@ -266,8 +266,6 @@ public interface SoB2cDeliveryService extends SuperService<SoB2cDeliveryEntity> 
 
     BatchResultDTO clearException(String id);
 
-    BatchResultDTO cancelShipment(SoB2cDeliveryDTO.CancelShipmentDTO dto);
-
     SoB2cDeliveryDTO.CancelShipmentView cancelShipmentView(List<String> ids);
 
     /**
@@ -347,4 +345,24 @@ public interface SoB2cDeliveryService extends SuperService<SoB2cDeliveryEntity> 
      * @param results 前置规则返回的仓位
      */
     void generatePickingDetail(SoB2cDeliveryEntity soB2cDeliveryEntity, List<SoB2cDeliveryDetailEntity> soB2cDeliveryDetailEntities, List<LocationInventoryResultDTO> results);
+    /**
+     * 发货出库
+     * @author will
+     * @date 2024/7/11 10:42
+     * @param entity
+     * @return Boolean
+     */
+    Boolean deliveryOutStock(SoB2cDeliveryEntity entity);
+    /**
+     * 重新出库
+     * @author will
+     * @date 2024/7/12 15:47
+     * @param id
+     * @return BatchResultDTO
+     */
+    BatchResultDTO retryOutstock(String id);
+    /**
+     * 取消发货
+     */
+    BatchResultDTO cancelShipment(String id, List<SoB2cDeliveryDTO.CancelShipmentDTO> detail);
 }
