41e9e951e235edad432b42576dff3e6cd8ab4ff4
liuruipeng <arthur@ulanzi.cn>
2024-07-17 17:02:32 +0800

    fix:获取跟踪号，更新跟踪号新增乐观锁

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/GetLogisticsTrackNoTaskJob.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/GetLogisticsTrackNoTaskJob.java
@@ -85,11 +85,13 @@ public class GetLogisticsTrackNoTaskJob {
                     for (LogisticsOrderResponseVO item : resultList) {
                         String b2cLogisticsId = finalQueryList.stream().filter(f -> f.getTransportNo().equals(item.getTransportNo())).
                                 map(SoB2cLogisticsDTO.TrackNoDTO::getId).findFirst().orElse("");
+                        Integer version = finalQueryList.stream().filter(f -> f.getTransportNo().equals(item.getTransportNo())).
+                                map(SoB2cLogisticsDTO.TrackNoDTO::getVersion).findFirst().orElse(null);
                         if (StringUtils.isNotBlank(b2cLogisticsId)) {
                             List<String> trackNoList = new ArrayList<>(2);
                             SoB2cLogisticsEntity entity = new SoB2cLogisticsEntity();
                             entity.setId(b2cLogisticsId);
-
+                            entity.setVersion(version);
                             //跟踪单号
                             String trackNo = item.getTrackNo();
                             if (StringUtils.isBlank(trackNo) || "null".equals(trackNo)) {
