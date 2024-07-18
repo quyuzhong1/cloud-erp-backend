39a6ff8a0ca69ada825bfe6f20b3a5bda165874b
liaohui <liaohui@ulanzi.cn>
2024-07-18 21:48:59 +0800

    fix: 临时关闭要货申请仓位移动

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
@@ -174,7 +174,9 @@ public class PickingListsServiceImpl extends SuperServiceImpl<PickingListsMapper
         save(entity);
         operateLogService.addModuleOperateLog(CharSequenceUtil.format("生成拣货单【{}】", entity.getCode()), ModuleTypeEnum.PICKING_LISTS.getCode(), entity.getId(), "新增操作");
         pickingDetailService.saveBatch(entities);
-        warehouseLocationMoveService.addAndApprove(moveDto);
+        if (!SourceTypeEnum.REQUISITION_APPLICATION.getCode().equals(entity.getSourceType())){
+            warehouseLocationMoveService.addAndApprove(moveDto);
+        }
     }
 
     @Override
@@ -354,8 +356,10 @@ public class PickingListsServiceImpl extends SuperServiceImpl<PickingListsMapper
         updateById(entity);
         // 进行对应的仓位移动
         if (!CollectionUtils.isEmpty(addDTOS)) {
-            moveDto.setDetailList(addDTOS);
-            warehouseLocationMoveService.addAndApprove(moveDto);
+            if (!SourceTypeEnum.REQUISITION_APPLICATION.getCode().equals(entity.getSourceType())){
+                moveDto.setDetailList(addDTOS);
+                warehouseLocationMoveService.addAndApprove(moveDto);
+            }
         }
         List<String> sourceDetailIds = detailList.stream().map(PickingDetailEntity::getSourceDetailId).distinct().collect(Collectors.toList());
         if (SourceTypeEnum.REQUISITION_APPLICATION.getCode().equals(entity.getSourceType())) {
