6887395bb9115315c8a5e09ff21ac8964236b8e0
liaohui <liaohui@ulanzi.cn>
2024-07-18 19:47:46 +0800

    fix: 优化库存扣减逻辑

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/PickingListsController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/PickingListsController.java
@@ -109,4 +109,10 @@ public class PickingListsController extends BaseController {
     public void initPicking(){
         pickingListsService.init();
     }
+
+
+    @GetMapping("/initDelivery")
+    public void initDelivery(@RequestBody List<String> codes) {
+
+    }
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
@@ -760,14 +760,10 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
         List<InOutStockDTO> members = new ArrayList<>();
         InventoryInOutStockDTO inventoryInOutStockDTO = new InventoryInOutStockDTO();
         if (SourceTypeEnum.SO_B2C_DELIVERY.getCode().equals(entity.getSourceType())){
-            List<SoOutstockDetailEntity> soOutstockDetails = soOutstockDetailService.listByMainIds(Collections.singletonList(entity.getId()));
             inventoryInOutStockDTO.setBusinessType(InventoryBusinessTypeEnum.SO_OUTSTOCK.getCode());
             List<PickingListsDTO.SourceView> pickingLists = pickingListsService.listBySourceIds(Collections.singletonList(entity.getSourceId()));
             for (PickingListsDTO.SourceView detail : pickingLists) {
-                SoOutstockDetailEntity outstockDetail = soOutstockDetails.stream().filter(v -> v.getSourceDetailId().equals(detail.getSourceDetailId()))
-                        .findFirst()
-                        .orElse(new SoOutstockDetailEntity());
-                InOutStockDTO stockDTO = InOutStockDTO.getInOutStockDTO(entity, outstockDetail.getId(), detail.getSkuId(),detail.getSkuNo(),detail.getWarehouseLocation(),detail.getQty());
+                InOutStockDTO stockDTO = InOutStockDTO.getInOutStockDTO(entity, detail.getSourceDetailId(), detail.getSkuId(),detail.getSkuNo(),detail.getWarehouseLocation(),detail.getQty());
                 members.add(stockDTO);
             }
         }else {
