22a9dd0646d0b3e664a135a5e677f9299210f71e
Will <wangwei@ulanzi.cn>
2024-07-19 16:59:27 +0800

    fix,生成直接调拨单

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
@@ -1864,6 +1864,7 @@ public class SoB2cDeliveryServiceImpl extends SuperServiceImpl<SoB2cDeliveryMapp
             detailAddDTO.setInWarehouseId(inWarehouseEntity.getId());
             detailList.add(detailAddDTO);
         }
+        addDTO.setDetailList(detailList);
         String id = transferInfoService.addAndSubmit(addDTO);
         //审核
         BaseApproveParamDTO baseApproveParamDTO = new BaseApproveParamDTO();
