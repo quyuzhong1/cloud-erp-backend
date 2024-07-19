7cb720fa9f242bbe672e01b9f773d1488848c0d7
Will <wangwei@ulanzi.cn>
2024-07-19 14:01:31 +0800

    fix,同类波次调整

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/SoB2cDeliveryEntity.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/SoB2cDeliveryEntity.java
@@ -190,8 +190,18 @@ public class SoB2cDeliveryEntity extends BaseEntity<SoB2cDeliveryEntity> {
     @TableField("height")
     private BigDecimal height;
 
+    /**
+     * 发货标记类型
+     */
     @TableField("shipment_mark")
     private String shipmentMark;
+
+    /**
+     * 同类波次标识
+     */
+    @TableField(exist = false)
+    private String sameWaveStr;
+
 
     public static final String CODE = "code";
 
