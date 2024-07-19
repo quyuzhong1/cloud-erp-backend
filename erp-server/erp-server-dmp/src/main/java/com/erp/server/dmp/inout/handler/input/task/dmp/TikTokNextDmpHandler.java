85f8ef4dea55b0a734739d3a4252df32721e26fa
luoweigang <821969511@qq.com>
2024-07-19 16:37:28 +0800

    TikTok订单对接问题处理2

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/TikTokNextDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/TikTokNextDmpHandler.java
@@ -1,13 +1,13 @@
 package com.erp.server.dmp.inout.handler.input.task.dmp;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * dmp处理下一个扩展handler，如何订单收货人信息单独一张表，使用此handler即可，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
@@ -18,9 +18,26 @@ import java.util.TreeMap;
 @Scope("prototype")
 public class TikTokNextDmpHandler extends DmpInputDoNextDmpHandler{
     @Override
-    protected Map<List<Map<String, Object>>, List<TreeMap<String, Object>>> convertData(List<Map<String, Object>> dmpInputMongoEntityList) {
+    protected List<Map<String, Object>> getDetailList(Map<String, Object> dmpInputMongoEntity){
+
+        List<Map<String, Object>> detailList = super.getDetailList(dmpInputMongoEntity);
+        if(CollUtil.isNotEmpty(detailList)) {
+            for(Map<String, Object> detail : detailList) {
+
+                Object recipientAddress = detail.get("recipientAddress");
+                if (ObjectUtil.isNotEmpty(recipientAddress)) {
+                    Map<String, Object> recipientAddressMap = (Map<String, Object>) recipientAddress;
+                    detail.put("buyerName", recipientAddressMap.get("name"));
+
+                    List<Map<String, Object>> districtInfoList = (List<Map<String, Object>>) recipientAddressMap.get("districtInfo");
 
 
-        return super.convertData(dmpInputMongoEntityList);
+                }
+
+
+            }
+        }
+
+        return detailList;
     }
 }
