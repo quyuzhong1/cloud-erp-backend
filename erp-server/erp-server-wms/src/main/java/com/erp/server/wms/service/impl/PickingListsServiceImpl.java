bc5928eaf23b30e8ff31f3f3ddedffc6b33be807
liaohui <liaohui@ulanzi.cn>
2024-07-22 15:31:32 +0800

    fix: 过滤运费和费用属性

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
@@ -369,7 +369,9 @@ public class PickingListsServiceImpl extends SuperServiceImpl<PickingListsMapper
                     .distinct().findFirst().orElse(new SkuVO());
             PickingListsDTO.PrintView view = new PickingListsDTO.PrintView();
             view.getPrintView(entity, detail, skuVO.getSkuName());
-            view.setWarehouseLocation(skuVO.getWarehouseLocationLarge());
+            if (ObjectUtil.isEmpty(view.getWarehouseLocation())) {
+                view.setWarehouseLocation(skuVO.getWarehouseLocationLarge());
+            }
             return view;
         }).collect(Collectors.toList());
         return new ArrayList<>(views.stream().collect(Collectors.groupingBy(v -> v.getSkuNo() + ":" + v.getWarehouseId() + ":" + v.getWarehouseLocation(),
