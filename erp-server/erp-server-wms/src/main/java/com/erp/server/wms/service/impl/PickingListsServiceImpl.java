5420deaa51dc7169c616b0b17c3a546f26cadcff
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-07-22 15:12:54 +0800

    拣货单打印，改为拣货记录来源单号

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PickingListsServiceImpl.java
@@ -336,23 +336,9 @@ public class PickingListsServiceImpl extends SuperServiceImpl<PickingListsMapper
         if (sourceCodes.size() > 1) {
             throw new ServiceException("来源单号不同不能同时打印");
         }
-        String sourceCode;
         PickingListsEntity pickingListsEntity = pickingLists.get(0);
-        //重置来源单号
-        if (SourceTypeEnum.SO_DELIVERY_NOTICE.getCode().equals(pickingListsEntity.getSourceType())) {
-            sourceCode = sourceCodes.get(0);
-        } else if (SourceTypeEnum.REQUISITION_APPLICATION.getCode().equals(pickingListsEntity.getSourceType())) {
-            //picking_lists.source_id = first_mile_delivery.source_id
-            List<FirstMileDeliveryEntity> firstMileDeliveryEntities = firstMileDeliveryService.listBySourceIds(Collections.singletonList(pickingListsEntity.getSourceId()));
-            if (!CollectionUtils.isEmpty(firstMileDeliveryEntities)) {
-                sourceCode = firstMileDeliveryEntities.get(0).getCode();
-            } else {
-                sourceCode = null;
-            }
-        } else {
-            sourceCode = null;
-        }
-        if (StrUtil.isBlank(sourceCode)) {
+        String sourceCode = pickingListsEntity.getSourceCode();
+        if (StrUtil.isBlank(sourceCode)){
             throw new ServiceException("来源单号未找到");
         }
         List<PickingDetailEntity> detailList = pickingDetailService.list(Wrappers.<PickingDetailEntity>lambdaQuery().in(PickingDetailEntity::getMainId, ids));
