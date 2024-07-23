7d446c0cc9b61a80e9839f5c33e1ac39978ea549
luoweigang <821969511@qq.com>
2024-07-23 14:27:15 +0800

    TIktok更新

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/TikTokSkuDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/TikTokSkuDmpHandler.java
@@ -19,20 +19,71 @@ import java.util.*;
 
 @Service
 @Scope("prototype")
-public class TikTokSkuDmpHandler extends DmpInputDoChildDmpHandler{
+public class TikTokSkuDmpHandler extends DmpInputDoChildDmpHandler {
 
 
     @Override
-    protected List<Map<String, Object>> getDmpInputMongoChildEntityList(List<Map<String, Object>> dmpInputMongoEntityList , String childMongoStorageName){
+    protected List<Map<String, Object>> getDmpInputMongoChildEntityList(List<Map<String, Object>> dmpInputMongoEntityList, String childMongoStorageName) {
+        List<Map<String, Object>> dmpInputMongoChildEntityList = new ArrayList<>();
         List<ParamData> paramDataList = new ArrayList<>();
         List<DmpInputTaskEntity> list = dmpInputTaskService.lambdaQuery().eq(DmpInputTaskEntity::getParentTaskId, inputTaskId).list();
         paramDataList.add(new ParamData(DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, PannoEnum.EQ, list.get(0).getId()));
-        List<Map<String, Object>> dmpInputMongoChildEntityList = mongoService.findMongoData(paramDataList, childMongoStorageName);
+        List<Map<String, Object>> dmpInputMongoChildList = mongoService.findMongoData(paramDataList, childMongoStorageName);
+
+        if (CollUtil.isNotEmpty(dmpInputMongoChildList)) {
+            for (Map<String, Object> dmpInputMongoChild : dmpInputMongoChildList) {
+                Object skuObj = dmpInputMongoChild.get("skus");
+                if (skuObj != null) {
+                    List<Map<String, Object>> skuList = (List<Map<String, Object>>) skuObj;
+
+                    skuList.forEach(l -> {
+                        l.put("platformCreateTime", dmpInputMongoChild.get("createTime"));
+                        l.put("platformUpdateTime", dmpInputMongoChild.get("updateTime"));
+                        l.put("spuId", dmpInputMongoChild.get("fid"));
+
+                        List<Map<String, Object>> mainImages = (List<Map<String, Object>>) dmpInputMongoChild.get("mainImages");
+                        if (CollectionUtil.isNotEmpty(mainImages)) {
+                            List<String> urls = (List<String>) mainImages.get(0).get("urls");
+                            l.put("imageUrls", urls.get(0));
+                        }
+
+                        //状态
+                        Object statusObj = dmpInputMongoChild.get("status");
+                        if (statusObj != null) {
+                            String status = String.valueOf(statusObj);
+                            if ("ACTIVATE".equalsIgnoreCase(status)) {
+                                l.put("status", "1");
+                            } else {
+                                l.put("status", "3");
+                            }
+                        }
+
+                        //品牌
+                        Object brandObj = dmpInputMongoChild.get("brand");
+                        if (brandObj != null) {
+                            Map<String, String> brandMap = (Map<String, String>) brandObj;
+                            l.put("brandName", brandMap.get("name"));
+                        }
+
+                        //类目
+                        Object categoryChainsObj = dmpInputMongoChild.get("categoryChains");
+                        if (categoryChainsObj != null) {
+                            List<Map<String, String>> categoryChainsList = (List<Map<String, String>>) categoryChainsObj;
+                            if (CollectionUtil.isNotEmpty(categoryChainsList)) {
+                                l.put("parent_category_name", categoryChainsList.get(0).get("localName"));
+                                l.put("category_name", categoryChainsList.get(1).get("localName"));
+                            }
+                        }
+                    });
+                    dmpInputMongoChildEntityList.addAll(skuList);
+                }
+            }
+        }
         return dmpInputMongoChildEntityList;
     }
 
     @Override
-    protected void putDmpId(List<Map<String, Object>> dmpInputMongoChildEntityList){
+    protected void putDmpId(List<Map<String, Object>> dmpInputMongoChildEntityList) {
         DmpCfgInputConvertEntity mainConvertId = this.getMainConvertId();
         String parentStorageName = mainConvertId.getStorageName();
         ServiceImpl parentServiceImpl = this.getServiceImpl(parentStorageName);
@@ -40,50 +91,16 @@ public class TikTokSkuDmpHandler extends DmpInputDoChildDmpHandler{
         wrapper.eq(INPUT_TASK_ID, inputTaskId);
         List<Map<String, Object>> listMaps = parentServiceImpl.listMaps(wrapper);
         Map<String, String> billNoIdMap = new HashMap<>();
-        if(CollUtil.isNotEmpty(listMaps)) {
-            for(Map<String, Object> listMap : listMaps) {
-                billNoIdMap.put(listMap.get(StrUtils.underlineByhump(mainConvertId.getUniqueFieldName())).toString(), listMap.get(BaseEntity.ID).toString());
+        if (CollUtil.isNotEmpty(listMaps)) {
+            for (Map<String, Object> listMap : listMaps) {
+                billNoIdMap.put(listMap.get("spu_id").toString(), listMap.get(BaseEntity.ID).toString());
             }
         }
-        for(Map<String, Object> dmpInputMongoChildEntity : dmpInputMongoChildEntityList) {
-            String billNo = dmpInputMongoChildEntity.get("fid").toString();
+        for (Map<String, Object> dmpInputMongoChildEntity : dmpInputMongoChildEntityList) {
+            String billNo = dmpInputMongoChildEntity.get("spuId").toString();
             String dmpId = billNoIdMap.get(billNo);
             dmpInputMongoChildEntity.put(MAIN_ID, dmpId);
         }
     }
 
-
-    @Override
-    protected void afterConvertData(Map<List<Map<String, Object>>, List<TreeMap<String, Object>>> dmpInputDataDmpRelationMaps) {
-        for(Map.Entry<List<Map<String, Object>>, List<TreeMap<String, Object>>> dmpInputDataDmpRelationMap : dmpInputDataDmpRelationMaps.entrySet()) {
-            List<TreeMap<String, Object>> dmpDataMaps = dmpInputDataDmpRelationMap.getValue();
-            for(TreeMap<String, Object> dmpDataMap : dmpDataMaps) {
-                //状态
-                Object statusObj = dmpDataMap.get("status");
-                if(statusObj != null) {
-                    String status = String.valueOf(statusObj);
-                    if ("ACTIVATE".equalsIgnoreCase(status)) {
-                        dmpDataMap.put("status", "1");
-                    } else {
-                        dmpDataMap.put("status", "3");
-                    }
-                }
-
-                Object brandObj = dmpDataMap.get("brand");
-                if (brandObj != null) {
-                    Map<String, String> brandMap = (Map<String, String>) brandObj;
-                    dmpDataMap.put("brandName", brandMap.get("name"));
-                }
-
-                Object categoryChainsObj = dmpDataMap.get("categoryChains");
-                if (categoryChainsObj != null) {
-                    List<Map<String, String>> categoryChainsList = (List<Map<String, String>>) categoryChainsObj;
-                    if (CollectionUtil.isNotEmpty(categoryChainsList)) {
-                        dmpDataMap.put("parent_category_name", categoryChainsList.get(0).get("localName"));
-                        dmpDataMap.put("category_name", categoryChainsList.get(1).get("localName"));
-                    }
-                }
-            }
-        }
-    }
 }
