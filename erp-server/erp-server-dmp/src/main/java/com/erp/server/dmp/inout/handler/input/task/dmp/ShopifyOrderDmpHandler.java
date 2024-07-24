e0072e9498c3b5a7764ecb6fe4125e57f4525e04
luoweigang <821969511@qq.com>
2024-07-24 16:36:34 +0800

    Shopify接口对接更新240724(4)

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
@@ -7,6 +7,7 @@ import com.erp.model.dmp.enums.DmpOrderReturnStatusEnum;
 import com.erp.model.dmp.enums.MabangOriginalOrderStatusEnum;
 import com.erp.model.dmp.enums.MabangSourcePlatformEnum;
 import com.erp.model.oms.enums.SoB2cBillStatusEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +19,7 @@ import java.util.TreeMap;
 /**
  * 订单主表字段映射转换
  */
+@Slf4j
 @Service
 @Scope("prototype")
 public class ShopifyOrderDmpHandler extends ShopifyDmpHandler {
@@ -29,22 +31,28 @@ public class ShopifyOrderDmpHandler extends ShopifyDmpHandler {
             for (TreeMap<String, Object> dmpDataMap : dmpDataMaps) {
                 //状态
                 Object financialStatusObj = dmpDataMap.get("financialStatus");
+                log.warn("1Shopify状态转换：{}" , financialStatusObj);
                 if (financialStatusObj != null) {
                     String financialStatus = String.valueOf(financialStatusObj);
                     if ("fulfilled".equalsIgnoreCase(financialStatus)) {
-                        dmpDataMap.put("order_status", ApproveStatusEnum.APPROVE.getCode());
+                        log.warn("Shopify2状态转换：{}" , financialStatus);
+                        dmpDataMap.put("orderStatus", ApproveStatusEnum.APPROVE.getCode());
                         dmpDataMap.put("deliveryStatus", SoB2cBillStatusEnum.ENUM_SHIPPED.getCode());
                     } else if ("refunded".equalsIgnoreCase(financialStatus)) {
-                        dmpDataMap.put("order_status", ApproveStatusEnum.REJECT.getCode());
+                        log.warn("Shopify3状态转换：{}" , financialStatus);
+                        dmpDataMap.put("orderStatus", ApproveStatusEnum.REJECT.getCode());
                         dmpDataMap.put("returnStatus", DmpOrderReturnStatusEnum.ORDER_RETURN.getCode());
                     } else if ("partially_refunded".equalsIgnoreCase(financialStatus)) {
-                        dmpDataMap.put("order_status", ApproveStatusEnum.REJECT.getCode());
+                        log.warn("Shopify4状态转换：{}" , financialStatus);
+                        dmpDataMap.put("orderStatus", ApproveStatusEnum.REJECT.getCode());
                         dmpDataMap.put("returnStatus", DmpOrderReturnStatusEnum.PARTIAL_RETURN.getCode());
                     } else if ("voided".equalsIgnoreCase(financialStatus)) {
-                        dmpDataMap.put("order_status", ApproveStatusEnum.REJECT.getCode());
+                        log.warn("Shopify5状态转换：{}" , financialStatus);
+                        dmpDataMap.put("orderStatus", ApproveStatusEnum.REJECT.getCode());
                         dmpDataMap.put("invalidStatus", Boolean.TRUE);
                     } else {
-                        dmpDataMap.put("order_status", ApproveStatusEnum.WAIT_SUBMIT.getCode());
+                        log.warn("Shopify6状态转换：{}" , financialStatus);
+                        dmpDataMap.put("orderStatus", ApproveStatusEnum.WAIT_SUBMIT.getCode());
                     }
                 }
                 //买家备注
