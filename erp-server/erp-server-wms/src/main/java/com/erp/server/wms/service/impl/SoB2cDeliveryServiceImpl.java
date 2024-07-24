91146e5136e2acc47679a577c84df13a104805e2
Will <wangwei@ulanzi.cn>
2024-07-24 10:43:59 +0800

    feat，拣货单库存回滚调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
@@ -1179,8 +1179,6 @@ public class SoB2cDeliveryServiceImpl extends SuperServiceImpl<SoB2cDeliveryMapp
         //删除拣货单
         pickingListsService.deleteBySourceId(ids);
         InventoryBatchUnApproveDTO inventoryBatchUnApproveDTO = new InventoryBatchUnApproveDTO(InventorySourceTypeEnum.SO_B2C_DELIVERY, ids);
-        //取消发货虚拟仓库存增加可用
-        addUsableVirtualInventory (deliveryEntityList);
         //回滚实体仓库存
         inventoryTransCoreService.batchUnApprove(inventoryBatchUnApproveDTO);
         //新增日志
