2d9b664ccee16e7148a5176b5bf8fc39846c23d8
tanmujin <tanmujin@ulanzi.cn>
2024-07-26 12:12:15 +0800

    fix：查询销售订单统计可用库存，冻结库存改为按仓库

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -3114,17 +3114,13 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
                     //可用库存
                     useableQty = inventoryList.stream().filter(obj -> obj.getSkuId().equals(detailDTO.getSkuId())
                                     && obj.getWarehouseId().equals(detailDTO.getWarehouseId())
-                                    && obj.getWarehouseLocationId().equals(detailDTO.getWarehouseLocation())
                                     && InventoryStatusEnum.USABLE.getCode().equals(obj.getInventoryStatus()))
-                            .findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal()))
-                            .orElse(MathUtil.ZERO);
+                            .mapToInt(obj -> obj.getInventoryTotal()).sum();
                     //冻结库存
                     freezeQty = inventoryList.stream().filter(obj -> obj.getSkuId().equals(detailDTO.getSkuId())
                                     && obj.getWarehouseId().equals(detailDTO.getWarehouseId())
-                                    && obj.getWarehouseLocationId().equals(detailDTO.getWarehouseLocation())
                                     && InventoryStatusEnum.FROZEN.getCode().equals(obj.getInventoryStatus()))
-                            .findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal()))
-                            .orElse(MathUtil.ZERO);
+                            .mapToInt(obj -> obj.getInventoryTotal()).sum();
                 }
                 detailDTO.setUseableQty(useableQty);
                 detailDTO.setFreezeQty(freezeQty);
@@ -3228,10 +3224,8 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
             //可用库存
             Integer childUseableQty = inventoryList.stream().filter(obj -> obj.getSkuId().equals(childrenSkuDTO.getSkuId())
                             && obj.getWarehouseId().equals(detailDTO.getWarehouseId())
-                            && obj.getWarehouseLocationId().equals(detailDTO.getWarehouseLocation())
                             && InventoryStatusEnum.USABLE.getCode().equals(obj.getInventoryStatus()))
-                    .findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal()))
-                    .orElse(MathUtil.ZERO);
+                    .mapToInt(obj -> obj.getInventoryTotal()).sum();
             if ((detailDTO.getQty() * childrenSkuDTO.getQuantity() > childUseableQty) && !ignoreInventorySkuIds.contains(childrenSkuDTO.getSkuId())) {
                 isOutStock = Boolean.TRUE;
                 break;
@@ -7564,10 +7558,9 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
                     //可用库存
                     useableQty = inventoryList.stream().filter(obj -> obj.getSkuId().equals(exportDTO.getSkuId())
                                     && obj.getWarehouseId().equals(exportDTO.getWarehouseId())
-                                    && obj.getWarehouseLocationId().equals(exportDTO.getWarehouseLocation())
+//                                    && obj.getWarehouseLocationId().equals(exportDTO.getWarehouseLocation())
                                     && InventoryStatusEnum.USABLE.getCode().equals(obj.getInventoryStatus()))
-                            .findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal()))
-                            .orElse(MathUtil.ZERO);
+                            .mapToInt(obj -> obj.getInventoryTotal()).sum();
                 }
                 exportDTO.setUseableQty(useableQty);
                 //存在仓库则需要判断是否缺货
