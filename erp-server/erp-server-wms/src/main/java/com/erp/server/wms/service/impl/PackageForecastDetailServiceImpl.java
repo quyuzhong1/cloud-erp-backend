8a5603088a78b9bf6774ef5951bac37011e04f16
liuruipeng <arthur@ulanzi.cn>
2024-07-26 15:28:51 +0800

    fix:组包预报更新去掉出库状态判断

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackageForecastDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackageForecastDetailServiceImpl.java
@@ -8,6 +8,7 @@ import com.common.core.exception.ServiceException;
 import com.common.core.utils.BeanMapperUtils;
 import com.erp.model.oms.entity.SoB2cEntity;
 import com.erp.model.oms.enums.PackageStatusEnum;
+import com.erp.model.oms.enums.SoB2cBillStatusEnum;
 import com.erp.model.tms.entity.SettingForecastEntity;
 import com.erp.model.wms.dto.PackageForecastDTO;
 import com.erp.model.wms.dto.PackageForecastDetailDTO;
@@ -101,15 +102,6 @@ public class PackageForecastDetailServiceImpl extends SuperServiceImpl<PackageFo
         List<PackageForecastDetailEntity> dbList = this.listDbByMainId(mainId);
         //删除的信息
         List<PackageForecastDetailEntity> deleteList = dbList.stream().filter(s -> !idList.contains(s.getId())).collect(Collectors.toList());
-        //删除的销售订单id 集合
-        List<String> deleteSoIdList = deleteList.stream().map(PackageForecastDetailEntity::getSoId).collect(Collectors.toList());
-        List<SoOutstockEntity> soOutstockList = soOutstockService.listBySoIds(deleteSoIdList);
-        String soCode= soOutstockList.stream ().filter(s->ApproveStatusEnum.APPROVE.equals(s.getApproveStatus())).
-                map(SoOutstockEntity::getSoCode).collect(Collectors.joining(","));
-        if (StringUtils.isNotBlank(soCode)) {
-            throw new ServiceException(soCode + "销售订单已出库,不能删除");
-        }
-
         //删除的id
         List<String> deleteIdList = deleteList.stream().map(PackageForecastDetailEntity::getId).collect(Collectors.toList());
         this.removeByIds(deleteIdList);
