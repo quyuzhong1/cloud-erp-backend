c4e3ebc7fa199ef969206fa37fabf01ed65686d7
liaohui <liaohui@ulanzi.cn>
2024-07-29 16:13:01 +0800

    chore:旺店通优化

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/wangdian/WdtOtherOutStockConsumer.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/consumer/wangdian/WdtOtherOutStockConsumer.java
@@ -64,7 +64,7 @@ public class WdtOtherOutStockConsumer<T extends DmpSyncTaskIdDTO> extends Abstra
     private ThirdMappingService thirdMappingService;
     @Resource
     private ThirdWarehouseService thirdWarehouseService;
-    private final RateLimiter limiter = RateLimiter.create(1, 1, TimeUnit.SECONDS);
+
 
     @Override
     public void updateSyncTaskStatus(String syncTaskId, SyncStatusEnum code, String msg) {
@@ -83,81 +83,81 @@ public class WdtOtherOutStockConsumer<T extends DmpSyncTaskIdDTO> extends Abstra
 
     @Override
     public ApiResult<?> handle(Object ext) {
-        String requestStr = JSONUtil.toJsonStr(ext);
-        // 处理参数中存在null字符串的数据
-        requestStr = requestStr.replace("null","");
-        CreateOtherStockoutRequest request = JSON.parseObject(requestStr, CreateOtherStockoutRequest.class);
+        RateLimiter limiter = RateLimiter.create(1, 1, TimeUnit.SECONDS);
+        if (limiter.tryAcquire()) {
+            String requestStr = JSONUtil.toJsonStr(ext);
+            // 处理参数中存在null字符串的数据
+            requestStr = requestStr.replace("null","");
+            CreateOtherStockoutRequest request = JSON.parseObject(requestStr, CreateOtherStockoutRequest.class);
 
-        //查询同一个来源单据下的推送任务
-        List<String> sourceCodeList = Collections.singletonList(request.getSourceId());
-        DmpSyncTaskDTO.ListCodeDTO listDTO = new DmpSyncTaskDTO.ListCodeDTO(sourceCodeList, request.getTargetPlatformName(), request.getSourcePlatformName());
-        List<DmpPushTaskEntity> taskList = dmpPushTaskService.listByCodeParam(listDTO);
+            //查询同一个来源单据下的推送任务
+            List<String> sourceCodeList = Collections.singletonList(request.getSourceId());
+            DmpSyncTaskDTO.ListCodeDTO listDTO = new DmpSyncTaskDTO.ListCodeDTO(sourceCodeList, request.getTargetPlatformName(), request.getSourcePlatformName());
+            List<DmpPushTaskEntity> taskList = dmpPushTaskService.listByCodeParam(listDTO);
 
-        //保证数据推送的先后顺序:
-        //1.同一个单据的审核操作必须早于反审核操作推送数据
-        if(StringUtils.equals(SyncOperateEnum.OPERATE_DISAPPROVE.getCode(), request.getOperateCode())){
+            //保证数据推送的先后顺序:
+            //1.同一个单据的审核操作必须早于反审核操作推送数据
+            if(StringUtils.equals(SyncOperateEnum.OPERATE_DISAPPROVE.getCode(), request.getOperateCode())){
+                long count = taskList.stream()
+                        .filter(task -> StringUtils.equals(SyncOperateEnum.OPERATE_APPROVE.getCode(), task.getSyncOperate())
+                                && !StringUtils.equals(task.getStatus(), SyncStatusEnum.SUCCESS_SYNC.getCode())
+                                && !StringUtils.equals(task.getStatus(), SyncStatusEnum.NO_NEED_SYNC.getCode()))
+                        .count();
+                if(count > 0){
+                    return ApiResult.error(ApiError.ERROR_WDT_CANCEL_PUSH.code, String.format("前序任务未完成, 跳过本次推送: %s", request));
+                }
+            }
+
+            //2.在单据的单一操作内部, 比如调拨单的审核操作, 出库单必须早于入库单推送
             long count = taskList.stream()
-                    .filter(task -> StringUtils.equals(SyncOperateEnum.OPERATE_APPROVE.getCode(), task.getSyncOperate())
+                    .filter(task -> StringUtils.equals(task.getSyncOperate(), request.getOperateCode())
                             && !StringUtils.equals(task.getStatus(), SyncStatusEnum.SUCCESS_SYNC.getCode())
-                            && !StringUtils.equals(task.getStatus(), SyncStatusEnum.NO_NEED_SYNC.getCode()))
+                            && !StringUtils.equals(task.getStatus(), SyncStatusEnum.NO_NEED_SYNC.getCode())
+                            && JSON.parseObject(task.getMqData(), CreateOtherStockoutRequest.class).getCreateTime().isBefore(request.getCreateTime()))
                     .count();
+            //先创建的任务必须先完成
             if(count > 0){
                 return ApiResult.error(ApiError.ERROR_WDT_CANCEL_PUSH.code, String.format("前序任务未完成, 跳过本次推送: %s", request));
             }
-        }
 
-        //2.在单据的单一操作内部, 比如调拨单的审核操作, 出库单必须早于入库单推送
-        long count = taskList.stream()
-                .filter(task -> StringUtils.equals(task.getSyncOperate(), request.getOperateCode())
-                        && !StringUtils.equals(task.getStatus(), SyncStatusEnum.SUCCESS_SYNC.getCode())
-                        && !StringUtils.equals(task.getStatus(), SyncStatusEnum.NO_NEED_SYNC.getCode())
-                        && JSON.parseObject(task.getMqData(), CreateOtherStockoutRequest.class).getCreateTime().isBefore(request.getCreateTime()))
-                .count();
-        //先创建的任务必须先完成
-        if(count > 0){
-            return ApiResult.error(ApiError.ERROR_WDT_CANCEL_PUSH.code, String.format("前序任务未完成, 跳过本次推送: %s", request));
-        }
-
-        ThirdMappingEntity thirdMapping = thirdMappingService.getByThirdCodeAndType(request.getWarehouseNo(), ThirdSysTypeEnum.WDT.getCode(), ThirdSysTypeEnum.WAREHOUSE.getCode());
-        if (ObjectUtils.isEmpty(thirdMapping)) {
-            throw new ServiceException(ApiError.ERROR_3000.code, String.format("推送旺店通其他出库单失败: 三方仓库%s未映射", request.getWarehouseNo()));
-        }
-        ThirdWarehouseEntity thirdWarehouse = thirdWarehouseService.getById(thirdMapping.getThirdInfoId());
-        if (ObjectUtils.isEmpty(thirdWarehouse)) {
-            throw new ServiceException(ApiError.ERROR_3000.code, String.format("推送旺店通其他出库单失败: 三方仓库%s不存在", request.getWarehouseNo()));
-        }
-        //根据旺店通仓库类型，决定调用的API
-        if (WdtWarehouseTypeEnum.SELF_TRANSFER.getCode().equals(thirdWarehouse.getType())) {
-            limiter.acquire();
-            StockExternalOutResponse stockExternalOutResponse = wdtService.querySelfOut(request);
-            if (ObjectUtils.isNotEmpty(stockExternalOutResponse) && CollectionUtils.isNotEmpty(stockExternalOutResponse.getOrder())) {
-                StockExternalOutResponse.Order order = stockExternalOutResponse.getOrder().get(0);
-                if (WdtExtOutStockStatusEnum.finish().contains(order.getStatus())) {
-                    return ApiResult.success();
-                } else {
-                    //修改任务的错误消息
-                    String format = String.format("单据推送成功，当前状态：%s，请手动处理", WdtExtOutStockStatusEnum.getName(order.getStatus()));
-                    return ApiResult.error(format);
-                }
+            ThirdMappingEntity thirdMapping = thirdMappingService.getByThirdCodeAndType(request.getWarehouseNo(), ThirdSysTypeEnum.WDT.getCode(), ThirdSysTypeEnum.WAREHOUSE.getCode());
+            if (ObjectUtils.isEmpty(thirdMapping)) {
+                throw new ServiceException(ApiError.ERROR_3000.code, String.format("推送旺店通其他出库单失败: 三方仓库%s未映射", request.getWarehouseNo()));
             }
-            wdtService.executeSelfConsumer(request);
-        }else {
-            //请求旺店通
-            limiter.acquire();
-            StockoutOtherQueryResponse queryResponse = wdtService.queryWithDetail(request);
-            List<StockoutOtherQueryResponse.OrderItem> order = queryResponse.getOrder();
-            //旺店通已经存在这个单据
-            if(! order.isEmpty()){
-                StockoutOtherQueryResponse.OrderItem orderInfoDto = order.get(0);
-                if (orderInfoDto.getStatus().equals(110)) {
-                    return ApiResult.success();
-                }else {
-                    //修改任务的错误消息
-                    String format = String.format("单据推送成功，当前状态：%s，请手动处理", WdtOutStockStatusEnum.getName(String.valueOf(orderInfoDto.getStatus())));
-                    return ApiResult.error(format);
-                }
+            ThirdWarehouseEntity thirdWarehouse = thirdWarehouseService.getById(thirdMapping.getThirdInfoId());
+            if (ObjectUtils.isEmpty(thirdWarehouse)) {
+                throw new ServiceException(ApiError.ERROR_3000.code, String.format("推送旺店通其他出库单失败: 三方仓库%s不存在", request.getWarehouseNo()));
             }
-            wdtService.executeConsumer(request);
+            //根据旺店通仓库类型，决定调用的API
+            if (WdtWarehouseTypeEnum.SELF_TRANSFER.getCode().equals(thirdWarehouse.getType())) {
+                StockExternalOutResponse stockExternalOutResponse = wdtService.querySelfOut(request);
+                if (ObjectUtils.isNotEmpty(stockExternalOutResponse) && CollectionUtils.isNotEmpty(stockExternalOutResponse.getOrder())) {
+                    StockExternalOutResponse.Order order = stockExternalOutResponse.getOrder().get(0);
+                    if (WdtExtOutStockStatusEnum.finish().contains(order.getStatus())) {
+                        return ApiResult.success();
+                    } else {
+                        //修改任务的错误消息
+                        String format = String.format("单据推送成功，当前状态：%s，请手动处理", WdtExtOutStockStatusEnum.getName(order.getStatus()));
+                        return ApiResult.error(format);
+                    }
+                }
+                wdtService.executeSelfConsumer(request);
+            }else {
+                StockoutOtherQueryResponse queryResponse = wdtService.queryWithDetail(request);
+                List<StockoutOtherQueryResponse.OrderItem> order = queryResponse.getOrder();
+                //旺店通已经存在这个单据
+                if(! order.isEmpty()){
+                    StockoutOtherQueryResponse.OrderItem orderInfoDto = order.get(0);
+                    if (orderInfoDto.getStatus().equals(110)) {
+                        return ApiResult.success();
+                    }else {
+                        //修改任务的错误消息
+                        String format = String.format("单据推送成功，当前状态：%s，请手动处理", WdtOutStockStatusEnum.getName(String.valueOf(orderInfoDto.getStatus())));
+                        return ApiResult.error(format);
+                    }
+                }
+                wdtService.executeConsumer(request);
+            }
         }
         return ApiResult.success();
     }
