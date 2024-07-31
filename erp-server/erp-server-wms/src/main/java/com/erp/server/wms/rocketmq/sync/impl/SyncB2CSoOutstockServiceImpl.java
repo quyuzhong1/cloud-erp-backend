4c67a6e7658fcb1993b7839e26d15c50ceab602d
tanmujin <tanmujin@ulanzi.cn>
2024-07-31 17:18:29 +0800

    fix：修改销售出库单异常提示

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
@@ -259,7 +259,7 @@ public class SyncB2CSoOutstockServiceImpl implements SyncB2CSoOutstockService {
                 String skuId = skuList.stream().filter(s -> s.getSkuNo().equals(detailEntity.getSkuNo())).
                         findFirst().map(SkuVO::getSkuId).orElse("");
                 if (StringUtils.isBlank(skuId)) {
-                    throw new ServiceException(ApiError.ERROR_92055, detailEntity.getSkuNo());
+                    throw new ServiceException(ApiError.ERROR_SKU_NOTFOUND, detailEntity.getSkuNo());
                 }
                 String detailId = IdWorker.getIdStr();
                 detailEntity.setId(detailId);
@@ -289,7 +289,7 @@ public class SyncB2CSoOutstockServiceImpl implements SyncB2CSoOutstockService {
                     String skuId = skuList.stream().filter(s -> s.getSkuNo().equals(detailEntity.getSkuNo())).
                             findFirst().map(SkuVO::getSkuId).orElse("");
                     if (StringUtils.isBlank(skuId)) {
-                        throw new ServiceException(ApiError.ERROR_92055, detailEntity.getSkuNo());
+                        throw new ServiceException(ApiError.ERROR_SKU_NOTFOUND, detailEntity.getSkuNo());
                     }
                     String detailId = IdWorker.getIdStr();
                     detailEntity.setId(detailId);
@@ -463,7 +463,7 @@ public class SyncB2CSoOutstockServiceImpl implements SyncB2CSoOutstockService {
             String skuId = skuList.stream().filter(s -> s.getSkuNo().equals(skuNo)).
                     findFirst().map(SkuVO::getSkuId).orElse("");
             if (StringUtils.isBlank(skuId)) {
-                throw new ServiceException(ApiError.ERROR_92055, skuNo);
+                throw new ServiceException(ApiError.ERROR_SKU_NOTFOUND, skuNo);
             }
             SoOutstockDetailEntity detailEntity = new SoOutstockDetailEntity();
             String detailId = IdWorker.getIdStr();
