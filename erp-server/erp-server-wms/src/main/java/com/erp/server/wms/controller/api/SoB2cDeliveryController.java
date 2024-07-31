48dadc05052b4fe82736ad5f04a09cbad3f3220a
Will <wangwei@ulanzi.cn>
2024-07-31 19:51:38 +0800

    feat，虚拟仓库存数据修复

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/SoB2cDeliveryController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/SoB2cDeliveryController.java
@@ -513,4 +513,33 @@ public class SoB2cDeliveryController extends BaseController {
         }
         return resultDTOS.stream().allMatch(BatchResultDTO::getSuccess) ? success(resultDTOS) : failure(resultDTOS);
     }
+
+    /**
+     * 库存数据修复
+     * @author will
+     * @date 2024/7/31 19:35
+     * @param dto
+     * @return ApiResult<List<BatchResultDTO>>
+     */
+    @PostMapping("/handleErrorData")
+    public ApiResult<List<BatchResultDTO>> handleErrorData(@RequestBody @Validated BaseIdsDTO.IdsDTO dto) {
+        List<BatchResultDTO> resultDTOS = new ArrayList<>(dto.getIds().size());
+        for (String id : dto.getIds()) {
+            BatchResultDTO receiverResult;
+            try {
+                receiverResult = soB2cDeliveryService.handleErrorData(id);
+            } catch (Exception e) {
+                log.error("处理数据", e);
+                SoB2cDeliveryEntity entity = soB2cDeliveryService.getById(id);
+                if (ObjectUtil.isEmpty(entity)) {
+                    receiverResult = BatchResultDTO.fail(id, entity.getCode(), "发货单不存在, 处理数据失败");
+                    resultDTOS.add(receiverResult);
+                    continue;
+                }
+                receiverResult = BatchResultDTO.fail(id, entity.getCode(), e.getMessage());
+            }
+            resultDTOS.add(receiverResult);
+        }
+        return resultDTOS.stream().allMatch(BatchResultDTO::getSuccess) ? success(resultDTOS) : failure(resultDTOS);
+    }
 }
