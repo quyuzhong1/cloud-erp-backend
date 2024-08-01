e2484f0233e5c06a57fe3bb302297f618495e8d9
luoweigang <821969511@qq.com>
2024-08-01 17:40:56 +0800

    修改TikTok数据清洗0801 1

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/MercadoProductDmpHandler.java
@@ -0,0 +1,94 @@
+package com.erp.server.dmp.inout.handler.input.task.dmp;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.CollectionUtil;
+import com.alibaba.fastjson.JSON;
+import com.common.core.anno.ParamData;
+import com.common.core.enums.PannoEnum;
+import com.erp.model.dmp.entity.DmpInputTaskEntity;
+import com.erp.model.dmp.entity.DmpSoDetailEntity;
+import com.erp.model.dmp.entity.DmpSoInfoEntity;
+import com.erp.model.dmp.enums.DmpBasicSystemCodeEnum;
+import com.erp.model.oms.enums.SoB2cPayStatusEnum;
+import com.erp.oms.aliexpress.constants.AliexpressConstants;
+import com.erp.oms.aliexpress.dto.response.AliExpressOrder;
+import com.erp.oms.aliexpress.dto.response.OrderItemDetail;
+import com.erp.server.dmp.inout.handler.input.task.mongo.DmpInputMongoHandler;
+import com.erp.server.dmp.service.DmpSoDetailService;
+import com.erp.server.dmp.service.DmpSoInfoService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+@Scope("prototype")
+public class MercadoProductDmpHandler extends DmpInputDbConvertDmpHandler{
+
+    @Autowired
+    private DmpSoInfoService dmpSoInfoService;
+
+    @Autowired
+    private DmpSoDetailService dmpSoDetailService;
+
+    @Override
+    protected void afterConvertData(Map<List<Map<String, Object>>, List<TreeMap<String, Object>>> dmpInputDataDmpRelationMaps) {
+        super.afterConvertData(dmpInputDataDmpRelationMaps);
+
+        String parentTaskId = dmpInputTaskEntity.getParentTaskId();
+        List<DmpInputTaskEntity> list = dmpInputTaskService.lambdaQuery().eq(DmpInputTaskEntity::getId, parentTaskId).list();
+        if (CollectionUtil.isEmpty(list)) {
+            return;
+        }
+        for(Map.Entry<List<Map<String, Object>>, List<TreeMap<String, Object>>> dmpInputDataDmpRelationMap : dmpInputDataDmpRelationMaps.entrySet()) {
+            List<TreeMap<String, Object>> dmpDataMaps = dmpInputDataDmpRelationMap.getValue();
+            List<Map<String, Object>> mongoDataMaps = dmpInputDataDmpRelationMap.getKey();
+            Map<String, Object> mongoDataMap = mongoDataMaps.get(0);
+            for(TreeMap<String, Object> dmpDataMap : dmpDataMaps) {
+                dmpDataMap.put("nextLevelId", list.get(0).getNextLevelId());
+                dmpDataMap.put("shopId", list.get(0).getNextLevelId());
+
+                //创建时间
+                Object createTimeObj = mongoDataMap.get("createTime");
+                if(createTimeObj != null) {
+                    dmpDataMap.put("dmpDataMap", createTimeObj);
+                }
+
+                //修改时间
+                Object updateTimeObj = mongoDataMap.get("updateTime");
+                if(updateTimeObj != null) {
+                    dmpDataMap.put("dmpDataMap", updateTimeObj);
+                }
+
+                //产品信息
+                Object attributesObj = mongoDataMap.get("attributes");
+                if(attributesObj != null) {
+                    List<Map<String, Object>> attributesList = (List<Map<String, Object>>) attributesObj;
+                    for (Map<String, Object> map : attributesList) {
+                        if ("SELLER_SKU".equalsIgnoreCase(String.valueOf(map.get("fid")))) {
+                            dmpDataMap.put("skuNo", map.get("valueName"));
+                        }
+                        if ("BRAND".equalsIgnoreCase(String.valueOf(map.get("fid")))) {
+                            dmpDataMap.put("brandName", map.get("valueName"));
+                        }
+                    }
+                }
+
+                //状态
+                Object statusObj = mongoDataMap.get("status");
+                if (statusObj != null) {
+                    String status = String.valueOf(statusObj);
+                    if ("active".equalsIgnoreCase(status)) {
+                        dmpDataMap.put("status", "1");
+                    } else {
+                        dmpDataMap.put("status", "3");
+                    }
+                }
+
+            }
+        }
+    }
+}
