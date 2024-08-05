e66e859862bba362ea273f99c5795d5cc2e7f854
Will <wangwei@ulanzi.cn>
2024-08-05 20:20:18 +0800

    feat，b2b销售订单是释放库存调整

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoDetailServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoDetailServiceImpl.java
@@ -1781,7 +1781,7 @@ public class SoDetailServiceImpl extends SuperServiceImpl<SoDetailMapper, SoDeta
                     .map(SoDeliveryNoticeDetailEntity::getDeliveryQty).reduce(MathUtil.ZERO, Integer::sum);
 
             //如果发货通知单没改sku，并且数量大于0再校验
-            if (skuIdList.size() == 1 && StrUtil.equals(skuIdList.get(0),soDetailEntity.getSkuId())  && MathUtil.compareTo(noticeApproveQty,MathUtil.ZERO) > MathUtil.ZERO) {
+            if ((skuIdList.size() == 1 && StrUtil.equals(skuIdList.get(0),soDetailEntity.getSkuId()) && MathUtil.compareTo(noticeApproveQty,MathUtil.ZERO) > MathUtil.ZERO)|| CollectionUtils.isEmpty(skuIdList)) {
                 if (noticeApproveQty + soDetailEntity.getFrozenQty() > updateDTO.getQty()) {
                     throw new ServiceException(StrUtil.format("SKU【{}】销售数量不能小于（冻结数量+发货通知单审核数量）",soDetailEntity.getSkuNo()));
                 }
