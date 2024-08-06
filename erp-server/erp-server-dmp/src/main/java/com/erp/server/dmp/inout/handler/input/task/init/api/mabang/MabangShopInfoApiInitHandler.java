54cf5f4f32d6c3ccf9867a6876885d173d4b5e85
luoweigang <821969511@qq.com>
2024-08-06 15:04:53 +0800

    修改马帮接口拉取数据

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/mabang/MabangShopInfoApiInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/mabang/MabangShopInfoApiInitHandler.java
@@ -33,12 +33,11 @@ public class MabangShopInfoApiInitHandler implements DmpInputApiInitHandler {
     @Override
     public List<DmpInputTaskInitDTO> getApiData(DmpInputApiInitRequest dmpInputApiInitRequest) {
         List<DmpInputTaskInitDTO> dmpInputTaskInitDTOList = new ArrayList<>();
-        DmpInputMabangApiInitRequest dmpInputMabangApiInitRequest = (DmpInputMabangApiInitRequest) dmpInputApiInitRequest;
 
-        String requestParam = dmpInputMabangApiInitRequest.getRequestParam();
+        String requestParam = dmpInputApiInitRequest.getRequestParam();
         Map<String, Object> paramMap = JSON.parseObject(requestParam, Map.class);
 
-        ParamHeaderVO paramVo = MabangTool.getParamMap(dmpInputMabangApiInitRequest.getApiType(), 0, paramMap);
+        ParamHeaderVO paramVo = MabangTool.getParamMap(dmpInputApiInitRequest.getApiType(), 0, paramMap);
         JSONObject responseMap = HttpCommonUtil.sendOkhttp(UrlContant.MABANG_HOST, paramVo.getParamsStr(), null, paramVo.getHeaderMap(), RequestMethod.POST);
         if (!Objects.equals(responseMap.getInteger("code"), 200)) {
             log.error("调用url={} param={}马帮店铺数据失败 responseMap={}",UrlContant.MABANG_HOST, paramVo.getParamsStr(), JSONUtil.toJsonStr(responseMap));
