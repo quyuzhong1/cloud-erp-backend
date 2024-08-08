7a81674a148b602f572739f7d4cc4e79c3800dd1
Jim <zhoujingbo@ulanzi.cn>
2024-08-08 10:07:37 +0800

    UPDATE:其他出库类型

--- erp-common/erp-common-business/src/main/java/com/common/business/constant/DictKindgeeConstant.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/constant/DictKindgeeConstant.java
@@ -29,6 +29,12 @@ public class DictKindgeeConstant {
 
     /**
      * 其他出库单库存调整
+     * 推送金蝶失败
      */
-    public static final String OTHER_OUT_INVENTORY_ADJUSTMENTS = "库存调整";
+//    public static final String OTHER_OUT_INVENTORY_ADJUSTMENTS = "库存调整";
+
+    /**
+     * 其他出库单物料领用
+     */
+    public static final String OTHER_OUT_MATERIAL_PICKING = "物料领用";
 }
