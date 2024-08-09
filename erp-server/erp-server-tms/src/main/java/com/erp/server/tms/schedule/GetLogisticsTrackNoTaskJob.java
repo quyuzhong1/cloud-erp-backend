e9334337da84604aff613e46d21205338b75b612
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-08-09 15:27:52 +0800

    修改获取物流跟踪号接口

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/GetLogisticsTrackNoTaskJob.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/GetLogisticsTrackNoTaskJob.java
@@ -86,7 +86,7 @@ public class GetLogisticsTrackNoTaskJob {
                     for (LogisticsOrderResponseVO item : resultList) {
                         String b2cLogisticsId = finalQueryList.stream().filter(f -> f.getTransportNo().equals(item.getTransportNo())).
                                 map(SoB2cLogisticsDTO.TrackNoDTO::getId).findFirst().orElse("");
-                        if (StringUtils.isNotBlank(b2cLogisticsId)){
+                        if (StringUtils.isNotBlank(b2cLogisticsId) && StringUtils.isNotBlank(item.getTrackNo())){
                             LogisticsBillDTO.TrackDTO dto = LogisticsBillDTO.TrackDTO.builder()
                                     .transportNo(item.getTransportNo())
                                     .trackNo(item.getTrackNo())
