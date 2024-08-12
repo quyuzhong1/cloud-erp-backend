0d178d6d745bcaa3dfeb5ec457d2283ad69fe2a3
shukai <shukai@ulanzi.cn>
2024-08-12 15:19:21 +0800

    add:重试次数加电话取值

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAliExpressSoOutstockInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAliExpressSoOutstockInitHandler.java
@@ -97,13 +97,18 @@ public class DmpInputAliExpressSoOutstockInitHandler extends DmpInputInitHandler
 	        
 	    	JSONObject data = null;
 	    	long sleepTime = 1000;
+	    	int count = 0;
 	    	while(data == null) {
 	    		data = this.execute(client, request, token, apiType);
 	    		if(data == null) {
+	    			if(count == 10) {
+        				throw new ServiceException("调用速卖通" + apiType + "接口重试" + count + "失败");
+        			}
 	    			try {
 						Thread.sleep(sleepTime);
 					} catch (InterruptedException e) {}
 	    			sleepTime = sleepTime + 1000;
+	    			count = count + 1;
 	    		}
 	    	}
 	    	
