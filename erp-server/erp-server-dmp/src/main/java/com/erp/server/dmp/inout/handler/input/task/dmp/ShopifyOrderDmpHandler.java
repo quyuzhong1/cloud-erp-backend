233fcea252650a91df37935ac599f54902075540
luoweigang <821969511@qq.com>
2024-08-13 12:23:12 +0800

    修改新中台Shopify

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
@@ -144,6 +144,24 @@ public class ShopifyOrderDmpHandler extends ShopifyDmpHandler {
                     // 存在退款的明细ID
                     Set<String> refundedLineItemIds = new HashSet<>();
 
+                    List<Map<String, Object>> refundsMap = (List<Map<String, Object>>) refundsObj;
+                    if (CollectionUtil.isNotEmpty(refundsMap)) {
+
+                        for (Map<String, Object> map : refundsMap) {
+                            Object refundLineItems = map.get("refundLineItems");
+                            if (ObjectUtil.isNotEmpty(refundLineItems)) {
+                                List<Map<String, Object>> refundLineItemsList = (List<Map<String, Object>>) refundLineItems;
+                                List<String> sourceFundedLineItemIds = refundLineItemsList.stream()
+                                        .map(req -> req.get("lineItemId").toString())
+                                        .distinct()
+                                        .collect(Collectors.toList());
+                                refundedLineItemIds.addAll(sourceFundedLineItemIds);
+
+                            }
+                        }
+                    }
+
+
                     List<ShopifyRefund> refunds = JSON.parseArray(JSON.toJSONString(refundsObj), ShopifyRefund.class);
                     if (!CollectionUtils.isEmpty(refunds)) {
                         List<ShopifyRefundLineItem> refundLineItem = refunds.stream().map(ShopifyRefund::getRefundLineItems)
