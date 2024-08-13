a556e3289a4f51c3bfca4ed31ca3c781e56a2b3e
luoweigang <821969511@qq.com>
2024-08-13 12:52:10 +0800

    修改新中台Shopify

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyOrderDmpHandler.java
@@ -144,6 +144,7 @@ public class ShopifyOrderDmpHandler extends ShopifyDmpHandler {
                     // 存在退款的明细ID
                     Set<String> refundedLineItemIds = new HashSet<>();
 
+                    log.warn("refunds:" + refundsObj);
                     List<Object> refundsMap = (List<Object>) refundsObj;
                     if (CollectionUtil.isNotEmpty(refundsMap)) {
 
