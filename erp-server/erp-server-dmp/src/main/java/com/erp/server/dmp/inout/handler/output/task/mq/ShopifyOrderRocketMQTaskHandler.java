a0e7fe1ec11e1185b18c4cbecf1652e177e00c0e
luoweigang <821969511@qq.com>
2024-08-13 13:06:20 +0800

    修改新中台Shopify

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/ShopifyOrderRocketMQTaskHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/ShopifyOrderRocketMQTaskHandler.java
@@ -265,7 +265,7 @@ public class ShopifyOrderRocketMQTaskHandler extends DmpOutputRocketMQTaskHandle
         Map<String, Object> lableMap = new HashMap<>();
         JSONObject jsonObject = JSONObject.parseObject(dmpSoInfoEntity.getExtendData());
         if (jsonObject.get("refundedLineItemIds") != null) {
-            Set<String> refundedLineItemIds = (Set<String>) jsonObject.get("refundedLineItemIds");
+            List<String> refundedLineItemIds = (List<String>) jsonObject.get("refundedLineItemIds");
             if (!CollectionUtils.isEmpty(refundedLineItemIds) && refundedLineItemIds.contains(soDetailEntity.getThirdDetailId())) {
                 lableMap.put("isRefunded", true);
                 detailDTO.setIsDetailRefund(true);
