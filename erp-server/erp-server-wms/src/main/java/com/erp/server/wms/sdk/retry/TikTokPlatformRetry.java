80ecb8bf425e7468f43d1e9465a4e23252ab2b7d
luoweigang <821969511@qq.com>
2024-08-23 17:08:54 +0800

    修改TikTok重新生成销售出库单

---
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/retry/TikTokPlatformRetry.java
@@ -0,0 +1,25 @@
+package com.erp.server.wms.sdk.retry;
+
+import com.common.business.annotation.PlatformRetryAnno;
+import com.common.business.enums.PlatformDictEnum;
+import com.erp.model.oms.entity.SoB2cEntity;
+import com.erp.server.wms.service.IPlatformRetryService;
+import com.erp.server.wms.service.SoOutstockService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Slf4j
+@Component
+@PlatformRetryAnno(method = PlatformDictEnum.TIK_TOK)
+public class TikTokPlatformRetry implements IPlatformRetryService {
+
+    @Resource
+    private SoOutstockService soOutstockService;
+    @Override
+    public Boolean retrySoOutStock(SoB2cEntity currentEntity, List list) {
+        return soOutstockService.defaultHandleRetry(currentEntity, list);
+    }
+}
