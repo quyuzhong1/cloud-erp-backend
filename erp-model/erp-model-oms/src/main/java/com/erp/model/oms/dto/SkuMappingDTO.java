4972ccd7167081789bc564b57c5169b79484c433
liuruipeng <arthur@ulanzi.cn>
2024-08-26 15:32:07 +0800

    Revert "feat:【安兔海外仓】库存sku高级查询调整"
    This reverts commit bee35e113b4a3002cff2326ff3ba4794fe170083.

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SkuMappingDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SkuMappingDTO.java
@@ -1,6 +1,5 @@
 package com.erp.model.oms.dto;
 
-import com.common.business.dto.AdvanceQueryDTO;
 import com.common.business.dto.base.PermissionsDTO;
 import com.common.business.dto.base.SortDTO;
 import com.common.core.anno.StateEnumValue;
@@ -19,7 +18,6 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author Lambda
@@ -229,16 +227,6 @@ public class SkuMappingDTO implements Serializable {
     @Data
     @NoArgsConstructor
     public static class WarehousePagingParamDTO extends SortDTO {
-
-        /**
-         * 页面高级查询
-         */
-        private List<AdvanceQueryDTO> advanceQueryDTOList;
-
-        /**
-         * sqlMap 默认key default
-         */
-        private Map<String,String> sqlMap;
 
         private String type;
 
--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SkuMappingController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SkuMappingController.java
@@ -2,7 +2,6 @@ package com.erp.server.oms.controller.api;
 
 
 import cn.hutool.core.util.ObjectUtil;
-import com.common.business.annotation.WebAdvanceQuery;
 import com.common.business.dto.base.BaseIdDTO;
 import com.common.business.dto.base.BaseIdsDTO;
 import com.common.business.dto.base.BatchResultDTO;
@@ -18,7 +17,6 @@ import com.erp.model.oms.dto.SkuMappingDTO;
 import com.erp.model.oms.entity.SkuMappingEntity;
 import com.erp.model.scm.dto.OperateLogDTO;
 import com.erp.model.workflow.dto.ProcessManagementDTO;
-import com.erp.server.oms.query.SkuMappingQueryHandler;
 import com.erp.server.oms.service.SkuMappingRuleService;
 import com.erp.server.oms.service.SkuMappingService;
 import lombok.extern.slf4j.Slf4j;
@@ -99,7 +97,6 @@ public class SkuMappingController extends BaseController {
      * @return
      */
     @PostMapping("/warehousePaging")
-    @WebAdvanceQuery(handler = SkuMappingQueryHandler.class)
     public ApiResult<PagingVO<SkuMappingDTO.WarehousePagingViewDTO>> queryWarehouseByPage(@RequestBody @Validated PagingDTO<SkuMappingDTO.WarehousePagingParamDTO> dto) {
         PagingVO<SkuMappingDTO.WarehousePagingViewDTO> pagingVO = skuMappingService.warehousePaging(dto);
         return success(pagingVO);
