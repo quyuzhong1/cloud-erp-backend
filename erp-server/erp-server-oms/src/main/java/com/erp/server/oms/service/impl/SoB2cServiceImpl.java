726889348855fcbf9b55224e1c5c2e960fb5bec4
liuruipeng <arthur@ulanzi.cn>
2024-08-28 11:57:08 +0800

    feat:【订单更新映射】 去掉平台仓订单更新映射限制

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -6931,7 +6931,9 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
         }
 
         if(TransferStatusEnum.SUCCESS.getCode().equals(entity.getTransferStatus())|| SoB2cBillStatusEnum.ENUM_SHIPPED.getCode().equals(entity.getBillStatus()) || SoB2cBillStatusEnum.ENUM_WAIT_SHIPPED.getCode().equals(entity.getBillStatus())){
-            throw new ServiceException("订单预报状态为预报成功或订单发货状态待发货&已发货 不允许更新映射");
+            if(!entity.hasPlatformWarehouseOrder()){
+                throw new ServiceException("订单预报状态为预报成功或订单发货状态待发货&已发货 不允许更新映射");
+            }
         }
         List<SoB2cDetailEntity> detailList = soB2cDetailService.listByMainIds(Collections.singletonList(entity.getId()));
         if (CollectionUtils.isEmpty(detailList)) {
