0eb80e99fa408bfe108976153caeb35448215401
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-09-01 11:53:06 +0800

    更新物流单状态问题处理

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillDetailServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillDetailServiceImpl.java
@@ -155,13 +155,14 @@ public class LogisticsBillDetailServiceImpl extends SuperServiceImpl<LogisticsBi
             throw new ServiceException(ApiError.NOT_EXIST_BILL, "自发货物流单详情");
         }
         String signCode = LogisticTrackStatusEnum.SIGN.getCode();
+        String manualCompleteCode = LogisticTrackStatusEnum.MANUAL_COMPLETE.getCode();
         String dbTrackStatus=detailEntity.getTrackStatus();
         if (signCode.equals(dbTrackStatus) && detailEntity.getIsApiUpdate()) {
             throw new ServiceException(ApiError.ERROR_NOT_UPDATE_TRACK_STATUS);
         }
         detailEntity.setIsApiUpdate(Boolean.FALSE);
         //表示签收
-        if (signCode.equals(trackStatus)) {
+        if (signCode.equals(trackStatus) || Objects.equals(manualCompleteCode, trackStatus)) {
             detailEntity.setSignTime(trackTime);
         } else {
             detailEntity.setSignTime(null);
