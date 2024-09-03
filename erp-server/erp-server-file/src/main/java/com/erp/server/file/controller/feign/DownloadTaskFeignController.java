de58700dd0c2a654902a37ecd63de30a332aadfc
luoweigang <821969511@qq.com>
2024-09-03 17:12:49 +0800

    修改下载中心流水号

--- erp-server/erp-server-file/src/main/java/com/erp/server/file/controller/feign/DownloadTaskFeignController.java
+++ erp-server/erp-server-file/src/main/java/com/erp/server/file/controller/feign/DownloadTaskFeignController.java
@@ -1,5 +1,8 @@
 package com.erp.server.file.controller.feign;
 
+import com.common.business.service.impl.RedisService;
+import com.common.business.utils.ExportUtil;
+import com.common.business.utils.RedisUtil;
 import com.common.core.utils.date.DateUtil;
 import com.erp.server.file.context.FileTaskContext;
 import com.erp.server.file.dto.FileTaskDTO;
@@ -14,13 +17,14 @@ public class DownloadTaskFeignController {
     @Resource
     private FileTaskContext fileTaskContext;
 
+    @Resource
+    private RedisService redisService;
+
     @PostMapping
     String saveDownloadTask(@RequestParam String fileName, @RequestParam String event, @RequestBody Object params) {
         //单据名称+年月日+流水号
-        StringBuffer sb = new StringBuffer();
-        String date = DateUtil.conversionDate(new Date(), DateUtil.DATE_PATTERN_SHORT_YEAR_NO_SP);
-        sb.append(fileName);
-        sb.append(date);
-        return fileTaskContext.add(new FileTaskDTO(event,sb.toString(), params));
+
+        String name = ExportUtil.getFileName(redisService, fileName);
+        return fileTaskContext.add(new FileTaskDTO(event,name, params));
     }
 }
