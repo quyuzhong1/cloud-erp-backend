a8e9f8a546e4c1c9c2766e11ec02504d3ffa5f8f
liuruipeng <arthur@ulanzi.cn>
2024-09-03 16:33:50 +0800

    feat:【头程优化分仓】 fba装箱消费方法

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/FbaShipmentPackingController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/FbaShipmentPackingController.java
@@ -35,38 +35,5 @@ public class FbaShipmentPackingController extends BaseController {
     @Resource
     private FbaShipmentPackingService fbaShipmentPackingService;
 
-    /**
-    * 新增
-    * @author lrp
-    * @date:  2024-09-03
-    * @param dto
-    * @return ApiResult<String>
-    */
-    @PostMapping("/add")
-    @LogAction(value = LogActionEnum.INSERT, desc = "fba货件装箱信息新增")
-    public ApiResult<BaseResultDTO.AddDTO> add(@RequestBody @Validated FbaShipmentPackingDTO.AddDTO dto) {
-        return success(fbaShipmentPackingService.add(dto));
-    }
-
-    /**
-    * 修改
-    * @author lrp
-    * @date:  2024-09-03
-    * @param dto
-    * @return ApiResult
-    */
-    @PostMapping("/update")
-    @LogAction(value = LogActionEnum.UPDATE, desc = "fba货件装箱信息修改")
-        @DataPermission(operationType = DataAttributeEnum.CHECK_BY_ID,
-        tableField = "create_user_id",
-        menuCode = "wms:fbaShipmentPacking:update",
-        serviceClass = FbaShipmentPackingService.class,
-        keyIdName = "id")
-    public ApiResult<?> update(@RequestBody @Validated FbaShipmentPackingDTO.UpdateDTO dto) {
-        fbaShipmentPackingService.update(dto);
-        return success();
-    }
-
-
 
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentPackingServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentPackingServiceImpl.java
@@ -3,25 +3,31 @@ package com.erp.server.wms.service.impl;
 
 import cn.hutool.core.collection.CollectionUtil;
 import com.common.business.annotation.DataIdempotent;
-import com.common.business.utils.CollectionUtils;
+import com.common.business.enums.PlatformDictEnum;
+import com.common.business.service.impl.SuperServiceImpl;
+import com.common.core.entity.BaseEntity;
+import com.erp.model.oms.dto.ListingInfoParamDTO;
+import com.erp.model.oms.dto.ListingInfoWithSkuMappingDTO;
+import com.erp.model.oms.enums.RuleTypeEnum;
+import com.erp.model.wms.dto.FbaShipmentPackingDTO;
 import com.erp.model.wms.entity.FbaShipmentEntity;
 import com.erp.model.wms.entity.FbaShipmentPackingEntity;
+import com.erp.rpc.oms.feign.SkuMappingFeign;
+import com.erp.server.wms.convert.FbaShipmentPackingConverter;
 import com.erp.server.wms.mapper.FbaShipmentPackingMapper;
 import com.erp.server.wms.service.FbaShipmentPackingService;
-import com.common.business.service.impl.SuperServiceImpl;
 import com.erp.server.wms.service.FbaShipmentService;
 import com.erp.server.wms.service.OperateLogService;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.stereotype.Service;
-import org.springframework.beans.factory.annotation.Autowired;
 import lombok.extern.slf4j.Slf4j;
-import com.erp.model.wms.dto.FbaShipmentPackingDTO;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 import javax.annotation.Resource;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -40,11 +46,14 @@ public class FbaShipmentPackingServiceImpl extends SuperServiceImpl<FbaShipmentP
     @Resource
     private FbaShipmentService fbaShipmentService;
 
+    @Resource
+    private SkuMappingFeign skuMappingFeign;
+
     @Override
-    @DataIdempotent(keyIdName = "data.fbaShipmentCode")
+    @DataIdempotent(keyIdName = "data.boxNo")
     @Transactional(rollbackFor = Exception.class)
     public void handle(FbaShipmentPackingDTO.PackingDTO data) {
-        if(Objects.isNull(data) || StringUtils.isBlank(data.getFbaShipmentCode())|| StringUtils.isBlank(data.getBoxNo())){
+        if(Objects.isNull(data) || StringUtils.isBlank(data.getFbaShipmentCode())|| StringUtils.isBlank(data.getBoxNo()) ||CollectionUtil.isEmpty(data.getDetailDTOList())){
             return;
         }
         FbaShipmentEntity fbaShipmentEntity = fbaShipmentService.getByCode(data.getFbaShipmentCode());
@@ -52,11 +61,35 @@ public class FbaShipmentPackingServiceImpl extends SuperServiceImpl<FbaShipmentP
             return;
         }
         String mainId = fbaShipmentEntity.getId();
-        List<FbaShipmentPackingEntity> existList = getByMainIdAndBoxNo(data.getFbaShipmentCode(), data.getBoxNo());
+        List<FbaShipmentPackingEntity> existList = getByMainIdAndBoxNo(mainId, data.getBoxNo());
         //已存在，删除后新增
         if(CollectionUtil.isNotEmpty(existList)){
-            List<String> removeIds = existList.stream().map(v->v.getId()).collect(Collectors.toList());
+            List<String> removeIds = existList.stream().map(BaseEntity::getId).collect(Collectors.toList());
+            log.error("删除装箱信息，idList:{}",removeIds);
             this.removeByIds(removeIds);
+        }
+        List<FbaShipmentPackingDTO.PackingDetailDTO> detailDTOList = data.getDetailDTOList();
+        List<String> mskuList = detailDTOList.stream().map(FbaShipmentPackingDTO.PackingDetailDTO::getMsku).collect(Collectors.toList());
+        ListingInfoParamDTO paramDTO = new ListingInfoParamDTO();
+        paramDTO.setPlatform(PlatformDictEnum.AMAZON.getCode());
+        paramDTO.setPlatformSkuNoList(mskuList);
+        paramDTO.setShopIdList(Collections.singletonList(fbaShipmentEntity.getShopId()));
+        paramDTO.setType(RuleTypeEnum.PLATFORM.getCode());
+        paramDTO.setMatchResult(true);
+        paramDTO.setIsExpire(false);
+        // 查询ListingInfo和skuMapping的关系
+        List<ListingInfoWithSkuMappingDTO> listingedInfoWithSkuMappingList = skuMappingFeign.listingInfoWithSkuMappingList(paramDTO);
+
+        // SKU相关信息
+        Map<String, ListingInfoWithSkuMappingDTO> listingInfoWithSkuMappingDTOMap = listingedInfoWithSkuMappingList.stream().collect(Collectors.toMap(ListingInfoWithSkuMappingDTO::getPlatformSkuNo, Function.identity()));
+
+        List<FbaShipmentPackingEntity> addList = new ArrayList<>();
+        detailDTOList.forEach(detailDTO -> {
+            FbaShipmentPackingEntity entity = FbaShipmentPackingConverter.INSTANCE.fbaShipmentPackingConvert(data.getBoxNo(),detailDTO,fbaShipmentEntity,listingInfoWithSkuMappingDTOMap.get(detailDTO.getMsku()));
+            addList.add(entity);
+        });
+        if(CollectionUtil.isNotEmpty(addList)){
+            this.saveBatch(addList);
         }
     }
 
