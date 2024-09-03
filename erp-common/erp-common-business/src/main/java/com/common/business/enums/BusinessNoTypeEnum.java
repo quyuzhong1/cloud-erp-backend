9dd0103dc978111c93933bd5ebee1f5069a73176
tanmujin <tanmujin@ulanzi.cn>
2024-09-03 18:57:44 +0800

    试产单：修改单号前缀

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/BusinessNoTypeEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/BusinessNoTypeEnum.java
@@ -87,7 +87,7 @@ public enum BusinessNoTypeEnum {
     CODE_JHBC(66, "JHBC","拣货波次",BusinessNoConstant.JHBC),
     CODE_QCFT(67, "QCFT","期初头程分摊",BusinessNoConstant.QCFT),
     CODE_CHCB(68, "CHCB","SKU成本分摊",BusinessNoConstant.CHCB),
-    CODE_SCLC(69, "SCLC","试产量产单",BusinessNoConstant.CHCB),
+    CODE_SCLC(69, "SCLC","试产量产单",BusinessNoConstant.SCLC),
 
     ;
 
