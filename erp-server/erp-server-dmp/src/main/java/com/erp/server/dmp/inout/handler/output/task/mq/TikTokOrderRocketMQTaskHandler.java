18b7e75288a244e34d5864f7abeb2e217a6665d7
luoweigang <821969511@qq.com>
2024-09-04 18:33:28 +0800

    修改美客多数据清洗(发货时间)

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/TikTokOrderRocketMQTaskHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/TikTokOrderRocketMQTaskHandler.java
@@ -151,7 +151,7 @@ public class TikTokOrderRocketMQTaskHandler extends DmpOutputRocketMQTaskHandler
 
 
         //平台订单号
-        orderDTO.setPlatformCode(dmpSoInfoEntity.getPlatformCode());
+        orderDTO.setPlatformCode(dmpSoInfoEntity.getThirdCode());
 
         //销售平台
         orderDTO.setDictPlatform(PlatformDictEnum.TIK_TOK.getCode());
