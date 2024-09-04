30f7d23512ab78ea880a74342f9f6bfc229bda87
luoweigang <821969511@qq.com>
2024-09-04 15:54:20 +0800

    修改美客多数据清洗

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/MercadoReceiverDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/MercadoReceiverDmpHandler.java
@@ -52,27 +52,37 @@ public class MercadoReceiverDmpHandler extends DmpInputDoNextDmpHandler {
                 Object shipmentId = shipmentIdMap.get("fid");
                 if (shipmentId != null) {
                     Map<String, Object> shipmentMap = dmpInputMongoChildList.stream().filter(req -> req.get("fid").equals(shipmentId)).findFirst().orElse(null);
-                    if (ObjectUtil.isNotEmpty(shipmentMap)) {
-                        //地址
-                        Map<String, Object> destinationMap = (Map<String, Object>) shipmentMap.get("destination");
-                        Object shippingAddressObj = destinationMap.get("shippingAddress");
-                        if (ObjectUtil.isNotEmpty(shippingAddressObj)) {
-                            Map<String, Object> shippingAddressMap = (Map<String, Object>) shippingAddressObj;
-                            Map<String, Object> countryMap = (Map<String, Object>) shippingAddressMap.get("country");
-                            Map<String, Object> stateMap = (Map<String, Object>) shippingAddressMap.get("state");
-                            Map<String, Object> cityMap = (Map<String, Object>) shippingAddressMap.get("city");
-                            detail.put("country", countryMap.get("fid"));
-                            detail.put("province", stateMap.get("name"));
-                            detail.put("city", cityMap.get("name"));
-                            detail.put("district", shippingAddressMap.get("addressLine"));
-                            detail.put("fullAddress", shippingAddressMap.get("comment"));
-                            Map<String, Object> neighborhoodMap = (Map<String, Object>) shippingAddressMap.get("neighborhood");
-                            Map<String, Object> municipalityMap = (Map<String, Object>) shippingAddressMap.get("municipality");
-                            detail.put("mainStreet", (ObjectUtil.isNotEmpty(neighborhoodMap.get("name")) ? neighborhoodMap.get("name") : "") + " "
-                                    + (ObjectUtil.isNotEmpty(municipalityMap.get("name")) ? municipalityMap.get("name") : "") + " "
-                                    + shippingAddressMap.get("comment"));
-                            detail.put("mainPhone", destinationMap.get("receiverPhone"));
-                        }
+
+                    //地址
+                    Map<String, Object> destinationMap = (Map<String, Object>) shipmentMap.get("destination");
+
+                    detail.put("receiverName", destinationMap.get("receiverName"));
+                    detail.put("receiverTelNumber", destinationMap.get("receiverPhone"));
+
+                    Object shippingAddressObj = destinationMap.get("shippingAddress");
+                    if (ObjectUtil.isNotEmpty(shippingAddressObj)) {
+                        Map<String, Object> shippingAddressMap = (Map<String, Object>) shippingAddressObj;
+                        Map<String, Object> countryMap = (Map<String, Object>) shippingAddressMap.get("country");
+                        Map<String, Object> stateMap = (Map<String, Object>) shippingAddressMap.get("state");
+                        Map<String, Object> cityMap = (Map<String, Object>) shippingAddressMap.get("city");
+                        detail.put("country", countryMap.get("fid"));
+
+                        detail.put("postCode", countryMap.get("zipCode"));
+                        detail.put("province", stateMap.get("name"));
+                        detail.put("city", cityMap.get("name"));
+                        detail.put("district", shippingAddressMap.get("addressLine"));
+
+                        Map<String, Object> neighborhoodMap = (Map<String, Object>) shippingAddressMap.get("neighborhood");
+                        Map<String, Object> municipalityMap = (Map<String, Object>) shippingAddressMap.get("municipality");
+
+                        detail.put("fullAddress", (ObjectUtil.isNotEmpty(neighborhoodMap.get("name")) ? neighborhoodMap.get("name") : "") + " "
+                                + (ObjectUtil.isNotEmpty(municipalityMap.get("name")) ? municipalityMap.get("name") : "") + " "
+                                + shippingAddressMap.get("comment"));
+
+                        detail.put("mainStreet", (ObjectUtil.isNotEmpty(neighborhoodMap.get("name")) ? neighborhoodMap.get("name") : "") + " "
+                                + (ObjectUtil.isNotEmpty(municipalityMap.get("name")) ? municipalityMap.get("name") : "") + " "
+                                + shippingAddressMap.get("comment"));
+                        detail.put("mainPhone", destinationMap.get("receiverPhone"));
                     }
                 }
             }
