ec5760bacb35074d7a63a2335dad093db8a7fe47
shukai <shukai@ulanzi.cn>
2024-09-06 10:35:02 +0800

    add:不删除用户

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/SysDepartmentUserServiceImpl.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/SysDepartmentUserServiceImpl.java
@@ -113,13 +113,9 @@ public class SysDepartmentUserServiceImpl extends ServiceImpl<SysDepartmentUserM
         Set<String> userIds = dto.getUserIds();
         String departmentId = dto.getDepartmentId();
 
-        List<SysDepartmentUserEntity> dbList = this.listByDepartmentIds(Arrays.asList(departmentId));
+        List<SysDepartmentUserEntity> dbList = lambdaQuery().eq(SysDepartmentUserEntity::getDepartmentId, departmentId).in(SysDepartmentUserEntity::getUserId, userIds).list();
         List<String> existUserIdList=dbList.stream().map(SysDepartmentUserEntity::getUserId).collect(Collectors.toList());
-        List<String> removeIdList = dbList.stream().filter(d -> !userIds.contains(d.getUserId())).
-                map(SysDepartmentUserEntity::getId).collect(Collectors.toList());
-        if(CollectionUtils.isNotEmpty(removeIdList)){
-             this.removeByIds(removeIdList);
-        }
+        
         List<String> addUserList=userIds.stream().filter(a->!existUserIdList.contains(a)).collect(Collectors.toList());
         //在添加
         List<SysDepartmentUserEntity> addList = new LinkedList<>();
@@ -132,7 +128,7 @@ public class SysDepartmentUserServiceImpl extends ServiceImpl<SysDepartmentUserM
         if (CollectionUtils.isNotEmpty(addList)) {
             return this.saveBatch(addList);
         }
-        return false;
+        return true;
     }
 
     @Override
