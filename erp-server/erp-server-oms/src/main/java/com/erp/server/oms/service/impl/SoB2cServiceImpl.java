403f3c5fdc0040d7ebc071e1cd950205cce18a09
liuruipeng <arthur@ulanzi.cn>
2024-09-10 18:46:13 +0800

    fix:销售订单导出新增出库时间

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -7666,7 +7666,17 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
             skuInventoryDTO.setSkuIdList(skuIdList);
             inventoryList = inventoryFeign.listSkuInventoryStatusByParam(skuInventoryDTO);
         }
+        //销售出库
+        List<String> soIds = records.stream().map(SoB2cDTO.ExcelExportDTO::getId).distinct().collect(Collectors.toList());
+        List<List<String>> partionSoIds = com.google.common.collect.Lists.partition(soIds, 5000);
+        List<SoOutstockEntity> soOutstockEntityList = new ArrayList<>();
+        partionSoIds.forEach(v->{
+            soOutstockEntityList.addAll(FeignQuery.create(SoOutstockEntity.class).in(SoOutstockEntity::getSoId,v).select(SoOutstockEntity::getSoId,SoOutstockEntity::getBillDate).list());
+        });
+
         for (SoB2cDTO.ExcelExportDTO exportDTO : records) {
+            SoOutstockEntity soOutstock = soOutstockEntityList.stream().filter(v->v.getSoId().equals(exportDTO.getId())).findFirst().orElse(new SoOutstockEntity());
+            exportDTO.setSoOutStockTime(soOutstock.getBillDate());
             //审核状态
             exportDTO.setApproveStatusName(ApproveStatusEnum.getName(exportDTO.getApproveStatus()));
             //订单状态
