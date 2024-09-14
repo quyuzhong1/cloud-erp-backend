5f302c9b38a70c8881f1bea2219b992de0e22671
Will <wangwei@ulanzi.cn>
2024-09-14 11:32:45 +0800

    fix,分货单权限配置调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/VirtualWarehouseAllocationController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/VirtualWarehouseAllocationController.java
@@ -222,6 +222,11 @@ public class VirtualWarehouseAllocationController extends BaseController {
      */
     @LogAction(value = LogActionEnum.EXPORT, desc = "导出分货单")
     @PostMapping("/export")
+    @DataPermission(operationType = DataAttributeEnum.LIST,
+            tableField = "create_user_id",
+            menuCode = "wms:virtualWarehouseAllocation:paging",
+            tableAlias = "vma"
+    )
     public ApiResult<Boolean> export(@RequestBody VirtualWarehouseAllocationDTO.ExportDTO dto) {
         virtualWarehouseAllocationService.export(dto);
         return success(true);
