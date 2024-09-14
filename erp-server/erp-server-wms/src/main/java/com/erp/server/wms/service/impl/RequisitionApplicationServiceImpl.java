278d9b402777943228f3303b013561489f825eae
liuruipeng <arthur@ulanzi.cn>
2024-09-14 18:20:57 +0800

    feat:【头程优化分仓】新增校验

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
@@ -1200,6 +1200,9 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
         }
 
         PackingTaskEntity packingTaskEntity = Optional.ofNullable(packingTaskService.getBySourceCode(entity.getCode())).orElseThrow(()-> new ServiceException("未生成装箱任务"));
+        if(!PackingTaskStatusEnum.PACKED.getCode().equals(packingTaskEntity.getPackingStatus())){
+            throw new ServiceException("已装箱才能下推发货单");
+        }
         List<WmsCartonEntity> cartonEntityList = Optional.ofNullable(cartonService.listByTaskIds(Arrays.asList(packingTaskEntity.getId()))).filter(list -> !list.isEmpty())
                 .orElseThrow(() -> new ServiceException("装箱数据为空"));
 
