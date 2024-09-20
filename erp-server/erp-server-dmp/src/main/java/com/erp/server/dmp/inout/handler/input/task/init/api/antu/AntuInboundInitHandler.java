be80a565b8734891d8b85fd5099a8bd6c453b105
luoweigang <821969511@qq.com>
2024-09-20 12:01:22 +0800

    【冲突文件处理 - luoweigang -安兔 】

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/antu/AntuInboundInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/antu/AntuInboundInitHandler.java
@@ -21,6 +21,7 @@ import com.erp.server.dmp.inout.utils.DmpHandlerCache;
 import com.sdk.wms.antu.dto.request.AntuGetReceiptReq;
 import com.sdk.wms.antu.dto.response.AntuReceiptResp;
 import com.sdk.wms.antu.dto.response.AntuResponse;
+import com.sdk.wms.antu.enums.AntuEnums;
 import com.sdk.wms.antu.utils.AntuUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * dmp输入init任务基础处理器下的安兔api获取数据方式
@@ -71,7 +73,7 @@ public class AntuInboundInitHandler extends DmpInputInitHandler {
             //列表数据较多情况下，进行分割集合
             List<List<String>> partition = ListUtil.partition(receiveCodeList, MathUtil.NUMBER_100);
 
-            for (int i = 1; i <= partition.size(); i++) {
+            for (int i = 0; i < partition.size(); i++) {
                 //查询数据
                 AntuGetReceiptReq antuGetReceiptReq = AntuGetReceiptReq.builder()
                         .page(i)
@@ -81,7 +83,11 @@ public class AntuInboundInitHandler extends DmpInputInitHandler {
                 String response = AntuUtils.callService(apiType, antuGetReceiptReq);
                 AntuResponse<List<AntuReceiptResp>> result = JSONObject.parseObject(response, new TypeReference<AntuResponse<List<AntuReceiptResp>>>() {
                 }.getType());
-                allResult.addAll(result.getData());
+
+                if ("Success".equals(result.getAsk())) {
+                    List<AntuReceiptResp> receiptRespList = result.getData().stream().filter(req -> AntuEnums.ReceivingStatusEnum.COMPLETE_LISTING.getCode().equals(req.getReceivingStatus())).collect(Collectors.toList());
+                    allResult.addAll(receiptRespList);
+                }
             }
         }
 
