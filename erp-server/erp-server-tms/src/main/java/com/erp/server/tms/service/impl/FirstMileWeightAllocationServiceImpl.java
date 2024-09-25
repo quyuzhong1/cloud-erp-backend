839eb1b5de945ffb3b10e51b3f5d754202c70765
jack chen <jackchen@ulanzi.cn>
2024-09-25 10:59:55 +0800

    2024-09-25 优化头程物流单下推头程重量分摊按箱号和创建时间排序

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileWeightAllocationServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileWeightAllocationServiceImpl.java
@@ -562,6 +562,10 @@ public class FirstMileWeightAllocationServiceImpl extends SuperServiceImpl<First
             if(cfgWeightAllocationType.equals("productWeight")){
                 entity.setAllocationType(WeightAllocationTypeEnum.PRODUCT_WEIGHT.getCode());
             }
+            Date now = new Date();
+            Instant instant = now.toInstant();
+            LocalDateTime creatTime = instant.atZone(ZoneId.systemDefault()).toLocalDateTime();
+            entity.setCreateTime(creatTime);
             saveList.add(entity);
         }
         computeAllocationWeight(saveList, cfgWeightAllocationType, logisticsChannelEntity);
