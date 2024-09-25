411e2cbcb13d002bbfb23e3b4298145ef20b30ae
luoweigang <821969511@qq.com>
2024-09-25 15:43:07 +0800

    修改中台同步时把无需同步状态取消

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/api/DmpInoutController.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/controller/api/DmpInoutController.java
@@ -257,6 +257,7 @@ public class DmpInoutController extends BaseController {
 										parseObject.put("dmpOutputTaskRecordId", q.getId());
 										q.setRequestData(JSON.toJSONString(parseObject));
 										q.setStatus(DmpOutputTaskRecordStatusEnum.INIT.getCode());
+										q.setIsNeedSync(Boolean.FALSE);
 									}
 									dmpOutputTaskRecordService.updateBatchById(queryDataList);
 									dmpOutputTaskRecordService.batchSync(queryDataList);
