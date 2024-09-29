ea299e3db45461bbea09b4d4d628dd2dea3ddc67
liuruipeng <arthur@ulanzi.cn>
2024-09-29 09:45:02 +0800

    fix:b2c销售订单查询修复

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/SoB2cQueryHandler.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/SoB2cQueryHandler.java
@@ -23,7 +23,7 @@ public class SoB2cQueryHandler extends AbstractQueryHandler {
         }
 
         if("sku".equals(field)){
-            return " EXISTS (SELECT 1 from so_b2c_detail sbd where sbd.main_id = sb2c.id and sbd.sku_no "+ compareCodeSplicingValueSql +" ) ";
+            return " EXISTS (SELECT 1 from so_b2c_detail sbd where sbd.is_deleted = false and sbd.main_id = sb2c.id and sbd.sku_no "+ compareCodeSplicingValueSql +" ) ";
         }
 
         if("platformSpuNo".equals(field)){
