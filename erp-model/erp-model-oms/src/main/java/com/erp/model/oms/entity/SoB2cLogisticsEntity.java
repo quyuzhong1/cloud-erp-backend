652df86d33d6469d6fa5dc96e79732d2a6395916
liuruipeng <arthur@ulanzi.cn>
2024-10-08 17:11:31 +0800

    feat:【更新跟踪号】调整字段名

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cLogisticsEntity.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cLogisticsEntity.java
@@ -151,8 +151,8 @@ public class SoB2cLogisticsEntity extends BaseEntity<SoB2cLogisticsEntity> {
     @TableField("transfer_logistics_channel_id")
     private String transferLogisticsChannelId;
 
-    @TableField("source_type")
-    private String sourceType;
+    @TableField("source_system")
+    private String sourceSystem;
 
     public static final String MAIN_ID = "main_id";
 
