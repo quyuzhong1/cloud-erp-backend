7062b69df19adc7740e84eefda60d0f7427a121d
liuruipeng <arthur@ulanzi.cn>
2024-10-14 16:40:32 +0800

    feat:tiktok,美客多销售出库单重复处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
@@ -2545,6 +2545,12 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
 
     @Override
     public Boolean handleCreateB2cSoOutstockWithoutTx(SoOutstockDTO.GenerateB2cDTO dto) {
+        SoOutstockEntity soOutstock = this.getBySoId(dto.getSoId());
+        if(Objects.nonNull(soOutstock)
+        && (PlatformDictEnum.TIK_TOK.getCode().equals(dto.getDictPlatform())
+        || PlatformDictEnum.MERCADOLIBRE.getCode().equals(dto.getDictPlatform()))){
+            return true;
+        }
         String id = soOutstockService.addB2cSoOutstock(dto);
         //表示添加成功
         if (StringUtils.isNotBlank(id)) {
