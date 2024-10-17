f52d8dd10be078994804e742817e3472728e8693
jack chen <jackchen@ulanzi.cn>
2024-10-17 18:17:52 +0800

    2024-10-17 试产量产首页审批功能开发

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/constant/NoticeMessageConstant.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/constant/NoticeMessageConstant.java
@@ -195,6 +195,13 @@ public interface NoticeMessageConstant {
     String AUDIT_PILOT_MSG_CONTENT = "**通知类型：**%s\n**产品经理：**%s\n**SKU：**%s";
 
 
+    /**
+     * 试产量产催办
+     */
+    String AUDIT_PILOT_PRESS_TITLE = "【加急催办】[%s]提醒【%s】有尚未完成审核，请及时审核";
+    String AUDIT_PILOT_PRESS_CONTENT = "**通知类型：**%s\n**产品经理：**%s\n**SKU：**%s";
+
+
 
 
 
