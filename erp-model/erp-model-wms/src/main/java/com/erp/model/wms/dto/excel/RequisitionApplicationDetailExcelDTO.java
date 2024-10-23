73669710d52e8f35b0260372b8113ecaf97a5364
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-10-23 16:23:00 +0800

    要货申请下推FBA发货单时增加装箱匹配导入功能2

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/excel/RequisitionApplicationDetailExcelDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/excel/RequisitionApplicationDetailExcelDTO.java
@@ -23,41 +23,41 @@ public class RequisitionApplicationDetailExcelDTO implements Serializable {
     /**
      * 装箱SKU
      */
-    @ExcelProperty(value = "装箱SKU", index = 0)
+    @ExcelProperty(value = "装箱SKU", index = 1)
     @FieldValid(fieldName = "装箱SKU", isNotBlank = true)
     private String  packingSku;
 
     /**
      * 期初数量
      */
-    @ExcelProperty(value = "装箱FNSKU", index = 1)
+    @ExcelProperty(value = "装箱FNSKU", index = 2)
     @FieldValid(fieldName = "装箱FNSKU")
     private String  packingFnSku;
 
     /**
      * 数量
      */
-    @ExcelProperty(value = "数量", index = 2)
+    @ExcelProperty(value = "数量", index = 3)
     @FieldValid(fieldName = "数量", isNotBlank = true, formatPattern = FieldFormatPatternTypeEnum.POSITIVEINTEGER,maxLength = 16)
     private String  qty;
 
     /**
      * 货件号
      */
-    @ExcelProperty(value = "*货件号", index = 3)
+    @ExcelProperty(value = "*货件号", index = 4)
     @FieldValid(fieldName = "货件号",isNotBlank = true)
     private String  fbaShipmentCode;
     /**
      * 货件箱号
      */
-    @ExcelProperty(value = "*货件箱号", index = 3)
+    @ExcelProperty(value = "*货件箱号", index = 5)
     @FieldValid(fieldName = "货件箱号",isNotBlank = true, formatPattern = FieldFormatPatternTypeEnum.POSITIVEINTEGER,maxLength = 16)
     private String  fbaBoxNo;
 
     /**
      * 错误数据
      */
-    @ExcelProperty(value = "错误数据", index = 4)
+    @ExcelProperty(value = "错误数据", index = 6)
     private String  errorMsg;
 
 }
