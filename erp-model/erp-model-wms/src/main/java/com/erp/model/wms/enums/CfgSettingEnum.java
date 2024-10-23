664bc6407610a26ed921f1286e1e65379175fe1a
jack chen <jackchen@ulanzi.cn>
2024-10-23 10:02:29 +0800

    2024-10-23 要货申请各个节点 飞书系统配置

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/CfgSettingEnum.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/CfgSettingEnum.java
@@ -16,8 +16,11 @@ public enum CfgSettingEnum implements EnumMessage {
     FINISH_PACKING_NOTICE("finishPackingNotice","装箱完成通知"),
     CFG_PRINT("cfgPrint", "打印配置"),
     SUBCONTRACT_IN_STOCK("subcontractInStock", "委外入库"),
-
     WAREHOUSE_LOCATION_MOVE_BLACKLIST("warehouseLocationMoveBlacklist","库位移动同步旺店通黑名单"),
+    FS_REQUISITION_WAITHANDLE_NOTICE("fsRequisitionWaitHandle","飞书要货申请待处理配置"),
+    FS_REQUISITION_HANDLEING_NOTICE("fsRequisitionHandleIngNotice","飞书要货申请处理中配置"),
+    FS_REQUISITION_PACKING_NOTICE("fsRequisitionPackingNotice","飞书要货申请已装箱配置"),
+    FS_FIRSTMILEDELIVERY_WAITHANDLE_NOTICE("fsFirstMileDeliveryWaitHandleNotice","飞书头程发货单待处理配置"),
     FS_REQUISITION_NOTICE("fsRequisitionNotice","飞书要货申请通知配置")
     ;
 
