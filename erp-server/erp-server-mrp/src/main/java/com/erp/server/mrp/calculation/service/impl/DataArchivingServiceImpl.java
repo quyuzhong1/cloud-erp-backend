46542515211bab78f81027c1f728656b3be61f1e
shukai <shukai@ulanzi.cn>
2024-10-24 15:57:12 +0800

    add:加入xxl配置，xxl参数使用help获取，任务加入分布式锁

--- erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/calculation/service/impl/DataArchivingServiceImpl.java
+++ erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/calculation/service/impl/DataArchivingServiceImpl.java
@@ -8,13 +8,18 @@ import com.erp.rpc.oms.feign.ShopInfoFeign;
 import com.erp.server.mrp.calculation.service.BasicReplenishmentDataService;
 import com.erp.server.mrp.calculation.service.DataArchivingService;
 import com.erp.server.mrp.service.CfgDataArchivingService;
+
+import cn.hutool.core.date.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @Slf4j
@@ -27,30 +32,45 @@ public class DataArchivingServiceImpl implements DataArchivingService {
 
     @Resource
     private ShopInfoFeign shopInfoFeign;
+    
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
 
     @Override
     public void dataArchiving(LocalDate calculationDate) {
-        calculationDate = ObjectUtils.isEmpty(calculationDate) ? LocalDate.now() : calculationDate;
-        List<CfgDataArchivingEntity> effectiveData = cfgDataArchivingService.getEffectiveData();
-        log.warn("开始处理归档数据,时间{}", System.currentTimeMillis());
-        for (CfgDataArchivingEntity config : effectiveData) {
-            // 执行归档逻辑
-            cfgDataArchivingService.archiveData(config);
-        }
-        log.warn("完成处理归档数据,时间{}", System.currentTimeMillis());
-        log.warn("开始增量更新建议基础数据,时间{}", System.currentTimeMillis());
-        //获取所有店铺
-        ApiResult<List<ShopInfoEntity>> allShopResult = shopInfoFeign.list();
-        if (!allShopResult.isSuccess()) {
-            throw new ServiceException(allShopResult.getMsg());
-        }
-        //增量更新建议基础数据
-        replenishmentDataService.initReplenishmentSku(calculationDate, allShopResult.getData());
-        log.warn("完成增量更新建议基础数据,时间{}", System.currentTimeMillis());
-        //清洗历史销量和库存
-        log.warn("开始清洗历史销量和库存数据,时间{}", System.currentTimeMillis());
-        replenishmentDataService.cleanHistorySalesAndInventory(calculationDate, allShopResult.getData());
-        log.warn("开始清洗历史销量和库存数据,时间{}", System.currentTimeMillis());
+    	String redisKey = "mrp:data:archiving";
+		if(redisTemplate.opsForValue().setIfAbsent(redisKey, DateUtil.now(), 7200, TimeUnit.SECONDS)) {
+			try {
+				calculationDate = ObjectUtils.isEmpty(calculationDate) ? LocalDate.now() : calculationDate;
+				List<CfgDataArchivingEntity> effectiveData = cfgDataArchivingService.getEffectiveData();
+				log.warn("开始处理归档数据,时间{}", System.currentTimeMillis());
+				for (CfgDataArchivingEntity config : effectiveData) {
+				    // 执行归档逻辑
+				    cfgDataArchivingService.archiveData(config);
+				}
+				log.warn("完成处理归档数据,时间{}", System.currentTimeMillis());
+				log.warn("开始增量更新建议基础数据,时间{}", System.currentTimeMillis());
+				//获取所有店铺
+				ApiResult<List<ShopInfoEntity>> allShopResult = shopInfoFeign.list();
+				if (!allShopResult.isSuccess()) {
+				    throw new ServiceException(allShopResult.getMsg());
+				}
+				//增量更新建议基础数据
+				replenishmentDataService.initReplenishmentSku(calculationDate, allShopResult.getData());
+				log.warn("完成增量更新建议基础数据,时间{}", System.currentTimeMillis());
+				//清洗历史销量和库存
+				log.warn("开始清洗历史销量和库存数据,时间{}", System.currentTimeMillis());
+				replenishmentDataService.cleanHistorySalesAndInventory(calculationDate, allShopResult.getData());
+				log.warn("开始清洗历史销量和库存数据,时间{}", System.currentTimeMillis());
+			} catch (Exception e) {
+				log.error("处理MRP归档失败" , e);
+				throw e;
+			} finally {
+				redisTemplate.delete(redisKey);
+			}
+		}else {
+			throw new ServiceException("处理MRP归档任务正在进行中");
+		}
     }
 
     @Override
--- erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/schedule/DataArchivingJob.java
+++ erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/schedule/DataArchivingJob.java
@@ -1,10 +1,13 @@
 package com.erp.server.mrp.schedule;
 
+import com.common.core.utils.date.LocalDateUtil;
 import com.erp.server.mrp.calculation.service.DataArchivingService;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.lang.StringUtils;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
@@ -25,8 +28,15 @@ public class DataArchivingJob {
      */
     @XxlJob("dataArchiving")
     public ReturnT dataArchiving(LocalDate calculationDate) {
+    	if(calculationDate == null) {
+    		String calculationDateParam = XxlJobHelper.getJobParam();
+    		if(StringUtils.isNotBlank(calculationDateParam)) {
+    			calculationDate = LocalDateUtil.parseStrToLocalDate(calculationDateParam);
+    		}
+    	}
+    	LocalDate finCalculationDate = calculationDate;
         XxlJobHelper.log("====开始更新信息====");
-        CompletableFuture.runAsync(() -> dataArchivingService.dataArchiving(calculationDate));
+        CompletableFuture.runAsync(() -> dataArchivingService.dataArchiving(finCalculationDate));
         return ReturnT.SUCCESS;
     }
 }
