ea68537ec33d70be1a066d61de81989a0c160fe9
luoweigang <821969511@qq.com>
2024-10-28 19:15:00 +0800

    TikTok接口更新3

--- erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpSoRefundDetailEntity.java
+++ erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpSoRefundDetailEntity.java
@@ -90,8 +90,11 @@ public class DmpSoRefundDetailEntity extends BaseEntity<DmpSoRefundDetailEntity>
     */
     @TableField("data_encrypt")
     private String dataEncrypt;
-
-
+    /**
+     * 当前明细总税费(币种跟主单一致)
+     */
+    @TableField("tax_amount")
+    private BigDecimal taxAmount;
     /**
      * 拓展字段
      */
