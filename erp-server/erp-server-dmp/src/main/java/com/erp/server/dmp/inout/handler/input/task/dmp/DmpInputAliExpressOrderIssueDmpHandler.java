baa09fe3204f82138a6ffef0889a6eb098af50e4
shukai <shukai@ulanzi.cn>
2024-10-29 15:01:28 +0800

    add:退货明细

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputAliExpressOrderIssueDmpHandler.java
@@ -0,0 +1,48 @@
+package com.erp.server.dmp.inout.handler.input.task.dmp;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.common.core.entity.BaseEntity;
+import com.erp.model.dmp.entity.DmpCfgInputConvertEntity;
+
+import cn.hutool.core.collection.CollUtil;
+
+/**
+ * dmp处理金蝶明细子类任务handler，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
+ * @author Administrator
+ *
+ */
+@Service
+@Scope("prototype")
+public class DmpInputAliExpressOrderIssueDmpHandler extends DmpInputAliExpressOrderDoChildDmpHandler{
+	
+	@Override
+	protected void putDmpId(List<Map<String, Object>> dmpInputMongoChildEntityList) {
+		DmpCfgInputConvertEntity mainConvertId = this.getMainConvertId();
+		String parentStorageName = mainConvertId.getStorageName();
+		ServiceImpl parentServiceImpl = this.getServiceImpl(parentStorageName);
+		QueryWrapper<?> wrapper = new QueryWrapper<>();
+		wrapper.eq(INPUT_TASK_ID, inputTaskId);
+		List<Map<String, Object>> listMaps = parentServiceImpl.listMaps(wrapper);
+		Map<String, String> billNoIdMap = new HashMap<>();
+		if(CollUtil.isNotEmpty(listMaps)) {
+			for(Map<String, Object> listMap : listMaps) {
+				billNoIdMap.put(listMap.get("third_code").toString(), listMap.get(BaseEntity.ID).toString());
+			}
+		}
+		for(Map<String, Object> dmpInputMongoChildEntity : dmpInputMongoChildEntityList) {
+			String billNo = dmpInputMongoChildEntity.get("parent_order_id").toString();
+			String dmpId = billNoIdMap.get(billNo);
+			dmpInputMongoChildEntity.put(MAIN_ID, dmpId);
+			dmpInputMongoChildEntity.put("shopId", nextLevelId);
+			dmpInputMongoChildEntity.put("sourceId", dmpId);
+		}
+	}
+}
