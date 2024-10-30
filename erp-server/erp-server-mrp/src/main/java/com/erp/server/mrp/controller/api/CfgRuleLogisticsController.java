6a7f427cfb7205a5bf1353b6586bd4c602ddcbac
Will <wangwei@ulanzi.cn>
2024-10-30 10:31:49 +0800

    feat,智能补货代码提交

--- erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/controller/api/CfgRuleLogisticsController.java
+++ erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/controller/api/CfgRuleLogisticsController.java
@@ -7,7 +7,9 @@ import com.common.core.controller.vo.ApiResult;
 import com.erp.model.mrp.dto.CfgRuleLogisticsDTO;
 import com.erp.server.mrp.service.CfgRuleLogisticsService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +38,7 @@ public class CfgRuleLogisticsController extends BaseController {
      * @return ApiResult<List<SelectLogisticsDTO>>
      */
     @PostMapping("/selectLogistics")
-    public ApiResult<List<CfgRuleLogisticsDTO.SelectLogisticsDTO>> selectLogistics(CfgRuleLogisticsDTO.SelectLogisticsParamDTO paramDTO) {
+    public ApiResult<List<CfgRuleLogisticsDTO.SelectLogisticsDTO>> selectLogistics(@RequestBody @Validated CfgRuleLogisticsDTO.SelectLogisticsParamDTO paramDTO) {
         List<CfgRuleLogisticsDTO.SelectLogisticsDTO> list = cfgRuleLogisticsService.selectLogistics(paramDTO);
         return success(list);
     }
