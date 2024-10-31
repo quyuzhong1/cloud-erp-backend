ccb870caab6d62bb94c95414fa1e452c52617f07
jack chen <jackchen@ulanzi.cn>
2024-10-31 16:41:33 +0800

    2024-10-31 销售出库单显示和导出优化

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoOutstockDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoOutstockDTO.java
@@ -386,6 +386,11 @@ public class SoOutstockDTO implements Serializable {
          * 物流渠道名称
          */
         private String logisticsChannelName;
+
+        /**
+         * 销售平台
+         */
+        private String dictPlatform;
     }
 
     /**·
