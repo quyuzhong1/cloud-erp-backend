62eab72f2e7c081c044baf6b23ba870b8a74d7aa
shukai <shukai@ulanzi.cn>
2024-11-04 17:27:52 +0800

    add:旺店通销售出库新加订单标签字段

--- erp-common/erp-common-business/src/main/java/com/common/business/dto/WdtSoOutStockDTO.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/dto/WdtSoOutStockDTO.java
@@ -244,6 +244,11 @@ public class WdtSoOutStockDTO extends UniqueDto {
      * 物流公司名称
      */
     private String logisticsCompanyName;
+    
+    /**
+     * 订单标签
+     */
+    private String tradeLabel;
 
     private List<WdtSoOutStockDetailDTO> detailList;
 }
