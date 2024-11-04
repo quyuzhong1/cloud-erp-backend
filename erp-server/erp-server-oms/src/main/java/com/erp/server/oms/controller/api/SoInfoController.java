e6892bb994e6052292e918e39d48f077787d7ee3
Will <wangwei@ulanzi.cn>
2024-11-04 19:06:37 +0800

    feat,销售合同按模板导出

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoInfoController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoInfoController.java
@@ -18,7 +18,6 @@ import com.common.core.enums.LogActionEnum;
 import com.erp.model.oms.dto.SoDetailDTO;
 import com.erp.model.oms.dto.SoInfoDTO;
 import com.erp.model.oms.dto.listAddDetailViewDTO;
-import com.erp.model.oms.entity.SoB2cEntity;
 import com.erp.model.oms.entity.SoChangeEntity;
 import com.erp.model.oms.entity.SoDetailEntity;
 import com.erp.model.oms.entity.SoInfoEntity;
@@ -470,15 +469,14 @@ public class SoInfoController extends BaseController {
      * @date 2023-05-18 12:01
      */
     @LogAction(value = LogActionEnum.EXPORT, desc = "导出销售订单合同PDF")
-    @GetMapping("/exportSoContractPdf")
+    @PostMapping("/exportSoContractPdf")
     @DataPermission(operationType = DataAttributeEnum.CHECK_BY_ID,
             tableField = "create_user_id,seller_id",
             menuCode = "oms:so:exportSoContractPdf",
             serviceClass = SoInfoService.class,
             keyIdName = "id")
-    public ApiResult<SoInfoDTO.ExportPdfDTO> exportSoContractPdf(@RequestParam("id") String id) {
-        SoInfoDTO.ExportPdfDTO result = soInfoService.exportSoContractPdf(id);
-        return success(result);
+    public void exportSoContractPdf(@RequestBody @Valid BaseIdDTO dto, HttpServletResponse response) {
+         soInfoService.exportSoContractPdf(dto.getId(),response);
     }
 
     /**
