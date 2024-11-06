31095c6ee659592c03c1e70108015bf949242042
Will <wangwei@ulanzi.cn>
2024-11-06 15:51:00 +0800

    feat,智能补货代码提交

--- erp-model/erp-model-mrp/src/main/java/com/erp/model/mrp/dto/PurchaseSuggestMergeDTO.java
+++ erp-model/erp-model-mrp/src/main/java/com/erp/model/mrp/dto/PurchaseSuggestMergeDTO.java
@@ -1,12 +1,10 @@
 package com.erp.model.mrp.dto;
 
 import cn.hutool.json.JSONArray;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.common.business.dto.AdvanceQueryDTO;
 import com.common.business.dto.base.SortDTO;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.ibatis.type.JdbcType;
 
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
