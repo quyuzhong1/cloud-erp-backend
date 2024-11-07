dc3456b7f035b017946b58cd2277f65706496ef3
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-11-07 09:23:38 +0800

    【销售流程优化】16、装箱任务显示未装明细

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/PackingTaskController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/PackingTaskController.java
@@ -268,7 +268,7 @@ public class PackingTaskController extends BaseController {
      * @date 2024-11-06
      * @param dto
      */
-    @PostMapping("/exportPackingDetail")
+    @PostMapping("/exportNotPackingDetail")
     @LogAction(value = LogActionEnum.EXPORT, desc = "导出未装箱明细Excel")
     public ApiResult exportNotPackingDetail(@RequestBody @Validated PackingTaskDTO.ExportDTO dto) {
         packingTaskService.exportNotPackingDetail(dto);
