f753fc73769665a3584e92f6e5cdf4ca9354e562
jack chen <jackchen@ulanzi.cn>
2024-11-08 16:05:29 +0800

    2024-11-07 fix 快粘贴参数

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoReturnDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoReturnDTO.java
@@ -587,5 +587,9 @@ public class SoReturnDTO {
          * 客户id
          */
         private String cutomerId;
+        /**
+         * 退货销售单id
+         */
+        private String id;
     }
 }
--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SkuMappingServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SkuMappingServiceImpl.java
@@ -1365,9 +1365,6 @@ public class SkuMappingServiceImpl extends SuperServiceImpl<SkuMappingMapper, Sk
 
     @Override
     public List<SkuMappingDTO.ProductSkuInfoDTO> listSkuBySkuNos(SkuMappingDTO.SkuParamDTO skuParamDTO) {
-        if(StringUtils.isBlank(skuParamDTO.getCutomerId()) || CollectionUtils.isEmpty(skuParamDTO.getSkuNoList())){
-            return Collections.emptyList();
-        }
         return this.baseMapper.listSkuBySkuNos(skuParamDTO);
     }
 
