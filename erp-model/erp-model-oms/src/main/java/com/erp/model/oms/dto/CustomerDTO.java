011f7fc46b9d69160a57f640a0327f1717babe77
jack chen <jackchen@ulanzi.cn>
2024-11-13 09:33:23 +0800

    2024-11-12 fix 客户远程搜索限制已审核状态

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/CustomerDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/CustomerDTO.java
@@ -190,6 +190,14 @@ public class CustomerDTO implements Serializable {
          * 名称
          */
         private String code;
+        /**
+         * 简称
+         */
+        private String shortName;
+        /**
+         * 简称
+         */
+        private String approveStatus;
     }
     /**
      * 远程搜索
@@ -780,6 +788,10 @@ public class CustomerDTO implements Serializable {
          * name
          */
         private String name;
+        /**
+         * 简称
+         */
+        private String shortName;
 
         private ApproveStatusEnum approveStatus;
 
