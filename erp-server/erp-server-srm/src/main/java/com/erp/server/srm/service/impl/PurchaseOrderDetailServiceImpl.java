a72336ea4de1730c3e21caab99b0391886fd1cbf
jack chen <jackchen@ulanzi.cn>
2024-11-16 14:16:06 +0800

    2024-11-16 srm 代码规范

--- erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PurchaseOrderDetailServiceImpl.java
+++ erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PurchaseOrderDetailServiceImpl.java
@@ -35,7 +35,10 @@ import com.erp.rpc.scm.feign.SupplierFeign;
 import com.erp.rpc.wms.feign.WmsTaskFeign;
 import com.erp.server.srm.convert.PurchaseOrderConverter;
 import com.erp.server.srm.mapper.PurchaseOrderDetailMapper;
-import com.erp.server.srm.service.*;
+import com.erp.server.srm.service.DeliveryOrderDetailService;
+import com.erp.server.srm.service.DeliveryOrderService;
+import com.erp.server.srm.service.OperateLogService;
+import com.erp.server.srm.service.PurchaseOrderDetailService;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
@@ -48,7 +51,6 @@ import javax.annotation.Resource;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
