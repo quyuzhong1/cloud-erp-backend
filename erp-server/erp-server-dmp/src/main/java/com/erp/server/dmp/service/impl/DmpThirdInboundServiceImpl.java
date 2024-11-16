e737bf5f6fb155c3ae3444b416210dc5cc2f1ecb
shukai <shukai@ulanzi.cn>
2024-11-16 10:35:37 +0800

    add:dmp

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpThirdInboundServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/DmpThirdInboundServiceImpl.java
@@ -65,7 +65,7 @@ public class DmpThirdInboundServiceImpl extends SuperServiceImpl<DmpThirdInbound
     @Override
     public Boolean update(DmpThirdInboundDTO.UpdateDTO updateDTO) {
         DmpThirdInboundEntity old = super.getById(updateDTO.getId());
-        Optional.ofNullable(old).orElseThrow(()->new ServiceException(ApiError.NOT_EXIST_BILL, "第三方仓库存"));
+        old = Optional.ofNullable(old).orElseThrow(()->new ServiceException(ApiError.NOT_EXIST_BILL, "第三方仓库存"));
         DmpThirdInboundEntity dmpThirdInboundEntity =  BeanMapperUtils.map(DmpThirdInboundEntity.class, updateDTO);
 
         // 数据处理
