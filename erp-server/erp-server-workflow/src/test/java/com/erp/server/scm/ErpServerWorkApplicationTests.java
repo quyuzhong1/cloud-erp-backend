4a273289447ae998e51c58c8c8099d87012ab1a7
Jim <zhoujingbo@ulanzi.cn>
2024-11-16 16:42:50 +0800

    UPDATE:规范优化11

--- erp-server/erp-server-workflow/src/test/java/com/erp/server/scm/ErpServerWorkApplicationTests.java
+++ erp-server/erp-server-workflow/src/test/java/com/erp/server/scm/ErpServerWorkApplicationTests.java
@@ -1,8 +1,10 @@
 package com.erp.server.scm;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.erp.model.workflow.dto.AuditorHandleDTO;
 import com.erp.server.workflow.service.ProcessTaskService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,7 @@ import java.util.List;
  * @Date 2023-04-03 9:21
  * @Created by yl
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {ErpServerWorkApplicationTests.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Profile("dev")
@@ -31,8 +34,7 @@ public class ErpServerWorkApplicationTests {
     @Test
     public void testAuditorHandleDTO() {
         List<AuditorHandleDTO> resultList =  processTaskService.getHistoryTaskByProcessId("fd226ba4-d76a-11ed-901e-2ed55b4d95a2");
-
-        System.out.println(JSONObject.toJSON(resultList));
+        log.warn(JSON.toJSONString(resultList));
     }
 
 
