f3dd0ba1003e607adf9581ad287511c55ba00f69
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-11-18 11:12:56 +0800

    合并处理

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoB2cDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/SoB2cDTO.java
@@ -2895,16 +2895,6 @@ public class SoB2cDTO implements Serializable {
          */
         private LocalDateTime finishPrintTime;
 
-        /**
-         * 提交发货时间
-         */
-        private LocalDateTime createDeliveryTime;
-
-        /**
-         * 面单打印时间
-         */
-        private LocalDateTime finishPrintTime;
-
         //get方法
         private String getLengthStr () {
             return this.length.stripTrailingZeros().toPlainString();
