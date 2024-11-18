5c4a08e5fc519d9d52e4b3a0fc24921708a17084
liuruipeng <arthur@ulanzi.cn>
2024-11-18 17:58:56 +0800

    feat:【发货通知变更】高级查询

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SoDeliveryNoticeQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/SoDeliveryNoticeQueryHandler.java
@@ -4,7 +4,7 @@ import com.common.business.enums.ApproveStatusEnum;
 import com.common.business.enums.QueryConditionEnum;
 import com.common.business.enums.QueryDataTypeEnum;
 import com.common.business.query.AbstractQueryHandler;
-import com.erp.model.wms.enums.PoReturnConfirmStatusEnum;
+import com.erp.model.wms.enums.PackingTaskStatusEnum;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -33,27 +33,9 @@ public class SoDeliveryNoticeQueryHandler extends AbstractQueryHandler {
 
         if("isPacked".equals(field)) {
             if((Boolean) value){
-                return "  sdn.id in ( SELECT main_id FROM so_delivery_notice_detail sdnd WHERE sdnd.is_deleted = FALSE " +
-                        "    GROUP BY main_id HAVING COUNT(*) = COUNT(     CASE " +
-                        "            WHEN (SELECT COALESCE(SUM(pd.qty), 0) " +
-                        "                  FROM picking_detail pd " +
-                        "                  WHERE pd.is_deleted = FALSE AND pd.source_detail_id = sdnd.id) = sdnd.delivery_qty " +
-                        "            THEN 1 " +
-                        "            ELSE NULL " +
-                        "        END " +
-                        "    ) " +
-                        " )";
+                super.buildDefaultDTO("pt.packing_status", PackingTaskStatusEnum.PACKED.getCode());
             }else{
-                return "  sdn.id not in ( SELECT main_id FROM so_delivery_notice_detail sdnd WHERE sdnd.is_deleted = FALSE " +
-                        "    GROUP BY main_id HAVING COUNT(*) = COUNT(     CASE " +
-                        "            WHEN (SELECT COALESCE(SUM(pd.qty), 0) " +
-                        "                  FROM picking_detail pd " +
-                        "                  WHERE pd.is_deleted = FALSE AND pd.source_detail_id = sdnd.id) = sdnd.delivery_qty " +
-                        "            THEN 1 " +
-                        "            ELSE NULL " +
-                        "        END " +
-                        "    ) " +
-                        " )";
+                super.buildSplicingSQLDTO("pt.packing_status",QueryConditionEnum.NE,PackingTaskStatusEnum.PACKED.getCode() ,QueryDataTypeEnum.STRING);
             }
         }
         return null;
