b0b6dbfca4e5bacf026e905fcb0aaf16d365dfe8
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-11-19 19:57:02 +0800

    头程物流单生成头程对账单

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsFirstMileLogisticServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsFirstMileLogisticServiceImpl.java
@@ -1196,17 +1196,8 @@ public class TmsFirstMileLogisticServiceImpl extends SuperServiceImpl<LogisticsB
         if (CharSequenceUtil.isBlank(currency)){
             throw new ServiceException("物流单费用配置币种类型不能为空");
         }
-        //修改业务编码
         //发货单
-        List<String> deliveryIds = sourceDetailList.stream().map(TmsFirstMileReconciliationDetailDTO.ListDTO::getDeliveryId).distinct().collect(Collectors.toList());
-        List<FirstMileDeliveryDTO.BusinessDTO> businessDTOList = wmsFirstMileDeliveryFeign.getBusinessCodeByIds(deliveryIds);
         sourceDetailList.forEach(e -> {
-            FirstMileDeliveryDTO.BusinessDTO businessDTO = businessDTOList.stream().filter(f -> Objects.nonNull(f) && Objects.equals(f.getId(), e.getDeliveryId())).findFirst().orElse(null);
-            if (Objects.nonNull(businessDTO)) {
-                e.setBusinessCode(businessDTO.getBusinessCode());
-            } else {
-                e.setBusinessCode("");
-            }
             e.setReconciliationId(reconciliationId);
             if (CharSequenceUtil.isBlank(reconciliationId)){
                 e.setReconciliationStatus(null);
@@ -1349,7 +1340,6 @@ public class TmsFirstMileLogisticServiceImpl extends SuperServiceImpl<LogisticsB
             detailDTOList.forEach(e -> e.setReconciliationType(reconciliationType));
         }
         updateDTO.setDetailList(detailDTOList);
-//        tmsFirstMileReconciliationService.update(updateDTO);
         // 修改明细数据（包含增删改）
         tmsFirstMileReconciliationDetailService.update(updateDTO.getDetailList(), reconciliationEntity);
 
@@ -1360,11 +1350,6 @@ public class TmsFirstMileLogisticServiceImpl extends SuperServiceImpl<LogisticsB
         return BatchResultDTO.success(id, curListDTO.getTransportNo(), OperationTypeEnum.ADD);
     }
 
-//    private List<TmsFirstMileReconciliationDetailDTO.ListDTO> buildReconciliationDetailData(String reconciliationId, LogisticsBillEntity logisticsBillEntity, List<LogisticsBillDetailEntity> logisticsBillDetailEntityList, List<LogisticsBillCostEntity> logisticsBillCostEntityList) {
-//        //根据物流单匹配对应的费用项
-//        List<TmsFirstMileReconciliationDetailDTO.ListDTO> list = new ArrayList<>();
-//
-//    }
 
     private List<TmsFirstMileReconciliationDetailDTO.ListDTO> resetSaveData(List<TmsFirstMileReconciliationDetailDTO.ListDTO> saveListDTO, List<TmsFirstMileReconciliationDetailDTO.ListDTO> oldListDTO) {
         if (CollectionUtils.isEmpty(oldListDTO)){
