4411d788cea2c2ef2892da60e29c0b2cb854b8cd
liaohui <liaohui@ulanzi.cn>
2024-11-20 15:51:37 +0800

    chore：fba bug修复

--- erp-server/erp-server-file/src/main/java/com/erp/server/file/business/mrp/ExportMrpFbaInventoryHandler.java
+++ erp-server/erp-server-file/src/main/java/com/erp/server/file/business/mrp/ExportMrpFbaInventoryHandler.java
@@ -4,10 +4,7 @@ import com.common.business.dto.base.PagingDTO;
 import com.common.business.enums.FileTaskEventEnum;
 import com.common.business.vo.PagingVO;
 import com.erp.model.mrp.dto.FbaHistoryInventoryDTO;
-import com.erp.model.mrp.dto.ReplenishmentSuggestionDTO;
-import com.erp.model.wms.dto.FbaInventoryDTO;
 import com.erp.rpc.mrp.feign.ExportMrpFeign;
-import com.erp.rpc.wms.feign.ExportWmsFeign;
 import com.erp.server.file.core.AbstractPageFileEventHandler;
 import com.erp.server.file.entity.FileTask;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -17,7 +14,6 @@ import javax.annotation.Resource;
 import java.util.List;
 
 import static com.common.business.enums.FileTaskEventEnum.EXPORT_MRP_FBA_INVENTORY;
-import static com.common.business.enums.FileTaskEventEnum.EXPORT_WMS_FBA_INVENTORY;
 
 @Component
 public class ExportMrpFbaInventoryHandler extends AbstractPageFileEventHandler<FbaHistoryInventoryDTO.ListDTO, FbaHistoryInventoryDTO.ExportDTO> {
@@ -43,6 +39,6 @@ public class ExportMrpFbaInventoryHandler extends AbstractPageFileEventHandler<F
 
     @Override
     public String getExcelPath() {
-        return "excel/wms/fbaInventory.xlsx";
+        return "excel/mrp/fbaHistoryInventory.xlsx";
     }
 }
