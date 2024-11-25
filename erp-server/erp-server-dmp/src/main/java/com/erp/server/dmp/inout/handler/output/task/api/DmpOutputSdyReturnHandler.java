12898b01377cb49a11c177cea9200c616a46875d
luoweigang <821969511@qq.com>
2024-11-25 19:56:55 +0800

    修改B2B订单映射数帝云1125 008

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyReturnHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyReturnHandler.java
@@ -212,9 +212,11 @@ public class DmpOutputSdyReturnHandler extends DmpOutputTaskHandler {
                         sdyDTO.setSales_company_code(salesOrg.getCode());
 
                         BaseIdDTO.CodeDTO sysAccountingCompanyEntity = companyEntities.stream().filter(req -> req.getId().equals(customerInfo.getFinancialOrganization())).findFirst().orElse(null);
-                        sdyDTO.setReceiving_company_code(sysAccountingCompanyEntity.getCode());
-                        sdyDTO.setOrganization_code(sysAccountingCompanyEntity.getCode());
-                        sdyDTO.setOrganization_name(sysAccountingCompanyEntity.getName());
+                        if (ObjectUtil.isNotEmpty(sysAccountingCompanyEntity)) {
+                            sdyDTO.setReceiving_company_code(sysAccountingCompanyEntity.getCode());
+                            sdyDTO.setOrganization_code(sysAccountingCompanyEntity.getCode());
+                            sdyDTO.setOrganization_name(sysAccountingCompanyEntity.getName());
+                        }
                     }
 
                     DictCurrencyEntity dictCurrencyEntity = FeignQuery.getById(DictCurrencyEntity.class, shopInfo.getTradeCurrency());
