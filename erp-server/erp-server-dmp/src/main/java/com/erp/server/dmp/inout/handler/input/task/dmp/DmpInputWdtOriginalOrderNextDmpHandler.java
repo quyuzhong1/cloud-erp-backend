d5f2f835b68cb594dddceb6a316a7065a0c444af
shukai <shukai@ulanzi.cn>
2024-11-25 10:53:20 +0800

    add:旺店通原始订单明细处理器

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputWdtOriginalOrderNextDmpHandler.java
@@ -0,0 +1,23 @@
+package com.erp.server.dmp.inout.handler.input.task.dmp;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+/**
+ * dmp处理下一个扩展handler，如何订单收货人信息单独一张表，使用此handler即可，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
+ * @author Administrator
+ *
+ */
+@Service
+@Scope("prototype")
+public class DmpInputWdtOriginalOrderNextDmpHandler extends DmpInputWdtNextDmpHandler{
+	
+	@Override
+	protected List<Map<String, Object>> getDetailList(Map<String, Object> dmpInputMongoEntity){
+		return (List<Map<String, Object>>) dmpInputMongoEntity.get("trade_orders");
+	}
+	
+}
