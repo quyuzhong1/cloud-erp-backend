62a162bef82a071a68a48c5000c5cfe2ba5a373e
shukai <shukai@ulanzi.cn>
2024-11-28 19:28:19 +0800

    add

--- erp-server/erp-server-file/src/main/java/com/erp/server/file/business/oms/ExportOmsSoB2CHandler.java
+++ erp-server/erp-server-file/src/main/java/com/erp/server/file/business/oms/ExportOmsSoB2CHandler.java
@@ -46,6 +46,6 @@ public class ExportOmsSoB2CHandler extends AbstractPageFileEventHandler<SoB2cDTO
 
     @Override
     protected int getPageSize() {
-        return 50000;
+        return 10000;
     }
 }
