95e896c16905a5102bb917e75659dd75ab345bfd
luoweigang <821969511@qq.com>
2024-11-29 14:18:00 +0800

    自动生成代码物流大表

---
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/mapper/LogisticsLargeMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.tms.mapper;
+import com.erp.model.tms.entity.LogisticsLargeEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 物流大表 Mapper 接口
+ * </p>
+ *
+ * @author Luo_WG
+ * @since 2024-11-29
+ */
+@Mapper
+public interface LogisticsLargeMapper extends BaseMapper<LogisticsLargeEntity> {
+
+}
