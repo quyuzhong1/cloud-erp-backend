fc6da590692e414eff988d7c12f13d9f199ff7da
jack chen <jackchen@ulanzi.cn>
2024-11-29 15:11:56 +0800

    2024-11-29 fix 退货入库单详情返回创建时间

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoReturnInstockDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoReturnInstockDTO.java
@@ -636,7 +636,7 @@ public class SoReturnInstockDTO {
         /**
          * 创建时间
          */
-        private LocalDate createTime;
+        private LocalDateTime createTime;
 
         /**
          * 退货物流单号
