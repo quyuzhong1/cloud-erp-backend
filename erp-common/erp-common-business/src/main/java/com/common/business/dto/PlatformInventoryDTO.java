ddf4cd71951227a606f3cc5f21ee194f5a1a5398
Jim <zhoujingbo@ulanzi.cn>
2024-12-03 11:06:24 +0800

    ADD:库龄DTO

--- erp-common/erp-common-business/src/main/java/com/common/business/dto/PlatformInventoryDTO.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/dto/PlatformInventoryDTO.java
@@ -3,13 +3,16 @@ package com.common.business.dto;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.common.business.enums.OmsPlatformEnum;
 import com.common.business.enums.WarehousePlatformTypeEnum;
+import jnr.ffi.annotations.SaveError;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import net.sf.cglib.core.Local;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * 仓库DTO 所有平台库存数据通用数据，转换为此类后发送mq统一消费处理
@@ -89,4 +92,29 @@ public class PlatformInventoryDTO extends UniqueDto {
      * 数据下载时间
      */
     private LocalDateTime downloadTime;
+
+    /**
+     * 库龄信息
+     */
+    private List<PlatformInventoryAgeDTO> ageInfoList;
+
+
+    /**
+     * 库存信息
+     */
+    @Data
+    @NoArgsConstructor
+    public static class PlatformInventoryAgeDTO implements Serializable {
+
+        /**
+         * 在库库存
+         */
+        private Integer inventoryQty;
+
+        /**
+         * 库龄
+         */
+        private Integer inventoryAge;
+    }
+
 }
