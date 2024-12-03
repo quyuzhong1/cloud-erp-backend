fdb9fced0dd21c67197cb9f382e62cfa52b88d54
Jim <zhoujingbo@ulanzi.cn>
2024-12-03 22:35:50 +0800

    ADD:记录中台

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/mapper/DmpAmzSoOutstockDetailMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.dmp.mapper;
+import com.erp.model.dmp.entity.DmpAmzSoOutstockDetailEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 亚马逊配送明细表 Mapper 接口
+ * </p>
+ *
+ * @author Jim
+ * @since 2024-12-03
+ */
+@Mapper
+public interface DmpAmzSoOutstockDetailMapper extends BaseMapper<DmpAmzSoOutstockDetailEntity> {
+
+}
