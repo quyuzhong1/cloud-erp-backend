02fe9c475e99f1354c0678165b1834e71348568a
luoweigang <821969511@qq.com>
2024-12-04 11:04:19 +0800

    修改退货入库单报错问题

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/kingdee/impl/SyncSoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/kingdee/impl/SyncSoB2cServiceImpl.java
@@ -276,9 +276,9 @@ public class SyncSoB2cServiceImpl implements SyncSoB2cService {
     }
 
     private void syncDataToSdyFieldHandlerBatch(SoB2cEntity soB2cEntity, List<SoB2cDetailEntity> soB2cDetailEntityList, String operate) {
-        List<String> skuNos = soB2cDetailEntityList.stream().map(req -> req.getSkuNo()).collect(Collectors.toList());
+        List<String> skuNos = soB2cDetailEntityList.stream().map(req -> req.getSkuNo()).distinct().collect(Collectors.toList());
         List<SkuVO> skuVOList = plmTaskFeign.listBySkuNoList(skuNos);
-        List<String> skuIds = soB2cDetailEntityList.stream().map(req -> req.getSkuId()).collect(Collectors.toList());
+        List<String> skuIds = soB2cDetailEntityList.stream().map(req -> req.getSkuId()).distinct().collect(Collectors.toList());
         List<BomChildrenSkuDTO> bomChildrenSkuDTOS = plmTaskFeign.listBomBySkuIds(skuIds);
 
         List<CurrencyDTO.ViewDTO> currencyList = sysUserFeign.listByCurrency(Arrays.asList(soB2cEntity.getCurrency()));
