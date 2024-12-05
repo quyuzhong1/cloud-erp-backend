0dcbef09899c08c822f35e9e886fd8437e5b8997
liaohui <liaohui@ulanzi.cn>
2024-12-05 17:33:30 +0800

    feat:修复试产量产飞书通知

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/feign/PlmWorkOptionFeignController.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/feign/PlmWorkOptionFeignController.java
@@ -1,6 +1,7 @@
 package com.erp.server.plm.controller.feign;
 
 import com.common.business.dto.base.ApproveOneDTO;
+import com.common.business.enums.ApproveTypeEnum;
 import com.erp.model.plm.dto.AuditParamDTO;
 import com.erp.model.plm.dto.PilotApplicationDTO;
 import com.erp.model.plm.dto.ProductDetailOperateDTO;
@@ -151,9 +152,12 @@ public class PlmWorkOptionFeignController {
     @PostMapping("/pilotApprovalPass")
     public void pilotApprovalPass(@RequestBody @Validated ApproveOneDTO dto) {
         PilotApplicationDTO.ApproveDTO approveDTO = new PilotApplicationDTO.ApproveDTO();
-        pilotApplicationService.approve(dto,approveDTO);
-        //回写产品管理--采购信息--一级和二级供应商
-        pilotApplicationService.writeProductPurchaseBack(dto.getId());
-        pilotApplicationService.approvePilotApplicationNotice(dto.getId());
+        pilotApplicationService.approve(dto, approveDTO);
+        if (ApproveTypeEnum.PASS.getStatus().equals(dto.getType())) {
+            //回写产品管理--采购信息--一级和二级供应商 审核流回调导致状态无法查询，则判断通过则直接通知
+            pilotApplicationService.writeProductPurchaseBackByWork(dto.getId());
+            pilotApplicationService.approvePilotApplicationNoticeByWork(dto.getId());
+        }
+
     }
 }
