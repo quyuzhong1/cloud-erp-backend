2eac24720726785e951fbebb8a6b566d3bee3efe
luoweigang <821969511@qq.com>
2024-12-05 16:22:02 +0800

    更新子订单状态

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/OrderSubTypeEnum.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/OrderSubTypeEnum.java
@@ -8,18 +8,18 @@ import org.apache.commons.lang3.StringUtils;
 import java.util.stream.Stream;
 
 public enum OrderSubTypeEnum implements EnumMessage {
-    OFFLINE_ORDER("100.30.01", "线下订单"),
-    ONLINE_ORDER("100.20.01", "线上订单"),
-    GIFT_ORDER("100.20.02", "赠品订单"),
-    GIFT_REPLENISHMENT("100.20.03", "赠品补发"),
-    INFLUENCER_SAMPLE("100.20.04", "红人样品"),
-    DEPARTMENT_USAGE("100.20.05", "部门领用"),
-    REVIEW_CASHBACK("100.20.06", "评价返现"),
-    COMPENSATION_COUPON("100.20.07", "补偿赠券"),
-    LUCKY_BAG_DRAW("100.20.08", "福袋抽奖"),
-    EXCHANGE_REPLACEMENT("100.20.09", "换货补发"),
-    REPLENISHMENT("100.20.10", "补发"),
-    OTHER("100.20.11", "其他"),
+    OFFLINE_ORDER("offlineOrder","线下订单"),
+    ONLINE_ORDER("onlineOrder","线上订单"),
+    GIFT_ORDER("giftOrder","赠品订单"),
+    GIFT_REPLENISHMENT("giftReplenishment","赠品补发"),
+    INFLUENCER_SAMPLE("influencerSample","红人样品"),
+    DEPARTMENT_USAGE("departmentUsage","部门领用"),
+    REVIEW_CASHBACK("reviewCashback","评价返现"),
+    COMPENSATION_COUPON("compensationCoupon","补偿赠券"),
+    LUCKY_BAG_DRAW("luckyBagDraw","福袋抽奖"),
+    EXCHANGE_REPLACEMENT("exchangeReplacement","换货补发"),
+    REPLENISHMENT("replenishment","补发"),
+    OTHER("other","其他"),
     ;
     OrderSubTypeEnum(String code, String name) {
         this.code = code;
