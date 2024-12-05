7a7cca8aba555b38921e52308907dcf407b98de7
liaohui <liaohui@ulanzi.cn>
2024-12-05 16:38:53 +0800

    chore：修复bug

--- erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/listener/HistorySalesQtyExcelListener.java
+++ erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/listener/HistorySalesQtyExcelListener.java
@@ -2,8 +2,10 @@ package com.erp.server.mrp.listener;
 
 import com.alibaba.excel.context.AnalysisContext;
 import com.alibaba.excel.event.AnalysisEventListener;
+import com.common.core.utils.BeanMapperUtils;
 import com.common.core.utils.FieldValidUtil;
 import com.common.core.utils.date.LocalDateUtil;
+import com.erp.model.mrp.dto.CalcSalesInfoHisDTO;
 import com.erp.model.mrp.dto.CfgRuleCalcDTO;
 import com.erp.model.mrp.entity.CfgRuleCalcEntity;
 import com.erp.model.oms.entity.ShopInfoEntity;
@@ -12,6 +14,7 @@ import com.erp.server.mrp.es.entity.CalcSalesInfoHisEsEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,15 +28,17 @@ public class HistorySalesQtyExcelListener extends AnalysisEventListener<CfgRuleC
     private List<SkuVO> skuVOS;
     private List<ShopInfoEntity> shopInfoList;
     private CfgRuleCalcEntity cfgRuleCalc;
+    private Map<String, String> platformMap;
 
-
-    public HistorySalesQtyExcelListener(List<SkuVO> skuVOS, List<ShopInfoEntity> shopInfoList, CfgRuleCalcEntity cfgRuleCalc) {
+    public HistorySalesQtyExcelListener(List<SkuVO> skuVOS, List<ShopInfoEntity> shopInfoList, Map<String, String> platformMap, CfgRuleCalcEntity cfgRuleCalc) {
         this.skuVOS = skuVOS;
         this.shopInfoList = shopInfoList;
         this.cfgRuleCalc = cfgRuleCalc;
+        this.platformMap = platformMap;
     }
 
     private List<CalcSalesInfoHisEsEntity> dataList = new ArrayList<>();
+    private List<CalcSalesInfoHisDTO> successList = new ArrayList<>();
     private final List<CfgRuleCalcDTO.HistorySaleImportDTO> errorList = new ArrayList<>();
 
     @Override
@@ -47,6 +52,11 @@ public class HistorySalesQtyExcelListener extends AnalysisEventListener<CfgRuleC
         }
         Map<String, String> skuMap = skuVOS.stream()
                 .collect(Collectors.toMap(SkuVO::getSkuNo, SkuVO::getSkuId, (o1, o2) -> o1));
+        if (!skuMap.containsKey(data.getSkuNo())) {
+            data.setErrorMsg("该sku不在已选中的试算sku中");
+            errorList.add(data);
+            return;
+        }
         Map<String, String> shopMap = shopInfoList.stream()
                 .collect(Collectors.toMap(ShopInfoEntity::getName, ShopInfoEntity::getId, (o1, o2) -> o1));
         if (!shopMap.containsKey(data.getShopName())) {
@@ -54,8 +64,25 @@ public class HistorySalesQtyExcelListener extends AnalysisEventListener<CfgRuleC
             errorList.add(data);
             return;
         }
-        if (!skuMap.containsKey(data.getSkuNo())) {
-            data.setErrorMsg("该sku不在已选中的试算sku中");
+        String dictPlatform = platformMap.get(data.getPlatform());
+        ShopInfoEntity info = shopInfoList.stream()
+                .filter(v -> v.getName().equals(data.getShopName()))
+                .filter(v -> v.getDictPlatform().equals(dictPlatform))
+                .findFirst()
+                .orElse(null);
+        if (ObjectUtils.isEmpty(info)) {
+            data.setErrorMsg("该平台下无此店铺");
+            errorList.add(data);
+            return;
+        }
+
+        boolean isExit = successList.stream().anyMatch(v -> v.getSkuNo().equals(data.getSkuNo()) &&
+                v.getShopName().equals(data.getShopName()) &&
+                v.getPlatform().equals(data.getPlatform())
+        );
+        //校验重复数据
+        if (isExit) {
+            data.setErrorMsg("sku+平台+店铺重复");
             errorList.add(data);
             return;
         }
@@ -63,7 +90,7 @@ public class HistorySalesQtyExcelListener extends AnalysisEventListener<CfgRuleC
         if (!CollectionUtils.isEmpty(errorList)) {
             return;
         }
-        CalcSalesInfoHisEsEntity entity = new CalcSalesInfoHisEsEntity();
+        CalcSalesInfoHisDTO entity = new CalcSalesInfoHisDTO();
         entity.setSkuId(skuMap.get(data.getSkuNo()));
         entity.setSkuNo(data.getSkuNo());
         entity.setShopId(shopMap.get(data.getShopName()));
@@ -71,12 +98,14 @@ public class HistorySalesQtyExcelListener extends AnalysisEventListener<CfgRuleC
         entity.setDate(LocalDateUtil.parseStrToLocalDate(data.getBillDate()));
         entity.setQty(Integer.parseInt(data.getQty()));
         entity.setCfgRuleCalcId(cfgRuleCalc.getId());
-        dataList.add(entity);
+        entity.setPlatform(data.getPlatform());
+        successList.add(entity);
     }
 
     @Override
     public void doAfterAllAnalysed(AnalysisContext context) {
-        dataList = new ArrayList<>(dataList.stream()
+        dataList = new ArrayList<>(successList.stream()
+                .map(v -> BeanMapperUtils.map(CalcSalesInfoHisEsEntity.class, v))
                 .collect(Collectors.toMap(
                         v -> new CfgRuleCalcDTO.GroupDTO(v.getSkuId(), v.getShopId(), v.getDate()),
                         v -> v,
