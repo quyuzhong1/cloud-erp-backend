e79257561080b824280e9b4027c7006d63253671
luoweigang <821969511@qq.com>
2024-12-06 14:12:42 +0800

    修改物流大表中转费用

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/LogisticsLargeJob.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/schedule/LogisticsLargeJob.java
@@ -1,18 +1,40 @@
 package com.erp.server.tms.schedule;
 
+import com.common.business.enums.SourceTypeEnum;
+import com.erp.model.tms.entity.LogisticsLargeEntity;
+import com.erp.model.tms.entity.SmallBagCostAllocationMainEntity;
+import com.erp.server.tms.service.LogisticsLargeService;
+import com.erp.server.tms.service.SmallBagCostAllocationMainService;
 import com.erp.server.tms.service.SmallBagCostAllocationService;
 import com.xxl.job.core.handler.annotation.XxlJob;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogisticsLargeJob {
     @Resource
     private SmallBagCostAllocationService smallBagCostAllocationService;
+
+    @Resource
+    private SmallBagCostAllocationMainService smallBagCostAllocationMainService;
+
+    @Resource
+    private LogisticsLargeService logisticsLargeService;
+
     /**
      * 小包费用分摊自动生成物流大表
      */
     @XxlJob("smallBagAllocationToLogisticsLarge")
     public void smallBagAllocationToLogisticsLarge() {
+        List<LogisticsLargeEntity> largeEntityList = logisticsLargeService.lambdaQuery()
+                .eq(LogisticsLargeEntity::getSourceType, SourceTypeEnum.SMALL_BAG_COST_ALLOCATION.getCode())
+                .ge(LogisticsLargeEntity::getReconciliationMonth, LocalDate.now().minusMonths(3))
+                .list();
+
+        List<String> ids = largeEntityList.stream().map(LogisticsLargeEntity::getSourceId).distinct().collect(Collectors.toList());
+        List<SmallBagCostAllocationMainEntity> list = smallBagCostAllocationMainService.list();
 
 
     }
