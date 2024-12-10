a318656711a46e8e1acfb6e9c733a29cda2a3ef0
jack chen <jackchen@ulanzi.cn>
2024-12-10 19:52:11 +0800

    2024-12-10 B2C销售订单导出sql调整

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/mapper/SoB2cRefCategoryMapper.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/mapper/SoB2cRefCategoryMapper.java
@@ -1,8 +1,12 @@
 package com.erp.server.oms.mapper;
+import com.erp.model.oms.dto.SoB2cRefCategoryDTO;
 import com.erp.model.oms.entity.SoB2cRefCategoryEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 
 /**
@@ -15,5 +19,5 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface SoB2cRefCategoryMapper extends BaseMapper<SoB2cRefCategoryEntity> {
-
+    List<SoB2cRefCategoryDTO.CategoryNamesDTO> listCategoryNamesBySoIds(@Param("soIds")List<String> soIds);
 }
