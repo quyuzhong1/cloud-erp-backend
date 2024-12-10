73ad67d29ef03b9062fe007c32f5bc448f2ba5bb
jack chen <jackchen@ulanzi.cn>
2024-12-10 20:07:31 +0800

    2024-12-10 三方仓消费者调整计算库龄

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformInventoryConsumerService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformInventoryConsumerService.java
@@ -178,7 +178,7 @@ public class PlatformInventoryConsumerService<T extends DmpSyncTaskIdDTO> extend
                         .list();
                 for (PlatformInventoryDTO.PlatformInventoryAgeDTO ageDTO : dto.getAgeInfoList()) {
                     // 计算日期差
-                    int daysBetween = (int) ChronoUnit.DAYS.between(ageDTO.getPullDate(), ageDTO.getPutAwayDate());
+                    int daysBetween = (int) ChronoUnit.DAYS.between(ageDTO.getPutAwayDate(), ageDTO.getPullDate());
 
                     OverseasInventoryAgeDetailEntity oldDetail = oldDetails.stream().filter(v -> v.getPutAwayDate().equals(ageDTO.getPutAwayDate())).findFirst().orElse(null);
                     if(null == oldDetail){
