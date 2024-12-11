0379d64c85a9927611a1b639bb2c66534937a879
Jim <zhoujingbo@ulanzi.cn>
2024-12-11 10:59:23 +0800

    UPDATE:速卖通库存推送fix

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAliExpressInventoryOnwayInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAliExpressInventoryOnwayInitHandler.java
@@ -3,7 +3,6 @@ package com.erp.server.dmp.inout.handler.input.task.init;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.exceptions.ExceptionUtil;
-import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
@@ -12,7 +11,6 @@ import com.common.core.enums.PannoEnum;
 import com.common.core.exception.ServiceException;
 import com.erp.model.dmp.entity.DmpCfgApiEntity;
 import com.erp.model.dmp.enums.DmpInputTaskStatusEnum;
-import com.erp.model.oms.entity.SoB2cEntity;
 import com.erp.oms.aliexpress.api.IopClient;
 import com.erp.oms.aliexpress.api.IopClientImpl;
 import com.erp.oms.aliexpress.api.IopRequest;
@@ -25,7 +23,6 @@ import com.erp.server.dmp.inout.dto.base.DmpInputTaskInitDTO;
 import com.erp.server.dmp.inout.dto.request.DmpInputInitRequest;
 import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
 import com.erp.server.dmp.inout.handler.input.task.mongo.DmpInputMongoHandler;
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Scope;
@@ -89,33 +86,14 @@ public class DmpInputAliExpressInventoryOnwayInitHandler extends DmpInputInitHan
 
         JSONArray result = new JSONArray();
         for (List<String> curList : partition) {
-            Map<String, Object> paramMap = new HashMap<>();
-            // 账套编码
-            paramMap.put("biz_type", 288000);
-            // 货品Id列表，最多30个
-            paramMap.put("sc_item_id_list", curList);
-            request.addApiParameter("on_way_inventory_query_dto", JSON.toJSONString(paramMap));
-
-            JSONObject data = null;
-            long sleepTime = 1000;
-            int count = 0;
-            while (data == null) {
-                data = this.execute(client, request, token, apiType);
-                log.debug("速卖通在途库存查询结果:{}", JSON.toJSONString(data));
-                if (data == null) {
-                    if (count == 10) {
-                        throw new ServiceException("调用速卖通" + apiType + "接口重试" + count + "失败");
-                    }
-                    try {
-                        Thread.sleep(sleepTime);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    sleepTime = sleepTime + 1000;
-                    count = count + 1;
-                }
-            }
+            // 库存类型(1 采购在途，2 调拨在途，3 销售在途，4 销退在途)
+            int inventoryType1 = 1;
+            JSONObject data = queryOnWayInfo(curList,inventoryType1, request, client, token, apiType);
             result.addAll(data.getJSONArray("data_list"));
+
+            int inventoryType4 = 4;
+            JSONObject data4 = queryOnWayInfo(curList,inventoryType4, request, client, token, apiType);
+            result.addAll(data4.getJSONArray("data_list"));
         }
 
 
@@ -126,6 +104,42 @@ public class DmpInputAliExpressInventoryOnwayInitHandler extends DmpInputInitHan
         });
         dmpInputTaskInitDTO.setMsg(result.toJSONString());
         return Collections.singletonList(dmpInputTaskInitDTO);
+    }
+
+    /**
+     * 请求响应数据
+     */
+    private JSONObject queryOnWayInfo(List<String> curList,Integer inventoryType, IopRequest request, IopClient client, String token, String apiType) {
+        Map<String, Object> paramMap = new HashMap<>();
+        // 账套编码
+        paramMap.put("biz_type", 288000);
+        // 货品Id列表，最多30个
+        paramMap.put("sc_item_id_list", curList);
+        // 库存类型(1 采购在途，2 调拨在途，3 销售在途，4 销退在途)
+        paramMap.put("inventory_type", inventoryType);
+        // 请求DTO
+        request.addApiParameter("on_way_inventory_query_dto", JSON.toJSONString(paramMap));
+
+        JSONObject data = null;
+        long sleepTime = 1000;
+        int count = 0;
+        while (data == null) {
+            data = this.execute(client, request, token, apiType);
+            log.debug("速卖通在途库存查询结果:{}", JSON.toJSONString(data));
+            if (data == null) {
+                if (count == 10) {
+                    throw new ServiceException("调用速卖通" + apiType + "接口重试" + count + "失败");
+                }
+                try {
+                    Thread.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                sleepTime = sleepTime + 1000;
+                count = count + 1;
+            }
+        }
+        return data;
     }
 
     private JSONObject execute(IopClient client, IopRequest request, String token, String apiType) {
