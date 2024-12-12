921b82848bc3579f6ba9f0a1b23b0899f50c33ef
jack chen <jackchen@ulanzi.cn>
2024-12-12 12:04:35 +0800

    2024-12-12 装箱任务优化 -- B2C类型装箱任务打印 -- 发货计划需要调整为取来源的要货申请的店铺

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackingTaskServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackingTaskServiceImpl.java
@@ -92,7 +92,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static cn.hutool.json.XMLTokener.entity;
 import static com.common.business.enums.FileTaskEventEnum.*;
 
 /**
@@ -2296,25 +2295,19 @@ public class PackingTaskServiceImpl extends SuperServiceImpl<PackingTaskMapper, 
                         printDTO.setChargeName(shopInfo.getChargeName());
                     }
                 }
-            }else if (Objects.nonNull(requisitionApplication) && CharSequenceUtil.isNotBlank(requisitionApplication.getSourceId())){
-                //要货计划
-                    WmsDeliveryPlanEntity deliveryPlan = wmsDeliveryPlanService.getById(requisitionApplication.getSourceId());
-                    if (Objects.nonNull(deliveryPlan)){
-                        printDTO.setCountryId(deliveryPlan.getCountry());
-                        printDTO.setCountryName(deliveryPlan.getCountryName());
-                        printDTO.setShopId(deliveryPlan.getShopId());
-                        printDTO.setShopName(deliveryPlan.getShopName());
-                        if (CharSequenceUtil.isNotBlank(deliveryPlan.getShopId())){
-                            ShopInfoEntity shopInfo = shopInfoFeign.getShopInfoById(deliveryPlan.getShopId());
-                            if (Objects.nonNull(shopInfo)){
-                                printDTO.setShopName(shopInfo.getName());
-                                printDTO.setChargeId(shopInfo.getChargeId());
-                                printDTO.setChargeName(shopInfo.getChargeName());
-                            }
-                        }
-                    }
+            }else if (Objects.nonNull(requisitionApplication) && CharSequenceUtil.isNotBlank(requisitionApplication.getChannelId())){
+                printDTO.setShopId(requisitionApplication.getChannelId());
+                printDTO.setShopName(requisitionApplication.getChannelName());
+                ShopInfoEntity shopInfo = shopInfoFeign.getShopInfoById(requisitionApplication.getChannelId());
+                if (Objects.nonNull(shopInfo)) {
+                    printDTO.setCountryId(shopInfo.getDictCountryCode());
+                    printDTO.setCountryName(shopInfo.getCountryName());
+                    printDTO.setShopName(shopInfo.getName());
+                    printDTO.setChargeId(shopInfo.getChargeId());
+                    printDTO.setChargeName(shopInfo.getChargeName());
                 }
             }
+        }
         return printDTO;
     }
 
