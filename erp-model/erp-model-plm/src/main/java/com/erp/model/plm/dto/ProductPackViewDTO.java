89285e4dd6713a2a60ba55c52a220f7cf930375a
liaohui <liaohui@ulanzi.cn>
2024-12-12 08:54:35 +0800

    feat:取消校验

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductPackViewDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductPackViewDTO.java
@@ -34,57 +34,47 @@ public class ProductPackViewDTO implements Serializable {
     /**
      * 产品尺寸长
      */
-    @NotNull(message = "产品尺寸长不能为空")
     private BigDecimal productLength;
     /**
      * 产品尺寸宽
      */
-    @NotNull(message = "产品尺寸宽不能为空")
     private BigDecimal productWidth;
     /**
      * 产品尺寸高
      */
-    @NotNull(message = "产品尺寸高不能为空")
     private BigDecimal productHeight;
 
     /**
      * 毛重
      */
-    @NotNull(message = "毛重不能为空")
     private BigDecimal grossWeight;
 
     /**
      * 净重
      */
-    @NotNull(message = "净重不能为空")
     private BigDecimal netWeight;
 
     /**
      * 箱规长
      */
-    @NotNull(message = "箱规长不能为空")
     private BigDecimal boxLength;
     /**
      * 箱规宽
      */
-    @NotNull(message = "箱规宽不能为空")
     private BigDecimal boxWidth;
     /**
      * 箱规高
      */
-    @NotNull(message = "箱规高不能为空")
     private BigDecimal boxHeight;
 
     /**
      * 单箱重量
      */
-    @NotNull(message = "单箱重量不能为空")
     private BigDecimal boxWeight;
 
     /**
      * 单箱数量
      */
-    @NotNull(message = "单箱数量不能为空")
     private BigDecimal boxQty;
 
 }
