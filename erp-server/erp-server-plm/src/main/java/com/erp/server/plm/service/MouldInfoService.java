b5a66e285729f240cab44c96c513d47d60e6ca00
liaohui <liaohui@ulanzi.cn>
2024-12-13 15:15:41 +0800

    chore：修复bug

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/MouldInfoService.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/MouldInfoService.java
@@ -40,7 +40,7 @@ public interface MouldInfoService extends SuperService<MouldInfoEntity> {
      * 暂存
      * @param dto 参数
      */
-    BatchResultDTO draft(MouldInfoDTO.CommonDTO dto);
+    BatchResultDTO draft(MouldInfoDTO.UpdateDTO dto);
 
     /**
      * 新增并提交
