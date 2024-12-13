3065d7b2aeeecac845ff6aeb2c36052455ad6aa6
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-12-13 18:28:11 +0800

    问题处理

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -1079,7 +1079,7 @@ public enum ApiError implements Serializable {
     ERROR_92263(92263,"未找到【{}】仓库"),
     ERROR_92264(92264,"目的国家不能为空"),
     ERROR_92265(92265,"重量不能为空"),
-    ERROR_92267(92267,"安兔运费试算时邮编不能为空"),
+    ERROR_92267(92267,"邮编不能为空"),
 
     ERROR_SUBCONTRACT_ISSUE_NOT_EXIST(92124,"委外发料单不存在"),
     ERROR_SUBCONTRACT_ISSUE_DETAIL_NOT_EXIST(92125,"委外发料单明细不存在"),
