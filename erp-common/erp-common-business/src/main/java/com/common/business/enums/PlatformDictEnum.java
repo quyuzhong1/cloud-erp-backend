961eb93db9bafe8d014845ff763bbdb642cee074
liuruipeng <arthur@ulanzi.cn>
2024-12-16 16:13:35 +0800

    feat:【领星】标发类

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
@@ -63,6 +63,8 @@ public enum PlatformDictEnum implements EnumMessage {
     TE_MU("TeMu", "TEMU", "拼多多海外版", "034"),
     ANTU("antu", "安兔", "安兔", "53"),
     LING_XING("lingxing", "领星", "领星", "54"),
+    LE_TIAN("letian", "乐天", "乐天", "55"),
+    EBAY("eBay", "eBay", "eBay", "56"),
         ;
 
 
