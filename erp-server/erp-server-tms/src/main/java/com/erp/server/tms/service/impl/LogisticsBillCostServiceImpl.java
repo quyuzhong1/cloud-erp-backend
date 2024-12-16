2d6e699dd3ab3a444907575744379f4b0e596da6
luoweigang <821969511@qq.com>
2024-12-16 10:53:26 +0800

    更新物流大表3

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
@@ -27,6 +27,8 @@ import java.util.stream.Collectors;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 
+import com.erp.model.tms.entity.*;
+import com.erp.server.tms.service.*;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,20 +92,6 @@ import com.erp.model.tms.dto.TmsCostDetailDTO.CostViewDTO;
 import com.erp.model.tms.dto.TmsCostDetailDTO.DetailDTO;
 import com.erp.model.tms.dto.TmsCostDetailDTO.UpdateDTO;
 import com.erp.model.tms.dto.excel.LogisticsBillCostExcelDTO;
-import com.erp.model.tms.entity.CfgSettingEntity;
-import com.erp.model.tms.entity.InventorySkuCostDetailEntity;
-import com.erp.model.tms.entity.InventorySkuCostEntity;
-import com.erp.model.tms.entity.LogisticsBillCostEntity;
-import com.erp.model.tms.entity.LogisticsBillDetailEntity;
-import com.erp.model.tms.entity.LogisticsBillEntity;
-import com.erp.model.tms.entity.LogisticsChannelEntity;
-import com.erp.model.tms.entity.SmallBagCostAllocationDetailEntity;
-import com.erp.model.tms.entity.SmallBagCostAllocationEntity;
-import com.erp.model.tms.entity.SmallBagCostAllocationMainEntity;
-import com.erp.model.tms.entity.TmsCfgCostEntity;
-import com.erp.model.tms.entity.TmsCostDetailEntity;
-import com.erp.model.tms.entity.TmsFirstMileReconciliationDetailEntity;
-import com.erp.model.tms.entity.TmsFirstMileReconciliationEntity;
 import com.erp.model.tms.enums.AllocationFeeTypeEnum;
 import com.erp.model.tms.enums.CfgSettingEnum;
 import com.erp.model.tms.enums.CostAllocationEnum;
@@ -135,20 +123,6 @@ import com.erp.server.tms.listener.LogisticsBillCostExcelListener;
 import com.erp.server.tms.mapper.LogisticsBillCostMapper;
 import com.erp.server.tms.query.LogisticsBillCostQueryHandler;
 import com.erp.server.tms.query.LogisticsLastMileCostQueryHandler;
-import com.erp.server.tms.service.CfgSettingService;
-import com.erp.server.tms.service.DictBasicService;
-import com.erp.server.tms.service.InventorySkuCostDetailService;
-import com.erp.server.tms.service.InventorySkuCostService;
-import com.erp.server.tms.service.LogisticsBillCostService;
-import com.erp.server.tms.service.LogisticsBillDetailService;
-import com.erp.server.tms.service.LogisticsBillService;
-import com.erp.server.tms.service.LogisticsChannelService;
-import com.erp.server.tms.service.OperateLogService;
-import com.erp.server.tms.service.SmallBagCostAllocationDetailService;
-import com.erp.server.tms.service.SmallBagCostAllocationMainService;
-import com.erp.server.tms.service.SmallBagCostAllocationService;
-import com.erp.server.tms.service.TmsCfgCostService;
-import com.erp.server.tms.service.TmsCostDetailService;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
@@ -218,6 +192,8 @@ public class LogisticsBillCostServiceImpl extends SuperServiceImpl<LogisticsBill
     private InventorySkuCostService inventorySkuCostService;
     @Resource
     private InventorySkuCostDetailService inventorySkuCostDetailService;
+    @Resource
+    private LogisticsLargeService logisticsLargeService;
     
 
     @GlobalTransactional(rollbackFor = Exception.class)
@@ -1408,6 +1384,15 @@ public class LogisticsBillCostServiceImpl extends SuperServiceImpl<LogisticsBill
 			        .set(LogisticsBillCostEntity::getPayStatus, payStatus)
 			        .set(LogisticsBillCostEntity::getPayTime, payTime)
 			        .update();
+
+
+        // 修改物流大表的支付状态
+        List<SmallBagCostAllocationMainEntity> costAllocationMainEntities = smallBagCostAllocationMainService.lambdaQuery().eq(SmallBagCostAllocationMainEntity::getCostId, id).list();
+        List<String> ids = costAllocationMainEntities.stream().map(req -> req.getId()).distinct().collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(ids)) {
+            logisticsLargeService.updatePayStatusBySourceId(ids, payStatus, payTime);
+        }
+
 		return BatchResultDTO.success(entity.getId(), entity.getTrackNo(), OperationTypeEnum.UPDATE_STATUS);
 	}
 
