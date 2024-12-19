b70af24640146f8c02cec322c180e6c2e40c49ef
luoweigang <821969511@qq.com>
2024-12-19 20:03:08 +0800

    修改物流大表查询

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/query/LogisticsLargeQueryHandler.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/query/LogisticsLargeQueryHandler.java
@@ -1,20 +1,12 @@
 package com.erp.server.tms.query;
 
-import com.common.business.enums.ApproveStatusEnum;
 import com.common.business.query.AbstractQueryHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LogisticsLargeQueryHandler extends AbstractQueryHandler {
-
-
     @Override
     protected String handleSqlLogic(String field, Object value, String compareCodeSplicingValueSql) {
-        if ("ll.reconciliation_month".equals(field)) {
-            super.buildDefaultDTO("TO_CHAR(ll.reconciliation_month, 'yyyy-MM')", value);
-        }
-        return super.getSplicingSQL();
+        return null;
     }
-
-
 }
