1c332d83456c61c2981f4e88b90563d7648c0a15
Administrator <admin@example.com>
2024-12-19 10:21:14 +0800

    k8s

---
+++ erp-server/erp-server-k8s/src/main/java/com/erp/server/admin/config/MybatisPlusConfig.java
@@ -0,0 +1,48 @@
+package com.erp.server.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.common.business.utils.IdGeneratorUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+/**
+ * @Classname MybatisPlusConfig
+
+ * @Date 2022-07-06 9:30
+ * @Created by yl
+ */
+@Slf4j
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+
+    }
+
+    /**
+     * 修改雪花算法生成方式
+     */
+    @Resource
+    private IdGeneratorUtil idGeneratorUtil;
+    @Bean
+    public IdentifierGenerator idGenerator() {
+        return idGeneratorUtil.idGenerator();
+    }
+
+
+}
