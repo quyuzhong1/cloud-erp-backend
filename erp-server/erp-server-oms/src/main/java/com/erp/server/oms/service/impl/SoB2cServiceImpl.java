db8f586c2dca8cd3d63fe7aa67e417ad1796f7de
jack chen <jackchen@ulanzi.cn>
2024-12-20 10:53:13 +0800

    2024-12-20 解决冲突

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -9368,6 +9368,8 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
         }
         lambdaUpdate()
                 .set(SoB2cEntity::getSoOutstockDate,DateTimeFormatter.ofPattern("yyyy-MM-dd").parse(soOutstockDate))
+                .set(SoB2cEntity::getUpdateTime,LocalDate.now())
+                .set(SoB2cEntity::getVersion,String.format("{}={}+1", BaseEntity.FIELD_VERSION, BaseEntity.FIELD_VERSION))
                 .eq(SoB2cEntity::getId,soId)
                 .update();
     }
