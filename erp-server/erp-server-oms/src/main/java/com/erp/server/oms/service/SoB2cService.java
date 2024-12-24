bb8de3189d1a41837e4b348bcf450d7bec9deeb4
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2024-12-24 17:38:18 +0800

    导入引用包

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/SoB2cService.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/SoB2cService.java
@@ -21,6 +21,7 @@ import com.erp.model.wms.dto.SoOutstockDTO;
 import com.erp.model.wms.dto.WmsDataCompareTaskDTO;
 import com.erp.model.wms.dto.inventory.InventoryQtyDTO;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
