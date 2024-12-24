b8d3e1f9d9eb2bbd1f5f09f4c97c88ac681d5470
Jim <zhoujingbo@ulanzi.cn>
2024-12-24 17:57:11 +0800

    UPDATE:委外订单调整高级查询fix

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderServiceImpl.java
@@ -186,7 +186,8 @@ public class SubcontractOrderServiceImpl extends SuperServiceImpl<SubcontractOrd
                 count = this.baseMapper.listCount(searchParamDTO);
             }
             resultDTO.setCount(ObjectUtils.isEmpty(count) ? MathUtil.ZERO : count);
-            resultDTO.setSearchType(item.getCode());
+            resultDTO.setTabFlag(item.getCode());
+            resultDTO.setTabFlagName(item.getName());
             list.add(resultDTO);
         }
         return list;
