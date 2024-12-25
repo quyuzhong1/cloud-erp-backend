f55519d4e92406709442e0939f543bd74485d981
Jim <zhoujingbo@ulanzi.cn>
2024-12-25 10:17:16 +0800

    UPDATE:B2C客户列表查询高级查询调整

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/CustomerB2CDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/CustomerB2CDTO.java
@@ -1,6 +1,7 @@
 package com.erp.model.oms.dto;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.common.business.dto.AdvanceQueryDTO;
 import com.common.business.dto.base.SortDTO;
 import com.common.business.enums.ApproveStatusEnum;
 import com.common.business.vo.PagingVO;
@@ -16,6 +17,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Lambda
@@ -93,7 +95,14 @@ public class CustomerB2CDTO implements Serializable {
          * 创建时间
          */
         private List<LocalDate> createTimeList;
-
+        /**
+         * 页面高级查询
+         */
+        private List<AdvanceQueryDTO> advanceQueryDTOList;
+        /**
+         * sqlMap 默认key default
+         */
+        private Map<String, String> sqlMap;
     }
 
 
@@ -104,9 +113,19 @@ public class CustomerB2CDTO implements Serializable {
     @NoArgsConstructor
     public static class TabListDTO {
 
-        private String searchType;
+        /**
+         * 类型(toBeApprove 待审核，reject 审核不通过，approve 已审核)
+         */
+        private String tabFlag;
 
+        /**
+         * tab名称
+         */
+        private String tabFlagName;
 
+        /**
+         * 数量
+         */
         private Integer count;
 
     }
