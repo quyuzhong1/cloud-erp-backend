5c0e2b206a4ba4b0f32fe6a2a7cd37460f8125f4
Jim <zhoujingbo@ulanzi.cn>
2024-12-25 10:55:15 +0800

    UPDATE:工时统计查询调整高级查询

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/api/ProjectTaskTimeRecordController.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/api/ProjectTaskTimeRecordController.java
@@ -1,6 +1,7 @@
 package com.erp.server.plm.controller.api;
 
 
+import com.common.business.annotation.WebAdvanceQuery;
 import com.common.business.dto.base.PagingDTO;
 import com.common.business.vo.PagingVO;
 import com.common.core.anno.LogAction;
@@ -43,6 +44,7 @@ public class ProjectTaskTimeRecordController extends BaseController {
 //            tableField = "charge_id",
 //            menuCode = "plm:task:time:record:paging",
 //            tableAlias = "pt")
+    @WebAdvanceQuery
     public ApiResult<PagingVO<ProjectTaskTimeRecordPageVO>> pageTaskTimeRecord(@RequestBody PagingDTO<ProjectTaskTimeRecordDTO.PageRecordDto> dto){
         PagingVO<ProjectTaskTimeRecordPageVO>  pageVO = projectTaskTimeRecordService.pageRecord(dto);
         return success(pageVO);
