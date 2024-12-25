e4495267b8aebb5f1b8a2acfa808f44659734e1a
Will <wangwei@ulanzi.cn>
2024-12-25 09:27:20 +0800

    feat,虚拟仓库龄代码提交

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualTransFlowServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualTransFlowServiceImpl.java
@@ -206,7 +206,7 @@ public class VirtualTransFlowServiceImpl extends SuperServiceImpl<VirtualTransFl
         }
         return lambdaQuery().in(VirtualTransFlowEntity::getId,oldVirtualTransFlowIdList)
                 .lt(VirtualTransFlowEntity::getQty,MathUtil.ZERO)
-                .ne(VirtualTransFlowEntity::getIsUnapproved,Boolean.FALSE)
+                .eq(VirtualTransFlowEntity::getIsUnapproved,Boolean.FALSE)
                 .list();
     }
 
