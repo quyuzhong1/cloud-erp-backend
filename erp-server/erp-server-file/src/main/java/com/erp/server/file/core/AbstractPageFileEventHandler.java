3917c0d5edf99e5c0adee3ef812379f7682d373f
liaohui <liaohui@ulanzi.cn>
2024-12-26 11:24:52 +0800

    feat: es导出分页问题

--- erp-server/erp-server-file/src/main/java/com/erp/server/file/core/AbstractPageFileEventHandler.java
+++ erp-server/erp-server-file/src/main/java/com/erp/server/file/core/AbstractPageFileEventHandler.java
@@ -35,8 +35,14 @@ public abstract class AbstractPageFileEventHandler<T, P> extends AbstractFileEve
             if (totalCount == 0) {
                 totalCount = data.getTotalCount();
             }
-            if (totalCount <= dto.getCurrPage() * getPageSize()) {
-                hasNext = false;
+            if (1 == getFirstPage()) {
+                if (totalCount <= dto.getCurrPage() * getPageSize()) {
+                    hasNext = false;
+                }
+            } else {
+                if (totalCount <= (dto.getCurrPage() + 1) * getPageSize()) {
+                    hasNext = false;
+                }
             }
             dto.setCurrPage(dto.getCurrPage() + 1);
         }
