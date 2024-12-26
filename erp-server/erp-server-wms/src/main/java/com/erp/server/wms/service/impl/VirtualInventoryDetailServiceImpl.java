039ad5f856bd4ae69e90939eafea128f48fcb7c8
Will <wangwei@ulanzi.cn>
2024-12-26 20:44:54 +0800

    fix,库龄分析流水调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualInventoryDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualInventoryDetailServiceImpl.java
@@ -350,6 +350,8 @@ public class VirtualInventoryDetailServiceImpl extends SuperServiceImpl<VirtualI
         VirtualInventoryAgeDTO.viewHisInventoryAgeDetailDTO viewHisInventoryAgeDetailDTO = this.viewHisInventoryAgeDetail(dto);
 
         for (VirtualInventoryAgeDTO.HisInventoryAgeDetailDTO ageDetailDTO : list) {
+            //对象中都有库龄字段，防止被清空先赋值
+            viewHisInventoryAgeDetailDTO.setInventoryAgeDays(ageDetailDTO.getInventoryAgeDays());
             BeanMapperUtils.copy(viewHisInventoryAgeDetailDTO,ageDetailDTO);
             ageDetailDTO.setSourceTypeName(SourceTypeEnum.getName(ageDetailDTO.getSourceType()));
             ageDetailDTO.setDictInventoryStatusName(InventoryStatusEnum.getNameByCode(ageDetailDTO.getDictInventoryStatus()));
