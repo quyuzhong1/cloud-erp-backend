2482628798de2d7d8a3ca2b7c16edb3e17beee16
liuruipeng <arthur@ulanzi.cn>
2024-12-27 18:32:01 +0800

    feat:【领星】标发调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/delivery/RakutenShipOrder.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/delivery/RakutenShipOrder.java
@@ -62,14 +62,6 @@ public class RakutenShipOrder extends AbstractShipOrder {
         SoB2cLogisticsEntity logisticsEntity = tuple.get(2);
         //渠道
         String channelId = logisticsEntity.getLogisticsChannelId();
-        //获取销售渠道信息
-        LogisticsChannelDTO.SignShipDTO tmsSignShipDTO = logisticsFeign.getScaleChannelByChannelById(
-                channelId,
-                PlatformDictEnum.RAKUTEN.getCode()
-        );
-        if (null == tmsSignShipDTO) {
-            throw new ServiceException("找不到渠道信息");
-        }
         //获取中台配置的渠道信息
         ThirdMappingDTO.ViewParamDTO viewParamDTO = new ThirdMappingDTO.ViewParamDTO();
         viewParamDTO.setSysId(channelId);
@@ -106,9 +98,7 @@ public class RakutenShipOrder extends AbstractShipOrder {
             }
 
             //获取渠道标发单号
-            String standardOrderType = tmsSignShipDTO.checkAndGetOrderDeliveryMarkType();
-            String logisticsNo = CharSequenceUtil.equals(OrderDeliveryMarkTypeEnum.TRANSPORT_NO.getCode(), standardOrderType)
-                    ? logisticsEntity.getCode() : logisticsEntity.getTrackNo();
+            String logisticsNo =  logisticsEntity.getCode();
             if (CharSequenceUtil.isBlank(logisticsNo)) {
                 throw new ServiceException("【乐天标记发货】操作失败，渠道标发单号为空");
             }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/delivery/TemuShipOrder.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/sdk/delivery/TemuShipOrder.java
@@ -70,14 +70,6 @@ public class TemuShipOrder extends AbstractShipOrder {
         SoB2cLogisticsEntity logisticsEntity = tuple.get(2);
         //渠道
         String channelId = logisticsEntity.getLogisticsChannelId();
-        //获取销售渠道信息
-        LogisticsChannelDTO.SignShipDTO tmsSignShipDTO = logisticsFeign.getScaleChannelByChannelById(
-                channelId,
-                PlatformDictEnum.TE_MU.getCode()
-        );
-        if (null == tmsSignShipDTO) {
-            throw new ServiceException("找不到渠道信息");
-        }
         //获取中台配置的渠道信息
         ThirdMappingDTO.ViewParamDTO viewParamDTO = new ThirdMappingDTO.ViewParamDTO();
         viewParamDTO.setSysId(channelId);
@@ -115,9 +107,7 @@ public class TemuShipOrder extends AbstractShipOrder {
             }
 
             //获取渠道标发单号
-            String standardOrderType = tmsSignShipDTO.checkAndGetOrderDeliveryMarkType();
-            String logisticsNo = CharSequenceUtil.equals(OrderDeliveryMarkTypeEnum.TRANSPORT_NO.getCode(), standardOrderType)
-                    ? logisticsEntity.getCode() : logisticsEntity.getTrackNo();
+            String logisticsNo = logisticsEntity.getCode();
             if (CharSequenceUtil.isBlank(logisticsNo)) {
                 throw new ServiceException("【TEMU标记发货】操作失败，渠道标发单号为空");
             }
