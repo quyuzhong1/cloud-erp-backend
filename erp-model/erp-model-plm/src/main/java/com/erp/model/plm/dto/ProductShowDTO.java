6f8d6ea301034299498012d65d1f7964f5863352
Jim <zhoujingbo@ulanzi.cn>
2024-12-27 15:21:36 +0800

    UPDATE:产品开发调整高级查询

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductShowDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductShowDTO.java
@@ -275,4 +275,9 @@ public class ProductShowDTO implements Serializable {
      * 结项日期
      */
     private LocalDate projectFinishDate;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
 }
