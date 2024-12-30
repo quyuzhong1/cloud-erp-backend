677be19c94584435175a5a67db20a8060e705db9
Jim <zhoujingbo@ulanzi.cn>
2024-12-30 14:43:52 +0800

    UPDATE:plm同步商品到领星

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/sync/lingxing/impl/SyncLingXingProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/rocketmq/sync/lingxing/impl/SyncLingXingProductDetailServiceImpl.java
@@ -1,46 +1,24 @@
 package com.erp.server.plm.rocketmq.sync.lingxing.impl;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
-import com.common.business.dto.DmpPushTaskFeignDTO;
 import com.common.business.enums.SourceTypeEnum;
 import com.common.business.enums.SyncOperateEnum;
-import com.common.business.wrapper.FeignQuery;
-import com.common.core.utils.LengthConverterUtil;
-import com.common.core.utils.MathUtil;
-import com.common.message.constant.RocketMqTopic;
-import com.common.message.enums.RocketMqTagEnum;
-import com.erp.model.dmp.entity.CfgSettingEntity;
+import com.common.core.exception.ServiceException;
 import com.erp.model.dmp.entity.DmpPushTaskEntity;
 import com.erp.model.dmp.enums.DmpBasicSystemCodeEnum;
-import com.erp.model.dmp.enums.PlatformEnum;
-import com.erp.model.dmp.enums.SettingEnum;
 import com.erp.model.plm.entity.*;
-import com.erp.model.plm.enums.BomTypeEnum;
-import com.erp.model.plm.enums.SaleMethodEnum;
 import com.erp.rpc.dmp.feign.DmpMqFeign;
 import com.erp.server.plm.rocketmq.sync.lingxing.SyncLingXingProductDetailService;
 import com.erp.server.plm.service.*;
 import com.sdk.third.lingxing.dto.ProductInfo;
-import com.sdk.wangdian.sdk.api.goods.dto.GoodsBatchPushDTO;
+import com.sdk.third.lingxing.utils.LingxingApiUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionSynchronizationAdapter;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import static com.erp.model.plm.enums.SaleMethodEnum.*;
-import static com.erp.server.plm.constant.ProductConstant.PRODUCT_PROPERTY_COST;
-import static com.erp.server.plm.constant.ProductConstant.PRODUCT_PROPERTY_SERVICE;
 
 @Service
 public class SyncLingXingProductDetailServiceImpl implements SyncLingXingProductDetailService {
@@ -61,38 +39,18 @@ public class SyncLingXingProductDetailServiceImpl implements SyncLingXingProduct
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void syncDataToLingxing(ProductDetailEntity entity) {
-        DmpPushTaskEntity dmpPushTask = getGoodsBatchPushDTO(entity);
-        if (ObjectUtil.isNotEmpty(dmpPushTask)) {
-            sendMTask(Collections.singletonList(dmpPushTask));
+        PlmPushMsgEntity plmPushMsgEntity = createPlmPushMsgEntity(entity);
+        boolean save = plmPushMsgService.save(plmPushMsgEntity);
+        if (!save){
+            ServiceException.runError("保存本地消息失败:{}", JSONUtil.toJsonStr(plmPushMsgEntity));
         }
     }
 
-    private DmpPushTaskEntity getGoodsBatchPushDTO(ProductDetailEntity entity) {
+    private static PlmPushMsgEntity createPlmPushMsgEntity(ProductDetailEntity entity) {
         ProductInfo productInfo = new ProductInfo();
-        productInfo.setSku(entity.getSkuNo());
+        productInfo.setSku(LingxingApiUtils.convertLxSku(entity.getSkuNo()));
         productInfo.setSkuIdentifier(entity.getId());
-        productInfo.setProductName(entity.getName());
-        SettingEnum settingEnum = SettingEnum.NEW_DMP_PUSH_SWTICH_LIST;
-        List<CfgSettingEntity> list = FeignQuery.create(CfgSettingEntity.class)
-        		.eq(CfgSettingEntity::getKey, SourceTypeEnum.LX_PRODUCT_DETAIL.getCode())
-        		.eq(CfgSettingEntity::getType, settingEnum.getType())
-        		.eq(CfgSettingEntity::getValue, "1")
-        		.list();
-        if(CollUtil.isEmpty(list)) {
-        	//添加推送任务
-            DmpPushTaskFeignDTO taskEntity = new DmpPushTaskFeignDTO();
-            taskEntity.setSourceId(entity.getId());
-            taskEntity.setSourceCode(entity.getSkuNo());
-            taskEntity.setSourceType(SourceTypeEnum.PRODUCT_DETAIL.getCode());
-            taskEntity.setMqTopic(RocketMqTopic.SYNC_LINGXING_ERP_TOPIC);
-            taskEntity.setMqTag(RocketMqTagEnum.LINGXING_PRODUCT_DETAIL_TAG.getName());
-            taskEntity.setMqData(JSONUtil.toJsonStr(productInfo));
-            taskEntity.setSourcePlatformName(PlatformEnum.ERP.getDesc());
-            taskEntity.setTargetPlatformName(PlatformEnum.LINGXING.getDesc());
-            taskEntity.setSyncOperate(SyncOperateEnum.OPERATE_APPROVE.getCode());
-            return dmpMqFeign.saveTask(taskEntity);
-        }
-        
+        productInfo.setProductName(LingxingApiUtils.convertLxProductName(entity.getName()));
         PlmPushMsgEntity plmPushMsgEntity = new PlmPushMsgEntity();
         plmPushMsgEntity.setTargetPlatform(DmpBasicSystemCodeEnum.LING_XING.getCode());
         plmPushMsgEntity.setSourceType(SourceTypeEnum.LX_PRODUCT_DETAIL.getCode());
@@ -100,27 +58,21 @@ public class SyncLingXingProductDetailServiceImpl implements SyncLingXingProduct
         plmPushMsgEntity.setSourceCode(entity.getSkuNo());
         plmPushMsgEntity.setSyncOperate(SyncOperateEnum.OPERATE_APPROVE.getCode());
         plmPushMsgEntity.setPushData(JSON.toJSONString(productInfo));
-        
-        plmPushMsgService.save(plmPushMsgEntity);
-        
-        return null;
+        return plmPushMsgEntity;
     }
+
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void syncDataToLingxing(List<ProductDetailEntity> entityList) {
-        List<DmpPushTaskEntity> taskEntities = entityList.stream().map(this::getGoodsBatchPushDTO).collect(Collectors.toList());
-        sendMTask(taskEntities);
+        List<PlmPushMsgEntity> msgList = entityList.stream().map(SyncLingXingProductDetailServiceImpl::createPlmPushMsgEntity).collect(Collectors.toList());
+        boolean save = plmPushMsgService.saveBatch(msgList);
+        if (!save){
+            ServiceException.runError("保存本地消息失败:{}", JSONUtil.toJsonStr(msgList));
+        }
     }
 
 
-    private void sendMTask(List<DmpPushTaskEntity> dmpPushTask) {
-        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
-            @Override
-            public void afterCommit() {
-                dmpMqFeign.sendTask(dmpPushTask);
-            }
-        });
-    }
 
 
     @Override
