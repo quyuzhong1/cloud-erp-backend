e9a3fdd2b2bbd3cc2e8f031b96249edce8f51fcd
Jim <zhoujingbo@ulanzi.cn>
2024-12-30 22:10:03 +0800

    BUG:B2C-客户列表高级查询fix

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/CustomerB2cQueryHandler.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/CustomerB2cQueryHandler.java
@@ -18,21 +18,24 @@ public class CustomerB2cQueryHandler extends AbstractQueryHandler {
 
 
     public String getTabSql (Object value) {
+        if ("all".equals(value)){
+            return getQueryAllSql();
+        }
         // 待提交
-        if (PageListTypeEnum.WAIT_SUBMIT.getCode().equals(value)) {
+        if (ApproveStatusEnum.WAIT_SUBMIT.getCode().equals(value)) {
             super.buildDefaultDTO("ci.approve_status", ApproveStatusEnum.WAIT_SUBMIT.getStatus());
         }
         // 待审核
-        if (PageListTypeEnum.TO_BE_APPROVE.getCode().equals(value)) {
-            super.buildDefaultDTO("mi.approve_status", ApproveStatusEnum.APPROVE_ING.getStatus());
+        if (ApproveStatusEnum.APPROVE_ING.getCode().equals(value)) {
+            super.buildDefaultDTO("ci.approve_status", ApproveStatusEnum.APPROVE_ING.getStatus());
         }
         // 已审核
-        if (PageListTypeEnum.APPROVE.getCode().equals(value)) {
-            super.buildDefaultDTO("mi.approve_status", ApproveStatusEnum.APPROVE.getStatus());
+        if (ApproveStatusEnum.APPROVE.getCode().equals(value)) {
+            super.buildDefaultDTO("ci.approve_status", ApproveStatusEnum.APPROVE.getStatus());
         }
         //不通过
-        if (PageListTypeEnum.REJECT.getCode().equals(value)) {
-            super.buildDefaultDTO("mi.approve_status", ApproveStatusEnum.REJECT.getStatus());
+        if (ApproveStatusEnum.REJECT.getCode().equals(value)) {
+            super.buildDefaultDTO("ci.approve_status", ApproveStatusEnum.REJECT.getStatus());
         }
         return super.getSplicingSQL();
     }
