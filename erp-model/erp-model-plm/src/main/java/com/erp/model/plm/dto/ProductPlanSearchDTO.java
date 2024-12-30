0c1dbb24b75e7a702221f1552a3189607cd1f957
Jim <zhoujingbo@ulanzi.cn>
2024-12-30 21:05:43 +0800

    UPDATE:产品规划高级查询fix

--- erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductPlanSearchDTO.java
+++ erp-model/erp-model-plm/src/main/java/com/erp/model/plm/dto/ProductPlanSearchDTO.java
@@ -95,4 +95,8 @@ public class ProductPlanSearchDTO extends SortDTO {
      * sqlMap 默认key default
      */
     private Map<String, String> sqlMap;
+
+    public ProductPlanSearchDTO(String type) {
+        this.type = type;
+    }
 }
