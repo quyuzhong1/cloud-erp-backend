5ae4dea56c24d13da34930d7ca8ee7a985129029
liuruipeng <arthur@ulanzi.cn>
2024-12-30 11:49:52 +0800

    feat:【领星】领星判断平台仓

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cEntity.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/entity/SoB2cEntity.java
@@ -471,7 +471,9 @@ public class SoB2cEntity extends BaseEntity<SoB2cEntity> {
                 return "fulfillment".equalsIgnoreCase(labelJsonDTO.getLogisticType());
             }
         }
-        if(PlatformDictEnum.LING_XING.getCode().equalsIgnoreCase(this.getThirdSystem())){
+        if(PlatformDictEnum.TE_MU.getCode().equalsIgnoreCase(this.dictPlatform)
+        ||PlatformDictEnum.RAKUTEN.getCode().equalsIgnoreCase(this.dictPlatform)
+        ||PlatformDictEnum.EBAY.getCode().equalsIgnoreCase(this.dictPlatform)){
             if (StrUtil.isNotBlank(this.labelJson)) {
                 SoB2cDTO.LabelDTO labelJsonDTO = JSONUtil.toBean(this.labelJson, SoB2cDTO.LabelDTO.class);
                 //平台仓发货
