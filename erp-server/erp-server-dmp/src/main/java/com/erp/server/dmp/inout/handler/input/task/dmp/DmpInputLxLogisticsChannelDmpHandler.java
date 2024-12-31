d7b6fc1a99334c3bd5ec3b438cbfbbf70a5bfd43
Jim <zhoujingbo@ulanzi.cn>
2024-12-31 21:08:58 +0800

    UPDATE:兼容领星物流渠道禁用fix

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputLxLogisticsChannelDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputLxLogisticsChannelDmpHandler.java
@@ -1,12 +1,12 @@
 package com.erp.server.dmp.inout.handler.input.task.dmp;
 
+import cn.hutool.core.text.CharSequenceUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * dmp处理下一个扩展handler，如何订单收货人信息单独一张表，使用此handler即可，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
@@ -18,7 +18,35 @@ import java.util.TreeMap;
 @Scope("prototype")
 public class DmpInputLxLogisticsChannelDmpHandler extends DmpInputDbConvertDmpHandler {
 
+    public static final String LX_USED_LOGISTICS_TYPE_DATA_MONGO_TABLE = "lingxing_listUsedLogisticsType_data";
 
+
+    @Override
+    protected Map<List<Map<String, Object>>, List<TreeMap<String, Object>>> convertData(List<Map<String, Object>> dmpInputMongoEntityList) {
+        // 查询历史所有信息
+        List<Map<String, Object>> mongoData = mongoService.findMongoData(new ArrayList<>(), LX_USED_LOGISTICS_TYPE_DATA_MONGO_TABLE);
+
+        List<String> newMongoListKeyList = dmpInputMongoEntityList.stream().map(e -> {
+            String type = e.getOrDefault("type", "").toString();
+            String logisticsProviderId = e.getOrDefault("logistics_provider_id", "").toString();
+            String typeId = e.getOrDefault("type_id", "").toString();
+            return CharSequenceUtil.format("{}_{}_{}", type, logisticsProviderId, typeId);
+        }).distinct().collect(Collectors.toList());
+
+        // 不存在mongo的信息设置为禁用
+        for (Map<String, Object> mongoItem : mongoData) {
+            String type = mongoItem.getOrDefault("type", "").toString();
+            String logisticsProviderId = mongoItem.getOrDefault("logistics_provider_id", "").toString();
+            String typeId = mongoItem.getOrDefault("type_id", "").toString();
+            String curKey = CharSequenceUtil.format("{}_{}_{}", type, logisticsProviderId, typeId);
+            if (newMongoListKeyList.contains(curKey)){
+                continue;
+            }
+            mongoItem.put("is_used", 0);
+            dmpInputMongoEntityList.add(mongoItem);
+        }
+        return super.convertData(dmpInputMongoEntityList);
+    }
 
     @Override
     protected void afterConvertData(Map<List<Map<String, Object>>, List<TreeMap<String, Object>>> dmpInputDataDmpRelationMaps) {
