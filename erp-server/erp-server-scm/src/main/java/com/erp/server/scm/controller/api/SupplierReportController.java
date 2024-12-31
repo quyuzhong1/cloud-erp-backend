f4026363f0d7f63d3ce274b740aa35f8eb265177
Jim <zhoujingbo@ulanzi.cn>
2024-12-31 15:51:42 +0800

    ADD:供应商报表高级查询

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/SupplierReportController.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/controller/api/SupplierReportController.java
@@ -9,6 +9,7 @@ import com.common.core.controller.BaseController;
 import com.common.core.controller.vo.ApiResult;
 import com.common.core.enums.LogActionEnum;
 import com.erp.model.scm.dto.SupplierReportDTO;
+import com.erp.server.scm.query.SupplierReportQueryHandler;
 import com.erp.server.scm.service.SupplierReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -36,7 +37,7 @@ public class SupplierReportController extends BaseController {
      * @return
      */
     @PostMapping("paging")
-    @WebAdvanceQuery
+    @WebAdvanceQuery(handler = SupplierReportQueryHandler.class)
     public ApiResult<PagingVO<SupplierReportDTO.PagingViewDTO>> paging(@RequestBody @Validated PagingDTO<SupplierReportDTO.PagingSearchParamDTO> dto) {
         return success(supplierReportService.supplierPaging(dto));
     }
@@ -48,6 +49,7 @@ public class SupplierReportController extends BaseController {
      */
     @LogAction(value = LogActionEnum.EXPORT, desc = "供应商报表导出")
     @PostMapping(value = "/exportExcel")
+    @WebAdvanceQuery(handler = SupplierReportQueryHandler.class)
     public ApiResult<Boolean> exportExcel(@RequestBody SupplierReportDTO.ExportSearchParamDTO dto) {
         supplierReportService.exportList(dto);
         return success(true);
