515904ff3a1bb0143018e11dbaf07d5cb2d20cad
Jim <zhoujingbo@ulanzi.cn>
2025-01-02 09:49:23 +0800

    BUG:库存高级查询导出fix

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/inventory/InventoryReportDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/inventory/InventoryReportDTO.java
@@ -480,7 +480,7 @@ public class InventoryReportDTO implements Serializable {
          * 日期类型 (approveDate审核日期，billDate单据日期)
          * /api/wms/dict/list，字典inventoryDate
          */
-        @NotBlank(message = "日期类型不能为空")
+//        @NotBlank(message = "日期类型不能为空")
         private String dateType;
 
         /**
