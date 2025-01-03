095e0e3bd2342f6c7190180d2d5f94dad2b99d59
Will <wangwei@ulanzi.cn>
2025-01-03 18:19:40 +0800

    fix,虚拟仓订单调整

--- erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/service/PurchaseSuggestService.java
+++ erp-server/erp-server-mrp/src/main/java/com/erp/server/mrp/service/PurchaseSuggestService.java
@@ -152,13 +152,4 @@ public interface PurchaseSuggestService extends SuperService<PurchaseSuggestEnti
      * @return List<PurchaseSuggestEntity>
      */
     List<PurchaseSuggestEntity> listGeneratePurchaseSuggestMerge(PurchaseSuggestEntity purchaseSuggestEntity);
-
-    /**
-     * 查询锁定数量
-     * @author will
-     * @date 2024/11/25 18:08
-     * @param entity
-     * @return Integer
-     */
-    Integer getLockingQty(PurchaseSuggestEntity entity);
 }
