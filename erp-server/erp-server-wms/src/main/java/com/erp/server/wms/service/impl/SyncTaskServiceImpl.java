d2767ac0943ed1a072e66de9e97c5ac75342f3a5
luoweigang <821969511@qq.com>
2025-01-03 19:40:10 +0800

    修改数帝云同步问题0103

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SyncTaskServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SyncTaskServiceImpl.java
@@ -1114,7 +1114,7 @@ public class SyncTaskServiceImpl implements SyncTaskService {
         orgList.addAll(salesOrgId);
         List<String> financialOrganization = customerInfoList.stream().map(req -> req.getFinancialOrganization()).distinct().collect(Collectors.toList());
         orgList.addAll(financialOrganization);
-        List<String> salesOrgIds = b2cEntity.stream().map(req -> req.getSalesOrgId()).distinct().collect(Collectors.toList());
+        List<String> salesOrgIds = soOutstockEntities.stream().map(req -> req.getSalesOrgId()).distinct().collect(Collectors.toList());
         orgList.addAll(salesOrgIds);
         List<BaseIdDTO.CodeDTO> companyEntities = sysUserFeign.getAccountingCompanyList(orgList);
 
