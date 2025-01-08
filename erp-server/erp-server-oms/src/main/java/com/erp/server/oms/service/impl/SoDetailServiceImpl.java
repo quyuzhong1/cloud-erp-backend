cf0ff17b79dae313366451569c07dd1514b9e4a2
liuruipeng <arthur@ulanzi.cn>
2025-01-08 16:49:05 +0800

    添加事务

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoDetailServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoDetailServiceImpl.java
@@ -1023,6 +1023,7 @@ public class SoDetailServiceImpl extends SuperServiceImpl<SoDetailMapper, SoDeta
      * @date 2023-05-23 10:26
      */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void updateDeliveryStatus(List<SoDetailDTO.UpdateDeliveryStatusDTO> paramList) {
         if (CollectionUtils.isEmpty(paramList)) {
             return;
