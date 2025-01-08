76e4facb58fbe2a276560bec2562b42b4b189313
shukai <shukai@ulanzi.cn>
2025-01-08 11:53:11 +0800

    add:判断是否有创建时间

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyOrderHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyOrderHandler.java
@@ -215,7 +215,9 @@ public class DmpOutputSdyOrderHandler extends DmpOutputTaskHandler {
             if (dmpSoInfoEntity.getPayTime() != null) {
                 shudiyunB2cOrderDTO.setBiz_time(localDateTime.format(dmpSoInfoEntity.getPayTime()));
             } else {
-                shudiyunB2cOrderDTO.setBiz_time(localDateTime.format(dmpSoInfoEntity.getPlatformCreateTime()));
+            	if(dmpSoInfoEntity.getPlatformCreateTime() != null) {
+            		shudiyunB2cOrderDTO.setBiz_time(localDateTime.format(dmpSoInfoEntity.getPlatformCreateTime()));
+            	}
             }
 
             //如果是旺店通中台表的订单属于配货单，其他的都是线上原始订单
