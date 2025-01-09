94149562eb8dabfc6145d0a8395b34fde3883400
jack chen <jackchen@ulanzi.cn>
2025-01-09 20:12:53 +0800

    2025-01-06 无销售订单查询销售出库单最新日期的金额和含税金额

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnDetailServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnDetailServiceImpl.java
@@ -765,31 +765,22 @@ public class SoReturnDetailServiceImpl extends SuperServiceImpl<SoReturnDetailMa
         params.setCustomerId(dto.getCustomerId());
         params.setSkuIds(Collections.singletonList(dto.getSkuId()));
         params.setReturnCreateDate(returnCeateDate.toString());
+        params.setCurrency(dto.getCurrency());
         List<SoOutstockDTO.AmountDTO> amountDTOS = soOutstockFeign.listAmountBySkuIds(params);
-
         if(CollectionUtils.isNotEmpty(amountDTOS)){
+            SoDetailEntity soDetailEntity = soDetailService.lambdaQuery()
+                    .eq(SoDetailEntity::getCurrency, dto.getCurrency())
+                    .eq(SoDetailEntity::getId, amountDTOS.get(0).getSoDetailId())
+                    .one();
             //退货金额
-            BigDecimal returnAmount = amountDTOS.get(0).getAmount()
-                    .divide(BigDecimal.valueOf(amountDTOS.get(0).getQty()), 4, RoundingMode.DOWN)
+            BigDecimal returnAmount = soDetailEntity.getPrice()
                     .multiply(BigDecimal.valueOf(dto.getReturnQty()))
                     .stripTrailingZeros();
             //含税退货金额
-            BigDecimal taxReturnAmount = amountDTOS.get(0).getTaxAmount()
-                    .divide(BigDecimal.valueOf(amountDTOS.get(0).getQty()), 4, RoundingMode.DOWN)
+            BigDecimal taxReturnAmount = soDetailEntity.getTaxAmount()
+                    .divide(BigDecimal.valueOf(soDetailEntity.getQty()), 4, RoundingMode.DOWN)
                     .multiply(BigDecimal.valueOf(dto.getReturnQty()))
                     .stripTrailingZeros();
-            //先判断币种和汇率是否跟销售出库单一致，若不同的情况下计算出详情页的币种的退货金额
-            if(!amountDTOS.get(0).equals(dto.getCurrency())){
-                //销售订单币种 转 CNY的汇率
-                Map<String, BigDecimal> currencyMap = soReturnService.getCurrencyMap(Collections.singletonList(amountDTOS.get(0).getCurrency()));
-                BigDecimal soRate = currencyMap.get(amountDTOS.get(0).getCurrency());
-                //页面汇率 转 CNY的汇率
-                BigDecimal viewRate = dto.getExchangeRate();
-                //
-                returnAmount = returnAmount.multiply(soRate).divide(viewRate, 4, RoundingMode.DOWN);
-                //
-                taxReturnAmount = taxReturnAmount.multiply(soRate).divide(viewRate, 4, RoundingMode.DOWN);
-            }
             //退货金额（本位币）
             BigDecimal returnAmountLocalCurrency = returnAmount
                     .multiply(dto.getExchangeRate())
