9707fe835b45ef7589f86da3e9d5db24e91c795d
Jim <zhoujingbo@ulanzi.cn>
2025-01-16 17:19:25 +0800

    UPDATE:FBA同步货件查询fix3

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/FbaShipmentSyncQueryHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/query/FbaShipmentSyncQueryHandler.java
@@ -4,6 +4,7 @@ import com.common.business.enums.PlatformDictEnum;
 import com.common.business.enums.QueryConditionEnum;
 import com.common.business.enums.QueryDataTypeEnum;
 import com.common.business.query.AbstractQueryHandler;
+import com.common.business.threadlocal.AdvanceQueryContext;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,8 +15,9 @@ public class FbaShipmentSyncQueryHandler extends AbstractQueryHandler {
 
     @Override
     protected String handleSqlLogic(String field, Object value, String compareCodeSplicingValueSql) {
+        QueryConditionEnum queryConditionEnum = AdvanceQueryContext.getCompareCode();
         if ("name".equals(field)) {
-            super.buildDefaultDTO("si.name", value);
+            super.buildSplicingSQLDTO("si.name", queryConditionEnum, value, QueryDataTypeEnum.STRING);
             super.buildDefaultDTO("si.dict_platform", PlatformDictEnum.AMAZON.getCode());
         }
         return null;
