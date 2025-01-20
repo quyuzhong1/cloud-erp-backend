818143c34188ba1eabc52173770c876782be4672
Jim <zhoujingbo@ulanzi.cn>
2025-01-20 10:19:47 +0800

    BUG:项目报表查询fix

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/query/ProjectReportFormsQueryHandler.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/query/ProjectReportFormsQueryHandler.java
@@ -3,7 +3,15 @@ package com.erp.server.plm.query;
 import com.common.business.enums.QueryConditionEnum;
 import com.common.business.query.AbstractQueryHandler;
 import com.common.business.threadlocal.AdvanceQueryContext;
+import com.erp.model.plm.enums.ApprovalStatusEnum;
+import com.erp.model.plm.enums.ProjectReportStatusEnum;
+import com.erp.model.plm.enums.ProjectStateEnum;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProjectReportFormsQueryHandler extends AbstractQueryHandler {
@@ -12,7 +20,7 @@ public class ProjectReportFormsQueryHandler extends AbstractQueryHandler {
     @Override
     protected String handleSqlLogic(String field, Object value, String compareCodeSplicingValueSql) {
         if ("sku_no".equals(field)) {
-            return "exists ( SELECT id FROM product_detail pd WHERE pd.sku_no "+ compareCodeSplicingValueSql + ")";
+            return " pi.id IN ( SELECT product_id FROM product_detail pd WHERE pd.sku_no "+ compareCodeSplicingValueSql + " GROUP BY product_id)";
         }
         QueryConditionEnum queryConditionEnum = AdvanceQueryContext.getCompareCode();
         if ("ps.sale_state".equals(field)) {
