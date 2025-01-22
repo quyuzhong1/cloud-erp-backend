f14eec8008d604bded3cb0cbf05bdc6989dd156c
shukai <shukai@ulanzi.cn>
2025-01-22 20:51:50 +0800

    add

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
@@ -1174,6 +1174,8 @@ public class LogisticsBillCostServiceImpl extends SuperServiceImpl<LogisticsBill
                 	List<TmsCostDetailEntity> dbActualList = tmsCostDetailService.lambdaQuery().eq(TmsCostDetailEntity::getMainId, entity.getId()).eq(TmsCostDetailEntity::getType, DetailReconciliationTypeEnum.ACTUAL.getCode()).list();
                     if(CollUtil.isEmpty(dbActualList)) {
                     	buildFirstMileCostDetail(detailEntity,entity);
+                    }else {
+                    	entity.setUpdateList(BeanUtil.copyToList(dbActualList, TmsCostDetailDTO.UpdateDTO.class));
                     }
                 }
             }
