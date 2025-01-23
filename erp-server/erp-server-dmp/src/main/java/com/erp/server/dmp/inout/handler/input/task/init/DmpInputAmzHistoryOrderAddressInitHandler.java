7c97c951b6b57809b913437719682b8d43c7388f
Jim <zhoujingbo@ulanzi.cn>
2025-01-23 23:33:07 +0800

    ADD:添加亚马逊查询老中台mongo推送

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAmzHistoryOrderAddressInitHandler.java
@@ -0,0 +1,89 @@
+package com.erp.server.dmp.inout.handler.input.task.init;
+
+import cn.hutool.core.collection.CollUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.common.core.exception.ServiceException;
+import com.erp.model.dmp.dto.AmazonShopInfoDTO;
+import com.erp.sdk.oms.amz.spapi.dto.PlatformAmazonOrderDTO;
+import com.erp.sdk.oms.amz.spapi.model.orders.*;
+import com.erp.server.dmp.inout.dto.base.DmpInputTaskInitDTO;
+import com.erp.server.dmp.inout.dto.request.DmpInputInitRequest;
+import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
+import com.erp.server.dmp.service.CfgAppClientService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * dmp输入init任务基础处理器，被init任务状态执行器继承，因有成员变量，最终实现类由spring管理需要是多例@Scope("prototype")
+ *
+ * @author Administrator
+ */
+@Slf4j
+@Service
+@Scope("prototype")
+public class DmpInputAmzHistoryOrderAddressInitHandler extends DmpInputAmzHistoryOrderAbstractApiInitHandler {
+    @Resource
+    private CfgAppClientService cfgAppClientService;
+
+    @Override
+    public List<DmpInputTaskInitDTO> getInitData(DmpInputInitRequest dmpRequest, DmpInputTaskResponse dmpResponse) {
+        // 获取上一级mongo数据
+        List<Map<String, Object>> findMongoData = getParentStorageMongoData();
+        if (CollUtil.isEmpty(findMongoData)) {
+            return Collections.emptyList();
+        }
+
+        List<JSONObject> curJsonList = new LinkedList<>();
+
+        List<String> uniqueIdList = findMongoData.stream()
+                .map(e -> e.getOrDefault("uniqueId", "").toString())
+                .distinct()
+                .collect(Collectors.toList());
+
+        List<PlatformAmazonOrderDTO> orderMongoData = findAllOrderMongoData(uniqueIdList);
+        if (CollectionUtils.isEmpty(orderMongoData)) {
+            ServiceException.runError("找不到mongo订单明细");
+        }
+        for (PlatformAmazonOrderDTO orderMongoDatum : orderMongoData) {
+
+            Address shippingAddress = orderMongoDatum.getOrder().getShippingAddress();
+            BuyerInfo buyerInfo = orderMongoDatum.getOrder().getBuyerInfo();
+            JSONObject jsonObject = setAmazonOrderIdAndToJsonObject(shippingAddress,
+                    buyerInfo,
+                    orderMongoDatum.getOrder().getAmazonOrderId(),
+                    orderMongoDatum.getPlatformShopCode()
+            );
+            curJsonList.add(jsonObject);
+
+        }
+
+        return Collections.singletonList(DmpInputTaskInitDTO.initMsg(JSONArray.toJSONString(curJsonList)));
+    }
+
+
+    /**
+     * 设置亚马逊订单ID和转换JSON
+     */
+    private JSONObject setAmazonOrderIdAndToJsonObject(Address address, BuyerInfo buyerInfo, String amazonOrderId, String platformShopCode) {
+        JSONObject json = (JSONObject) JSON.toJSON(address);
+        json.put("amazonOrderId", amazonOrderId);
+        json.put("platformShopCode", platformShopCode);
+        JSONObject jsonBuyer = (JSONObject) JSON.toJSON(buyerInfo);
+        json.put("buyerInfo", jsonBuyer);
+        return json;
+    }
+
+
+
+}
---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputAmzHistoryOrderApiInitHandler.java
@@ -0,0 +1,102 @@
+package com.erp.server.dmp.inout.handler.input.task.init;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.common.business.enums.PlatformDictEnum;
+import com.common.business.utils.RedisUtil;
+import com.common.core.exception.ServiceException;
+import com.erp.model.dmp.dto.AmazonShopInfoDTO;
+import com.erp.model.dmp.entity.DmpSoInfoEntity;
+import com.erp.model.oms.entity.ShopInfoEntity;
+import com.erp.rpc.oms.feign.ShopInfoFeign;
+import com.erp.sdk.oms.amz.spapi.dto.PlatformAmazonOrderDTO;
+import com.erp.sdk.oms.amz.spapi.model.orders.Order;
+import com.erp.server.dmp.inout.dto.base.DmpInputTaskInitDTO;
+import com.erp.server.dmp.inout.dto.request.DmpInputInitRequest;
+import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
+import com.erp.server.dmp.service.CfgAppClientService;
+import com.erp.server.dmp.service.DmpSoInfoService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * dmp输入init任务基础处理器下的旺店通api获取数据方式
+ *
+ * @author Administrator
+ */
+@Slf4j
+@Service
+@Scope("prototype")
+public class DmpInputAmzHistoryOrderApiInitHandler extends DmpInputAmzHistoryOrderAbstractApiInitHandler {
+
+    @Resource
+    private ShopInfoFeign shopInfoFeign;
+
+    @Resource
+    private DmpSoInfoService dmpSoInfoService;
+
+    @Override
+    public List<DmpInputTaskInitDTO> getInitData(DmpInputInitRequest dmpRequest, DmpInputTaskResponse dmpResponse) {
+        // 获取店铺信息
+        String shopId = nextLevelId;
+        ShopInfoEntity shopInfo = shopInfoFeign.getShopInfoById(shopId);
+        List<ShopInfoEntity> relatedshopInfoList = shopInfoFeign.getRelatedShopById(shopInfo);
+        AmazonShopInfoDTO shopInfoDTO = initShopInfoDTO(shopInfo,  relatedshopInfoList);
+        return getDmpInputTaskInitDTOS(shopInfoDTO);
+    }
+
+
+
+    /**
+     * 查询订单
+     */
+    private List<DmpInputTaskInitDTO> getDmpInputTaskInitDTOS(AmazonShopInfoDTO shopInfoDTO) {
+        // 开始时间
+        LocalDateTime startTime = dmpInputTaskEntity.getStartTime();
+        // 结束时间
+        LocalDateTime endTime = dmpInputTaskEntity.getEndTime();
+        List<PlatformAmazonOrderDTO> mongoOrderList = findOrdersByCriteria(startTime.toString(), endTime.toString(), shopInfoDTO.getPlatformShopCode(), Order.OrderStatusEnum.SHIPPED.getValue());
+        if (CollectionUtils.isEmpty(mongoOrderList)){
+            return Collections.emptyList();
+        }
+        List<String> orderIdList = mongoOrderList.stream().map(e -> e.getOrder().getAmazonOrderId()).distinct().collect(Collectors.toList());
+
+        List<DmpSoInfoEntity> dmpList = dmpSoInfoService.lambdaQuery()
+                .eq(DmpSoInfoEntity::getSourceSystem, PlatformDictEnum.AMAZON.getCode())
+                .eq(DmpSoInfoEntity::getSourcePlatform, PlatformDictEnum.AMAZON.getCode())
+                .in(DmpSoInfoEntity::getThirdCode, orderIdList)
+                .list();
+        List<String> existOrderId = dmpList.stream().map(DmpSoInfoEntity::getThirdCode).collect(Collectors.toList());
+
+
+        List<JSONObject> curJsonList = mongoOrderList.stream()
+                .filter(e-> !existOrderId.contains(e.getOrder().getAmazonOrderId()))
+                .map(e -> fillDataAndToJsonObject(e.getOrder(), shopInfoDTO, e.getUniqueId())).collect(Collectors.toList());
+
+        // 返回下载源数据
+        return Collections.singletonList(DmpInputTaskInitDTO.initMsg(JSONArray.toJSONString(curJsonList)));
+    }
+
+    /**
+     * 设置亚马逊账号和转换JSON
+     */
+    private JSONObject fillDataAndToJsonObject(Order entity, AmazonShopInfoDTO shopInfoDTO, String uniqueId) {
+        JSONObject json = (JSONObject) JSON.toJSON(entity);
+        json.put("platformShopCode", shopInfoDTO.getPlatformShopCode());
+        // 根据站点判断店铺ID
+        AmazonShopInfoDTO.ShopNameDTO shopNameDTO = shopInfoDTO.getMarketplaceShopIdMap().get(entity.getMarketplaceId());
+        json.put("shopId", shopNameDTO.getShopId());
+        json.put("shopName", shopNameDTO.getShopName());
+        json.put("uniqueId", uniqueId);
+        return json;
+    }
+}
