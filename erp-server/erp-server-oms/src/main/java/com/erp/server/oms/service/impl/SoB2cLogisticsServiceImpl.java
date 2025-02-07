360f5d6400d4e8741e2c65507ab90f26f243f9ce
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-02-07 10:04:56 +0800

    处理自动生成费用分摊异常记录

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cLogisticsServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cLogisticsServiceImpl.java
@@ -524,32 +524,4 @@ public class SoB2cLogisticsServiceImpl extends SuperServiceImpl<SoB2cLogisticsMa
         this.lambdaUpdate().eq(SoB2cLogisticsEntity::getMainId, b2cSoId)
                 .set(SoB2cLogisticsEntity::getEstimatedShippingCost, totalShippingCost).set(SoB2cLogisticsEntity::getEstimatedShippingCurrency,currency).update();
     }
-
-    private LogisticsBillDTO.AddDTO buildLogisticsBill(SoB2cLogisticsEntity entity, SoB2cEntity mainEntity) {
-        LogisticsBillDTO.AddDTO addDTO = new LogisticsBillDTO.AddDTO();
-        addDTO.setShopId(mainEntity.getShopId());
-        addDTO.setShopName(mainEntity.getShopName());
-        addDTO.setSalesPlatform(LogisticsPlatformEnum.SHOPEE.getCode());
-        addDTO.setDeliveryTime(entity.getDeliveryTime());
-        addDTO.setOrderTime(mainEntity.getPayTime());
-        addDTO.setTransportNo(entity.getCode());
-        addDTO.setDetailList(buildDetailList(entity));
-        addDTO.setSourceId(mainEntity.getId());
-        addDTO.setSourceCode(mainEntity.getPlatformCode());
-        addDTO.setSourceType(SourceTypeEnum.SO_B2C.getCode());
-        addDTO.setOutstockId("");
-        addDTO.setOutstockCode("");
-        addDTO.setChannelId(entity.getLogisticsChannelId());
-        addDTO.setOrderType(OrderTypeEnum.B2C.getCode());
-        return addDTO;
-    }
-
-    private List<LogisticsBillDetailDTO.AddDTO> buildDetailList(SoB2cLogisticsEntity entity) {
-        List<LogisticsBillDetailDTO.AddDTO> addDTOList = new ArrayList<>();
-        LogisticsBillDetailDTO.AddDTO addDTO = new LogisticsBillDetailDTO.AddDTO();
-        addDTO.setTrackNo(entity.getCode());
-        addDTO.setTrackStatus("0");
-        addDTOList.add(addDTO);
-        return addDTOList;
-    }
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/sync/impl/SyncB2CSoOutstockServiceImpl.java
@@ -329,6 +329,7 @@ public class SyncB2CSoOutstockServiceImpl implements SyncB2CSoOutstockService {
         //保存销售出库单详情
         soOutstockDetailService.saveBatch(detailList);
         //根据销售出库单创建物流单和自发货费用
+        soOutstock.setId(id);
         soOutstockService.saveLogisticsBill(soOutstock);
         //扣减库存
         InventoryInOutStockRuleDTO inventoryInOutStockDTO = getInventoryInOutStockRuleDTO(inOutStockList);
