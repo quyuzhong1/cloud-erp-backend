d141948d7194332c1432737e9181745a8be82200
shukai <shukai@ulanzi.cn>
2025-02-10 11:42:10 +0800

    add：去除wms没有用到的mq消费者

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformSoOutStockConsumerService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformSoOutStockConsumerService.java
@@ -57,11 +57,11 @@ import java.util.stream.Collectors;
  */
 @Service
 @Slf4j
-@RocketMQMessageListener(topic = RocketMqTopic.PLATFORM_PULL_DATA_TOPIC,
-        selectorExpression = "third_system_so_out_stock_tag",
-        consumerGroup = "${spring.cloud.nacos.discovery.namespace}-platform_pull_so_out_stock_consumer",
-        consumeMode = ConsumeMode.CONCURRENTLY
-)
+//@RocketMQMessageListener(topic = RocketMqTopic.PLATFORM_PULL_DATA_TOPIC,
+//        selectorExpression = "third_system_so_out_stock_tag",
+//        consumerGroup = "${spring.cloud.nacos.discovery.namespace}-platform_pull_so_out_stock_consumer",
+//        consumeMode = ConsumeMode.CONCURRENTLY
+//)
 public class PlatformSoOutStockConsumerService<T extends DmpSyncTaskIdDTO> extends AbstractPlatformConsumerHandler<T> {
 
     @Resource
