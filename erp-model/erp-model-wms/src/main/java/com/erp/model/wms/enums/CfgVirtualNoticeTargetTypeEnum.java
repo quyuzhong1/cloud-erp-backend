05934cc5b769ad418ea87d4dc2de7f70daaa5343
Will <wangwei@ulanzi.cn>
2025-02-12 18:25:36 +0800

    feat,通知类型枚举添加

---
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/enums/CfgVirtualNoticeTargetTypeEnum.java
@@ -0,0 +1,62 @@
+package com.erp.model.wms.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.common.core.constant.EnumMessage;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+/**
+ * 通知对象类型
+ * @Auther will
+ * @Date 2025/2/12 16:13
+ */
+public enum CfgVirtualNoticeTargetTypeEnum implements EnumMessage {
+
+    BY_USER("byUser","发送到人员"),
+    BY_GROUP("byGroup","发送到群"),
+    ;
+
+    /**
+     * 类型
+     */
+    @EnumValue
+    @JsonValue
+    private String code;
+    /**
+     * 名称
+     */
+    private String name;
+
+
+    CfgVirtualNoticeTargetTypeEnum(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public static String getName(String code) {
+        for (CfgVirtualNoticeTargetTypeEnum settingEnum : CfgVirtualNoticeTargetTypeEnum.values()) {
+            if (code.equals(settingEnum.getCode())) {
+                return settingEnum.getName();
+            }
+        }
+        return "";
+    }
+
+    public static CfgVirtualNoticeTargetTypeEnum getEnum(String code) {
+        for (CfgVirtualNoticeTargetTypeEnum settingEnum : CfgVirtualNoticeTargetTypeEnum.values()) {
+            if (code.equals(settingEnum.getCode())) {
+                return settingEnum;
+            }
+        }
+        return null;
+    }
+}
