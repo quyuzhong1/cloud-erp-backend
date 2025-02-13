41e817136be6b20c5978cde47782bbfbce097415
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-02-13 11:34:59 +0800

    渠道增加海外仓面单推送配置

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/LogisticsChannelDTO.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/LogisticsChannelDTO.java
@@ -402,7 +402,10 @@ public class LogisticsChannelDTO implements Serializable {
          * 纸张大小
          */
         private String paperSize;
-
+        /**
+         * 是否推送海外仓面单
+         */
+        private Boolean isPushOverseasWarehouseLabel;
         /**
          * 分拣码
          */
@@ -706,6 +709,11 @@ public class LogisticsChannelDTO implements Serializable {
          */
         @Size(max = 30, message = "分拣码最大长度不能超过10位")
         private String sortingCode;
+
+        /**
+         * 是否推送海外仓面单
+         */
+        private Boolean isPushOverseasWarehouseLabel;
 
 
         /**
