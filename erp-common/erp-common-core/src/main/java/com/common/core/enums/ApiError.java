2328a6245b33f0865a71e93cf1f12f1374f456e9
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-02-17 14:29:31 +0800

    修改文件超限制后提示语

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -115,7 +115,7 @@ public enum ApiError implements Serializable {
     ERROR_NAME_EXIST(1062,"名称【{}】已存在"),
     ERROR_EXCEL_EXPORT_SIZE(1060,"导出数据已超过50000条，请进行条件筛选后导出"),
     ERROR_EXCEL_IMPORT_HEAD_EXIST(1061,"导入表头不能重复"),
-
+    ERROR_EXCEL_IMPORT_SIZE(1063,"导入明细不能超过5000条"),
     /**
      * 警告信息 从800 开始
      */
