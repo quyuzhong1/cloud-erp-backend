2152a8dfc7b28a54faa02d3624c2047d939a7f7f
shukai <shukai@ulanzi.cn>
2025-02-18 12:23:16 +0800

    add:订单

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputShopeeReceiverDoChildDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputShopeeReceiverDoChildDmpHandler.java
@@ -13,9 +13,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.common.core.anno.ParamData;
 import com.common.core.entity.BaseEntity;
 import com.common.core.enums.PannoEnum;
+import com.erp.model.dmp.entity.DmpBasicSystemEntity;
+import com.erp.model.dmp.entity.DmpCfgInputChildEntity;
 import com.erp.model.dmp.entity.DmpCfgInputConvertEntity;
+import com.erp.model.dmp.entity.DmpCfgInputEntity;
 import com.erp.model.dmp.entity.DmpInputTaskEntity;
+import com.erp.model.dmp.enums.DmpInputTaskStatusEnum;
 import com.erp.server.dmp.inout.handler.input.task.mongo.DmpInputMongoHandler;
+import com.erp.server.dmp.inout.utils.DmpHandlerUtils;
 
 import cn.hutool.core.collection.CollUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -31,12 +36,62 @@ import lombok.extern.slf4j.Slf4j;
 public class DmpInputShopeeReceiverDoChildDmpHandler extends DmpInputDoChildDmpHandler{
 	
 	@Override
-	protected List<Map<String, Object>> getDmpInputMongoChildEntityList(List<Map<String, Object>> dmpInputMongoEntityList , String childMongoStorageName){
+	protected void beforeConvertData(List<Map<String, Object>> dmpInputMongoEntityList) {
+		String mongoStatus = DmpInputTaskStatusEnum.MONGO.getCode();
+		String cfgInputId = dmpCfgInputEntity.getId();
+		List<DmpCfgInputChildEntity> dmpCfgInputChildEntityList = dmpCfgInputChildService.lambdaQuery()
+			.eq(DmpCfgInputChildEntity::getParentId, cfgInputId)
+			.eq(DmpCfgInputChildEntity::getInputStatus, mongoStatus)
+			.list();
+		if(CollUtil.isEmpty(dmpCfgInputChildEntityList)) {
+			return;
+		}
+		
+		DmpCfgInputChildEntity dmpCfgInputChildEntity = dmpCfgInputChildEntityList.get(0);
+		if(dmpCfgInputChildEntityList.size() >  1) {
+			List<DmpCfgInputConvertEntity> dmpCfgInputConvertEntityList = dmpHandlerCache.getDmpCfgInputConvertEntityList(d -> d.getMainId().equals(cfgInputId) 
+						&& d.getInputStatus().equals(DmpInputTaskStatusEnum.DMP.getCode())
+						&& d.getOrder() == 1);
+			if(CollUtil.isEmpty(dmpCfgInputConvertEntityList)) {
+				return;
+			}
+			dmpCfgInputChildEntity = dmpCfgInputChildEntityList.stream()
+					.filter(d -> d.getConvertId().equals(dmpCfgInputConvertEntityList.get(0).getId()))
+					.findFirst()
+					.orElse(null);
+			if(dmpCfgInputChildEntity == null) {
+				return;
+			}
+		}
+		String childId = dmpCfgInputChildEntity.getChildId();
+		DmpCfgInputEntity childDmpCfgInputEntity = dmpHandlerCache.getDmpCfgInputEntityList(d -> d.getId().equals(childId)).get(0);
+		DmpBasicSystemEntity childDmpBasicSystemEntity = dmpHandlerCache.getDmpBasicSystemEntityList(d -> d.getId().equals(childDmpCfgInputEntity.getSystemId())).get(0);
+		DmpCfgInputConvertEntity childDmpCfgInputConvertEntity = dmpHandlerCache.getDmpCfgInputConvertEntityList(d -> d.getMainId().equals(childId) 
+				&& d.getInputStatus().equals(mongoStatus)).get(0);
+		String mongoStorageName = DmpHandlerUtils.getMongoStorageName(childDmpBasicSystemEntity, childDmpCfgInputEntity, childDmpCfgInputConvertEntity);
+		
+		List<DmpInputTaskEntity> dmpInputTaskEntityList = dmpInputTaskService.lambdaQuery()
+			.eq(DmpInputTaskEntity::getParentTaskId, inputTaskId)
+			.eq(DmpInputTaskEntity::getCfgInputId, childId)
+			.list();
+		if(CollUtil.isEmpty(dmpInputTaskEntityList)) {
+			return;
+		}
+		
+		DmpInputTaskEntity childDmpInputTaskEntity = dmpInputTaskEntityList.get(0);
 		List<ParamData> paramDataList = new ArrayList<>();
-		List<DmpInputTaskEntity> list = dmpInputTaskService.lambdaQuery().eq(DmpInputTaskEntity::getParentTaskId, inputTaskId).orderByAsc(DmpInputTaskEntity::getCreateTime).list();
-		paramDataList.add(new ParamData(DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, PannoEnum.EQ, list.get(0).getId()));
-		List<Map<String, Object>> dmpInputMongoChildList = mongoService.findMongoData(paramDataList, childMongoStorageName);
-		return this.afterDoDmpInputMongoChildEntityList(dmpInputMongoChildList);
+		paramDataList.add(new ParamData(DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, DmpInputMongoHandler.MONGO_BASE_INPUTTASKID, PannoEnum.EQ, childDmpInputTaskEntity.getId()));
+		
+		List<Map<String, Object>> findMongoData = mongoService.findMongoData(paramDataList, mongoStorageName);
+		if(CollUtil.isNotEmpty(findMongoData)) {
+			dmpInputMongoEntityList.clear();
+			dmpInputMongoEntityList.addAll(findMongoData);
+		}
+	}
+	
+	@Override
+	protected List<Map<String, Object>> getDmpInputMongoChildEntityList(List<Map<String, Object>> dmpInputMongoEntityList , String childMongoStorageName){
+		return this.afterDoDmpInputMongoChildEntityList(dmpInputMongoEntityList);
 	}
 	
 	protected List<Map<String, Object>> afterDoDmpInputMongoChildEntityList(List<Map<String, Object>> dmpInputMongoChildList){
@@ -91,5 +146,10 @@ public class DmpInputShopeeReceiverDoChildDmpHandler extends DmpInputDoChildDmpH
 			String dmpId = billNoIdMap.get(billNo);
 			dmpInputMongoChildEntity.put(MAIN_ID, dmpId);
 		}
+	}
+	
+	@Override
+	protected String getChildMongoStorageName() {
+		return "shopee";
 	}
 }
