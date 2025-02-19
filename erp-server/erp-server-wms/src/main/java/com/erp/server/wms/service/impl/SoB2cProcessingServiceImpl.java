33b6d8efcb4c6cc3759b73b4cb3226fd2e0b91b7
Will <wangwei@ulanzi.cn>
2025-02-19 14:07:32 +0800

    fix,b2c虚拟仓订单直接调拨单冻结库存合计

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cProcessingServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cProcessingServiceImpl.java
@@ -230,14 +230,14 @@ public class SoB2cProcessingServiceImpl extends SuperServiceImpl<SoB2cProcessing
             return;
         }
         //直接调拨单
-        SoB2bProcessingDTO.ResponseDTO transferResponseDTO = transferList.stream().filter(obj ->
+        List<SoB2bProcessingDTO.ResponseDTO> transferResponseDTOList = transferList.stream().filter(obj ->
                         CharSequenceUtil.equals(obj.getSourceId(), entity.getDeliveryId())
                                 && CharSequenceUtil.equals(obj.getApproveStatus(),ApproveStatusEnum.APPROVE.getStatus())
                                 && CharSequenceUtil.equals(obj.getWarehouseId(), entity.getWarehouseId())
                                 &&  CharSequenceUtil.equals(obj.getSourceDetailId(), entity.getDeliveryDetailId()))
-                .findFirst().orElse(null);
-        if (ObjectUtil.isNotEmpty(transferResponseDTO)) {
-            handleOutstock (entity,transferResponseDTO, SourceTypeEnum.TRANSFER_INFO.getCode());
+                .collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(transferResponseDTOList)) {
+            handleOutstock (entity,transferResponseDTOList, SourceTypeEnum.TRANSFER_INFO.getCode());
             return;
         }
         //销售出库单
@@ -247,15 +247,7 @@ public class SoB2cProcessingServiceImpl extends SuperServiceImpl<SoB2cProcessing
                                 &&  CharSequenceUtil.equals(obj.getSourceDetailId(), entity.getDeliveryDetailId()))
                 .collect(Collectors.toList());
         if (CollUtil.isNotEmpty(soOutstockResponseDTOList)) {
-            SoB2bProcessingDTO.ResponseDTO responseDTO = soOutstockResponseDTOList.get(0);
-            Integer totalQty = soOutstockResponseDTOList.stream().map(SoB2bProcessingDTO.ResponseDTO::getQty).reduce(MathUtil.ZERO, MathUtil::add);
-            entity.setOutstockOrderId(responseDTO.getId());
-            entity.setOutstockOrderStatus(responseDTO.getApproveStatus());
-            entity.setOutstockQty(totalQty);
-            entity.setOutstockOrderCode(responseDTO.getCode());
-            entity.setOutstockOrderTime(responseDTO.getApproveTime());
-            entity.setOutstockOrderType(SourceTypeEnum.SO_OUTSTOCK.getCode());
-            entity.setFrozenQty(MathUtil.valueOfZero(entity.getFrozenQty()) - MathUtil.valueOfZero(totalQty));
+            handleOutstock (entity,soOutstockResponseDTOList, SourceTypeEnum.SO_OUTSTOCK.getCode());
         }
     }
 
@@ -265,17 +257,19 @@ public class SoB2cProcessingServiceImpl extends SuperServiceImpl<SoB2cProcessing
      * @author will
      * @date 2024/12/19 20:56
      * @param entity
-     * @param responseDTO
+     * @param responseDTOList
      * @param sourceType
      */
-    private void handleOutstock (SoB2cProcessingEntity entity, SoB2bProcessingDTO.ResponseDTO responseDTO,String sourceType) {
+    private void handleOutstock (SoB2cProcessingEntity entity, List<SoB2bProcessingDTO.ResponseDTO> responseDTOList,String sourceType) {
+        SoB2bProcessingDTO.ResponseDTO responseDTO = responseDTOList.get(0);
+        Integer totalQty = responseDTOList.stream().map(SoB2bProcessingDTO.ResponseDTO::getQty).reduce(MathUtil.ZERO, MathUtil::add);
         entity.setOutstockOrderId(responseDTO.getId());
         entity.setOutstockOrderStatus(responseDTO.getApproveStatus());
-        entity.setOutstockQty(responseDTO.getQty());
+        entity.setOutstockQty(totalQty);
         entity.setOutstockOrderCode(responseDTO.getCode());
         entity.setOutstockOrderTime(responseDTO.getApproveTime());
         entity.setOutstockOrderType(sourceType);
-        entity.setFrozenQty(MathUtil.valueOfZero(entity.getFrozenQty()) - MathUtil.valueOfZero(responseDTO.getQty()));
+        entity.setFrozenQty(MathUtil.valueOfZero(entity.getFrozenQty()) - MathUtil.valueOfZero(totalQty));
     }
 
     /**
