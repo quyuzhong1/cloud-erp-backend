345e8a54b3c8c80be8337ed26666f0e95c3174b2
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-02-19 15:12:13 +0800

    增加面单异常类型获取

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/SoB2cErrorTypeEnum.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/SoB2cErrorTypeEnum.java
@@ -26,6 +26,7 @@ public enum SoB2cErrorTypeEnum {
     GENERATE_TRANSFER_INFO("generateTransferInfo",  "生成直接调拨单"),
     VIRTUAL_FREEZE_QTY("virtualFreezeQty",  "扣减虚拟冻结库存"),
     ORDER_FETCH("orderFetch",  "订单拉取失败"),
+    GET_LOGISTICS_LABEL("getLogisticsLabel",  "获取物流面单异常"),
     ;
     /**
      * 类型
