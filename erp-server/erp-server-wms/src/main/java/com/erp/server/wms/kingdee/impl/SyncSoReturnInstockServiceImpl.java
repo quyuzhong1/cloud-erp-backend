8e63a32f6b71b13fb3c3f0734e74c48bda79886d
luoweigang <821969511@qq.com>
2025-02-19 12:11:12 +0800

    修改推送数帝王销售出库单旺店通不同步问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/kingdee/impl/SyncSoReturnInstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/kingdee/impl/SyncSoReturnInstockServiceImpl.java
@@ -131,7 +131,8 @@ public class SyncSoReturnInstockServiceImpl implements SyncSoReturnInstockServic
         if (ObjectUtil.isNotEmpty(bomChildrenSkuDTO) && BomTypeEnum.COMBINATION.getType().equals(bomChildrenSkuDTO.getType())) {
             shudiyunB2cOrderDTO.setIs_comb(1);
             shudiyunB2cOrderDTO.setSuite_no(bomChildrenSkuDTO.getParentSkuNo());
-            String skuName = parentSkuList.stream().filter(req -> req.getId().equals(bomChildrenSkuDTO.getParentSkuId())).map(ProductDetailEntity::getName).findFirst().orElse("");
+            BomChildrenSkuDTO finalBomChildrenSkuDTO1 = bomChildrenSkuDTO;
+            String skuName = parentSkuList.stream().filter(req -> req.getId().equals(finalBomChildrenSkuDTO1.getParentSkuId())).map(ProductDetailEntity::getName).findFirst().orElse("");
             shudiyunB2cOrderDTO.setSuite_name(skuName);
         } else {
             bomChildrenSkuDTO = bomChildrenSkuDTOS.stream().filter(req -> req.getSkuId().equals(detailEntity.getSkuId())).findFirst().orElse(null);
