80ebdced8c42a6d4296ed4a91387312cb89aecf5
liuruipeng <arthur@ulanzi.cn>
2025-02-20 18:14:25 +0800

    feat:【TikTok物流】拦截问题处理

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -2712,10 +2712,12 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
             return BatchResultDTO.fail(entity.getId(), entity.getCode(), ApiError.ERROR_LOGISTICS_CHANNEL_NOT_EXIST.msg + logisticsEntity.getLogisticsChannelId());
         }
         LogisticsSupplierDTO.AuthDTO auth = logisticsAuthFeign.getAuthByChannelId(logisticsEntity.getLogisticsChannelId());
-
+        //检测是否是API 对接的仓库
+        List<OverseasProviderWarehouseDTO.ViewDTO> overseasWarehouseList = wmsOverseasWarehouseFeign.listByWarehouseIdList(Collections.singletonList(soB2cDetailEntityList.get(0).getWarehouseId()));
+        Boolean isApi = CollectionUtils.isNotEmpty(overseasWarehouseList);
         //三方仓直接调接口，不生成拦截单
-        if (Objects.nonNull(auth) && OmsPlatformEnum.getByCode(auth.getLogisticsPlatform()) != null) {
-            LogisticsPlatformEnum platformEnum = LogisticsPlatformEnum.getByCode(auth.getLogisticsPlatform());
+        if (isApi) {
+            LogisticsPlatformEnum platformEnum = LogisticsPlatformEnum.getByCode(overseasWarehouseList.get(0).getProviderCode());
             //API海外物流拦截
             BatchResultDTO resultDTO = this.overseasProviderIntercept(entity, platformEnum, detailList.get(0).getWarehouseId(), remark);
             return resultDTO;
