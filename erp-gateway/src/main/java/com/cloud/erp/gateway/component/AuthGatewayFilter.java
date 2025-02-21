72ced17835b0563765ba4c63abe4fb70b9628015
Jim <zhoujingbo@ulanzi.cn>
2025-02-21 16:06:36 +0800

    UPDATE:网关调整解析埋点请求参数token

--- erp-gateway/src/main/java/com/cloud/erp/gateway/component/AuthGatewayFilter.java
+++ erp-gateway/src/main/java/com/cloud/erp/gateway/component/AuthGatewayFilter.java
@@ -1,12 +1,14 @@
 package com.cloud.erp.gateway.component;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.cloud.erp.gateway.utils.ServletUtils;
 import com.cloud.erp.gateway.web.server.TokenService;
 import com.common.business.constant.AuthPassPath;
 import com.common.business.constant.TokenConstants;
 import com.common.business.vo.LoginUser;
 import com.common.core.enums.ApiError;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -17,11 +19,13 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import javax.annotation.Resource;
 import java.io.UnsupportedEncodingException;
 import java.lang.annotation.Annotation;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -87,6 +91,12 @@ public class AuthGatewayFilter implements GlobalFilter, Order {
             if (flag) {
                 return chain.filter(exchange);
             }
+            // 埋点路径解析
+            if (AuthPassPath.EVENT_TRACKING_PATH.contains(uri)) {
+                // 解析请求参数token用户
+                return parseRequestBodyToken(exchange, chain, request);
+            }
+
             HttpHeaders headers = request.getHeaders();
             String token = headers.getFirst(TokenConstants.AUTHENTICATION);
             if (StringUtils.isBlank(token)) {
@@ -117,5 +127,35 @@ public class AuthGatewayFilter implements GlobalFilter, Order {
         return ServletUtils.webFluxResponseWriter(exchange.getResponse(), msg, code);
     }
 
-
+    private Mono<Void> parseRequestBodyToken(ServerWebExchange exchange, GatewayFilterChain chain, ServerHttpRequest request) {
+        // 获取 POST 请求体的Token内容
+        return exchange.getRequest().getBody()
+                .collectList()  // 收集所有的数据块
+                .publishOn(Schedulers.boundedElastic())  // 收集所有的数据块
+                .flatMap(dataBuffers -> {
+                    // 将请求体内容合并为一个字符串
+                    String body = dataBuffers.stream()
+                            .map(dataBuffer -> dataBuffer.toString(StandardCharsets.UTF_8))
+                            .reduce((s1, s2) -> s1 + s2)
+                            .orElse("");
+                    try {
+                        // 解析 JSON 获取 token 字段
+                        JSONObject jsonObject = JSON.parseObject(body);
+                        String token = jsonObject.getOrDefault("token", "").toString();
+                        if (StringUtils.isNotBlank(token)) {
+                            //解析token
+                            LoginUser loginUser = tokenService.getLoginUser(token);
+                            if (Objects.isNull(loginUser)) {
+                                return unauthorizedResponse(exchange, ApiError.ERROR_403.msg, ApiError.ERROR_403.code);
+                            }
+                            loginUser.setAccessToken(token);
+                            request.mutate().header("tokenUserInfo", URLEncoder.encode(JSON.toJSONString(loginUser), "UTF-8")).build();
+                        }
+                    } catch (Exception e) {
+                        return Mono.error(e); // 处理解析异常
+                    }
+                    // 继续处理过滤链
+                    return chain.filter(exchange);
+                });
+    }
 }
