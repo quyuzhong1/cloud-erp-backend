907da47f621b5bf5b3840a36e529334d476ade76
jack chen <jackchen@ulanzi.cn>
2025-02-21 15:34:27 +0800

    2025-02-21 sonar代码规范检查 优化可靠性问题

--- erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/aspect/SysLoggingAspect.java
+++ erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/aspect/SysLoggingAspect.java
@@ -310,13 +310,17 @@ public class SysLoggingAspect {
         } else if (Collection.class.isAssignableFrom(clazz)) {
             Collection collection = (Collection) o;
             for (Object value : collection) {
-                return value instanceof MultipartFile;
+                if(value instanceof MultipartFile){
+                    return true;
+                }
             }
         } else if (Map.class.isAssignableFrom(clazz)) {
             Map map = (Map) o;
             for (Object value : map.entrySet()) {
                 Map.Entry entry = (Map.Entry) value;
-                return entry.getValue() instanceof MultipartFile;
+                if(entry.getValue() instanceof MultipartFile){
+                    return true;
+                }
             }
         }
         return o instanceof MultipartFile || o instanceof HttpServletRequest || o instanceof HttpServletResponse
