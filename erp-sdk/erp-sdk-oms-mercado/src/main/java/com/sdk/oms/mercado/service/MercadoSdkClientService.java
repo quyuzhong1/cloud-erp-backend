b73a61c18d9d22b95d269538a7f504e7b4dc2501
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-02-25 18:29:40 +0800

    销售订单撤销流程修改

--- erp-sdk/erp-sdk-oms-mercado/src/main/java/com/sdk/oms/mercado/service/MercadoSdkClientService.java
+++ erp-sdk/erp-sdk-oms-mercado/src/main/java/com/sdk/oms/mercado/service/MercadoSdkClientService.java
@@ -58,62 +58,62 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class MercadoSdkClientService {
-    public static void main(String[] args) {
-
-        String orderUrl = "https://api.mercadolibre.com/marketplace/orders/2000006213527517";
-
-
-        //组装授权url
-        String clientId = "3457166802805723";
-        String clientSecret = "QucvI4VWHO0w3AZftOElz5liVOurfjQG";
-        String url = "https://api.mercadolibre.com/marketplace/orders/2000010507016480";
-
-
-        //入参
-        HashMap<String, Object> orderParams = new HashMap<>(1);
-
-        //设置请求头
-        Map<String, String> orderHeaderMap = new HashMap<>(1);
-        orderHeaderMap.put("Authorization", "Bearer " + "APP_USR-3457166802805723-012018-7d948085801db7d1625f3859c50f9d20-2201503196");
-//			https://api.mercadolibre.com/marketplace/orders/2000007633674134
-        //拉取数据
-        ApiResult apiResult = new ApiResult();
-        Object data = null;
-        long sleepTime = 1000;
-        int count = 0;
-        while(ObjectUtil.isEmpty(data)) {
-            apiResult = HttpCommonUtil.sendOkHttpApiResult(url, JSONUtil.toJsonStr(orderParams), null, orderHeaderMap, RequestMethod.GET);
-            if(apiResult.getMsg().equalsIgnoreCase("Read timed out")) {
-                if(count == 10) {
-                    throw new ServiceException("调用美客多" + url + "接口重试" + count + "失败");
-                }
-                try {
-                    Thread.sleep(sleepTime);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                sleepTime = sleepTime + 1000;
-                count = count + 1;
-            }
-            data = apiResult.getData();
-        }
-
-        if (!Objects.equals(apiResult.getCode(), 200) && !Objects.equals(apiResult.getCode(), 201)) {
-            System.out.println("errrrrrrrrrrrr");
-        }
-
-        //解析数据
-        com.sdk.oms.mercado.dto.mercado.order.OrderViewDTO orderViewDTO = null;
-        ObjectMapper objectMapperBase = new ObjectMapper();
-        try {
-            orderViewDTO = objectMapperBase.readValue(JSONUtil.toJsonStr(apiResult.getData()), OrderViewDTO.class);
-        } catch (JsonProcessingException e) {
-            log.error("调用url={},入参params={}, 数据解析失败，返回值 responseMap={}", url, orderParams.toString(), JSONUtil.toJsonStr(apiResult.getData()));
-            throw new RuntimeException(StrUtil.format("调用url={},入参params={}, 数据解析失败，返回值 responseMap={}",
-                    url, orderParams.toString(), JSONUtil.toJsonStr(apiResult.getData())));
-        }
-        System.out.println(orderViewDTO);
-    }
+//    public static void main(String[] args) {
+//
+//        String orderUrl = "https://api.mercadolibre.com/marketplace/orders/2000006213527517";
+//
+//
+//        //组装授权url
+//        String clientId = "3457166802805723";
+//        String clientSecret = "QucvI4VWHO0w3AZftOElz5liVOurfjQG";
+//        String url = "https://api.mercadolibre.com/marketplace/orders/2000010507016480";
+//
+//
+//        //入参
+//        HashMap<String, Object> orderParams = new HashMap<>(1);
+//
+//        //设置请求头
+//        Map<String, String> orderHeaderMap = new HashMap<>(1);
+//        orderHeaderMap.put("Authorization", "Bearer " + "APP_USR-3457166802805723-012018-7d948085801db7d1625f3859c50f9d20-2201503196");
+////			https://api.mercadolibre.com/marketplace/orders/2000007633674134
+//        //拉取数据
+//        ApiResult apiResult = new ApiResult();
+//        Object data = null;
+//        long sleepTime = 1000;
+//        int count = 0;
+//        while(ObjectUtil.isEmpty(data)) {
+//            apiResult = HttpCommonUtil.sendOkHttpApiResult(url, JSONUtil.toJsonStr(orderParams), null, orderHeaderMap, RequestMethod.GET);
+//            if(apiResult.getMsg().equalsIgnoreCase("Read timed out")) {
+//                if(count == 10) {
+//                    throw new ServiceException("调用美客多" + url + "接口重试" + count + "失败");
+//                }
+//                try {
+//                    Thread.sleep(sleepTime);
+//                } catch (InterruptedException e) {
+//                    Thread.currentThread().interrupt();
+//                }
+//                sleepTime = sleepTime + 1000;
+//                count = count + 1;
+//            }
+//            data = apiResult.getData();
+//        }
+//
+//        if (!Objects.equals(apiResult.getCode(), 200) && !Objects.equals(apiResult.getCode(), 201)) {
+//            System.out.println("errrrrrrrrrrrr");
+//        }
+//
+//        //解析数据
+//        com.sdk.oms.mercado.dto.mercado.order.OrderViewDTO orderViewDTO = null;
+//        ObjectMapper objectMapperBase = new ObjectMapper();
+//        try {
+//            orderViewDTO = objectMapperBase.readValue(JSONUtil.toJsonStr(apiResult.getData()), OrderViewDTO.class);
+//        } catch (JsonProcessingException e) {
+//            log.error("调用url={},入参params={}, 数据解析失败，返回值 responseMap={}", url, orderParams.toString(), JSONUtil.toJsonStr(apiResult.getData()));
+//            throw new RuntimeException(StrUtil.format("调用url={},入参params={}, 数据解析失败，返回值 responseMap={}",
+//                    url, orderParams.toString(), JSONUtil.toJsonStr(apiResult.getData())));
+//        }
+//        System.out.println(orderViewDTO);
+//    }
 
 
     private static RedisUtil redisUtil;
