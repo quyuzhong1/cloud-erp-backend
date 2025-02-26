41ce31e153f32fedc87fc53f1f2d9b5bc80464c5
jack chen <jackchen@ulanzi.cn>
2025-02-26 14:15:06 +0800

    feat: 新增spt仓库类型字段及枚举

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/OverseasProviderWarehouseEntity.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/OverseasProviderWarehouseEntity.java
@@ -1,12 +1,13 @@
 package com.erp.model.wms.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.common.core.entity.BaseEntity;
-import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 
 /**
@@ -28,6 +29,11 @@ public class OverseasProviderWarehouseEntity extends BaseEntity<OverseasProvider
     */
     @TableField("main_id")
     private String mainId;
+    /**
+     * 仓库类型 0标准 1中转 2虚拟
+     */
+    @TableField("platform_warehouse_type")
+    private String platformWarehouseType;
     /**
     * 仓库编码
     */
@@ -71,6 +77,8 @@ public class OverseasProviderWarehouseEntity extends BaseEntity<OverseasProvider
 
 
     public static final String MAIN_ID = "main_id";
+
+    public static final String PLATFORM_WAREHOUSE_TYPE = "platform_warehouse_type";
 
     public static final String PLATFORM_WAREHOUSE_CODE = "platform_warehouse_code";
 
