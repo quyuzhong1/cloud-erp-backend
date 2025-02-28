0a8639f48c146c94c3063290b2ad14e1b659cfbb
jack chen <jackchen@ulanzi.cn>
2025-02-28 09:53:40 +0800

    feat(ui): 新增对象复制异常处理和非空字段复制功能

--- erp-common/erp-common-core/src/main/java/com/common/core/utils/BeanMapper.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/utils/BeanMapper.java
@@ -3,8 +3,12 @@ package com.common.core.utils;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Classname BeanMapper
@@ -34,5 +38,36 @@ public class BeanMapper {
         return destinationList;
     }
 
+    public static <S, T> T copyNonNull(S source, T target) throws IllegalAccessException {
+        if (source == null || target == null) {
+            throw new IllegalArgumentException("Source and target objects must not be null");
+        }
+
+        Field[] sourceFields = source.getClass().getDeclaredFields();
+        Field[] targetFields = target.getClass().getDeclaredFields();
+
+        Map<String, Field> targetFieldMap = new HashMap<>();
+        for (Field field : targetFields) {
+            field.setAccessible(true);
+            targetFieldMap.put(field.getName(), field);
+        }
+
+        for (Field sourceField : sourceFields) {
+            sourceField.setAccessible(true);
+            Object value = sourceField.get(source);
+            if (value != null) {
+                Field targetField = targetFieldMap.get(sourceField.getName());
+                if (targetField != null) {
+                    int mod = targetField.getModifiers();
+                    if (!Modifier.isStatic(mod) && !Modifier.isFinal(mod)) {
+                        targetField.setAccessible(true);
+                        targetField.set(target, value);
+                    }
+                }
+            }
+        }
+
+        return target;
+    }
 
 }
