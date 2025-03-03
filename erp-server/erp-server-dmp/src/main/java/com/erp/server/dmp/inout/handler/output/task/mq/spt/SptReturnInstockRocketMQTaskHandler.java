c515c1709e060acea21fc91fcac95f83af9356ed
jack chen <jackchen@ulanzi.cn>
2025-03-03 09:41:33 +0800

    feat(eccang): 新增Eccang的RocketMQ任务处理器,并且SPT继承Eccang

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/spt/SptReturnInstockRocketMQTaskHandler.java
@@ -0,0 +1,13 @@
+package com.erp.server.dmp.inout.handler.output.task.mq.spt;
+
+import com.erp.server.dmp.inout.handler.output.task.mq.eccang.EccangReturnInstockRocketMQTaskHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@Scope("prototype")
+public class SptReturnInstockRocketMQTaskHandler extends EccangReturnInstockRocketMQTaskHandler {
+
+}
