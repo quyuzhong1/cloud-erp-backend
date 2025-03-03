640ea79c9d27629ed0883937dc5980ca91cb2abe
Will <wangwei@ulanzi.cn>
2025-03-03 10:41:35 +0800

    feat,代码还原

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/spt/SptRegionRocketMQTaskHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/spt/SptRegionRocketMQTaskHandler.java
@@ -1,19 +1,8 @@
 package com.erp.server.dmp.inout.handler.output.task.mq.spt;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.collection.CollUtil;
-import com.alibaba.fastjson.JSON;
-import com.common.business.dto.PlatformCityDictDTO;
-import com.common.core.entity.BaseEntity;
-import com.erp.model.dmp.entity.DmpCfgInputConvertEntity;
-import com.erp.model.dmp.entity.DmpThirdRegionEntity;
-import com.erp.server.dmp.inout.dto.request.DmpOutputTaskRequest;
-import com.erp.server.dmp.inout.dto.response.DmpOutputTaskResponse;
 import com.erp.server.dmp.inout.handler.output.task.mq.eccang.EccangRegionRocketMQTaskHandler;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import java.util.*;
 
 @Service
 @Scope("prototype")
