5ab1463e53ca8698b7a7dc3c32534a2d7b58df25
Jim <zhoujingbo@ulanzi.cn>
2025-03-04 18:53:31 +0800

    UPDATE:添加日志

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/api/SysEventTrackingController.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/api/SysEventTrackingController.java
@@ -43,6 +43,7 @@ public class SysEventTrackingController extends BaseController {
     */
     @PostMapping(value = "/add", consumes = MediaType.TEXT_PLAIN_VALUE)
     public ApiResult<BaseResultDTO.AddDTO> add(@RequestBody String data, HttpServletRequest request) {
+        log.info("前端埋点事件记录:{}", data);
         SysEventTrackingDTO.AddDTO addDTO = JSONUtil.toBean(JSONUtil.toJsonStr(data), SysEventTrackingDTO.AddDTO.class);
         return success(sysEventTrackingService.add(addDTO));
     }
