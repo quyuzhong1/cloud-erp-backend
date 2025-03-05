b11d87e8e9fb15f99507cd1968517f538bc492a5
jack chen <jackchen@ulanzi.cn>
2025-03-05 17:41:29 +0800

    refactor: 抽象化安兔和速派通的init处理器，引入易仓支持

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/antu/AntuWarehouseInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/antu/AntuWarehouseInitHandler.java
@@ -1,34 +1,10 @@
 package com.erp.server.dmp.inout.handler.input.task.init.api.antu;
 
-import cn.hutool.core.collection.CollUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import com.common.business.enums.OmsPlatformEnum;
-import com.common.business.threadlocal.ThirdWarehouseContext;
-import com.common.core.exception.ServiceException;
-import com.erp.model.dmp.entity.DmpCfgApiEntity;
 import com.erp.model.dmp.enums.DmpBasicSystemCodeEnum;
-import com.erp.model.wms.entity.OverseasProviderEntity;
-import com.erp.server.dmp.inout.dto.base.DmpInputTaskInitDTO;
-import com.erp.server.dmp.inout.dto.request.DmpInputInitRequest;
-import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
-import com.erp.server.dmp.inout.handler.input.task.init.DmpInputInitHandler;
-import com.erp.server.dmp.inout.utils.DmpHandlerCache;
-import com.sdk.wms.antu.dto.request.AntuGetProductReq;
-import com.sdk.wms.antu.dto.response.AntuResponse;
-import com.sdk.wms.antu.dto.response.AntuWarehouseResp;
-import com.sdk.wms.antu.utils.AntuUtils;
+import com.erp.server.dmp.inout.handler.input.task.init.api.eccang.EccangWarehouseInitHandler;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 /**
@@ -36,84 +12,14 @@ import java.util.stream.Collectors;
  */
 @Service
 @Scope("prototype")
-public class AntuWarehouseInitHandler extends DmpInputInitHandler {
-
-	@Resource
-    private DmpHandlerCache dmpHandlerCache;
-	
+public class AntuWarehouseInitHandler extends EccangWarehouseInitHandler {
 	@Override
-	public List<DmpInputTaskInitDTO> getInitData(DmpInputInitRequest dmpRequest, DmpInputTaskResponse dmpResponse) {
-		List<DmpInputTaskInitDTO> resultList = new ArrayList<>();
-		String typeId = dmpCfgInputEntity.getTypeId();
-		DmpCfgApiEntity dmpCfgApiEntity = dmpCfgApiService.getById(typeId);
-		String apiType = dmpCfgApiEntity.getApiType();
-
-		AntuGetProductReq antuGetProductReq = new AntuGetProductReq();
-		Integer page = 1;
-		antuGetProductReq.setPageSize(100);
-		int currTotal = 0;
-		List<Object> allResult = new ArrayList<>();
-		List<OverseasProviderEntity> overseasProviderEntityList = dmpHandlerCache.getOverseasProviderEntityList(d -> d.getCode().equals(DmpBasicSystemCodeEnum.ANTU.getCode()));
-		if(CollUtil.isEmpty(overseasProviderEntityList)) {
-			return Collections.emptyList();
-		}
-		// 取对应授权ID授权
-		OverseasProviderEntity overseasProviderEntity = overseasProviderEntityList.stream()
-				.filter(e -> e.getId().equalsIgnoreCase(dmpInputTaskEntity.getNextLevelId()))
-				.findFirst()
-				.orElse(null);
-		if(null == overseasProviderEntity) {
-			throw new ServiceException("安兔对应授权ID信息不存在");
-		}
-
-			if (overseasProviderEntity.getEnableDate().isAfter(LocalDate.now())) {
-				return Collections.emptyList();
-			}
-			ThirdWarehouseContext.setAuthMap(overseasProviderEntity.getAuthJson());
-			while(true) {
-				antuGetProductReq.setPage(page);
-				String apiResponse = AntuUtils.callService(getPlatForm(),apiType, antuGetProductReq);
-				AntuResponse<List<AntuWarehouseResp>> response = JSONObject.parseObject(apiResponse,new TypeReference<AntuResponse<List<AntuWarehouseResp>>>() {}.getType());
-
-				List<AntuWarehouseResp> data = response.getData();
-				int size = data.size();
-				if(size == 0) {
-					break;
-				}
-
-				if ("warehouse".equalsIgnoreCase(dmpResponse.getDmpCfgInputEntity().getCode())) {
-					List<AntuWarehouseResp> collect = data.stream().filter(v -> "0".equals(v.getWarehouseType())).collect(Collectors.toList());
-					allResult.addAll(collect);
-				} else {
-					List<AntuWarehouseResp> collect = data.stream().filter(v -> "1".equals(v.getWarehouseType())).collect(Collectors.toList());
-					allResult.addAll(collect);
-				}
-
-				currTotal = currTotal + size;
-				Integer count = response.getCount();
-				if(count == null) {
-					count  = 0;
-				}
-				if(currTotal >= count) {
-					break;
-				}
-				page = page + 1;
-			}
-			String id = overseasProviderEntity.getId();
-			DmpInputTaskInitDTO dmpInputTaskInitDTO = new DmpInputTaskInitDTO();
-			JSONArray parseArray = JSON.parseArray(JSONObject.toJSONString(allResult));
-			parseArray.forEach(p -> {
-				JSONObject j = (JSONObject)p;
-				j.put("authId", id);
-			});
-			dmpInputTaskInitDTO.setMsg(parseArray.toJSONString());
-			resultList.add(dmpInputTaskInitDTO);
-		return resultList;
-	}
-
-	private static OmsPlatformEnum getPlatForm() {
+	public OmsPlatformEnum getPlatForm() {
 		return OmsPlatformEnum.OMS_ANTU;
 	}
 
-
+	@Override
+	public DmpBasicSystemCodeEnum getDmpBasicSystemCodeEnum() {
+		return DmpBasicSystemCodeEnum.ANTU;
+	}
 }
--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/spt/SptOutboundInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/spt/SptOutboundInitHandler.java
@@ -1,98 +1,29 @@
 package com.erp.server.dmp.inout.handler.input.task.init.api.spt;
 
-import cn.hutool.core.collection.CollUtil;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.TypeReference;
 import com.common.business.enums.OmsPlatformEnum;
-import com.common.business.threadlocal.ThirdWarehouseContext;
-import com.common.core.exception.ServiceException;
-import com.erp.model.dmp.entity.DmpCfgApiEntity;
 import com.erp.model.dmp.enums.DmpBasicSystemCodeEnum;
-import com.erp.model.wms.entity.OverseasProviderEntity;
-import com.erp.server.dmp.inout.dto.base.DmpInputTaskInitDTO;
-import com.erp.server.dmp.inout.dto.request.DmpInputInitRequest;
-import com.erp.server.dmp.inout.dto.response.DmpInputTaskResponse;
-import com.erp.server.dmp.inout.handler.input.task.init.DmpInputInitHandler;
-import com.sdk.wms.antu.dto.request.AntuGetOutboundReq;
-import com.sdk.wms.antu.dto.response.AntuResponse;
-import com.sdk.wms.antu.utils.AntuUtils;
+import com.erp.server.dmp.inout.handler.input.task.init.api.eccang.EccangOutboundInitHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * dmp输入init任务基础处理器下的速派通api获取数据方式
  * @author Administrator
  *
  */
+@Slf4j
 @Service
 @Scope("prototype")
-public class SptOutboundInitHandler extends DmpInputInitHandler {
+public class SptOutboundInitHandler extends EccangOutboundInitHandler {
 
 	@Override
-	public List<DmpInputTaskInitDTO> getInitData(DmpInputInitRequest dmpRequest, DmpInputTaskResponse dmpResponse) {
-		String typeId = dmpCfgInputEntity.getTypeId();
-		DmpCfgApiEntity dmpCfgApiEntity = dmpCfgApiService.getById(typeId);
-		String apiType = dmpCfgApiEntity.getApiType();
-
-		//查询数据
-		AntuGetOutboundReq antuGetOutboundReq = AntuGetOutboundReq.builder()
-				.modifyDateFrom(dmpInputTaskEntity.getStartTime())
-				.modifyDateTo(dmpInputTaskEntity.getEndTime())
-				.pageSize(100)
-//				.orderStatus(AntuEnums.OrderStatusEnum.INITIAL_RECEIVING.getCode())
-				.build();
-		Integer page = 1;
-		int currTotal = 0;
-		List<Object> allResult = new ArrayList<>();
-		List<OverseasProviderEntity> overseasProviderEntityList = dmpHandlerCache.getOverseasProviderEntityList(d -> d.getCode().equals(DmpBasicSystemCodeEnum.SPT.getCode()));
-		if(CollUtil.isEmpty(overseasProviderEntityList)) {
-			return Collections.emptyList();
-		}
-		// 取对应授权ID授权
-		OverseasProviderEntity overseasProviderEntity = overseasProviderEntityList.stream()
-				.filter(e -> e.getId().equalsIgnoreCase(dmpInputTaskEntity.getNextLevelId()))
-				.findFirst()
-				.orElse(null);
-		if(null == overseasProviderEntity) {
-			throw new ServiceException("速派通对应授权ID信息不存在");
-		}
-		if (overseasProviderEntity.getEnableDate().isAfter(LocalDate.now())) {
-			return Collections.emptyList();
-		}
-
-		ThirdWarehouseContext.setAuthMap(overseasProviderEntityList.get(0).getAuthJson());
-		while(true) {
-			antuGetOutboundReq.setPage(page);
-			String response = AntuUtils.callService(getPlatForm(),apiType,antuGetOutboundReq);
-			AntuResponse<List<?>> result = JSONObject.parseObject(response,new TypeReference<AntuResponse<List<Object>>>() {}.getType());
-			List<?> data = result.getData();
-			int size = data.size();
-			if(size == 0) {
-				break;
-			}
-			allResult.addAll(data);
-			currTotal = currTotal + size;
-			Integer count = result.getCount();
-			if(count == null) {
-				count  = 0;
-			}
-			if(currTotal >= count) {
-				break;
-			}
-			page = page + 1;
-		}
-		DmpInputTaskInitDTO dmpInputTaskInitDTO = new DmpInputTaskInitDTO();
-		dmpInputTaskInitDTO.setMsg(JSONObject.toJSONString(allResult));
-		return Collections.singletonList(dmpInputTaskInitDTO);
-	}
-
-	private static OmsPlatformEnum getPlatForm() {
+	public OmsPlatformEnum getPlatForm() {
 		return OmsPlatformEnum.OMS_SPT;
 	}
 
+	@Override
+	public DmpBasicSystemCodeEnum getDmpBasicSystemCodeEnum() {
+		return DmpBasicSystemCodeEnum.SPT;
+	}
 }
