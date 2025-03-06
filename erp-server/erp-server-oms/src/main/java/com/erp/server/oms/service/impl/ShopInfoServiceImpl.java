cfe63ec9ca0439e167e3f9b5b238d78214260981
liuruipeng <arthur@ulanzi.cn>
2025-03-06 16:32:59 +0800

    全球枚举

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/ShopInfoServiceImpl.java
@@ -256,7 +256,7 @@ public class ShopInfoServiceImpl extends SuperServiceImpl<ShopInfoMapper, ShopIn
 //        }
 
         if (StringUtils.isBlank(countryId)) {
-            countryId = DictValueEnum.GL.getCode();
+            countryId = DictValueEnum.ALL.getCode();
         }
         customer.setName(shop.getName());
         customer.setCountryId(countryId);
