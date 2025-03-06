5a4cc2841e52d3e9958a227da8758818c1afd067
jack chen <jackchen@ulanzi.cn>
2025-03-06 18:33:52 +0800

    fix: 修改订单数据解析逻辑并使用Eccang相关处理类

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/eccang/EccangInventoryRocketMQTaskHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/mq/eccang/EccangInventoryRocketMQTaskHandler.java
@@ -15,6 +15,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -101,7 +102,8 @@ public class EccangInventoryRocketMQTaskHandler extends DmpOutputRocketMQTaskHan
         // 库龄信息
         List<PlatformInventoryDTO.PlatformInventoryAgeDTO> ageList = new LinkedList<>();
         if (CollectionUtils.isNotEmpty(dmpDetailList)){
-            ageList = dmpDetailList.stream().map(this::convertAgeInfo).collect(Collectors.toList());
+            // 按日期分组合并
+            ageList = convertAndGroupByPutAwayData(dmpDetailList);
         }
         platformInventoryDTO.setAgeInfoList(ageList);
         return platformInventoryDTO;
@@ -109,13 +111,21 @@ public class EccangInventoryRocketMQTaskHandler extends DmpOutputRocketMQTaskHan
 
     /**
      * 明细转换
+     * 按日期合并
      */
-    private PlatformInventoryDTO.PlatformInventoryAgeDTO  convertAgeInfo(DmpThirdInventoryAgeEntity ageEntity) {
-        PlatformInventoryDTO.PlatformInventoryAgeDTO platformInventoryAgeDTO = new PlatformInventoryDTO.PlatformInventoryAgeDTO();
-        platformInventoryAgeDTO.setInventoryQty(ageEntity.getInventoryQty());
-        platformInventoryAgeDTO.setPullDate(ageEntity.getUpdateTime().toLocalDate());
-        platformInventoryAgeDTO.setPutAwayDate(ageEntity.getPutAwayDate());
-        return platformInventoryAgeDTO;
+    private List<PlatformInventoryDTO.PlatformInventoryAgeDTO> convertAndGroupByPutAwayData(List<DmpThirdInventoryAgeEntity> ageList) {
+        Map<LocalDate, List<DmpThirdInventoryAgeEntity>> groupList = ageList.stream()
+                .collect(Collectors.groupingBy(DmpThirdInventoryAgeEntity::getPutAwayDate));
+
+        return groupList.entrySet().stream().map(entry -> {
+            List<DmpThirdInventoryAgeEntity> value = entry.getValue();
+            int allQty = value.stream().mapToInt(DmpThirdInventoryAgeEntity::getInventoryQty).sum();
+            PlatformInventoryDTO.PlatformInventoryAgeDTO platformInventoryAgeDTO = new PlatformInventoryDTO.PlatformInventoryAgeDTO();
+            platformInventoryAgeDTO.setInventoryQty(allQty);
+            platformInventoryAgeDTO.setPullDate(value.get(0).getUpdateTime().toLocalDate());
+            platformInventoryAgeDTO.setPutAwayDate(entry.getKey());
+            return platformInventoryAgeDTO;
+        }).collect(Collectors.toList());
     }
 
     @Override
