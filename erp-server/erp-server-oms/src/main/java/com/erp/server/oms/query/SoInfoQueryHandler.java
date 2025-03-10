2c5547897b945898b14154fba80e1547297f7a30
liuruipeng <arthur@ulanzi.cn>
2025-03-10 18:32:47 +0800

    B2B缺货查询

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/SoInfoQueryHandler.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/query/SoInfoQueryHandler.java
@@ -66,13 +66,21 @@ public class SoInfoQueryHandler extends AbstractQueryHandler {
         /**
          * 虚拟仓是否缺货
          */
-        if("isVirtualScarce".equals(field) || "isVirtualOutStock".equals(field)){
-            String sql = "COALESCE(sdnd.deliveryQty,0) - COALESCE(vi.virtualQty,0)";
-            if ((Boolean) value) {
-                return sql + "< 0";
-            } else {
-                return sql + ">= 0";
-            }
+        if("isVirtualScarce".equals(field)){
+            return getQueryAllSql();
+        }
+
+        /**
+         * 虚拟仓是否缺货
+         */
+        if("isVirtualOutStock".equals(field)){
+//            String sql = "COALESCE(sdnd.deliveryQty,0) - COALESCE(vi.virtualQty,0)";
+//            if ((Boolean) value) {
+//                return sql + "< 0";
+//            } else {
+//                return sql + ">= 0";
+//            }
+            return getQueryAllSql();
         }
 
         if("tab".equals(field)){
