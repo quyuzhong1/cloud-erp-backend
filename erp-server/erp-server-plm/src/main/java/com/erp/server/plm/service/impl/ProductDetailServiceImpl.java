b975de0310e973f47970d753a7d627fd543c9220
jack chen <jackchen@ulanzi.cn>
2025-03-11 11:10:39 +0800

    fix(ui): 修正产品款名和产品品名关系处理逻辑

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -1046,7 +1046,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
         }
 
         //产品款名和产品品名关系处理
-        handleProductNames(productSkuBaseInfoDTO,Boolean.TRUE);
+        handleProductNames(productSkuBaseInfoDTO);
 
         //SKU操作日志-产品信息
         ProductInfoEntity productInfoEntity = productInfoService.getById(productSpuBaseInfoDTO.getId());
