3feee4802c3634c55d6397491d0707a1e0fad8e0
Will <wangwei@ulanzi.cn>
2025-03-11 10:20:27 +0800

    feat,代码合并调整

--- erp-server/erp-server-msg/src/main/java/com/erp/server/msg/service/impl/FeishuSendServiceImpl.java
+++ erp-server/erp-server-msg/src/main/java/com/erp/server/msg/service/impl/FeishuSendServiceImpl.java
@@ -3,10 +3,10 @@ package com.erp.server.msg.service.impl;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.LocalDateTimeUtil;
 import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.spring.SpringUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson.JSON;
-import com.common.business.dto.FindUserDTO;
 import com.common.business.enums.ErpServerModuleEnum;
 import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
@@ -114,17 +114,11 @@ public class FeishuSendServiceImpl extends BaseMessageSendService {
             log.error("飞书接收人为空，本次不发生消息");
             return null;
         }
-        List<String> receiverUserIds = noticeMsgWrapInfoDTO.getReceiverUserIds();
-        //清除禁用、删除人员
-        List<FindUserDTO> userList = sysUserFeign.getUserList();
-        List<String> userIdList = userList.stream().map(FindUserDTO::getUserId).filter(receiverUserIds::contains).distinct().collect(Collectors.toList());
-        if(CollUtil.isEmpty(userIdList)) {
-            log.error("存在禁用人员，飞书接收人为空，本次不发生消息");
-            return null;
-        }
+
         MsgResultVO<T> msgResult;
-        noticeMsgWrapInfoDTO.setReceiverUserIds(userIdList);
-        boolean isBatch = userIdList.size() >  1;
+        List<String> receiverUserIds = noticeMsgWrapInfoDTO.getReceiverUserIds();
+        noticeMsgWrapInfoDTO.setReceiverUserIds(receiverUserIds.stream().distinct().collect(Collectors.toList()));
+        Boolean isBatch = noticeMsgWrapInfoDTO.getReceiverUserIds().size() >  1;
         if(!isBatch) { // 单条消息
             msgResult = sendSingleMsg(noticeMsgInfo);
         } else { // 批量消息
