1a8166e60cbb25e4a8e19513dabe308d129a1d63
jack chen <jackchen@ulanzi.cn>
2025-03-13 10:27:52 +0800

    feat: 优化消息通知的列表查询，下拉值查询

--- erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/NoticeDTO.java
+++ erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/NoticeDTO.java
@@ -160,9 +160,14 @@ public class NoticeDTO implements Serializable {
     @NoArgsConstructor
     public static class PagingParamDTO extends SortDTO {
         /**
+         * 系统模块
+         */
+        @NotEmpty(message = "系统模块不能为空")
+        private List<String> system;
+        /**
          * 业务模块
          */
-        @NotBlank(message = "业务模块不能为空")
+//        @NotBlank(message = "业务模块不能为空")
         private String module;
 
         /**
