31c0d46be4503555de33c3480c06630774a2cd26
jack chen <jackchen@ulanzi.cn>
2025-03-17 11:31:30 +0800

    feat: 增加产品详情变更数据处理

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/NoticeMessageServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/NoticeMessageServiceImpl.java
@@ -191,6 +191,9 @@ public class NoticeMessageServiceImpl extends ServiceImpl<NoticeMessageMapper, N
         if (CollectionUtils.isEmpty(otherPeopleList) && CollectionUtils.isEmpty(itemPeopleList)) {
             throw new ServiceException(ApiError.ERROR_95055);
         }
+        if(Objects.nonNull(dto.getProductDetailChangeDTO())){
+            messageEntity.setDataJson(JSONUtil.toJsonStr(dto.getProductDetailChangeDTO()));
+        }
         boolean flag = this.save(messageEntity);
         if (flag) {
             //更改节点
@@ -234,6 +237,9 @@ public class NoticeMessageServiceImpl extends ServiceImpl<NoticeMessageMapper, N
         }
         if (CollectionUtils.isEmpty(otherPeopleList) && CollectionUtils.isEmpty(itemPeopleList)) {
             throw new ServiceException(ApiError.ERROR_95055);
+        }
+        if(Objects.nonNull(dto.getProductDetailChangeDTO())){
+            messageEntity.setDataJson(JSONUtil.toJsonStr(dto.getProductDetailChangeDTO()));
         }
         messageEntity.setId(dto.getId());
         return this.updateById(messageEntity);
