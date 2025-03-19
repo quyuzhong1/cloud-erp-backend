4d092a0e7555b87d5a1c3199b164801417e07ed6
luoweigang <821969511@qq.com>
2025-03-19 14:03:17 +0800

    修复美客多本土授权2

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ShopAuthorizeDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ShopAuthorizeDTO.java
@@ -114,15 +114,15 @@ public class ShopAuthorizeDTO implements Serializable {
             return this;
         }
 
-        // 美客多
-        if (StringUtils.isNotBlank(this.state) && StringUtils.isNotBlank(this.code) && StrUtil.startWith(this.state, PlatformDictEnum.MERCADOLIBRE.getCode())){
-            this.setPlatformCode(PlatformDictEnum.MERCADOLIBRE.getCode());
-            return this;
-        }
-
         // 美客多-本土站
         if (StringUtils.isNotBlank(this.state) && StringUtils.isNotBlank(this.code) && StrUtil.startWith(this.state, PlatformDictEnum.MERCADOLIBRE_LOCAL.getCode())){
             this.setPlatformCode(PlatformDictEnum.MERCADOLIBRE_LOCAL.getCode());
+            return this;
+        }
+
+        // 美客多
+        if (StringUtils.isNotBlank(this.state) && StringUtils.isNotBlank(this.code) && StrUtil.startWith(this.state, PlatformDictEnum.MERCADOLIBRE.getCode())){
+            this.setPlatformCode(PlatformDictEnum.MERCADOLIBRE.getCode());
             return this;
         }
 
