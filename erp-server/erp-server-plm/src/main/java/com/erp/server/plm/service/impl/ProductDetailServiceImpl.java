6061359fdaddcea3ad23b4197e0bb28b9ec7cc11
jack chen <jackchen@ulanzi.cn>
2025-03-21 09:45:22 +0800

    fix: 修正sku编号字段在Excel中的必填标识并优化库存查询逻辑

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -4967,7 +4967,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
                     ProductInfoEntity productInfoEntity = productInfoMap.get(productBy.getProductId());
                     // 如果商品属性ID与实体中的属性ID不匹配,查sku库存
                     if(Objects.nonNull(productInfoEntity) && !productInfoEntity.getPropertyId().equals(productProperty.getId())){
-                        Integer qty = inventoryMap.get(dto.getSkuNo());
+                        Integer qty =  Objects.isNull(inventoryMap.get(dto.getSkuNo())) ? inventoryMap.get(dto.getSkuNo()) : 0;
                         if(qty > 0){
                             errorMsgList.add("SKU存在库存，产品属性不允许变更");
                         }
