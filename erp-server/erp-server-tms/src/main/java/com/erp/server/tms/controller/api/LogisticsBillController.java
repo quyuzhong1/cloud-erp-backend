4d4585ed27ac6905d42e88997b9686cbbf6aa325
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-03-21 16:41:16 +0800

    线上问题处理，高级搜索/导入字段修改/更新头程物流单轨迹

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/controller/api/LogisticsBillController.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/controller/api/LogisticsBillController.java
@@ -151,7 +151,7 @@ public class LogisticsBillController extends BaseController {
         for (String id : dto.getIds()) {
             BatchResultDTO result;
             try {
-                result = logisticsBillDetailService.updateStatus(id,trackStatus,dto.getTrackTime(),dto.getTrackDesc());
+                result = logisticsBillDetailService.updateStatus(id,trackStatus,dto.getTrackTime(),dto.getLogisticsTrack());
             } catch (Exception e) {
                 log.error("物流商更改状态失败{}", e);
                 LogisticsBillDetailEntity entity = logisticsBillDetailService.getById(id);
