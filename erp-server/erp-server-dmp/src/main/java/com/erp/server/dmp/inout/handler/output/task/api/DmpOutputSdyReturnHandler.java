f9450ffed8da5dd611f5d11870d861e6b137e4a8
shukai <shukai@ulanzi.cn>
2025-03-31 12:29:27 +0800

    add：调整为退款不退货

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyReturnHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyReturnHandler.java
@@ -169,16 +169,13 @@ public class DmpOutputSdyReturnHandler extends DmpOutputTaskHandler {
             String shopId = "";
             if (PlatformDictEnum.WDT.getCode().equalsIgnoreCase(dmpSoReturnEntity.getSourceSystem())) {
                 //RMA.退货单
-            	if ("1".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
-                    sdyDTO.setTransaction_type("RMA.退货单");
-                    sdyDTO.setTransaction_sub_type("售前退款");
-                } else if ("2".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
+            	if ("2".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
                     sdyDTO.setTransaction_type("RMA.退货单");
                     sdyDTO.setTransaction_sub_type("退款退货");
                 } else if ("3".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
                     sdyDTO.setTransaction_type("RMA.换货单");
                     sdyDTO.setTransaction_sub_type("换货退货");
-                } else if ("4".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
+                } else if ("1".equals(dmpSoReturnDetailEntity.getReturnOriginalType()) || "4".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
                     sdyDTO.setTransaction_type("RMA.退货单");
                     sdyDTO.setTransaction_sub_type("退款不退货");
                 } else if ("6".equals(dmpSoReturnDetailEntity.getReturnOriginalType())) {
