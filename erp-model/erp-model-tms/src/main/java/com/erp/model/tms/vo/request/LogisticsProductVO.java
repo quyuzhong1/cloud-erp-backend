d9b1147eb8a117d1b899b6548e2f68764f6c334b
liuruipeng <arthur@ulanzi.cn>
2025-04-01 16:06:10 +0800

    feat:【TT全托管】创建送货单

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/vo/request/LogisticsProductVO.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/vo/request/LogisticsProductVO.java
@@ -43,8 +43,10 @@ public class LogisticsProductVO  implements Serializable {
 
     private String id;
 
-    private String platformSkuId;
+    private String platformLineNumber;
 
+
+    private Integer deliveryQty;
 
     /**
      * 产品sku表id
