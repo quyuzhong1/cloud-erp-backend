e1194e9766c720b48e78c643b4b8ef1a486b314a
jack chen <jackchen@ulanzi.cn>
2025-04-02 15:33:25 +0800

    feat: 增加权限SQL参数支持

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/api/PilotApplicationController.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/controller/api/PilotApplicationController.java
@@ -85,7 +85,7 @@ public class PilotApplicationController extends BaseController {
     @DataPermission(operationType = DataAttributeEnum.LIST,
             tableField = "create_user_id",
             menuCode = "plm:pilotApplication:paging",
-            tableAlias = ""
+            tableAlias = "pa"
     )
     public ApiResult<List<PilotApplicationDTO.TabListDTO>> tabList(@RequestBody PermissionsDTO dto) {
        return success(pilotApplicationService.tabList(dto));
@@ -102,7 +102,7 @@ public class PilotApplicationController extends BaseController {
     @DataPermission(operationType = DataAttributeEnum.LIST,
             tableField = "create_user_id",
             menuCode = "plm:pilotApplication:paging",
-            tableAlias = ""
+            tableAlias = "pa"
     )
     @WebAdvanceQuery(handler = PilotApplicationQueryHandler.class)
     public ApiResult<PagingVO<PilotApplicationDTO.ListDTO>> paging(@RequestBody @Validated PagingDTO<PilotApplicationDTO.PagingParamDTO> dto) {
