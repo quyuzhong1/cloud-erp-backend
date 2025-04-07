f810c5feadfc26a670479b83c831308678329f8b
jack chen <jackchen@ulanzi.cn>
2025-04-07 14:31:25 +0800

    refactor(api): 将静态常量替换为配置属性

--- erp-sdk/erp-sdk-wx-miniapp/src/main/java/com/sdk/wx/miniapp/api/impl/BaseWxMiniAppServiceImpl.java
+++ erp-sdk/erp-sdk-wx-miniapp/src/main/java/com/sdk/wx/miniapp/api/impl/BaseWxMiniAppServiceImpl.java
@@ -9,6 +9,7 @@ import com.sdk.wx.miniapp.response.WxJscodeToSessionResponse;
 import com.sdk.wx.miniapp.response.WxTokenResponse;
 import jodd.util.StringUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -19,21 +20,15 @@ import java.util.Map;
 @Service
 public class BaseWxMiniAppServiceImpl implements WxMiniAppService {
 
-  public static final String APPID = "wxe61ca423cded1079";
-
-  public static final String APPSECRET = "267176f1c8c9e1e5e5af05c81f00976d";
-
-  public static final String GRANT_TYPE = "authorization_code";
+  @Value("${wx.appId}")
+  private String appId;
+  @Value("${wx.appsecret}")
+  private String appsecret;
+  public static final String grantType = "authorization_code";
 
   @Resource
   private RedisUtil redisUtil;
 
-  public static void main(String[] args) {
-    String url = String.format(WxConstants.BASE_URL + WxConstants.GET_ACCESS_TOKEN, APPID, APPSECRET);
-    String bodyStr = sendGet(url, new HashMap<>());
-    WxTokenResponse resultMap = JSONUtil.toBean(bodyStr, WxTokenResponse.class);
-    System.out.println(resultMap.getAccessToken());
-  }
 
   @Override
   public  String getAccessToken() {
@@ -43,7 +38,7 @@ public class BaseWxMiniAppServiceImpl implements WxMiniAppService {
     if (redisUtil.hasKey(key)) {
       accessToken = String.valueOf(redisUtil.get(key));
     } else {
-      String url = String.format(WxConstants.BASE_URL + WxConstants.GET_ACCESS_TOKEN, APPID, APPSECRET);
+      String url = String.format(WxConstants.BASE_URL + WxConstants.GET_ACCESS_TOKEN, appId, appsecret);
       String bodyStr = sendGet(url, new HashMap<>());
       WxTokenResponse resultMap = JSONUtil.toBean(bodyStr, WxTokenResponse.class);
       // 调用微信接口并缓存结果
@@ -60,12 +55,12 @@ public class BaseWxMiniAppServiceImpl implements WxMiniAppService {
         return null;
     }
 
-    String url = String.format(WxConstants.BASE_URL + WxConstants.JSCODE_TO_SESSION, APPID, APPSECRET,jsCode);
+    String url = String.format(WxConstants.BASE_URL + WxConstants.JSCODE_TO_SESSION, appId, appsecret,jsCode);
     Map<String, Object> paramMap = new HashMap<>();
-    paramMap.put("appid", APPID);
-    paramMap.put("secret", APPSECRET);
+    paramMap.put("appid", appId);
+    paramMap.put("secret", appsecret);
     paramMap.put("js_code", jsCode);
-    paramMap.put("grant_type", GRANT_TYPE);
+    paramMap.put("grant_type", grantType);
     String bodyStr = sendGet(url, paramMap);
     return JSONUtil.toBean(bodyStr, WxJscodeToSessionResponse.class);
 
