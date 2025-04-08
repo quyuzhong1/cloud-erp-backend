2000262465c6b74fbbd3847756e201daedf501f6
liuruipeng <arthur@ulanzi.cn>
2025-04-08 16:11:52 +0800

    feat:【TT全托管】新增校验

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackageForecastServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/PackageForecastServiceImpl.java
@@ -1314,7 +1314,13 @@ public class PackageForecastServiceImpl extends SuperServiceImpl<PackageForecast
         if(allSoB2cEntityList.stream().map(SoB2cEntity::getDictPlatform).distinct().count() > 1){
             throw new ServiceException("组包预报单明细数据平台不一致");
         }
-        return allSoB2cEntityList.get(0).getDictPlatform();
+        String platform = allSoB2cEntityList.get(0).getDictPlatform();
+        if(!platform.equals(PlatformDictEnum.TIK_TOK_FULLY.getCode())
+                && !platform.equals(PlatformDictEnum.ALI_EXPRESS.getCode())
+                && !platform.equals(PlatformDictEnum.TIK_TOK.getCode())){
+            throw new ServiceException("非tiktok,tiktok全托管，速卖通平台无需上传");
+        }
+        return platform;
     }
 
     @Override
