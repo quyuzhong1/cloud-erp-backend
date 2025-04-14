ed89468a448f3f2484d07fb36b501d5acb781e9b
jason <jason.ulanzi.cn>
2025-04-14 17:36:28 +0800

    作废发票号V1

--- erp-rpc/erp-rpc-oms/src/main/java/com/erp/rpc/oms/feign/ExportOmsFeign.java
+++ erp-rpc/erp-rpc-oms/src/main/java/com/erp/rpc/oms/feign/ExportOmsFeign.java
@@ -54,4 +54,7 @@ public interface ExportOmsFeign {
 
     @PostMapping("/feign/export/exportInvoice")
     PagingVO<InvoiceInfoDTO.PagingViewDTO> exportInvoice(@RequestBody PagingDTO<InvoiceInfoDTO.PagingParamDTO> dto);
+
+    @PostMapping("/feign/export/exportInvoiceInvalid")
+    PagingVO<CfgInvoiceInvalidDTO.PagingViewDTO> exportInvoiceInvalid(@RequestBody PagingDTO<CfgInvoiceInvalidDTO.PagingParamDTO> dto);
 }
---
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/CfgInvoiceInvalidServiceImpl.java
@@ -0,0 +1,91 @@
+package com.erp.server.oms.service.impl;
+
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.common.business.dto.base.BaseResultDTO;
+import com.common.business.dto.base.PagingDTO;
+import com.common.business.vo.PagingVO;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.oms.dto.CfgInvoiceSettingDTO;
+import com.erp.model.oms.dto.CustomerDTO;
+import com.erp.model.oms.entity.CfgInvoiceInvalidEntity;
+import com.erp.rpc.file.feign.DownloadTaskFeign;
+import com.erp.server.oms.mapper.CfgInvoiceInvalidMapper;
+import com.erp.server.oms.service.CfgInvoiceInvalidService;
+import com.common.business.service.impl.SuperServiceImpl;
+import com.common.business.threadlocal.UserContext;
+import com.erp.server.oms.service.OperateLogService;
+import com.erp.server.oms.service.CommonService;
+import com.common.core.exception.ServiceException;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
+import com.erp.model.oms.dto.CfgInvoiceInvalidDTO;
+import java.util.*;
+import com.common.core.utils.*;
+import com.common.core.enums.ApiError;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import static com.common.business.enums.FileTaskEventEnum.EXPORT_INVOICE_INFO;
+import static com.common.business.enums.FileTaskEventEnum.EXPORT_INVOICE_INVALID;
+
+/**
+ * <p>
+ * 作废发票号 服务实现类
+ * </p>
+ *
+ * @author hcg
+ * @since 2025-04-14
+ */
+@Slf4j
+@Service
+public class CfgInvoiceInvalidServiceImpl extends SuperServiceImpl<CfgInvoiceInvalidMapper, CfgInvoiceInvalidEntity> implements CfgInvoiceInvalidService {
+    @Autowired
+    private OperateLogService operateLogService;
+
+    @Resource
+    private DownloadTaskFeign downloadTaskFeign;
+
+    @GlobalTransactional(rollbackFor = Exception.class)
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public BaseResultDTO.AddDTO add(CfgInvoiceInvalidDTO.AddDTO addDTO) {
+        CfgInvoiceInvalidEntity cfgInvoiceInvalidEntity = new CfgInvoiceInvalidEntity();
+        BeanMapperUtils.copy(addDTO, cfgInvoiceInvalidEntity);
+
+        log.info("开始新增作废发票号");
+        boolean save = super.save(cfgInvoiceInvalidEntity);
+        if(!save) {
+            throw new ServiceException("作废发票号保存失败");
+        }
+
+        // 操作日志
+        String msg = StrUtil.format("用户【{}】新增【{}】单据id为【{}】", UserContext.getDefaultLoginUser().getUserName(), "作废发票号" , cfgInvoiceInvalidEntity.getId());
+        operateLogService.addModuleOperateLog(msg, null, cfgInvoiceInvalidEntity.getId(), "新增操作");
+
+        return new BaseResultDTO.AddDTO(cfgInvoiceInvalidEntity.getId(), cfgInvoiceInvalidEntity.getId());
+    }
+
+    @Override
+    public PagingVO<CfgInvoiceInvalidDTO.PagingViewDTO> paging(PagingDTO<CfgInvoiceInvalidDTO.PagingParamDTO> dto) {
+        CfgInvoiceInvalidDTO.PagingParamDTO params = dto.getParams();
+        params.setPermissionSql(dto.getPermissionSql());
+        Page<CfgInvoiceSettingDTO.PagingViewDTO> query = new Page<>(dto.getCurrPage(), dto.getPageSize());
+        //分页数据
+        IPage<CfgInvoiceInvalidDTO.PagingViewDTO> pageData = baseMapper.paging(query, params);
+        return new PagingVO(pageData);
+    }
+
+    @Override
+    public Boolean export(CustomerDTO.@Valid ExportDTO dto) {
+        downloadTaskFeign.saveDownloadTask("作废发票号", EXPORT_INVOICE_INVALID.getCode(), dto);
+        return null;
+    }
+
+}
