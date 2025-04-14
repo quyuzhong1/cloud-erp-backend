23e7e45a37216af524aec4b71b0bb3d46ee21cb5
jason <jason.ulanzi.cn>
2025-04-14 14:27:47 +0800

    InvoiceRuleEnum开票规则枚举类

---
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/InvoiceRuleEnum.java
@@ -0,0 +1,61 @@
+package com.erp.model.oms.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import java.util.Objects;
+
+/**
+ * @description: 开票规则
+ * @author: hcg
+ * @date: 2025/4/14 14:21
+ */
+public enum InvoiceRuleEnum {
+    AMOUNT("amount", "按产品全额开票"),
+    CUSTOM("custom", "按（产品全额×自定义百分比）后开票"),
+    DEDUCT("deduct", "按（产品全额-佣金）后开票"),
+    ;
+
+    /**
+     * 开票类型
+     */
+    @EnumValue
+    @JsonValue
+    private final String code;
+
+    /**
+     * 开票规则描述
+     */
+    private final String name;
+
+    InvoiceRuleEnum(String code, String description) {
+        this.code = code;
+        this.name = description;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return name;
+    }
+
+    public static String getDescription(String code) {
+        for (InvoiceRuleEnum rule : InvoiceRuleEnum.values()) {
+            if (code.equals(rule.getCode())) {
+                return rule.getDescription();
+            }
+        }
+        return "";
+    }
+
+    public static String getCodeByDesc(String description) {
+        for (InvoiceRuleEnum rule : InvoiceRuleEnum.values()) {
+            if(Objects.equals(description, rule.name)) {
+                return rule.code;
+            }
+        }
+        return "";
+    }
+}
