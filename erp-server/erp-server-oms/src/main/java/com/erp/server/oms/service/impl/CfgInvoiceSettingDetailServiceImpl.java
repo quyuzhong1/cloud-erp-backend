0da2d71efc8127444adca6e6e4aa1c4a13680a01
jason <jason.ulanzi.cn>
2025-04-15 12:13:47 +0800

    作废发票号和V7

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/CfgInvoiceSettingDetailServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/CfgInvoiceSettingDetailServiceImpl.java
@@ -2,6 +2,7 @@ package com.erp.server.oms.service.impl;
 
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
@@ -29,7 +30,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
 import java.util.stream.Collectors;
-
 
 
 /**
@@ -89,30 +89,33 @@ public class CfgInvoiceSettingDetailServiceImpl extends SuperServiceImpl<CfgInvo
     @Transactional(rollbackFor = Exception.class)
     @Override
     public BaseResultDTO.AddDTO addOrUpdate(List<CfgInvoiceSettingDetailDTO.AddDTO> dtoList) {
-        CfgInvoiceSettingEntity cfgInvoiceSettingEntity = cfgInvoiceSettingService.getById(dtoList.get(0).getMainId());
-        if (ObjectUtil.isEmpty(cfgInvoiceSettingEntity)){
-            throw new ServiceException("发票设置不存在");
+        if (CollUtil.isEmpty(dtoList)) {
+            throw new ServiceException("参数不能为空");
         }
-        //校验shopid是否唯一
-        if (super.count(new LambdaQueryWrapper<CfgInvoiceSettingDetailEntity>().eq(CfgInvoiceSettingDetailEntity::getShopId, dtoList.get(0).getShopId()))>0){
-            throw new ServiceException("店铺已存在!请不要重复添加");
-        }
-        List<CfgInvoiceSettingDetailEntity> entityList = dtoList.stream()
-                .map(item -> {
-                    CfgInvoiceSettingDetailEntity entity = BeanUtil.copyProperties(item, CfgInvoiceSettingDetailEntity.class);
-                    entity.setDictPlatform(item.getPlatformValue()); // 单独设置字段
-                    return entity;
-                })
+        CfgInvoiceSettingEntity cfgInvoiceSettingEntity = validateInvoiceSetting(dtoList.get(0).getMainId());
+        // 处理删除逻辑
+        handleDeletedDetails(dtoList);
+        // 拆分新增 & 更新
+        List<CfgInvoiceSettingDetailDTO.AddDTO> createList = dtoList.stream()
+                .filter(item -> ObjectUtil.isEmpty(item.getId()))
                 .collect(Collectors.toList());
-        //保存平台value
-        log.info("开始新增发票设置明细");
-        boolean save = super.saveOrUpdateBatch(entityList);
-        if (!save) {
-            throw new ServiceException("发票设置明细保存失败");
+        List<CfgInvoiceSettingDetailDTO.AddDTO> updateList = dtoList.stream()
+                .filter(item -> ObjectUtil.isNotEmpty(item.getId()))
+                .collect(Collectors.toList());
+        // 处理新增
+        if (CollUtil.isNotEmpty(createList)) {
+            handleCreateDetails(createList, cfgInvoiceSettingEntity.getId());
+        }
+        // 处理更新
+        if (CollUtil.isNotEmpty(updateList)) {
+            handleUpdateDetails(updateList);
         }
         // 操作日志
-        String msg = StrUtil.format("用户【{}】新增【{}】单据id为【{}】", UserContext.getDefaultLoginUser().getUserName(), "发票设置明细", dtoList.get(0).getId());
-        operateLogService.addModuleOperateLog(msg, ModuleTypeEnum.INVOICE_SETTING_DETAIL.getCode(), dtoList.get(0).getId(), "新增操作");
+        String msg = StrUtil.format("用户【{}】新增【{}】单据id为【{}】",
+                UserContext.getDefaultLoginUser().getUserName(), "发票设置明细", dtoList.get(0).getId());
+        operateLogService.addModuleOperateLog(msg,
+                ModuleTypeEnum.INVOICE_SETTING_DETAIL.getCode(),
+                dtoList.get(0).getId(), "新增操作");
         return new BaseResultDTO.AddDTO(dtoList.get(0).getId(), dtoList.get(0).getId());
     }
 
@@ -143,6 +146,109 @@ public class CfgInvoiceSettingDetailServiceImpl extends SuperServiceImpl<CfgInvo
 
     @Override
     public CfgInvoiceSettingDetailEntity getInvoiceSettingDetail(String dictPlatform, String shopId) {
-        return baseMapper.getInvoiceSettingDetail(dictPlatform,shopId);
+        return baseMapper.getInvoiceSettingDetail(dictPlatform, shopId);
+    }
+
+    /**
+      * @description: 校验发票设置是否存在
+      * @author: hcg
+      * @date: 2025/4/15 12:08
+      * @param: mainId
+      * @return: null
+      **/
+    private CfgInvoiceSettingEntity validateInvoiceSetting(String mainId) {
+        CfgInvoiceSettingEntity entity = cfgInvoiceSettingService.getById(mainId);
+        if (ObjectUtil.isEmpty(entity)) {
+            throw new ServiceException("发票设置不存在");
+        }
+        return entity;
+    }
+    
+    /**
+      * @description:删除逻辑封装
+      * @author: hcg 
+      * @date: 2025/4/15 12:09
+      * @param: dtoList
+      * @return: null
+      **/
+    private void handleDeletedDetails(List<CfgInvoiceSettingDetailDTO.AddDTO> dtoList) {
+        String mainId = dtoList.get(0).getMainId();
+        Set<String> existingIds = this.list(
+                        new LambdaQueryWrapper<CfgInvoiceSettingDetailEntity>()
+                                .eq(CfgInvoiceSettingDetailEntity::getMainId, mainId)
+                ).stream()
+                .map(CfgInvoiceSettingDetailEntity::getId)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+        Set<String> incomingIds = dtoList.stream()
+                .map(CfgInvoiceSettingDetailDTO.AddDTO::getId)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toSet());
+        existingIds.removeAll(incomingIds);
+        if (CollUtil.isNotEmpty(existingIds)) {
+            log.info("开始删除以下发票设置明细: {}", existingIds);
+            boolean removed = this.lambdaUpdate()
+                    .in(CfgInvoiceSettingDetailEntity::getId, existingIds)
+                    .remove();
+            if (!removed) {
+                throw new ServiceException("删除发票设置明细失败");
+            }
+        }
+    }
+    
+    /**
+      * @description:新增逻辑封装
+      * @author: hcg 
+      * @date: 2025/4/15 12:10
+      * @param: createList，mainId
+      * @return: null
+      **/
+    private void handleCreateDetails(List<CfgInvoiceSettingDetailDTO.AddDTO> createList, String mainId) {
+        List<String> incomingShopIds = createList.stream()
+                .map(CfgInvoiceSettingDetailDTO.AddDTO::getShopId)
+                .filter(ObjectUtil::isNotEmpty)
+                .collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(incomingShopIds)) {
+            long count = super.count(
+                    new LambdaQueryWrapper<CfgInvoiceSettingDetailEntity>()
+                            .in(CfgInvoiceSettingDetailEntity::getShopId, incomingShopIds)
+                            .eq(CfgInvoiceSettingDetailEntity::getMainId, mainId)
+            );
+            if (count > 0) {
+                throw new ServiceException("店铺已存在!请不要重复添加");
+            }
+        }
+        List<CfgInvoiceSettingDetailEntity> entityCreateList = createList.stream()
+                .map(item -> {
+                    CfgInvoiceSettingDetailEntity entity = BeanUtil.copyProperties(item, CfgInvoiceSettingDetailEntity.class);
+                    entity.setDictPlatform(item.getPlatformValue());
+                    return entity;
+                })
+                .collect(Collectors.toList());
+        log.info("开始新增发票设置明细");
+        if (!super.saveBatch(entityCreateList)) {
+            throw new ServiceException("新增发票设置明细失败");
+        }
+    }
+    
+    /**
+      * @description:更新逻辑封装
+      * @author: hcg 
+      * @date: 2025/4/15 12:10
+      * @param: updateList
+      * @return: null
+      **/
+    private void handleUpdateDetails(List<CfgInvoiceSettingDetailDTO.AddDTO> updateList) {
+        List<CfgInvoiceSettingDetailEntity> entityUpdateList = updateList.stream()
+                .map(item -> {
+                    CfgInvoiceSettingDetailEntity entity = BeanUtil.copyProperties(item, CfgInvoiceSettingDetailEntity.class);
+                    entity.setDictPlatform(item.getPlatformValue());
+                    return entity;
+                })
+                .collect(Collectors.toList());
+        log.info("开始更新发票设置明细");
+        if (!super.updateBatchById(entityUpdateList)) {
+            throw new ServiceException("更新发票设置明细失败");
+        }
     }
 }
