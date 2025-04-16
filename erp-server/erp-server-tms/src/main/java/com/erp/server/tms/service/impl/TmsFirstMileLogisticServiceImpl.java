eecb1f9df5e3306defe02d2813fa0c966ff99683
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-04-16 15:37:51 +0800

    更新渠道信息时，重算计费重

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsFirstMileLogisticServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsFirstMileLogisticServiceImpl.java
@@ -2094,7 +2094,6 @@ public class TmsFirstMileLogisticServiceImpl extends SuperServiceImpl<LogisticsB
             logisticsBillEntity.setTransportNo(dto.getTransportNo());
             updateList.add(logisticsBillEntity);
             logisticsBillCostEntity.setCurrency(supplierEntity.getPayCurrency());
-            updateCostList.add(logisticsBillCostEntity);
             //更新体积重
             FirstMileDeliveryDTO.GenerateLogisticDTO deliveryLogisticDto = generateLogisticDTOList.stream().filter(v->v.getOutstockId().equals(logisticsBillEntity.getOutstockId())).findFirst().orElse(null);
             if(Objects.nonNull(deliveryLogisticDto) && CollectionUtils.isNotEmpty(deliveryLogisticDto.getPackingDTOList())
@@ -2104,6 +2103,9 @@ public class TmsFirstMileLogisticServiceImpl extends SuperServiceImpl<LogisticsB
                         .reduce(BigDecimal.ZERO, BigDecimal::add);
                 logisticsBillCostEntity.setVolumeWeight(totalSize.divide(BigDecimal.valueOf(logisticsChannelEntity.getVolumeSetting()),4, RoundingMode.HALF_UP));
             }
+            //更新计费重 取 体积重和实际重最大值
+            logisticsBillCostEntity.setBillingWeight(logisticsBillCostEntity.getVolumeWeight().max(logisticsBillCostEntity.getActualWeight()));
+            updateCostList.add(logisticsBillCostEntity);
             //设置消息发送
             if(Objects.nonNull(detailEntity) && detailEntity.getTrackStatus().equals(FmLogisticTrackStatusEnum.ORDERED.getCode()) && !oldChannelId.equals(dto.getLogisticsChannelId())){
                 TmsFirstMileLogisticDTO.MsgDTO msgDTO = new TmsFirstMileLogisticDTO.MsgDTO();
