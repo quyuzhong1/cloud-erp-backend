884588abb83005b754e529659791886be778958e
liuruipeng <arthur@ulanzi.cn>
2025-04-18 16:43:13 +0800

    合并问题

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoB2cServiceImpl.java
@@ -386,13 +386,6 @@ public class SoB2cServiceImpl extends SuperServiceImpl<SoB2cMapper, SoB2cEntity>
     @Resource
     private InvoiceInfoService invoiceInfoService;
 
-    @Resource
-    private MercadoLocalSdkClientService mercadoLocalSdkClientService;
-
-    @Lazy
-    @Resource
-    private InvoiceInfoService invoiceInfoService;
-
     @Override
     public PagingVO<SoB2cDTO.ListDTO> paging(PagingDTO<SoB2cDTO.PagingParamDTO> pagingParamDTO) {
         pagingParamDTO.getParams().setPermissionSql(pagingParamDTO.getPermissionSql());
