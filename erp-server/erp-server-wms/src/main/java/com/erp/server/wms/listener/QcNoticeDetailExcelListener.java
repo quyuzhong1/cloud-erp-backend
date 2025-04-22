3f717263fed321d8bbcd497225e53d4328fa7569
jack chen <jackchen@ulanzi.cn>
2025-04-22 18:20:31 +0800

    refactor(wms): 重构 QcNoticeDetailExcelListener 类

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/listener/QcNoticeDetailExcelListener.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/listener/QcNoticeDetailExcelListener.java
@@ -7,32 +7,33 @@ import com.common.core.utils.FieldValidUtil;
 import com.common.core.utils.StrUtils;
 import com.erp.model.oms.dto.ListingInfoDTO;
 import com.erp.model.oms.dto.ListingInfoWithSkuMappingDTO;
+import com.erp.model.wms.dto.QcNoticeDTO;
 import com.erp.model.wms.dto.excel.DeliveryPlanDetailExportExcelDTO;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class QcNoticeDetailExcelListener extends AnalysisEventListener<DeliveryPlanDetailExportExcelDTO> {
+public class QcNoticeDetailExcelListener extends AnalysisEventListener<QcNoticeDTO.QcNoticeDetailExportExcelDTO> {
 
     /**
      * 导入数据，用于判断导入是否为空
      */
-    private List<DeliveryPlanDetailExportExcelDTO> allList = new ArrayList<>();
+    private List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> allList = new ArrayList<>();
 
     /**
      * 导入错误数据
      */
-    private List<DeliveryPlanDetailExportExcelDTO> errorList = new ArrayList<>();
+    private List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> errorList = new ArrayList<>();
 
     /**
      * 导入正确数据
      */
-    private List<ListingInfoDTO.PageDTO> successList = new ArrayList<>();
+    private List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> successList = new ArrayList<>();
 
     @Override
-    public void invoke(DeliveryPlanDetailExportExcelDTO deliveryPlanDetailExportExcelDTO, AnalysisContext analysisContext) {
-        ListingInfoDTO.PageDTO viewDTO = new ListingInfoDTO.PageDTO();
+    public void invoke(QcNoticeDTO.QcNoticeDetailExportExcelDTO deliveryPlanDetailExportExcelDTO, AnalysisContext analysisContext) {
+        QcNoticeDTO.QcNoticeDetailExportExcelDTO viewDTO = new QcNoticeDTO.QcNoticeDetailExportExcelDTO();
         //添加数据用于判断是否为空
         allList.add(deliveryPlanDetailExportExcelDTO);
 
@@ -41,41 +42,6 @@ public class QcNoticeDetailExcelListener extends AnalysisEventListener<DeliveryP
         List<String> msgList = FieldValidUtil.fieldValid(deliveryPlanDetailExportExcelDTO);
         if (CollectionUtils.isNotEmpty(msgList)) {
             errorMsgList.addAll(msgList);
-        }
-        if (CollectionUtils.isEmpty(allThirdWarehouseSkuList)) {
-            errorMsgList.add("系统中第三方仓sku为空");
-        } else {
-            //fba 不需要匹配仓库
-            ListingInfoWithSkuMappingDTO listingInfoWithSkuMappingDTO = allThirdWarehouseSkuList.stream().filter(
-                    v->v.getPlatformSkuNo().equals(deliveryPlanDetailExportExcelDTO.getSkuNo()) && (isFba || v.getHasMappingAll() || v.getWarehouseId().equals(this.warehouseId))
-                    )
-                    .findFirst().orElse(null);
-            if (Objects.nonNull(listingInfoWithSkuMappingDTO)) {
-                if (!StrUtils.isInteger(deliveryPlanDetailExportExcelDTO.getPlanQty())) {
-                    errorMsgList.add("计划数量只能为正整数");
-                } else {
-                    if (thirdSkuNoList.contains(deliveryPlanDetailExportExcelDTO.getSkuNo())) {
-                        errorMsgList.add("明细列表已存在该SKU");
-                    } else if (importSkuIds.contains(deliveryPlanDetailExportExcelDTO.getSkuNo())) {
-                        errorMsgList.add("导入数据中已存在该SKU");
-                    } else {
-                        viewDTO = ListingInfoDTO.PageDTO.builder()
-                                .id(listingInfoWithSkuMappingDTO.getTableId())
-                                .platformSku(listingInfoWithSkuMappingDTO.getPlatformSkuNo())
-                                .mSKU(listingInfoWithSkuMappingDTO.getPlatformSkuNo())
-                                .platformSkuName(listingInfoWithSkuMappingDTO.getPlatformSkuName())
-                                .skuId(listingInfoWithSkuMappingDTO.getProductSkuId())
-                                .skuNo(listingInfoWithSkuMappingDTO.getProductSkuNo())
-                                .productName(listingInfoWithSkuMappingDTO.getProductName())
-                                .fnSku(listingInfoWithSkuMappingDTO.getPlatformFnSku())
-                                .asin(listingInfoWithSkuMappingDTO.getPlatformSpuNo())
-                                .qty(Integer.valueOf(deliveryPlanDetailExportExcelDTO.getPlanQty()))
-                                .build();
-                    }
-                }
-            }else{
-                errorMsgList.add("系统中没有该第三方仓sku");
-            }
         }
         //存在错误数据则直接返回
         if (!errorMsgList.isEmpty()) {
@@ -91,15 +57,15 @@ public class QcNoticeDetailExcelListener extends AnalysisEventListener<DeliveryP
 
     }
 
-    public List<DeliveryPlanDetailExportExcelDTO> getAllList(){
+    public List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> getAllList(){
         return allList;
     }
 
-    public List<DeliveryPlanDetailExportExcelDTO> getErrorList(){
+    public List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> getErrorList(){
         return errorList;
     }
 
-    public List<ListingInfoDTO.PageDTO> getSuccessList(){
+    public List<QcNoticeDTO.QcNoticeDetailExportExcelDTO> getSuccessList(){
         return successList;
     }
 }
