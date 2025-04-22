c454adc368f308d3144703ee416c736f44c722c6
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-04-22 12:29:08 +0800

    批量编辑增加字段校验

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoB2cController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/SoB2cController.java
@@ -12,6 +12,7 @@ import com.common.business.enums.ApproveStatusEnum;
 import com.common.business.enums.ApproveTypeEnum;
 import com.common.business.enums.PlatformDictEnum;
 import com.common.business.enums.SourceTypeEnum;
+import com.common.business.validator.ValidList;
 import com.common.business.vo.PagingVO;
 import com.common.core.anno.LogAction;
 import com.common.core.controller.BaseController;
@@ -1350,7 +1351,7 @@ public class SoB2cController extends BaseController {
      * @date: 2024-06-17
      */
     @PostMapping("/addGift")
-    public ApiResult<List<BatchResultDTO>> addGift(@RequestBody @Validated List<SoB2cDTO.GiftDTO> dtoList) {
+    public ApiResult<List<BatchResultDTO>> addGift(@RequestBody @Validated ValidList<SoB2cDTO.GiftDTO> dtoList) {
         Map<String, List<SoB2cDTO.GiftDTO>> collect = dtoList.stream().collect(Collectors.groupingBy(SoB2cDTO.GiftDTO::getId));
         List<BatchResultDTO> resultDTOS = new ArrayList<>(collect.size());
         List<String> ids = dtoList.stream().map(SoB2cDTO.GiftDTO::getId).distinct().collect(Collectors.toList());
@@ -1392,7 +1393,7 @@ public class SoB2cController extends BaseController {
      * @return
      */
     @PostMapping("/updateReceiverInfo")
-    public ApiResult<List<BatchResultDTO>> updateReceiverInfo(@RequestBody @Validated List<SoB2cReceiverDTO.UpdateBaseDTO> dtoList) {
+    public ApiResult<List<BatchResultDTO>> updateReceiverInfo(@RequestBody @Validated ValidList<SoB2cReceiverDTO.UpdateBaseDTO> dtoList) {
         List<BatchResultDTO> resultDTOS = new ArrayList<>(dtoList.size());
         for (SoB2cReceiverDTO.UpdateBaseDTO dto : dtoList) {
             BatchResultDTO receiverResult;
