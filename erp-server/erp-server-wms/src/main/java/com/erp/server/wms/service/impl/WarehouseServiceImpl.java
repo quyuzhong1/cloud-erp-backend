c2bb99ef63b8361ed2fbc5bc27f484c4a70421bc
Will <wangwei@ulanzi.cn>
2025-05-07 12:28:15 +0800

    feat,代码合并错误处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseServiceImpl.java
@@ -429,6 +429,24 @@ public class WarehouseServiceImpl extends SuperServiceImpl<WarehouseMapper, Ware
     }
 
     @Override
+    public List<WarehouseDTO.UpdateDTO> listWarehouseByNameList(List<String> warehouseNameList) {
+        if (CollectionUtils.isEmpty(warehouseNameList)) {
+            return new ArrayList<>();
+        }
+        List<WarehouseEntity> list = lambdaQuery().in(WarehouseEntity::getName,warehouseNameList).list();
+        if (CollectionUtils.isEmpty(list)) {
+            return new ArrayList<>();
+        }
+        List<WarehouseDTO.UpdateDTO> resultList = new ArrayList<>();
+        for (WarehouseEntity warehouseEntity : list) {
+            WarehouseDTO.UpdateDTO updateDTO = BeanMapperUtils.map(WarehouseDTO.UpdateDTO.class, warehouseEntity);
+            updateDTO.setApproveStatusEnum(warehouseEntity.getApproveStatus());
+            resultList.add(updateDTO);
+        }
+        return resultList;
+    }
+
+    @Override
     public PagingVO<WarehouseDTO.ListDTO> selectPaging(PagingDTO<WarehouseDTO.SelectDTO> searchDTO) {
         Page query = new Page(searchDTO.getCurrPage(), searchDTO.getPageSize());
         WarehouseDTO.SelectDTO params = searchDTO.getParams();
