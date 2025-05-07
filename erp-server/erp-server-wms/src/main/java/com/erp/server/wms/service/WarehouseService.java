b4e4df4be6f1e6aaf65cc42a699bd16e295fb4b1
Will <wangwei@ulanzi.cn>
2025-05-07 12:30:49 +0800

    feat,代码合并错误处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/WarehouseService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/WarehouseService.java
@@ -303,4 +303,11 @@ public interface WarehouseService extends SuperService<WarehouseEntity> {
      * @param warehouseEntity
      */
     boolean checkOpenCloseTime(WarehouseEntity warehouseEntity);
+
+    /**
+     * 根据仓库名称获取仓库信息
+     * @param warehouseNameList
+     * @return
+     */
+    List<WarehouseEntity> listByWarehouseNameList(List<String> warehouseNameList);
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WarehouseServiceImpl.java
@@ -1468,6 +1468,14 @@ public class WarehouseServiceImpl extends SuperServiceImpl<WarehouseMapper, Ware
         return Boolean.FALSE.equals(disabled) && warehouseEntity.getOpenTime() == null;
 	}
 
+    @Override
+    public List<WarehouseEntity> listByWarehouseNameList(List<String> warehouseNameList) {
+        if (CollUtil.isEmpty(warehouseNameList)){
+            return Collections.emptyList();
+        }
+        return this.lambdaQuery().in(WarehouseEntity::getName,warehouseNameList).list();
+    }
+
     private void validateOpenCloseTime(WarehouseEntity warehouseEntity) {
 		if(this.checkOpenCloseTime(warehouseEntity)) {
 			throw new ServiceException(ApiError.OPEN_STATUS_OPEN_TIME_NOT_NULL);
