2b7bd015a384ed8a857633cd7d19d15e85b676a1
Will <wangwei@ulanzi.cn>
2025-05-13 16:03:18 +0800

    feat,委托审批和操作日志代码提交

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/query/ProcessDelegateQueryHandler.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/query/ProcessDelegateQueryHandler.java
@@ -1,8 +1,7 @@
 package com.erp.server.workflow.query;
 
-import com.common.business.enums.ApproveStatusEnum;
 import com.common.business.query.AbstractQueryHandler;
-import com.erp.model.scm.enums.PageListTypeEnum;
+import com.erp.model.workflow.enums.ProcessDelegateStatusEnum;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -14,7 +13,9 @@ public class ProcessDelegateQueryHandler extends AbstractQueryHandler {
 
     @Override
     protected String handleSqlLogic(String field, Object value, String compareCodeSplicingValueSql) {
-
+        if("tab".equals(field)){
+            return getTabSql(value);
+        }
         return null;
     }
 
@@ -26,17 +27,17 @@ public class ProcessDelegateQueryHandler extends AbstractQueryHandler {
      * @return String
      */
     public String getTabSql (Object value) {
-        // 待提交
-        if (PageListTypeEnum.WAIT_SUBMIT.getCode().equals(value)) {
-            super.buildDefaultDTO("pd.status", Collections.singletonList(ApproveStatusEnum.WAIT_SUBMIT.getStatus()));
+        // 待执行
+        if (ProcessDelegateStatusEnum.PENDING.getCode().equals(value)) {
+            super.buildDefaultDTO("pd.status", Collections.singletonList(ProcessDelegateStatusEnum.PENDING.getCode()));
         }
-        // 待审核
-        if (PageListTypeEnum.TO_BE_APPROVE.getCode().equals(value)) {
-            super.buildDefaultDTO("pd.status", Collections.singletonList(ApproveStatusEnum.APPROVE_ING.getStatus()));
+        // 运行中
+        if (ProcessDelegateStatusEnum.RUNNING.getCode().equals(value)) {
+            super.buildDefaultDTO("pd.status", Collections.singletonList(ProcessDelegateStatusEnum.RUNNING.getCode()));
         }
-        // 已审核
-        if (PageListTypeEnum.APPROVE.getCode().equals(value)) {
-            super.buildDefaultDTO("pd.status", Collections.singletonList(ApproveStatusEnum.APPROVE.getStatus()));
+        // 已结束
+        if (ProcessDelegateStatusEnum.ENDED.getCode().equals(value)) {
+            super.buildDefaultDTO("pd.status", Collections.singletonList(ProcessDelegateStatusEnum.ENDED.getCode()));
         }
         return super.getSplicingSQL();
     }
