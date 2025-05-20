6010214b97334553f306f283753c81f1dc4b0b0d
jason <jason.ulanzi.cn>
2025-05-20 17:40:33 +0800

    feat,流程配置提交

--- erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/CfgQueryOptionDTO.java
+++ erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/dto/CfgQueryOptionDTO.java
@@ -76,6 +76,21 @@ public class CfgQueryOptionDTO implements Serializable {
          * 前端请求体
          */
         private String props;
+
+        private String apiType;
+
+        /**
+         * 字段类型：checkboxV2=多选,radioV2=单选,input=文本,datetime=日期,number=数值,amount=金额,attachmentV2=附件,fieldList=明细
+         */
+        private String fieldType;
+        /**
+         * 是否必填
+         */
+        private Boolean isRequired;
+
+        private String conditionField;
+
+        private String fieldBelongsType;
     }
 
     /**
@@ -189,8 +204,8 @@ public class CfgQueryOptionDTO implements Serializable {
         /**
          * 配置单据：purchaseOrder=采购订单，purchaseApplication=采购申请单，purchaseChange=采购变更单，productChange=变更管理，productBomInfo=BOM管理，salesDemand=备货申请单，poInstock=入库单，soOutstock=销售出库单，customerB2bChangeSeller=B2B客户表变更销售员，requisitionApplicationChange=要货申请变更单，ProductLogistics=物流产品信息，transferApplication=调拨申请单，supplier=供应商列表，poReturn=采购退货单，pilotApplication=试产量产单，soDeliveryNoticeChange=发货通知变更单，stocktakingTask=盘点任务，transferInfo=直接调拨单，poReceive=收货单，productDetail=产品管理，projectTask=任务列表，subcontractOrder=委外订单，purchasePrice=采购价目表，purchasePriceChange=采购调价表，soB2c=B2C销售订单，soInfo=B2B销售订单，firstMileDelivery=发货单，customerInfo=B2B客户列表，soChange=B2B销售变更单，deliveryPlan=第三方仓发货计划，requisitionApplication=要货申请，soPrice=销售价目表，soPriceChange=销售调价表，transferIn=分步式调入单，transferOut=分步式调出单
          */
-        @NotBlank(message = "配置单据：purchaseOrder=采购订单，purchaseApplication=采购申请单，purchaseChange=采购变更单，productChange=变更管理，productBomInfo=BOM管理，salesDemand=备货申请单，poInstock=入库单，soOutstock=销售出库单，customerB2bChangeSeller=B2B客户表变更销售员，requisitionApplicationChange=要货申请变更单，ProductLogistics=物流产品信息，transferApplication=调拨申请单，supplier=供应商列表，poReturn=采购退货单，pilotApplication=试产量产单，soDeliveryNoticeChange=发货通知变更单，stocktakingTask=盘点任务，transferInfo=直接调拨单，poReceive=收货单，productDetail=产品管理，projectTask=任务列表，subcontractOrder=委外订单，purchasePrice=采购价目表，purchasePriceChange=采购调价表，soB2c=B2C销售订单，soInfo=B2B销售订单，firstMileDelivery=发货单，customerInfo=B2B客户列表，soChange=B2B销售变更单，deliveryPlan=第三方仓发货计划，requisitionApplication=要货申请，soPrice=销售价目表，soPriceChange=销售调价表，transferIn=分步式调入单，transferOut=分步式调出单不能为空")
-        @Size(max = 30, message = "配置单据：purchaseOrder=采购订单，purchaseApplication=采购申请单，purchaseChange=采购变更单，productChange=变更管理，productBomInfo=BOM管理，salesDemand=备货申请单，poInstock=入库单，soOutstock=销售出库单，customerB2bChangeSeller=B2B客户表变更销售员，requisitionApplicationChange=要货申请变更单，ProductLogistics=物流产品信息，transferApplication=调拨申请单，supplier=供应商列表，poReturn=采购退货单，pilotApplication=试产量产单，soDeliveryNoticeChange=发货通知变更单，stocktakingTask=盘点任务，transferInfo=直接调拨单，poReceive=收货单，productDetail=产品管理，projectTask=任务列表，subcontractOrder=委外订单，purchasePrice=采购价目表，purchasePriceChange=采购调价表，soB2c=B2C销售订单，soInfo=B2B销售订单，firstMileDelivery=发货单，customerInfo=B2B客户列表，soChange=B2B销售变更单，deliveryPlan=第三方仓发货计划，requisitionApplication=要货申请，soPrice=销售价目表，soPriceChange=销售调价表，transferIn=分步式调入单，transferOut=分步式调出单最大长度不能超过30位")
+        @NotBlank(message = "配置单据不能为空")
+        @Size(max = 30, message = "配置单据最大长度不能超过30位")
         private String bussinessKey;
 
         /**
