281b363d5718d50d4797b424bdd3319a28ff3c9a
liuruipeng <arthur@ulanzi.cn>
2025-05-27 15:53:16 +0800

    feat:【迭代1.39】退货入库导入新增类型字段

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoReturnInstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoReturnInstockServiceImpl.java
@@ -1869,7 +1869,7 @@ public class SoReturnInstockServiceImpl extends SuperServiceImpl<SoReturnInstock
 
     @Override
     public void downloadTemplate(HttpServletResponse response) {
-        String path = "classpath:excel/soReturnInstockTemplate.xlsx";
+        String path = "excel/soReturnInstockTemplate.xlsx";
         String excelName = "template.xlsx";
         ResourceLoader resourceLoader = new DefaultResourceLoader();
         try {
@@ -2008,6 +2008,7 @@ public class SoReturnInstockServiceImpl extends SuperServiceImpl<SoReturnInstock
                 } else {
                     addDetail.setWarehouseId(warehouseEntity.getId());
                 }
+                addDetail.setReturnTypeDict(ReturnTypeEnum.getCode(soReturnStockImportExcelDTO.getReturnType()));
                 //仓位信息
                 WarehouseLocationEntity warehouseLocationEntity = warehouseLocationMap.get(CharSequenceUtil.format("{}-{}",ObjectUtil.isEmpty(warehouseEntity) ? "" : warehouseEntity.getId(), soReturnStockImportExcelDTO.getWarehouseLocationName()));
                 if (ObjectUtil.isEmpty(warehouseLocationEntity) && CharSequenceUtil.isNotBlank(soReturnStockImportExcelDTO.getWarehouseLocationName())) {
