17264e24a4088c6f573d6e541622c50fb02407a9
jack chen <jackchen@ulanzi.cn>
2025-05-29 16:00:14 +0800

    refactor(erp-server-tms): 移除重量分摊优化

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileWeightAllocationServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileWeightAllocationServiceImpl.java
@@ -241,14 +241,6 @@ public class FirstMileWeightAllocationServiceImpl extends SuperServiceImpl<First
                         .in(FirstMileWeightAllocationEntity::getSkuId, groupedBySku.keySet())
                         .eq(FirstMileWeightAllocationEntity::getLogisticsBillId, logisticsBillId)
                         .update();
-
-                //sku维度 费用重量分摊不存在则更新为未分摊
-                this.lambdaUpdate()
-                        .set(FirstMileWeightAllocationEntity::getCostAllocationStatus, CostAllocationStatusEnum.NOT.getCode())
-                        .set(FirstMileWeightAllocationEntity::getCalculateMonth, calculateMonth)
-                        .notIn(FirstMileWeightAllocationEntity::getSkuId, groupedBySku.keySet())
-                        .eq(FirstMileWeightAllocationEntity::getLogisticsBillId, logisticsBillId)
-                        .update();
             }
         }
     }
