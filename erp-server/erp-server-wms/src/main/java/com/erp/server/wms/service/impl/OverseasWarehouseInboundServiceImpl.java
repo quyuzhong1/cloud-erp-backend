00cbf05fff95f29b1cca4cbe1f8850c932c536ab
liuruipeng <arthur@ulanzi.cn>
2025-05-30 14:45:05 +0800

    feat:【极风】入库单问题修复

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasWarehouseInboundServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasWarehouseInboundServiceImpl.java
@@ -246,7 +246,7 @@ public class OverseasWarehouseInboundServiceImpl extends SuperServiceImpl<Overse
         // 装箱信息item
         List<ThirdWarehouseCreateInboundReq.Item> itemList = new LinkedList<>();
         for (WmsCartonSpecDTO.PackingItemDTO itemDTO : itemDTOList) {
-            FirstMileDeliveryDetailEntity  firstMileDeliveryDetailEntity = deliveryDetailEntityList.stream().filter(v->v.getSkuId().equals(itemDTO.getSkuId())).findFirst().orElse(null);
+            FirstMileDeliveryDetailEntity  firstMileDeliveryDetailEntity = deliveryDetailEntityList.stream().filter(v->v.getSkuId().equals(itemDTO.getSkuId()) && v.getPlatformSkuNo().equals(itemDTO.getPlatformSkuNo())).findFirst().orElse(null);
             if (null == firstMileDeliveryDetailEntity) {
                 String msg = CharSequenceUtil.format("海外仓入库单明细中找不到skuId为【{}】的明细", itemDTO.getSkuId());
                 throw new ServiceException(msg);
