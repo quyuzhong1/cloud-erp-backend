376a37d426c071ca1311650c71f3eeed747e63ae
jack chen <jackchen@ulanzi.cn>
2025-05-30 10:24:03 +0800

    feat(workflow): 审批同步记录添加通知节点字段并实现相关功能

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ApproveSyncRecordServiceImpl.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ApproveSyncRecordServiceImpl.java
@@ -17,6 +17,7 @@ import com.erp.model.sys.enums.ThirdNoticePushRecordStatusEnum;
 import com.erp.model.workflow.entity.ApproveSyncRecordEntity;
 import com.erp.model.workflow.enums.ApproveSyncRecordNoticeTypeEnum;
 import com.erp.model.workflow.enums.ApproveSyncRecordStatusEnum;
+import com.erp.model.workflow.enums.CfgApproveNoticeNoticeTypeEnum;
 import com.erp.model.workflow.enums.CfgApproveSyncSyncPlatformEnum;
 import com.erp.rpc.file.feign.DownloadTaskFeign;
 import com.erp.server.workflow.mapper.ApproveSyncRecordMapper;
@@ -86,6 +87,8 @@ public class ApproveSyncRecordServiceImpl extends SuperServiceImpl<ApproveSyncRe
             record.setNoticeMethodName(CfgApproveSyncSyncPlatformEnum.getName(record.getNoticeMethod()));
 
             record.setStatusName(ApproveSyncRecordStatusEnum.getName(record.getStatus()));
+
+            record.setNoticeNodeName(CfgApproveNoticeNoticeTypeEnum.getName(record.getNoticeNode()));
         }
     }
 
