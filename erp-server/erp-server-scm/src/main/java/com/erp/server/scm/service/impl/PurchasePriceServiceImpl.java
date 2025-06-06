86ca6ab1000489656511e859b9e8a22fd12be5ef
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-06-06 18:43:04 +0800

    采购金额审核通过后增加采购组织和sku关联关系

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchasePriceServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchasePriceServiceImpl.java
@@ -480,8 +480,6 @@ public class PurchasePriceServiceImpl extends SuperServiceImpl<PurchasePriceMapp
         }
         //调用审核流程
         approveProcess(entity, type, comment, isNeedProcess);
-        //记录sku与采购组织关系
-        purchaseSkuOrgRefService.addByPurchasePrice(entity);
         //添加日志
         moduleOperateLogService.addModuleOperateLog(String.format("审核【%s】了一个采购价目【%s】", ApproveTypeEnum.getName(type), entity.getCode()).concat(StringUtils.isNotBlank(comment) ? String.format(",意见：%s", comment) : ""), ModuleTypeEnum.PURCHASE_PRICE.getCode(), entity.getId(), "审核操作");
         return BatchResultDTO.success(entity.getId(), entity.getCode(), "操作成功");
@@ -711,6 +709,8 @@ public class PurchasePriceServiceImpl extends SuperServiceImpl<PurchasePriceMapp
                     obj.setApproveUserName("");
                 }
                 obj.setApproveStatus(statusEnum);
+                //记录sku与采购组织关系
+                purchaseSkuOrgRefService.addByPurchasePrice(obj);
             });
             return this.updateBatchById(list);
         }
