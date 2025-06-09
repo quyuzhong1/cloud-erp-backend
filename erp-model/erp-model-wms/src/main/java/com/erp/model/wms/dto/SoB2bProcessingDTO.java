32d25649b7f58b735bf82d19ca7be0ea2ee3bad1
Will <wangwei@ulanzi.cn>
2025-06-09 17:54:27 +0800

    fix,合并处理

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoB2bProcessingDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SoB2bProcessingDTO.java
@@ -525,10 +525,6 @@ public class SoB2bProcessingDTO implements Serializable {
          * 来源id
          */
         private String sourceDetailId;
-        /**
-         * 来源类型
-         */
-        private String sourceType;
     }
 
     /**
