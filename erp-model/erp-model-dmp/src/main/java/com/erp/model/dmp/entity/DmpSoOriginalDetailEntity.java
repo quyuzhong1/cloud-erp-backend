981aa3362ba6e8fec0d8a78a8eb7c65e9189103c
shukai <shukai@ulanzi.cn>
2025-06-10 14:47:36 +0800

    add:还原，新加分支修改

--- erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpSoOriginalDetailEntity.java
+++ erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpSoOriginalDetailEntity.java
@@ -47,11 +47,6 @@ public class DmpSoOriginalDetailEntity extends BaseEntity<DmpSoOriginalDetailEnt
     @TableField("third_detail_id")
     private String thirdDetailId;
     /**
-     * 平台原始详情id
-     */
-    @TableField("platform_detail_id")
-    private String platformDetailId = "";
-    /**
     * 状态
     */
     @TableField("status")
