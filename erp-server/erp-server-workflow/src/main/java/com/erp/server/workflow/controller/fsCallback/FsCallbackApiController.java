f6b9fcaa7023db98b05b11256766e5343c5dc38e
jack chen <jackchen@ulanzi.cn>
2025-06-10 21:06:25 +0800

    test(workflow): 修改审批回调测试接口参数

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
@@ -1,27 +1,14 @@
 package com.erp.server.workflow.controller.fsCallback;
 
-import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson.JSON;
-import com.common.business.wrapper.FeignQuery;
 import com.common.core.controller.vo.ApiResult;
-import com.erp.model.sys.dto.OpenApiInputDTO;
-import com.erp.model.sys.dto.OpenApiReqDTO;
-import com.erp.model.sys.entity.SysRefererConfigEntity;
 import com.erp.model.workflow.dto.FsCallbackApiReqDTO;
 import com.erp.server.workflow.handler.CfgApproveSyncCallbackHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 @Slf4j
