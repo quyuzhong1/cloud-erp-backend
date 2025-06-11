2b1ca92e543e58c355985e33a4d58d8fa668956f
jason <jason.ulanzi.cn>
2025-06-11 18:17:46 +0800

    feat,流程配置自测V10

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ProcessManagementServiceImpl.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ProcessManagementServiceImpl.java
@@ -164,7 +164,8 @@ public class ProcessManagementServiceImpl extends SuperServiceImpl<ProcessManage
     @Override
     @Transactional(rollbackFor = Exception.class)
     public ProcessManagementDTO.StartResultDTO startProcessManagement(ProcessManagementDTO.StartDTO dto) {
-        String processDefinitionId = getProcessDefinitionId(dto);
+//        String processDefinitionId = getProcessDefinitionId(dto);
+        String processDefinitionId = "12E23076-0B9C-4393-97A4-85C411EB42DC";
         if (CharSequenceUtil.isBlank(processDefinitionId)) {
             // 业务无已启用的Erp流程配置
             return new ProcessManagementDTO.StartResultDTO(dto);
@@ -262,7 +263,8 @@ public class ProcessManagementServiceImpl extends SuperServiceImpl<ProcessManage
             CfgProcessDTO.StartDTO startDTO = BeanUtil.copyProperties(dto, CfgProcessDTO.StartDTO.class);
             startDTO.setRuleId(ruleId);
             startDTO.setRuleType(one.getType());
-            cfgProcessService.startThirdProcess(startDTO);
+            return null;
+//            cfgProcessService.startThirdProcess(startDTO);
         }
         // 绑定流程发起人
         identityService.setAuthenticatedUserId(dto.getUserId());
