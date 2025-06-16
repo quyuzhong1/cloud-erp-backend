32b35ff2cfaf80e7e4ebaf9925d0b98ebd539b3e
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-06-16 16:50:12 +0800

    开票规则，审核通过后才进行发票开具

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/PlatformOrderConsumerHandleServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/PlatformOrderConsumerHandleServiceImpl.java
@@ -238,7 +238,10 @@ public class PlatformOrderConsumerHandleServiceImpl implements PlatformOrderCons
             }
         }
         //生成nf-e发票
-        cfgInvoiceSettingDetailService.generateNfeInvoice (mainEntity,InvoiceNodeEnum.AFTER_AUDIT.getCode());
+        SoB2cEntity entity = CharSequenceUtil.isNotBlank(mainEntity.getId()) ? soB2cService.getById(mainEntity.getId()) : null;
+        if (Objects.nonNull(entity) && ApproveStatusEnum.APPROVE.getCode().equals(entity.getApproveStatus().getCode())){
+            cfgInvoiceSettingDetailService.generateNfeInvoice (mainEntity,InvoiceNodeEnum.AFTER_AUDIT.getCode());
+        }
     }
 
     /**
