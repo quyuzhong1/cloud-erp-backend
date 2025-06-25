02a70104af8a03a59f8652673dd6bc8ac1d7dd7f
Jim <zhoujingbo@ulanzi.cn>
2025-06-25 11:38:23 +0800

    UPDATE:Shopify退货单明细添加金额fix

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyReturnOrderDetailDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/ShopifyReturnOrderDetailDmpHandler.java
@@ -59,6 +59,7 @@ public class ShopifyReturnOrderDetailDmpHandler extends DmpInputDoNextDmpHandler
                 jsonObject.put("platformUpdateTime", shopifyRefund.getProcessedAt());
                 jsonObject.put("qty", refundLineItem.getQuantity());
                 jsonObject.put("amount", refundLineItem.getSubtotal());
+                jsonObject.put("totalAmount", refundLineItem.getSubtotal());
 
                 jsonObject.put("buyerUserId", shopifyRefund.getUserId());
                 jsonObject.put("reason", shopifyRefund.getNote());
