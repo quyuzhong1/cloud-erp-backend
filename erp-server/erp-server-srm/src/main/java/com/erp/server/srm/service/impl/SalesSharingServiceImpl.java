6544f0ec8679fcf11b14a836fc4b8baa0d1c67df
jack chen <jackchen@ulanzi.cn>
2025-06-27 10:45:15 +0800

    refactor(srm): 优化供应商信息和销量设置信息的获取逻辑 - 当供应商信息或销量设置信息为空时，不再抛出异常

--- erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/SalesSharingServiceImpl.java
+++ erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/SalesSharingServiceImpl.java
@@ -82,13 +82,13 @@ public class SalesSharingServiceImpl extends SuperServiceImpl<SalesSharingMapper
         //获取供应商id
         List<String> supplierIds = getSupplierIds();
         if(CollUtil.isEmpty(supplierIds)){
-            throw new ServiceException("供应商信息不存在");
+            return new PagingVO();
         }
 
         //获取供应商的销量设置信息
         List<CfgSupplierSalesEntity> cfgSupplierSalesList = getCfgSupplierSalesEntities(supplierIds);
         if(CollUtil.isEmpty(cfgSupplierSalesList)){
-            throw new ServiceException("供应商的销量设置信息不存在");
+            return new PagingVO();
         }
         CfgSupplierSalesEntity cfgSupplierSalesEntity = cfgSupplierSalesList.get(0);
 
