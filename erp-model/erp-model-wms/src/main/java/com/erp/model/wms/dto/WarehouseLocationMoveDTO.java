c4e1de81cc26486dd997dca4be55701a36c086bd
Will <wangwei@ulanzi.cn>
2025-06-30 11:26:51 +0800

    feat,仓位移动新增编辑仓库去掉非必填（还原）

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/WarehouseLocationMoveDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/WarehouseLocationMoveDTO.java
@@ -544,6 +544,7 @@ public class WarehouseLocationMoveDTO implements Serializable {
         /**
         * 仓库id
         */
+        @NotBlank(message = "仓库id不能为空")
         @Size(max = 19,message = "仓库id最大长度不能超过19位")
         private String warehouseId;
 
