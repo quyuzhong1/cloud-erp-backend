b859c863777ae5e57649ae6dc3f7b4fa3c1ce824
jack chen <jackchen@ulanzi.cn>
2025-07-03 17:22:16 +0800

    fix(scm): 优化供应商现场考察和证照管理的附件处理

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierCredentialServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SupplierCredentialServiceImpl.java
@@ -249,6 +249,9 @@ public class SupplierCredentialServiceImpl extends SuperServiceImpl<SupplierCred
                 String type = tableName.value();
                 List<AttachmentEntity> batchAttachmentList = new ArrayList<>(10);
                 for (int i = 0; i < attachmentUrlList.size(); i++) {
+                    if(!add.contains(attachmentUrlList.get(i))){
+                        continue;
+                    }
                     AttachmentEntity addAttachment = new AttachmentEntity();
                     addAttachment.setAttachUrl(attachmentUrlList.get(i));
                     addAttachment.setAttachName(attachmentNameList.get(i));
