6bc149f3b18edf6982f181ca0f9690cd2f31f745
jack chen <jackchen@ulanzi.cn>
2025-07-07 19:03:04 +0800

    Revert "fix(erp-server): 优化产品包装信息验证逻辑"
    This reverts commit 446383075abc9759273bd9461a35cae8f6fa3fdc.

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/ProductDetailServiceImpl.java
@@ -5189,7 +5189,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
             ProductKeyDTO productKey = productDetailMapper.getProductKey(productBy.getId());
             //旧包装信息
             ProductPackEntity oldPackEntity = productPackService.getById(productKey.getPackId());
-            if(StringUtils.isNotBlank(dto.getBoxLength()) || StringUtils.isNotBlank(dto.getProductLength())){
+            if(!(StringUtils.isBlank(dto.getBoxLength()) && StringUtils.isBlank(dto.getProductLength()))){
                 BigDecimal boxLength = MathUtil.valueOf(dto.getBoxLength());
                 if(StringUtils.isBlank(dto.getBoxLength())){
                     boxLength = oldPackEntity.getBoxLength();
@@ -5202,7 +5202,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
                     errorMsgList.add(ApiError.ERROR_LENGTH_BOX_LITTER_THAN_PRODUCT.msg);
                 }
             }
-            if(StringUtils.isNotBlank(dto.getBoxWidth()) || StringUtils.isNotBlank(dto.getProductWidth())){
+            if(!(StringUtils.isBlank(dto.getBoxWidth()) && StringUtils.isBlank(dto.getProductWidth()))){
                 BigDecimal boxWidth = MathUtil.valueOf(dto.getBoxWidth());
                 if(StringUtils.isBlank(dto.getBoxWidth())){
                     boxWidth = oldPackEntity.getBoxWidth();
@@ -5215,7 +5215,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
                     errorMsgList.add(ApiError.ERROR_WIDTH_BOX_LITTER_THAN_PRODUCT.msg);
                 }
             }
-            if(StringUtils.isNotBlank(dto.getBoxHeight()) || StringUtils.isNotBlank(dto.getProductHeight())){
+            if(!(StringUtils.isBlank(dto.getBoxHeight()) && StringUtils.isBlank(dto.getProductHeight()))){
                 BigDecimal boxHeight = MathUtil.valueOf(dto.getBoxHeight());
                 if(StringUtils.isBlank(dto.getBoxHeight())){
                     boxHeight = oldPackEntity.getBoxHeight();
@@ -5228,7 +5228,7 @@ public class ProductDetailServiceImpl extends ServiceImpl<ProductDetailMapper, P
                     errorMsgList.add(ApiError.ERROR_HEIGHT_BOX_LITTER_THAN_PRODUCT.msg);
                 }
             }
-            if(StringUtils.isNotBlank(dto.getGrossWeight()) || StringUtils.isNotBlank(dto.getNetWeight())){
+            if(!(StringUtils.isBlank(dto.getGrossWeight()) && StringUtils.isBlank(dto.getNetWeight()))){
                 BigDecimal grossWeight = MathUtil.valueOf(dto.getGrossWeight());
                 if(StringUtils.isBlank(dto.getGrossWeight())){
                     grossWeight = oldPackEntity.getGrossWeight();
