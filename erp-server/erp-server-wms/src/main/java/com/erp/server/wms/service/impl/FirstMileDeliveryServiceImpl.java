95cbc248a3ae803b34bfbac6ee012bef79eada5e
liuruipeng <arthur@ulanzi.cn>
2025-07-15 17:26:18 +0800

    feat:【速卖通三方仓】修复问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FirstMileDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FirstMileDeliveryServiceImpl.java
@@ -919,7 +919,10 @@ public class FirstMileDeliveryServiceImpl extends SuperServiceImpl<FirstMileDeli
                 OverseasProviderEntity providerEntity = overseasProviderWarehouseService.findPlatformByWarehouseId(entity.getDestWarehouseId());
 
                 //有对接海外仓API：调用入库单的提交审核，获取审核结果，审核通过后入库单状态为待签收；审核不通过为异常，操作日志记录失败原因，并显示在备注栏
-                if (CollectionUtils.isNotEmpty(overseasProviderWarehouseEntities) && Objects.nonNull(providerEntity) && !OmsPlatformEnum.JIFENG.getCode().equals(providerEntity.getCode())) {
+                if (CollectionUtils.isNotEmpty(overseasProviderWarehouseEntities)
+                        && Objects.nonNull(providerEntity)
+                        && !OmsPlatformEnum.JIFENG.getCode().equals(providerEntity.getCode())
+                        && !OmsPlatformEnum.CAI_NIAO.getCode().equals(providerEntity.getCode())) {
                     // 推送第三方发货单审核通过
                     ApiResult<String> resultInfo = overseasWarehouseInboundService.pullThirdOverseasPlatform(providerEntity, inboundEntity, detailEntityList, OverseasVerifyEnum.PASS.getCode());
                     if (200 != resultInfo.getCode()) {
