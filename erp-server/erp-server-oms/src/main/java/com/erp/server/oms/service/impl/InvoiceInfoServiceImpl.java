c05bcf21cd9e1de6fac22564feb84c757d609cb2
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-07-16 10:26:37 +0800

    nfe开票增加状态和性质校验

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/InvoiceInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/InvoiceInfoServiceImpl.java
@@ -276,9 +276,11 @@ public class InvoiceInfoServiceImpl extends SuperServiceImpl<InvoiceInfoMapper, 
         }
         //开票中不再生成
         SoB2cEntity finalSoB2cEntity = soB2cEntity;
-        InvoiceInfoEntity existInvoiceInfoEntity = existList.stream().filter(e -> e.getSoId().equals(finalSoB2cEntity.getId()) && e.getStatus().equals(InvoiceInfoStatusEnum.INVOICING.getCode())).findFirst().orElse(null);
+        InvoiceInfoEntity existInvoiceInfoEntity = existList.stream().filter(e -> e.getSoId().equals(finalSoB2cEntity.getId())
+                && InvoiceNatureEnum.ORDINARY.getCode().equals(e.getInvoiceNature())
+                && (e.getStatus().equals(InvoiceInfoStatusEnum.INVOICING.getCode()) || e.getStatus().equals(InvoiceInfoStatusEnum.INVOICE_SUCCESS.getCode()))).findFirst().orElse(null);
         if(Objects.nonNull(existInvoiceInfoEntity)){
-            return BatchResultDTO.fail(soB2cEntity.getId(), soB2cEntity.getCode(), "已存在开票中的发票");
+            return BatchResultDTO.fail(soB2cEntity.getId(), soB2cEntity.getCode(), CharSequenceUtil.format("发票性质为【{}】已存在开票中/开票成功的发票不生成新的开票任务",InvoiceNatureEnum.ORDINARY.getName()));
         }
         InvoiceInfoEntity invoiceInfoEntity = buildNfeInvoiceEntity(soB2cEntity);
         List<InvoiceDetailEntity> detailEntityList = buildInvoiceDetail(soB2cDetailEntityList, invoiceInfoEntity,skuNameMap);
