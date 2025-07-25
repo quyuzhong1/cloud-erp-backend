4610d2283c7e307cf750148858819b7cb6aa5b3a
shukai <shukai@ulanzi.cn>
2025-07-25 14:55:02 +0800

    add:生成输出任务

--- erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpCfgOutputEntity.java
+++ erp-model/erp-model-dmp/src/main/java/com/erp/model/dmp/entity/DmpCfgOutputEntity.java
@@ -63,8 +63,29 @@ public class DmpCfgOutputEntity extends BaseEntity<DmpCfgOutputEntity> {
     /**
      * 输出处理类
      */
-     @TableField("output_class")
-     private String outputClass;
+    @TableField("output_class")
+    private String outputClass;
+     
+    /**
+     * 应用id
+     */
+    @TableField("app_id")
+    private String appId;
+     /**
+     * 流程编号
+     */
+    @TableField("flow_code")
+    private String flowCode;
+     /**
+     * 流程名称
+     */
+    @TableField("flow_name")
+    private String flowName;   
+    /**
+     * 执行系统：DmpCfgInputExecSystemEnum 枚举
+     */
+    @TableField("exec_system")
+    private String execSystem;
 
     public static final String SYSTEM_ID = "system_id";
 
