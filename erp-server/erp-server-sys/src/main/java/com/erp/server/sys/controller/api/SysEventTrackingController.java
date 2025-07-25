214123661acd799aa7b2f8b5f38b00dcc2abf97c
wuhaotian <wuhaotian@ulanzi.cn>
2025-07-25 09:21:58 +0800

    feat,日志注解补全,修改

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/api/SysEventTrackingController.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/api/SysEventTrackingController.java
@@ -1,9 +1,7 @@
 package com.erp.server.sys.controller.api;
 
 
-import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import com.alibaba.fastjson.JSON;
 import com.common.business.dto.base.BaseResultDTO;
 import com.common.core.anno.LogSystemModule;
 import com.common.core.controller.BaseController;
@@ -12,12 +10,13 @@ import com.erp.model.sys.dto.SysEventTrackingDTO;
 import com.erp.server.sys.service.SysEventTrackingService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 /**
  * 前端埋点事件记录
