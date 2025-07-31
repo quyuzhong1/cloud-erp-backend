5b07f7cb90886a6dbbd4432badc36304682c3784
liuruipeng <arthur@ulanzi.cn>
2025-07-31 09:21:08 +0800

    feat:【weishi三方仓】添加状态日志

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/ThirdWarehouseDeliveryFeignController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/feign/ThirdWarehouseDeliveryFeignController.java
@@ -9,12 +9,15 @@ import com.common.core.anno.LogSystemModule;
 import com.common.core.controller.BaseController;
 import com.common.core.controller.vo.ApiResult;
 import com.common.message.constant.RedisKeyConstant;
+import com.erp.model.scm.enums.ModuleTypeEnum;
 import com.erp.model.sys.openapi.DimensionalWeightDTO;
 import com.erp.model.wms.dto.SoB2cDeliveryDTO;
 import com.erp.model.wms.entity.SoB2cDeliveryDetailEntity;
 import com.erp.model.wms.entity.SoB2cDeliveryEntity;
 import com.erp.model.wms.entity.ThirdWarehouseDeliveryEntity;
 import com.erp.model.wms.enums.CfgRuleOutEnum;
+import com.erp.model.wms.enums.SoB2cWarehouseDeliveryStatusEnum;
+import com.erp.server.wms.service.OperateLogService;
 import com.erp.server.wms.service.SoB2cDeliveryDetailService;
 import com.erp.server.wms.service.SoB2cDeliveryService;
 import com.erp.server.wms.service.ThirdWarehouseDeliveryService;
@@ -34,6 +37,8 @@ import java.util.List;
 @LogSystemModule("三方仓发货")
 @RequestMapping("/feign/thirdWarehouseDelivery")
 public class ThirdWarehouseDeliveryFeignController extends BaseController {
+    @Resource
+    private OperateLogService operateLogService;
 
     @Resource
     private ThirdWarehouseDeliveryService thirdWarehouseDeliveryService;
@@ -50,6 +55,7 @@ public class ThirdWarehouseDeliveryFeignController extends BaseController {
 
     @PostMapping("/update")
     public boolean update(@RequestBody ThirdWarehouseDeliveryEntity entity) {
+        operateLogService.addModuleOperateLog(StrUtil.format("状态变更为{}", SoB2cWarehouseDeliveryStatusEnum.getName(entity.getStatus())), ModuleTypeEnum.THIRD_WAREHOUSE_DELIVERY.getCode(),entity.getId(), "状态变更");
        return thirdWarehouseDeliveryService.updateById(entity);
     }
 }
