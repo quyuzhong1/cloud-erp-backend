367c3ccc7d79be80e690b60be9c4458dc9549c2a
jack chen <jackchen@ulanzi.cn>
2025-08-05 19:11:19 +0800

    fix(erp-server): 优化产品详情处理逻辑

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputAmzProductDetailDmpHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/dmp/DmpInputAmzProductDetailDmpHandler.java
@@ -138,7 +138,7 @@ public class DmpInputAmzProductDetailDmpHandler extends DmpInputDoChildDmpHandle
             }
 
             String listingMongoDataReportId = listingMongoDataItem.getOrDefault(REPORT_ID, "").toString();
-            if(StringUtils.isNotBlank(listingMongoDataReportId) && StringUtils.isNotBlank(reportId) && !listingMongoDataReportId.equalsIgnoreCase(reportId)){
+            if(StringUtils.isNotBlank(reportId) && !listingMongoDataReportId.equalsIgnoreCase(reportId)){
                 //标记为删除状态
                 listingMongoDataItem.put("status", "Delete");
             }
