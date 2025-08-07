8bdb14cb9dbf2f486983c848f7d797c807b90509
Will <wangwei@ulanzi.cn>
2025-08-07 16:23:00 +0800

    feat，代码合并调整

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/KingdeeOperatorRefPostServiceImpl.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/KingdeeOperatorRefPostServiceImpl.java
@@ -343,13 +343,8 @@ public class KingdeeOperatorRefPostServiceImpl extends SuperServiceImpl<KingdeeO
             }else {
                 item.setIsMyState(0);
             }
-            Integer deleteState = item.getDeleteState();
             Integer userState = item.getUserState();
-            if (MathUtil.ZERO.equals(deleteState) || MathUtil.ZERO.equals(userState)) {
-                item.setDisabled(Boolean.TRUE);
-            } else {
-                item.setDisabled(Boolean.FALSE);
-            }
+            item.setDisabled(MathUtil.ZERO.equals(userState));
             //部门为空时设置为时效
             if (CharSequenceUtil.isBlank(item.getDepartmentId()) || CharSequenceUtil.isBlank(item.getDepartmentName())){
                 item.setDisabled(Boolean.TRUE);
