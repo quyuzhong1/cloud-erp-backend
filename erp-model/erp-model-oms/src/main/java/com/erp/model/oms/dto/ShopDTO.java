d19542598ce965c11779c4d90f2a0626d57367ed
wuhaotian <wuhaotian@ulanzi.cn>
2025-08-08 15:50:25 +0800

    feat,自测调整

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ShopDTO.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/ShopDTO.java
@@ -328,6 +328,11 @@ public class ShopDTO implements Serializable {
          * VOEC税号
          */
         private String voecTaxNo;
+
+        /**
+         * EORI税号
+         */
+        private String eoriTaxNo;
         
         /**
          * 结算币别 http://172.16.100.11:3002/project/36/interface/api/8485
