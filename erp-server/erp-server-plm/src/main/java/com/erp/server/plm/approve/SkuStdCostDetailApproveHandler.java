1984a25fca56be076b04b3ed901aa582d7fb4db2
Jim <zhoujingbo@ulanzi.cn>
2025-08-12 10:26:56 +0800

    ADD:sku标准成本addComment冲突fix

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/approve/SkuStdCostDetailApproveHandler.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/approve/SkuStdCostDetailApproveHandler.java
@@ -1,5 +1,7 @@
 package com.erp.server.plm.approve;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.common.business.annotation.ApproveBusinessKey;
 import com.common.business.dto.ApproveDTO;
@@ -11,18 +13,29 @@ import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
 import com.erp.model.plm.entity.ProductDetailEntity;
 import com.erp.model.plm.entity.SkuStdCostDetailEntity;
+import com.erp.model.plm.entity.SysLogEntity;
+import com.erp.model.scm.enums.ModuleTypeEnum;
+import com.erp.model.wms.dto.OperateLogDTO;
 import com.erp.server.plm.service.ProductDetailService;
 import com.erp.server.plm.service.SkuStdCostDetailService;
+import com.erp.server.plm.service.SysLogService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Component
 @ApproveBusinessKey(SourceTypeEnum.SKU_STD_COST_DETAIL)
 public class SkuStdCostDetailApproveHandler extends AbstractApproveHandler {
 
     @Resource
     private SkuStdCostDetailService skuStdCostDetailService;
+    @Resource
+    private SysLogService sysLogService;
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
@@ -51,5 +64,25 @@ public class SkuStdCostDetailApproveHandler extends AbstractApproveHandler {
         approveOne.setId(dto.getBusinessId());
         approveOne.setVariablesMap(dto.getVariablesMap());
         return skuStdCostDetailService.approveEnd(approveOne,entity);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+        if (CollUtil.isEmpty(dto.getComments())) {
+            log.warn("无评论无需添加日志，businessKey = {},id={}",dto.getBusinessKey(),dto.getId());
+            return;
+        }
+        List<SysLogEntity> list = new ArrayList<>();
+        for (String comment : dto.getComments()) {
+            SysLogEntity sysLogEntity = new SysLogEntity();
+            sysLogEntity.setBusinessId(dto.getId());
+            sysLogEntity.setOperation("添加评论");
+            //添加评论日志
+            String operateContent = CharSequenceUtil.format("【{}】流程添加评论【{}】",dto.getApprovePlatformEnum().getName(), comment);
+            sysLogEntity.setContent(operateContent);
+            list.add(sysLogEntity);
+        }
+        sysLogService.addSysLogByBatchSave(list);
     }
 }
