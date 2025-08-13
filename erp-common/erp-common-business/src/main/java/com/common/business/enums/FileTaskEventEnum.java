6a52aca0c8779f75e32b5148132c54b8fd037da7
Jim <zhoujingbo@ulanzi.cn>
2025-08-13 10:52:56 +0800

    update:FileTaskEventEnumSKU标准成本冲突fix

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/FileTaskEventEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/FileTaskEventEnum.java
@@ -59,6 +59,8 @@ public enum FileTaskEventEnum implements EnumMessage {
     EXPORT_PLM_SKU("EXPORT_PLM_SKU", "产品管理导出","",""),
     IMPORT_PLM_SKU_IMAGES("IMPORT_PLM_SKU_IMAGES", "导入SKU图片","com.erp.rpc.plm.feign.ImportPlmFeign","productDetailImages"),
     EXPORT_PLM_PRODUCT_CUSTOMS("EXPORT_PLM_PRODUCT_CUSTOMS", "目的国清关导出","",""),
+    EXPORT_PLM_SKU_STD_COST("EXPORT_PLM_SKU_STD_COST", "SKU标准成本导出","",""),
+    IMPORT_PLM_SKU_STD_COST("IMPORT_PLM_SKU_STD_COST", "SKU标准成本导入","",""),
 
 
     //oms
