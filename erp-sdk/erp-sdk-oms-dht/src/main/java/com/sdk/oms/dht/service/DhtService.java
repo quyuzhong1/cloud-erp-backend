56e914260ec8ed9ba30d9a6215043caa132da146
liuruipeng <arthur@ulanzi.cn>
2025-08-14 17:20:25 +0800

    feat:【订货通】sdk提交

---
+++ erp-sdk/erp-sdk-oms-dht/src/main/java/com/sdk/oms/dht/service/DhtService.java
@@ -0,0 +1,17 @@
+package com.sdk.oms.dht.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 订货通
+ *
+ **/
+@Slf4j
+@Component
+public class DhtService {
+
+    public static void main(String[] args) {
+
+    }
+}
