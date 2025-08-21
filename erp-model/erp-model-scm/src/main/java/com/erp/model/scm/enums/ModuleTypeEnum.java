63fb5bd4a2e1b9d605e92bb6ec1fc75ea6f23e82
wuhaotian <wuhaotian@ulanzi.cn>
2025-08-21 17:37:58 +0800

    feat,日志枚举新增枚举项

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
@@ -160,6 +160,11 @@ public enum ModuleTypeEnum {
     SAMPLE_BORROW_INFO("", "样品借用单"),
     SAMPLE_SCRAP_INFO("", "样品报废单"),
     EXHIBITION_ORDER("", "展会订单"),
+
+    SAMPLE_RECIPIENT("134", "样品领用单"),
+    SAMPLE_RETURN("135", "样品退回单"),
+    SAMPLE_LEDGER("136", "样品台账"),
+    SAMPLE_LEDGER_INIT("137", "样品期初台账"),
     ;
 
 
