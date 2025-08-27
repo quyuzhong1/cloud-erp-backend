19b5cf8477e3c3ff2b8d83e63c4defa3acda7dc9
Will <wangwei@ulanzi.cn>
2025-08-27 10:09:31 +0800

    hotfix,虚拟仓库龄跳转

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualInventoryAgeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/VirtualInventoryAgeServiceImpl.java
@@ -277,7 +277,7 @@ public class VirtualInventoryAgeServiceImpl extends SuperServiceImpl<VirtualInve
         VirtualInventoryAgeDTO.FrameParamDTO params = dto.getParams();
         VirtualInventoryAgeDTO.HisInventoryAgeDetailParamDTO paramDTO = BeanMapperUtils.map(VirtualInventoryAgeDTO.HisInventoryAgeDetailParamDTO.class, params);
         paramDTO.setDaysList(daysList);
-        paramDTO.setDate(LocalDate.now().minusDays(1L));
+        paramDTO.setDate(dto.getParams().getDate());
         IPage<VirtualInventoryAgeDTO.HisInventoryAgeDetailDTO> pageData = this.baseMapper.hisInventoryAgeDetailPaging(dto.page(), paramDTO);
         handleHisInventoryAgeDetail(pageData.getRecords());
         return new PagingVO<>(pageData);
