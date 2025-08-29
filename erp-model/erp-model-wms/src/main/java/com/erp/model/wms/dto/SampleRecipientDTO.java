d5028acf45b338c26605976fd92fad74426d5514
wuhaotian <wuhaotian@ulanzi.cn>
2025-08-29 11:29:25 +0800

    feat,样品领用新增接口备注非必填

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SampleRecipientDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/SampleRecipientDTO.java
@@ -588,7 +588,6 @@ public class SampleRecipientDTO implements Serializable {
         /**
          * 备注
          */
-        @NotBlank(message = "备注不能为空")
         @Size(max = 200, message = "备注最大长度不能超过200位")
         private String remark;
 
