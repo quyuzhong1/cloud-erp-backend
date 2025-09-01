3f6d78f2a787541356c0171140957758ceec42cf
shukai <shukai@ulanzi.cn>
2025-09-01 09:23:57 +0800

    add:计算折扣相关分摊

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyOrderHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdyOrderHandler.java
@@ -770,9 +770,15 @@ public class DmpOutputSdyOrderHandler extends DmpOutputSdyBaseTaskHandler {
             	BigDecimal goods_transaction_amount = shudiyunB2cOrderDTO.getGoods_transaction_amount();
     			BigDecimal total_goods_transaction_amount = shudiyunB2cOrderDTO.getTotal_goods_transaction_amount();
     			if(BigDecimal.ZERO.compareTo(goods_transaction_amount) != 0 && BigDecimal.ZERO.compareTo(total_goods_transaction_amount) != 0) {
-    				shudiyunB2cOrderDTO.setGoods_discount_deduction_amount(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getDiscount_deduction_amount()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
-    				shudiyunB2cOrderDTO.setFreight(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTotal_freight()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
-    				shudiyunB2cOrderDTO.setGoods_taxation(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTaxation()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				if(shudiyunB2cOrderDTO.getDiscount_deduction_amount() != null) {
+    					shudiyunB2cOrderDTO.setGoods_discount_deduction_amount(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getDiscount_deduction_amount()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
+    				if(shudiyunB2cOrderDTO.getTotal_freight() != null) {
+    					shudiyunB2cOrderDTO.setFreight(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTotal_freight()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
+    				if(shudiyunB2cOrderDTO.getTaxation() != null) {
+    					shudiyunB2cOrderDTO.setGoods_taxation(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTaxation()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
     			}
             }
             shudiyunB2cOrderDTO.setDefaultValue();
--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdySoDeliveryHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/output/task/api/DmpOutputSdySoDeliveryHandler.java
@@ -1,6 +1,7 @@
 package com.erp.server.dmp.inout.handler.output.task.api;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -345,6 +346,20 @@ public class DmpOutputSdySoDeliveryHandler extends DmpOutputSdyBaseTaskHandler {
     	        // 部门名称
     	        shudiyunB2cOrderDTO.setDepartment_name(kingdeeDepartmentName);
     			
+    	        BigDecimal goods_transaction_amount = shudiyunB2cOrderDTO.getGoods_transaction_amount();
+    			BigDecimal total_goods_transaction_amount = shudiyunB2cOrderDTO.getTotal_goods_transaction_amount();
+    			if(BigDecimal.ZERO.compareTo(goods_transaction_amount) != 0 && BigDecimal.ZERO.compareTo(total_goods_transaction_amount) != 0) {
+    				if(shudiyunB2cOrderDTO.getDiscount_deduction_amount() != null) {
+    					shudiyunB2cOrderDTO.setGoods_discount_deduction_amount(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getDiscount_deduction_amount()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
+    				if(shudiyunB2cOrderDTO.getTotal_freight() != null) {
+    					shudiyunB2cOrderDTO.setFreight(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTotal_freight()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
+    				if(shudiyunB2cOrderDTO.getTaxation() != null) {
+    					shudiyunB2cOrderDTO.setGoods_taxation(goods_transaction_amount.multiply(shudiyunB2cOrderDTO.getTaxation()).divide(total_goods_transaction_amount , 4 , RoundingMode.HALF_UP));
+    				}
+    			}
+    	        
     	        shudiyunB2cOrderDTO.setDefaultValue();
     			result.put(detailId, shudiyunB2cOrderDTO);
     		}
