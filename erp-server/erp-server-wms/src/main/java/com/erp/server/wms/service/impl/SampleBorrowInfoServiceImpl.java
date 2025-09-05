3f9afe0954ac066586416776c2ea640c5816d37c
jack chen <jackchen@ulanzi.cn>
2025-09-05 16:02:17 +0800

    fix(wms): 优化用户和部门信息处理逻辑

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SampleBorrowInfoServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SampleBorrowInfoServiceImpl.java
@@ -1043,7 +1043,7 @@ public class SampleBorrowInfoServiceImpl extends SuperServiceImpl<SampleBorrowIn
         }
 
         List<FindUserDTO> users = sysUserFeign.getUserListByUserIds(Arrays.asList(borrowUserId, lendUserId));
-        if(CollUtil.isEmpty(users) || users.size() != 2){
+        if(CollUtil.isEmpty(users)){
             throw new ServiceException(ApiError.USER_NOT_EXIST);
         }
 
