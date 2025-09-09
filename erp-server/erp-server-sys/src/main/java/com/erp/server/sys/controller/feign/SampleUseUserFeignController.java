7a5ccd48960dd8564dcd57e33c7786b7ff651df1
wuhaotian <wuhaotian@ulanzi.cn>
2025-09-09 15:56:14 +0800

    feat,样品领用导入修改

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/feign/SampleUseUserFeignController.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/controller/feign/SampleUseUserFeignController.java
@@ -2,6 +2,8 @@ package com.erp.server.sys.controller.feign;
 
 import com.common.business.dto.base.BaseIdDTO;
 import com.common.core.controller.BaseController;
+import com.common.core.controller.vo.ApiResult;
+import com.erp.model.sys.dto.SampleUseUserDTO;
 import com.erp.server.sys.service.DictSampleUseUserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,4 +31,18 @@ public class SampleUseUserFeignController extends BaseController {
         List<BaseIdDTO> list = sampleUseUserService.getByIds(ids);
         return list;
     }
+    /**
+     * 根据名称列表批量查询示例用户
+     *
+     * @param nameList 名称列表
+     * @return com.common.core.controller.vo.ApiResult
+     * @author Lambda
+     * @date 2025-01-27 16:29
+     */
+    @PostMapping("/getListByNameList")
+    public ApiResult<List<SampleUseUserDTO.ViewDTO>> getListByNameList(@RequestBody List<String> nameList) {
+        List<SampleUseUserDTO.ViewDTO> list = sampleUseUserService.getListByNameList(nameList);
+        return success(list);
+    }
+
 }
