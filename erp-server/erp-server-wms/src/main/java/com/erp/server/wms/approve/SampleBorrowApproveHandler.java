ffa8c9594409699ed0af0cf3eddf924ae153efbf
jack chen <jackchen@ulanzi.cn>
2025-09-11 10:29:16 +0800

    feat(审批): 添加评论日志记录功能
    - 在 ExhibitionOrderApproveHandler、SampleBorrowApproveHandler、SampleReturnApproveHandler 和 SampleScrapApproveHandler 中实现 addComment 方法
    - 用于在审批流程中添加评论时记录操作日志
    - 优化了日志记录逻辑，避免记录空评论

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleBorrowApproveHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleBorrowApproveHandler.java
@@ -1,5 +1,7 @@
 package com.erp.server.wms.approve;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.common.business.annotation.ApproveBusinessKey;
 import com.common.business.dto.ApproveDTO;
@@ -9,21 +11,31 @@ import com.common.business.enums.SourceTypeEnum;
 import com.common.business.handler.AbstractApproveHandler;
 import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
+import com.erp.model.scm.enums.ModuleTypeEnum;
+import com.erp.model.wms.dto.OperateLogDTO;
 import com.erp.model.wms.entity.SampleBorrowInfoEntity;
 import com.erp.model.wms.entity.TransferOutEntity;
+import com.erp.server.wms.service.OperateLogService;
 import com.erp.server.wms.service.SampleBorrowInfoService;
 import com.erp.server.wms.service.TransferOutService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+@Slf4j
 @Component
 @ApproveBusinessKey(SourceTypeEnum.SAMPLE_BORROW_INFO)
 public class SampleBorrowApproveHandler extends AbstractApproveHandler {
 
     @Resource
     private SampleBorrowInfoService sampleBorrowInfoService;
+
+    @Resource
+    private OperateLogService operateLogService;
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
@@ -43,5 +55,16 @@ public class SampleBorrowApproveHandler extends AbstractApproveHandler {
         ApproveOneDTO approveOneDTO = new ApproveOneDTO();
         approveOneDTO.setType(dto.getApproveStatus().getStatus());
         return sampleBorrowInfoService.approveEnd(approveOneDTO,entity);
+    }
+    @Override
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+        if (CollUtil.isEmpty(dto.getComments())) {
+            log.warn("无评论无需添加日志，businessKey = {},id={}",dto.getBusinessKey(),dto.getId());
+            return;
+        }
+        List<com.erp.model.wms.dto.OperateLogDTO.AddModuleOperateLogDTO> operateLogList = new ArrayList<>();
+        dto.getComments().stream().map(obj -> new OperateLogDTO.AddModuleOperateLogDTO(CharSequenceUtil.format("【{}】流程添加评论【{}】",dto.getApprovePlatformEnum().getName(),obj), ModuleTypeEnum.SAMPLE_BORROW_INFO.getCode(), dto.getId(), "添加评论"))
+                .forEach(operateLogList::add);
+        operateLogService.batchAddModuleOperateLog(operateLogList);
     }
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleReturnApproveHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleReturnApproveHandler.java
@@ -1,5 +1,7 @@
 package com.erp.server.wms.approve;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import com.common.business.annotation.ApproveBusinessKey;
 import com.common.business.dto.ApproveDTO;
 import com.common.business.dto.base.ApproveOneDTO;
@@ -7,17 +9,27 @@ import com.common.business.dto.base.BatchResultDTO;
 import com.common.business.enums.SourceTypeEnum;
 import com.common.business.handler.AbstractApproveHandler;
 import com.common.core.exception.ServiceException;
+import com.erp.model.scm.enums.ModuleTypeEnum;
+import com.erp.model.wms.dto.OperateLogDTO;
 import com.erp.model.wms.entity.SampleReturnInfoEntity;
+import com.erp.server.wms.service.OperateLogService;
 import com.erp.server.wms.service.SampleReturnInfoService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Component
 @ApproveBusinessKey(SourceTypeEnum.SAMPLE_RETURN_INFO)
 public class SampleReturnApproveHandler extends AbstractApproveHandler {
 
     @Resource
     private SampleReturnInfoService sampleReturnInfoService;
+
+    @Resource
+    private OperateLogService operateLogService;
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
@@ -37,5 +49,16 @@ public class SampleReturnApproveHandler extends AbstractApproveHandler {
         ApproveOneDTO approveOneDTO = new ApproveOneDTO();
         approveOneDTO.setType(dto.getApproveStatus().getStatus());
         return sampleReturnInfoService.approveEnd(approveOneDTO,entity);
+    }
+    @Override
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+        if (CollUtil.isEmpty(dto.getComments())) {
+            log.warn("无评论无需添加日志，businessKey = {},id={}",dto.getBusinessKey(),dto.getId());
+            return;
+        }
+        List<com.erp.model.wms.dto.OperateLogDTO.AddModuleOperateLogDTO> operateLogList = new ArrayList<>();
+        dto.getComments().stream().map(obj -> new OperateLogDTO.AddModuleOperateLogDTO(CharSequenceUtil.format("【{}】流程添加评论【{}】",dto.getApprovePlatformEnum().getName(),obj), ModuleTypeEnum.SAMPLE_RETURN_INFO.getCode(), dto.getId(), "添加评论"))
+                .forEach(operateLogList::add);
+        operateLogService.batchAddModuleOperateLog(operateLogList);
     }
 }
