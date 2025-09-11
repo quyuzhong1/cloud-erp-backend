a3040cbb4441acd75ea826229794031f93f4c8d3
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-09-11 12:15:48 +0800

    销售出库单记录客户PO号并同步金蝶

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
@@ -885,6 +885,7 @@ public class SoOutstockDetailServiceImpl extends SuperServiceImpl<SoOutstockDeta
             }
             detailEntity.setAllAmountLocalCurrency(allAmountLocalCurrency);
             detailEntity.setRemark(soDetailEntity.getRemark());
+            detailEntity.setCustomerPO(soDetailEntity.getCustomerPO());
         }
     }
 
@@ -902,7 +903,6 @@ public class SoOutstockDetailServiceImpl extends SuperServiceImpl<SoOutstockDeta
         if(StringUtils.isBlank(entity.getSoId())){
             return;
         }
-        List<String> soDetailIdList = detailList.stream().map(SoOutstockDetailEntity::getSoDetailId).collect(Collectors.toList());
         List<String> outSkuIds = detailList.stream().map(SoOutstockDetailEntity::getSkuId).collect(Collectors.toList());
         SoB2cEntity soB2cEntity = soB2cFeign.getById(entity.getSoId());
         if(Objects.isNull(soB2cEntity)){
