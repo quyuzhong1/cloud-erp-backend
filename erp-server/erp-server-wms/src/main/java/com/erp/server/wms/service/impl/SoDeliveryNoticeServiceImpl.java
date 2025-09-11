43241347b3b2394de2475c1a0af6bde643c24856
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-09-11 15:43:02 +0800

    B2B订单修改销售组织限制

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoDeliveryNoticeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoDeliveryNoticeServiceImpl.java
@@ -2290,6 +2290,7 @@ public class SoDeliveryNoticeServiceImpl extends SuperServiceImpl<SoDeliveryNoti
                     .set(SoDeliveryNoticeEntity::getSellerId, soInfoEntity.getSellerId())
                     .set(SoDeliveryNoticeEntity::getSellerName, soInfoEntity.getSellerName())
                     .eq(SoDeliveryNoticeEntity::getSourceId, soInfoEntity.getId())
+                    .eq(SoDeliveryNoticeEntity::getSourceType, SourceTypeEnum.SO_INFO.getCode())
                     .update();
         }
     }
