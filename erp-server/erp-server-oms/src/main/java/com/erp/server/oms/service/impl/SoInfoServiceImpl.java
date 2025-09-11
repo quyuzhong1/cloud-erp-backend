5261f10eb74e3bd0a5cb4b9ab5ea283a03b2b5e6
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-09-11 15:03:52 +0800

    B2B订单修改销售组织限制

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -1349,6 +1349,18 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
         if (Objects.isNull(soInfo)) {
             throw new ServiceException(ApiError.ERROR_92016);
         }
+        Boolean needUpdateDeliveryNotice = false;
+        if (!soInfo.getSalesOrgId().equals(dto.getSalesOrgId()) || !soInfo.getSellerId().equals(dto.getSellerId()) || !soInfo.getSalesDeptId().equals(dto.getSalesDeptId())) {
+            //销售组织和销售部门销售员变更校验,是否存在已审核发货通知单
+            List<SoDeliveryNoticeEntity> soDeliveryNoticeEntities = soDeliveryNoticeFeign.listDeliveryNoticeBySoIds(Collections.singletonList(soInfo.getId()));
+            if (CollUtil.isNotEmpty(soDeliveryNoticeEntities)){
+                long count = soDeliveryNoticeEntities.stream().filter(e -> ApproveStatusEnum.APPROVE.getCode().equals(e.getApproveStatus())).count();
+                if(count > 0) {
+                    throw new ServiceException("下游发货单通知单已审核通过，不允许修改");
+                }
+                needUpdateDeliveryNotice = true;
+            }
+        }
         String customerId = dto.getCustomerId();
         if (StringUtils.isNotBlank(customerId)) {
             String oldCustomerId = soInfo.getCustomerId();
@@ -1435,7 +1447,9 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
         }
         Boolean updateResult = this.updateById(soInfo);
         if (updateResult) {
-
+            if (needUpdateDeliveryNotice){
+                soDeliveryNoticeFeign.updateSalesInfo(soInfo);
+            }
             // 保存附件
             TableName tableName = SoInfoEntity.class.getDeclaredAnnotation(TableName.class);
             omsAttachmentService.batchSave(dto.getAttachUrlList(), dto.getAttachNameList(), tableName.value(), id);
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoDeliveryNoticeService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/SoDeliveryNoticeService.java
@@ -6,6 +6,7 @@ import com.common.business.dto.base.PermissionsDTO;
 import com.common.business.service.SuperService;
 import com.common.business.vo.PagingVO;
 import com.erp.model.oms.dto.SoInfoDTO;
+import com.erp.model.oms.entity.SoInfoEntity;
 import com.erp.model.wms.dto.SoDeliveryNoticeDTO;
 import com.erp.model.wms.dto.SoOutstockDTO;
 import com.erp.model.wms.dto.WarehouseLocationMoveDTO;
@@ -332,4 +333,8 @@ public interface SoDeliveryNoticeService extends SuperService<SoDeliveryNoticeEn
      * @return Map<String, SoDeliveryNoticeEntity>
      */
     Map<String, SoDeliveryNoticeEntity> mapByIds(List<String> ids);
+
+    List<SoDeliveryNoticeEntity> listDeliveryNoticeBySoIds(List<String> soIds);
+
+    void updateSalesInfo(SoInfoEntity soInfoEntity);
 }
