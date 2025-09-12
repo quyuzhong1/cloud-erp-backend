9c442e39bb7d14c842eb160d76ba6c2981e7202f
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-09-12 11:12:52 +0800

    部分单据支持扫码录入SKU明细

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnDetailServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnDetailServiceImpl.java
@@ -14,6 +14,8 @@ import com.erp.model.oms.entity.SoDetailEntity;
 import com.erp.model.oms.entity.SoInfoEntity;
 import com.erp.model.oms.entity.SoReturnDetailEntity;
 import com.erp.model.oms.entity.SoReturnEntity;
+import com.erp.model.oms.enums.ListingMatchResultEnum;
+import com.erp.model.oms.enums.RuleTypeEnum;
 import com.erp.model.plm.dto.BomChildrenSkuDTO;
 import com.erp.model.plm.entity.ProductDetailEntity;
 import com.erp.model.scm.enums.ModuleTypeEnum;
@@ -436,6 +438,8 @@ public class SoReturnDetailServiceImpl extends SuperServiceImpl<SoReturnDetailMa
         SkuMappingDTO.SkuParamDTO skuParamDTO = new SkuMappingDTO.SkuParamDTO();
         skuParamDTO.setCutomerId(dto.getCustomerId());
         skuParamDTO.setPlatformSkuNoList(dto.getPlatformSkuNoList());
+        skuParamDTO.setType(RuleTypeEnum.CUSTOMER.getCode());
+        skuParamDTO.setMatchResult(ListingMatchResultEnum.TRUE.getCode());
         List<SkuMappingDTO.ProductSkuInfoDTO> productSkuInfoList = skuMappingService.listSkuBySkuNos(skuParamDTO);
         if(CollectionUtils.isEmpty(productSkuInfoList)){
             return new SoDetailDTO.ListAddDetailNoBomViewDTO();
