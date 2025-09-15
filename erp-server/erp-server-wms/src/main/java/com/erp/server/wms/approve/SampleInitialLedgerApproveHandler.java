cc15df5d33f3845ee0a0617c8ba42cf78a31846f
wuhaotian <wuhaotian@ulanzi.cn>
2025-09-15 15:55:24 +0800

    test,修复编译报错

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleInitialLedgerApproveHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleInitialLedgerApproveHandler.java
@@ -1,5 +1,7 @@
 package com.erp.server.wms.approve;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.common.business.annotation.ApproveBusinessKey;
 import com.common.business.dto.ApproveDTO;
@@ -9,24 +11,34 @@ import com.common.business.enums.SourceTypeEnum;
 import com.common.business.handler.AbstractApproveHandler;
 import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
+import com.erp.model.scm.enums.ModuleTypeEnum;
+import com.erp.model.wms.dto.OperateLogDTO;
 import com.erp.model.wms.entity.SampleInitialLedgerEntity;
+import com.erp.server.wms.service.OperateLogService;
 import com.erp.server.wms.service.SampleInitialLedgerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * 样品期初台账审核处理器
  * @author wuhaotian
  * @date 2025-09-15
  */
+@Slf4j
 @Component
 @ApproveBusinessKey(SourceTypeEnum.SAMPLE_LEDGER_INIT)
 public class SampleInitialLedgerApproveHandler extends AbstractApproveHandler {
 
     @Resource
     private SampleInitialLedgerService sampleInitialLedgerService;
+
+    @Resource
+    private OperateLogService operateLogService;
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
@@ -46,5 +58,17 @@ public class SampleInitialLedgerApproveHandler extends AbstractApproveHandler {
         ApproveOneDTO approveOneDTO = new ApproveOneDTO();
         approveOneDTO.setType(dto.getApproveStatus().getStatus());
         return sampleInitialLedgerService.approveEnd(approveOneDTO, entity);
+    }
+
+    @Override
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+        if (CollUtil.isEmpty(dto.getComments())) {
+            log.warn("无评论无需添加日志，businessKey = {},id={}",dto.getBusinessKey(),dto.getId());
+            return;
+        }
+        List<OperateLogDTO.AddModuleOperateLogDTO> operateLogList = new ArrayList<>();
+        dto.getComments().stream().map(obj -> new OperateLogDTO.AddModuleOperateLogDTO(CharSequenceUtil.format("【{}】流程添加评论【{}】",dto.getApprovePlatformEnum().getName(),obj), ModuleTypeEnum.SAMPLE_LEDGER_INIT.getCode(), dto.getId(), "添加评论"))
+                .forEach(operateLogList::add);
+        operateLogService.batchAddModuleOperateLog(operateLogList);
     }
 }
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleRecipientApproveHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SampleRecipientApproveHandler.java
@@ -1,5 +1,7 @@
 package com.erp.server.wms.approve;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
 import com.common.business.annotation.ApproveBusinessKey;
 import com.common.business.dto.ApproveDTO;
 import com.common.business.dto.base.ApproveOneDTO;
@@ -7,23 +9,33 @@ import com.common.business.dto.base.BatchResultDTO;
 import com.common.business.enums.SourceTypeEnum;
 import com.common.business.handler.AbstractApproveHandler;
 import com.common.core.exception.ServiceException;
+import com.erp.model.scm.enums.ModuleTypeEnum;
+import com.erp.model.wms.dto.OperateLogDTO;
 import com.erp.model.wms.entity.SampleRecipientEntity;
+import com.erp.server.wms.service.OperateLogService;
 import com.erp.server.wms.service.SampleRecipientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 样品领用单审核处理器
  * @author wuhaotian
  * @date 2025-09-15
  */
+@Slf4j
 @Component
 @ApproveBusinessKey(SourceTypeEnum.SAMPLE_RECIPIENT)
 public class SampleRecipientApproveHandler extends AbstractApproveHandler {
 
     @Resource
     private SampleRecipientService sampleRecipientService;
+
+    @Resource
+    private OperateLogService operateLogService;
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
@@ -43,5 +55,17 @@ public class SampleRecipientApproveHandler extends AbstractApproveHandler {
         ApproveOneDTO approveOneDTO = new ApproveOneDTO();
         approveOneDTO.setType(dto.getApproveStatus().getStatus());
         return sampleRecipientService.approveEnd(approveOneDTO, entity);
+    }
+
+    @Override
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+        if (CollUtil.isEmpty(dto.getComments())) {
+            log.warn("无评论无需添加日志，businessKey = {},id={}",dto.getBusinessKey(),dto.getId());
+            return;
+        }
+        List<OperateLogDTO.AddModuleOperateLogDTO> operateLogList = new ArrayList<>();
+        dto.getComments().stream().map(obj -> new OperateLogDTO.AddModuleOperateLogDTO(CharSequenceUtil.format("【{}】流程添加评论【{}】",dto.getApprovePlatformEnum().getName(),obj), ModuleTypeEnum.SAMPLE_RECIPIENT.getCode(), dto.getId(), "添加评论"))
+                .forEach(operateLogList::add);
+        operateLogService.batchAddModuleOperateLog(operateLogList);
     }
 }
