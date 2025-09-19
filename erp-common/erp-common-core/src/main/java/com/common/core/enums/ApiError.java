89ba8daaa4dcfa0aee80d0465af38358fe191c3a
Will <wangwei@ulanzi.cn>
2025-09-19 09:36:03 +0800

    feat,发货通知单调整

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -1325,6 +1325,7 @@ public enum ApiError implements Serializable {
     TMS_DECLARE_BILL_EXISTS_NOT_DEL(92287,"报关单【{}】已生成，不可删除"),
 
     ERROR_UPDATE_IS_ALLOW_OUTSTOCK(92288,"只有待通知出库状态下允许操作待通知出库"),
+    ERROR_IS_ALLOW_OUTSTOCK_PUSH(92289,"  - 只有允许出库的通知单允许下推销售出库单"),
 
 
     /**
