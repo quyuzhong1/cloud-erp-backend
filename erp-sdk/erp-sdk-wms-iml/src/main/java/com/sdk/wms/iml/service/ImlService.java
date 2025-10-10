cb9217b40f32afd9b0af9ccaeaee5fed1bd3ea9f
liuruipeng <arthur@ulanzi.cn>
2025-10-10 15:57:59 +0800

    feat:【艾姆勒】sdk提交

--- erp-sdk/erp-sdk-wms-iml/src/main/java/com/sdk/wms/iml/service/ImlService.java
+++ erp-sdk/erp-sdk-wms-iml/src/main/java/com/sdk/wms/iml/service/ImlService.java
@@ -3,7 +3,11 @@ package com.sdk.wms.iml.service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.common.business.constant.BusinessCommonConstants;
+import com.common.core.utils.Md5Util;
+import com.common.core.utils.OkHttpUtils;
 import com.sdk.wms.iml.constants.ImlConstants;
+import com.sdk.wms.iml.dto.ImlBaseResp;
 import com.sdk.wms.iml.dto.request.*;
 import com.sdk.wms.iml.dto.response.*;
 import com.sdk.wms.iml.utils.ImlUtils;
@@ -14,6 +18,7 @@ import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,12 +32,40 @@ public class ImlService {
 
     public static final String RECEIVING_CODE = "receiving_code";
 
+    private String getPreUrl(){
+        if (BusinessCommonConstants.hasProfile("prod")) {
+            return "https://open.imlb2c.com/";
+        } else {
+            return "https://pre-open.imlb2c.cn/";
+        }
+    }
+
+    private static final String APP_ID = "1929841041771364354";
+    private static final String APP_SECRET = "dx-zosnwtgwo3=u=276qgzu+3weguyst";
+    private static final String API_URL = "https://pre-open.imlb2c.cn/open-sdk/oms/query_warehouse";
+    private static final String REQUEST_TOKEN = "ZOFsMc85N29ly-sA4qKbDXQgJS6QF2A8IzlCWWXH_UgoaGoY6Az8aZuU_uWuQ6s0";
+
+    public static void main(String[] args) {
+        Map<String,Object> body = new HashMap<>();
+        String timestamp = String.valueOf(new Date().getTime());
+        String appSign = Md5Util.md5(APP_SECRET + timestamp + JSONObject.toJSONString(body));
+        Map<String,String> headerMap = new HashMap<>();
+        headerMap.put("x-app-id",APP_ID);
+        headerMap.put("x-app-sign",appSign);
+        headerMap.put("x-request-time",timestamp);
+        headerMap.put("x-request-token",REQUEST_TOKEN);
+        String bodyStr = OkHttpUtils.doPostJson(API_URL,body, headerMap);
+        System.out.println(bodyStr);
+    }
+
     /**
      * 获取仓库列表
      */
-    public ImlResponse<List<ImlWarehouseResp>> getWarehouse(ImlBaseRequest imlBaseRequest){
-        String response = ImlUtils.callService(ImlConstants.METHOD_GET_WAREHOUSE,imlBaseRequest);
-        return JSON.parseObject(response,new TypeReference<ImlResponse<List<ImlWarehouseResp>>>() {}.getType());
+    public ImlBaseResp<String> getWarehouse(){
+        Map<String, String> headerMap = ImlUtils.buildHearderMap(new HashMap<>());
+        String path = "open-sdk/oms/query_warehouse";
+        String bodyStr = OkHttpUtils.doPostJson(getPreUrl()+path,new HashMap<>(), headerMap);
+        return ImlUtils.parseToImlResp(bodyStr, String.class);
     }
 
     /**
