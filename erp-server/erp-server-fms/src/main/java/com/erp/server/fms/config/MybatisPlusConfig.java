7fd9936a5f686ada98a45c1d9ef23dd68acce125
wuhaotian <wuhaotian@ulanzi.cn>
2025-10-11 11:18:01 +0800

    feat，fms初始化

---
+++ erp-server/erp-server-fms/src/main/java/com/erp/server/fms/config/MybatisPlusConfig.java
@@ -0,0 +1,57 @@
+package com.erp.server.fms.config;
+
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.DefaultIdentifierGenerator;
+import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.common.business.constant.RedisCacheConstants;
+import com.common.business.utils.IdGeneratorUtil;
+import com.common.business.utils.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+/**
+ * MyBatis Plus 配置
+ * @author yl
+ */
+@Slf4j
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.POSTGRE_SQL);
+        //关闭生成countSql 优化掉join
+        paginationInnerInterceptor.setOptimizeJoin(false);
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+
+    }
+
+    /**
+     * 修改雪花算法生成方式
+     */
+    @Resource
+    private IdGeneratorUtil idGeneratorUtil;
+    @Bean
+    public IdentifierGenerator idGenerator() {
+        return idGeneratorUtil.idGenerator();
+    }
+
+
+}
+
