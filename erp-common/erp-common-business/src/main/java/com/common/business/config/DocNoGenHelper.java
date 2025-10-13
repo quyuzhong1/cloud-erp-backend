13d64a114855d9ffe3f26dc0222c329ed6a45075
jack chen <jackchen@ulanzi.cn>
2025-10-13 16:54:56 +0800

    feat(business): 新增模具编号生成方法

--- erp-common/erp-common-business/src/main/java/com/common/business/config/DocNoGenHelper.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/config/DocNoGenHelper.java
@@ -203,4 +203,26 @@ public class DocNoGenHelper implements InitializingBean {
 
         return businessNoPrefix;
     }
+
+    /**
+     * 按单据类型 + 产品分类code + 日期获取递增单号
+     * 单据前缀+6位日期+5位顺序
+     * 注意事项：需在枚举类BusinessNoTypeEnum定义单号前缀，上线切换时需手工把最新的最大值放入到数据库
+     * @param businessNoTypeEnum
+     * @return
+     */
+    public String generateMoldCode(BusinessNoTypeEnum businessNoTypeEnum, String code) {
+        String currentDateStr = LocalDateUtil.formatTime(LocalDateTime.now(), "yyMMdd");
+        //注意，不保证绝对有序，有可能中间某个单生成了单号，但是后面数据库报错不会回收
+        String docNoKey = BusinessNoTypeEnum.REDIS_GEN_KEY + ":" + businessNoTypeEnum.getName() +  ":" + currentDateStr;
+        Long currentIndex = redisTemplate.execute(redisScript, stringRedisSerializer, stringRedisSerializer, Lists.newArrayList(docNoKey),String.valueOf(1),String.valueOf(ONE_DAY_CACHE_TIME));
+        int fillZeroDigit = BusinessNoTypeEnum.FILL_0_DIGIT;
+        if(BusinessCommonConstants.hasProfile("test") || BusinessCommonConstants.hasProfile("dev")){
+            fillZeroDigit = fillZeroDigit +1;
+        }
+        // 单据前缀+6位日期+5位顺序位
+        String docNo = CharSequenceUtil.format("{}{}{}{}",StrUtils.null2EmptyWithTrim(businessNoTypeEnum.getPrefix()),code, currentDateStr, StrUtils.leftPadding(String.valueOf(currentIndex),fillZeroDigit,"0"));
+        log.info("单据类型：【{}】生成的单号为【{}】", businessNoTypeEnum.getName(), docNo);
+        return docNo;
+    }
 }
