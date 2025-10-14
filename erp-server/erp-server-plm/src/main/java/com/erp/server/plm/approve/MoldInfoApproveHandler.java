4228d500d63e801a8fdd6839f2c1c9fff4491ee6
jack chen <jackchen@ulanzi.cn>
2025-10-14 09:16:13 +0800

    refactor(plm): 替换操作日志服务类名并调整相关引用
    - 将 SysLogService 替换为 OperateLogService
    - 更新 MoldInfoServiceImpl 中的操作日志实体类引用
    - 更新 MoldRefSkuServiceImpl 中的日志服务引用
    - 移除未使用的 CommonService 和 Autowired 注解- 在 MoldInfoApproveHandler 中添加空实现的 addComment 方法

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/approve/MoldInfoApproveHandler.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/approve/MoldInfoApproveHandler.java
@@ -44,4 +44,9 @@ public class MoldInfoApproveHandler extends AbstractApproveHandler {
         approveOne.setVariablesMap(dto.getVariablesMap());
         return moldInfoService.approveEnd(approveOne,entity);
     }
+
+    @Override
+    public void addComment(ApproveDTO.AddCommentDTO dto) {
+
+    }
 }
