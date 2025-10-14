4c43103996e458f7b25bc3b602f3ca27818cd2a4
jack chen <jackchen@ulanzi.cn>
2025-10-14 15:25:46 +0800

    feat(plm): 新增模具关联SKU功能

--- erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/enums/CfgQueryOptionBussinessKeyEnum.java
+++ erp-model/erp-model-workflow/src/main/java/com/erp/model/workflow/enums/CfgQueryOptionBussinessKeyEnum.java
@@ -54,6 +54,7 @@ public enum CfgQueryOptionBussinessKeyEnum implements EnumMessage {
 	SAMPLE_SCRAP_INFO("sampleScrapInfo", "样品报废单"),
 	EXHIBITION_ORDER("exhibitionOrder", "展会订单"),
 	MOLD_INFO("moldInfo", "模具档案"),
+	MOLD_REF_SKU("moldRefSku", "模具关联SKU"),
     ;
     /**
      * 类型
