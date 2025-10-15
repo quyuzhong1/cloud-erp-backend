d4f734a006db57c15424f38ce19d66a9dfc7dc74
jack chen <jackchen@ulanzi.cn>
2025-10-15 15:36:23 +0800

    fix(plm):修复BOM信息校验逻辑- 修改SKU属性ID去重逻辑，确保正确判断是否为相同产品属性组合 - 避免因重复属性ID导致的组合品创建异常

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/BomInfoServiceImpl.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/impl/BomInfoServiceImpl.java
@@ -1172,7 +1172,7 @@ public class BomInfoServiceImpl extends ServiceImpl<BomInfoMapper, BomInfoEntity
                 .distinct()
                 .collect(Collectors.toList());
         List<SkuVO> skuVOList = productDetailService.getSkuBySkuIds(mergedList);
-        long count = skuVOList.stream().map(SkuVO::getPropertyId).count();
+        long count = skuVOList.stream().map(SkuVO::getPropertyId).distinct().count();
         if(count > 1){
             throw new ServiceException("只允许添加相同产品属性组合成组合品");
         }
