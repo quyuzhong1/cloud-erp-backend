77399b5aa4018c5cc6593f6c73d059d80e3c9e49
jack chen <jackchen@ulanzi.cn>
2025-10-20 11:31:11 +0800

    feat(plm): 新增模具预警策略基础代码

---
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/mapper/CfgMoldAlertRuleMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.plm.mapper;
+import com.erp.model.plm.entity.CfgMoldAlertRuleEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 模具预警策略 Mapper 接口
+ * </p>
+ *
+ * @author jack
+ * @since 2025-10-20
+ */
+@Mapper
+public interface CfgMoldAlertRuleMapper extends BaseMapper<CfgMoldAlertRuleEntity> {
+
+}
