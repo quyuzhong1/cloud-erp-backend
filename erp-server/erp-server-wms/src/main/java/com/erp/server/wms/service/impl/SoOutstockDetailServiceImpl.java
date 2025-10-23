8a3db9927c7634116c22eb2f714d32ba26edd38f
jack chen <jackchen@ulanzi.cn>
2025-10-23 14:59:07 +0800

    refactor(wms): 注释掉出库明细库存校验逻辑

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
@@ -346,14 +346,14 @@ public class SoOutstockDetailServiceImpl extends SuperServiceImpl<SoOutstockDeta
                     if (outStockQty + planQty > soQty) {
                         throw new ServiceException(ApiError.ERROR_92028);
                     }
-                    if (ObjectUtil.isEmpty(batchNo)) {
-                        //即时库存
-                        Integer inventory = skuInventoryList.stream().filter(s -> s.getSkuId().equals(skuId) && s.getWarehouseLocationId().
-                                equals(warehouseLocation)).findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal())).orElse(0);
-                        if (planQty > inventory) {
-                            throw new ServiceException(ApiError.ERROR_92030);
-                        }
-                    }
+//                    if (ObjectUtil.isEmpty(batchNo)) {
+//                        //即时库存
+//                        Integer inventory = skuInventoryList.stream().filter(s -> s.getSkuId().equals(skuId) && s.getWarehouseLocationId().
+//                                equals(warehouseLocation)).findFirst().flatMap(obj -> Optional.ofNullable(obj.getInventoryTotal())).orElse(0);
+//                        if (planQty > inventory) {
+//                            throw new ServiceException(ApiError.ERROR_92030);
+//                        }
+//                    }
                 }
             }
         }
