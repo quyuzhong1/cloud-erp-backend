a103f1c2427296d6cdab877b84977c3cba10a808
Will <wangwei@ulanzi.cn>
2025-10-24 18:15:35 +0800

    feat,飞书撤销流程调整

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ProcessManagementServiceImpl.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/service/impl/ProcessManagementServiceImpl.java
@@ -851,7 +851,7 @@ public class ProcessManagementServiceImpl extends SuperServiceImpl<ProcessManage
     public ProcessManagementDTO.RevokeResultDTO revoke(ProcessManagementDTO.RevokeDTO dto) {
         //判断是否走飞书流程
         Boolean isFsApprove = isFsApprovePass(dto.getBusinessId(),dto.getBusinessKey());
-        if (isFsApprove) {
+        if (isFsApprove && ProcessSourcePlatformEnum.ERP.getCode().equals(dto.getSourcePlatform())) {
             throw new ServiceException(ApiError.PROCESS_APPROVE_FS_PROCESS);
         }
 
