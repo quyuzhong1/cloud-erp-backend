a31976e190048e4d15d0ae3058e4e75ed8ef6480
shukai <shukai@ulanzi.cn>
2025-10-24 10:50:05 +0800

    add:调整lua返回结果

--- erp-server/erp-server-wms/src/main/resources/lua/override.lua
+++ erp-server/erp-server-wms/src/main/resources/lua/override.lua
@@ -2,7 +2,9 @@ local delimiter = '&&';
 local i = 0;
 local key = ARGV[1];
 local qty = ARGV[2];
+local result = {};
 local value = redis.call('get', key);
+local beforeinventorys = (key .. '==' .. value);
 if value == 0 or value == false then
     redis.call('set', key, qty);
 else
@@ -10,9 +12,19 @@ else
         i = i + 1;
     end
     if i > 1 then
-        return '存在未提交流水'
+        result['success'] = false;
+        result['errormsg'] = '存在未提交流水';
+        return cjson.encode(result);
     else
         redis.call('set', key, qty);
     end
 end
-return '0';
+local afterinventorys = (key .. '==' .. qty);
+result['success'] = true;
+result['beforeinventorys'] = beforeinventorys;
+result['afterinventorys'] = afterinventorys;
+local opallcount = redis.call('INCRBY' , 'inventory:op:all' , 1);
+local overridecount = redis.call('INCRBY' , 'inventory:op:override' , 1);
+result['opallcount'] = opallcount;
+result['overridecount'] = overridecount;
+return cjson.encode(result);
