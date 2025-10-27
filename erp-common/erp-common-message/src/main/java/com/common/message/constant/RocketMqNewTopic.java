2bd808274bf6ec4bd8655530354442087376b214
liuruipeng <arthur@ulanzi.cn>
2025-10-27 09:45:02 +0800

    代码合并处理

--- erp-common/erp-common-message/src/main/java/com/common/message/constant/RocketMqNewTopic.java
+++ erp-common/erp-common-message/src/main/java/com/common/message/constant/RocketMqNewTopic.java
@@ -216,11 +216,6 @@ public class RocketMqNewTopic {
      */
     public static final String RESTCLOUD_PLATFORM_RECEIPT_TO_OMS_TOPIC = "${spring.cloud.nacos.discovery.namespace}-restcloud_platform_receipt_to_oms_topic";
 
-    /**
-     * 新中台同步飞书审批详情
-     */
-    public static final String DMP_FS_INSTANCES_TO_WORKFLOW_TOPIC = "${spring.cloud.nacos.discovery.namespace}-dmp_fs_instances_to_workflow_topic";
-
 
     /**
      * restcloud同步平台仓库
