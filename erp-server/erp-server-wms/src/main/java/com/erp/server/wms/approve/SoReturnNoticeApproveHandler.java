daf299b6e0f61b99653fdd865f7c95d3ca4f661b
Will <wangwei@ulanzi.cn>
2025-10-27 18:36:02 +0800

    feat,代码合并调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SoReturnNoticeApproveHandler.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/approve/SoReturnNoticeApproveHandler.java
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.Collections;
 
 @Slf4j
 @Component
@@ -37,7 +36,7 @@ public class SoReturnNoticeApproveHandler extends AbstractApproveHandler {
 
     @Override
     public Boolean cancelProcess(ApproveDTO.CancelProcessDTO dto) {
-        return soReturnNoticeService.cancelProcess(Collections.singletonList(dto.getId()));
+        return soReturnNoticeService.cancelProcess(new ApproveDTO.BatchCancelProcessDTO(dto));
     }
 
     @Override
