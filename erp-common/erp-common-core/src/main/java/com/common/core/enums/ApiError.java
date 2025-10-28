b4ffa8a5af7323848352b8db5fa898b4c2b29748
Will <wangwei@ulanzi.cn>
2025-10-28 10:28:51 +0800

    feat,头程发货单校验调整

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -1323,6 +1323,7 @@ public enum ApiError implements Serializable {
     TMS_FIRST_MILE_LOGISTIC_EXISTS_NOT_DEL(92286,"物流单【{}】已生成，不可删除"),
     TMS_DECLARE_BILL_EXISTS_NOT_DEL(92287,"报关单【{}】已生成，不可删除"),
     ERROR_FBA_FNSKU_NOT_EXIST(92281, "平台SKU【{}】未匹配到FNSKU，请检查SKU是否已绑定FNSKU"),
+    ERROR_TRANSFER_NOT_RETRY_OUTSTOCK(92288, "发货单已调拨出库，不允许重新出库"),
 
     /**
      * OMS 错误
