72b0b491a4b4a579098fdf46539e8c6113de73ac
liuruipeng <arthur@ulanzi.cn>
2025-10-28 18:26:41 +0800

    feat:【艾姆勒】入库接口

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ImlHandlerServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ImlHandlerServiceImpl.java
@@ -80,8 +80,8 @@ public class ImlHandlerServiceImpl extends AbstractThirdWarehouseHandler {
                 .needCustomerAudit("N")
                 .platformOrderNo(createInboundReq.getReferenceNo())
                 .bizType("TOC")
-                .destWarehouseCode(createInboundReq.getWarehouseCode())
                 .customsType(createInboundReq.getDeclareType().equals("Y")?"SEPARATE_TAX":createInboundReq.getDeclareType().equals("N")?"NO_TAX":"")
+                .destWarehouseCode(createInboundReq.getWarehouseCode())
                 .inboundType(createInboundReq.getReceivingType().equals(OverseasInstockTypeEnum.SELF_HEADWAY.getCode())?"DIRECT":createInboundReq.getReceivingType().equals(OverseasInstockTypeEnum.TRANSFER_AGENT.getCode())?"TRANSIT":"")
                 .expectedDate(createInboundReq.getEtaDate().atZone(ZoneId.systemDefault())
                         .toInstant()
@@ -147,6 +147,7 @@ public class ImlHandlerServiceImpl extends AbstractThirdWarehouseHandler {
             boxListDTO.setBoxLength(firstItem.getBoxLength());
             boxListDTO.setBoxWidth(firstItem.getBoxWidth());
             boxListDTO.setBoxHeight(firstItem.getBoxHeight());
+            boxListDTO.setBoxNo(createInboundReq.getReferenceNo() + "-" + boxNo);
             List<ImlCreateInboundReq.BoxsDTO.BoxDetailsDTO> skuVosDTOS = new ArrayList<>();
             for (ThirdWarehouseCreateInboundReq.Item item : itemList) {
                 ImlCreateInboundReq.BoxsDTO.BoxDetailsDTO skuVosDTO = new ImlCreateInboundReq.BoxsDTO.BoxDetailsDTO();
