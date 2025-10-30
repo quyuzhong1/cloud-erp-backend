2707bb196deeab16186df83db3582ad8cbc21a82
wtr <981404651@qq.com>
2025-10-30 09:14:25 +0800

    处理采购单返回信息

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/AssetPurchaseOrderSupplierDTO.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/dto/AssetPurchaseOrderSupplierDTO.java
@@ -47,6 +47,11 @@ public class AssetPurchaseOrderSupplierDTO implements Serializable {
         private String supplierId;
 
         /**
+         * 供应商名称
+         */
+        private String supplierName;
+
+        /**
         * 结算方式
         */
         private String payMethodId;
