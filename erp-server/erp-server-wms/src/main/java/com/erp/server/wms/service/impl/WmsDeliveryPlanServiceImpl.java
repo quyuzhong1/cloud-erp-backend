c235bb9616c7401ffba8c01711ed5361175bfda2
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-11-03 09:10:42 +0800

    合并问题处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WmsDeliveryPlanServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/WmsDeliveryPlanServiceImpl.java
@@ -1278,7 +1278,7 @@ revokeDTO.setExecuteSystem(dto.getExecuteSystem());
                 throw new ServiceException("店铺不能为空");
             }
             //明细中的fnsku不能为空
-            if(DeliveryPlanTypeEnum.FBA.getCode().equals(wmsDeliveryPlanEntity.getType()){
+            if(DeliveryPlanTypeEnum.FBA.getCode().equals(wmsDeliveryPlanEntity.getType())){
                 long count = detailList.stream().filter(obj -> CharSequenceUtil.isBlank(obj.getFnSku())).count();
                 if (count > 0) {
                     throw new ServiceException(ApiError.ERROR_FBA_FNSKU_NOT_BLANK);
