a0fb6e81527092af59bc56c3dd4f08baf2a03356
Jim <zhoujingbo@ulanzi.cn>
2025-11-03 12:02:40 +0800

    UPDATE:调整中台配置模块代号

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
@@ -172,15 +172,15 @@ public enum ModuleTypeEnum {
     PAYABLE_INFO("145", "应付单"),
 
     // DMP配置
-    DMP_BASIC_SYSTEM("149", "平台管理"),
-    DMP_CFG_ETL("150", "清洗调度"),
-    DMP_ETL_TASK("151", "清洗任务"),
-    DMP_CFG_INPUT("152", "拉取配置"),
-    DMP_CFG_INPUT_DETAIL("153", "拉取调度"),
-    DMP_INPUT_TASK("154", "拉取任务"),
-    DMP_CFG_OUTPUT("155", "推送配置"),
-    DMP_CFG_OUTPUT_DETAIL("156", "推送调度"),
-    DMP_OUTPUT_TASK("157", "推送任务"),
+    DMP_BASIC_SYSTEM("1000", "平台管理"),
+    DMP_CFG_INPUT("1001", "拉取配置"),
+    DMP_CFG_INPUT_DETAIL("1002", "拉取调度"),
+    DMP_INPUT_TASK("1003", "拉取任务"),
+    DMP_CFG_ETL("1004", "清洗调度"),
+    DMP_ETL_TASK("1005", "清洗任务"),
+    DMP_CFG_OUTPUT("1006", "推送配置"),
+    DMP_CFG_OUTPUT_DETAIL("1007", "推送调度"),
+    DMP_OUTPUT_TASK("1008", "推送任务"),
     ;
 
 
