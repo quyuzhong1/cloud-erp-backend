62a9fedb2a6caf3e88e9d3bcbad10354540873d7
wuhaotian <wuhaotian@ulanzi.cn>
2025-11-06 18:52:49 +0800

    feat,报错修复

--- erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysDictFeign.java
+++ erp-rpc/erp-rpc-sys/src/main/java/com/erp/rpc/sys/feign/SysDictFeign.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.common.business.validator.ValidList;
 
 import javax.validation.Valid;
 import java.util.List;
