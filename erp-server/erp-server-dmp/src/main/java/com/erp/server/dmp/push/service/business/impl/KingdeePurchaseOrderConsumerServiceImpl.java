da99f5897557118685253fdb17c59ea762be21a0
wtr <981404651@qq.com>
2025-11-06 14:12:54 +0800

    反审核后删除订单

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeePurchaseOrderConsumerServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/push/service/business/impl/KingdeePurchaseOrderConsumerServiceImpl.java
@@ -85,6 +85,7 @@ public class KingdeePurchaseOrderConsumerServiceImpl implements KingdeePurchaseO
          */
         if (SyncOperateEnum.OPERATE_DISAPPROVE.getCode().equals(operate)) {
             operateDisapprove(apiUtils,platformEntity, map,type);
+            operateDelete(apiUtils,platformEntity,map,operate);
         }
         /**
          * 审核
