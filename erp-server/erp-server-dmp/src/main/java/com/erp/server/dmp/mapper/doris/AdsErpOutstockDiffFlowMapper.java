5261676bb023a475191a8d57a66b8d514f5e8b1b
shukai <shukai@ulanzi.cn>
2025-11-12 19:25:50 +0800

    add:单据差异

---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/mapper/doris/AdsErpOutstockDiffFlowMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.dmp.mapper.doris;
+import com.erp.model.dmp.entity.doris.AdsErpOutstockDiffFlowEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * 第三方仓出库单据差异表 Mapper 接口
+ * </p>
+ *
+ * @author shukai
+ * @since 2025-11-12
+ */
+@Mapper
+public interface AdsErpOutstockDiffFlowMapper extends BaseMapper<AdsErpOutstockDiffFlowEntity> {
+
+}
---
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/AdsErpOutstockDiffFlowService.java
@@ -0,0 +1,44 @@
+package com.erp.server.dmp.service;
+import com.common.business.dto.base.BaseResultDTO;
+import com.common.business.dto.base.PagingDTO;
+import com.common.business.service.SuperService;
+import com.common.business.vo.PagingVO;
+import com.erp.model.dmp.dto.AdsErpOutstockDiffFlowDTO;
+import com.erp.model.dmp.entity.doris.AdsErpOutstockDiffFlowEntity;
+
+/**
+ * <p>
+ * 第三方仓出库单据差异表 服务类
+ * </p>
+ *
+ * @author shukai
+ * @since 2025-11-12
+ */
+public interface AdsErpOutstockDiffFlowService extends SuperService<AdsErpOutstockDiffFlowEntity> {
+
+    /**
+    * 新增
+    * @author shukai
+    * @date: 2025-11-12
+    * @param dto
+    * @return
+    */
+    BaseResultDTO.AddDTO add(AdsErpOutstockDiffFlowDTO.AddDTO dto);
+
+    /**
+    * 修改
+    * @author shukai
+    * @date: 2025-11-12
+    * @param dto
+    * @return
+    */
+    Boolean update(AdsErpOutstockDiffFlowDTO.UpdateDTO dto);
+
+    PagingVO<AdsErpOutstockDiffFlowDTO.PagingDTO> paging(PagingDTO<AdsErpOutstockDiffFlowDTO.PagingParamDTO> dto);
+    
+    PagingVO<AdsErpOutstockDiffFlowDTO.TotalDTO> total(PagingDTO<AdsErpOutstockDiffFlowDTO.PagingParamDTO> dto);
+    
+    Boolean reCreate(AdsErpOutstockDiffFlowDTO.ReCreateDTO dto);
+    
+    Boolean exportExcel(AdsErpOutstockDiffFlowDTO.ExpotParamDTO dto);
+}
