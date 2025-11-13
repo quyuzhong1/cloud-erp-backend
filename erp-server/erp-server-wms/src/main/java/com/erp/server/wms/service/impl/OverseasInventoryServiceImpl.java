3cdf19d57f7c05161c3f9d0c3b187e088cd5ceff
liuruipeng <arthur@ulanzi.cn>
2025-11-13 19:36:38 +0800

    库龄BUG处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasInventoryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/OverseasInventoryServiceImpl.java
@@ -252,6 +252,9 @@ public class OverseasInventoryServiceImpl extends SuperServiceImpl<OverseasInven
                 .eq(OverseasInventoryEntity::getOverseasProviderId, entity.getOverseasProviderId())
                 .eq(OverseasInventoryEntity::getPlatformSku, entity.getPlatformSku());
         OverseasInventoryEntity existingEntity = this.getOne(queryWrapper);
+        if(Objects.nonNull(existingEntity)){
+            entity.setId(existingEntity.getId());
+        }
         if (existingEntity == null || entity.getDownloadTime().isAfter(existingEntity.getDownloadTime())) {
             boolean flag = this.saveOrUpdate(entity, queryWrapper);
             if(existingEntity != null){
