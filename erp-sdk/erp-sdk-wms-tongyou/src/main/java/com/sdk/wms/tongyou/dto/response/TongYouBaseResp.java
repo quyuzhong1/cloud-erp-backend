ec8452002308c3167ebe88252f0f627ec4c744b1
Will <wangwei@ulanzi.cn>
2025-11-13 10:47:44 +0800

    feat,通邮对接

--- erp-sdk/erp-sdk-wms-tongyou/src/main/java/com/sdk/wms/tongyou/dto/response/TongYouBaseResp.java
+++ erp-sdk/erp-sdk-wms-tongyou/src/main/java/com/sdk/wms/tongyou/dto/response/TongYouBaseResp.java
@@ -7,7 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 /**
- * @author liuruipeng
+ * tongyou基础响应类
+ * @author will
+ * @date 2025/11/13 10:39
  */
 @Data
 @AllArgsConstructor
@@ -18,27 +20,27 @@ public class TongYouBaseResp<T> {
     /**
      * 错误码：0为成功，其他为失败
      */
-    private Integer code;
+    private String error;
 
     private T data;
     /**
      * 错误提示信息
      */
-    private String message;
+    private String content;
 
 
     public static <T> TongYouBaseResp<T> error(String msg) {
         TongYouBaseResp<T> apiResult = new TongYouBaseResp<>();
-        apiResult.setCode(-1);
-        apiResult.setMessage(msg);
+        apiResult.setError("F");
+        apiResult.setContent(msg);
         return apiResult;
     }
 
     public static <T> TongYouBaseResp<T> error(String formatedErrMsg, Object... params) {
         TongYouBaseResp<T> apiResult = new TongYouBaseResp<>();
-        apiResult.setCode(-1);
+        apiResult.setError("F");
         String msg = CharSequenceUtil.format(formatedErrMsg,params);
-        apiResult.setMessage(msg);
+        apiResult.setContent(msg);
         return apiResult;
     }
 }
