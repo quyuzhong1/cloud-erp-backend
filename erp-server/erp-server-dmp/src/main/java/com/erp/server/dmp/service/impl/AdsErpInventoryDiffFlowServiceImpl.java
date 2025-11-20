fb622daf1e9bf3f4357b78fe54d74f31576820cd
shukai <shukai@ulanzi.cn>
2025-11-20 15:04:40 +0800

    add:加入月份

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/AdsErpInventoryDiffFlowServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/AdsErpInventoryDiffFlowServiceImpl.java
@@ -224,9 +224,10 @@ public class AdsErpInventoryDiffFlowServiceImpl extends SuperServiceImpl<AdsErpI
 	public Boolean reCreate(ReCreateDTO dto) {
 		String checkMonth = dto.getCheckMonth();
 		checkMonth = checkMonth.replace("-", "年") + "月";
-		Integer count = lambdaQuery().eq(AdsErpInventoryDiffFlowEntity::getExecStatus, "doing").count();
+		Integer count = lambdaQuery().eq(AdsErpInventoryDiffFlowEntity::getCheckMonth, checkMonth)
+				.eq(AdsErpInventoryDiffFlowEntity::getExecStatus, "doing").count();
 		if(count != null && count > 0) {
-			throw new ServiceException("核对任务正在执行中");
+			throw new ServiceException(dto.getCheckMonth() + "核对任务正在执行中");
 		}
 		boolean reCreate = RestCloudApiUtil.reCreate(checkMonth, "ods_antu/ods_flow_antu_excel_inventory" , "ods_antu/ods_flow_antu_excel_inventory_flow");
 		if(reCreate) {
