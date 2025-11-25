b31208ae6730e5536482b5ed8e37510c32af0664
liuruipeng <arthur@ulanzi.cn>
2025-11-25 16:27:05 +0800

    SKU对照表 启用时间默认值问题，未映射时应该默认为空

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SkuMappingRuleServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SkuMappingRuleServiceImpl.java
@@ -16,6 +16,7 @@ import com.common.core.constant.EnumMessage;
 import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
 import com.common.core.utils.BeanMapperUtils;
+import com.common.core.utils.MathUtil;
 import com.erp.model.oms.dto.ListingInfoParamDTO;
 import com.erp.model.oms.dto.ListingInfoWithSkuMappingDTO;
 import com.erp.model.oms.dto.SkuMappingRuleDTO;
@@ -45,6 +46,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -492,6 +495,8 @@ public class SkuMappingRuleServiceImpl extends SuperServiceImpl<SkuMappingRuleMa
                             skuMappingEntity.setProductSkuId(skuVO.getSkuId());
                             skuMappingEntity.setProductSkuNo(skuVO.getSkuNo());
                             skuMappingEntity.setProductName(skuVO.getSkuName());
+                            skuMappingEntity.setEffectiveTime(LocalDateTime.now(ZoneId.systemDefault()));
+                            skuMappingEntity.setExpireTime(LocalDateTime.now(ZoneId.systemDefault()).plusYears(MathUtil.NUMBER_100));
                             skuMappingEntity.setListingId(listingInfoEntity.getId());
                             updateSkuMappingList.add(skuMappingEntity);
                             break ruleLoop;
