de4da58b38f22eb8fcfad8bc05e73f9ca928a4dc
wuhaotian <wuhaotian@ulanzi.cn>
2025-11-26 10:55:03 +0800

    uat,解决msg打包失败 缺少import org.springframework.boot.test.context.SpringBootTest;

--- erp-server/erp-server-msg/src/main/java/com/erp/server/msg/controller/TestController.java
+++ erp-server/erp-server-msg/src/main/java/com/erp/server/msg/controller/TestController.java
@@ -26,7 +26,6 @@ import org.junit.runner.RunWith;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
