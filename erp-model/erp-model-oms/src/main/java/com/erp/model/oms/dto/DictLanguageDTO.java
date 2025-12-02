a4e2091cd4104dd00b613b896a639767624747e5
jack chen <jackchen@ulanzi.cn>
2025-12-02 08:59:29 +0800

    feat(oms): 新增ISO 639-1语言标准字典功能 - 添加DictLanguage 下拉接口 - 在SysUserInfoController中增加findList接口，支持基础搜索功能

---
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/dto/DictLanguageDTO.java
@@ -0,0 +1,139 @@
+package com.erp.model.oms.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * <p>
+ * ISO 639-1 语言标准请求响应实体
+ * </p>
+ *
+ * @author jack
+ * @since 2025-12-01
+*/
+@Data
+@NoArgsConstructor
+public class DictLanguageDTO implements Serializable {
+
+
+
+
+    /**
+    * 详情
+    */
+    @Data
+    @NoArgsConstructor
+    public static class ViewDTO {
+
+        /**
+        * 主键id
+        */
+        private String  id;
+
+        /**
+        * 编号
+        */
+        private String code;
+
+        /**
+        * 英文名称
+        */
+        private String nameEn;
+
+        /**
+        * 中文名称
+        */
+        private String nameZh;
+
+
+    }
+
+    /**
+    * 新增
+    */
+    @Data
+    @NoArgsConstructor
+    public static class AddDTO extends CommonDTO {
+
+
+    }
+
+    /**
+    * 修改
+    */
+    @Data
+    @NoArgsConstructor
+    public static class UpdateDTO extends CommonDTO {
+
+        /**
+        * 主键id
+        */
+        @NotBlank(message = "主键id不能为空")
+        private String id;
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class CommonDTO {
+
+        /**
+        * 英文名称
+        */
+        @NotBlank(message = "英文名称不能为空")
+        @Size(max = 32,message = "英文名称最大长度不能超过32位")
+        private String nameEn;
+
+        /**
+        * 中文名称
+        */
+        @NotBlank(message = "中文名称不能为空")
+        @Size(max = 32,message = "中文名称最大长度不能超过32位")
+        private String nameZh;
+
+
+    }
+
+
+    /**
+     * 列表
+     */
+    @Data
+    @NoArgsConstructor
+    public static class ListDTO {
+        /**
+         * 编号
+         */
+        private String code;
+
+        /**
+         * 英文名称
+         */
+        private String nameEn;
+
+        /**
+         * 中文名称
+         */
+        private String nameZh;
+
+
+    }
+
+    @Data
+    @NoArgsConstructor
+    public static class SelectDTO {
+        /**
+         * 关键词
+         */
+        private String searchKeyword;
+    }
+
+
+
+}
---
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/mapper/DictLanguageMapper.java
@@ -0,0 +1,19 @@
+package com.erp.server.oms.mapper;
+import com.erp.model.oms.entity.DictLanguageEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * <p>
+ * ISO 639-1 语言标准 Mapper 接口
+ * </p>
+ *
+ * @author jack
+ * @since 2025-12-01
+ */
+@Mapper
+public interface DictLanguageMapper extends BaseMapper<DictLanguageEntity> {
+
+}
