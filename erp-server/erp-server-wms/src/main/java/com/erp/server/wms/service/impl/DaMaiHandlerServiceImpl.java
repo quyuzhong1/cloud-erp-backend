6c2df062ce276897b2b83c782bfc9da68b770ee8
liuruipeng <arthur@ulanzi.cn>
2025-12-03 19:08:28 +0800

    大卖仓取消出库单 增加拦截中

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/DaMaiHandlerServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/DaMaiHandlerServiceImpl.java
@@ -245,6 +245,9 @@ public class DaMaiHandlerServiceImpl extends AbstractThirdWarehouseHandler {
         if("订单取消失败".equals(resp.getMsg())){
             return failure(resp.getMsg());
         }
+        if("请求已受理".equals(resp.getMsg())){
+            return success(ThirdWarehouseCancelResultEnum.INTERCEPTING.getCode());
+        }
         return success(ThirdWarehouseCancelResultEnum.INTERCEPTION_SUCCESSFUL.getCode());
     }
     @Override
