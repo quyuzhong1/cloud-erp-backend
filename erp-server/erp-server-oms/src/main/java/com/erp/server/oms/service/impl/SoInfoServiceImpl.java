97527810d2971eb554d1dc7654f484c079d6b2b9
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-12-05 12:31:22 +0800

    b2b三方发货单明细修改

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -4082,7 +4082,7 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
 
     @Override
     public B2bThirdDeliveryDTO.ViewDTO getB2bThirdDeliveryView(B2bThirdDeliveryDTO.ViewQueryDTO dto) {
-        SoInfoEntity soInfoEntity = this.getById(dto.getId());
+        SoInfoEntity soInfoEntity = this.getById(dto.getSoId());
         if (ObjectUtil.isEmpty(soInfoEntity)) {
             throw new ServiceException(ApiError.ERROR_92016);
         }
@@ -4091,7 +4091,7 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
         if (CollUtil.isEmpty(updateDTOList) || !WarehouseManageTypeEnum.THIRD_PARTY.getCode().equals(updateDTOList.get(0).getWarehouseManageType())){
             throw new ServiceException("只允许三方仓类型仓库下推b2b三方发货单");
         }
-        List<SoDetailEntity> soDetailEntityList = soDetailService.listBaseByMainId(dto.getId());
+        List<SoDetailEntity> soDetailEntityList = soDetailService.listBaseByMainId(dto.getSoId());
         soDetailEntityList = soDetailEntityList.stream().filter(e -> (e.getQty() - e.getDeliveryQty()) > 0 && dto.getSoDetailIds().contains(e.getId())).collect(Collectors.toList());
         if (CollUtil.isEmpty(soDetailEntityList)){
             throw new ServiceException("订单明细中发货数量已全部下推,不允许再生成三方发货单");
