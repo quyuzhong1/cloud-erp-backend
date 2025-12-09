02c52b6103c870e901c454fa3fbeac9f50a571f0
Will <wangwei@ulanzi.cn>
2025-12-09 16:49:37 +0800

    feat,kol需求开发

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/KolB2bApplicationServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/KolB2bApplicationServiceImpl.java
@@ -9,6 +9,7 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.exception.ExcelCommonException;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.common.business.annotation.DistributeLocker;
 import com.common.business.config.DocNoGenHelper;
@@ -605,6 +606,7 @@ public class KolB2bApplicationServiceImpl extends SuperServiceImpl<KolB2bApplica
 
             SoInfoDTO.AddDTO addDTO = new SoInfoDTO.AddDTO();
             BeanUtil.copyProperties(mainEntity,addDTO);
+            addDTO.setId(IdWorker.getIdStr());
             addDTO.setOrderType(BillTypeEnum.B2B.getCode());
             addDTO.setBillDate(LocalDate.now());
             addDTO.setRequireDate(mainEntity.getDate());
@@ -624,6 +626,7 @@ public class KolB2bApplicationServiceImpl extends SuperServiceImpl<KolB2bApplica
                 }
                 SoDetailDTO.AddDTO soDetailAddDTO = new SoDetailDTO.AddDTO();
                 BeanUtil.copyProperties(detailEntity,soDetailAddDTO);
+                soDetailAddDTO.setId(IdWorker.getIdStr());
                 soDetailList.add(soDetailAddDTO);
             }
             addDTO.setDetailList(soDetailList);
