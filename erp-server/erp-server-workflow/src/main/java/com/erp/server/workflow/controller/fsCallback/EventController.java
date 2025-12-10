e65464c087f9c6ff2dea43283d4ead40dc8b4706
Will <wangwei@ulanzi.cn>
2025-12-10 16:44:16 +0800

    feat,飞书优化

---
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/EventController.java
@@ -0,0 +1,40 @@
+package com.erp.server.workflow.controller.fsCallback;
+
+import com.lark.oapi.core.request.EventReq;
+import com.lark.oapi.event.CustomEventHandler;
+import com.lark.oapi.event.EventDispatcher;
+import com.lark.oapi.sdk.servlet.ext.ServletAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+
+// SDK 使用说明 SDK user guide：https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/server-side-sdk/java-sdk-guide/preparations
+@RestController
+public class EventController {
+
+    // 注册事件 Register event
+    private static final EventDispatcher EVENT_HANDLER = EventDispatcher.newBuilder("verificationToken", "encryptKey")
+            .onCustomizedEvent("approval_instance", new CustomEventHandler() {
+                @Override
+                public void handle(EventReq event) throws Exception {
+                    System.out.printf("[ onCustomizedEvent access ], type: message, data: %s\n", new String(event.getBody(), StandardCharsets.UTF_8));
+                }
+            })
+            .build();
+
+    // 注入 ServletAdapter 实例 Inject ServletAdapter instance
+    @Autowired
+    private ServletAdapter servletAdapter;
+
+    // 创建路由处理器 Create route handler
+    @RequestMapping("/webhook/event")
+    public void event(HttpServletRequest request, HttpServletResponse response)
+            throws Throwable {
+        // 回调扩展包提供的事件回调处理器 Callback handler provided by the extension package
+        servletAdapter.handleEvent(request, response, EVENT_HANDLER);
+    }
+}
