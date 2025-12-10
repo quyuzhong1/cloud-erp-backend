4c88f699aca25bb126062e377e8371c14efd0f8e
Will <wangwei@ulanzi.cn>
2025-12-10 17:32:02 +0800

    feat,飞书优化

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
@@ -1,22 +1,25 @@
 package com.erp.server.workflow.controller.fsCallback;
 
-import cn.hutool.core.util.IdUtil;
 import com.alibaba.fastjson.JSON;
-import com.common.core.controller.vo.ApiResult;
-import com.common.core.enums.ApiError;
-import com.common.message.constant.RocketMqTopic;
-import com.common.message.enums.RocketMqTagEnum;
 import com.common.message.service.mq.MQProducerService;
 import com.erp.model.workflow.dto.FsCallbackApiReqDTO;
 import com.erp.model.workflow.dto.FsCallbackApiRespDTO;
 import com.erp.server.workflow.handler.CfgApproveSyncCallbackHandler;
-import jodd.util.StringUtil;
+import com.lark.oapi.core.request.EventReq;
+import com.lark.oapi.event.CustomEventHandler;
+import com.lark.oapi.event.EventDispatcher;
+import com.lark.oapi.sdk.servlet.ext.ServletAdapter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @Slf4j
@@ -28,6 +31,9 @@ public class FsCallbackApiController {
 
     @Resource
     private CfgApproveSyncCallbackHandler handler;
+
+    @Resource
+    private ServletAdapter servletAdapter;
 
     @PostMapping("/approve")
     @ResponseBody
@@ -41,7 +47,27 @@ public class FsCallbackApiController {
     }
 
 
+    /**
+     * 注册事件 Register event
+     */
+    private static final EventDispatcher EVENT_HANDLER = EventDispatcher.newBuilder("verificationToken", "encryptKey")
+            .onCustomizedEvent("approval_instance", new CustomEventHandler() {
+                @Override
+                public void handle(EventReq event) throws Exception {
+                    System.out.printf("[ onCustomizedEvent access ], type: message, data: %s\n", new String(event.getBody(), StandardCharsets.UTF_8));
+                }
+            })
+            .build();
 
+    /**
+     * 创建路由处理器 Create route handler
+     */
+    @RequestMapping("/webhook/event")
+    public void event(HttpServletRequest request, HttpServletResponse response)
+            throws Throwable {
+        // 回调扩展包提供的事件回调处理器 Callback handler provided by the extension package
+        servletAdapter.handleEvent(request, response, EVENT_HANDLER);
+    }
 
 
 }
