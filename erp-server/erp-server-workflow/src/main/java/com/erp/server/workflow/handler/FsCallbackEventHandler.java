986ba8b437c7e9526e62e9a4773a41c9586909aa
Will <wangwei@ulanzi.cn>
2025-12-11 13:22:14 +0800

    feat,飞书优化

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/handler/FsCallbackEventHandler.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/handler/FsCallbackEventHandler.java
@@ -1,12 +1,17 @@
 package com.erp.server.workflow.handler;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.ObjUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.common.business.constant.DmpPullConstant;
 import com.common.business.enums.ApprovePlatformEnum;
 import com.common.core.exception.ServiceException;
 import com.erp.model.dmp.dto.DmpInoutDTO;
 import com.erp.model.dmp.enums.DmpInputTaskTaskTypeEnum;
+import com.erp.model.workflow.dto.FsCallbackEventDTO;
 import com.erp.rpc.dmp.feign.DmpInoutTaskFeign;
 import com.erp.sdk.fs.config.FsProperties;
 import com.lark.oapi.core.request.EventReq;
@@ -40,18 +45,20 @@ public class FsCallbackEventHandler {
 
     private EventDispatcher eventDispatcher;
 
+    private final static String APPROVAL_INSTANCE_EVENT = "approval_instance";
+
     @PostConstruct
     public void init() {
         /**
          * 注册事件 Register event
          */
-        this.eventDispatcher = EventDispatcher.newBuilder(fsProperties.getAppSecret(), null)
-                .onCustomizedEvent("approval_instance", new CustomEventHandler() {
+        this.eventDispatcher = EventDispatcher.newBuilder(fsProperties.getVerificationToken(), null)
+                .onCustomizedEvent(APPROVAL_INSTANCE_EVENT, new CustomEventHandler() {
                     @Override
                     public void handle(EventReq event) throws Exception {
                         log.warn("收到飞书审批实例自定义事件: {}", Jsons.DEFAULT.toJson(event));
                         // 处理审批实例事件的逻辑
-
+                        approvalInstanceHandle(event);
                     }
                 })
                 .build();
@@ -63,15 +70,30 @@ public class FsCallbackEventHandler {
      * @date 2025/12/11 11:47
      * @return void
      */
-    private void ss () {
+    private void approvalInstanceHandle (EventReq event) {
+        String plain = event.getPlain();
+        JSONObject jsonObject = JSON.parseObject(plain);
+        JSONObject thisEvent = jsonObject.getJSONObject("event");
+        if (ObjUtil.isEmpty(thisEvent)) {
+            return;
+        }
+        FsCallbackEventDTO.ApprovalInstanceEventDTO bean = BeanUtil.toBean(thisEvent, FsCallbackEventDTO.ApprovalInstanceEventDTO.class);
+        if (!CharSequenceUtil.equals(fsProperties.getAppId(), bean.getAppId())) {
+            log.warn("飞书审批实例自定义事件，应用ID未匹配，跳过处理，appId={}", bean.getAppId());
+            return;
+        }
+        if (!CharSequenceUtil.equals(APPROVAL_INSTANCE_EVENT,bean.getType())) {
+            log.warn("飞书审批实例自定义事件，事件类型未匹配，跳过处理，type={}", bean.getType());
+            return;
+        }
         //根据审批定义和审批实例id生成中台即时拉取任务
         DmpInoutDTO.CreateInputDTO dto = new DmpInoutDTO.CreateInputDTO();
         dto.setSystemCode(ApprovePlatformEnum.FEI_SHU.getCode());
         dto.setBillType(DmpPullConstant.INSTANCE_IDS);
-        dto.setNextLevelId("");
+        dto.setNextLevelId(bean.getApprovalCode());
         dto.setTaskType(DmpInputTaskTaskTypeEnum.HOTFIX.getCode());
         // 手动指定创建审批实例id
-        Map<String, Object> map = Collections.singletonMap("instanceId","");
+        Map<String, Object> map = Collections.singletonMap("instanceId",bean.getInstanceCode());
         dto.setDetailExtendJson(JSON.toJSONString(map));
 
         List<String> requestList = new ArrayList<>();
