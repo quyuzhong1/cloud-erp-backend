316a1b734dbe9f3452f213de0345405aa4a5e310
liuruipeng <arthur@ulanzi.cn>
2025-12-11 15:41:44 +0800

    退货入库单更新问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoReturnInstockDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoReturnInstockDetailServiceImpl.java
@@ -369,8 +369,8 @@ public class SoReturnInstockDetailServiceImpl extends SuperServiceImpl<SoReturnI
                 detailEntity.setSkuNo(skuVO.getSkuNo());
                 detailEntity.setRealQty(detailDto.getRealQty());
                 detailEntity.setReceiveQty(detailDto.getReceiveQty());
-                detailEntity.setReturnTypeDict(soReturnReceiveDetailEntity.getReturnTypeDict());
-                detailEntity.setReturnReasonDict(soReturnReceiveDetailEntity.getReturnReasonDict());
+                detailEntity.setReturnTypeDict(detailDto.getReturnTypeDict());
+                detailEntity.setReturnReasonDict(detailDto.getReturnReasonDict());
                 detailEntity.setWarehouseLocation(detailDto.getWarehouseLocation());
                 detailEntity.setRemark(detailDto.getRemark());
                 detailEntity.setSourceDetailId(detailDto.getSourceDetailId());
