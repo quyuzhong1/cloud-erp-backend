d507aa2b801da8ac9172afb4a4de7e67fff1fd43
Will <wangwei@ulanzi.cn>
2025-12-15 15:21:57 +0800

    feat,飞书添加事件

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/fsCallback/FsCallbackApiController.java
@@ -8,12 +8,12 @@ import com.erp.server.workflow.handler.CfgApproveSyncCallbackHandler;
 import com.erp.server.workflow.handler.FsCallbackEventHandler;
 import com.lark.oapi.sdk.servlet.ext.ServletAdapter;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -64,7 +64,14 @@ public class FsCallbackApiController {
     @PostMapping("/webhook/event")
     public void event(HttpServletRequest request, HttpServletResponse response)
             throws Throwable {
-        log.warn("飞书事件触发器:url:{},method:{}，body:{}", request.getRequestURL(), request.getMethod(), IOUtils.toString(request.getInputStream(), request.getCharacterEncoding()));
+        // 包装请求，使其可以重复读取body
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+
+        // 第一次读取（用于日志）
+        byte[] requestBodyBytes = wrappedRequest.getContentAsByteArray();
+        String requestBody = new String(requestBodyBytes, wrappedRequest.getCharacterEncoding());
+
+        log.warn("飞书事件触发器:url:{},method:{}，body:{}", request.getRequestURL(), request.getMethod(), requestBody);
         // 回调扩展包提供的事件回调处理器
         servletAdapter.handleEvent(request, response, fsCallbackEventHandler.getEventHandler());
     }
