27b070b85ebd97a51629b668548bbc8c84ef4e99
吴天任 <981404651@qq.com>
2025-12-15 16:01:11 +0800

    修改销售变更单回写销售出库单价格

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockDetailServiceImpl.java
@@ -1119,7 +1119,7 @@ public class SoOutstockDetailServiceImpl extends SuperServiceImpl<SoOutstockDeta
             if (ObjectUtils.isEmpty(soDetailEntity)) {
                 continue;
             }
-            BigDecimal price=soDetailEntity.getPrice();
+            BigDecimal price=MathUtil.multiplyWithTwo(soDetailEntity.getPrice(),soDetailEntity.getBoxQty());
             //单价信息
             detailEntity.setPrice(price);
             BigDecimal exchangeRate=soDetailEntity.getExchangeRate();
