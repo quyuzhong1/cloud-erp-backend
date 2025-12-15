ad3a9022bc406bd95f3ebac4694943e8711330b3
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-12-15 18:30:12 +0800

    产品确认-店铺推送类型改为匹配店铺客户id/店铺id

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/entity/LogisticsThirdChannelRefDetailEntity.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/entity/LogisticsThirdChannelRefDetailEntity.java
@@ -50,6 +50,11 @@ public class LogisticsThirdChannelRefDetailEntity extends BaseEntity<LogisticsTh
     @TableField("shop_name")
     private String shopName;
     /**
+     * 客户id
+     */
+    @TableField("customer_id")
+    private String customerId;
+    /**
     * 平台
     */
     @TableField("dict_platform")
