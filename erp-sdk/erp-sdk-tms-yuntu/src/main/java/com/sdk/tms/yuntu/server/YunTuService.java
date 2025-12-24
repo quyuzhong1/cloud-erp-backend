f8a38ca3ccc281f79eb67ed7b219d58ae2da86d2
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-12-24 17:33:21 +0800

    Revert "临时模拟云途打印异常"
    This reverts commit 37965f2dd2515194e07e77c369cf07b242c582b3.

--- erp-sdk/erp-sdk-tms-yuntu/src/main/java/com/sdk/tms/yuntu/server/YunTuService.java
+++ erp-sdk/erp-sdk-tms-yuntu/src/main/java/com/sdk/tms/yuntu/server/YunTuService.java
@@ -77,13 +77,12 @@ public class YunTuService {
      *  标签打印
      */
     public YunTuResponse<List<YunTuPrintLabel>> getPrintLabel(@Valid YunTuPrintLabelRequest request,Map<String, String> authMap){
-        throw new ServiceException("模拟打印接口异常");
-//        String appKey = authMap.get(CLIENT_ID);
-//        String appSecret = authMap.get(CLIENT_SECRET);
-//        String url = authMap.get("url");
-//        validate(appKey,appSecret,url);
-//        String response = YunTuUtils.sendPostList(url,YunTuConstants.METHOD_PRINT_LABEL,request.getOrderNumbers(),appKey,appSecret);
-//        return JSON.parseObject(response,new TypeReference<YunTuResponse<List<YunTuPrintLabel>>>() {}.getType());
+        String appKey = authMap.get(CLIENT_ID);
+        String appSecret = authMap.get(CLIENT_SECRET);
+        String url = authMap.get("url");
+        validate(appKey,appSecret,url);
+        String response = YunTuUtils.sendPostList(url,YunTuConstants.METHOD_PRINT_LABEL,request.getOrderNumbers(),appKey,appSecret);
+        return JSON.parseObject(response,new TypeReference<YunTuResponse<List<YunTuPrintLabel>>>() {}.getType());
     }
 
     /**
