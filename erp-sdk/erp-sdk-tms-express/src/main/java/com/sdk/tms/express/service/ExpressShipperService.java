6880b6a1893c04308712ba5b5e6f3aeaeeddc489
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2025-12-24 18:41:27 +0800

    Revert "回退云途修改"
    This reverts commit 0071e67b3c77d76f6ccfc8d52c32e9ba3c302b49.

--- erp-sdk/erp-sdk-tms-express/src/main/java/com/sdk/tms/express/service/ExpressShipperService.java
+++ erp-sdk/erp-sdk-tms-express/src/main/java/com/sdk/tms/express/service/ExpressShipperService.java
@@ -115,13 +115,12 @@ public class ExpressShipperService {
      * @throws UnsupportedEncodingException
      */
     public BaseResult getLabel(Map<String, String> authMap, OrderLabelRequest orderLabelRequest) throws UnsupportedEncodingException {
-        throw new ServiceException("模拟打印接口异常");
-//        String partnerId = authMap.get(CLIENT_ID);
-//        String md5Key = authMap.get(CLIENT_SECRET);
-//        String url = authMap.get("url");
-//        validate(partnerId, md5Key, url);
-//        IServiceCodeStandard standardService = ExpressServiceCodeEnum.COM_RECE_CLOUD_PRINT_WAYBILLS; //面单打印
-//        return doPost(url, partnerId, md5Key, JSONUtil.toJsonStr(orderLabelRequest), standardService.getCode());
+        String partnerId = authMap.get(CLIENT_ID);
+        String md5Key = authMap.get(CLIENT_SECRET);
+        String url = authMap.get("url");
+        validate(partnerId, md5Key, url);
+        IServiceCodeStandard standardService = ExpressServiceCodeEnum.COM_RECE_CLOUD_PRINT_WAYBILLS; //面单打印
+        return doPost(url, partnerId, md5Key, JSONUtil.toJsonStr(orderLabelRequest), standardService.getCode());
     }
 
     /**
