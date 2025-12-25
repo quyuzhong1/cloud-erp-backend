d391bb0a6ea924404e318649836c4333922f413f
jack chen <jackchen@ulanzi.cn>
2025-12-25 12:22:08 +0800

    feat(scm): 添加采购价格实体的付款条件名称字段

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/entity/PurchasePriceEntity.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/entity/PurchasePriceEntity.java
@@ -121,6 +121,11 @@ public class PurchasePriceEntity extends BaseEntity<PurchasePriceEntity> {
      */
     @TableField("payment_condition")
     private String paymentCondition;
+    /**
+     * 付款条件
+     */
+    @TableField("payment_condition_name")
+    private String paymentConditionName;
 
     /**
      * 明细ids
