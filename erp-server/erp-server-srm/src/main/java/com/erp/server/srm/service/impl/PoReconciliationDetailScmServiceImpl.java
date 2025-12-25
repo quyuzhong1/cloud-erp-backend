99b141ed215aa2695029d165ac4266a4df43e1ed
Will <wangwei@ulanzi.cn>
2025-12-25 18:10:18 +0800

    feat,对账单优化调整

--- erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PoReconciliationDetailScmServiceImpl.java
+++ erp-server/erp-server-srm/src/main/java/com/erp/server/srm/service/impl/PoReconciliationDetailScmServiceImpl.java
@@ -601,10 +601,10 @@ public class PoReconciliationDetailScmServiceImpl extends SuperServiceImpl<PoRec
                     .map(PoReconciliationRefDetailEntity::getQty).reduce(MathUtil.ZERO,Integer::sum);
             if (Objects.equals(detailEntity.getQty(), totalQty)) {
                 detailEntity.setStatus(PoReconciliationDetailEnum.StatusEnum.RECONCILED.getCode());
-            } else if (Math.abs(detailEntity.getQty()) > Math.abs(totalQty)) {
-                detailEntity.setStatus(PoReconciliationDetailEnum.StatusEnum.PART_RECONCILIATION.getCode());
-            } else {
+            } else if (totalQty == 0) {
                 detailEntity.setStatus(PoReconciliationDetailEnum.StatusEnum.WAIT_RECONCILIATION.getCode());
+            } else {
+                detailEntity.setStatus(PoReconciliationDetailEnum.StatusEnum.PART_RECONCILIATION.getCode());
             }
         }
         super.updateBatchById(poReconciliationDetailList);
