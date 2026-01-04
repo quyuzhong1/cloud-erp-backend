c6eebf80cc44fc1b1bd6c978c8d8a8666accc9f4
jack chen <jackchen@ulanzi.cn>
2026-01-04 18:37:01 +0800

    feat(mold): 通知配置添加模具监控功能支持

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/SourceTypeEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/SourceTypeEnum.java
@@ -184,6 +184,7 @@ public enum SourceTypeEnum {
     ASSET_PURCHASE_CHANGE("assetPurchaseChange","资产采购变更单","asset_purchase_change"),
     CFG_MOLD_RETURN_ALERT_RULE("cfgMoldReturnAlertRule","资产采购变更单","cfg_mold_return_alert_rule"),
     CFG_MOLD_ALERT_RULE("cfgMoldAlertRule","资产采购变更单","cfg_mold_alert_rule"),
+    MOLD_MONITOR("moldMonitor", "模具监控","mold_monitor"),
 
     //SYS
     SYS_DEPARTMENT("sysDepartment", "部门","sys_department"),
