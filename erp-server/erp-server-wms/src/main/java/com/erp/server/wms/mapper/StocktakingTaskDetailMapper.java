add34219bd150c275b56560a70f2b20ef0178fd1
Cloud <geyunf2016@163.com>
2026-01-04 12:32:33 +0800

    【feat stocktasking sql error update  】

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/StocktakingTaskDetailMapper.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/StocktakingTaskDetailMapper.java
@@ -1,8 +1,8 @@
 package com.erp.server.wms.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.erp.model.wms.dto.StocktakingTaskDetailDTO;
 import com.erp.model.wms.entity.StocktakingTaskDetailEntity;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -22,12 +22,15 @@ public interface StocktakingTaskDetailMapper extends BaseMapper<StocktakingTaskD
     List<StocktakingTaskDetailDTO.ExportDTO> listExportByMainId(@Param("mainId") String mainId);
 
 
-    /**
-     * 根据仓库id、组织id、skuId列表查询每个sku的最新盘点时间
-     * @param warehouseIds 仓库id列表
-     * @param orgIds 组织id列表
-     * @param skuIds skuId列表
-     * @return 每个sku的最新盘点时间列表
+    /** 根据参数获取最新的盘点日期
+     * @author yl
+     * @date 2023-09-05 10:28
+     * @param warehouseIdList
+     * @param orgIdList
+     * @param skuIdList
+     * @return java.util.List<com.erp.model.wms.dto.StocktakingTaskDetailDTO.LastDTO>
      */
-    List<StocktakingTaskDetailDTO.LastDTO> maxDateByParams(List<String> warehouseIds, List<String> orgIds, List<String> skuIds);
+    List<StocktakingTaskDetailDTO.LastDTO> maxDateByParams(@Param("warehouseIdList")List<String> warehouseIdList,
+                                                           @Param("orgIdList") List<String> orgIdList,
+                                                           @Param("skuIdList") List<String> skuIdList);
 }
