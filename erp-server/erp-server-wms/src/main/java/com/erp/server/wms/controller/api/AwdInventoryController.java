4822b0ffe09079cf5169caf46eb087d39e6c73c1
吴天任 <981404651@qq.com>
2026-01-08 18:17:15 +0800

    awd库存查询问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/AwdInventoryController.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/controller/api/AwdInventoryController.java
@@ -1,32 +1,25 @@
 package com.erp.server.wms.controller.api;
 
 
-import com.erp.model.wms.dto.FbaInventoryDTO;
+import com.common.business.annotation.WebAdvanceQuery;
+import com.erp.server.wms.query.AwdInventoryQueryHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import javax.annotation.Resource;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.common.core.anno.LogAction;
 import com.common.core.anno.LogSystemModule;
-import com.common.core.anno.LogViewService;
 import com.common.core.enums.LogActionEnum;
 import com.common.business.dto.base.*;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.common.core.controller.BaseController;
 import com.erp.server.wms.service.AwdInventoryService;
 import com.common.core.controller.vo.ApiResult;
 import com.common.business.vo.PagingVO;
-import com.common.business.dto.base.*;
-import cn.hutool.core.util.ObjectUtil;
 import com.common.business.annotation.DataPermission;
 import com.common.business.enums.DataAttributeEnum;
 import com.erp.model.wms.dto.AwdInventoryDTO;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
-import java.util.stream.Collectors;
-import com.erp.model.wms.entity.AwdInventoryEntity;
 
 /**
  * 
@@ -56,6 +49,7 @@ public class AwdInventoryController extends BaseController {
             menuCode = "wms:awdInventory:paging",
             tableAlias = "ai"
     )
+    @WebAdvanceQuery(handler = AwdInventoryQueryHandler.class)
     public ApiResult<PagingVO<AwdInventoryDTO.ListDTO>> paging(@RequestBody @Validated PagingDTO<AwdInventoryDTO.PagingParamDTO> dto) {
         return success(awdInventoryService.paging(dto));
     }
