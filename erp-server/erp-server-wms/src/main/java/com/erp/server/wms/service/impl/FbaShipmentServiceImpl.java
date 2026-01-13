22e471ea51596ecef91ffd020e788d7c172ec291
吴天任 <981404651@qq.com>
2026-01-13 16:03:33 +0800

    修改sku映射

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FbaShipmentServiceImpl.java
@@ -220,28 +220,30 @@ public class FbaShipmentServiceImpl extends SuperServiceImpl<FbaShipmentMapper, 
         dto.setShopId(entity.getShopId());
         dto.setPlatform(PlatformDictEnum.AMAZON.getCode());
         SkuMappingDTO.MappingSkuViewDTO mappingSkuViewDTO = skuDTOS.get(0);
+
+        //更新awd的sku映射
+        FbaShipmentEntity fbaShipmentEntity = this.getById(detailEntity.getMainId());
+        AwdOutstockEntity awdOutstockEntity = awdOutstockService.lambdaQuery()
+                .eq(AwdOutstockEntity::getFbaShipmentId,fbaShipmentEntity.getId())
+                .one();
+        List<AwdOutstockDetailEntity> detailList = awdOutstockDetailService.lambdaQuery()
+                .eq(AwdOutstockDetailEntity::getMainId, awdOutstockEntity.getId())
+                .list();
+        for (AwdOutstockDetailEntity awdOutstockDetailEntity : detailList) {
+            if (awdOutstockDetailEntity.getMsku().equals(detailEntity.getMsku())) {
+                awdOutstockDetailService.lambdaUpdate()
+                        .set(AwdOutstockDetailEntity::getSkuId,mappingSkuViewDTO.getProductSkuId())
+                        .set(AwdOutstockDetailEntity::getSkuNo,mappingSkuViewDTO.getProductSkuNo())
+                        .set(AwdOutstockDetailEntity::getAsin,mappingSkuViewDTO.getPlatformSpuNo())
+                        .set(AwdOutstockDetailEntity::getProductName,mappingSkuViewDTO.getProductName())
+                        .eq(AwdOutstockDetailEntity::getId,awdOutstockDetailEntity.getId())
+                        .update();
+            }
+        }
+
         dto.setId(mappingSkuViewDTO.getId());
         Boolean flag = omsListingInfoFeign.skuMapping(dto);
         if (flag) {
-            //更新awd的sku映射
-            FbaShipmentEntity fbaShipmentEntity = this.getById(detailEntity.getMainId());
-            AwdOutstockEntity awdOutstockEntity = awdOutstockService.lambdaQuery()
-                    .eq(AwdOutstockEntity::getFbaShipmentId,fbaShipmentEntity.getId())
-                    .one();
-            List<AwdOutstockDetailEntity> detailList = awdOutstockDetailService.lambdaQuery()
-                    .eq(AwdOutstockDetailEntity::getMainId, awdOutstockEntity.getId())
-                    .list();
-            for (AwdOutstockDetailEntity awdOutstockDetailEntity : detailList) {
-                if (awdOutstockDetailEntity.getMsku().equals(detailEntity.getMsku())) {
-                    awdOutstockDetailService.lambdaUpdate()
-                            .set(AwdOutstockDetailEntity::getSkuId,mappingSkuViewDTO.getProductSkuId())
-                            .set(AwdOutstockDetailEntity::getSkuNo,mappingSkuViewDTO.getProductSkuNo())
-                            .set(AwdOutstockDetailEntity::getAsin,mappingSkuViewDTO.getPlatformSpuNo())
-                            .set(AwdOutstockDetailEntity::getProductName,mappingSkuViewDTO.getProductName())
-                            .eq(AwdOutstockDetailEntity::getId,awdOutstockDetailEntity.getId())
-                            .update();
-                }
-            }
 
             detailEntity.setSkuNo(dto.getSkuNo());
 
