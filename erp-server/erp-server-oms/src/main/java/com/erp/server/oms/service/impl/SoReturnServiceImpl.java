09a3ada14950b324c613617671b3da8400c9f6b8
liuruipeng <arthur@ulanzi.cn>
2026-01-16 18:54:08 +0800

    旺店通退货单时间为空处理

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoReturnServiceImpl.java
@@ -1426,11 +1426,9 @@ public class SoReturnServiceImpl extends SuperServiceImpl<SoReturnMapper, SoRetu
         soB2cReturnDetailEntityList.forEach(v->v.setMainId(soReturn.getId()));
         soReturnDetailService.saveBatch(soB2cReturnDetailEntityList);
         this.submit(soReturn,false);
-        if(Objects.nonNull(soReturn.getBillDate())){
-            ApproveOneDTO dto = new ApproveOneDTO();
-            dto.setType(ApproveTypeEnum.PASS.getStatus());
-            this.approveEnd(dto,soReturn);
-        }
+        ApproveOneDTO dto = new ApproveOneDTO();
+        dto.setType(ApproveTypeEnum.PASS.getStatus());
+        this.approveEnd(dto,soReturn);
     }
 
     @Override
