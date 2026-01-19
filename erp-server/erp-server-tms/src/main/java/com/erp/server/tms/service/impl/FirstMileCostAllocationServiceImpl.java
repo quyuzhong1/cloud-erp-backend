cddfc85dc591e580282fd447b2b5c448170a4090
jack chen <jackchen@ulanzi.cn>
2026-01-19 09:34:11 +0800

    refactor(tms): 优化首公里成本分配服务中的组织ID逻辑
    - 重构了组织ID获取的条件判断逻辑
    - 添加了对物流供应商组织类型的处理分支
    - 增加了代码注释以提高可读性
    - 保持了原有的单据成本组织逻辑
    - 完善了不同组织类型的选择机制

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileCostAllocationServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/FirstMileCostAllocationServiceImpl.java
@@ -587,7 +587,16 @@ public class FirstMileCostAllocationServiceImpl extends SuperServiceImpl<FirstMi
                     .collect(Collectors.toList());
         }
         //当前组织
-        String orgId = CharSequenceUtil.isBlank(allocationSettingDTO.getFirstOrgId()) || Objects.equals(CostAllocationOrgTypeEnum.BILL_ORG.getCode(),allocationSettingDTO.getFirstOrgId()) ? reportPeriodMonth.getOrgId() :  allocationSettingDTO.getFirstOrgId();
+        String orgId = "";
+        if(CharSequenceUtil.isBlank(allocationSettingDTO.getFirstOrgId()) || Objects.equals(CostAllocationOrgTypeEnum.BILL_ORG.getCode(),allocationSettingDTO.getFirstOrgId())){
+            //单据成本组织
+            orgId = reportPeriodMonth.getOrgId();
+        }else if(Objects.equals(CostAllocationOrgTypeEnum.LOGISTICS_SUPPLIER_ORG.getCode(),allocationSettingDTO.getFirstOrgId())){
+            //物流组织
+
+        }else {
+            orgId = allocationSettingDTO.getFirstOrgId();
+        }
         String toWarehouseId = CharSequenceUtil.isNotBlank(allocationSettingDTO.getFirstWarehouseId()) ? allocationSettingDTO.getFirstWarehouseId() : entity.getFromWarehouseId();
         //sku成本
         List<InventorySkuCostDTO.PagingVO> skuCostList = inventorySkuCostService.listDetailByOrgIdAndSkuIds(orgId, skuIds, ApproveStatusEnum.APPROVE.getStatus(), reportPeriodMonth.getMonth(),toWarehouseId);
