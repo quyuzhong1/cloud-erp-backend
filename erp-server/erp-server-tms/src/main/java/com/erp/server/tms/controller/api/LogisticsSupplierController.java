e977e18358730278b4e574ad0f3867e7c0681de9
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2026-01-22 16:53:48 +0800

    供应商关联渠道下拉框增加传参

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/controller/api/LogisticsSupplierController.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/controller/api/LogisticsSupplierController.java
@@ -224,9 +224,19 @@ public class LogisticsSupplierController extends BaseController {
      * 物流商渠道树形结构
      * @return
      */
+    @Deprecated
     @GetMapping("/tree")
     public ApiResult<List<LogisticsSupplierDTO.ListChildTreeDTO>> tree(){
         return success(logisticsSupplierService.tree());
+    }
+
+    /**
+     * 物流商渠道树形结构
+     * @return
+     */
+    @PostMapping("/tree")
+    public ApiResult<List<LogisticsSupplierDTO.ListChildTreeDTO>> listSupplierTree(@RequestBody LogisticsSupplierDTO.SelectDTO dto){
+        return success(logisticsSupplierService.listSupplierTree(dto));
     }
 
 
