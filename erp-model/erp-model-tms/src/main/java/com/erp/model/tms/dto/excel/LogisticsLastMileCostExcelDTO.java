03dcef0fab6a0fd6f9e1d5e63c21a5dbbebc0274
Will <wangwei@ulanzi.cn>
2026-01-29 16:55:56 +0800

    feat,费用分摊二期调整

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/excel/LogisticsLastMileCostExcelDTO.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/excel/LogisticsLastMileCostExcelDTO.java
@@ -20,7 +20,7 @@ public class LogisticsLastMileCostExcelDTO implements Serializable {
      * 物流商
      */
     @ExcelProperty(value = "物流商", index = 0)
-    @FieldValid(fieldName = "物流商", isNotBlank = true, maxLength = 100)
+    @FieldValid(fieldName = "物流商",  maxLength = 100)
     private String logisticsSupplierName;
 
     /**
