67e710ed02cc828148c9b8cf34459a4c1206bd3e
wuhaotian <wuhaotian@ulanzi.cn>
2026-02-02 17:34:18 +0800

    feat(oms),【B2C销售订单】订单重试达到最大次数还是失败后，需要增加一个日志记录，记录“系统有重试，超过了最大次数”

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/schedule/SoB2cRetryJob.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/schedule/SoB2cRetryJob.java
@@ -191,6 +191,10 @@ public class SoB2cRetryJob {
                                 soB2cErrorEntity.getMainId(),
                                 ExceptionUtil.stacktraceToString(e)
                         );
+                        // 重试达到最大次数仍失败时，记录到操作日志
+                        if (soB2cErrorEntity.getRetryCount() + 1 > maxRetryCount) {
+                            operateLogService.addModuleOperateLog("系统有重试，超过了最大次数", ModuleTypeEnum.SO_B2C.getCode(), soB2cErrorEntity.getMainId(), "重试失败");
+                        }
                         // 成功重新记录重试数量任务
                         boolean update = soB2cErrorService.lambdaUpdate()
                                 .set(SoB2cErrorEntity::getRetryCount, soB2cErrorEntity.getRetryCount() + 1)
