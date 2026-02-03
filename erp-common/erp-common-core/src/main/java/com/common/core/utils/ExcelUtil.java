f881cc737c7446055104ebf664021e2199939009
Will <wangwei@ulanzi.cn>
2026-02-03 15:37:15 +0800

    feat,导入调整

--- erp-common/erp-common-core/src/main/java/com/common/core/utils/ExcelUtil.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/utils/ExcelUtil.java
@@ -732,7 +732,7 @@ public class ExcelUtil {
             File filePath = new File(tempDirectory,"template"+ LocalDate.now() +".xlsx");
             File tempFile = File.createTempFile(fileName, ".xlsx");
             if (!filePath.exists()) {
-                EasyExcel.write(filePath).head(hs).sheet(fileName).doWrite(new ArrayList<>());
+                EasyExcel.write(filePath).sheet(fileName).doWrite(new ArrayList<>());
             }
             if (list2.size() > BATCH_COUNT){
                 List<List<List<String>>> partition = ListUtil.partition(list2, BATCH_COUNT);
