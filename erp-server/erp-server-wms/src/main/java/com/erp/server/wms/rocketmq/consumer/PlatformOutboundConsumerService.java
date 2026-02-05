0e151b98f86a8a5eba2aed7238cdd71911c7f808
jack chen <jackchen@ulanzi.cn>
2026-02-05 10:03:27 +0800

    fix(wms): 修复第三方出库单处理中的B2C销售订单验证问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformOutboundConsumerService.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/rocketmq/consumer/PlatformOutboundConsumerService.java
@@ -182,6 +182,14 @@ public class PlatformOutboundConsumerService<T extends DmpSyncTaskIdDTO> extends
                 }
                 String soCode = thirdWarehouseDeliveryEntity.getSoCode();
                 SoB2cEntity mainEntity = soB2cFeign.getSoCode(soCode);
+                if(Objects.isNull(mainEntity)){
+                    log.error("第三方出库单: 未找到B2C销售订单 >>>>>>>{}",JSONUtil.toJsonStr(dto));
+                    return ApiResult.success();
+                }
+                if( Objects.equals(mainEntity.getInvalidStatus(),true)){
+                    log.error("第三方出库单: B2C销售订单已作废 >>>>>>>{}",JSONUtil.toJsonStr(dto));
+                    return ApiResult.success();
+                }
 
                 SoB2cDTO.UpdateStatusDTO updateStatus = new SoB2cDTO.UpdateStatusDTO();
                 updateStatus.setSoCode(mainEntity.getCode());
@@ -206,6 +214,10 @@ public class PlatformOutboundConsumerService<T extends DmpSyncTaskIdDTO> extends
                     return ApiResult.success();
                 }
                 log.error("第三方出库单: 未找到B2C销售订单 >>>>>>>{}",JSONUtil.toJsonStr(dto));
+                return ApiResult.success();
+            }
+            if( Objects.equals(mainEntity.getInvalidStatus(),true)){
+                log.error("第三方出库单: B2C销售订单已作废 >>>>>>>{}",JSONUtil.toJsonStr(dto));
                 return ApiResult.success();
             }
             ThirdWarehouseDeliveryEntity thirdWarehouseDeliveryEntity = thirdWarehouseDeliveryService.getByCodeAndSoId(mainEntity.getShippingOrderNo(),mainEntity.getId());
