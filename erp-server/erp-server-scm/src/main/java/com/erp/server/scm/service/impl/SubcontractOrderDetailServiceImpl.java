f3c3a029488422b2d26bc5dc30fa15e6845c660e
吴天任 <981404651@qq.com>
2026-02-12 11:02:32 +0800

    修改委外订单数量校验和同步金蝶逻辑

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderDetailServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/SubcontractOrderDetailServiceImpl.java
@@ -754,7 +754,7 @@ public class SubcontractOrderDetailServiceImpl extends SuperServiceImpl<Subcontr
                     break;
                 }
 
-                if (detailEntity.getRepairQty() % childEntity.getQty() == 0 || childEntity.getQty()  % detailEntity.getRepairQty() == 0) {
+                if (detailEntity.getRepairQty() % childEntity.getQty() != 0 && childEntity.getQty()  % detailEntity.getRepairQty() != 0) {
                     errorMessages.add(StrUtil.format("父级SKU【{}】和子件SKU【{}】数量需要为整数倍", skuVO.getSkuNo(), childSkuVO.getSkuNo()));
                     hasChildError = true;
                     break;
