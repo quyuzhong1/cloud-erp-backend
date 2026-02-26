07f2d7d6173c40958d835250e40cd418adfa72db
jack chen <jackchen@ulanzi.cn>
2026-02-26 09:49:50 +0800

    fix(tms): 物流系统配置 +1 --小包费用分摊单据类型增加售后单据

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/enums/CostAllocationBillTypeEnum.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/enums/CostAllocationBillTypeEnum.java
@@ -10,9 +10,9 @@ import com.fasterxml.jackson.annotation.JsonValue;
  * @Created jack
  */
 public enum CostAllocationBillTypeEnum implements EnumMessage {
-    SO_B2C( "soB2c", "B2C销售订单"),
-    SO_INFO("soInfo", "B2B销售订单"),
-    AFTER_SALE("afterSale", "售后申请"),
+    B2C( "B2C", "B2C销售订单"),
+    B2B("B2B", "B2B销售订单"),
+    AFTER_SALE("AfterSale", "售后订单-补/换/赠"),
     OTHER("other", "其他"),
     ;
 
