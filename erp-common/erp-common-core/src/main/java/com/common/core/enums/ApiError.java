64de3b570eaff26d30da26a19be601a4d2cdf7ec
吴天任 <981404651@qq.com>
2026-02-28 12:11:07 +0800

    --修改盘点任务是否允许下推状态 --修改盘点任务下推异常消息

--- erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
+++ erp-common/erp-common-core/src/main/java/com/common/core/enums/ApiError.java
@@ -1408,6 +1408,7 @@ public enum ApiError implements Serializable {
     WH_STOCKTAKING_PUSH_OVER(11133,"【{0}】已生成盘盈/亏单{1}，不允许再次生成"),
     WH_STOCKTAKING_APPROVE_BILL_DATE_NEED_GREATER_THAN_TODAY(11134,"盘点日期不能小于当前日期，请修改后重新审核"),
     WH_STOCKTAKING_PROFIT_LOSS_NOT_ALLOW_UPDATE(11135,"下推生成的盘盈盘亏单不允许修改"),
+    WH_STOCKTAKING_NOT_NEED_PUSH(11136,"【{0}】无需下推盘盈/亏单{1}"),
 
     /**
      * 头程发货单 错误 信息 11500-12000
