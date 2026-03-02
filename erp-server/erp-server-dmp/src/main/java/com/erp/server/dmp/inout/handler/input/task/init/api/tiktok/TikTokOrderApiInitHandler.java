c4192007bbb3890ef2f2f5abc8c3cfd8d5ae721d
shukai <shukai@ulanzi.cn>
2026-03-02 14:07:06 +0800

    add:创建时间

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/tiktok/TikTokOrderApiInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/api/tiktok/TikTokOrderApiInitHandler.java
@@ -112,8 +112,8 @@ public class TikTokOrderApiInitHandler implements DmpInputApiInitHandler {
             //请求body，平台用于计算签名
             Map<String, Object> bodyMap = new HashMap<>();
             if(StringUtils.isNotBlank(platformOrderCreateTime)) {
-            	bodyMap.put("update_time_ge", Long.valueOf(platformOrderCreateTime) - 5);
-                bodyMap.put("update_time_lt", Long.valueOf(platformOrderCreateTime) + 5);
+            	bodyMap.put("create_time_ge", Long.valueOf(platformOrderCreateTime) - 5);
+                bodyMap.put("create_time_lt", Long.valueOf(platformOrderCreateTime) + 5);
             }else {
             	bodyMap.put("update_time_ge", dmpInputApiInitRequest.getStartTime().toInstant(ZoneOffset.ofHours(8)).toEpochMilli() / 1000);
                 bodyMap.put("update_time_lt", dmpInputApiInitRequest.getEndTime().toInstant(ZoneOffset.ofHours(8)).toEpochMilli() / 1000);
