65622d43e43e8d8e9ea6615516248316e6b433f2
zhangdaoyin <zhangdaoyin@ulanzi.cn>
2026-03-06 15:16:01 +0800

    众包海外仓-产品拉取开发

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/PlatformDictEnum.java
@@ -71,6 +71,7 @@ public enum PlatformDictEnum implements EnumMessage {
     DHT("dht", "订货通", "订货通", "57"),
     QI_MEN("qimen", "奇门", "奇门", "52"),
     TONG_YOU_WAREHOUSE("tongyou", "通邮", "通邮", "62"),
+    ZHONG_BAO_WAREHOUSE("zhongbao", "众包", "众包", "63"),
 
     // 拼多多海外版
     TE_MU("TeMu", "TEMU半托管", "TEMU半托管", "034"),
