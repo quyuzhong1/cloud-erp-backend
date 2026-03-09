b2d3eb0ce6cfd43716d09ed7fed95b8f2c63ecff
Will <wangwei@ulanzi.cn>
2026-03-09 18:23:25 +0800

    feat,部门表添加逻辑删除

--- erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/DepartmentDTO.java
+++ erp-model/erp-model-sys/src/main/java/com/erp/model/sys/dto/DepartmentDTO.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -37,11 +37,11 @@ public class DepartmentDTO {
     /**
      * 创建时间
      */
-    private Date createTime;
+    private LocalDateTime createTime;
     /**
      * 更新时间
      */
-    private Date updateTime;
+    private LocalDateTime updateTime;
 
 
     private String parentId;
