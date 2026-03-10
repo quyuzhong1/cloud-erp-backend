8e3a36180280f7727c3d6c301f84c4585af2a836
shukai <shukai@ulanzi.cn>
2026-03-10 14:46:30 +0800

    add:修改为当前时间

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingTaskServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/StocktakingTaskServiceImpl.java
@@ -836,7 +836,7 @@ public class StocktakingTaskServiceImpl extends SuperServiceImpl<StocktakingTask
                 throw new ServiceException(ApiError.WH_STOCKTAKING_TASK_EXIST, warehouseName, item.getWarehouseLocation(), item.getSkuNo(), item.getDictInventoryStatus());
             }
             String redisKey = CharSequenceUtil.format(RedisKeyConstant.INVENTORY_LOCK, entity.getCode(), item.getOrgId(), item.getWarehouseId(), item.getWarehouseLocation(), item.getSkuId(), item.getDictInventoryStatus());
-            redisUtil.set(redisKey, planCode);
+            redisUtil.set(redisKey, cn.hutool.core.date.DateUtil.now());
         });
         // 3. 对库存记录进行分组，按照分单规则进行分组
         SeparateRuleEnum separateRule = entity.getSeparateRule();
