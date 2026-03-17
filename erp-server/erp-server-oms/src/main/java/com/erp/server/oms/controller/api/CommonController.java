e986793239fab9e8ece0264250c12c8242d97016
liuruipeng <arthur@ulanzi.cn>
2026-03-17 17:48:55 +0800

    测试代码

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/CommonController.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/controller/api/CommonController.java
@@ -69,6 +69,7 @@ public class CommonController extends BaseController {
         // 获取 SkyWalking TraceId
         String traceId = TraceContext.traceId();
         String segmentId = TraceContext.segmentId();
+        log.error("skywalking tid:{}",MDC.get("traceId"));
         return success("skywalking tid:"+traceId+"segmentId:"+segmentId);
 
     }
