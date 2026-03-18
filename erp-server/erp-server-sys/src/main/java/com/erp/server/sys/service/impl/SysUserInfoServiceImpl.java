5a1c42ec65ceba17367c9f9be60c2d63c2e7602b
Jim <zhoujingbo@ulanzi.cn>
2026-03-18 18:12:24 +0800

    update:移除新增用户老中台同步

--- erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/SysUserInfoServiceImpl.java
+++ erp-server/erp-server-sys/src/main/java/com/erp/server/sys/service/impl/SysUserInfoServiceImpl.java
@@ -234,14 +234,7 @@ public class SysUserInfoServiceImpl extends ServiceImpl<SysUserInfoMapper, SysUs
             sysDepartmentUserService.batchSaveOrUpdate(entity.getUid(),sysUserInfoDTO.getDepartmentIdList(),true);
 
             //同步金蝶员工数据
-            DmpPushTaskEntity pushTaskEntity = syncKingdeeSysUserInfoService.syncDataToKingdee(entity, SyncOperateEnum.OPERATE_APPROVE.getCode());
-            //推送金蝶
-            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronizationAdapter() {
-                @Override
-                public void afterCommit() {
-                    dmpMqFeign.sendTask(Arrays.asList(pushTaskEntity));
-                }
-            });
+            syncKingdeeSysUserInfoService.syncDataToKingdee(entity, SyncOperateEnum.OPERATE_APPROVE.getCode());
         }
     }
     @Override
