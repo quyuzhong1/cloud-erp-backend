648bdf7b50248d9d3b77be8b08c35da74445997b
吴天任 <981404651@qq.com>
2026-03-19 11:54:16 +0800

    修改日志

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ZhongBaoHandlerServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/ZhongBaoHandlerServiceImpl.java
@@ -339,9 +339,9 @@ public class ZhongBaoHandlerServiceImpl extends AbstractThirdWarehouseHandler {
     protected ApiResult<String> createFbaOutboundBill(ThirdWarehouseCreateFbaOutboundReq createOutboundReq) {
         OverseasOutboundCreateRequest overseasOutboundCreateRequest = buildCreateFbaOutboundDto(createOutboundReq);
         String token = AuthUtils.getToken(apiKey, apiSecret);
-        log.error(getPlatForm().getName() + "创建b2b出库单请求:{}", JSONUtil.toJsonStr(overseasOutboundCreateRequest));
+        log.warn(getPlatForm().getName() + "创建b2b出库单请求:{}", JSONUtil.toJsonStr(overseasOutboundCreateRequest));
         OverseasOutboundCreateResponse response = zhongbaoService.createOutboundBill(token, overseasOutboundCreateRequest);
-        log.error(getPlatForm().getName() + "创建b2b出库单结果:{}", JSONUtil.toJsonStr(response));
+        log.warn(getPlatForm().getName() + "创建b2b出库单结果:{}", JSONUtil.toJsonStr(response));
         return response.getCode() == "20000" ? success(response.getResponseData().getOrderNo()) : failure(response.getMessage());
     }
 
