e6a06bc6338d8351dbbc1c8ee56d07eff0c57baf
吴天任 <981404651@qq.com>
2026-03-25 12:11:26 +0800

    质检通知单代码

---
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/entity/QcSamplingPlanRefEntity.java
@@ -0,0 +1,71 @@
+package com.erp.model.wms.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.common.core.entity.BaseEntity;
+import com.baomidou.mybatisplus.annotation.TableField;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+import lombok.NoArgsConstructor;
+import com.common.business.enums.ApproveStatusEnum;
+
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author wtr
+ * @since 2026-03-25
+*/
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@NoArgsConstructor
+@TableName("qc_sampling_plan_ref")
+public class QcSamplingPlanRefEntity extends BaseEntity<QcSamplingPlanRefEntity> {
+
+    /**
+    * 质检单id
+    */
+    @TableField("main_id")
+    private String mainId;
+    /**
+    * 抽样方案id
+    */
+    @TableField("sampling_plan_id")
+    private String samplingPlanId;
+    /**
+    * 建议抽样数量
+    */
+    @TableField("suggest_sampling_qty")
+    private Integer suggestSamplingQty;
+    /**
+    * 附件url
+    */
+    @TableField("attach_url")
+    private String attachUrl;
+    /**
+    * 附件名称
+    */
+    @TableField("attach_name")
+    private String attachName;
+
+
+    public static final String MAIN_ID = "main_id";
+
+    public static final String SAMPLING_PLAN_ID = "sampling_plan_id";
+
+    public static final String SUGGEST_SAMPLING_QTY = "suggest_sampling_qty";
+
+    public static final String ATTACH_URL = "attach_url";
+
+    public static final String ATTACH_NAME = "attach_name";
+
+    @Override
+    public Serializable pkVal() {
+        return null;
+    }
+
+}
