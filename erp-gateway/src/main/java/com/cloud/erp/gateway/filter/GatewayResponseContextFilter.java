b6294f8ea2f0718b33061ecde559661eddca70be
吴天任 <981404651@qq.com>
2026-04-01 18:25:54 +0800

    修改请求头

--- erp-gateway/src/main/java/com/cloud/erp/gateway/filter/GatewayResponseContextFilter.java
+++ erp-gateway/src/main/java/com/cloud/erp/gateway/filter/GatewayResponseContextFilter.java
@@ -206,6 +206,7 @@ public class GatewayResponseContextFilter implements GlobalFilter, Ordered {
     private static Mono<Void> handleSseRequest(ServerWebExchange exchange, GatewayFilterChain chain) {
         HttpHeaders responseHeaders = exchange.getResponse().getHeaders();
         responseHeaders.setCacheControl(CacheControl.noCache());
+        responseHeaders.setContentType(MediaType.TEXT_EVENT_STREAM);
         return chain.filter(exchange);
     }
 }
