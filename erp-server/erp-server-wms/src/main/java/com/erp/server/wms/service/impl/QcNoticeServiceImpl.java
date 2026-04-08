3aa2d17543525eca0239eed5f2fccfc305e66c3d
吴天任 <981404651@qq.com>
2026-04-08 08:49:44 +0800

    修改合格率和不良率

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
@@ -1359,10 +1359,10 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
 
                 if (qcInfoView.getQcBadQty() > 0 && qcInfoView.getQcGoodQty() > 0) {
                     qcGoodRate = BigDecimal.valueOf(qcInfoView.getQcGoodQty())
-                            .divide(totalQty, 4, RoundingMode.HALF_UP)
+                            .divide(totalQty, 4, RoundingMode.HALF_UP);
 
                     qcBadRate = BigDecimal.valueOf(qcInfoView.getQcBadQty())
-                            .divide(totalQty, 4, RoundingMode.HALF_UP)
+                            .divide(totalQty, 4, RoundingMode.HALF_UP);
                 }
 
                 qcResultUpdateWrapper
