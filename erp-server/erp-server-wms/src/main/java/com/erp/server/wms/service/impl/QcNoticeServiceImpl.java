aee5dd3a7e97bde36bdc18b972b813adcc31289e
吴天任 <981404651@qq.com>
2026-04-08 12:56:10 +0800

    修改质检项目

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
@@ -634,7 +634,6 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
             }
 
             //质检项目
-            QcNoticeDTO.QcInspectItemAddDTO qcInspectItemAddDTO = new QcNoticeDTO.QcInspectItemAddDTO();
             QcStandardEntity qcStandardEntity = qcStandardService.lambdaQuery()
                     .eq(QcStandardEntity::getSkuId, qcNoticeDetail.getSkuId())
                     .one();
@@ -644,6 +643,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
                         .list();
                 if (!list.isEmpty()) {
                     for (QcStandardDetailEntity qcStandardDetailEntity : list) {
+                        QcNoticeDTO.QcInspectItemAddDTO qcInspectItemAddDTO = new QcNoticeDTO.QcInspectItemAddDTO();
                         qcInspectItemAddDTO.setInspectItem(qcStandardDetailEntity.getInspectItemName());
                         qcInspectItemAddDTO.setInspectRequirement(qcStandardDetailEntity.getInspectRequirement());
                         qcInspectItemAddDTOS.add(qcInspectItemAddDTO);
