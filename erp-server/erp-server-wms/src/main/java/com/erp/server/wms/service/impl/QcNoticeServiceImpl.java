218f332915fb6cca93193f4e324fa117e47f3d08
吴天任 <981404651@qq.com>
2026-04-13 09:41:08 +0800

    处理字段赋值

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
@@ -723,6 +723,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
             qcInfoView.setMajorRejectQty(samplingPlan.getMajorRejectQty());
 
             qcStandardView.setSamplingPlanId(samplingPlan.getId());
+            qcStandardView.setSamplingPlanCode(samplingPlan.getCode());
             qcStandardView.setSamplingPlanName(QcTypeEnum.getByCode(qcInfoView.getQcType()) + "抽样方案");
             qcStandardView.setSuggestSamplingQty(samplingPlan.getSampleQty());
 
@@ -867,6 +868,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
             qcInfoView.setMajorRejectQty(samplingPlan.getMajorRejectQty());
 
             qcStandardView.setSamplingPlanId(samplingPlan.getId());
+            qcStandardView.setSamplingPlanCode(samplingPlan.getCode());
             qcStandardView.setSamplingPlanName(QcTypeEnum.getByCode(qcInfoView.getQcType()) + "抽样方案");
             qcStandardView.setSuggestSamplingQty(samplingPlan.getSampleQty());
 
@@ -1922,6 +1924,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
                 planParamDTO.setSkuId(qcNoticeDetail.getSkuId());
 
                 SamplingPlanDTO.PlanDTO samplingPlan = qcSamplingPlanService.getSamplingPlan(planParamDTO);
+                printQcStandardDTO.setSamplingPlanCode(samplingPlan.getCode());
                 printQcStandardDTO.setSamplingPlanName(QcTypeEnum.getByCode(qcNoticeEntity.getQcType()) + "抽样方案");
                 printQcStandardDTO.setSuggestSamplingQty(samplingPlan.getSampleQty());
             }
