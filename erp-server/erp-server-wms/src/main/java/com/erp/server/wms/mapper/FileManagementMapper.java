78401c99eaa57abe79debbfe811f642552b84ce4
zdy <zhangdaoyin@ulanzi.cn>
2026-04-15 10:51:56 +0800

    文件管理-修改文件上传展示逻辑调整

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/FileManagementMapper.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/mapper/FileManagementMapper.java
@@ -34,10 +34,10 @@ public interface FileManagementMapper extends BaseMapper<FileManagementEntity> {
      *  根据sku和类型统计数量
      * @param skuIds
      * @param fileType
-     * @param id
+     * @param firstCategoryId
      * @return
      */
-    List<FileManagementDTO.CountDTO> countBySkuAndFileType(@Param("skuIds") List<String> skuIds, @Param("fileType")String fileType, @Param("id") String id);
+    List<FileManagementDTO.CountDTO> countBySkuAndFileType(@Param("skuIds") List<String> skuIds, @Param("fileType")String fileType, @Param("firstCategoryId") String firstCategoryId);
 
 
     FileManagementDTO.AttachDTO getCategoryGeneralStandardFileUrl(@Param("skuId") String skuId);
