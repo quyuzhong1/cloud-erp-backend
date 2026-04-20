94085d5bf43dd1efa2059627b94c28111348f379
shukai <shukai@ulanzi.cn>
2026-04-20 10:06:34 +0800

    add:指定数据源替换

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/config/MybatisArchiveInterceptor.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/config/MybatisArchiveInterceptor.java
@@ -11,6 +11,8 @@ import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Signature;
 import org.springframework.stereotype.Component;
 
+import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import com.common.business.enums.DynamicDataSourceTypeEnum;
 import com.common.business.enums.ServiceCodeNameEnum;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,17 +23,20 @@ import lombok.extern.slf4j.Slf4j;
 public class MybatisArchiveInterceptor implements Interceptor{
 	
     public Object intercept(Invocation invocation) throws Throwable {
-        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
-        BoundSql boundSql = statementHandler.getBoundSql();
-        Field field = boundSql.getClass().getDeclaredField("sql");
-        field.setAccessible(true);
-        field.set(boundSql, getNewSql(boundSql.getSql()));
+    	String dataSourceName = DynamicDataSourceContextHolder.peek();
+    	if(DynamicDataSourceTypeEnum.ARCHIVE_DORIS.getCode().equals(dataSourceName)) {
+    		StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+            BoundSql boundSql = statementHandler.getBoundSql();
+            Field field = boundSql.getClass().getDeclaredField("sql");
+            field.setAccessible(true);
+            field.set(boundSql, getNewSql(boundSql.getSql()));
+    	}
         return invocation.proceed();
     }
 
 	
 	private String getNewSql(String oldSql) {
-		log.debug("替换前sql语句{}" , oldSql);
+		log.debug("归档替换前sql语句{}" , oldSql);
 		String newSql = oldSql;
 		ServiceCodeNameEnum[] values = ServiceCodeNameEnum.values();
 		for(ServiceCodeNameEnum value : values) {
@@ -40,7 +45,7 @@ public class MybatisArchiveInterceptor implements Interceptor{
 				newSql = newSql.replace("erp_" + code + ".", "erp_" + code + "_archive.");
 			}
 		}
-		log.debug("替换后sql语句{}" , newSql);
+		log.debug("归档替换后sql语句{}" , newSql);
 		return newSql;
 	}
 
