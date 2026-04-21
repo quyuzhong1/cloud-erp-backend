02c03c8d094c1603b8f60344006d495eace0aa5f
jack chen <jackchen@ulanzi.cn>
2026-04-21 11:58:59 +0800

    fix(logistics): track123注册传“查询CODE” + 1

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/listener/LogisticsThirdChannelRefListener.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/listener/LogisticsThirdChannelRefListener.java
@@ -216,8 +216,8 @@ public class LogisticsThirdChannelRefListener extends AnalysisEventListener<Impo
         }
 
         // 推送类型解析
-        String pushType = LogisticsThirdChannelRefPushTypeEnum.getName(excelDTO.getPushTypeName());
-        if (CharSequenceUtil.isNotBlank(excelDTO.getPushTypeName())) {
+        String pushType = LogisticsThirdChannelRefPushTypeEnum.getCodeByName(excelDTO.getPushTypeName());
+        if (CharSequenceUtil.isNotBlank(pushType)) {
             errorMsgList.add("推送类型不存在");
         }
         excelDTO.setPushType(pushType);
