45be1c1e123a964fe8126c33da2f780675778c4e
Will <wangwei@ulanzi.cn>
2026-04-22 17:09:46 +0800

    feat,拉取平台订单时物流单发货单号赋值

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoOutstockServiceImpl.java
@@ -1228,6 +1228,9 @@ public class SoOutstockServiceImpl extends SuperServiceImpl<SoOutstockMapper, So
             String orderType = entity.getOrderType();
             String b2cType = OrderTypeEnum.B2C.getCode();
             addDTO.setOrderType(orderType);
+            //发货单信息
+            addDTO.setSoDeliveryId(entity.getSourceId());
+            addDTO.setSoDeliveryCode(entity.getSourceCode());
             //表明是是b2b
             if (!b2cType.equals(orderType)) {
                 SoInfoDTO.CustomerDTO soInfo = soInfoFeign.getSoBaseById(soId);
