5884d2dbe81a2524f7dde6167513ca58f56fcaf1
zdy <zhangdaoyin@ulanzi.cn>
2026-04-22 14:39:26 +0800

    增加奇门回调接口-测试使用，后面删除

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/factory/WebhookHandlerFactory.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/factory/WebhookHandlerFactory.java
@@ -23,6 +23,8 @@ public class WebhookHandlerFactory {
                 return new InboundHandler();
             case "returnInstock":
                 return new ReturnInstockHandler();
+            case "qimenCallback":
+                return new QimenCallbackHandler();
             default:
                 throw new IllegalArgumentException("Unknown platform");
         }
