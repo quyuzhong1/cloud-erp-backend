ae46e6a72f8deb790b506c9883eb1a37d57a0d59
shukai <shukai@ulanzi.cn>
2026-04-24 10:35:35 +0800

    add:判断是否开启动态数据源

--- erp-common/erp-common-business/src/main/java/com/common/business/filter/DynamicDataSourceFilter.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/filter/DynamicDataSourceFilter.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -40,7 +39,7 @@ public class DynamicDataSourceFilter implements Filter {
                          FilterChain chain) throws IOException, ServletException {
     	String requestURI = "";
     	requestURI = ((HttpServletRequest) request).getRequestURI();
-    	if(BusinessCommonConstants.isArchive()) {
+    	if(BusinessCommonConstants.isArchive() && BusinessCommonConstants.isDynamicEnabled()) {
     		try {
         		DynamicDataSourceThreadLocal.set(DynamicDataSourceTypeEnum.ARCHIVE_DORIS);
 	            DynamicDataSourceContextHolder.push(DynamicDataSourceTypeEnum.ARCHIVE_DORIS.getCode());
