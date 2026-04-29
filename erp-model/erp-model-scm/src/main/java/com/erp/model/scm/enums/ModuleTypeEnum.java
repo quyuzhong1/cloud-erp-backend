467031f374ee88e9a7a9c9f3e124e9193332975a
lei.nie <1250861896@qq.com>
2026-04-29 16:39:17 +0800

    修改编码

--- erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
+++ erp-model/erp-model-scm/src/main/java/com/erp/model/scm/enums/ModuleTypeEnum.java
@@ -211,7 +211,7 @@ public enum ModuleTypeEnum {
     QC_APPLICATION("163", "质检申请单"),
     FILE_MANAGEMENT("164", "文件管理"),
 
-    LOGISTICS_ORDER("165", "物流下单"),
+    LOGISTICS_ORDER("168", "物流下单"),
 
 
 
