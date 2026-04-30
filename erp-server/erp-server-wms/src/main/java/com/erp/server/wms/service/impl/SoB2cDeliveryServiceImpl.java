8cc57c9ca76e4317ea5caf22c90ae530d2886e54
zdy <zhangdaoyin@ulanzi.cn>
2026-04-30 17:59:02 +0800

    中转报关-入库预报兼容空数据情况

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/SoB2cDeliveryServiceImpl.java
@@ -511,9 +511,9 @@ public class SoB2cDeliveryServiceImpl extends SuperServiceImpl<SoB2cDeliveryMapp
         List<SoB2cDeliveryDTO.AllocateCargoViewDTO> viewList = new ArrayList<>();
         List<PickingListsEntity> list = pickingListsService.list(Wrappers.<PickingListsEntity>lambdaQuery().in(PickingListsEntity::getSourceId, deliveryIds));
         List<String> pickingIds = list.stream().map(PickingListsEntity::getId).collect(Collectors.toList());
-        List<PickingDetailEntity> pickingDetails = pickingDetailService.list(Wrappers.<PickingDetailEntity>lambdaQuery().in(PickingDetailEntity::getMainId, pickingIds));
+        List<PickingDetailEntity> pickingDetails = CollUtil.isNotEmpty(pickingIds) ? pickingDetailService.list(Wrappers.<PickingDetailEntity>lambdaQuery().in(PickingDetailEntity::getMainId, pickingIds)) : new ArrayList<>();
         List<String> skuIds = pickingDetails.stream().map(PickingDetailEntity::getSkuId).distinct().collect(Collectors.toList());
-        List<SkuVO> skuVOList = plmTaskFeign.listSkuProductByIds(skuIds);
+        List<SkuVO> skuVOList = CollUtil.isNotEmpty(skuIds) ? plmTaskFeign.listSkuProductByIds(skuIds) : new ArrayList<>();
         List<PrintWayBillPdfDetailDTO> allDetailDTOList = new ArrayList<>();
         for (PickingDetailEntity pickingDetail : pickingDetails) {
             PickingListsEntity pickingLists = list.stream()
