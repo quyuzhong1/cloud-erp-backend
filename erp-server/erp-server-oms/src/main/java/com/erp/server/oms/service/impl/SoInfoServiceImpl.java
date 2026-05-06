b55360ff4eb9b338bd08fc36ae0bf69ff0496c79
Will <wangwei@ulanzi.cn>
2026-05-06 15:38:24 +0800

    hotfix,合并错误调整

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/SoInfoServiceImpl.java
@@ -3804,7 +3804,7 @@ public class SoInfoServiceImpl extends SuperServiceImpl<SoInfoMapper, SoInfoEnti
                 }
                 //如果是审核中，撤销审核
                 if(BillApproveStatusEnum.APPROVE_ING.equals(exist.getApproveStatus())){
-                    this.cancelProcess(Arrays.asList(exist.getId()));
+                    this.cancelProcess(new ApproveDTO.BatchCancelProcessDTO(Collections.singletonList(exist.getId())));
                 }
                 //如果是已审核，反审核
                 if(BillApproveStatusEnum.APPROVE.equals(exist.getApproveStatus())){
