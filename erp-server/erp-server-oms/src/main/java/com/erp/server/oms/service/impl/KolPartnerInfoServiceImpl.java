c7106324d6ae642df3ebf82647635b3d64bc35ae
lei.nie <1250861896@qq.com>
2026-05-06 17:32:02 +0800

    修改测试问题

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/KolPartnerInfoServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/KolPartnerInfoServiceImpl.java
@@ -895,6 +895,7 @@ public class KolPartnerInfoServiceImpl extends SuperServiceImpl<KolPartnerInfoMa
             AddressParseDTO.BatchParseResultDTO batchParseResultDTO = new AddressParseDTO.BatchParseResultDTO();
             BeanMapperUtils.copy(resultDTO, batchParseResultDTO);
             batchParseResultDTO.setId(dto.getId());
+            batchParseResultDTO.setDetailAddress(resultDTO.getDistrict() + resultDTO.getDetailAddress());
             resultDTOS.add(batchParseResultDTO);
         });
         return resultDTOS;
