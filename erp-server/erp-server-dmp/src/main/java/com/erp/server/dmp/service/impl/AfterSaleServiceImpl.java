48c77f5e1a065926a98fb27b43e3a2027044568c
lei.nie <1250861896@qq.com>
2026-05-07 15:38:22 +0800

    修改提示语

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/AfterSaleServiceImpl.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/service/impl/AfterSaleServiceImpl.java
@@ -1777,7 +1777,7 @@ public class AfterSaleServiceImpl extends SuperServiceImpl<AfterSaleMapper, Afte
                 if (dmpAttachmentEntity != null) {
                     attachmentService.removeById(dmpAttachmentEntity.getId());
                 }
-                cancelResult = BatchResultDTO.success(afterSaleEntity.getId(), afterSaleEntity.getCode(), "");
+                cancelResult = BatchResultDTO.success(afterSaleEntity.getId(), afterSaleEntity.getCode(), "操作成功");
                 resultList.add(cancelResult);
             }
         }
