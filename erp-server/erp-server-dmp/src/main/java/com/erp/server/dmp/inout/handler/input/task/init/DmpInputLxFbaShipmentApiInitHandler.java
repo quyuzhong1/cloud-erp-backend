670bebed4869887b9310dcb05f08b4c0e0786111
吴天任 <981404651@qq.com>
2026-05-08 12:38:28 +0800

    恢复领星代码

--- erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputLxFbaShipmentApiInitHandler.java
+++ erp-server/erp-server-dmp/src/main/java/com/erp/server/dmp/inout/handler/input/task/init/DmpInputLxFbaShipmentApiInitHandler.java
@@ -153,8 +153,7 @@ public class DmpInputLxFbaShipmentApiInitHandler extends DmpInputInitHandler {
 
         List<JSONObject> allResultList = extractShipmentList(resultData.getData());
         allResultList.forEach(e -> e.put("shopId", shopId));
-        //return Collections.singletonList(DmpInputTaskInitDTO.initMsg(JSON.toJSONString(allResultList)));
-        return new ArrayList<DmpInputTaskInitDTO>();
+        return Collections.singletonList(DmpInputTaskInitDTO.initMsg(JSON.toJSONString(allResultList)));
     }
 
     /**
