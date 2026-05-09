1a9a6d3562ca183208b05a10997596f9b2ce653e
liuruipeng <arthur@ulanzi.cn>
2026-05-09 15:13:52 +0800

    迭代26.5.1 新增批量获取已完结任务的备注 openApi

--- erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/feign/ProcessTaskManagementFeignController.java
+++ erp-server/erp-server-workflow/src/main/java/com/erp/server/workflow/controller/feign/ProcessTaskManagementFeignController.java
@@ -1,13 +1,17 @@
 package com.erp.server.workflow.controller.feign;
 
+import com.common.business.dto.base.BaseIdsDTO;
 import com.erp.model.workflow.dto.ProcessTaskManagementDTO;
 import com.erp.server.workflow.service.ProcessTaskManagementService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 import java.util.List;
 
 /**
@@ -24,5 +28,13 @@ public class ProcessTaskManagementFeignController {
     @GetMapping("/listApproveHistory")
     public List<ProcessTaskManagementDTO.ApproveHistoryDTO> listApproveHistory(@RequestParam String businessId){
         return processTaskManagementService.listApproveHistory(businessId);
+    }
+
+    /**
+     * 批量获取已完结任务的备注
+     */
+    @PostMapping("/finishedRemark")
+    public List<ProcessTaskManagementDTO.FinishedRemarkDTO> finishedRemark(@RequestBody @Valid BaseIdsDTO.IdsDTO dto) {
+        return processTaskManagementService.listFinishedTaskRemarksBatch(dto.getIds());
     }
 }
