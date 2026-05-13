19a1368a11701c459eeb52d0a922298e35dd5f50
zdy <zhangdaoyin@ulanzi.cn>
2026-05-13 09:31:44 +0800

    飞书质检单改造高级搜索条件与web端一致

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcInfoServiceImpl.java
@@ -3260,11 +3260,10 @@ public class QcInfoServiceImpl extends SuperServiceImpl<QcInfoMapper, QcInfoEnti
     }
 
     @Override
-    public PagingVO<QcInfoDTO.OpenPagingViewDTO> qcPaging(PagingDTO<QcInfoDTO.OpenPagingParamDTO> dto) {
+    public PagingVO<QcInfoDTO.OpenPagingViewDTO> qcPaging(PagingDTO<QcInfoDTO.PagingParamDTO> dto) {
+        QcInfoDTO.PagingParamDTO params = dto.getParams();
+        params.setPermissionSql(dto.getPermissionSql());
         Page query = new Page(dto.getCurrPage(), dto.getPageSize());
-        QcInfoDTO.OpenPagingParamDTO params = dto.getParams();
-        //构建查询条件
-        buildOpenPagingQuery(params);
         IPage pageData = baseMapper.qcPaging(query, params);
         List<QcInfoDTO.OpenPagingViewDTO> list = pageData.getRecords();
         if (CollectionUtils.isEmpty(list)) {
