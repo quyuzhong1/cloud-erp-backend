1418e83d247b3d212f55b851b1e4afc90c84e32d
Will <wangwei@ulanzi.cn>
2026-05-14 10:04:01 +0800

    feat，质检相关字段名称调整

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/QcInfoDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/QcInfoDTO.java
@@ -18,10 +18,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -1078,6 +1075,10 @@ public class QcInfoDTO implements Serializable {
          * 来源编号
          */
         private String sourceCode;
+        /**
+         * 允许入库量
+         */
+        private Integer allowInstockQty;
     }
 
     @Data
