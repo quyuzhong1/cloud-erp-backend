79ef443505c98c537c658c17e1b570046e55fafb
吴天任 <981404651@qq.com>
2026-05-15 16:29:11 +0800

    处理下推采购订单赋值问题

--- erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderDetailServiceImpl.java
+++ erp-server/erp-server-scm/src/main/java/com/erp/server/scm/service/impl/PurchaseOrderDetailServiceImpl.java
@@ -413,8 +413,7 @@ public class PurchaseOrderDetailServiceImpl extends SuperServiceImpl<PurchaseOrd
                             && obj.getSupplierId().equals(supplierEntity.getSupplierId())
                             && StrUtil.equals(obj.getPurchaseOrgId(),entity.getPurchaseOrgId())).findFirst().orElse(null);
             if (PurchaseOrderTypeEnum.ENUM_PURCHASE.getCode().equals(entity.getType())
-                    || PurchaseOrderTypeEnum.ENUM_SUBCONTRACT.getCode().equals(entity.getType())
-                    ||PurchaseOrderTypeEnum.ENUM_REPAIR.getCode().equals(entity.getType())){
+                    || PurchaseOrderTypeEnum.ENUM_SUBCONTRACT.getCode().equals(entity.getType())){
                 //委外成品时，取委外订单中的含税单价
                 if (PurchaseOrderTypeEnum.ENUM_SUBCONTRACT.getCode().equals(entity.getType()) && SubcontractTypeEnum.ENUM_PARENT.getCode().equals(entity.getSubcontractType())){
                     //sku是组合品时，取委外订单含税单价
