557aa45859966b9cbb1de8a2a5ae98efcf8eddf2
吴天任 <981404651@qq.com>
2026-05-15 09:30:04 +0800

    增加任务动态配置

--- erp-common/erp-common-business/src/main/java/com/common/business/enums/SourceTypeEnum.java
+++ erp-common/erp-common-business/src/main/java/com/common/business/enums/SourceTypeEnum.java
@@ -74,6 +74,7 @@ public enum SourceTypeEnum {
     FBA_SHIPMENT("fbaShipment", "FBA货件","fba_shipment"),
     AWD_SHIPMENT("awdShipment", "AWD货件","fba_shipment"),
     FBA_SHIPMENT_DETAIL("fbaShipmentDetail", "FBA货件明细","fba_shipment_detail"),
+    FBA_INBOUND_PLANS("fbaInboundPlans", "FBA入库货件列表","fba_inbound_plans"),
     FIRST_MILE_DELIVERY("firstMileDelivery", "头程发货单", "first_mile_delivery"),
     //当前仓可用减少，中转仓冻结增加
     FIRST_MILE_DELIVERY_TRANSFER_TO_THIRD("firstMileDeliveryTransferToThird", "头程发货单-中转（三方仓发三方仓）", "first_mile_delivery"),
