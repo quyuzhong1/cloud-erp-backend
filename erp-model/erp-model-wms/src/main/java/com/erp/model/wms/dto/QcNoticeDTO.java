df8ad5305cd7b4f032452286dc6c31804b600d43
Will <wangwei@ulanzi.cn>
2026-05-19 12:11:06 +0800

    hotfix,质检措施调整

--- erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/QcNoticeDTO.java
+++ erp-model/erp-model-wms/src/main/java/com/erp/model/wms/dto/QcNoticeDTO.java
@@ -543,6 +543,12 @@ public class QcNoticeDTO implements Serializable {
         private Integer majorRejectQty;
 
         /**
+         * 处理措施
+         */
+        @NotBlank(message = "处理措施不能为空")
+        private String handleModeDict;
+
+        /**
          *良品数量
          */
         @NotNull(message = "良品数量不能为空")
--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/QcNoticeServiceImpl.java
@@ -1231,7 +1231,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
                 qcResult.setQcQty(qcResultView.getQcQty());
                 qcResult.setQcResult(qcResultView.getQcResult());
                 qcResult.setLotQualifiedQty(qcResultView.getLotQualifiedQty());
-
+                qcResult.setHandleModeDict(qcResultView.getHandleModeDict());
                 if (Objects.nonNull(qcResultView.getQcBadQty()) && qcResultView.getQcBadQty() >= 0
                         && Objects.nonNull(qcResultView.getQcGoodQty()) && qcResultView.getQcGoodQty() >= 0) {
 
@@ -1582,6 +1582,7 @@ public class QcNoticeServiceImpl extends SuperServiceImpl<QcNoticeMapper, QcNoti
                     qcResult.setQcBadQty(badQty);
                     qcResult.setQcGoodRate(qcGoodRate);
                     qcResult.setQcBadRate(qcBadRate);
+                    qcResult.setHandleModeDict(qcInfoView.getHandleModeDict());
                 } else {
                     throw new ServiceException(ApiError.PO_QC_QTY_NOT_ALLOW_LESS_THAN_ZERO);
                 }
