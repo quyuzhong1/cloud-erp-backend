497dae01cd8c04a342a4eae10046d032d88d694c
jack chen <jackchen@ulanzi.cn>
2026-05-25 14:40:57 +0800

    feat(plm): 解决冲突

--- erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/SkuStdCostDetailService.java
+++ erp-server/erp-server-plm/src/main/java/com/erp/server/plm/service/SkuStdCostDetailService.java
@@ -99,14 +99,6 @@ public interface SkuStdCostDetailService extends SuperService<SkuStdCostDetailEn
     List<BatchResultDTO> autoFetchBatch(SkuStdCostDTO.AutoFetchBatchDTO dto);
 
 
-    /**
-     * 批量自动获取标准成本
-     *
-     * @param dto
-     * @return
-     */
-    List<BatchResultDTO> autoFetchBatch(SkuStdCostDTO.AutoFetchBatchDTO dto);
-
 
     /**
      * 修改
