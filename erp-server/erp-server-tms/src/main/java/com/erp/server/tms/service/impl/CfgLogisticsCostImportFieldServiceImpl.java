5293ae3b76ff81ddaee92ef9ee643ee2aeacee76
jack chen <jackchen@ulanzi.cn>
2026-05-26 11:17:56 +0800

    feat(tms): 添加物流费用导入字段的查询选项关联功能

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/CfgLogisticsCostImportFieldServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/CfgLogisticsCostImportFieldServiceImpl.java
@@ -1,17 +1,24 @@
 package com.erp.server.tms.service.impl;
 
+import cn.hutool.core.collection.CollUtil;
+import com.common.business.wrapper.FeignQuery;
+import com.erp.model.sys.dto.CfgQueryConditionDTO;
+import com.erp.model.sys.entity.CfgQueryOptionEntity;
 import com.erp.model.tms.entity.CfgLogisticsCostImportFieldEntity;
 import com.erp.model.tms.enums.CfgLogisticsCostImportFieldFieldTypeEnum;
 import com.erp.server.tms.mapper.CfgLogisticsCostImportFieldMapper;
 import com.erp.server.tms.service.CfgLogisticsCostImportFieldService;
 import com.common.business.service.impl.SuperServiceImpl;
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
 import com.erp.model.tms.dto.CfgLogisticsCostImportFieldDTO;
 import java.util.stream.Collectors;
 import java.util.*;
 import com.common.core.utils.*;
+
+import static jdk.nashorn.internal.objects.NativeArray.forEach;
 
 /**
  * <p>
@@ -29,6 +36,18 @@ public class CfgLogisticsCostImportFieldServiceImpl extends SuperServiceImpl<Cfg
     @Override
     public List<CfgLogisticsCostImportFieldDTO.ListDTO> listByBusinessType(String businessType) {
         List<CfgLogisticsCostImportFieldDTO.ListDTO> list = this.baseMapper.listByBusinessType(businessType);
+
+        List<String> queryOptionIds = list.stream().map(CfgLogisticsCostImportFieldDTO.ListDTO::getQueryOptionId).filter(StringUtils::isNotBlank).collect(Collectors.toList());
+        if(CollUtil.isNotEmpty(queryOptionIds)){
+            List<CfgQueryOptionEntity> cfgQueryOptionEntities = FeignQuery.create(CfgQueryOptionEntity.class).in(CfgQueryOptionEntity::getId, queryOptionIds).list();
+            if(CollUtil.isNotEmpty(cfgQueryOptionEntities)){
+                for (CfgLogisticsCostImportFieldDTO.ListDTO listDTO : list) {
+                    if(StringUtils.isNotBlank(listDTO.getQueryOptionId())){
+                        listDTO.setQueryOptionItem(cfgQueryOptionEntities.stream().filter(item -> item.getId().equals(listDTO.getQueryOptionId())).findFirst().orElse(null));
+                    }
+                }
+            }
+        }
         list.forEach(e -> e.setFieldTypeName(CfgLogisticsCostImportFieldFieldTypeEnum.getName(e.getFieldType())));
         return list;
     }
@@ -46,7 +65,6 @@ public class CfgLogisticsCostImportFieldServiceImpl extends SuperServiceImpl<Cfg
                     item.setChildrenList(getChildren(item, feildList, flagList));
                     return item;
                 }).collect(Collectors.toList());
-
         return treeList;
     }
 
