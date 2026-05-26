176b36ee1d9c17a97b91d52e29f33ffdb81a6add
Will <wangwei@ulanzi.cn>
2026-05-26 10:16:41 +0800

    feature,小包费用分摊定时任务查询需要分摊物流费用查询调整

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
@@ -64,8 +64,6 @@ import com.erp.server.tms.query.LogisticsBillCostQueryHandler;
 import com.erp.server.tms.query.LogisticsLastMileCostQueryHandler;
 import com.erp.server.tms.service.*;
 import io.seata.spring.annotation.GlobalTransactional;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -2431,6 +2429,11 @@ public class LogisticsBillCostServiceImpl extends SuperServiceImpl<LogisticsBill
             updateCount += baseMapper.batchConfirmImport(batch, reconciliationStatus, confirmUserId, confirmUserName);
         }
         log.info("导入确认批量更新完成，入参条数：{}，去重后条数：{}，更新条数：{}", confirmList.size(), distinctConfirmList.size(), updateCount);
+    }
+
+    @Override
+    public List<LogisticsBillCostEntity> listNeedPushAllocation(LogisticsBillCostDTO.NeedPushAllocationParamDTO paramDTO) {
+        return baseMapper.listNeedPushAllocation(paramDTO);
     }
 
     @Override
