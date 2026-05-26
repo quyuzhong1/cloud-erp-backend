e06b41759d5ebb38f240c891410319d204690a07
zdy <zhangdaoyin@ulanzi.cn>
2026-05-26 19:35:11 +0800

    优化尾程分页查询速度-按照规范修改

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/LogisticsBillCostDTO.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/LogisticsBillCostDTO.java
@@ -85,14 +85,6 @@ public class LogisticsBillCostDTO implements Serializable {
          * 动态数据源，需要重新get方法
          */
         private String dynamicDataSource;
-
-        //dynamicDataSource需要重新此方法
-        public String getDynamicDataSource(){
-            if(StringUtils.isNotBlank(dynamicDataSource) && dynamicDataSource.toUpperCase().contains(DynamicDataSourceTypeEnum.DORIS.getCode().toUpperCase())) {
-                return DynamicDataSourceTypeEnum.DORIS.getCode();
-            }
-            return dynamicDataSource;
-        }
     }
 
 
