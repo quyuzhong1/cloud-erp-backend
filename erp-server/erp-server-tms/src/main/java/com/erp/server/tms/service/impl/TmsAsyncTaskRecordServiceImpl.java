692d09f3f8515ab09690044681af324f37e990bb
zdy <zhangdaoyin@ulanzi.cn>
2026-05-28 19:49:22 +0800

    合并问题处理

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsAsyncTaskRecordServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/TmsAsyncTaskRecordServiceImpl.java
@@ -47,6 +47,7 @@ import org.apache.rocketmq.client.producer.SendStatus;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
