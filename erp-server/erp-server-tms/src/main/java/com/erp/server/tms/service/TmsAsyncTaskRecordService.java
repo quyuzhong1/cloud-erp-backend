9c2e543c3b4b8c6fb0e5853358905d50df3c0519
wuhaotian <wuhaotian@ulanzi.cn>
2026-05-28 20:05:39 +0800

    uat(tms),合并问题处理

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/TmsAsyncTaskRecordService.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/TmsAsyncTaskRecordService.java
@@ -55,10 +55,6 @@ public interface TmsAsyncTaskRecordService extends SuperService<TmsAsyncTaskReco
      */
     void startTask();
 
-    /**
-     * 实现类需使用独立事务领取并派发任务。
-     */
-    void claimAndDispatch(TmsAsyncTaskRecordEntity entity);
 
     void genAutoTask();
 }
