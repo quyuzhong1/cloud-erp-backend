b1ed0838f715e49d58b03ed06468b85c9748e763
jack chen <jackchen@ulanzi.cn>
2026-06-10 18:04:59 +0800

    feat(tms): 恢复物流费用标准导入新增按新单功能

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/LogisticsBillCostServiceImpl.java
@@ -1426,7 +1426,8 @@ public class LogisticsBillCostServiceImpl extends SuperServiceImpl<LogisticsBill
 
             List<String> errorMsgList = new ArrayList<>();
             List<LogisticsBillDTO.LogisticsBillVo> logisticsBillVoList = groupLogisticsBillVoMap.getOrDefault(entry.getKey(), Collections.emptyList());
-            if (CollUtil.isEmpty(logisticsBillVoList)) {
+            boolean importAddNew =Objects.equals("import_add_new",importType);
+            if (CollUtil.isEmpty(logisticsBillVoList) && !importAddNew) {
                 errorMsgList.add("未找到对应物流单");
             }
             if (CollectionUtils.isNotEmpty(errorMsgList)) {
