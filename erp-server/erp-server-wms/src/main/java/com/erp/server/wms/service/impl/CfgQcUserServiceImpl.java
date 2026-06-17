5a8118ee0ab68651cb3a3b49a2440fd451ad2724
吴天任 <981404651@qq.com>
2026-06-17 14:35:27 +0800

    代码审查问题

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/CfgQcUserServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/CfgQcUserServiceImpl.java
@@ -46,7 +46,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
