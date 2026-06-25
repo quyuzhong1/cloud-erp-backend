105e9fb940e021026e0ff168ceb796a06142f706
wangwei <wangwei95912@163.com>
2026-06-25 10:26:50 +0800

    hotfix,物流费用项导入查询物流费用项金额位数校验调整

--- erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/TmsCostDetailDTO.java
+++ erp-model/erp-model-tms/src/main/java/com/erp/model/tms/dto/TmsCostDetailDTO.java
@@ -334,7 +334,7 @@ public class TmsCostDetailDTO implements Serializable {
         /**
          * 费用值
          */
-        @FieldValid(fieldName = "费用值",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL)
+        @FieldValid(fieldName = "费用值",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL6)
         private String costValue;
 
     }
@@ -347,13 +347,13 @@ public class TmsCostDetailDTO implements Serializable {
         /**
          * 实际金额
          */
-        @FieldValid(fieldName = "实际金额",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL)
+        @FieldValid(fieldName = "实际金额",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL6)
         private String actualAmount;
 
         /**
          * 预估金额
          */
-        @FieldValid(fieldName = "预估金额",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL)
+        @FieldValid(fieldName = "预估金额",formatPattern = FieldFormatPatternTypeEnum.AMOUNT_NORMAL6)
         private String estimatedAmount;
 
     }
