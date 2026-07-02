0c2debf5bbbef2a9e541f1d4049328c63ca7edca
liuruipeng <arthur@ulanzi.cn>
2026-07-02 21:32:02 +0800

    启动循环依赖处理

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FirstMileDeliveryServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/FirstMileDeliveryServiceImpl.java
@@ -142,6 +142,7 @@ public class FirstMileDeliveryServiceImpl extends SuperServiceImpl<FirstMileDeli
     private MachineInfoService machineInfoService;
     @Resource
     private InventoryService inventoryService;
+    @Lazy
     @Resource
     private FbaShipmentService fbaShipmentService;
     @Resource
