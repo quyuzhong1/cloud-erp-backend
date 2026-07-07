f6098f4bad541342d66f1c9822930104d6d5dc12
jack chen <jackchen@ulanzi.cn>
2026-07-07 17:23:58 +0800

    Revert "refactor(tms): 优化小包成本分摊服务异步处理+1"
    This reverts commit e799ba42fd3dc8d09d7eccb363ad4c7b382d4ce3.

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/SmallBagCostAllocationServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/SmallBagCostAllocationServiceImpl.java
@@ -51,7 +51,7 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 /**
@@ -99,9 +99,9 @@ public class SmallBagCostAllocationServiceImpl extends SuperServiceImpl<SmallBag
     @Resource
     private DmpTaskFeign dmpTaskFeign;
 
-	@Autowired
+	@Resource
 	@Qualifier("costAllocationPool")
-	private ExecutorService costAllocationPool;
+	private ThreadPoolExecutor costAllocationPool;
 
     @GlobalTransactional(rollbackFor = Exception.class, timeoutMills = 120000)
     @Transactional(rollbackFor = Exception.class)
