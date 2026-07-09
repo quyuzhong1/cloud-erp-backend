6ca718e959ef5325c75d3e98b8a2af9e8b17254c
wangwei <wangwei95912@163.com>
2026-07-09 14:20:42 +0800

    feature：删除测试标识

--- erp-server/erp-server-auth/src/main/java/com/erp/server/auth/server/LoginAuthService.java
+++ erp-server/erp-server-auth/src/main/java/com/erp/server/auth/server/LoginAuthService.java
@@ -1,22 +1,15 @@
 package com.erp.server.auth.server;
 
-import java.util.Date;
-import java.util.Objects;
-
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-
-import com.common.business.vo.LoginUser;
-import com.common.core.utils.MessageUtils;
-import org.springframework.stereotype.Component;
-
+import cn.hutool.core.util.StrUtil;
 import com.common.business.constant.RedisCacheConstants;
 import com.common.business.enums.UserTypeEnum;
 import com.common.business.utils.RedisUtil;
+import com.common.business.vo.LoginUser;
 import com.common.core.controller.vo.ApiResult;
 import com.common.core.enums.ApiError;
 import com.common.core.exception.ServiceException;
 import com.common.core.utils.IpUtils;
+import com.common.core.utils.MessageUtils;
 import com.erp.model.scm.entity.SupplierEntity;
 import com.erp.model.sys.dto.AccountLoginDTO;
 import com.erp.model.sys.dto.SysFeignDTO;
@@ -27,8 +20,12 @@ import com.erp.model.sys.vo.SysUserMenuAuthVO;
 import com.erp.model.sys.vo.SysUserPermissionAuthVO;
 import com.erp.rpc.scm.feign.SupplierFeign;
 import com.erp.rpc.sys.feign.SysUserFeign;
+import org.springframework.stereotype.Component;
 
-import cn.hutool.core.util.StrUtil;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Objects;
 
 /**
  * 登录认证服务
@@ -124,16 +121,10 @@ public class LoginAuthService {
         ipDTO.setUid(info.getUid());
         sysUserFeign.setLoginIp(ipDTO);
         SysLoginUserVO sysLoginUserVO = new SysLoginUserVO();
-        if (loginDTO.getIsTest()) {
-            // 创建token（仅缓存用户基础信息，菜单与权限由独立接口获取）
-            info.setOverallMenuList(null);
-            info.setLeftMenuList(null);
-            info.setPermissionList(null);
-        } else {
-            sysLoginUserVO.setOverallMenuList(info.getOverallMenuList());
-            sysLoginUserVO.setPermissionList(info.getPermissionList());
-            sysLoginUserVO.setLeftMenuList(info.getLeftMenuList());
-        }
+        // 创建token（仅缓存用户基础信息，菜单与权限由独立接口获取）
+        info.setOverallMenuList(null);
+        info.setLeftMenuList(null);
+        info.setPermissionList(null);
         info.setUserType(loginDTO.getUserType());
         String accessToken = authTokenService.createSlimToken(info);
         sysLoginUserVO.setAccessToken(accessToken);
