12bd23e0d1166d716770cf06d142d5f41e79eb54
wuhaotian <wuhaotian@ulanzi.cn>
2026-07-13 11:10:54 +0800

    feat(tms),合并修改

--- erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
+++ erp-server/erp-server-wms/src/main/java/com/erp/server/wms/service/impl/RequisitionApplicationServiceImpl.java
@@ -155,9 +155,6 @@ public class RequisitionApplicationServiceImpl extends SuperServiceImpl<Requisit
     private DmpMqFeign dmpMqFeign;
 
     @Resource
-    private WmsAttachmentService wmsAttachmentService;
-
-    @Resource
     private SkuMappingFeign skuMappingFeign;
 
     @Resource
