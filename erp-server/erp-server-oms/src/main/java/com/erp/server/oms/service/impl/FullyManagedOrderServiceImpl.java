5ef2a243822b8858133cdcfcd524aaf2799fd686
zdy <zhangdaoyin@ulanzi.cn>
2026-07-15 09:49:30 +0000

    细化部分未知异常提示语

--- erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/FullyManagedOrderServiceImpl.java
+++ erp-server/erp-server-oms/src/main/java/com/erp/server/oms/service/impl/FullyManagedOrderServiceImpl.java
@@ -487,7 +487,7 @@ public class FullyManagedOrderServiceImpl extends SuperServiceImpl<SoB2cMapper, 
         ValidList<ProcessManagementDTO.HistoryActivityDTO> dtoList = ids.stream().map(obj -> new ProcessManagementDTO.HistoryActivityDTO(SourceTypeEnum.TIK_TOK_FULLY.getCode(), obj)).collect(Collectors.toCollection(ValidList::new));
         ApiResult<List<ProcessManagementDTO.CurApproveInfoDTO>> listApiResult = workflowFeign.curApprover(dtoList);
         if (200 != listApiResult.getCode()) {
-            throw new ServiceException(new ApiResult(ApiError.HTTP_UNKNOWN.getCode(),listApiResult.getMsg()));
+            throw new ServiceException(ApiError.WF_CUR_APPROVER_QUERY_FAILED, listApiResult.getMsg());
         }
 
         List<String> codeList = new ArrayList<>();
