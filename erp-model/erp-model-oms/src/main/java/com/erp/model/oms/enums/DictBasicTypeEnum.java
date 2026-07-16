9f2d40089aee4c8c5f6a62bfe0c453d470d93c96
lei.nie <1250861896@qq.com>
2026-07-16 17:28:29 +0800

    修改需求优化

--- erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/DictBasicTypeEnum.java
+++ erp-model/erp-model-oms/src/main/java/com/erp/model/oms/enums/DictBasicTypeEnum.java
@@ -51,6 +51,7 @@ public enum DictBasicTypeEnum {
 
     /** WMS 虚拟仓渠道：店铺和军区均为全部时跳过重复绑定校验的平台编码 */
     VM_CHANNEL_SKIP_CHECK_PLATFORM("vmChannelSkipCheckPlatform", "虚拟仓渠道跳过重复校验平台"),
+    SKU_NO("skuNo", "skuNo"),
 
     ;
 
