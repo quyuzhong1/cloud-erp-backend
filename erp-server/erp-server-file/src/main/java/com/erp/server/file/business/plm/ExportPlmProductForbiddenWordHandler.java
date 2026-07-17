58347a89c5b25bb0e658b36554249ea9a553695c
wuhaotian <wuhaotian@ulanzi.cn>
2026-07-17 11:03:10 +0800

    feat(plm),报错修改

--- erp-server/erp-server-file/src/main/java/com/erp/server/file/business/plm/ExportPlmProductForbiddenWordHandler.java
+++ erp-server/erp-server-file/src/main/java/com/erp/server/file/business/plm/ExportPlmProductForbiddenWordHandler.java
@@ -3,11 +3,11 @@ package com.erp.server.file.business.plm;
 import com.common.business.dto.base.PagingDTO;
 import com.common.business.enums.FileTaskEventEnum;
 import com.common.business.vo.PagingVO;
+import com.erp.model.file.entity.FileTask;
 import com.erp.model.plm.dto.CfgProductForbiddenWordDTO;
 import com.erp.model.plm.dto.excel.CfgProductForbiddenWordExportExcelDTO;
 import com.erp.rpc.plm.feign.ExportPlmFeign;
 import com.erp.server.file.core.AbstractPageFileEventHandler;
-import com.erp.server.file.entity.FileTask;
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -27,12 +27,6 @@ public class ExportPlmProductForbiddenWordHandler extends AbstractPageFileEventH
     @Resource
     private ExportPlmFeign exportPlmFeign;
 
-    @Override
-    protected List<CfgProductForbiddenWordExportExcelDTO> getData(FileTask fileTask) {
-        CfgProductForbiddenWordDTO.PagingParamDTO dto = readValue(fileTask.getMetaInfo(), new TypeReference<CfgProductForbiddenWordDTO.PagingParamDTO>() {
-        });
-        return listSeqData(dto);
-    }
 
     @Override
     protected PagingVO<CfgProductForbiddenWordExportExcelDTO> getPageData(PagingDTO<CfgProductForbiddenWordDTO.PagingParamDTO> dto) {
