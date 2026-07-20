807e0b830226215c7458be44bef4315328e9f534
jack chen <jackchen@ulanzi.cn>
2026-07-20 17:38:35 +0800

    fix: 修复费用导入 costItem 字段重复取值 +2

--- erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/ImportHistoryRecordServiceImpl.java
+++ erp-server/erp-server-tms/src/main/java/com/erp/server/tms/service/impl/ImportHistoryRecordServiceImpl.java
@@ -2590,34 +2590,22 @@ public class ImportHistoryRecordServiceImpl extends SuperServiceImpl<ImportHisto
         if (ObjectUtil.isNotNull(detail.getMappingIndex())) {
             rowData.set(detail.getMappingIndex().toString(), text);
         }
-        if (CharSequenceUtil.isNotBlank(detail.getTargetField())) {
-            if(Objects.equals(detail.getTargetField(), COST_ITEM_FIELD)){
-                if(StringUtils.isNotBlank(detail.getTargetDetailField())){
-                    rowData.set(detail.getTargetDetailField(), text);
-                }
-            }else {
-                rowData.set(detail.getTargetField(), text);
-            }
+        if (CharSequenceUtil.isNotBlank(detail.getTargetField())
+                && !CharSequenceUtil.equals(COST_ITEM_FIELD, detail.getTargetField())) {
+            rowData.set(detail.getTargetField(), text);
         }
     }
 
     private String getPreparedValue(JSONObject rowData, CfgLogisticsCostImportDetailEntity detail) {
-        Object value = ObjectUtil.isNotNull(detail.getMappingIndex()) ? rowData.get(detail.getMappingIndex().toString()) : null;
+        Object value = ObjectUtil.isNotNull(detail.getMappingIndex())
+                ? rowData.get(detail.getMappingIndex().toString())
+                : null;
         if (ObjectUtil.isEmpty(value)
                 && CharSequenceUtil.isNotBlank(detail.getTargetField())
-                &&!CharSequenceUtil.equals(COST_ITEM_FIELD, detail.getTargetField())) {
+                && !CharSequenceUtil.equals(COST_ITEM_FIELD, detail.getTargetField())) {
             value = rowData.get(detail.getTargetField());
         }
-        if (ObjectUtil.isEmpty(value)
-                && CharSequenceUtil.isNotBlank(detail.getTargetField())
-                && CharSequenceUtil.equals(COST_ITEM_FIELD, detail.getTargetField())
-                && CharSequenceUtil.isNotBlank(detail.getTargetDetailField())) {
-            value = rowData.get(detail.getTargetDetailField());
-        }
-        if (ObjectUtil.isEmpty(value)) {
-            return "";
-        }
-        return String.valueOf(value);
+        return ObjectUtil.isEmpty(value) ? "" : String.valueOf(value);
     }
 
     private void standardizeImportRowWeightValues(List<CfgLogisticsCostImportDetailEntity> cfgImportDetailList,
